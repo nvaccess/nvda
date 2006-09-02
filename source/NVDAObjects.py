37c65260c9b09f0063cfe18c770d2957b9139b1a
mick <>
2006-09-02 09:47:45 +0000

    deleted _WwG entry in NVDAObjects classMap ... was a test, shouldn't have been committed

--- source/NVDAObjects.py
+++ source/NVDAObjects.py
@@ -769,7 +769,6 @@ class NVDAObject_mozillaText(NVDAObject):
 classMap={
 "Edit":NVDAObject_Edit,
 "RICHEDIT50W":NVDAObject_Edit,
-"_WwG":NVDAObject_Edit,
 "Button_44":NVDAObject_checkBox,
 "MozillaContentWindowClass_15":NVDAObject_mozillaDocument,
 "MozillaContentWindowClass_30":NVDAObject_mozillaLink,
