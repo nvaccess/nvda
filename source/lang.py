74d16045e696493b93cc63084638b4c5e7f1531a
mick <>
2006-11-12 00:51:26 +0000

    Step2 of dictionaries to lang. Now dictionaries is called lang. lang no longer needs to load files individually, import lang gets what it needs automatically. the .py files in dictionaries dir are now .enu files in lang directory. A new config section language is added with a value language = enu. You can now place other characterSymbols, roleNames, textSymbols or stateNames files ending in .<your language code> and lang will pick up the right ones.

--- source/lang.py
+++ source/lang.py
@@ -4,11 +4,13 @@ from constants import *
 from config import conf
 
 def load():
-	dirList=filter(lambda x: os.path.splitext('.\\lang')[1]==conf["language"]["language"],os.listdir('.\\lang'))
-	for item in dirList:    
-		f=open(".\\lang\\%s"%item)
+	l=os.listdir('lang')
+	l=filter(lambda x: os.path.splitext(x)[1]==".%s"%conf["language"]["language"],l)
+	debug.writeMessage("lang.load: dirList %s"%l)
+	for item in l:    
+		f=open("lang\\%s"%item)
 		try:
-			globals()[os.path.splitext('.\\lang\\%s'%item)[0]]=eval(f.read())
+			globals()[os.path.splitext('%s'%item)[0]]=eval(f.read())
 		except:
 			debug.writeException("lang.load: exception loading %s data"%item)
 
