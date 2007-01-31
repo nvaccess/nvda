8977461fa088812e20996b10c07306718d5dd2c1
mdcurran <>
2007-01-31 13:38:32 +0000

    Changed version in versionInfo.py to 'svn-trunk' I keep forgetting to change it, so, perhaps if we only set it when zipping a source or compiling a binary.

--- source/versionInfo.py
+++ source/versionInfo.py
@@ -6,7 +6,7 @@
 
 name="NVDA"
 longName=_("NonVisual Desktop Access")
-version="svn-r320"
+version="svn-trunk"
 description=_("A free and open-source screen reader for MS Windows")
 maintainer="Michael Curran"
 maintainer_email="mick@kulgan.net"
