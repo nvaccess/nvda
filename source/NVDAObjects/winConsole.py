c2a7ec77c47754d8b575d7f4f332d9e7f02808db
mdcurran <>
2007-03-01 03:54:20 +0000

    Possibly fixed a bug where NVDA would sometimes freeze or die when a dos console gets focus (example typing in a dos command in the run dialog box). NVDA no longer tries to free its console first - we really just have to assume it never had one. Also commented a few of the methods in NVDAObject_winConsole for better understandability.

--- source/NVDAObjects/winConsole.py
+++ source/NVDAObjects/winConsole.py
@@ -20,47 +20,54 @@ class NVDAObject_winConsole(IAccessible.NVDAObject_IAccessible):
 
 	def __init__(self,*args,**vars):
 		IAccessible.NVDAObject_IAccessible.__init__(self,*args,**vars)
-		self.consoleEventHookHandles=[]
-
-	text_caretSayAllGenerator=None
+		self.consoleEventHookHandles=[] #Holds the handles for all the win events we register so we can remove them later
 
 	def connectConsole(self):
+		#Get the process ID of the console this NVDAObject is fore
 		processID=self.windowProcessID[0]
-		try:
-			winKernel.freeConsole()
-		except:
-			debug.writeException("freeConsole")
-			pass
+		#Attatch NVDA to this console so we can access its text etc
 		winKernel.attachConsole(processID)
+		#Try and get the handle for this console's standard out
 		res=winKernel.getStdHandle(winKernel.STD_OUTPUT_HANDLE)
 		if not res:
 			raise OSError("NVDAObject_consoleWindowClassClient: could not get console std handle") 
 		self.consoleHandle=res
+		#Create a callback function which will be used to receive events for console updates etc
 		self.cConsoleEventHook=ctypes.CFUNCTYPE(ctypes.c_voidp,ctypes.c_int,ctypes.c_int,ctypes.c_int,ctypes.c_int,ctypes.c_int,ctypes.c_int,ctypes.c_int)(self.consoleEventHook)
+		#Register this callback with all the win events we need, storing the given handles for removal later
 		for eventID in [winUser.EVENT_CONSOLE_CARET,winUser.EVENT_CONSOLE_UPDATE_REGION,winUser.EVENT_CONSOLE_UPDATE_SIMPLE,winUser.EVENT_CONSOLE_UPDATE_SCROLL,winUser.EVENT_CONSOLE_LAYOUT]:
 			handle=winUser.setWinEventHook(eventID,eventID,0,self.cConsoleEventHook,0,0,0)
 			if handle:
 				self.consoleEventHookHandles.append(handle)
 			else:
 				raise OSError('Could not register console event %s'%eventID)
+		#Setup the monitoring thread which will watch a variable, and speak new text at the appropriate time
+		#Each event doesn't individually speak its own text since speaking text is quite intensive due to the diff algorithms  
 		self.keepMonitoring=True
 		self.lastConsoleEvent=None
 		thread.start_new_thread(self.monitorThread,())
 
 	def disconnectConsole(self):
+		#Unregister any win events we are using
 		for handle in self.consoleEventHookHandles:
 			winUser.unhookWinEvent(handle)
 		self.consoleEventHookHandles=[]
+		#Get ready to stop monitoring - give it a little time to finish
 		self.keepMonitoring=False
 		time.sleep(0.001)
+		#Get rid of the console handle we were holding
 		if hasattr(self,'consoleHandle'):
 			del self.consoleHandle
+		#Try freeing NVDA from this console
 		try:
 			winKernel.freeConsole()
 		except:
 			pass
 
 	def isConsoleDead(self):
+		#Every console should have at least one process associated with it
+		#This console should have two if NVDA is also connected
+		#if there is only one (it must be NVDA) so we free NVDA from it so it can close
 		num=winKernel.getConsoleProcessList((ctypes.c_int*2)(),2)
 		if num<2:
 			return True
@@ -68,39 +75,45 @@ class NVDAObject_winConsole(IAccessible.NVDAObject_IAccessible):
 			return False
 
 	def consoleEventHook(self,handle,eventID,window,objectID,childID,threadID,timestamp):
+		#We don't want to do anything with the event if the event is not for the window this console is in
 		if window!=self.windowHandle:
 			return
+		#Update the review cursor position with the caret position
 		self.text_reviewOffset=self.text_caretOffset
+		#For any events other than caret movement, we want to let the monitor thread know that there might be text to speak
 		if eventID!=winUser.EVENT_CONSOLE_CARET:
 			self.lastConsoleEvent=eventID
-		#audio.speakMessage("event %d"%eventID)
 
 	def monitorThread(self):
 		try:
-			#tones.beep(440,200)
 			update_timer=0
 			checkDead_timer=0
+			#Keep the thread alive while keepMonitoring is true - disconnectConsole will make it false if the focus moves away 
 			while self.keepMonitoring:
+				#If there has been a console event lately, remember it and reset the notification to None
 				if self.lastConsoleEvent:
 					consoleEvent=self.lastConsoleEvent
 					self.lastConsoleEvent=None
+					#When we know there's an event, we wait for some time befor acting on it, since there may be quite a few all in a row 
 					update_timer=1
 				if update_timer>0:
 					update_timer+=1
 				if update_timer>=4:
 					update_timer=0
+					#the timer got up to 4, so now we can collect the lines of the console and try and find out the new text
 					newLines=self.consoleVisibleLines
 					newText=self.calculateNewText(newLines,self.prevConsoleVisibleLines).strip()
 					if len(newText)>0 and (not consoleEvent==winUser.EVENT_CONSOLE_UPDATE_SIMPLE or (self.lastConsoleEvent or len(newText)>1)):
 						audio.speakText(newText)
 					self.prevConsoleVisibleLines=newLines
+				#Every 10 times we also make sure the console isn't dead, if so we need to stop the thread ourselves
 				if checkDead_timer>=10:
 					checkDead_timer=0
 					if self.isConsoleDead():
 						self.disconnectConsole()
 				checkDead_timer+=1
+				#Each round of the while loop we wait 10 milliseconds
 				time.sleep(0.01)
-			#tones.beep(880,200)
 		except:
 			debug.writeException("console monitorThread")
 
