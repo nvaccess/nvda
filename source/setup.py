481934907cbf48f63c3ed0cc330a265e8f298667
mdcurran <>
2007-03-08 07:17:19 +0000

    Final part for NVDA to have a console while starting for usage info etc. Now nvda.pyw is called nvda.py.

--- source/setup.py
+++ source/setup.py
@@ -29,7 +29,7 @@ setup(
 'Programming Language :: Python',
 'Operating System :: Microsoft :: Windows',
 ],
-	windows = ["nvda.pyw"],
+	windows = ["nvda.py"],
 	service=['nvda_service'],
 	options = {"py2exe": {"bundle_files": 3}},
 	zipfile = None,
