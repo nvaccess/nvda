e5848fa5c6351aea0b43ab83890b3aec5b663d62
jteh <>
2007-03-13 21:13:51 +0000

    Working scriptUI code\! This was failing because although windows were being shown in the GUI thread, they weren't being created in the GUI thread. Also, Destroy() needs to be called on the windows to stop them from hanging NVDA on exit.

--- source/gui/scriptUI.py
+++ source/gui/scriptUI.py
@@ -21,17 +21,23 @@ class MessageDialog(ModalDialog):
 		self._window = wx.MessageDialog(None, message, title, style)
 
 	def run_gui(self):
-		return self._window.ShowModal() in (wx.ID_OK, wx.ID_YES)
+		ret = self._window.ShowModal()
+		self._window.Destroy()
+		return ret in (wx.ID_OK, wx.ID_YES)
 
 class TextEntryDialog(ModalDialog):
 
 	def __init__(self, message, title=_("NVDA"), default="", style=wx.OK | wx.CANCEL, callback=None):
 		ModalDialog.__init__(self, callback)
-		self._window = wx.TextEntryDialog(None, message, title, defaultValue=default, style=style)
+		self._makeDialog = lambda: wx.TextEntryDialog(None, message, title, defaultValue=default, style=style)
 
 	def run_gui(self):
-		if self._window.ShowModal() == wx.ID_OK:
-			return self._window.GetValue()
+		dialog = self._makeDialog()
+		dialog.Raise()
+		ret = dialog.ShowModal()
+		dialog.Destroy()
+		if ret == wx.ID_OK:
+			return dialog.GetValue()
 		else:
 			return None
 
@@ -39,21 +45,31 @@ class SingleChoiceDialog(wx.Dialog, ModalDialog):
 
 	def __init__(self, message, title=_("NVDA"), choices=(), default=0, style=wx.OK|wx.CANCEL, callback=None):
 		ModalDialog.__init__(self, callback)
-		wx.Dialog.__init__(self, gui.mainFrame, -1, title)
-		mainSizer = wx.BoxSizer(wx.VERTICAL)
-		mainSizer.Add(wx.StaticText(self, -1, label=message))
-		self.list = wx.ListView(self, -1, style=wx.LC_LIST | wx.LC_SINGLE_SEL)
-		for index, choice in enumerate(choices):
-			self.list.InsertStringItem(index, choice)
-		self.list.Focus(default)
-		mainSizer.Add(self.list)
-		mainSizer.Add(self.CreateButtonSizer(style), flag=wx.BOTTOM)
-		mainSizer.Fit(self)
-		self.SetSizer(mainSizer)
+		def initDialog():
+			wx.Dialog.__init__(self, None, -1, title)
+			mainSizer = wx.BoxSizer(wx.VERTICAL)
+			mainSizer.Add(wx.StaticText(self, -1, label=message))
+			self.list = wx.ListView(self, -1, style=wx.LC_LIST | wx.LC_SINGLE_SEL)
+			self.list.Bind(wx.EVT_LIST_ITEM_ACTIVATED, lambda evt: self.EndModal(wx.ID_OK))
+			for index, choice in enumerate(choices):
+				self.list.InsertStringItem(index, choice)
+			self.list.Focus(default)
+			self.list.Select(default)
+			mainSizer.Add(self.list)
+			mainSizer.Add(self.CreateButtonSizer(style), flag=wx.BOTTOM)
+			mainSizer.Fit(self)
+			self.SetSizer(mainSizer)
+			self.list.SetFocus()
+		self._initDialog = initDialog
 
 	def run_gui(self):
-		if self.ShowModal() == wx.ID_OK:
+		self._initDialog()
+		self.Raise()
+		ret = self.ShowModal()
+		self.Destroy()
+		if ret == wx.ID_OK:
 			sel = self.list.GetFirstSelected()
+			if sel == -1: return None
 			return sel, self.list.GetItemText(sel)
 		else:
 			return None
