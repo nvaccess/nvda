8696e0cc7a650de8edade9cb7361a7ea235078a5
mdcurran <>
2007-05-04 01:20:20 +0000

    Convert all non-breaking spaces (unicode 0XA0) to normal spaces. Fixes a problem with many websites where NVDA does not say the space when arrowing over it, or some synthesizers would spell out the whole chunk of text containing the symbol.

--- source/speech.py
+++ source/speech.py
@@ -41,6 +41,8 @@ def initialize():
 def processTextSymbols(text,expandPunctuation=False):
 	if (text is None) or (len(text)==0) or (isinstance(text,basestring) and (set(text)<=set(characterSymbols.blankList))):
 		return _("blank") 
+	#Convert non-breaking spaces to spaces
+	text=text.replace(u'\xa0',u' ')
 	#Limit groups of the same character to 5 or less.
 	trunkatedText=""
 	lastChar=""
@@ -103,6 +105,7 @@ def processTextSymbols(text,expandPunctuation=False):
 	return text
 
 def processSymbol(symbol):
+	symbol=symbol.replace(u'\xa0',u' ')
 	newSymbol=characterSymbols.names.get(symbol,symbol)
 	return newSymbol
 
