4246eb2ecdc2f91fd4c409c52625d83bbc72bca1
mdcurran <>
2007-05-29 09:08:03 +0000

    Finally changed copyright of all files, and NVDA copyright message, to NVDA Contributors, rather than Michael Curran. The NVDA project url is also after NVDA Contributors, since all contributors are listed clearly on the home page at the bottom.

--- source/versionInfo.py
+++ source/versionInfo.py
@@ -1,6 +1,6 @@
 #versionInfo.py
 #A part of NonVisual Desktop Access (NVDA)
-#Copyright (C) 2006-2007 Michael Curran <mick@kulgan.net>
+#Copyright (C) 2006-2007 NVDA Contributors <http://www.nvda-project.org/>
 #This file is covered by the GNU General Public License.
 #See the file COPYING for more details.
 
@@ -11,7 +11,7 @@ description=_("A free and open-source screen reader for MS Windows")
 maintainer="Michael Curran"
 maintainer_email="mick@kulgan.net"
 url="http://www.nvda-project.org/"
-copyrightInfo=_("""Copyright (C) 2006-2007 Michael Curran <mick@kulgan.net>
+copyrightInfo=_("""Copyright (C) 2006-2007 NVDA Contributors <http://www.nvda-project.org/>
 %(name)s is covered by the GNU General Public License. You are free to share or change this software in any way you like as long as you distribute the licence along with the software, and make all source code available to anyone who wants it. This applies to both origional and modified copies of the software, plus any software that uses code taken from this software.
 For further details, you can view the licence online at:
 http://www.gnu.org/licenses/gpl.html
--- source/virtualBuffers/IAccessible/__init__.py
+++ source/virtualBuffers/IAccessible/__init__.py
@@ -1,6 +1,6 @@
 #virtualBuffers/IAccessible.py
 #A part of NonVisual Desktop Access (NVDA)
-#Copyright (C) 2006-2007 Michael Curran <mick@kulgan.net>
+#Copyright (C) 2006-2007 NVDA Contributors <http://www.nvda-project.org/>
 #This file is covered by the GNU General Public License.
 #See the file COPYING for more details.
 
