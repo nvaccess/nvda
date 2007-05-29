22231a0a789ff16ec6dec4c078adc2fde1251ae2
mdcurran <>
2007-05-29 09:08:03 +0000

    Finally changed copyright of all files, and NVDA copyright message, to NVDA Contributors, rather than Michael Curran. The NVDA project url is also after NVDA Contributors, since all contributors are listed clearly on the home page at the bottom.

--- source/winUser.py
+++ source/winUser.py
@@ -1,7 +1,7 @@
 import debug
 #winUser.py
 #A part of NonVisual Desktop Access (NVDA)
-#Copyright (C) 2006-2007 Michael Curran <mick@kulgan.net>
+#Copyright (C) 2006-2007 NVDA Contributors <http://www.nvda-project.org/>
 #This file is covered by the GNU General Public License.
 #See the file COPYING for more details.
 
