1900fcd7774446d43873264e60a07c565be79441
mdcurran <>
2007-06-07 04:18:44 +0000

    Fixed long-standing bug with Outlook Express where deleting a message in the message list would cause the list itself to be announced before announcing the next undeleted item in the list. Also fixed a bug where sometimes the next undeleted item wouldn't get announced because Outlook Express failed to set the state_system_focused state on the next list item.

--- source/appModules/msimn.py
+++ source/appModules/msimn.py
@@ -5,11 +5,15 @@
 #See the file COPYING for more details.
 
 import winUser
+import controlTypes
 import api
 import IAccessibleHandler
 import appModuleHandler
 import speech
 
+lastFocusRole=None
+
+#Labels for the header fields of an email, by control ID
 envelopeNames={
 	1000:_("Attachments"),
 	1001:_("To:"),
@@ -34,5 +38,23 @@ class appModule(appModuleHandler.appModule):
 		windowHandle=obj.windowHandle
 		parentWindow=winUser.getAncestor(windowHandle,winUser.GA_PARENT)
 		parentClassName=winUser.getClassName(parentWindow)
+		#If this object is an email header field, and we have a custom label for it,
+		#Then set the object's name to the label 
 		if parentClassName=="OE_Envelope" and obj.IAccessibleChildID==0 and envelopeNames.has_key(controlID):
 			obj.name=envelopeNames[controlID]
+		#Sometimes list items in the message list get a focus event, yet their focus state is not set
+		#So allow reportFocus to run on these objects
+		if obj.role==controlTypes.ROLE_LISTITEM:
+			obj.reportFocusNeedsIAccessibleFocusState=False
+
+	def event_gainFocus(self,obj,nextHandler):
+		global lastFocusRole
+		ignore=False
+		focusRole=obj.role
+		#Outlook express has a bug where deleting a message causes focus to move to the message list.
+		if focusRole==controlTypes.ROLE_LIST and lastFocusRole==controlTypes.ROLE_LISTITEM:
+			ignore=True
+		lastFocusRole=focusRole
+		if not ignore:
+			return nextHandler()
+
