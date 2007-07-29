e1b9ce52d8bcb66936c7cced759948ba41116b4e
mdcurran <>
2007-07-29 01:56:28 +0000

    The code supporting Microsoft Outlook 2000 message lists is now also used for any version of Outlook below 2003 (so this is both 2000 and xp). Hopweuflly now people using 2000/xp Outlook versions can read message lists, and contact lists. No support for calandar lists has been wwritten yet. Outlook 2000 / XP has no way of comunicating to NVDA exactly what fields are showing for a view, so message lists for now just announce senderName, subject, and received time. Contact lists announce fullName, company, job title, email addres. This should be enough info at least to be able to find a contact, if you want more, actually go in to that particular one.

--- source/appModules/outlook.py
+++ source/appModules/outlook.py
@@ -15,16 +15,34 @@ from keyUtils import key, sendKey
 from NVDAObjects.IAccessible import IAccessible
 from NVDAObjects.window import Window
 
-_nativeOm=None
+try:
+	nativeOm=win32com.client.GetActiveObject("outlook.application")
+except:
+	nativeOm=win32com.client.Dispatch("outlook.application")
+outlookVersion=int(nativeOm.version[0])
 
-def getNativeOm():
-	global _nativeOm
-	if not _nativeOm:
-		try:
-			_nativeOm=win32com.client.GetActiveObject("outlook.Application")
-		except:
-			_nativeOm=win32com.client.Dispatch("outlook.Application")
-	return _nativeOm
+def getContactString(obj):
+		return ", ".join([x for x in [obj.fullName,obj.companyName,obj.jobTitle,obj.email1address] if x and not x.isspace()])
+
+def getReceivedMessageString(obj):
+	nameList=[]
+	nameList.append(obj.senderName)
+	nameList.append(_("subject: %s")%obj.subject)
+	nameList.append(_("received: %s")%obj.receivedTime)
+
+	text=", ".join(nameList)
+	if obj.unread:
+		text="%s %s"%(_("unread"),text)
+	if obj.attachments.count>0:
+		text="%s %s"%(_("attachment"),text)
+	return text
+
+def getSentMessageString(obj):
+	nameList=[]
+	nameList.append(obj.to)
+	nameList.append(_("subject: %s")%obj.subject)
+	nameList.append(_("sent: %s")%obj.sentOn)
+	return ", ".join(nameList)
 
 class appModule(appModuleHandler.appModule):
 
@@ -32,14 +50,14 @@ class appModule(appModuleHandler.appModule):
 		obj.description=None
 		controlID=obj.windowControlID
 		className=obj.windowClassName
-		nativeOm=getNativeOm()
-		if nativeOm and nativeOm.version.startswith("9.0") and isinstance(obj,IAccessible) and className=="SUPERGRID" and controlID==4704:
-			obj.__class__=MessageList_2000
+		if outlookVersion<11 and isinstance(obj,IAccessible) and ((className=="SUPERGRID" and controlID==4704) or (className=="rctrl_renwnd32" and controlID==109)):
+			obj.__class__=MessageList_pre2003
 
-class MessageList_2000(IAccessible):
+class MessageList_pre2003(IAccessible):
 
 	def _get_name(self):
-		return _("Messages")
+		if hasattr(self,'curMessageItem'):
+			return self.curMessageItem.msg.parent.name
 
 	def _get_role(self):
 		return controlTypes.ROLE_LIST
@@ -55,24 +73,40 @@ class MessageList_2000(IAccessible):
 			return []
 
 	def event_gainFocus(self):
-		super(MessageList_2000,self).event_gainFocus()
-		nativeOm=getNativeOm()
-		if nativeOm:
-			self.curMessageItem=MessageItem(self,nativeOm.ActiveExplorer().selection[0])
+		try:
+			msg=nativeOm.ActiveExplorer().selection[0]
+		except:
+			msg=None
+			pass
+		if msg:
+			self.curMessageItem=MessageItem(self,msg)
+		super(MessageList_pre2003,self).event_gainFocus()
+		if msg:
 			eventHandler.manageEvent("gainFocus",self.curMessageItem)
 
 	def script_moveByMessage(self,keyPress,nextScript):
-		oldEntryID=self.curMessageItem.msg.entryID
+		if hasattr(self,'curMessageItem'):
+			oldEntryID=self.curMessageItem.msg.entryID
+		else:
+			oldEntryID=None
 		sendKey(keyPress)
-		messageItem=MessageItem(self,getNativeOm().ActiveExplorer().selection[0])
-		newEntryID=messageItem.msg.entryID
-		if newEntryID!=oldEntryID:
-			self.curMessageItem=messageItem
-			eventHandler.manageEvent("gainFocus",messageItem)
+		try:
+			msg=nativeOm.ActiveExplorer().selection[0]
+		except:
+			msg=None
+			pass
+		if msg:
+			messageItem=MessageItem(self,msg)
+			newEntryID=messageItem.msg.entryID
+			if newEntryID!=oldEntryID:
+				self.curMessageItem=messageItem
+				eventHandler.manageEvent("gainFocus",messageItem)
 
-[MessageList_2000.bindKey(keyName,scriptName) for keyName,scriptName in [
+[MessageList_pre2003.bindKey(keyName,scriptName) for keyName,scriptName in [
 	("extendedDown","moveByMessage"),
 	("extendedUp","moveByMessage"),
+	("extendedHome","moveByMessage"),
+	("extendedEnd","moveByMessage"),
 ]]
 
 class MessageItem(Window):
@@ -83,13 +117,11 @@ class MessageItem(Window):
 		Window.__init__(self,parent.windowHandle)
 
 	def _get_name(self):
-		nameList=[]
-		if self.msg.attachments.count>0:
-			nameList.append(_("attachment"))
-		if self.msg.unread:
-			nameList.append(_("unread"))
-		nameList.append(self.msg.senderName)
-		return " ".join(nameList)
+		typeID=self.msg.Class
+		if typeID==40:
+			return getContactString(self.msg)
+		elif typeID==43:
+			return getReceivedMessageString(self.msg)
 
 	def _get_role(self):
 		return controlTypes.ROLE_LISTITEM
@@ -97,5 +129,3 @@ class MessageItem(Window):
 	def _get_states(self):
 		return frozenset([controlTypes.STATE_SELECTED])
 
-	def _get_value(self):
-		return _("Subject: %s, Received: %s")%(self.msg.subject,self.msg.receivedTime)
