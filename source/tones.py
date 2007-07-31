b414a69b3dba6a44b819d00d99da3980321f60e6
mdcurran <>
2007-07-31 00:55:47 +0000

    More improvement to the sound of tones.beep. This time its back to a sine wave, but flattened off for the top of the peaks, so that its a bit more squarish. Rather more like how a PC speaker would produce sound.

--- source/tones.py
+++ source/tones.py
@@ -5,34 +5,29 @@
 #See the file COPYING for more details.
 
 import nvwave
+import debug
 import os
 import struct
 import math
 from glob import glob
 
-sampleRate=22050.0
-slopeRatio=0.01
-amplitude=10000.0
+sampleRate=22050
+amplitude=6000
 
 player = nvwave.WavePlayer(channels=1, samplesPerSec=int(sampleRate), bitsPerSample=16)
 
 def beep(hz,length):
 	player.stop()
-	sampleLength=int(length*(sampleRate/1000.0))
+	sampleLength=length*(sampleRate/1000)
 	data=""
-	cycleLength=sampleRate/hz
-	halfCycleLength=cycleLength/2
-	quarterCycleLength=cycleLength/4
-	threeQuarterCycleLength=halfCycleLength+quarterCycleLength
 	for sampleCount in xrange(sampleLength):
-		cyclePos=sampleCount%cycleLength
-		if cyclePos<=quarterCycleLength:
-			sample=amplitude*(cyclePos/quarterCycleLength)
-		elif cyclePos<=halfCycleLength:
-			sample=amplitude*((halfCycleLength-cyclePos)/quarterCycleLength)
-		elif cyclePos<=threeQuarterCycleLength:
-			sample=0-(amplitude*((cyclePos-halfCycleLength)/quarterCycleLength))
-		elif cyclePos<=cycleLength:
-			sample=0-(amplitude*((halfCycleLength-(cyclePos-halfCycleLength))/quarterCycleLength))
+		sampleCount=sampleCount%sampleLength
+		sample=math.sin((sampleCount*math.pi*2)/(sampleRate/hz))*3
+		if sample>1:
+			sample=1
+		elif sample<-1:
+			sample=-1
+		sample*=amplitude
 		data+=struct.pack('h',sample)
+	debug.writeMessage("sample: %s"%sample)
 	player.feed(data)
