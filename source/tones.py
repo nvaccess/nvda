f1107fc4d5bac50443d5756e5d87c2f774b0caf1
mdcurran <>
2007-09-02 10:05:02 +0000

    Stopped beeps from clicking and distorting a little at start and end.

--- source/tones.py
+++ source/tones.py
@@ -12,6 +12,8 @@ import math
 from glob import glob
 import config
 
+piTwo=math.pi*2
+
 sampleRate=22050
 amplitude=14000
 
@@ -19,17 +21,22 @@ player = nvwave.WavePlayer(channels=2, samplesPerSec=int(sampleRate), bitsPerSam
 
 def beep(hz,length,left=50,right=50):
 	player.stop()
-	sampleLength=length*(sampleRate/1000)
+	samplesPerCycle=int(sampleRate/hz)
+	numCycles=int((length/1000.0)/(samplesPerCycle*(1.0/sampleRate)))
 	data=""
-	for sampleCount in xrange(sampleLength):
-		sampleCount=sampleCount%sampleLength
-		sample=math.sin((sampleCount*math.pi*2)/(sampleRate/hz))*2
-		if sample>1:
-			sample=1
-		elif sample<-1:
-			sample=-1
-		sample*=amplitude
+	cycleNum=0
+	sampleNum=0
+	while cycleNum<=numCycles:
+		sample=min(max(math.sin(piTwo*(float(sampleNum)/samplesPerCycle))*2,-1),1)*amplitude
+		if cycleNum==0:
+			sample=sample*(float(sampleNum)/samplesPerCycle)
+		elif cycleNum==numCycles:
+			sample=sample*(1-(float(sampleNum)/samplesPerCycle))
 		leftSample=sample*(left/100.0)
 		rightSample=sample*(right/100.0)
-		data+=struct.pack('hh',leftSample,rightSample)
+		data+=struct.pack('hh',int(leftSample),int(rightSample))
+		sampleNum+=1
+		if sampleNum==samplesPerCycle:
+			cycleNum+=1
+			sampleNum=0
 	player.feed(data)
