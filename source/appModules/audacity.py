11c501c7cf749af18bb0de7c79832176ca01f5a5
pvagner <>
2007-09-26 18:36:43 +0000

    * added appmodule for audacity. At the moment it makes some checkboxes and radio buttons accessible. Object navigation does not work at all with these buggy controls.

---
+++ source/appModules/audacity.py
@@ -0,0 +1,14 @@
+#appModules/audacity.py
+#A part of NonVisual Desktop Access (NVDA)
+#Copyright (C) 2006-2007 NVDA Contributors <http://www.nvda-project.org/>
+#This file is covered by the GNU General Public License.
+#See the file COPYING for more details.
+
+import appModuleHandler
+import winUser
+
+class appModule(appModuleHandler.appModule):
+
+	def event_NVDAObject_init(self,obj):
+		if obj.windowClassName=="Button":
+			obj.name=winUser.getWindowText(obj.windowHandle).replace('&','')
