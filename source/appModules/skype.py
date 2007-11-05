41b4af98369bd85018afef4c2585fa7ad4acad95
pvagner <>
2007-11-05 19:00:18 +0000

    * added skype appmodule which just prevents speaking of contact's name twice when arrowing in the contactlist window

---
+++ source/appModules/skype.py
@@ -0,0 +1,15 @@
+#appModules/skype.py
+#A part of NonVisual Desktop Access (NVDA)
+#Copyright (C) 2006-2007 NVDA Contributors <http://www.nvda-project.org/>
+#This file is covered by the GNU General Public License.
+#See the file COPYING for more details.
+
+import appModuleHandler
+from NVDAObjects.IAccessible import IAccessible
+
+class appModule(appModuleHandler.appModule):
+
+	def event_NVDAObject_init(self,obj):
+		if obj.windowClassName=="TMainUserList":
+			obj.name=obj.value
+			obj.value=None
