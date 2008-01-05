d6e3d1949e6cabe310dfa72e350dfc9d97ca3c43
mdcurran <>
2008-01-05 13:37:08 +0000

    New Adobe virtualBuffer: support event_valueChange, so that the virtualBuffer is re-rendered when changing pages.

--- source/virtualBuffers_new/adobe.py
+++ source/virtualBuffers_new/adobe.py
@@ -1,4 +1,10 @@
+import controlTypes
 from .IAccessible import IAccessible
 
 class Adobe(IAccessible):
 	pass
+
+	def event_valueChange(self,obj,nextHandler):
+		if obj.role in (controlTypes.ROLE_DOCUMENT,controlTypes.ROLE_PAGE):
+			self.rootNVDAObject=obj
+			self.fillVBuf()
