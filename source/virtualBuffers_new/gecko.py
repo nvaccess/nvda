6d37bb075b1465b028a922b66fae4c9b54b1a1e7
mdcurran <>
2008-01-12 01:13:29 +0000

    New Gecko virtualBuffer: isAlive other than doing the same as IAccessible's isAlive, should also check that state_readonly is in the rootNVDAObject's states. If not, then the document is editable and we shouldn't use a virtualBuffer here. This fixes a bug in NVA with Thunderbird3 where replying to a message in plain text uses a virtualBuffer as as the document first appears, it has the readonly state set.

--- source/virtualBuffers_new/gecko.py
+++ source/virtualBuffers_new/gecko.py
@@ -2,3 +2,9 @@ from .IAccessible import IAccessible
 
 class Gecko(IAccessible):
 	pass
+
+	def isAlive(self):
+		if super(Gecko,self).isAlive() and controlTypes.STATE_READONLY in self.rootNVDAObject.states:
+			return True
+		else:
+			return False
