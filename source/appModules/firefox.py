bd587f3db3bf50be3ae6b6bf0df42872f63c605f
James Teh <jamie@jantrid.net>
2008-04-09 21:51:30 +1000

    Add a Firefox appModule. This simply reads the status bar whenever a document becomes busy or a busy document receives focus.

---
+++ source/appModules/firefox.py
@@ -0,0 +1,24 @@
+#appModules/firefox.py
+#A part of NonVisual Desktop Access (NVDA)
+#Copyright (C) 2006-2008 NVDA Contributors <http://www.nvda-project.org/>
+#This file is covered by the GNU General Public License.
+#See the file COPYING for more details.
+
+import appModuleHandler
+import controlTypes
+import api
+import speech
+import winUser
+
+class appModule(appModuleHandler.appModule):
+
+	def event_stateChange(self, obj, nextHandler):
+		if obj.role == controlTypes.ROLE_DOCUMENT and controlTypes.STATE_BUSY in obj.states and winUser.isWindowVisible(obj.windowHandle):
+			statusBar = api.getStatusBar()
+			if statusBar:
+				statusText = api.getStatusBarText(statusBar)
+				speech.cancelSpeech()
+				speech.speakMessage(statusText)
+		nextHandler()
+
+	event_gainFocus = event_stateChange
