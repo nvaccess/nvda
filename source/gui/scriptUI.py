1665a3425c308b61c188411a05f7f5bbec04d5d5
James Teh <jamie@jantrid.net>
2008-04-22 15:21:06 +1000

    Major clean up of scriptUI: 	* Make ModalDialog a new-style class. 	* Remove the dodgy selfIsDialog idea. 		* ModalDialog sub-classes can no longer inherit both ModalDialog and the wx dialog class. 		* Instead, makeDialog must return a dialog instance to use. 		* Although this means the dialog can't be self, it makes the code a bit clearer and avoids overly complicated multiple inheritance. 	* Remove TextEntriesDialog. 		* It is no longer needed and I didn't see much point in converting it. 		* If it is needed in future, a better implementation might be discussed. 	* Minor cosmetic changes.

--- source/gui/scriptUI.py
+++ source/gui/scriptUI.py
@@ -1,27 +1,31 @@
-"Windows, dialogs and utilities for GUI interaction from scripts."
+"""Windows, dialogs and utilities for GUI interaction from scripts.
+"""
 
 import queueHandler
 import wx
 import gui
 
-class ModalDialog:
+class ModalDialog(object):
 	"""Base class for modal script dialogs.
 	Subclasses should override L{__init__}, L{makeDialog} and L{getResponse}.
-	@ivar selfIsDialog: C{True} if the instance itself is the dialog, C{False} if the dialog is contained in self.dialog.
-	@type selfIsDialog: bool
-	@ivar dialog: The dialog if L{selfIsDialog} is C{False}.
+	@ivar dialog: The dialog or C{None} if it has not yet been created.
 	@type dialog: wx.Dialog
-"""
-
-	selfIsDialog = False
+	"""
 
 	def __init__(self, callback):
 		"""Constructor.
-		Subclasses should override this, but they must call C{ModalDialog.__init__(self, callback)}.
+		Subclasses should extend this; i.e. they must call this base class method.
 		@param callback: A function which takes the response from the dialog as its only argument.
 		@type callback: callable
-	"""
+		"""
 		self._callback = callback
+
+	def makeDialog(self):
+		"""Make the dialog.
+		@return: The dialog.
+		@rtype: wx.Dialog
+		"""
+		raise NotImplementedError
 
 	def getResponse(self, response):
 		"""Obtain the response to supply to the callback.
@@ -31,11 +35,7 @@ class ModalDialog:
 		return response
 
 	def _run_gui(self):
-		if self.selfIsDialog:
-			self.makeDialog()
-			dialog = self
-		else:
-			dialog = self.dialog = self.makeDialog()
+		dialog = self.dialog = self.makeDialog()
 		dialog.Raise()
 		ret = self.getResponse(dialog.ShowModal())
 		dialog.Destroy()
@@ -44,14 +44,14 @@ class ModalDialog:
 	def run(self):
 		"""Run this dialog.
 		This displays the dialog and calls the callback with the relevant response once the user has dismissed the dialog.
-	"""
+		"""
 		gui.execute(self._run_gui, callback=self._callback)
 
 class MessageDialog(ModalDialog):
 
 	def __init__(self, message, title = _("NVDA"), style=wx.OK, callback=None):
-		ModalDialog.__init__(self, callback)
 		self.makeDialog = lambda: wx.MessageDialog(None, message, title, style)
+		super(MessageDialog, self).__init__(callback)
 
 	def getResponse(self, response):
 		return response in (wx.ID_OK, wx.ID_YES)
@@ -59,8 +59,8 @@ class MessageDialog(ModalDialog):
 class TextEntryDialog(ModalDialog):
 
 	def __init__(self, message, title=_("NVDA"), default="", style=wx.OK | wx.CANCEL, callback=None):
-		ModalDialog.__init__(self, callback)
 		self.makeDialog = lambda: wx.TextEntryDialog(None, message, title, defaultValue=default, style=style)
+		super(TextEntryDialog, self).__init__(callback)
 
 	def getResponse(self, response):
 		if response == wx.ID_OK:
@@ -68,48 +68,47 @@ class TextEntryDialog(ModalDialog):
 		else:
 			return None
 
-class SingleChoiceDialog(wx.Dialog, ModalDialog):
-	selfIsDialog = True
+class SingleChoiceDialog(ModalDialog):
 
 	def __init__(self, message, title=_("NVDA"), choices=(), default=0, style=wx.OK|wx.CANCEL, callback=None):
-		ModalDialog.__init__(self, callback)
 		def makeDialog():
-			wx.Dialog.__init__(self, None, -1, title)
+			dialog = wx.Dialog(None, wx.ID_ANY, title)
 			mainSizer = wx.BoxSizer(wx.VERTICAL)
-			mainSizer.Add(wx.StaticText(self, -1, label=message))
-			self.list = wx.ListView(self, -1, style=wx.LC_LIST | wx.LC_SINGLE_SEL)
-			self.list.Bind(wx.EVT_LIST_ITEM_ACTIVATED, lambda evt: self.EndModal(wx.ID_OK))
+			mainSizer.Add(wx.StaticText(dialogD_ANY, label=message))
+			self.list = wx.ListView(dialog, wx.ID_ANY, style=wx.LC_LIST | wx.LC_SINGLE_SEL)
+			self.list.Bind(wx.EVT_LIST_ITEM_ACTIVATED, lambda evt: dialog.EndModal(wx.ID_OK))
 			for index, choice in enumerate(choices):
 				self.list.InsertStringItem(index, choice)
 			self.list.Focus(default)
 			self.list.Select(default)
 			mainSizer.Add(self.list)
-			mainSizer.Add(self.CreateButtonSizer(style), flag=wx.BOTTOM)
-			mainSizer.Fit(self)
-			self.SetSizer(mainSizer)
+			mainSizer.Add(dialog.CreateButtonSizer(style), flag=wx.BOTTOM)
+			mainSizer.Fit(dialog)
+			dialog.SetSizer(mainSizer)
 			self.list.SetFocus()
+			return dialog
 		self.makeDialog = makeDialog
+		super(SingleChoiceDialog, self).__init__(callback)
 
 	def getResponse(self, response):
 		if response == wx.ID_OK:
 			sel = self.list.GetFirstSelected()
-			if sel == -1: return None
+			if sel == -1:
+				return None
 			return sel, self.list.GetItemText(sel)
 		else:
 			return None
 
-class LinksListDialog(wx.Dialog, ModalDialog):
-	selfIsDialog = True
+class LinksListDialog(ModalDialog):
 	ID_MOVETO = 1000
 	ID_ACTIVATE = wx.ID_OK
 
 	def __init__(self, choices, default=0, callback=None):
-		ModalDialog.__init__(self, callback)
 		def makeDialog():
-			wx.Dialog.__init__(self, None, -1, _("Links List"))
+			dialog = wx.Dialog(None, wx.ID_ANY, _("Links List"))
 			mainSizer = wx.BoxSizer(wx.VERTICAL)
-			self.list = wx.ListView(self, -1, style=wx.LC_LIST | wx.LC_SINGLE_SEL)
-			self.list.Bind(wx.EVT_LIST_ITEM_ACTIVATED, lambda evt: self.EndModal(wx.ID_OK))
+			self.list = wx.ListView(dialog, wx.ID_ANY, style=wx.LC_LIST | wx.LC_SINGLE_SEL)
+			self.list.Bind(wx.EVT_LIST_ITEM_ACTIVATED, lambda evt: dialog.EndModal(wx.ID_OK))
 			for index, choice in enumerate(choices):
 				self.list.InsertStringItem(index, choice)
 			self.list.Focus(default)
@@ -117,52 +116,24 @@ class LinksListDialog(wx.Dialog, ModalDialog):
 			mainSizer.Add(self.list)
 			buttonSizer = wx.BoxSizer(wx.HORIZONTAL)
 			# Activate is our OK button, so need for an event.
-			buttonSizer.Add(wx.Button(self, self.ID_ACTIVATE, _("&Activate Link")))
-			movetoButton = wx.Button(self, self.ID_MOVETO, _("&Move to link"))
-			movetoButton.Bind(wx.EVT_BUTTON, lambda evt: self.EndModal(self.ID_MOVETO))
+			buttonSizer.Add(wx.Button(dialog, self.ID_ACTIVATE, _("&Activate Link")))
+			movetoButton = wx.Button(dialog, self.ID_MOVETO, _("&Move to link"))
+			movetoButton.Bind(wx.EVT_BUTTON, lambda evt: dialog.EndModal(self.ID_MOVETO))
 			buttonSizer.Add(movetoButton)
-			buttonSizer.Add(wx.Button(self, wx.ID_CANCEL))
+			buttonSizer.Add(wx.Button(dialog, wx.ID_CANCEL))
 			mainSizer.Add(buttonSizer)
-			mainSizer.Fit(self)
-			self.SetSizer(mainSizer)
+			mainSizer.Fit(dialog)
+			dialog.SetSizer(mainSizer)
 			self.list.SetFocus()
+			return dialog
 		self.makeDialog = makeDialog
+		super(LinksListDialog, self).__init__(callback)
 
 	def getResponse(self, response):
 		if response != wx.ID_CANCEL:
 			sel = self.list.GetFirstSelected()
-			if sel == -1: return None
+			if sel == -1:
+				return None
 			return response == self.ID_ACTIVATE, sel, self.list.GetItemText(sel)
-		else:
-			return None
-
-class TextEntriesDialog(wx.Dialog, ModalDialog):
-	selfIsDialog = True
-
-	def __init__(self, messages=(), title=_("NVDA"), defaults=(), style=wx.OK|wx.CANCEL, callback=None):
-		ModalDialog.__init__(self, callback)
-		def makeDialog():
-			wx.Dialog.__init__(self, None, -1, title)
-			mainSizer = wx.BoxSizer(wx.VERTICAL)
-			self.edits=[]
-			for counter in range(0,len(messages)):
-				mainSizer.Add(wx.StaticText(self, -1, label=messages[counter]))
-				self.edits.append(wx.TextCtrl(self,wx.NewId()))
-				if len(defaults)>counter:
-					self.edits[counter].SetValue(defaults[counter])
-				mainSizer.Add(self.edits[counter])
-			mainSizer.Add(self.CreateButtonSizer(style), flag=wx.BOTTOM)
-			mainSizer.Fit(self)
-			self.SetSizer(mainSizer)
-			self.Bind(wx.EVT_BUTTON, lambda evt: self.EndModal(wx.ID_OK),id=wx.ID_OK)
-			self.edits[0].SetFocus()
-		self.makeDialog = makeDialog
-
-	def getResponse(self, response):
-		if response == wx.ID_OK:
-			value=[]
-			for counter in range(0,len(self.edits)):
-				value.append(self.edits[counter].GetValue())
-			return value
 		else:
 			return None
