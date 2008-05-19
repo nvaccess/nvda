b5b5a312e84824434235f8c760c13f150f1f8d88
mdcurran <>
2008-05-19 03:53:14 +0000

    Thunderbird appModule: only speak the status message if needed on gainFocus, no longer on stateChange. Also no longer chancel speech when speaking the message. Although this means you may need to wait longer to hear it, at least you will still know where you are.

--- source/appModules/thunderbird.py
+++ source/appModules/thunderbird.py
@@ -12,7 +12,7 @@ import winUser
 
 class appModule(appModuleHandler.AppModule):
 
-	def event_stateChange(self, obj, nextHandler):
+	def event_gainFocus(self, obj, nextHandler):
 		if obj.role == controlTypes.ROLE_DOCUMENT and controlTypes.STATE_BUSY in obj.states and winUser.isWindowVisible(obj.windowHandle):
 			statusBar = api.getStatusBar()
 			if statusBar:
@@ -22,10 +22,7 @@ class appModule(appModuleHandler.AppModule):
 				except:
 					# Fall back to reading the entire status bar.
 					statusText = api.getStatusBarText(statusBar)
-				speech.cancelSpeech()
 				speech.speakMessage(controlTypes.speechStateLabels[controlTypes.STATE_BUSY])
 				speech.speakMessage(statusText)
 				return
 		nextHandler()
-
-	event_gainFocus = event_stateChange
