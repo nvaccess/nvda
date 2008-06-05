af77cac68e0cb60797f9d05dc468d167cf4691ca
mdcurran <>
2008-06-05 09:28:26 +0000

    eventHandler.isPendingEvents: now it takes an optional keyword argument of 'obj' which is an NVDAObject who the events should be for. So for instance if you wanted to know if there are any gainFocus events pending for a particular object, you can give eventName as 'gainFocus' and obj as the object you are talking about. For this to work _pendingEventCountsByName, _pendingEventCountsByObj and _pendingEventCountsByNameAndObj dicts needed to be added, and _pendingEventCounts has been removed. Also rather than leaving an item in there when its count gets to 0, it is now removed completely.

--- source/eventHandler.py
+++ source/eventHandler.py
@@ -6,32 +6,56 @@ import virtualBufferHandler
 import globalVars
 import controlTypes
 
-#A dict to store the count of pending events by type
-_pendingEventCounts={}
+#Some dicts to store event counts by name and or obj
+_pendingEventCountsByName={}
+_pendingEventCountsByObj={}
+_pendingEventCountsByNameAndObj={}
 
 def queueEvent(eventName,obj):
 	"""Queues an NVDA event to be executed.
 	@param eventName: the name of the event type (e.g. 'gainFocus', 'nameChange')
 	@type eventName: string
 	"""
-	_pendingEventCounts[eventName]=_pendingEventCounts.get(eventName,0)+1
+	_pendingEventCountsByName[eventName]=_pendingEventCountsByName.get(eventName,0)+1
+	_pendingEventCountsByObj[obj]=_pendingEventCountsByObj.get(obj,0)+1
+	_pendingEventCountsByNameAndObj[(eventName,obj)]=_pendingEventCountsByNameAndObj.get((eventName,obj),0)+1
 	queueHandler.queueFunction(queueHandler.eventQueue,_queueEventCallback,eventName,obj)
 
 def _queueEventCallback(eventName,obj):
-	_pendingEventCounts[eventName]-=1
+	curCount=_pendingEventCountsByName.get(eventName,0)
+	if curCount>1:
+		_pendingEventCountsByName[eventName]=(curCount-1)
+	elif curCount==1:
+		del _pendingEventCountsByName[eventName]
+	curCount=_pendingEventCountsByObj.get(obj,0)
+	if curCount>1:
+		_pendingEventCountsByObj[obj]=(curCount-1)
+	elif curCount==1:
+		del _pendingEventCountsByObj[obj]
+	curCount=_pendingEventCountsByNameAndObj.get((eventName,obj),0)
+	if curCount>1:
+		_pendingEventCountsByNameAndObj[(eventName,obj)]=(curCount-1)
+	elif curCount==1:
+		del _pendingEventCountsByNameAndObj[(eventName,obj)]
 	executeEvent(eventName,obj)
 
-def isPendingEvents(eventName=None):
+def isPendingEvents(eventName=None,obj=None):
 	"""Are there currently any events queued?
 	@param eventName: an optional name of an event type. If given then only if there are events of this type queued will it return True.
 	@type eventName: string
+	@param obj: the NVDAObject the event is for
+	@type obj: L{NVDAObjects.NVDAObject}
 	@returns: True if there are events queued, False otherwise.
 	@rtype: boolean
 	"""
-	if not eventName:
-		return any(_pendingEventCounts.itervalues())
-	else:
-		return bool(_pendingEventCounts.get(eventName,0))
+	if not eventName and not obj:
+		return bool(len(_pendingEventCountsByName))
+	elif not eventName and obj:
+		return obj in _pendingEventCountsByObj
+	elif eventName and not obj:
+		return eventName in _pendingEventCountsByName
+	elif eventName and obj:
+		return (eventName,obj) in _pendingEventCountsByNameAndObj
 
 def executeEvent(eventName,obj):
 	"""Executes an NVDA event.
