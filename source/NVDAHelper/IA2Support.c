6058088554568fc2db671c61fd2fc764196132c7
mdcurran <>
2008-07-12 07:10:39 +0000

    NVDAHelper: in ia2_initialize, call installIA2Support(). This mean that when NVDAHelper is first initialized in NVDA, it will register IA2 for NVDA, rather than NVDA doing it itself. Saves duplicating code in two places. Code will have to be removed from IAccessibleHandler once the updated dll goes in to miscDeps and it works.  Also now rather than checking to see if IA2 support is already registered, and only registering if its not, instead find out if IA2 is registered and save off the possible old proxy stub IID for later use. Then register IAccessible2 ourselves no matter if it previously was or not. When uninstalling IA2 support, rather than setting the proxy stub IIDs to NULLIIDs, set them back to what they previously were before we installed. So although now it may mean we load a dll and instanciate a classObject when we may not have had to, at least we can count on IA2 support really working. Should fix problems where IA2 was registered system-wide but then that dll was removed with out unregistering. We will find out when it goes in to miscDeps.

--- source/NVDAHelper/IA2Support.c
+++ source/NVDAHelper/IA2Support.c
@@ -10,9 +10,14 @@
 #include "ia2.h"
 #include "IA2Support.h"
 
+#define ia2InterfaceCount 12
+
 #pragma data_seg(".hookManagerShared")
 BOOL isIA2Initialized=FALSE;
 wchar_t IA2DllPath[256]={0};
+//This array can hold items containing a clsid to be registered, and the old proxy stub clsid for that clsid that may have been pointed to 
+//Both values are NULL Iids (all zeros) by default 
+IID ia2ClsidArray[ia2InterfaceCount][2]={0};
 #pragma data_seg()
 #pragma comment(linker, "/section:.hookManagerShared,rws")
 
@@ -23,53 +28,28 @@ HANDLE IA2DllHandle=0;
 DWORD IA2RegCooky=0;
 IUnknown* IA2DllPunk=NULL;
 
-BOOL checkForIA2() {
-	CLSID proxyIid;
-	CLSID NULLIid;
-	memset(&NULLIid,0,sizeof(CLSID));
-	return (CoGetPSClsid(&IID_IAccessible2,&proxyIid)==S_OK)&&!IsEqualCLSID(&proxyIid,&NULLIid);
-}
-
 void installIA2Support() {
 	LPFNGETCLASSOBJECT IA2Dll_DllGetClassObject;
+	int i;
 	if(isIA2Installed) return;
-	if(!checkForIA2()) {
-		IA2DllHandle=CoLoadLibrary(IA2DllPath,TRUE);
-		if(IA2DllHandle==0) return;
-		IA2Dll_DllGetClassObject=(LPFNGETCLASSOBJECT)GetProcAddress(IA2DllHandle,"DllGetClassObject");
-		IA2Dll_DllGetClassObject(&IAccessible2ProxyIID,&IID_IUnknown,(LPVOID*)&IA2DllPunk);
-		CoRegisterClassObject(&IAccessible2ProxyIID,IA2DllPunk,CLSCTX_LOCAL_SERVER,REGCLS_MULTIPLEUSE,(LPDWORD)&IA2RegCooky);
-		CoRegisterPSClsid(&IID_IAccessible2,&IAccessible2ProxyIID);
-		CoRegisterPSClsid(&IID_IAccessibleAction,&IAccessible2ProxyIID);
-		CoRegisterPSClsid(&IID_IAccessibleApplication,&IAccessible2ProxyIID);
-		CoRegisterPSClsid(&IID_IAccessibleComponent,&IAccessible2ProxyIID);
-		CoRegisterPSClsid(&IID_IAccessibleEditableText,&IAccessible2ProxyIID);
-		CoRegisterPSClsid(&IID_IAccessibleHyperlink,&IAccessible2ProxyIID);
-		CoRegisterPSClsid(&IID_IAccessibleHypertext,&IAccessible2ProxyIID);
-		CoRegisterPSClsid(&IID_IAccessibleImage,&IAccessible2ProxyIID);
-		CoRegisterPSClsid(&IID_IAccessibleRelation,&IAccessible2ProxyIID);
-		CoRegisterPSClsid(&IID_IAccessibleTable,&IAccessible2ProxyIID);
-		CoRegisterPSClsid(&IID_IAccessibleText,&IAccessible2ProxyIID);
-		CoRegisterPSClsid(&IID_IAccessibleValue,&IAccessible2ProxyIID);
+	IA2DllHandle=CoLoadLibrary(IA2DllPath,TRUE);
+	if(IA2DllHandle==0) return;
+	IA2Dll_DllGetClassObject=(LPFNGETCLASSOBJECT)GetProcAddress(IA2DllHandle,"DllGetClassObject");
+	IA2Dll_DllGetClassObject(&IAccessible2ProxyIID,&IID_IUnknown,(LPVOID*)&IA2DllPunk);
+	CoRegisterClassObject(&IAccessible2ProxyIID,IA2DllPunk,CLSCTX_LOCAL_SERVER,REGCLS_MULTIPLEUSE,(LPDWORD)&IA2RegCooky);
+	for(i=0;i<ia2InterfaceCount;i++) {
+		CoGetPSClsid(&(ia2ClsidArray[i][0]),&(ia2ClsidArray[i][1]));
+		CoRegisterPSClsid(&(ia2ClsidArray[i][0]),&IAccessible2ProxyIID);
 	}
 	isIA2Installed=TRUE;
 }
 
 void uninstallIA2Support() {
-	CLSID NULLIid;
-	memset(&NULLIid,0,sizeof(CLSID));
+	int i;
 	if(IA2DllHandle!=0) {
-		CoRegisterPSClsid(&IID_IAccessible2,&NULLIid);
-		CoRegisterPSClsid(&IID_IAccessibleAction,&NULLIid);
-		CoRegisterPSClsid(&IID_IAccessibleApplication,&NULLIid);
-		CoRegisterPSClsid(&IID_IAccessibleComponent,&NULLIid);
-		CoRegisterPSClsid(&IID_IAccessibleEditableText,&NULLIid);
-		CoRegisterPSClsid(&IID_IAccessibleHypertext,&NULLIid);
-		CoRegisterPSClsid(&IID_IAccessibleImage,&NULLIid);
-		CoRegisterPSClsid(&IID_IAccessibleRelation,&NULLIid);
-		CoRegisterPSClsid(&IID_IAccessibleTable,&NULLIid);
-		CoRegisterPSClsid(&IID_IAccessibleText,&NULLIid);
-		CoRegisterPSClsid(&IID_IAccessibleValue,&NULLIid);
+	for(i=0;i<ia2InterfaceCount;i++) {
+			CoRegisterPSClsid(&(ia2ClsidArray[i][0]),&(ia2ClsidArray[i][1]));
+		}
 		CoRevokeClassObject(IA2RegCooky);
 		CoFreeUnusedLibrariesEx(0,0);
 		IA2DllHandle=0;
@@ -78,6 +58,21 @@ void uninstallIA2Support() {
 }
 
 void IA2Support_initialize() {
+	int count=0;
 	GetFullPathName(L"lib/IAccessible2Proxy.dll",256,IA2DllPath,NULL);
+	//Initialize the IAccessible2 Iids
+	ia2ClsidArray[count++][0]=IID_IAccessible2;
+	ia2ClsidArray[count++][0]=IID_IAccessibleAction;
+	ia2ClsidArray[count++][0]=IID_IAccessibleApplication;
+	ia2ClsidArray[count++][0]=IID_IAccessibleComponent;
+	ia2ClsidArray[count++][0]=IID_IAccessibleEditableText;
+	ia2ClsidArray[count++][0]=IID_IAccessibleHyperlink;
+	ia2ClsidArray[count++][0]=IID_IAccessibleHypertext;
+	ia2ClsidArray[count++][0]=IID_IAccessibleImage;
+	ia2ClsidArray[count++][0]=IID_IAccessibleRelation;
+	ia2ClsidArray[count++][0]=IID_IAccessibleTable;
+	ia2ClsidArray[count++][0]=IID_IAccessibleText;
+	ia2ClsidArray[count++][0]=IID_IAccessibleValue;
 	isIA2Initialized=TRUE;
+	installIA2Support();
 }
