bf9b8ad7172f93b48295438bd62aa8c35ad6ad9f
James Teh <jamie@jantrid.net>
2008-07-17 18:22:55 +1000

    Clarify that we are currently licensed under version 2 of the GPL and update license URL accordingly.

--- source/versionInfo.py
+++ source/versionInfo.py
@@ -42,9 +42,9 @@ version="unknown"
 description=_("A free and open-source screen reader for MS Windows")
 url="http://www.nvda-project.org/"
 copyrightInfo=_("""Copyright (C) 2006-2008 NVDA Contributors <http://www.nvda-project.org/>
-%(name)s is covered by the GNU General Public License. You are free to share or change this software in any way you like as long as you distribute the licence along with the software, and make all source code available to anyone who wants it. This applies to both origional and modified copies of the software, plus any software that uses code taken from this software.
+%(name)s is covered by the GNU General Public License (Version 2). You are free to share or change this software in any way you like as long as you distribute the licence along with the software, and make all source code available to anyone who wants it. This applies to both origional and modified copies of the software, plus any software that uses code taken from this software.
 For further details, you can view the licence online at:
-http://www.gnu.org/licenses/gpl.html
+http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 Or see the file Copying.txt that came with this software.""")%globals()
 
 _updateVersionFromVCS()
--- user_docs/en/readme.txt
+++ user_docs/en/readme.txt
@@ -7,7 +7,7 @@ For more information, please see the included user guide. Additional information
 
 Copyright:
 Copyright (C) 2006-2008 NVDA Contributors <http://www.nvda-project.org/>
-NVDA is covered by the GNU General Public License. You are free to share or change this software in any way you like as long as you distribute the licence along with the software, and make all source code available to anyone who wants it. This applies to both origional and modified copies of the software, plus any software that uses code taken from this software.
+NVDA is covered by the GNU General Public License (Version 2). You are free to share or change this software in any way you like as long as you distribute the licence along with the software, and make all source code available to anyone who wants it. This applies to both origional and modified copies of the software, plus any software that uses code taken from this software.
 For further details, you can view the licence online at:
-http://www.gnu.org/licenses/gpl.html
+http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 Or see the file Copying.txt that came with this software.
