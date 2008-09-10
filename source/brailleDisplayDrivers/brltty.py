3926dad0dab297d87f9b906e838ecb0863b2ab7f
James Teh <jamie@jantrid.net>
2008-09-10 11:45:23 +1000

    brltty driver: Add basic key press handling.

--- source/brailleDisplayDrivers/brltty.py
+++ source/brailleDisplayDrivers/brltty.py
@@ -1,8 +1,11 @@
+import wx
 import braille
 try:
 	import brlapi
 except ImportError:
 	pass
+
+KEY_CHECK_INTERVAL = 50
 
 class BrailleDisplayDriver(braille.BrailleDisplayDriver):
 	"""brltty braille display driver.
@@ -22,8 +25,12 @@ class BrailleDisplayDriver(braille.BrailleDisplayDriver):
 	def __init__(self):
 		self._con = brlapi.Connection()
 		self._con.enterTtyModeWithPath()
+		self._keyCheckTimer = wx.PyTimer(self._handleKeyPresses)
+		self._keyCheckTimer.Start(KEY_CHECK_INTERVAL)
 
 	def __del__(self):
+		self._keyCheckTimer.Stop()
+		self._keyCheckTimer = None
 		self._con.leaveTtyMode()
 		self._con = None
 
@@ -38,3 +45,23 @@ class BrailleDisplayDriver(braille.BrailleDisplayDriver):
 		# The string sent to the display needs to be the length of the display, so pad with zeroes if necessary.
 		out = "".join(chr(cell) for cell in self._cells) + "\0" * (self.numCells - len(self._cells))
 		self._con.writeDots(out)
+
+	def _handleKeyPresses(self):
+		while True:
+			key = self._con.readKey(False)
+			if not key:
+				break
+			key = self._con.expandKeyCode(key)
+			self._onKeyPress(key)
+
+	def _onKeyPress(self, key):
+		keyType = key["type"]
+		command = key["command"]
+		argument = key["argument"]
+		if keyType == brlapi.KEY_TYPE_CMD:
+			if command == brlapi.KEY_CMD_FWINLT:
+				braille.handler.buffer.scrollBack()
+			elif command == brlapi.KEY_CMD_FWINRT:
+				braille.handler.buffer.scrollForward()
+			elif command == brlapi.KEY_CMD_ROUTE:
+				braille.handler.buffer.routeTo(argument)
