ac21399cbc8c904ab914bf2058ffda499b174c85
James Teh <jamie@jantrid.net>
2008-10-01 20:42:39 +1000

    Suppress wx assertion errors altogether for now, as wx raises spurious assertion errors when a timer is stopped but there is already an event for that timer in the queue. This happens far too often with BrailleDisplayDriverWithCursor. This hack should be removed if this issue is fixed in wx.

--- source/core.py
+++ source/core.py
@@ -82,6 +82,10 @@ This initializes all modules such as audio, IAccessible, keyboard, mouse, and GU
 	import wx
 	log.info("Using wx version %s"%wx.version())
 	app = wx.App(redirect=False)
+	# HACK: wx currently raises spurious assertion failures when a timer is stopped but there is already an event in the queue for that timer.
+	# Unfortunately, these assertion exceptions are raised in the middle of other code, which causes problems.
+	# Therefore, disable assertions for now.
+	app.SetAssertMode(wx.PYAPP_ASSERT_SUPPRESS)
 	# We do support QueryEndSession events, but we don't want to do anything for them.
 	app.Bind(wx.EVT_QUERY_END_SESSION, lambda evt: None)
 	def onEndSession(evt):
