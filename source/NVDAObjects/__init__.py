d74b195771ee4398e64ed6b5c1ecb74c6414953d
James Teh <jamie@jantrid.net>
2008-10-03 11:12:37 +1000

    When testing whether two objects are the same object, use the "is" operator instead of comparing the result of id() on both objects.

--- source/NVDAObjects/__init__.py
+++ source/NVDAObjects/__init__.py
@@ -25,7 +25,7 @@ import virtualBufferHandler
 class NVDAObjectTextInfo(textHandler.TextInfo):
 
 	def __eq__(self,other):
-		if id(self)==id(other) or (isinstance(other,NVDAObjectTextInfo) and self._startOffset==other._startOffset and self._endOffset==other._endOffset):
+		if self is other or (isinstance(other,NVDAObjectTextInfo) and self._startOffset==other._startOffset and self._endOffset==other._endOffset):
 			return True
 		else:
 			return False
@@ -633,7 +633,7 @@ Tries to force this object to take the focus.
 				speech.speakText(text)
 
 	def event_stateChange(self):
-		if id(self)==id(api.getFocusObject()):
+		if self is api.getFocusObject():
 			speech.speakObjectProperties(self,states=True, reason=speech.REASON_CHANGE)
 
 	def event_focusEntered(self):
@@ -655,15 +655,15 @@ This method will speak the object if L{speakOnForeground} is true and this objec
 		speech.speakObjectProperties(self,name=True,role=True,reason=speech.REASON_FOCUS)
 
 	def event_valueChange(self):
-		if id(self)==id(api.getFocusObject()):
+		if self is api.getFocusObject():
 			speech.speakObjectProperties(self, value=True, reason=speech.REASON_CHANGE)
 
 	def event_nameChange(self):
-		if id(self)==id(api.getFocusObject()):
+		if self is api.getFocusObject():
 			speech.speakObjectProperties(self, name=True, reason=speech.REASON_CHANGE)
 
 	def event_descriptionChange(self):
-		if id(self)==id(api.getFocusObject()):
+		if self is api.getFocusObject():
 			speech.speakObjectProperties(self, description=True, reason=speech.REASON_CHANGE)
 
 	def _get_basicText(self):
