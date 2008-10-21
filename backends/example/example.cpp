1e1c8c1b853ef64500961d33c6477ef622153e53
Michael Curran <mick@kulgan.net>
2008-10-21 13:01:46 +1100

    Example backend: split out the definition of the Example backend class from its implementation, placing it in example.h. Moved VBufBackend_create in to its own file: create.cpp so that that example.cpp and example.h could be used in tests etc with out also including these factory functions.

--- backends/example/example.cpp
+++ backends/example/example.cpp
@@ -7,42 +7,32 @@
  */
 
 #include <base/base.h>
+#include "example.h"
 
-class __declspec(dllexport) TestVBufBackend_t: public VBufBackend_t {
-	public:
+ExampleVBufBackend_t::ExampleVBufBackend_t(int docHandle, int ID, VBufStorage_buffer_t* storageBuffer): VBufBackend_t(docHandle,ID,storageBuffer) {
+	DEBUG_MSG(L"Initializing Test backend");
+	storageBuffer->lock.acquire();
+	VBufStorage_controlFieldNode_t* nodeA=storageBuffer->addControlFieldNode(NULL,NULL,1,1,true);
+	nodeA->addAttribute(L"role",L"document");
+	VBufStorage_controlFieldNode_t* nodeAA=storageBuffer->addControlFieldNode(nodeA,NULL,1,2,true);
+	nodeAA->addAttribute(L"role",L"heading");
+	nodeAA->addAttribute(L"level",L"1");
+	VBufStorage_textFieldNode_t* nodeAAA=storageBuffer->addTextFieldNode(nodeAA,NULL,L"Test for Virtual Buffer Library");
+	VBufStorage_controlFieldNode_t* nodeAB=storageBuffer->addControlFieldNode(nodeA,nodeAA,1,3,true);
+	nodeAB->addAttribute(L"role",L"paragraph");
+	VBufStorage_textFieldNode_t* nodeABA=storageBuffer->addTextFieldNode(nodeAB,NULL,L"This content has been rendered by the Test backend. For a much better example of how the Virtual buffer library can be used, please visit the ");
+	VBufStorage_controlFieldNode_t* nodeABB=storageBuffer->addControlFieldNode(nodeAB,nodeABA,1,4,false);
+	nodeABB->addAttribute(L"role",L"link");
+	nodeABB->addAttribute(L"value",L"http://www.nvda-project.org/");
+	VBufStorage_textFieldNode_t* nodeABBA=storageBuffer->addTextFieldNode(nodeABB,NULL,L"NVDA website");
+	VBufStorage_textFieldNode_t* nodeABC=storageBuffer->addTextFieldNode(nodeAB,nodeABB,L"where you can find the NVDA screen reader.");
+	VBufStorage_controlFieldNode_t* nodeAC=storageBuffer->addControlFieldNode(nodeA,nodeAB,1,5,true);
+	nodeAC->addAttribute(L"role",L"paragraph");
+	VBufStorage_textFieldNode_t* nodeACA=storageBuffer->addTextFieldNode(nodeAC,NULL,L"Copyright (c)2008 NV Access");
+	storageBuffer->lock.release();
+	DEBUG_MSG(L"Test backend initialized");
+}
 
-	TestVBufBackend_t(int docHandle, int ID, VBufStorage_buffer_t* storageBuffer): VBufBackend_t(docHandle,ID,storageBuffer) {
-		DEBUG_MSG(L"Initializing Test backend");
-		storageBuffer->lock.acquire();
-		VBufStorage_controlFieldNode_t* nodeA=storageBuffer->addControlFieldNode(NULL,NULL,1,1,true);
-		nodeA->addAttribute(L"role",L"document");
-		VBufStorage_controlFieldNode_t* nodeAA=storageBuffer->addControlFieldNode(nodeA,NULL,1,2,true);
-		nodeAA->addAttribute(L"role",L"heading");
-		nodeAA->addAttribute(L"level",L"1");
-		VBufStorage_textFieldNode_t* nodeAAA=storageBuffer->addTextFieldNode(nodeAA,NULL,L"Test for Virtual Buffer Library");
-		VBufStorage_controlFieldNode_t* nodeAB=storageBuffer->addControlFieldNode(nodeA,nodeAA,1,3,true);
-		nodeAB->addAttribute(L"role",L"paragraph");
-		VBufStorage_textFieldNode_t* nodeABA=storageBuffer->addTextFieldNode(nodeAB,NULL,L"This content has been rendered by the Test backend. For a much better example of how the Virtual buffer library can be used, please visit the ");
-		VBufStorage_controlFieldNode_t* nodeABB=storageBuffer->addControlFieldNode(nodeAB,nodeABA,1,4,false);
-		nodeABB->addAttribute(L"role",L"link");
-		nodeABB->addAttribute(L"value",L"http://www.nvda-project.org/");
-		VBufStorage_textFieldNode_t* nodeABBA=storageBuffer->addTextFieldNode(nodeABB,NULL,L"NVDA website");
-		VBufStorage_textFieldNode_t* nodeABC=storageBuffer->addTextFieldNode(nodeAB,nodeABB,L"where you can find the NVDA screen reader.");
-		VBufStorage_controlFieldNode_t* nodeAC=storageBuffer->addControlFieldNode(nodeA,nodeAB,1,5,true);
-		nodeAC->addAttribute(L"role",L"paragraph");
-		VBufStorage_textFieldNode_t* nodeACA=storageBuffer->addTextFieldNode(nodeAC,NULL,L"Copyright (c)2008 NV Access");
-		storageBuffer->lock.release();
-		DEBUG_MSG(L"Test backend initialized");
-	}
-
-	~TestVBufBackend_t() {
-		DEBUG_MSG(L"Test backend being destroied");
-	}
-
-};
-
-extern "C" __declspec(dllexport) VBufBackend_t* VBufBackend_create(int docHandle, int ID, VBufStorage_buffer_t* storageBuffer) {
-	VBufBackend_t* backend=new TestVBufBackend_t(docHandle,ID,storageBuffer);
-	DEBUG_MSG(L"Created new backend at "<<backend);
-	return backend;
+ExampleVBufBackend_t::~ExampleVBufBackend_t() {
+	DEBUG_MSG(L"Test backend being destroied");
 }
