436133c385d5a328203dbd726c1d1ed87cc4b908
James Teh <jamie@jantrid.net>
2008-11-05 16:32:12 +1000

    Add a new SAPI4 synth driver written by Mick with some slight modifications by me. This new driver is called "sapi4". It is intended to replace both sapi4serotek and sapi4activeVoice, and these old drivers will be removed once this new driver has been tested. Please test thoroughly and report any regressions from the old drivers.

---
+++ source/synthDrivers/_sapi4.py
@@ -0,0 +1,162 @@
+#_sapi4.py
+# Contributed by Serotek Corporation under the GPL
+#A part of NonVisual Desktop Access (NVDA)
+#Copyright (C) 2006-2008 NVDA Contributors <http://www.nvda-project.org/>
+#This file is covered by the GNU General Public License.
+#See the file COPYING for more details.
+
+from ctypes import *
+from ctypes.wintypes import *
+from comtypes import *
+
+S_OK=0
+
+###comtypes class definitions
+
+SVFN_LEN = 262
+LANG_LEN = 64
+TTSDATAFLAG_TAGGED = 1
+TTSI_NAMELEN = SVFN_LEN
+TTSI_STYLELEN = SVFN_LEN
+TTSATTR_MINPITCH = 0
+TTSATTR_MAXPITCH = 0xffff
+TTSATTR_MINSPEED = 0
+TTSATTR_MAXSPEED = 0xffffffff
+TTSATTR_MINVOLUME=0
+TTSATTR_MAXVOLUME=0xffffffff
+TTSFEATURE_VOLUME=2
+TTSFEATURE_SPEED=4
+TTSFEATURE_PITCH=8
+TTSFEATURE_FIXEDAUDIO=1024
+
+LANGID = WORD
+QWORD = c_ulonglong
+
+class VOICECHARSET(c_int):
+    CHARSET_TEXT = 0
+    CHARSET_IPAPHONETIC = 1
+    CHARSET_ENGINEPHONETIC = 2
+
+class FILETIME(Structure):
+    _fields_ = [("dwLowDateTime", DWORD), ("dwHighDateTime", DWORD)]
+
+class LANGUAGEW(Structure):
+    _fields_ = [("LanguageID", LANGID),
+                ("szDialect", c_wchar * LANG_LEN)]
+LANGUAGE = LANGUAGEW
+
+class TTSMODEINFOW(Structure):
+    _fields_ = [("gEngine", GUID),
+                ("szMfgName", c_wchar * TTSI_NAMELEN),
+                ("szProductName", c_wchar * TTSI_NAMELEN),
+                ("gModeID", GUID),
+                ("szModeName", c_wchar * TTSI_NAMELEN),
+                ("language", LANGUAGEW),
+                ("szSpeaker", c_wchar * TTSI_NAMELEN),
+                ("szStyle", c_wchar * TTSI_STYLELEN),
+                ("wGender", WORD),
+                ("wAge", WORD),
+                ("dwFeatures", DWORD),
+                ("dwInterfaces", DWORD),
+                ("dwEngineFeatures", DWORD)]
+TTSMODEINFO = TTSMODEINFOW
+
+class SDATA(Structure):
+    _fields_ = [("pData", c_void_p), ("dwSize", DWORD)]
+
+class TTSMOUTH(Structure):
+    _fields_ = [("bMouthHeight", BYTE), ("bMouthWidth", BYTE),
+                ("bMouthUpturn", BYTE), ("bJawOpen", BYTE),
+                ("bTeethUpperVisible", BYTE), ("bTeethLowerVisible", BYTE),
+                ("bTonguePosn", BYTE), ("bLipTension", BYTE)]
+
+def TextSDATA(text):
+    d = SDATA()
+    d.pData = cast(c_wchar_p(text), c_void_p)
+    d.dwSize = (len(text) * sizeof(c_wchar)) + 1
+    return d
+
+class ITTSAttributesW(IUnknown):
+    _iid_ = GUID("{1287A280-4A47-101B-931A-00AA0047BA4F}")
+
+ITTSAttributesW._methods_ = [
+    STDMETHOD(HRESULT, "PitchGet", [POINTER(WORD)]),
+    STDMETHOD(HRESULT, "PitchSet", [WORD]),
+    STDMETHOD(HRESULT, "RealTimeGet", [POINTER(DWORD)]),
+    STDMETHOD(HRESULT, "RealTimeSet", [DWORD]),
+    STDMETHOD(HRESULT, "SpeedGet", [POINTER(DWORD)]),
+    STDMETHOD(HRESULT, "SpeedSet", [DWORD]),
+    STDMETHOD(HRESULT, "VolumeGet", [POINTER(DWORD)]),
+    STDMETHOD(HRESULT, "VolumeSet", [DWORD])
+]
+
+ITTSAttributes = ITTSAttributesW
+
+class ITTSBufNotifySink(IUnknown):
+    _iid_ = GUID("{E4963D40-C743-11cd-80E5-00AA003E4B50}")
+
+ITTSBufNotifySink._methods_ = [
+    STDMETHOD(HRESULT, "TextDataDone", [QWORD, DWORD]),
+    STDMETHOD(HRESULT, "TextDataStarted", [QWORD]),
+    STDMETHOD(HRESULT, "BookMark", [QWORD, DWORD]),
+    STDMETHOD(HRESULT, "WordPosition", [QWORD, DWORD])
+]
+
+class ITTSCentralW(IUnknown):
+    _iid_ = GUID("{28016060-4A47-101B-931A-00AA0047BA4F}")
+
+ITTSCentralW._methods_ = [
+    STDMETHOD(HRESULT, "Inject", [LPCWSTR]),
+    STDMETHOD(HRESULT, "ModeGet", [POINTER(TTSMODEINFOW)]),
+    STDMETHOD(HRESULT, "Phoneme", [VOICECHARSET, DWORD, SDATA, POINTER(SDATA)]),
+    STDMETHOD(HRESULT, "PosnGet", [POINTER(QWORD)]),
+    STDMETHOD(HRESULT, "TextData", [VOICECHARSET, DWORD, SDATA, c_void_p, GUID]),
+    STDMETHOD(HRESULT, "ToFileTime", [POINTER(QWORD), POINTER(FILETIME)]),
+    STDMETHOD(HRESULT, "AudioPause"),
+    STDMETHOD(HRESULT, "AudioResume"),
+    STDMETHOD(HRESULT, "AudioReset"),
+    STDMETHOD(HRESULT, "Register", [c_void_p, GUID, POINTER(DWORD)]),
+    STDMETHOD(HRESULT, "UnRegister", [DWORD])
+]
+
+ITTSCentral = ITTSCentralW
+
+class IAudioMultiMediaDevice(IUnknown):
+    _iid_ = GUID("{B68AD320-C743-11cd-80E5-00AA003E4B50}")
+
+IAudioMultiMediaDevice._methods_ = [
+    STDMETHOD(HRESULT, "CustomMessage", [c_uint, SDATA]),
+    STDMETHOD(HRESULT, "DeviceNumGet", [POINTER(DWORD)]),
+    STDMETHOD(HRESULT, "DeviceNumSet", [DWORD])
+]
+
+class ITTSEnumW(IUnknown):
+    _iid_ = GUID("{6B837B20-4A47-101B-931A-00AA0047BA4F}")
+
+ITTSEnumW._methods_ = [
+    STDMETHOD(HRESULT, "Next", [c_ulong, POINTER(TTSMODEINFOW),
+                                POINTER(c_ulong)]),
+    STDMETHOD(HRESULT, "Skip", [c_ulong]),
+    STDMETHOD(HRESULT, "Reset"),
+    STDMETHOD(HRESULT, "Clone", [POINTER(POINTER(ITTSEnumW))]),
+    STDMETHOD(HRESULT, "Select", [GUID, POINTER(POINTER(ITTSCentralW)),
+                                  POINTER(IUnknown)])
+]
+
+ITTSEnum = ITTSEnumW
+
+class ITTSNotifySinkW(IUnknown):
+    _iid_ = GUID("{C0FA8F40-4A46-101B-931A-00AA0047BA4F}")
+
+ITTSNotifySinkW._methods_ = [
+  STDMETHOD(HRESULT, "AttribChanged", [DWORD]),
+  STDMETHOD(HRESULT, "AudioStart", [QWORD]),
+  STDMETHOD(HRESULT, "AudioStop", [QWORD]),
+  STDMETHOD(HRESULT, "Visual", [QWORD, c_wchar, c_wchar, DWORD,
+                                POINTER(TTSMOUTH)])
+]
+
+ITTSNotifySink = ITTSNotifySinkW
+
+CLSID_MMAudioDest = GUID("{CB96B400-C743-11cd-80E5-00AA003E4B50}")
+CLSID_TTSEnumerator = GUID("{D67C0280-C743-11cd-80E5-00AA003E4B50}")
---
+++ source/synthDrivers/sapi4.py
@@ -0,0 +1,159 @@
+#synthDrivers/sapi4.py
+#A part of NonVisual Desktop Access (NVDA)
+#Copyright (C) 2006-2008 NVDA Contributors <http://www.nvda-project.org/>
+#This file is covered by the GNU General Public License.
+#See the file COPYING for more details.
+
+import _winreg
+from comtypes import COMObject
+from ctypes import *
+import synthDriverHandler
+from logHandler import log
+from _sapi4 import *
+import config
+import nvwave
+
+class SynthDriverBufSink(COMObject):
+	_com_interfaces_ = [ITTSBufNotifySink]
+
+	def __init__(self,synthDriver):
+		self._synthDriver=synthDriver
+		super(SynthDriverBufSink,self).__init__()
+
+	def ITTSBufNotifySink_BookMark(self, this, qTimeStamp, dwMarkNum):
+		self._synthDriver.lastIndex=dwMarkNum
+
+class SynthDriver(synthDriverHandler.SynthDriver):
+
+	name="sapi4"
+	description="Microsoft Speech API version 4"
+	hasVoice=True
+	hasRate=True
+	hasPitch=True
+	hasVolume=True
+
+	@classmethod
+	def check(cls):
+		try:
+			_winreg.OpenKey(_winreg.HKEY_CLASSES_ROOT, r"CLSID\%s" % CLSID_TTSEnumerator).Close()
+			return True
+		except WindowsError:
+			return False
+
+	def _fetchEnginesList(self):
+		enginesList=[]
+		self._ttsEngines.Reset()
+		while True:
+			mode=TTSMODEINFO()
+			fetched=c_ulong()
+			try:
+				self._ttsEngines.Next(1,byref(mode),byref(fetched))
+			except:
+				log.error("can't get next engine",exc_info=True)
+				break
+			if fetched.value==0:
+				break
+			enginesList.append(mode)
+		return enginesList
+
+	def initialize(self):
+		self.lastIndex=None
+		self._bufSink=SynthDriverBufSink(self)
+		self._ttsEngines=CoCreateInstance(CLSID_TTSEnumerator, ITTSEnumW)
+		self._enginesList=self._fetchEnginesList()
+		if len(self._enginesList)==0:
+			raise RuntimeError("No Sapi4 engines available")
+		self.voice=1
+
+	def speakText(self,text,index=None):
+		if index is not None:
+			text="\mrk=%d\\%s"%(index,text)
+		self._ttsCentral.TextData(VOICECHARSET.CHARSET_TEXT, TTSDATAFLAG_TAGGED,TextSDATA(text),self._bufSink._com_pointers_[ITTSBufNotifySink._iid_],ITTSBufNotifySink._iid_)
+
+	def cancel(self):
+		self._ttsCentral.AudioReset()
+
+	def pause(self,switch):
+		if switch:
+			self._ttsCentral.AudioPause()
+		else:
+			self._ttsCentral.AudioResume()
+
+	def _set_voice(self,val):
+		self._voice=val-1
+		self._ttsAudio=CoCreateInstance(CLSID_MMAudioDest,IAudioMultiMediaDevice)
+		self._ttsAudio.DeviceNumSet(nvwave.outputDeviceNameToID(config.conf["speech"]["outputDevice"], True))
+		self._ttsCentral=POINTER(ITTSCentralW)()
+		self._ttsEngines.Select(self._enginesList[self._voice].gModeID,byref(self._ttsCentral),self._ttsAudio)
+		self._ttsAttrs=self._ttsCentral.QueryInterface(ITTSAttributes)
+		#Find out rate limits
+		oldVal=DWORD()
+		self._ttsAttrs.SpeedGet(byref(oldVal))
+		self._ttsAttrs.SpeedSet(TTSATTR_MINSPEED)
+		newVal=DWORD()
+		self._ttsAttrs.SpeedGet(byref(newVal))
+		self._minRate=newVal.value
+		self._ttsAttrs.SpeedSet(TTSATTR_MAXSPEED)
+		self._ttsAttrs.SpeedGet(byref(newVal))
+		# ViaVoice (and perhaps other synths) doesn't seem to like the speed being set to maximum.
+		self._maxRate=newVal.value-1
+		self._ttsAttrs.SpeedSet(oldVal.value)
+		#Find out pitch limits
+		oldVal=WORD()
+		self._ttsAttrs.PitchGet(byref(oldVal))
+		self._ttsAttrs.PitchSet(TTSATTR_MINPITCH)
+		newVal=WORD()
+		self._ttsAttrs.PitchGet(byref(newVal))
+		self._minPitch=newVal.value
+		self._ttsAttrs.PitchSet(TTSATTR_MAXPITCH)
+		self._ttsAttrs.PitchGet(byref(newVal))
+		self._maxPitch=newVal.value
+		self._ttsAttrs.PitchSet(oldVal.value)
+		#Find volume limits
+		oldVal=DWORD()
+		self._ttsAttrs.VolumeGet(byref(oldVal))
+		self._ttsAttrs.VolumeSet(TTSATTR_MINVOLUME)
+		newVal=DWORD()
+		self._ttsAttrs.VolumeGet(byref(newVal))
+		self._minVolume=newVal.value
+		self._ttsAttrs.VolumeSet(TTSATTR_MAXVOLUME)
+		self._ttsAttrs.VolumeGet(byref(newVal))
+		self._maxVolume=newVal.value
+		self._ttsAttrs.VolumeSet(oldVal.value)
+
+	def _get_voice(self):
+		return self._voice+1
+
+	def getVoiceName(self,val):
+		return self._enginesList[val-1].szModeName
+
+	def _get_voiceCount(self):
+		return len(self._enginesList)
+
+	def _get_rate(self):
+		val=DWORD()
+		self._ttsAttrs.SpeedGet(byref(val))
+		return self._paramToPercent(val.value,self._minRate,self._maxRate)
+
+	def _set_rate(self,val):
+		val=self._percentToParam(val,self._minRate,self._maxRate)
+		self._ttsAttrs.SpeedSet(val)
+
+	def _get_pitch(self):
+		val=WORD()
+		self._ttsAttrs.PitchGet(byref(val))
+		return self._paramToPercent(val.value,self._minPitch,self._maxPitch)
+
+	def _set_pitch(self,val):
+		val=self._percentToParam(val,self._minPitch,self._maxPitch)
+		self._ttsAttrs.PitchSet(val)
+
+	def _get_volume(self):
+		val=DWORD()
+		self._ttsAttrs.VolumeGet(byref(val))
+		return self._paramToPercent(val.value&0xffff,self._minVolume&0xffff,self._maxVolume&0xffff)
+
+	def _set_volume(self,val):
+		val=self._percentToParam(val,self._minVolume&0xffff,self._maxVolume&0xffff)
+		val+=val<<16
+		self._ttsAttrs.VolumeSet(val)
