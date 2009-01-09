74df98c6ade5d317b1856ad1f84f13cc9a643655
Michael Curran <mick@kulgan.net>
2009-01-09 13:19:17 +1100

    Add copyright statements to the top of the new edit, scintilla and akelEdit modules in NVDAObjects/IAccessible.

--- source/NVDAObjects/IAccessible/akelEdit.py
+++ source/NVDAObjects/IAccessible/akelEdit.py
@@ -1,3 +1,9 @@
+#NVDAObjects/IAccessible/akelEdit.py
+#A part of NonVisual Desktop Access (NVDA)
+#Copyright (C) 2006-2007 NVDA Contributors <http://www.nvda-project.org/>
+#This file is covered by the GNU General Public License.
+#See the file COPYING for more details.
+
 from NVDAObjects.window.akelEdit import AkelEdit
 from . import IAccessible
 
--- source/NVDAObjects/IAccessible/edit.py
+++ source/NVDAObjects/IAccessible/edit.py
@@ -1,3 +1,9 @@
+#NVDAObjects/IAccessible/edit.py
+#A part of NonVisual Desktop Access (NVDA)
+#Copyright (C) 2006-2007 NVDA Contributors <http://www.nvda-project.org/>
+#This file is covered by the GNU General Public License.
+#See the file COPYING for more details.
+
 from . import IAccessible
 from NVDAObjects.window.edit import *
 
