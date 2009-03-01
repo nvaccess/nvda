f5788bd28880a907a722d8e76f53a91489d16de5
James Teh <jamie@jantrid.net>
2009-03-01 20:37:02 +1000

    backends/adobeAcrobat: Add and update header comments.

--- backends/adobeAcrobat/adobeAcrobat.cpp
+++ backends/adobeAcrobat/adobeAcrobat.cpp
@@ -1,3 +1,11 @@
+/**
+ * backends/adobeAcrobat/adobeAcrobat.cpp
+ * Part of the NV  Virtual Buffer Library
+ * This library is copyright 2007-2008 NV Virtual Buffer Library Contributors
+ * This library is licensed under the GNU Lesser General Public Licence. See license.txt which is included with this library, or see
+ * http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ */
+
 #define UNICODE
  #include <set>
  #include <sstream>
