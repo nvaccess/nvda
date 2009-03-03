dfd421e84b2d042c3185c4db89b3c5ec97a40da7
James Teh <jamie@jantrid.net>
2009-03-03 13:29:03 +1000

    NVDAObjects.IAccessible.adobe.AdobeAcrobatDocumentNode.event_valueChange(): Oops. IAccessibleChildID will always be 0. We should be checking event_childID instead.

--- source/NVDAObjects/IAccessible/adobe.py
+++ source/NVDAObjects/IAccessible/adobe.py
@@ -5,7 +5,7 @@ from . import IAccessible, getNVDAObjectFromEvent
 class AdobeAcrobatDocumentNode(IAccessible):
 
 	def event_valueChange(self):
-		if self.IAccessibleChildID==0 and winUser.isDescendantWindow(winUser.getForegroundWindow(),self.windowHandle):
+		if self.event_childID==0 and winUser.isDescendantWindow(winUser.getForegroundWindow(),self.windowHandle):
 			# This page may die and be replaced by another with the same event params, so always grab a new one.
 			obj = getNVDAObjectFromEvent(self.windowHandle, -4, 0)
 			if not obj:
