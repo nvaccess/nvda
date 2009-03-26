0bdce8481e32fd2f7fff84cd2508518866c9f82e
Michael Curran <mick@kulgan.net>
2009-03-26 21:32:01 +1100

    VBufBase rpc server: do not use auto-listen interfaces, instead just use RpcServerListen and rpcWaitServerListen in another thread etc. This didn't seem to work a long time ago, but works quite well now... though this should be tried on more than just my machine. If this does work, we then can again support Windows 2000.

--- base/rpcSrv.cpp
+++ base/rpcSrv.cpp
@@ -24,12 +24,12 @@ RPC_STATUS startServer() {
 	}
 	//Register the interfaces
 	printf("Registering interface...\n");
-	if((status=RpcServerRegisterIfEx(VBufRemote_VBufRemote_v2_0_s_ifspec,NULL,NULL,RPC_IF_AUTOLISTEN,RPC_C_LISTEN_MAX_CALLS_DEFAULT,NULL))!=RPC_S_OK) {
+	//if((status=RpcServerRegisterIfEx(VBufRemote_VBufRemote_v2_0_s_ifspec,NULL,NULL,RPC_IF_AUTOLISTEN,RPC_C_LISTEN_MAX_CALLS_DEFAULT,NULL))!=RPC_S_OK) {
+	if((status=RpcServerRegisterIf(VBufRemote_VBufRemote_v2_0_s_ifspec,NULL,NULL))!=RPC_S_OK) {
 		fprintf(stderr,"Error registering interface, code 0X%X\n",status);
 		return status;
 	}
 	printf("done\n");
-/*
 	//Start listening
 	printf("starting server\n");
 	if((status=RpcServerListen(1,RPC_C_LISTEN_MAX_CALLS_DEFAULT,TRUE))!=RPC_S_OK) {
@@ -37,13 +37,12 @@ RPC_STATUS startServer() {
 		return status;
 	}
 	CreateThread(NULL,0,(LPTHREAD_START_ROUTINE)RpcMgmtWaitServerListen,NULL,0,NULL);
-*/
 	return status;
 }
 
 void stopServer() {
 	printf("unregistering interface...\n");
-	RpcServerUnregisterIfEx(VBufRemote_VBufRemote_v2_0_s_ifspec,NULL,1);
+	RpcServerUnregisterIf(VBufRemote_VBufRemote_v2_0_s_ifspec,NULL,1);
 	printf("Done\n");
 }
 
