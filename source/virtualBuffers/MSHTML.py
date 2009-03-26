1f2547fb9f4458577e785990499ce3c951095683
Michael Curran <mick@kulgan.net>
2009-03-26 21:53:38 +1100

    MSHTML virtualBuffer: handle the situation when IAccessible::role can not be found as an attribute on a node. Its possible that some nodes may not have an equivilant IAccessible, plus the version of the backend with this attribute is not yet in misc deps.

--- source/virtualBuffers/MSHTML.py
+++ source/virtualBuffers/MSHTML.py
@@ -37,7 +37,7 @@ class MSHTMLTextInfo(VirtualBufferTextInfo):
 	def _normalizeControlField(self,attrs):
 		print attrs
 		level=None
-		accRole=attrs['IAccessible::role']
+		accRole=attrs.get('IAccessible::role',0)
 		accRole=int(accRole) if accRole.isdigit() else accRole
 		role=IAccessibleHandler.IAccessibleRolesToNVDARoles.get(accRole,controlTypes.ROLE_UNKNOWN)
 		states=set(IAccessibleHandler.IAccessibleStatesToNVDAStates[x] for x in [1<<y for y in xrange(32)] if int(attrs.get('IAccessible::state_%s'%x,0)) and x in IAccessibleHandler.IAccessibleStatesToNVDAStates)
