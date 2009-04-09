9850702f86ae840292f9c4ae8e378b22bc92fc95
Michael Curran <mick@kulgan.net>
2009-04-09 14:22:47 +1000

    Change dosvox appModule to use the new selfVoicing implementation. Please report if this doesn't work, as I do not have access to dosvox.

--- source/appModules/dosvox.py
+++ source/appModules/dosvox.py
@@ -1,10 +1,5 @@
 import _default
-import speech
 
 class AppModule(_default.AppModule):
 
-	def event_appGainFocus(self):
-		speech.speechMode=speech.speechMode_off
-
-	def event_appLoseFocus(self):
-		speech.speechMode=speech.speechMode_talk
+	selfVoicing=True
