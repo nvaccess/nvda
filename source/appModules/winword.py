da33253af99fc11f33b8c5818f4a29f7fe5bafd1
Michael Curran <mick@kulgan.net>
2009-04-10 09:49:32 +1000

    Added an MS Word appModule, which so far, adds some basic support for the Spell Checker. It is now possible to arrow around the spelling error field (which contains the line the current spelling error is on). Work still needs to be done to actually announce the current spelling error itself, though for now you could arrow though the line listening for 'in spelling error' 'out of spelling error' etc. This code is only tested so far in Word 2007.

---
+++ source/appModules/winword.py
@@ -0,0 +1,38 @@
+#appModules/winword.py
+#A part of NonVisual Desktop Access (NVDA)
+#Copyright (C) 2006-2009 NVDA Contributors <http://www.nvda-project.org/>
+#This file is covered by the GNU General Public License.
+#See the file COPYING for more details.
+
+import comtypes.client
+import comtypes.automation
+import controlTypes
+import winUser
+import IAccessibleHandler
+import NVDAObjects.IAccessible
+import speech
+import _default
+from NVDAObjects.window.winword import WordDocument
+
+class AppModule(_default.AppModule):
+
+	def event_NVDAObject_init(self,obj):
+		if obj.windowClassName=="_WwN" and obj.role==controlTypes.ROLE_EDITABLETEXT:
+				self.overlayCustomNVDAObjectClass(obj,SpellCheckErrorField,outerMost=True)
+
+class SpellCheckErrorField(WordDocument):
+
+	def _get_WinwordDocumentObject(self):
+		if not hasattr(self,'_WinwordDocumentObject'):
+			try:
+				self._WinwordDocumentObject=comtypes.client.dynamic.Dispatch(comtypes.client.GetActiveObject('word.application',interface=comtypes.automation.IDispatch)).activeWindow.activePane
+			except:
+				return None
+		return self._WinwordDocumentObject
+
+	def _get_name(self):
+		return super(SpellCheckErrorField,self).description
+
+	def _get_description(self):
+		return ""
+
