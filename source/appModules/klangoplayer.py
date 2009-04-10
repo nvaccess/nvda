15b7e501696db2ad19a3676f3528186888f59ea9
Peter Vágner <peter.v@datagate.sk>
2009-04-10 09:49:11 +0200

    * klangoplayer appmodule: make use of the new appModule selfVoicing property

--- source/appModules/klangoplayer.py
+++ source/appModules/klangoplayer.py
@@ -1,33 +1,7 @@
 #appModules/klangoplayer.py
 
 import _default
-from keyUtils import sendKey
-import config
 
 class AppModule(_default.AppModule):
 
-	def event_appGainFocus(self):
-		self.speakTypedCharacters=config.conf["keyboard"]["speakTypedCharacters"]
-		if self.speakTypedCharacters:
-			config.conf["keyboard"]["speakTypedCharacters"]=False
-		self.speakTypedWords=config.conf["keyboard"]["speakTypedWords"]
-		if self.speakTypedWords:
-			config.conf["keyboard"]["speakTypedWords"]=False
-		self.speakCommandKeys=config.conf["keyboard"]["speakCommandKeys"]
-		if self.speakCommandKeys:
-			config.conf["keyboard"]["speakCommandKeys"]=False
-
-	def event_appLoseFocus(self):
-		config.conf["keyboard"]["speakTypedCharacters"]=self.speakTypedCharacters
-		config.conf["keyboard"]["speakTypedWords"]=self.speakTypedWords
-		config.conf["keyboard"]["speakCommandKeys"]=self.speakCommandKeys
-
-	def script_sayAll(self,keyPress):
-		sendKey(([],"extendedinsert"))
-		sendKey((["extendedinsert"],"extendeddown"))
-	script_sayAll.__doc__ = _("reads from the system caret up to the end of the text, moving the caret as it goes")
-
-	def script_reportCurrentLine(self,keyPress):
-		sendKey(([],"extendedinsert"))
-		sendKey((["extendedinsert"],"extendedup"))
-	script_reportCurrentLine.__doc__=_("Reports the current line under the application cursor. Pressing this key twice will spell the current line")
+	selfVoicing=True
