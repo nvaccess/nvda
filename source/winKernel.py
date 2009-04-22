981d571b135385d7adffdee65dbc970777cc29d5
Michael Curran <mick@kulgan.net>
2009-04-22 19:56:07 +1000

    Re-implemented all the Windows console API functions in their own module (wincon.py) to match the Windows API header, wincon.h. These new implementations also raise much more useful errors which will help with debugging. All old implementations of the console API have been removed from winKernel.py. This commit completely breaks windows console support. A future commit will introduce completely new support.

--- source/winKernel.py
+++ source/winKernel.py
@@ -30,61 +30,17 @@ LOCALE_USER_DEFAULT=0x800
 DATE_LONGDATE=0x00000002 
 TIME_NOSECONDS=0x00000002
 
-class coordType(ctypes.Structure):
-	_fields_=[
-('x',ctypes.c_short),
-('y',ctypes.c_short),
-]
-
-class consoleWindowRectType(ctypes.Structure):
-	_fields_=[
-('left',ctypes.c_short),
-('top',ctypes.c_short),
-('right',ctypes.c_short),
-('bottom',ctypes.c_short),
-]
-
-class consoleScreenBufferInfoType(ctypes.Structure):
-	_fields_=[
-('consoleSize',coordType),
-('cursorPosition',coordType),
-('attributes',ctypes.c_short),
-('windowRect',consoleWindowRectType),
-('maxWindowSize',coordType),
-]
-
-def attachConsole(processID):
-	return kernel32.AttachConsole(processID)
-
-def freeConsole():
-	return kernel32.FreeConsole()
-
-def getStdHandle(handleID):
-	return kernel32.GetStdHandle(handleID)
-
-def getConsoleProcessList(processList,processCount):
-	return kernel32.GetConsoleProcessList(processList,processCount)
-
-def readConsoleOutputCharacter(handle,length,x,y):
-	point=coordType()
-	point.x=x
-	point.y=y
-	buf=ctypes.create_unicode_buffer(length)
-	kernel32.ReadConsoleOutputCharacterW(handle,buf,length,point,ctypes.byref(ctypes.c_int(0)))
-	return buf.value
-
-def getConsoleScreenBufferInfo(handle):
-	info=consoleScreenBufferInfoType()
-	kernel32.GetConsoleScreenBufferInfo(handle,ctypes.byref(info))
-	return info
+def GetStdHandle(handleID):
+	h=kernel32.GetStdHandle(handleID)
+	if h==0:
+		raise WinError()
+	return h
 
 def openProcess(*args):
 	return kernel32.OpenProcess(*args)
 
 def closeHandle(*args):
 	return kernel32.CloseHandle(*args)
-
-
 
 #added by Rui Batista to use on Say_battery_status script 
 #copied from platform sdk documentation (with required changes to work in python) 
---
+++ source/wincon.py
@@ -0,0 +1,51 @@
+from ctypes import *
+from ctypes.wintypes import *
+
+CONSOLE_REAL_OUTPUT_HANDLE=-2
+
+class COORD(Structure):
+	_fields_=[
+		('x',c_short),
+		('y',c_short),
+	]
+
+class CONSOLE_SCREEN_BUFFER_INFO(Structure):
+	_fields_=[
+		('consoleSize',COORD),
+		('cursorPosition',COORD),
+		('attributes',WORD),
+		('windowRect',SMALL_RECT),
+		('maxWindowSize',COORD),
+	]
+
+def ReadConsoleOutputCharacter(handle,length,x,y):
+	buf=create_unicode_buffer(length)
+	numCharsRead=c_int()
+	if windll.kernel32.ReadConsoleOutputCharacterW(handle,buf,length,COORD(x,y),byref(numCharsRead))==0:
+		raise WinError()
+	return buf.value
+
+def GetConsoleScreenBufferInfo(handle):
+	info=CONSOLE_SCREEN_BUFFER_INFO()
+	if windll.kernel32.GetConsoleScreenBufferInfo(handle,byref(info))==0:
+		raise WinError()
+	return info
+
+def FreeConsole():
+	if windll.kernel32.FreeConsole()==0:
+		raise WinError()
+
+def AttachConsole(processID):
+	if windll.kernel32.AttachConsole(processID)==0:
+		raise WinError()
+
+def GetConsoleWindow():
+	return windll.kernel32.GetConsoleWindow()
+
+def GetConsoleProcessList(maxProcessCount):
+	processList=(c_int*maxProcessCount)()
+	num=windll.kernel32.GetConsoleProcessList(processList,maxProcessCount)
+	return processList[0:num]
+
+ 
+
