e7c73c06bd3b36779597b8927ec2275c7a8b1167
James Teh <jamie@jantrid.net>
2009-06-04 09:29:24 +1000

    Update contributors and add copyright header for Alva BC6 driver.

--- source/brailleDisplayDrivers/alvaBC6.py
+++ source/brailleDisplayDrivers/alvaBC6.py
@@ -1,3 +1,9 @@
+#brailleDisplayDrivers/alvaBC6.py
+#A part of NonVisual Desktop Access (NVDA)
+#Copyright (C) 2006-2009 NVDA Contributors <http://www.nvda-project.org/>
+#This file is covered by the GNU General Public License.
+#See the file COPYING for more details.
+
 import braille
 import queueHandler
 from logHandler import log
