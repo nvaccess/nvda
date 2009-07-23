7f53737904a163087327d0aaa4429d049edbd774
Michael Curran <mick@kulgan.net>
2009-07-23 20:10:19 +1000

    generate.py: no longer need to compile appModules, brailleDisplayDrivers or synthDrivers as py2exe includes these packages.

--- source/generate.py
+++ source/generate.py
@@ -8,7 +8,6 @@
 This script:
 * Generates Python code for COM interfaces to avoid doing this at runtime;
 * Compiles source language files into binary form for use by NVDA;
-* Compiles appModules, synthDrivers and brailleDisplayDrivers into Python byte code to eliminate the need to do this on launch.
 This should be run prior to executing NVDA from a clean source tree for the first time and before building a binary distribution with py2exe.
 """
 
@@ -32,7 +31,6 @@ COM_INTERFACES = (
 	("Rich Edit library", comtypes.client.GetModule, "msftedit.dll"),
 	("SAPI 5", comtypes.client.CreateObject, "Sapi.SPVoice"),
 )
-COMPILE_DIRS = ("appModules", "synthDrivers", "brailleDisplayDrivers")
 
 def main():
 	print "COM interfaces:"
@@ -51,10 +49,6 @@ def main():
 		print f
 		os.spawnv(os.P_WAIT,r"%s\python.exe"%sys.exec_prefix,['python',r'"%s\Tools\i18n\msgfmt.py"'%sys.exec_prefix,f])
 	print
-
-	print "Byte code compilation:"
-	for dir in COMPILE_DIRS:
-		compileall.compile_dir(dir, maxlevels=0)
 
 if __name__ == "__main__":
 	main()
