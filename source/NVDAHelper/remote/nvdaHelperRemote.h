3a61ecad9104df3909803c6f0435bdb5ce50aba2
Michael Curran <mick@kulgan.net>
2009-07-31 16:22:36 +1000

    Rename nvdaHelper to nvdaHelperRemote inside remote. This incodes now generating nvdaHelperRemote.dll rather than nvdaHelper.dll.

--- source/NVDAHelper/remote/nvdaHelperRemote.h
+++ source/NVDAHelper/remote/nvdaHelperRemote.h
@@ -1,8 +1,8 @@
 //Copyright (c) 2007 Michael Curran <mick@kulgan.net>
 //This file is covered by the GNU General Public Licence
 
-#ifndef NVDAHELPER_H
-#define NVDAHELPER_H
+#ifndef NVDAHELPERREMOTE_H
+#define NVDAHELPERREMOTE_H
 
 #define DLLEXPORT extern "C" __declspec(dllexport)
 
