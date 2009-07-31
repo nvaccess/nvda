50eb340be7f27ccdf606ac96567211e595b580df
Michael Curran <mick@kulgan.net>
2009-07-31 21:19:39 +1000

    Move files from nvVBufLib's client directory to nvdaHelper's local directory. VBufClient.dll will now be called nvdaHelperLocal.dll. renamed VBufClient_connect and VBufClient_disconnect to createConnection and destroyConnection respectively. These functions just deal with creating or destroying a binding handle. also the address they bind to now is nvdaHelperRemote_pid instead of nvVBufServer_pid.  (where pid is the process ID of the process you are connecting to). nvdaHelperLocal contains these two functions for managing a connection, and of course also the client stubs for the VBuf interface. The root nvdaHelper Makefile now also builds local. These changes all now mean that nvdaHelperLocal.dll is built.

---
+++ source/NVDAHelper/local/nvdaHelperLocal.cpp
@@ -0,0 +1,31 @@
+#define UNICODE
+#include <cstdio>
+#include <sstream>
+#include <windows.h>
+#include "nvdaHelperLocal.h"
+
+handle_t createConnection(int processID) {
+	RPC_STATUS rpcStatus;
+	std::wostringstream addr;
+	addr<<L"ncalrpc:[nvdaHelperRemote_"<<processID<<L"]";
+	handle_t bindingHandle;
+	if((rpcStatus=RpcBindingFromStringBinding((RPC_WSTR)(addr.str().c_str()),&bindingHandle))!=RPC_S_OK) {
+		fprintf(stderr,"Error creating binding handle from string binding, rpc code 0X%X\n",rpcStatus);
+		return NULL;
+	} 
+	return bindingHandle;
+}
+
+void destroyConnection(handle_t bindingHandle) {
+	RpcBindingFree(&bindingHandle);
+}
+
+//memory allocation functions
+
+void* __RPC_USER midl_user_allocate(size_t size) {
+	return malloc(size);
+}
+
+void __RPC_USER midl_user_free(void* p) {
+	free(p);
+}
