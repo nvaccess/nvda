5959bf2124336d59188d1fbe1ef058d9163a0651
C:\Users\mick\AppData\Roaming\The Bat! <>
2009-07-31 15:34:20 +1000

    Added hookRegistration.h which was previously forgotton.

---
+++ source/NVDAHelper/hookRegistration.h
@@ -0,0 +1,43 @@
+//Copyright (c) 2007 Michael Curran <mick@kulgan.net>
+//This file is covered by the GNU General Public Licence
+
+#ifndef HOOKREGISTRATION_H
+#define HOOKREGISTRATION_H
+
+#include <windows.h>
+
+//Win event registration
+
+/**
+ * Registers a callback function to be called with in future win events fired for the calling thread
+ * @param procHook the callback function which should be called
+ * @return true if the hook was registered, false otherwise.
+ */
+bool registerWinEventHook(WINEVENTPROC hookProc);
+
+/**
+ * Unregisters a previously registered callback function for a win event for the calling thread
+ * @param hookProc the callback function to be unregistered
+ * @return True if it was unregistered, false otherwize.
+ */
+bool unregisterWinEventHook(WINEVENTPROC hookProc);
+
+//Windows hook registration
+
+/**
+ * Registers a callback function to be called with in future windows hooks fired for the calling thread with the given hook type
+ * @param hookType the type of windows hook (WH_CALLWNDPROC, WH_GETMESSAGE)
+ * @param procHook the callback function which should be called
+ * @return true if the hook was registered, false otherwise.
+ */
+bool registerWindowsHook(int hookType, HOOKPROC hookProc);
+
+/**
+ * Unregisters a previously registered callback function for a windows hook for the calling thread with the given hook type
+ * @param hookType the type of windows hook
+ * @param hookProc the callback function to be unregistered
+ * @return True if it was unregistered, false otherwize.
+ */
+bool unregisterWindowsHook(int hookType, HOOKPROC hookProc);
+
+#endif
