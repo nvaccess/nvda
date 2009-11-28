d6dabb12c6299c038644933043cdd95eaf8e6fc7
Michael Curran <mick@kulgan.net>
2009-11-28 21:31:01 +1100

    nvdaHelperRemote IAccessible2 live region support: *Provide more comments in the code. *getTextFromIAccessible: if the object does not support IAccessibleText, then recurse in to its children using accessibleChildren. *getTextFromIAccessible: when looping through characters in the text checking for embedded objects, save off the character for later use. This means less array indexing, but more importantly fixes a bug where if a normal char was found, the char that was placed in the string for speaking was totally incorrect -- startOffset+index, rather than just index. *For a 'show' event, where both additions and text are allowed, force allowText to false if this object's parent does not support IAccessibleText. If the parent doesn't support IAccessibleText, that means there will be no textInserted event and there fore we must not ignore this show event.

--- source/NVDAHelper/remote/ia2LiveRegions.cpp
+++ source/NVDAHelper/remote/ia2LiveRegions.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <sstream>
 #include <windows.h>
 #include <ia2/ia2.h>
 #include <interfaces/nvdaController/nvdaController.h>
@@ -20,12 +21,32 @@ void speakText(const wchar_t* text) {
 void getTextFromIAccessible(wstring& textBuf, IAccessible2* pacc2, bool useNewText=false, bool recurse=true) {
 	IAccessibleText* paccText=NULL;
 	if(pacc2->QueryInterface(IID_IAccessibleText,(void**)&paccText)!=S_OK||!paccText) {
+		long childCount=0;
+		if(pacc2->get_accChildCount(&childCount)!=S_OK) {
+			textBuf.append(L"unknown");
+			return;
+		}
+		VARIANT* varChildren=new VARIANT[childCount];
+		AccessibleChildren(pacc2,0,childCount,varChildren,&childCount);
+		for(int i=0;i<childCount;i++) {
+			if(varChildren[i].vt==VT_DISPATCH) {
+				IAccessible2* pacc2Child=NULL;
+				if(varChildren[i].pdispVal->QueryInterface(IID_IAccessible2,(void**)&pacc2Child)==S_OK) {
+					getTextFromIAccessible(textBuf,pacc2Child,false,true);
+					pacc2Child->Release();
+				}
+			}
+			VariantClear(varChildren+i);
+		}
+		delete varChildren;
 		return;
 	}
+	//We can use IAccessibleText because it exists
 	BSTR bstrText=NULL;
 	long startOffset=0;
+	//If requested, get the text from IAccessibleText::newText rather than just IAccessibleText::text.
 	if(useNewText) {
-		IA2TextSegment newSeg;
+		IA2TextSegment newSeg={0};
 		if(paccText->get_newText(&newSeg)==S_OK&&newSeg.text) {
 			bstrText=newSeg.text;
 			startOffset=newSeg.start;
@@ -33,6 +54,7 @@ void getTextFromIAccessible(wstring& textBuf, IAccessible2* pacc2, bool useNewTe
 	} else {
 		paccText->get_text(0,-1,&bstrText);
 	}
+	//If we got text, add it to  the string provided, however if there are embedded objects in the text, recurse in to these
 	if(bstrText) {
 		long textLength=SysStringLen(bstrText);
 		IAccessibleHypertext* paccHypertext=NULL;
@@ -41,8 +63,9 @@ void getTextFromIAccessible(wstring& textBuf, IAccessible2* pacc2, bool useNewTe
 			textBuf.append(bstrText);
 		} else {
 			for(long index=0;index<textLength;index++) {
+				wchar_t realChar=bstrText[index];
 				bool charAdded=false;
-				if(bstrText[index]==L'\xfffc') {
+				if(realChar==L'\xfffc') {
 					long hyperlinkIndex;
 					if(paccHypertext&&paccHypertext->get_hyperlinkIndex(startOffset+index,&hyperlinkIndex)==S_OK) {
 						IAccessibleHyperlink* paccHyperlink=NULL;
@@ -58,7 +81,7 @@ void getTextFromIAccessible(wstring& textBuf, IAccessible2* pacc2, bool useNewTe
 					}
 				}
 				if(!charAdded) {
-					textBuf.append(1,bstrText[startOffset+index]);
+					textBuf.append(1,realChar);
 					charAdded=true;
 				}
 			}
@@ -128,12 +151,28 @@ void CALLBACK winEventProcHook(HWINEVENTHOOK hookID, DWORD eventID, HWND hwnd, l
 	i=attribsMap.find(L"container-relevant");
 	bool allowAdditions=false;
 	bool allowText=false;
+	//If relevant is not specify we will default to additions and text, if all is specified then we also use additions and text
 	if(i==attribsMap.end()||i->second.compare(L"all")==0) {
 		allowText=allowAdditions=true;
-	} else {
+	} else { //we support additions if its specified, we support text if its specified
 		allowText=(i->second.find(L"text",0)!=wstring::npos);
 		allowAdditions=(i->second.find(L"additions",0)!=wstring::npos);
 	} 
+	//If additions and text are specified, and this is a show event, turn off allowText if this IAccessible's parent doesn't support accessibleText.
+	//This is necessary so that we still announce additions via the show event, rather than waiting for the textInserted event which will never arrive.
+	if(allowText&&allowAdditions&&eventID==EVENT_OBJECT_SHOW) {
+		IDispatch* pdispParent=NULL;
+		pacc2->get_accParent(&pdispParent);
+		if(pdispParent) {
+			IAccessibleText* paccTextParent=NULL;
+			if(pdispParent->QueryInterface(IID_IAccessibleText,(void**)&paccTextParent)!=S_OK) {
+				allowText=false;
+			} else {
+				paccTextParent->Release();
+			}
+			pdispParent->Release();
+		}
+	}
 	wstring textBuf;
 	if(!allowText&&allowAdditions&&eventID==EVENT_OBJECT_SHOW) {
 		getTextFromIAccessible(textBuf,pacc2,false,true);
