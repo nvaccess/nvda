461e712274bc533af855500515b0d04b042e2e06
Michael Curran <mick@kulgan.net>
2010-02-17 22:15:20 +1100

    MSRPC SCons tool: *Allow for providing a .acf file as one of the sources. The builder will now make sure that /acf is inserted before each .acf that appears in the resulting commandline. We use an action generator to do this. *The build emitter was accidentilly using the first source file as the base for all target names. It now uses the first target if it exists, else it falls back to the first source.

--- source/NVDAHelper/site_scons/site_tools/msrpc.py
+++ source/NVDAHelper/site_scons/site_tools/msrpc.py
@@ -4,10 +4,22 @@
 from SCons import Util
 from SCons.Builder import Builder
 
+#This build emitter tells the builder that a header file, a client stub c file, and a server stub c file will be generated 
 def MSRPCStubs_buildEmitter(target,source,env):
-	base,ext=Util.splitext(str(source[0]))
+	base,ext=Util.splitext(str(target[0] if len(target)>0 else source[0]))
 	targets=[base+'.h',base+'_c.c',base+'_s.c']
 	return (targets,source)
+
+def MSRPCStubs_builder_actionGenerator(target,source,env,for_signature):
+	#As we support .acf files as sources, we must insert the /acf switch before each one
+	sourcesString=" ".join([('/acf %s'%src if str(src).endswith('.acf') else str(src)) for src in source])
+	return "${MIDL} ${MIDLFLAGS} /header ${TARGETS[0]} /cstub ${TARGETS[1]} /sstub ${TARGETS[2]} %s"%sourcesString
+
+MSRPCStubs_builder=Builder(
+	generator=MSRPCStubs_builder_actionGenerator,
+	src_suffix=['.idl','.acf'],
+	emitter=MSRPCStubs_buildEmitter,
+)
 
 def exists(env):
 	from SCons.Tool import midl
@@ -17,4 +29,4 @@ def generate(env):
 	if not 'MIDL' in env:
 		from SCons.Tool import midl
 		midl.generate(env)
-	env['BUILDERS']['MSRPCStubs']=Builder(action="${MIDL} ${MIDLFLAGS} /header ${TARGETS[0]} /cstub ${TARGETS[1]} /sstub ${TARGETS[2]} $SOURCE",suffix='.h',src_suffix=['.idl'],emitter=MSRPCStubs_buildEmitter)
+	env['BUILDERS']['MSRPCStubs']=MSRPCStubs_builder
