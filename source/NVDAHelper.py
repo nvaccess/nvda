7430d1d4e957c1d9a6203e753ef64dcb6e189b29
Michael Curran <mick@kulgan.net>
2010-02-20 23:05:12 +1100

    *nvdaControllerInternal interface: add  the method nvdaControllerInternal_logMessage. This allows for NVDA to remotely log a message to NVDA, also including such info as file, function, line number and process ID. *nvdaHelper.py: implemented nvdaControllerInternal_logMessage: use the info such as file, function, line number and app name (via the processID) as the codepath when logging the provided message.

--- source/NVDAHelper.py
+++ source/NVDAHelper.py
@@ -71,6 +71,13 @@ def _lookupKeyboardLayoutNameWithHexString(layoutString):
 		log.debugWarning("Could not find reg value 'Layout Text' for reg key %s"%layoutString)
 		return None
 
+@WINFUNCTYPE(c_long,c_long,c_long,c_wchar_p,c_wchar_p,c_wchar_p,c_long,c_wchar_p)
+def nvdaControllerInternal_logMessage(pid,tid,level,fileName,funcName,lineNo,message):
+	from appModuleHandler import getAppNameFromProcessID
+	codepath="RPC: %s, %s, %s, line %d"%(getAppNameFromProcessID(pid,includeExt=True),fileName,funcName, lineNo)
+	getattr(log,level)(message,codepath=codepath)
+	return 0
+
 @WINFUNCTYPE(c_long,c_long,c_ulong,c_wchar_p)
 def nvdaControllerInternal_inputLangChangeNotify(threadID,hkl,layoutString):
 	global lastInputLangChangeTime
@@ -163,6 +170,7 @@ def initialize():
 		("nvdaController_cancelSpeech",nvdaController_cancelSpeech),
 		("nvdaController_brailleMessage",nvdaController_brailleMessage),
 		("nvdaControllerInternal_inputLangChangeNotify",nvdaControllerInternal_inputLangChangeNotify),
+		("nvdaControllerInternal_logMessage",nvdaControllerInternal_logMessage),
 	]:
 		try:
 			_setDllFuncPointer(localLib,"_%s"%name,func)
--- source/NVDAHelper/local/nvdaControllerInternal.c
+++ source/NVDAHelper/local/nvdaControllerInternal.c
@@ -5,3 +5,8 @@ __declspec(dllexport) error_status_t(__stdcall *_nvdaControllerInternal_inputLan
 error_status_t __stdcall nvdaControllerInternal_inputLangChangeNotify(const long threadID, const unsigned long hkl, const wchar_t* layoutString) {
 	return _nvdaControllerInternal_inputLangChangeNotify(threadID,hkl,layoutString);
 }
+
+__declspec(dllexport) error_status_t(__stdcall *_nvdaControllerInternal_logMessage)(const long, const long, const wchar_t*, const wchar_t*, const wchar_t*, const long, const wchar_t*);
+error_status_t __stdcall nvdaControllerInternal_logMessage(const long processID, const long threadID, const wchar_t* level, const wchar_t* fileName, const wchar_t* funcName, const long lineNo, const wchar_t* message) {
+	return _nvdaControllerInternal_logMessage(processID,threadID,level,fileName,funcName,lineNo,message);
+}
