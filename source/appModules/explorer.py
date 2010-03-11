a0ada14a84e0b93f811960bc042b3e1eaeedd64e
Aleksey Sadovoy <lex@onm.su>
2010-03-12 00:34:16 +0200

    appModules: remove unnecessary imports and slightly update copyright year.

--- source/appModules/explorer.py
+++ source/appModules/explorer.py
@@ -1,12 +1,11 @@
 #appModules/explorer.py
 #A part of NonVisual Desktop Access (NVDA)
-#Copyright (C) 2006-2008 NVDA Contributors <http://www.nvda-project.org/>
+#Copyright (C) 2006-2010 NVDA Contributors <http://www.nvda-project.org/>
 #This file is covered by the GNU General Public License.
 #See the file COPYING for more details.
 
 import _default
 import controlTypes
-import api
 import speech
 import eventHandler
 from NVDAObjects.window import Window
--- source/appModules/winamp.py
+++ source/appModules/winamp.py
@@ -1,12 +1,11 @@
 #appModules/winamp.py
 #A part of NonVisual Desktop Access (NVDA)
-#Copyright (C) 2006-2007 NVDA Contributors <http://www.nvda-project.org/>
+#Copyright (C) 2006-2010 NVDA Contributors <http://www.nvda-project.org/>
 #This file is covered by the GNU General Public License.
 #See the file COPYING for more details.
 
 from ctypes import *
 from ctypes.wintypes import *
-import IAccessibleHandler
 import winKernel
 import winUser
 from keyUtils import sendKey
