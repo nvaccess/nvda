40c3eb7f06916a93ae4857822407f096e0b34cb6
James Teh <jamie@jantrid.net>
2010-03-25 14:28:42 +1000

    Initial core code for new input framework. This will allow for multiple input sources to be handled in a unified way. Every gesture of input (key press on keyboard, key press on Braille display, etc.) should be represented by an InputGesture instance. InputGestures determine the effect on speech when executed, an associated script, the key to use when looking up the gesture in maps, the name to be displayed to the user, etc. InputGestures can then be executed using InputManager.executeGesture.

---
+++ source/inputCore.py
@@ -0,0 +1,135 @@
+#inputCore.py
+#A part of NonVisual Desktop Access (NVDA)
+#This file is covered by the GNU General Public License.
+#See the file COPYING for more details.
+#Copyright (C) 2010 James Teh <jamie@jantrid.net>
+
+import baseObject
+import scriptHandler
+import queueHandler
+import api
+import speech
+import config
+from logHandler import log
+
+class NoInputGestureAction(LookupError):
+	"""Informs that there is no action to execute for a gesture.
+	"""
+
+class InputGesture(baseObject.AutoPropertyObject):
+	"""A single gesture of input from the user.
+	For example, this could be a key press on a keyboard or Braille display or a click of the mouse.
+	"""
+	cachePropertiesByDefault = True
+
+	def _get_mapKeys(self):
+		"""The key(s) which will be used in input gesture maps to represent this gesture.
+		These keys will be looked up in order until a match is found.
+		A single key should take the form: C{source:identifier}
+		where C{source} is a few characters representing the source of this gesture
+		and C{identifier} identifies the specific gesture.
+		An example key is: C{kb(desktop):NVDA+1}
+		@return: One or more keys which uniquely identify this gesture.
+		@rtype: list or tuple of str
+		"""
+		raise NotImplementedError
+
+	def _get_displayName(self):
+		"""The name of this gesture as presented to the user.
+		@return: The display name.
+		@rtype: str
+		"""
+		raise NotImplementedError
+
+	#: Whether this gesture should be reported when reporting of command gestures is enabled.
+	#: @type: bool
+	shouldReportAsCommand = True
+
+	SPEECHEFFECT_CANCEL = "cancel"
+	SPEECHEFFECT_PAUSE = "pause"
+	SPEECHEFFECT_RESUME = "resume"
+	#: The effect on speech when this gesture is executed; one of the SPEECHEFFECT_* constants or C{None}.
+	speechEffectWhenExecuted = SPEECHEFFECT_CANCEL
+
+	#: Whether this gesture is only a modifier, in which case it will not search for a script to execute.
+	#: @type: bool
+	isModifier = False
+
+	#: Whether this gesture should bypass input help.
+	#: @type: bool
+	bypassInputHelp = False
+
+	def reportExtra(self):
+		"""Report any extra information about this gesture to the user.
+		This is called just after command gestures are reported.
+		For example, it could be used to report toggle states.
+		"""
+
+	def _get_script(self):
+		"""The script bound to this input gesture.
+		@return: The script to be executed.
+		@rtype: script function
+		"""
+		# FIXME: Support other gesture types.
+		import keyUtils
+		return scriptHandler.findScript(keyUtils.key(self.keyName))
+
+	def send(self):
+		"""Send this gesture to the operating system.
+		This is not possible for all sources.
+		@raise NotImplementedError: If the source does not support sending of gestures.
+		"""
+		raise NotImplementedError
+
+class InputManager(baseObject.AutoPropertyObject):
+	"""Manages functionality related to input from the user.
+	Input includes key presses on the keyboard, as well as key presses on Braille displays, etc.
+	"""
+
+	def __init__(self):
+		#: Whether input help is enabled, wherein the function of each key pressed by the user is reported but not executed.
+		#: @type: bool
+		self.isInputHelpActive = False
+
+	def executeGesture(self, gesture):
+		"""Perform the action associated with a gesture.
+		@param gesture: The gesture to execute.
+		@type gesture: L{InputGesture}
+		@raise NoInputGestureAction: If there is no action to perform.
+		"""
+		if api.getFocusObject().appModule.selfVoicing:
+			raise NoInputGestureAction
+
+		speechEffect = gesture.speechEffectWhenExecuted
+		if speechEffect == gesture.SPEECHEFFECT_CANCEL:
+			queueHandler.queueFunction(queueHandler.eventQueue, speech.cancelSpeech)
+		elif speechEffect in (gesture.SPEECHEFFECT_PAUSE, gesture.SPEECHEFFECT_RESUME):
+			queueHandler.queueFunction(queueHandler.eventQueue, speech.pauseSpeech, speechEffect == gesture.SPEECHEFFECT_PAUSE)
+
+		if log.isEnabledFor(log.IO) and not gesture.isModifier:
+			log.io("Input: %s" % gesture.mapKeys[0])
+
+		if self.isInputHelpActive and not gesture.bypassInputHelp:
+			scriptHandler.queueScript("inputHelp", gesture)
+			return
+
+		if gesture.isModifier:
+			raise NoInputGestureAction
+
+		if config.conf["keyboard"]["speakCommandKeys"] and gesture.shouldReportAsCommand:
+			queueHandler.queueFunction(queueHandler.eventQueue, speech.speakMessage, gesture.displayName)
+
+		gesture.reportExtra()
+
+		script = gesture.script
+		if script:
+			# FIXME: Support other gesture types.
+			import keyUtils
+			scriptHandler.queueScript(script, keyUtils.key(gesture.keyName))
+			return
+
+		raise NoInputGestureAction
+
+#: The singleton input manager instance.
+#: @type: L{InputManager}
+manager = InputManager()
