7f739a9767e28ce93bef9668ee06f42da65fe21b
Michael Curran <mick@kulgan.net>
2010-03-26 14:18:08 +1100

    base NVDAObject's presentationType property: rather than just classing staticText as layout if it has no name and no description, instead  class it as layout if its text (from its textInfo)  is empty or only contains whitespace. Note that  objects with no specific textInfo have an NVDAObjectTextInfo who's text is made up of name, description and value, so this works quite well. This change specifically helps MSHTML as previously all textNodes could not be navigated to with review, but now textNodes with meaningfull text can be.

--- source/NVDAObjects/__init__.py
+++ source/NVDAObjects/__init__.py
@@ -461,11 +461,17 @@ class NVDAObject(baseObject.ScriptableObject):
 		role=self.role
 		if controlTypes.STATE_FOCUSED in states:
 			return self.presType_content
+
+		#Static text should be content only if it really use usable text
+		if role==controlTypes.ROLE_STATICTEXT:
+			text=self.makeTextInfo(textInfos.POSITION_ALL).text
+			return self.presType_content if text and not text.isspace() else self.presType_layout
+
 		if role in (controlTypes.ROLE_UNKNOWN, controlTypes.ROLE_PANE, controlTypes.ROLE_ROOTPANE, controlTypes.ROLE_LAYEREDPANE, controlTypes.ROLE_SCROLLPANE, controlTypes.ROLE_SECTION,controlTypes.ROLE_PARAGRAPH,controlTypes.ROLE_TITLEBAR):
 			return self.presType_layout
 		name = self.name
 		description = self.description
-		if not name and not description and role in (controlTypes.ROLE_WINDOW,controlTypes.ROLE_LABEL,controlTypes.ROLE_PANEL, controlTypes.ROLE_PROPERTYPAGE, controlTypes.ROLE_TEXTFRAME, controlTypes.ROLE_GROUPING,controlTypes.ROLE_STATICTEXT,controlTypes.ROLE_OPTIONPANE,controlTypes.ROLE_INTERNALFRAME,controlTypes.ROLE_FORM,controlTypes.ROLE_TABLEBODY):
+		if not name and not description and role in (controlTypes.ROLE_WINDOW,controlTypes.ROLE_LABEL,controlTypes.ROLE_PANEL, controlTypes.ROLE_PROPERTYPAGE, controlTypes.ROLE_TEXTFRAME, controlTypes.ROLE_GROUPING,controlTypes.ROLE_OPTIONPANE,controlTypes.ROLE_INTERNALFRAME,controlTypes.ROLE_FORM,controlTypes.ROLE_TABLEBODY):
 			return self.presType_layout
 		if not name and not description and role in (controlTypes.ROLE_TABLE,controlTypes.ROLE_TABLEROW,controlTypes.ROLE_TABLECOLUMN,controlTypes.ROLE_TABLECELL) and not config.conf["documentFormatting"]["reportTables"]:
 			return self.presType_layout
