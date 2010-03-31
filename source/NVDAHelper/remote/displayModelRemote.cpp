7d89539025191d89dfeb23611fc813cd856dab92
Michael Curran <mick@kulgan.net>
2010-03-31 12:38:19 +1100

    nvdaHelper display model: a first try at a basic full off-screen model. displayModel_getWindowTextInRect now includes text not only from the window given, but also from all the given window's descendants as well. It manages this by making a temporary display model, collecting all the descendant windows in z-order with EnumChildWindows, and then  walking trhough them backwards (from most behind to most in front), and if the window does have a display model, copying its content in to the temp model. Finally it grabs all the text from the temp model with renderText. This basic approach may mean you see some extra text that may be really covered by another window's background, though its a good experiment for performance and also does not hinder the way we currently make use of getWindowTextInRect.

--- source/NVDAHelper/remote/displayModelRemote.cpp
+++ source/NVDAHelper/remote/displayModelRemote.cpp
@@ -5,13 +5,29 @@
 
 using namespace std;
 
+BOOL CALLBACK EnumChildWindowsProc(HWND hwnd, LPARAM lParam) {
+	((deque<HWND>*)lParam)->push_back(hwnd);
+	return TRUE;
+}
+
 error_status_t displayModelRemote_getWindowTextInRect(handle_t bindingHandle, const long windowHandle, const int left, const int top, const int right, const int bottom, BSTR* textBuf) {
-	displayModelsByWindow_t::iterator i=displayModelsByWindow.find((HWND)windowHandle);
-	if(i!=displayModelsByWindow.end()) {
-		wstring text;
-		RECT rect={left,top,right,bottom};
-		i->second->renderText(&rect,text);
-		*textBuf=SysAllocString(text.c_str());
+	const HWND hwnd=(HWND)windowHandle;
+	deque<HWND> windowDeque;
+	EnumChildWindows(hwnd,EnumChildWindowsProc,(LPARAM)&windowDeque);
+	windowDeque.push_back(hwnd);
+	displayModel_t tempModel;
+	RECT textRect={left,top,right,bottom};
+	RECT curWindowRect;
+	for(deque<HWND>::reverse_iterator i=windowDeque.rbegin();i!=windowDeque.rend();i++) {
+		GetWindowRect(*i,&curWindowRect);
+		//tempModel.clearRectangle(curWindowRect);
+		displayModelsByWindow_t::iterator j=displayModelsByWindow.find(*i);
+		if(j!=displayModelsByWindow.end()) {
+			j->second->copyRectangleToOtherModel(textRect,&tempModel,FALSE,textRect.left,textRect.top);
+		}
 	}
+	wstring text;
+	tempModel.renderText(&textRect,text);
+	*textBuf=SysAllocString(text.c_str());
 	return 0;
 }
