66a73aace3d7a10833604a532d733ac5e0911d0b
James Teh <jamie@jantrid.net>
2010-04-05 05:02:16 +1000

    teamtalk4classic: Remove some weird characters. Not sure how they got there. :)

--- source/appModules/teamtalk4classic.py
+++ source/appModules/teamtalk4classic.py
@@ -12,7 +12,7 @@ class AppModule(_default.AppModule):
 	def event_NVDAObject_init(self,obj):
 		# The richedit control displaying incoming chat does not return correct _isWindowUnicode flag.
 		if obj.windowClassName=="RichEdit20A":
-			obj._isWindowUnicode=False
+			obj._isWindowUnicode=False
 
 	def findExtraNVDAObjectOverlayClasses(self, obj, clsList):
 		# There is a VU meter progress bar in the main window which we don't want to get anounced as all the other progress bars.
