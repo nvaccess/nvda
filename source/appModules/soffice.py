2a32aa2c82a0f4be5f9d9ec67ff1af399af95e5f
James Teh <jamie@jantrid.net>
2010-04-22 15:39:32 +1000

    soffice app module: 	* Change the canvas role in OpenOffice.org to document, as document is more correct. 	* Change the role of table cells in OpenOffice.org from label to table cell. 	* Don't use CompoundDocument for OpenOffice/Symphony spreadsheets, as it doesn't work here. It is only meant for editable text documents; spreadsheets don't have a caret.

--- source/appModules/soffice.py
+++ source/appModules/soffice.py
@@ -7,10 +7,22 @@
 import _default
 import controlTypes
 from compoundDocuments import CompoundDocument
+from NVDAObjects.JAB import JAB
 
 class AppModule(_default.AppModule):
 
 	def event_NVDAObject_init(self, obj):
-		if (obj.windowClassName == "SALTMPSUBFRAME" and obj.role == controlTypes.ROLE_DOCUMENT) or (obj.windowClassName == "SALFRAME" and obj.role == controlTypes.ROLE_CANVAS):
+		windowClass = obj.windowClassName
+		if isinstance(obj, JAB) and windowClass == "SALFRAME":
+			# OpenOffice.org has some strange role mappings due to its use of JAB.
+			if obj.role == controlTypes.ROLE_CANVAS:
+				obj.role = controlTypes.ROLE_DOCUMENT
+			elif obj.role == controlTypes.ROLE_LABEL:
+				parent = obj.parent
+				if parent and parent.role == controlTypes.ROLE_TABLE:
+					obj.role = controlTypes.ROLE_TABLECELL
+
+		if windowClass in ("SALTMPSUBFRAME", "SALFRAME") and obj.role == controlTypes.ROLE_DOCUMENT and obj.description:
+			# This is a word processor document.
 			obj.description = None
 			obj.treeInterceptorClass = CompoundDocument
