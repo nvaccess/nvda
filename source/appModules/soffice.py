413a2c85218f098d1eaff258363f7e8a0b492631
James Teh <jamie@jantrid.net>
2010-04-29 14:31:23 +1000

    Initial support for text attributes in Lotus Symphony. Supported for now are text position, font name, font size, italic, strikethrough, underline, spelling errors and bold.

--- source/appModules/soffice.py
+++ source/appModules/soffice.py
@@ -4,15 +4,53 @@
 #See the file COPYING for more details.
 #Copyright (C) 2006-2010 Michael Curran <mick@kulgan.net>, James Teh <jamie@jantrid.net>
 
+import IAccessibleHandler
 import _default
 import controlTypes
+import textInfos
 from compoundDocuments import CompoundDocument
 from NVDAObjects.JAB import JAB
 from NVDAObjects.IAccessible import IAccessible, IA2TextTextInfo
 from NVDAObjects.behaviors import EditableText
 
 class SymphonyTextInfo(IA2TextTextInfo):
-	pass
+
+	def _getFormatFieldAndOffsets(self,offset,formatConfig,calculateOffsets=True):
+		try:
+			startOffset,endOffset,attribsString=self.obj.IAccessibleTextObject.attributes(offset)
+		except COMError:
+			log.debugWarning("could not get attributes",exc_info=True)
+			return textInfos.FormatField(),(self._startOffset,self._endOffset)
+		formatField=textInfos.FormatField()
+		if not attribsString and offset>0:
+			try:
+				attribsString=self.obj.IAccessibleTextObject.attributes(offset-1)[2]
+			except COMError:
+				pass
+		if attribsString:
+			formatField.update(IAccessibleHandler.splitIA2Attribs(attribsString))
+
+		escapement = int(formatField["CharEscapement"])
+		if escapement < 0:
+			textPos = "sub"
+		elif escapement > 0:
+			textPos = "super"
+		else:
+			textPos = "baseline"
+		formatField["text-position"] = textPos
+		formatField["font-name"] = formatField["CharFontName"]
+		formatField["font-size"] = "%spt" % formatField["CharHeight"]
+		formatField["italic"] = formatField["CharPosture"] == "2"
+		formatField["strikethrough"] = formatField["CharStrikeout"] == "1"
+		underline = formatField["CharUnderline"]
+		if underline == "10":
+			# Symphony doesn't provide for semantic communication of spelling errors, so we have to rely on the WAVE underline type.
+			formatField["invalid-spelling"] = True
+		else:
+			formatField["underline"] = underline != "0"
+		formatField["bold"] = formatField["CharWeight"] != "100.000"
+
+		return formatField,(startOffset,endOffset)
 
 class SymphonyText(IAccessible, EditableText):
 	TextInfo = SymphonyTextInfo
