d27918d5a8eee00611d6c2061966d5b00ad9853b
James Teh <jamie@jantrid.net>
2010-05-14 09:56:50 +1000

    A start on providing better documentation for the textInfos package.

--- source/textInfos/offsets.py
+++ source/textInfos/offsets.py
@@ -1,8 +1,8 @@
 #textInfos/offsets.py
 #A part of NonVisual Desktop Access (NVDA)
-#Copyright (C) 2006-2007 NVDA Contributors <http://www.nvda-project.org/>
 #This file is covered by the GNU General Public License.
 #See the file COPYING for more details.
+#Copyright (C) 2006 Michael Curran <mick@kulgan.net>, James Teh <jamie@jantrid.net>
 
 import re
 import textInfos
@@ -127,6 +127,20 @@ def findEndOfWord(text,offset,lineLength=None):
 	return offset
 
 class OffsetsTextInfo(textInfos.TextInfo):
+	"""An abstract TextInfo for text implementations which represent ranges using numeric offsets relative to the start of the text.
+	In such implementations, the start of the text is represented by 0 and the end is the length of the entire text.
+	
+	All subclasses must implement L{_getStoryLength}.
+	Aside from this, there are two possible implementations:
+		* If the underlying text implementation does not support retrieval of line offsets, L{_getStoryText} should be implemented.
+		In this case, the base implementation of L{_getLineOffsets} will retrieve the entire text of the object and use text searching algorithms to find line offsets.
+		This is very inefficient and should be avoided if possible.
+		* Otherwise, subclasses must implement at least L{_getTextRange} and L{_getLineOffsets}.
+		Retrieval of other offsets (e.g. L{_getWordOffsets}) should also be implemented if possible for greatest accuracy and efficiency.
+	
+	If a caret and/or selection should be supported, L{_getCaretOffset} and/or L{_getSelectionOffsets} should be implemented, respectively.
+	To support conversion from/to screen points (e.g. for mouse tracking), L{_getOffsetFromPoint}/L{_getPointFromOffset} should be implemented.
+	"""
 
 	def __eq__(self,other):
 		if self is other or (isinstance(other,OffsetsTextInfo) and self._startOffset==other._startOffset and self._endOffset==other._endOffset):
@@ -156,6 +170,10 @@ class OffsetsTextInfo(textInfos.TextInfo):
 		raise NotImplementedError
 
 	def _getFormatFieldAndOffsets(self,offset,formatConfig,calculateOffsets=True):
+		"""Retrieve the formatting information for a given offset and the offsets spanned by that field.
+		Subclasses must override this if support for text formatting is desired.
+		The base implementation associates text with line numbers if possible.
+		"""
 		formatField=textInfos.FormatField()
 		startOffset,endOffset=self._startOffset,self._endOffset
 		if formatConfig["reportLineNumber"]:
@@ -199,6 +217,9 @@ class OffsetsTextInfo(textInfos.TextInfo):
 		raise NotImplementedError
 
 	def __init__(self,obj,position):
+		"""Constructor.
+		Subclasses may extend this to perform implementation specific initialisation, calling their superclass method first.
+		"""
 		super(OffsetsTextInfo,self).__init__(obj,position)
 		if isinstance(position,textInfos.Point):
 			offset=self._getOffsetFromPoint(position.x,position.y)
