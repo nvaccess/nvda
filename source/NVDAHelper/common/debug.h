95759383ec1bf4fdb22dd6c751164368b3d88686
Michael Curran <mick@kulgan.net>
2010-05-15 11:51:52 +1000

    Ensure  all code in common, client, and the nvdaController interface, is all clearly licensed under the GNU LGPL v2.1.

--- source/NVDAHelper/common/debug.h
+++ source/NVDAHelper/common/debug.h
@@ -1,10 +1,16 @@
-/**
- * base/debug.h
- * Part of the NV  Virtual Buffer Library
- * This library is copyright 2007, 2008 NV Virtual Buffer Library Contributors
- * This library is licensed under the GNU Lesser General Public Licence. See license.txt which is included with this library, or see
- * http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
- */
+/*
+This file is a part of the NVDA project.
+URL: http://www.nvda-project.org/
+Copyright 2006-2010 NVDA contributers.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License version 2.1, as published by
+the Free Software Foundation.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+This license can be found at:
+http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+*/
 
 #ifndef DEBUG_H
 #define DEBUG_H
