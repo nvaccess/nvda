180af1dc2930f45a8927eeb8053d47270c62a331
James Teh <jamie@jantrid.net>
2010-06-25 11:01:46 +1000

    Introduce early experimental support for Google Chrome. Please note that Chrome's screen reader support is far from complete and additional work may also be required in NVDA. You will need a recent development build of Chrome to try this. This uses the existing gecko_ia2 virtual buffer. For now, this is provided as a chrome app module, as I suspect Chrome itself isn't intended to be embeddable like Gecko and MSHTML. Chrome Frame embeds Chrome in IE, but even there, it uses the chrome executable, so the app module works. We can always change this later if need be.

---
+++ source/appModules/chrome.py
@@ -0,0 +1,33 @@
+#appModules/chrome.py
+#A part of NonVisual Desktop Access (NVDA)
+#This file is covered by the GNU General Public License.
+#See the file COPYING for more details.
+# Copyright (C) 2010 James Teh <jamie@jantrid.net>
+
+"""App module for Google Chrome
+"""
+
+import controlTypes
+from . import _default
+from NVDAObjects.IAccessible import IAccessible, getNVDAObjectFromEvent
+from virtualBuffers.gecko_ia2 import Gecko_ia2 as GeckoVBuf
+
+class ChromeVBuf(GeckoVBuf):
+
+	def __contains__(self, obj):
+		return obj.windowHandle == self.rootNVDAObject.windowHandle
+
+class Document(IAccessible):
+
+	def _get_treeInterceptorClass(self):
+		states = self.states
+		if controlTypes.STATE_READONLY in states and controlTypes.STATE_BUSY not in states:
+			return ChromeVBuf
+		return super(Document, self).treeInterceptorClass
+
+class AppModule(_default.AppModule):
+
+	def chooseNVDAObjectOverlayClasses(self, obj, clsList):
+		if obj.windowClassName == "Chrome_RenderWidgetHostHWND" and obj.role == controlTypes.ROLE_DOCUMENT:
+			clsList.insert(0, Document)
+			return
