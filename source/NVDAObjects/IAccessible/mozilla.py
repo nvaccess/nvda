cee258765406f20d9a3b305c41fbd42cd309e467
James Teh <jamie@jantrid.net>
2010-07-28 10:15:07 +1000

    mozilla NVDAObjects: 	* Detect the version of Gecko using IAccessibleApplication. Cache it on the app module to avoid looking this up for every accessible. There should only ever be one version of Gecko in use in a given application, so caching on the app module (per process) is fine. 	* Add an overlay class for Gecko 1.9. 	* Gecko >= 2.0 will no longer expose position information (and thus the "Description: " prefix) in accDescription, so move this into the Gecko 1.9 overlay. 	* Mozilla.event_scrollingStart is only needed for Firefox 3.6, so put this in the Gecko 1.9 overlay as well.

--- source/NVDAObjects/IAccessible/mozilla.py
+++ source/NVDAObjects/IAccessible/mozilla.py
@@ -7,6 +7,7 @@
 import IAccessibleHandler
 import oleacc
 import winUser
+from comtypes import IServiceProvider, COMError
 import eventHandler
 import controlTypes
 from . import IAccessible, Dialog
@@ -21,13 +22,6 @@ class Mozilla(IAccessible):
 			return True
 		return super(Mozilla,self).beTransparentToMouse
 
-	def _get_description(self):
-		rawDescription=super(Mozilla,self).description
-		if isinstance(rawDescription,basestring) and rawDescription.startswith('Description: '):
-			return rawDescription[13:]
-		else:
-			return ""
-
 	def _get_parent(self):
 		#Special code to support Mozilla node_child_of relation (for comboboxes)
 		res=IAccessibleHandler.accNavigate(self.IAccessibleObject,self.IAccessibleChildID,IAccessibleHandler.NAVRELATION_NODE_CHILD_OF)
@@ -37,17 +31,26 @@ class Mozilla(IAccessible):
 				return newObj
 		return super(Mozilla,self).parent
 
-	def event_scrollingStart(self):
-		#Firefox 3.6 fires scrollingStart on leaf nodes which is not useful to us.
-		#Bounce the event up to the node's parent so that any possible virtualBuffers will detect it.
-		if self.role==controlTypes.ROLE_EDITABLETEXT and controlTypes.STATE_READONLY in self.states:
-			eventHandler.queueEvent("scrollingStart",self.parent)
-
 	def _get_states(self):
 		states = super(Mozilla, self).states
 		if self.IAccessibleStates & oleacc.STATE_SYSTEM_MARQUEED:
 			states.add(controlTypes.STATE_CHECKABLE)
 		return states
+
+class Gecko1_9(Mozilla):
+
+	def _get_description(self):
+		rawDescription=super(Mozilla,self).description
+		if isinstance(rawDescription,basestring) and rawDescription.startswith('Description: '):
+			return rawDescription[13:]
+		else:
+			return ""
+
+	def event_scrollingStart(self):
+		#Firefox 3.6 fires scrollingStart on leaf nodes which is not useful to us.
+		#Bounce the event up to the node's parent so that any possible virtualBuffers will detect it.
+		if self.role==controlTypes.ROLE_EDITABLETEXT and controlTypes.STATE_READONLY in self.states:
+			eventHandler.queueEvent("scrollingStart",self.parent)
 
 class BrokenFocusedState(Mozilla):
 	shouldAllowIAccessibleFocusEvent=True
@@ -89,6 +92,19 @@ class EmbeddedObject(Mozilla):
 			return False
 		return super(EmbeddedObject, self).shouldAllowIAccessibleFocusEvent
 
+def _getGeckoVersion(obj):
+	appMod = obj.appModule
+	try:
+		return appMod._geckoVersion
+	except AttributeError:
+		pass
+	try:
+		ver = obj.IAccessibleObject.QueryInterface(IServiceProvider).QueryService(IAccessibleHandler.IAccessibleApplication._iid_, IAccessibleHandler.IAccessibleApplication).toolkitVersion
+	except COMError:
+		return None
+	appMod._geckoVersion = ver
+	return ver
+
 def findExtraOverlayClasses(obj, clsList):
 	"""Determine the most appropriate class if this is a Mozilla object.
 	This works similarly to L{NVDAObjects.NVDAObject.findOverlayClasses} except that it never calls any other findOverlayClasses method.
@@ -99,6 +115,10 @@ def findExtraOverlayClasses(obj, clsList):
 		clsList.append(cls)
 	if iaRole in _IAccessibleRolesWithBrokenFocusedState:
 		clsList.append(BrokenFocusedState)
+
+	if _getGeckoVersion(obj).startswith("1.9"):
+		clsList.append(Gecko1_9)
+
 	clsList.append(Mozilla)
 
 #: Maps IAccessible roles to NVDAObject overlay classes.
