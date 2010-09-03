3a9658dcf7c3d0acbb99e8cd0029ffa94f8782af
James Teh <jamie@jantrid.net>
2010-09-03 13:29:11 +1000

    Work around a bug in Eclipse which was causing the previously focused item to be announced when focusing on a new item in tree views.

---
+++ source/appModules/eclipse.py
@@ -0,0 +1,16 @@
+#appModules/eclipse.py
+#A part of NonVisual Desktop Access (NVDA)
+#This file is covered by the GNU General Public License.
+#See the file COPYING for more details.
+#Copyright (C) 2010 James Teh <jamie@jantrid.net>
+
+import controlTypes
+from . import _default
+
+class AppModule(_default.AppModule):
+
+	def event_NVDAObject_init(self, obj):
+		if obj.windowClassName == "SysTreeView32" and obj.role == controlTypes.ROLE_TREEVIEWITEM and controlTypes.STATE_FOCUSED not in obj.states:
+			# Eclipse tree views seem to fire a focus event on the previously focused item before firing focus on the new item (EclipseBug:315339).
+			# Try to filter this out.
+			obj.shouldAllowIAccessibleFocusEvent = False
