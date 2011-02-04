a9f36263fdde4b3a66f2f0dc4c017ab7efcf863f
James Teh <jamie@jantrid.net>
2011-02-04 16:20:12 +1000

    winConsoleHandler: Don't queue a caret event to NVDA if there is already one pending. This stops consoles from flooding the event queue and causing pointless braille updates.

--- source/winConsoleHandler.py
+++ source/winConsoleHandler.py
@@ -106,7 +106,7 @@ def consoleWinEventHook(handle,eventID,window,objectID,childID,threadID,timestam
 	#We don't want to do anything with the event if the event is not for the window this console is in
 	if window!=consoleObject.windowHandle:
 		return
-	if eventID==winUser.EVENT_CONSOLE_CARET:
+	if eventID==winUser.EVENT_CONSOLE_CARET and not eventHandler.isPendingEvents("caret",consoleObject):
 		eventHandler.queueEvent("caret",consoleObject)
 	# It is safe to call this event from this callback.
 	# This avoids an extra core cycle.
