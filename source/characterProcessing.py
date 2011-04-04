518492c122d03897c21907b1c4efef68cb903280
Michael Curran <mick@kulgan.net>
2011-04-04 16:56:43 +1000

    characterProcessing: Added a LocaleDataMap class, which can handle the dynamic loading and storage and lookup of data objects for particular locales. The reason for this abstraction is that symbol Framework can hopefully make use of this also. Also added CharacterDescriptions, which is a class  when instanciated with a locale, loads characters and their descriptions from a file and allows lookup. Based on code by Timothy Lee. Finally the module-level getCharacterDescriptions makes use of all this completing its implementation. Note though that LocaleDataMap could be improved to do timed caching (removing old objects to save memory).

--- source/characterProcessing.py
+++ source/characterProcessing.py
@@ -5,6 +5,82 @@
 #This file is covered by the GNU General Public License.
 #See the file COPYING for more details.
 
+import time
+import os
+import codecs
+from logHandler import log
+
+class LocaleDataMap(object):
+	"""Allows access to locale-specific data objects, dynamically loading them if needed on request"""
+
+	def __init__(self,localeDataClass):
+		"""
+		@param localeDataClass: this class will be used to instanciate data objects representing the requested locale.
+		""" 
+		self._localeDataClass=localeDataClass
+		self._dataMap={}
+
+	def fetchLocaleData(self,locale):
+		"""
+		Fetches a data object for the given locale. 
+		This may mean that the data object is first created and sotred if it does not yet exist in the map.
+		The locale is also simplified (country is dropped) if the full locale can not be used to instanciate a data object.
+		@param locale: the locale of the data object requested
+		@type locale: string
+		@return: the data object for the given locale
+		"""
+		localeList=[locale]
+		if '_' in locale:
+			localeList.append(locale.split('_')[0])
+		for l in localeList:
+			data=self._dataMap.get(l)
+			if data: return data
+			try:
+				data=self._localeDataClass(l)
+			except LookupError:
+				data=None
+			if not data: continue
+			self._dataMap[l]=data
+			return data
+		raise LookupError(locale)
+
+class CharacterDescriptions(object):
+	"""
+	Represents a map of characters to one or more descriptions (examples) for that character.
+	The data is loaded from a file from the requested locale.
+	"""
+
+	def __init__(self,locale):
+		"""
+		@param locale: The characterDescriptions.dic file will be found by using this locale.
+		@type locale: string
+		"""
+		self._entries = {}
+		fileName=os.path.join('locale',locale,'characterDescriptions.dic')
+		if not os.path.isfile(fileName): 
+			raise LookupError(fileName)
+		f = codecs.open(fileName,"r","utf_8_sig",errors="replace")
+		for line in f:
+			if line.isspace() or line.startswith('#'):
+				continue
+			line=line.rstrip('\r\n')
+			temp=line.split("\t")
+			if len(temp) > 1:
+				key=temp.pop(0)
+				self._entries[key] = temp
+			else:
+				log.warning("can't parse line '%s'" % line)
+		log.debug("Loaded %d entries." % len(self._entries))
+		f.close()
+
+	def getCharacterDescription(self, character):
+		"""
+		Looks up the given character and returns a string containing all the descriptions found.
+		"""
+		return u"\u3002".join(self._entries.get(character))
+
+_charDescLocaleDataMap=LocaleDataMap(CharacterDescriptions)
+
 def getCharacterDescription(locale,character):
 	"""
 	Finds a description or example for the given character, which makes sence in the given locale.
@@ -15,5 +91,7 @@ def getCharacterDescription(locale,character):
 	@return:  the found description for the given character
 	@rtype: string
 	"""
-	return "%s - %c description"%(locale,character)
+	l=_charDescLocaleDataMap.fetchLocaleData(locale)
+	desc=l.getCharacterDescription(character)
+	return desc
  
