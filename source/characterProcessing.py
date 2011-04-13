4f31fb7f60f1e6f43efbc1099e90072ab96e4221
James Teh <jamie@jantrid.net>
2011-04-13 12:50:26 +1000

    characterProcessing: Add SpeechSymbols class which handles the pronunciation of symbols. Simple character-based symbols are supported, as well as complex symbols such as sentence and phrase endings using regular expressions. The data is loaded from a file for a specific locale. Currently, all locales inherit from English. Multiple symbol verbosity levels are handled. Currently, these are none, some, most, all and char, where char is used for symbols which should only be spoken when moving by character. The preservation of symbols in the text to provide for correct inflection is also handled. This class also handles processing of the text based on the symbol data. It has not yet been integrated into NVDA's speech support.

--- source/characterProcessing.py
+++ source/characterProcessing.py
@@ -8,6 +8,8 @@
 import time
 import os
 import codecs
+import collections
+import re
 from logHandler import log
 
 class LocaleDataMap(object):
@@ -104,3 +106,239 @@ def getCharacterDescription(locale,character):
 		desc=getCharacterDescription('en',character)
 	return desc
  
+SYMLVL_NONE = 0
+SYMLVL_SOME = 100
+SYMLVL_MOST = 200
+SYMLVL_ALL = 300
+SYMLVL_CHAR = 1000
+SYMBOL_LEVELS = {
+	"none": SYMLVL_NONE,
+	"some": SYMLVL_SOME,
+	"most": SYMLVL_MOST,
+	"all": SYMLVL_ALL,
+	"char": SYMLVL_CHAR,
+}
+
+class SpeechSymbol(object):
+	__slots__ = ("identifier", "pattern", "replacement", "level", "preserve", "displayName")
+
+	def __init__(self, identifier, pattern=None, replacement=None, level=None, preserve=None, displayName=None):
+		self.identifier = identifier
+		self.pattern = pattern
+		self.replacement = replacement
+		self.level = level
+		self.preserve = preserve
+		self.displayName = None
+
+class SpeechSymbols(object):
+	"""
+	Handles pronunciation of symbols.
+	The data is loaded from a file for the requested locale.
+	"""
+
+	def __init__(self, locale):
+		"""Constructor.
+		@param locale: The locale for which to load symbol information.
+		@type locale: str
+		@raise LookupError: If there is no symbol information for this locale.
+		"""
+		self.locale = locale
+
+		fileName = os.path.join("locale", locale, "symbols.dic")
+		if not os.path.isfile(fileName): 
+			raise LookupError("No symbol information for locale %s" % locale)
+
+		self.rawComplexSymbols = collections.OrderedDict()
+		self.rawSymbols = []
+
+		# Read data from file.
+		with codecs.open(fileName, "r", "utf_8_sig", errors="replace") as f:
+			handler = None
+			for line in f:
+				if line.isspace() or line.startswith("#"):
+					# Whitespace or comment.
+					continue
+				line = line.rstrip("\r\n")
+				if line == "complexSymbols:":
+					handler = self._loadComplexSymbol
+				elif line == "symbols:":
+					handler = self._loadSymbol
+				elif handler:
+					# This is a line within a section, so handle it according to which section we're in.
+					try:
+						handler(line)
+					except ValueError:
+						log.warning("Invalid line %r" % line)
+				else:
+					log.warning("Invalid line %r" % line)
+
+		self.initProcessor()
+
+	def _loadComplexSymbol(self, line):
+		try:
+			identifier, pattern = line.split("\t")
+		except TypeError:
+			raise ValueError
+		self.rawComplexSymbols[identifier] = pattern
+
+	IDENTIFIER_ESCAPES = {
+		"0": "\0",
+		"t": "\t",
+		"n": "\n",
+		"r": "\r",
+		"f": "\f",
+		"v": "\v",
+	}
+
+	def _loadSymbol(self, line):
+		line = iter(line.split("\t"))
+		identifier = replacement = level = preserve = displayName = None
+		try:
+			identifier = next(line)
+			if not identifier:
+				# Empty identifier is not allowed.
+				raise ValueError
+			if len(identifier) == 2 and identifier.startswith("\\"):
+				identifier = self.IDENTIFIER_ESCAPES.get(identifier[1], identifier[1])
+			replacement = next(line)
+			if replacement == "-":
+				replacement = None
+		except StopIteration:
+			# These fields are mandatory.
+			raise ValueError
+		try:
+			level = SYMBOL_LEVELS.get(next(line))
+			preserve = next(line)
+			if preserve == "y":
+				preserve = True
+			elif preserve == "n":
+				preserve = False
+			else:
+				preserve = None
+			displayName = next(line)
+			if displayName == "-":
+				displayName = None
+		except StopIteration:
+			# These fields are optional. Defaults will be used for unspecified fields.
+			pass
+		self.rawSymbols.append(SpeechSymbol(identifier, None, replacement, level, preserve, displayName))
+
+	def initProcessor(self):
+		"""Initialise the symbol processor.
+		"""
+		# We need to merge symbol data from several sources, starting with this instance.
+		sources = [self]
+		# Always use English as a base.
+		if self.locale != "en":
+			sources.append(_speechSymbolsLocaleDataMap.fetchLocaleData("en"))
+
+		# The computed symbol information from all sources.
+		symbols = self.computedSymbols = collections.OrderedDict()
+		# Single character symbols.
+		characters = set()
+
+		# Add all complex symbols first, as they take priority.
+		for source in sources:
+			for identifier, pattern in source.rawComplexSymbols.iteritems():
+				symbols[identifier] = SpeechSymbol(identifier, pattern)
+
+		# Supplement the data for complex symbols and add all simple symbols.
+		for source in sources:
+			for sourceSymbol in source.rawSymbols:
+				identifier = sourceSymbol.identifier
+				try:
+					symbol = symbols[identifier]
+				except KeyError:
+					# This is a simple symbol.
+					# The pattern for a simple symbol is the escaped identifier.
+					symbol = symbols[identifier] = SpeechSymbol(identifier, re.escape(identifier))
+					if len(identifier) == 1:
+						characters.add(identifier)
+				# If fields weren't explicitly specified, inherit the value from later sources.
+				if symbol.replacement is None:
+					symbol.replacement = sourceSymbol.replacement
+				if symbol.level is None:
+					symbol.level = sourceSymbol.level
+				if symbol.preserve is None:
+					symbol.preserve = sourceSymbol.preserve
+				if symbol.displayName is None:
+					symbol.displayName = sourceSymbol.displayName
+
+		# Set defaults for any fields not explicitly set.
+		for symbol in symbols.values():
+			if symbol.replacement is None:
+				# Complex symbols without a replacement specified are useless.
+				del symbols[symbol.identifier]
+				continue
+			if symbol.level is None:
+				symbol.level = SYMBOL_LEVELS["all"]
+			if symbol.preserve is None:
+				symbol.preserve = False
+			if symbol.displayName is None:
+				symbol.displayName = symbol.identifier
+
+		# We need an indexable symbols list to execute the regexp.
+		symbolsList = self._computedSymbolsList = symbols.values()
+
+		# Build the regexp.
+		patterns = [
+			# Strip repeated spaces from the end of the line to stop them from being picked up by repeated.
+			r"(?P<rstripSpace>  +$)",
+			# Repeated characters.
+			r"(?P<repeated>(?P<repTmp>[%s])(?P=repTmp){3,})" % re.escape("".join(characters))
+		]
+		patterns.extend(
+			u"(?P<s{index}>{pattern})".format(index=index, pattern=symbol.pattern)
+			for index, symbol in enumerate(symbolsList))
+		pattern = "|".join(patterns)
+		self._pattern = re.compile(pattern, re.UNICODE)
+
+	def _process(self, m):
+		group = m.lastgroup
+
+		if group == "rstripSpace":
+			return ""
+
+		elif group == "repeated":
+			# Repeated character.
+			text = m.group()
+			symbol = self.computedSymbols[text[0]]
+			if self._level >= symbol.level:
+				return u" {count} {char} ".format(count=len(text), char=symbol.replacement)
+			else:
+				return " "
+
+		else:
+			# One of the defined symbols.
+			index = int(group[1:])
+			symbol = self._computedSymbolsList[index]
+			if symbol.preserve:
+				suffix = "%s " % m.group()
+			else:
+				suffix = " "
+			if self._level >= symbol.level:
+				return u" {repl}{suffix}".format(repl=symbol.replacement, suffix=suffix)
+			else:
+				return suffix
+
+	def processText(self, text, level):
+		self._level = level
+		return self._pattern.sub(self._process, text)
+
+_speechSymbolsLocaleDataMap = LocaleDataMap(SpeechSymbols)
+
+def processSymbols(locale, text, level):
+	"""Process some text, converting symbols according to desired pronunciation.
+	@param locale: The locale of the text.
+	@type locale: str
+	@param text: The text to process.
+	@type text: str
+	@param level: The symbol level to use; one of the SYMLVL_* constants.
+	"""
+	try:
+		ss = _speechSymbolsLocaleDataMap.fetchLocaleData(locale)
+	except LookupError:
+		if not locale.startswith("en"):
+			return processSymbols("en", text)
+		raise
+	return ss.processText(text, level)
