c4a0db710fb5b6e7ec088e2716d5b49f2ad55438
Peter Vágner <peter.v@datagate.sk>
2011-05-22 16:50:08 +0200

    Again updated AkelEdit support. I have now properly examined changes in their api and implemented _getLineOffsets method properly. This fixes broken say all.

--- source/NVDAObjects/window/akelEdit.py
+++ source/NVDAObjects/window/akelEdit.py
@@ -14,12 +14,12 @@ AEM_CHARFROMPOS       =(winUser.WM_USER + 2151)
 AEM_POSFROMCHAR       =(winUser.WM_USER + 2152)
 AEM_INDEXTORICHOFFSET =(winUser.WM_USER + 2112)
 AEM_RICHOFFSETTOINDEX =(winUser.WM_USER + 2113)
-
+AEM_CONTROLVERSION        =(winUser.WM_USER + 2200)
 
 #AEM_GETINDEX flags
 AEGI_LASTCHAR         =2
 AEGI_CARETCHAR             =5
-AEGI_NEXTLINE              =8
+AEGI_NEXTLINE              =24
 
 
 #Structures
@@ -68,8 +68,15 @@ class AkelEditTextInfo(edit.EditTextInfo):
 
 	def _getLineOffsets(self,offset):
 		(start,end)=super(AkelEditTextInfo,self)._getLineOffsets(offset)
-		if self._getLineNumFromOffset(start) != self._getLineNumFromOffset(end):
-			return (start,end-1)
+		if end == self._getStoryLength():
+			return (start,end)
+		ciChar=AECHARINDEX()
+		processHandle=self.obj.processHandle
+		internalCiChar=winKernel.virtualAllocEx(processHandle,None,ctypes.sizeof(ciChar),winKernel.MEM_COMMIT,winKernel.PAGE_READWRITE)
+		winUser.sendMessage(self.obj.windowHandle,AEM_RICHOFFSETTOINDEX,offset,internalCiChar)
+		winUser.sendMessage(self.obj.windowHandle,AEM_GETINDEX,AEGI_NEXTLINE,internalCiChar)
+		end=winUser.sendMessage(self.obj.windowHandle,AEM_INDEXTORICHOFFSET,0,internalCiChar)
+		winKernel.virtualFreeEx(processHandle,internalCiChar,0,winKernel.MEM_RELEASE)
 		return (start,end)
 
 
@@ -78,5 +85,20 @@ class AkelEdit(edit.RichEdit20):
 	TextInfo=AkelEditTextInfo
 
 	def initOverlayClass(self):
+		global AEGI_NEXTLINE
+		version=self._getControlVersion()
+		if version <1.6:
+			AEGI_NEXTLINE =8
+		else:
+			AEGI_NEXTLINE =24
+
 		for gesture in ("kb:control+upArrow", "kb:control+downArrow"):
 			self.bindGesture(gesture, "caret_moveByLine")
+
+	def _getControlVersion(self):
+		res=winUser.sendMessage(self.windowHandle,AEM_CONTROLVERSION,None,None)
+		major=winUser.LOBYTE(winUser.LOWORD(res))
+		minor=winUser.HIBYTE(winUser.LOWORD(res))
+		version=major+(0.1*minor)
+		return version
+
