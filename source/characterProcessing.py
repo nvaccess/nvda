522d6f000d8ea2cc0be5883eca1dc8ab2769562c
James Teh <jamie@jantrid.net>
2011-05-28 21:51:02 +1000

    SpeechSymbols.load: Don't barf when reading an identifier that just contains "\". SpeechSymbols.save: Escape "\". This isn't strictly necessary, but it's more consistent.

--- source/characterProcessing.py
+++ source/characterProcessing.py
@@ -223,6 +223,7 @@ class SpeechSymbols(object):
 		"f": "\f",
 		"v": "\v",
 		"#": "#",
+		"\\": "\\",
 	}
 	IDENTIFIER_ESCAPES_OUTPUT = {v: k for k, v in IDENTIFIER_ESCAPES_INPUT.iteritems()}
 	LEVEL_INPUT = {
@@ -254,7 +255,7 @@ class SpeechSymbols(object):
 			if not identifier:
 				# Empty identifier is not allowed.
 				raise ValueError
-			if identifier.startswith("\\"):
+			if identifier.startswith("\\") and len(identifier) >= 2:
 				identifier = self.IDENTIFIER_ESCAPES_INPUT.get(identifier[1], identifier[1]) + identifier[2:]
 			replacement = self._loadSymbolField(next(line))
 		except StopIteration:
