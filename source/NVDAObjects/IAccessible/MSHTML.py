8fd4ac5fb3b655cfd35e289979d485e7ad0c8637
Michael Curran <mick@kulgan.net>
2011-06-21 16:07:55 +1000

    MSHTML NVDAObject's presentationType property: when checking if a table cell/row etc should be classed as layout, only check self.treeInterceptor after checking the role and the old presentation type, otherwise  a possibly costly fetch of the treeInterceptor would happen unnecessarily. this in some cases can dramatically improve the speed of object navigation in MSHTML.

--- source/NVDAObjects/IAccessible/MSHTML.py
+++ source/NVDAObjects/IAccessible/MSHTML.py
@@ -426,7 +426,7 @@ class MSHTML(IAccessible):
 		presType=super(MSHTML,self).presentationType
 		if presType==self.presType_content and self.HTMLAttributes['role']=="presentation":
 			presType=self.presType_layout
-		if self.treeInterceptor and presType==self.presType_content and self.role in (controlTypes.ROLE_TABLECELL,controlTypes.ROLE_TABLEROW,controlTypes.ROLE_TABLE,controlTypes.ROLE_TABLEBODY) and self.treeInterceptor.isNVDAObjectPartOfLayoutTable(self):
+		if presType==self.presType_content and self.role in (controlTypes.ROLE_TABLECELL,controlTypes.ROLE_TABLEROW,controlTypes.ROLE_TABLE,controlTypes.ROLE_TABLEBODY) and self.treeInterceptor and self.treeInterceptor.isNVDAObjectPartOfLayoutTable(self):
 			presType=self.presType_layout
 		return presType
 
