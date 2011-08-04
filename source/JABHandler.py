206f10e9e819d8ae4430e846cbb24c6a6521a300
Michael Curran <mick@kulgan.net>
2011-08-04 15:01:17 +1000

    Support for Java Access Bridge 2.02 (including for 64 bit JVMs). Hopefully does not break older Java Access Bridge versions. Specific changes: 	* Rather than loading windowsAccessBridge.dll in initialize, load either windowsAccessBridge-32 or windowsAccessBridge at the top of JABHandler, and take note of which one worked (whether we're using legacy or not). 	* Rewrite defines of all structs, types and function prototypes using java-specific types (jint, JOBJECT64) and make sure that JOBJECT64 is 64 bit for the new java access bridge and 32 bit for legacy. 	* Don't ever handle JOBJECT64 as an int, but instead always an opaque type.

--- source/JABHandler.py
+++ source/JABHandler.py
@@ -17,7 +17,180 @@ import eventHandler
 import controlTypes
 import NVDAObjects.JAB
 
-bridgeDll=None
+#Some utility functions to help with function defines
+
+def _errcheck(res, func, args):
+	if not res:
+		raise RuntimeError("Result %s" % res)
+	return res
+
+def _fixBridgeFunc(restype,name,*argtypes,**kwargs):
+	func=getattr(bridgeDll,name)
+	func.restype=restype
+	func.argtypes=argtypes
+	if kwargs.get('errcheck'):
+		func.errcheck=_errcheck
+
+#Load the first available access bridge dll
+legacyAccessBridge=True
+try:
+	bridgeDll=getattr(cdll,'windowsAccessBridge-32')
+	legacyAccessBridge=False
+except WindowsError:
+	try:
+		bridgeDll=cdll.windowsAccessBridge
+	except WindowsError:
+		bridgeDll=None
+
+#Definitions of access bridge types, structs and prototypes
+
+jint=c_int
+jfloat=c_float
+class JOBJECT64(c_int if legacyAccessBridge else c_int64):
+	pass
+
+MAX_STRING_SIZE=1024
+SHORT_STRING_SIZE=256
+
+class AccessBridgeVersionInfo(Structure):
+	_fields_=[
+		('VMVersion',WCHAR*SHORT_STRING_SIZE),
+		('bridgeJavaClassVersion',WCHAR*SHORT_STRING_SIZE),
+		('bridgeJavaDLLVersion',WCHAR*SHORT_STRING_SIZE),
+		('bridgeWinDLLVersion',WCHAR*SHORT_STRING_SIZE),
+	]
+
+class AccessibleContextInfo(Structure):
+	_fields_=[
+		('name',WCHAR*MAX_STRING_SIZE),
+		('description',WCHAR*MAX_STRING_SIZE),
+		('role',WCHAR*SHORT_STRING_SIZE),
+		('role_en_US',WCHAR*SHORT_STRING_SIZE),
+		('states',WCHAR*SHORT_STRING_SIZE),
+		('states_en_US',WCHAR*SHORT_STRING_SIZE),
+		('indexInParent',jint),
+		('childrenCount',jint),
+		('x',jint),
+		('y',jint),
+		('width',jint),
+		('height',jint),
+		('accessibleComponent',BOOL),
+		('accessibleAction',BOOL),
+		('accessibleSelection',BOOL),
+		('accessibleText',BOOL),
+		('accessibleValue',BOOL),
+	]
+
+class AccessibleTextInfo(Structure):
+	_fields_=[
+		('charCount',jint),
+		('caretIndex',jint),
+		('indexAtPoint',jint),
+	]
+
+class AccessibleTextItemsInfo(Structure):
+	_fields_=[
+		('letter',WCHAR),
+		('word',WCHAR*SHORT_STRING_SIZE),
+		('sentence',WCHAR*MAX_STRING_SIZE),
+	]
+
+class AccessibleTextSelectionInfo(Structure):
+	_fields_=[
+		('selectionStartIndex',jint),
+		('selectionEndIndex',jint),
+		('selectedText',WCHAR*MAX_STRING_SIZE),
+	]
+
+class AccessibleTextRectInfo(Structure):
+	_fields_=[
+		('x',jint),
+		('y',jint),
+		('width',jint),
+		('height',jint),
+	]
+
+class AccessibleTextAttributesInfo(Structure):
+	_fields_=[
+		('bold',BOOL),
+		('italic',BOOL),
+		('underline',BOOL),
+		('strikethrough',BOOL),
+		('superscript',BOOL),
+		('subscript',BOOL),
+		('backgroundColor',WCHAR*SHORT_STRING_SIZE),
+		('foregroundColor',WCHAR*SHORT_STRING_SIZE),
+		('fontFamily',WCHAR*SHORT_STRING_SIZE),
+		('fontSize',jint),
+		('alignment',jint),
+		('bidiLevel',jint),
+		('firstLineIndent',jfloat),
+		('LeftIndent',jfloat),
+		('rightIndent',jfloat),
+		('lineSpacing',jfloat),
+		('spaceAbove',jfloat),
+		('spaceBelow',jfloat),
+		('fullAttributesString',WCHAR*MAX_STRING_SIZE),
+	]
+
+MAX_RELATION_TARGETS = 25
+MAX_RELATIONS = 5
+
+class AccessibleRelationInfo(Structure):
+	_fields_ = [
+		("key", WCHAR * SHORT_STRING_SIZE),
+		("targetCount", jint),
+		("targets", JOBJECT64 * MAX_RELATION_TARGETS),
+	]
+
+class AccessibleRelationSetInfo(Structure):
+	_fields_ = [
+		("relationCount", jint),
+		("relations", AccessibleRelationInfo * MAX_RELATIONS),
+	]
+
+AccessBridge_FocusGainedFP=CFUNCTYPE(None,c_long,JOBJECT64,JOBJECT64)
+AccessBridge_PropertyStateChangeFP=CFUNCTYPE(None,c_long,JOBJECT64,JOBJECT64,c_wchar_p,c_wchar_p)
+AccessBridge_PropertyCaretChangeFP=CFUNCTYPE(None,c_long,JOBJECT64,JOBJECT64,c_int,c_int)
+AccessBridge_PropertyActiveDescendentChangeFP=CFUNCTYPE(None,c_long,JOBJECT64,JOBJECT64,JOBJECT64,JOBJECT64)
+
+#Appropriately set the return and argument types of all the access bridge dll functions
+if bridgeDll:
+	_fixBridgeFunc(None,'Windows_run')
+	_fixBridgeFunc(None,'setFocusGainedFP',c_void_p)
+	_fixBridgeFunc(None,'setPropertyStateChangeFP',c_void_p)
+	_fixBridgeFunc(None,'setPropertyCaretChangeFP',c_void_p)
+	_fixBridgeFunc(None,'setPropertyActiveDescendentChangeFP',c_void_p)
+	_fixBridgeFunc(None,'releaseJavaObject',c_long,JOBJECT64)
+	_fixBridgeFunc(BOOL,'getVersionInfo',POINTER(AccessBridgeVersionInfo),errcheck=True)
+	_fixBridgeFunc(BOOL,'isJavaWindow',HWND)
+	_fixBridgeFunc(BOOL,'isSameObject',c_long,JOBJECT64,JOBJECT64)
+	_fixBridgeFunc(BOOL,'getAccessibleContextFromHWND',HWND,POINTER(c_long),POINTER(JOBJECT64),errcheck=True)
+	_fixBridgeFunc(HWND,'getHWNDFromAccessibleContext',c_long,JOBJECT64,errcheck=True)
+	_fixBridgeFunc(BOOL,'getAccessibleContextAt',c_long,JOBJECT64,jint,jint,POINTER(JOBJECT64),errcheck=True)
+	_fixBridgeFunc(BOOL,'getAccessibleContextWithFocus',HWND,POINTER(c_long),POINTER(JOBJECT64),errcheck=True)
+	_fixBridgeFunc(BOOL,'getAccessibleContextInfo',c_long,JOBJECT64,POINTER(AccessibleContextInfo),errcheck=True)
+	_fixBridgeFunc(JOBJECT64,'getAccessibleChildFromContext',c_long,JOBJECT64,jint,errcheck=True)
+	_fixBridgeFunc(JOBJECT64,'getAccessibleParentFromContext',c_long,JOBJECT64)
+	_fixBridgeFunc(BOOL,'getAccessibleRelationSet',c_long,JOBJECT64,POINTER(AccessibleRelationSetInfo),errcheck=True)
+	_fixBridgeFunc(BOOL,'getAccessibleTextInfo',c_long,JOBJECT64,POINTER(AccessibleTextInfo),jint,jint,errcheck=True)
+	_fixBridgeFunc(BOOL,'getAccessibleTextItems',c_long,JOBJECT64,POINTER(AccessibleTextItemsInfo),jint,errcheck=True)
+	_fixBridgeFunc(BOOL,'getAccessibleTextSelectionInfo',c_long,JOBJECT64,POINTER(AccessibleTextSelectionInfo),errcheck=True)
+	_fixBridgeFunc(BOOL,'getAccessibleTextAttributes',c_long,JOBJECT64,jint,POINTER(AccessibleTextAttributesInfo),errcheck=True)
+	_fixBridgeFunc(BOOL,'getAccessibleTextLineBounds',c_long,JOBJECT64,jint,POINTER(jint),POINTER(jint),errcheck=True)
+	_fixBridgeFunc(BOOL,'getAccessibleTextRange',c_long,JOBJECT64,jint,jint,POINTER(c_wchar),c_short,errcheck=True)
+	_fixBridgeFunc(BOOL,'getCurrentAccessibleValueFromContext',c_long,JOBJECT64,POINTER(c_wchar),c_short,errcheck=True)
+	_fixBridgeFunc(BOOL,'selectTextRange',c_long,JOBJECT64,c_int,c_int,errcheck=True)
+	_fixBridgeFunc(BOOL,'getTextAttributesInRange',c_long,JOBJECT64,c_int,c_int,POINTER(AccessibleTextAttributesInfo),POINTER(c_short),errcheck=True)
+	_fixBridgeFunc(JOBJECT64,'getTopLevelObject',c_long,JOBJECT64,errcheck=True)
+	_fixBridgeFunc(c_int,'getObjectDepth',c_long,JOBJECT64)
+	_fixBridgeFunc(JOBJECT64,'getActiveDescendent',c_long,JOBJECT64)
+	_fixBridgeFunc(BOOL,'requestFocus',c_long,JOBJECT64,errcheck=True)
+	_fixBridgeFunc(BOOL,'setCaretPosition',c_long,JOBJECT64,c_int,errcheck=True)
+	_fixBridgeFunc(BOOL,'getCaretLocation',c_long,JOBJECT64,POINTER(AccessibleTextRectInfo),jint,errcheck=True)
+
+#NVDA specific code
+
 isRunning=False
 vmIDsToWindowHandles={}
 internalFunctionQueue=Queue.Queue(1000)
@@ -26,18 +199,14 @@ internalFunctionQueue.__name__="JABHandler.internalFunctionQueue"
 def internalQueueFunction(func,*args,**kwargs):
 	internalFunctionQueue.put_nowait((func,args,kwargs))
 
-MAX_STRING_SIZE=1024
-SHORT_STRING_SIZE=256
-
 class JABContext(object):
 
 	def __init__(self,hwnd=None,vmID=None,accContext=None):
 		if hwnd and not vmID:
  			vmID=c_int()
-			accContext=c_int()
+			accContext=JOBJECT64()
 			bridgeDll.getAccessibleContextFromHWND(hwnd,byref(vmID),byref(accContext))
 			vmID=vmID.value
-			accContext=accContext.value
 			#Record  this vm ID and window handle for later use with other objects
 			vmIDsToWindowHandles[vmID]=hwnd
 		elif vmID and not hwnd:
@@ -169,9 +338,8 @@ class JABContext(object):
 			return None
 
 	def getAccessibleContextAt(self,x,y):
-		newAccContext=c_int()
+		newAccContext=JOBJECT64()
 		res=bridgeDll.getAccessibleContextAt(self.vmID,self.accContext,x,y,byref(newAccContext))
-		newAccContext=newAccContext.value
 		if not res or not newAccContext:
 			return None
 		if not bridgeDll.isSameObject(self.vmID,newAccContext,self.accContext):
@@ -202,104 +370,7 @@ class JABContext(object):
 		bridgeDll.getAccessibleRelationSet(self.vmID, self.accContext, byref(relations))
 		return relations
 
-class AccessBridgeVersionInfo(Structure):
-	_fields_=[
-		('VMVersion',WCHAR*SHORT_STRING_SIZE),
-		('bridgeJavaClassVersion',WCHAR*SHORT_STRING_SIZE),
-		('bridgeJavaDLLVersion',WCHAR*SHORT_STRING_SIZE),
-		('bridgeWinDLLVersion',WCHAR*SHORT_STRING_SIZE),
-	]
-
-class AccessibleContextInfo(Structure):
-	_fields_=[
-		('name',WCHAR*MAX_STRING_SIZE),
-		('description',WCHAR*MAX_STRING_SIZE),
-		('role',WCHAR*SHORT_STRING_SIZE),
-		('role_en_US',WCHAR*SHORT_STRING_SIZE),
-		('states',WCHAR*SHORT_STRING_SIZE),
-		('states_en_US',WCHAR*SHORT_STRING_SIZE),
-		('indexInParent',c_int),
-		('childrenCount',c_int),
-		('x',c_int),
-		('y',c_int),
-		('width',c_int),
-		('height',c_int),
-		('accessibleComponent',BOOL),
-		('accessibleAction',BOOL),
-		('accessibleSelection',BOOL),
-		('accessibleText',BOOL),
-		('accessibleValue',BOOL),
-	]
-
-class AccessibleTextInfo(Structure):
-	_fields_=[
-		('charCount',c_int),
-		('caretIndex',c_int),
-		('indexAtPoint',c_int),
-	]
-
-class AccessibleTextItemsInfo(Structure):
-	_fields_=[
-		('letter',WCHAR),
-		('word',WCHAR*SHORT_STRING_SIZE),
-		('sentence',WCHAR*MAX_STRING_SIZE),
-	]
-
-class AccessibleTextSelectionInfo(Structure):
-	_fields_=[
-		('selectionStartIndex',c_int),
-		('selectionEndIndex',c_int),
-		('selectedText',WCHAR*MAX_STRING_SIZE),
-	]
-
-class AccessibleTextRectInfo(Structure):
-	_fields_=[
-		('x',c_int),
-		('y',c_int),
-		('width',c_int),
-		('height',c_int),
-	]
-
-class AccessibleTextAttributesInfo(Structure):
-	_fields_=[
-		('bold',BOOL),
-		('italic',BOOL),
-		('underline',BOOL),
-		('strikethrough',BOOL),
-		('superscript',BOOL),
-		('subscript',BOOL),
-		('backgroundColor',WCHAR*SHORT_STRING_SIZE),
-		('foregroundColor',WCHAR*SHORT_STRING_SIZE),
-		('fontFamily',WCHAR*SHORT_STRING_SIZE),
-		('fontSize',c_int),
-		('alignment',c_int),
-		('bidiLevel',c_int),
-		('firstLineIndent',c_float),
-		('LeftIndent',c_float),
-		('rightIndent',c_float),
-		('lineSpacing',c_float),
-		('spaceAbove',c_float),
-		('spaceBelow',c_float),
-		('fullAttributesString',WCHAR*MAX_STRING_SIZE),
-	]
-
-MAX_RELATION_TARGETS = 25
-MAX_RELATIONS = 5
-
-class AccessibleRelationInfo(Structure):
-	_fields_ = [
-		("key", WCHAR * SHORT_STRING_SIZE),
-		("targetCount", c_int),
-		("targets", c_int * MAX_RELATION_TARGETS),
-	]
-
-class AccessibleRelationSetInfo(Structure):
-	_fields_ = [
-		("relationCount", c_int),
-		("relations", AccessibleRelationInfo * MAX_RELATIONS),
-	]
-
-@CFUNCTYPE(None,c_int,c_int,c_int)
+@AccessBridge_FocusGainedFP
 def internal_event_focusGained(vmID, event,source):
 	internalQueueFunction(event_gainFocus,vmID,source)
 	bridgeDll.releaseJavaObject(vmID,event)
@@ -331,13 +402,13 @@ def event_gainFocus(vmID,accContext):
 		return
 	eventHandler.queueEvent("gainFocus",obj)
 
-@CFUNCTYPE(None,c_int,c_int,c_int,c_int,c_int)
+@AccessBridge_PropertyActiveDescendentChangeFP
 def internal_event_activeDescendantChange(vmID, event,source,oldDescendant,newDescendant):
 	internalQueueFunction(event_gainFocus,vmID,newDescendant)
 	for accContext in [event,oldDescendant]:
 		bridgeDll.releaseJavaObject(vmID,accContext)
 
-@CFUNCTYPE(None,c_int,c_int,c_int,c_wchar_p,c_wchar_p)
+@AccessBridge_PropertyStateChangeFP
 def internal_event_stateChange(vmID,event,source,oldState,newState):
 	internalQueueFunction(event_stateChange,vmID,source,oldState,newState)
 	bridgeDll.releaseJavaObject(vmID,event)
@@ -362,7 +433,7 @@ def event_stateChange(vmID,accContext,oldState,newState):
 			return
 	eventHandler.queueEvent("stateChange",obj)
 
-@CFUNCTYPE(None,c_int,c_int,c_int,c_int,c_int)
+@AccessBridge_PropertyCaretChangeFP
 def internal_event_caretChange(vmID, event,source,oldPos,newPos):
 	if oldPos<0 and newPos>=0:
 		internalQueueFunction(event_gainFocus,vmID,source)
@@ -370,57 +441,42 @@ def internal_event_caretChange(vmID, event,source,oldPos,newPos):
 
 def event_enterJavaWindow(hwnd):
 	vmID=c_int()
-	accContext=c_int()
+	accContext=JOBJECT64()
 	try:
 		bridgeDll.getAccessibleContextFromHWND(hwnd,byref(vmID),byref(accContext))
 	except:
 		return
-	vmIDsToWindowHandles[vmID.value]=hwnd
+	vmID=vmID.value
+	vmIDsToWindowHandles[vmID]=hwnd
 	lastFocus=eventHandler.lastQueuedFocusObject
 	if isinstance(lastFocus,NVDAObjects.JAB.JAB) and lastFocus.windowHandle==hwnd:
 		return
-	internalQueueFunction(event_gainFocus,vmID.value,accContext.value)
+	internalQueueFunction(event_gainFocus,vmID,accContext)
 
 def isJavaWindow(hwnd):
-	if not isRunning:
+	if not bridgeDll or not isRunning:
 		return False
 	return bridgeDll.isJavaWindow(hwnd)
 
-def _errcheck(res, func, args):
-	if not res:
-		raise RuntimeError("Result %d" % res)
-
 def initialize():
-	global bridgeDll, isRunning
-	try:
-		bridgeDll=cdll.WINDOWSACCESSBRIDGE
-		for func in (
-			bridgeDll.Windows_run, bridgeDll.getAccessibleContextFromHWND, bridgeDll.getVersionInfo, 
-			bridgeDll.getAccessibleContextInfo, bridgeDll.getAccessibleTextInfo, bridgeDll.getAccessibleTextItems,
-			bridgeDll.getAccessibleTextSelectionInfo, bridgeDll.getAccessibleTextRange, bridgeDll.getAccessibleTextLineBounds,
-			bridgeDll.getCurrentAccessibleValueFromContext, bridgeDll.selectTextRange, bridgeDll.setCaretPosition,
-			bridgeDll.getAccessibleContextWithFocus, 
-		):
-			func.errcheck = _errcheck
-		bridgeDll.Windows_run()
-		#Accept wm_copydata and any wm_user messages from other processes even if running with higher privilages
-		ChangeWindowMessageFilter=getattr(windll.user32,'ChangeWindowMessageFilter',None)
-		if ChangeWindowMessageFilter:
-			if not ChangeWindowMessageFilter(winUser.WM_COPYDATA,1):
+	global isRunning
+	if not bridgeDll:
+		raise NotImplementedError("dll not available")
+	bridgeDll.Windows_run()
+	#Accept wm_copydata and any wm_user messages from other processes even if running with higher privilages
+	ChangeWindowMessageFilter=getattr(windll.user32,'ChangeWindowMessageFilter',None)
+	if ChangeWindowMessageFilter:
+		if not ChangeWindowMessageFilter(winUser.WM_COPYDATA,1):
+			raise WinError()
+		for msg in xrange(winUser.WM_USER+1,65535):
+			if not ChangeWindowMessageFilter(msg,1):
 				raise WinError()
-			for msg in xrange(winUser.WM_USER+1,65535):
-				if not ChangeWindowMessageFilter(msg,1):
-					raise WinError()
-		#Register java events
-		bridgeDll.setFocusGainedFP(internal_event_focusGained)
-		bridgeDll.setPropertyActiveDescendentChangeFP(internal_event_activeDescendantChange)
-		bridgeDll.setPropertyStateChangeFP(internal_event_stateChange)
-		bridgeDll.setPropertyCaretChangeFP(internal_event_caretChange)
-		isRunning=True
-		return True
-	except:
-		log.debugWarning("Error initializing java access bridge",exc_info=True)
-		return False
+	#Register java events
+	bridgeDll.setFocusGainedFP(internal_event_focusGained)
+	bridgeDll.setPropertyActiveDescendentChangeFP(internal_event_activeDescendantChange)
+	bridgeDll.setPropertyStateChangeFP(internal_event_stateChange)
+	bridgeDll.setPropertyCaretChangeFP(internal_event_caretChange)
+	isRunning=True
 
 def pumpAll():
 	if isRunning: 
@@ -428,12 +484,17 @@ def pumpAll():
 
 def terminate():
 	global isRunning, bridgeDll
-	if not isRunning:
+	if not bridgeDll or not isRunning:
 		return
 	bridgeDll.setFocusGainedFP(None)
 	bridgeDll.setPropertyActiveDescendentChangeFP(None)
 	bridgeDll.setPropertyStateChangeFP(None)
 	bridgeDll.setPropertyCaretChangeFP(None)
-	windll.kernel32.FreeLibrary(bridgeDll._handle)
-	cdll.WINDOWSACCESSBRIDGE=bridgeDll=None
+	h=bridgeDll._handle
+	bridgeDll=None
+	if legacyAccessBridge:
+		del cdll.windowsAccessBridge 
+	else:
+		delattr(cdll,'windowsAccessBridge-32')
+	windll.kernel32.FreeLibrary(h)
 	isRunning=False
