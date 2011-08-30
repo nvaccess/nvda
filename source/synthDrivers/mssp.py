1529322bd3a8d8887f76f7957ac1760781668ed8
James Teh <jamie@jantrid.net>
2011-08-30 16:51:01 +1000

    The SAPI 5.4 driver is actually for the Microsoft Speech Platform. Rename it accordingly. Also add info to the User Guide.

--- source/synthDrivers/mssp.py
+++ source/synthDrivers/mssp.py
@@ -1,13 +1,13 @@
-#synthDrivers/sapi5.4.py
+#synthDrivers/mssp.py
 #A part of NonVisual Desktop Access (NVDA)
-#Copyright (C) 2006-2011 NVDA Contributors <http://www.nvda-project.org/>
+#Copyright (C) 2011 NV Access Inc
 #This file is covered by the GNU General Public License.
 #See the file COPYING for more details.
 
-from sapi5 import *
+from sapi5 import SynthDriver
 
 class SynthDriver(SynthDriver):
 	COM_CLASS = "speech.SPVoice"
 
-	name="sapi5_4"
-	description="Microsoft Speech API version 5.4"
+	name="mssp"
+	description="Microsoft Speech Platform"
