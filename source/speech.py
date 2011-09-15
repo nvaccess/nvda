47c7a021ebdf1fa447ae13b0e43dff677d4c9366
James Teh <jamie@jantrid.net>
2011-09-15 17:16:39 +1000

    Don't speak clickable for menu items, tabs or sliders in browse mode.

--- source/speech.py
+++ source/speech.py
@@ -830,7 +830,7 @@ def getControlFieldSpeech(attrs,ancestorAttrs,fieldType,formatConfig=None,extraD
 	level=attrs.get('level',None)
 
 	#Remove the clickable state from controls that are clearly clickable according to their role
-	if role in (controlTypes.ROLE_LINK,controlTypes.ROLE_BUTTON,controlTypes.ROLE_CHECKBOX,controlTypes.ROLE_RADIOBUTTON,controlTypes.ROLE_TOGGLEBUTTON):
+	if role in (controlTypes.ROLE_LINK,controlTypes.ROLE_BUTTON,controlTypes.ROLE_CHECKBOX,controlTypes.ROLE_RADIOBUTTON,controlTypes.ROLE_TOGGLEBUTTON,controlTypes.ROLE_MENUITEM,controlTypes.ROLE_TAB,controlTypes.ROLE_SLIDER):
 		states=states.copy()
 		states.discard(controlTypes.STATE_CLICKABLE)
 
