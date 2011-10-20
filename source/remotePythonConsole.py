45b3cdfd6c74cf625b719e60448524530e6b979d
James Teh <jamie@jantrid.net>
2011-10-20 21:20:11 +1000

    remotePythonConsole: Wait until one line has finished executing before retrieving the next. Fixes exit(). Also some other minor fixes.

--- source/remotePythonConsole.py
+++ source/remotePythonConsole.py
@@ -21,31 +21,47 @@ class RequestHandler(SocketServer.StreamRequestHandler):
 		pass
 
 	def setPrompt(self, prompt):
+		if not self._keepRunning:
+			# We're about to exit, so don't output the prompt.
+			return
 		self.wfile.write(prompt + " ")
 
 	def exit(self):
 		self._keepRunning = False
 
+	def execute(self, line):
+		self.console.push(line)
+		# Notify handle() that the line has finished executing.
+		self._execDoneEvt.set()
+
 	def handle(self):
+		self._keepRunning = True
+
 		try:
 			self.wfile.write("NVDA Remote Python Console\n")
-			console = pythonConsole.PythonConsole(outputFunc=self.wfile.write, echoFunc=self.echo, setPromptFunc=self.setPrompt, exitFunc=self.exit)
-			console.namespace.update({
-				"snap": console.updateNamespaceSnapshotVars,
-				"rmSnap": console.removeNamespaceSnapshotVars,
+			self.console = pythonConsole.PythonConsole(outputFunc=self.wfile.write, echoFunc=self.echo, setPromptFunc=self.setPrompt, exitFunc=self.exit)
+			self.console.namespace.update({
+				"snap": self.console.updateNamespaceSnapshotVars,
+				"rmSnap": self.console.removeNamespaceSnapshotVars,
 			})
 
-			self._keepRunning = True
+			self._execDoneEvt = threading.Event()
 			while self._keepRunning:
 				line = self.rfile.readline()
 				if not line:
 					break
 				line = line.rstrip("\r\n")
 				# Execute in the main thread.
-				wx.CallAfter(console.push, line)
+				wx.CallAfter(self.execute, line)
+				# Wait until the line has finished executing before retrieving the next.
+				self._execDoneEvt.wait()
+				self._execDoneEvt.clear()
 
 		except:
 			log.exception("Error handling remote Python console request")
+		finally:
+			# Clean up the console.
+			self.console = None
 
 def initialize():
 	global server
