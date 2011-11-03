c0876df412dd5fc168a10ec87d6424db8b526561
Michael Curran <mick@kulgan.net>
2011-11-03 15:24:56 +1000

    Some tiny performance improvements to caret scripts: *EditableText._hasCaretMoved: instead of just returning a bool, return a tuple containing both a bool and a TextInfo representing the last gotten caret position, or None if the method was interupted by a script or focus event. Also provide a doc string. *EditableText.caretScriptPostMovedHelper: take a TextInfo as a new optional argument, which if given, should be used as the caret position. *EditableText.caretMovementScriptHelper:  Provide the caret position from _hasCaretMoved (if any) to caretScriptPostMovedHelper so that it does not have to fetch the caret position needlessly again.

--- source/editableText.py
+++ source/editableText.py
@@ -37,32 +37,46 @@ class EditableText(ScriptableObject):
 	shouldFireCaretMovementFailedEvents = False
 
 	def _hasCaretMoved(self, bookmark, retryInterval=0.01, timeout=0.03):
+		"""
+		Waits for the caret to move, for a timeout to elapse, or for a new focus event or script to be queued.
+		@param bookmark: a bookmark representing the position of the caret before  it was instructed to move
+		@type bookmark: bookmark
+		@param retryInterval: the interval of time in seconds this method should  wait before checking the caret each time.
+		@type retryInterval: float 
+		@param timeout: the over all amount of time in seconds the method should wait before giving up completely.
+		@type timeout: float
+		@return: a tuple containing a boolean denoting whether this method timed out, and  a TextInfo representing the old or updated caret position or None if interupted by a script or focus event.
+		@rtype: tuple
+ 		"""
 		elapsed = 0
+		newInfo=None
 		while elapsed < timeout:
 			if isScriptWaiting():
-				return False
+				return (False,None)
 			api.processPendingEvents(processEventQueue=False)
 			if eventHandler.isPendingEvents("gainFocus"):
-				return True
+				return (True,None)
 			#The caret may stop working as the focus jumps, we want to stay in the while loop though
 			try:
-				newBookmark = self.makeTextInfo(textInfos.POSITION_CARET).bookmark
+				newInfo = self.makeTextInfo(textInfos.POSITION_CARET)
+				newBookmark = newInfo.bookmark
 			except (RuntimeError,NotImplementedError):
-				pass
+				newInfo=None
 			else:
 				if newBookmark!=bookmark:
-					return True
+					return (True,newInfo)
 			time.sleep(retryInterval)
 			elapsed += retryInterval
-		return False
+		return (False,newInfo)
 
-	def _caretScriptPostMovedHelper(self, speakUnit):
+	def _caretScriptPostMovedHelper(self, speakUnit,info=None):
 		if isScriptWaiting():
 			return
-		try:
-			info = self.makeTextInfo(textInfos.POSITION_CARET)
-		except:
-			return
+		if not info:
+			try:
+				info = self.makeTextInfo(textInfos.POSITION_CARET)
+			except:
+				return
 		if config.conf["reviewCursor"]["followCaret"] and api.getNavigatorObject() is self:
 			api.setReviewPosition(info)
 		if speakUnit:
@@ -77,9 +91,10 @@ class EditableText(ScriptableObject):
 			return
 		bookmark=info.bookmark
 		gesture.send()
-		if not self._hasCaretMoved(bookmark) and self.shouldFireCaretMovementFailedEvents:
+		caretMoved,newInfo=self._hasCaretMoved(bookmark) 
+		if not caretMoved and self.shouldFireCaretMovementFailedEvents:
 			eventHandler.executeEvent("caretMovementFailed", self, gesture=gesture)
-		self._caretScriptPostMovedHelper(unit)
+		self._caretScriptPostMovedHelper(unit,newInfo)
 
 	def script_caret_moveByLine(self,gesture):
 		self._caretMovementScriptHelper(gesture, textInfos.UNIT_LINE)
