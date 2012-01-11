c6437f17fed0a738d4fb562e9b75389794b5b708
James Teh <jamie@jantrid.net>
2012-01-12 09:45:04 +1000

    Sleep mode is automatically enabled when using OpenBook. Fixes #1209.

---
+++ source/appModules/obu.py
@@ -0,0 +1,14 @@
+#appModules/obu.py
+#A part of NonVisual Desktop Access (NVDA)
+#This file is covered by the GNU General Public License.
+#See the file COPYING for more details.
+#Copyright (C) 2012 Bernd Dorer
+
+"""App module for OpenBook
+"""
+
+import appModuleHandler
+
+class AppModule(appModuleHandler.AppModule):
+
+	sleepMode=True
