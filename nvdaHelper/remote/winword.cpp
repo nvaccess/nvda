36e1e1f8b6758817610428b4f2e5bdf25af97b57
Michael Curran <mick@kulgan.net>
2012-03-23 15:08:34 +1000

    Fix freezes in MS Word which would result when it took more than 2 seconds to fetch text from a document (extremely long lines or tables of contents). Fixes #2191. Specifically: use SendMessage instead of SendMessageTimeout in the rpc functions as even if the main thread took too long, simply timing out is more dangerous as it causes memory corruption and therefore possibly endless loops.

--- nvdaHelper/remote/winword.cpp
+++ nvdaHelper/remote/winword.cpp
@@ -556,9 +556,9 @@ LRESULT CALLBACK winword_callWndProcHook(int code, WPARAM wParam, LPARAM lParam)
 }
 
 error_status_t nvdaInProcUtils_winword_expandToLine(handle_t bindingHandle, const long windowHandle, const int offset, int* lineStart, int* lineEnd) {
-	winword_expandToLine_args args={offset,3,4};
+	winword_expandToLine_args args={offset,offset,offset+1};
 	DWORD_PTR wmRes=0;
-	SendMessageTimeout((HWND)windowHandle,wm_winword_expandToLine,(WPARAM)&args,0,SMTO_ABORTIFHUNG,2000,&wmRes);
+	SendMessage((HWND)windowHandle,wm_winword_expandToLine,(WPARAM)&args,0);
 	*lineStart=args.lineStart;
 	*lineEnd=args.lineEnd;
 	return RPC_S_OK;
@@ -566,8 +566,7 @@ error_status_t nvdaInProcUtils_winword_expandToLine(handle_t bindingHandle, cons
 
 error_status_t nvdaInProcUtils_winword_getTextInRange(handle_t bindingHandle, const long windowHandle, const int startOffset, const int endOffset, const long formatConfig, BSTR* text) { 
 	winword_getTextInRange_args args={startOffset,endOffset,formatConfig,NULL};
-	DWORD_PTR wmRes=0;
-	SendMessageTimeout((HWND)windowHandle,wm_winword_getTextInRange,(WPARAM)&args,0,SMTO_ABORTIFHUNG,2000,&wmRes);
+	SendMessage((HWND)windowHandle,wm_winword_getTextInRange,(WPARAM)&args,0);
 	*text=args.text;
 	return RPC_S_OK;
 }
