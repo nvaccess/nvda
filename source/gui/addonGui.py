0ffbd35f9e270af0c939c92c65297f011b091e6a
James Teh <jamie@jantrid.net>
2012-05-10 05:04:33 +1000

    Add/update copyright headers.

--- source/gui/addonGui.py
+++ source/gui/addonGui.py
@@ -1,3 +1,9 @@
+#gui/addonGui.py
+#A part of NonVisual Desktop Access (NVDA)
+#This file is covered by the GNU General Public License.
+#See the file COPYING for more details.
+#Copyright (C) 2012 NV Access Limited
+
 import wx
 import core
 import gui
