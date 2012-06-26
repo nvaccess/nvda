5cc6a7c020910599603a1c1658eb9653ca65c898
Michael Herrmann <mherrmann.at@gmail.com>
2012-06-26 15:41:23 +0200

    NVDAHelperLocal's dllImportTableHooks now uses the logging (mini-)framework instead of printf. This keeps the console output much cleaner when running nvda.pyw on the command line.

--- nvdaHelper/local/dllImportTableHooks.cpp
+++ nvdaHelper/local/dllImportTableHooks.cpp
@@ -15,6 +15,7 @@ http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 #include <cstdio>
 #include <string.h>
 #include "dllImportTableHooks.h"
+#include <common\log.h>
 #include <shlwapi.h>
 #include <psapi.h>
 
@@ -27,28 +28,28 @@ BOOL DllImportTableHooks::hookFunctions() {
 	char moduleName[MAX_PATH];
 	GetModuleFileNameA(this->targetModule,moduleName,MAX_PATH);
 	PathStripPathA(moduleName);
-	printf("_hookFunctionsOnModule: called for %s\n",moduleName);
+	LOG_DEBUG(L"_hookFunctionsOnModule: called for " << moduleName << endl);
 	UINT_PTR  imageBaseAddress=(UINT_PTR)this->targetModule;
 	if(!imageBaseAddress) {
-		fprintf(stderr,"_hookFunctionsOnModule: could not locate base address of %s, returning false\n",moduleName); 
+		LOG_ERROR(L"_hookFunctionsOnModule: could not locate base address of " << moduleName << L" returning false" << endl);
 		return FALSE;
 	}
 	UINT_PTR impDescRVA=((IMAGE_NT_HEADERS*)(imageBaseAddress+((IMAGE_DOS_HEADER*)imageBaseAddress)->e_lfanew))->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress;
 	if(impDescRVA==0) {
-		printf("_hookFunctionsOnModule: %s has no import table, nothing to do, returning true\n",moduleName);
+		LOG_DEBUG(L"_hookFunctionsOnModule: " << moduleName << L" has no import table, nothing to do, returning true" << endl);
 		return TRUE;
 	}
-	printf("_hookFunctionsOnModule: %s import table at address 0X%X\n",moduleName,imageBaseAddress+impDescRVA);
+	LOG_DEBUG(L"_hookFunctionsOnModule: " << moduleName << L" import table at address 0X" << imageBaseAddress+impDescRVA << endl);
 	for(IMAGE_IMPORT_DESCRIPTOR* pImpDesc=(IMAGE_IMPORT_DESCRIPTOR*)(imageBaseAddress+impDescRVA);pImpDesc->Name!=0;pImpDesc++) {
 		char* curModuleName=(char*)(imageBaseAddress+pImpDesc->Name);
 		moduleNameToFuncToFunc_t::const_iterator i=this->hookRequests.find(curModuleName);
 		if(i!=this->hookRequests.end()) {
-			printf("_hookFunctionsOnModule: import table for %s: module section for %s: looking for functions to hook\n",moduleName,curModuleName); 
+			LOG_DEBUG(L"_hookFunctionsOnModule: import table for " << moduleName << L": module section for " << curModuleName << L": looking for functions to hook\n");
 			for(IMAGE_THUNK_DATA* pThunk=(IMAGE_THUNK_DATA*)(imageBaseAddress+(pImpDesc->FirstThunk));pThunk->u1.Function!=0;pThunk++) {
 				void* funcAddress=(void*)(pThunk->u1.Function);
 				funcToFunc_t::const_iterator j=i->second.find(funcAddress);
 				if(j!=i->second.end()) {
-					printf("_hookFunctionsOnModule: import table for %s: module section for %s: function at address 0X%X found , changing thunk to use function at address 0X%X\n",moduleName,curModuleName,pThunk->u1.Function,j->second);
+					LOG_DEBUG(L"_hookFunctionsOnModule: import table for " << moduleName << L": module section for " << curModuleName << L": function at address 0X" << (VOID*)pThunk->u1.Function << L" found , changing thunk to use function at address 0X" << j->second << L"\n");
 					this->hookedFunctions[funcAddress]=pThunk;
 					DWORD oldProtect;
 					VirtualProtect(pThunk,sizeof(IMAGE_THUNK_DATA),PAGE_READWRITE,&oldProtect);
@@ -57,49 +58,49 @@ BOOL DllImportTableHooks::hookFunctions() {
 				}
 			}
 		} else {
-			printf("_hookFunctionsOnModule: import table for %s: module section for %s: no requested hooks\n",moduleName,curModuleName);
+			LOG_DEBUG(L"_hookFunctionsOnModule: import table for " << moduleName << L": module section for " << curModuleName << L": no requested hooks\n");
 		}
 	}
-	printf("_hookFunctionsOnModule: successfully walked %s's import table, returning true\n",moduleName); 
+	LOG_DEBUG(L"_hookFunctionsOnModule: successfully walked " << moduleName << L"'s import table, returning true\n");
 	return TRUE;
 }
 
 void* DllImportTableHooks::requestFunctionHook(const char* moduleName, const char* functionName, void* newHookProc) {
 	HMODULE moduleHandle=GetModuleHandleA(moduleName);
 	if(!moduleHandle) {
-		fprintf(stderr,"apiHook_requestFunctionHook: module %s not loaded\n");
+		LOG_ERROR(L"apiHook_requestFunctionHook: module " << moduleName << L" not loaded\n");
 		return NULL;
 	}
 	void* realFunc=GetProcAddress(moduleHandle,functionName);
 	if(!realFunc) {
-		fprintf(stderr,"apiHook_requestFunctionHook: function %s does not exist in module %s\n",functionName,moduleName);
+		LOG_ERROR(L"apiHook_requestFunctionHook: function " << functionName << L" does not exist in module " << moduleName << L"\n");
 		return NULL;
 	}
-	printf("apiHook_requestFunctionHook: requesting to hook function %s at address 0X%X in module %s at address 0X%X with  new function at address 0X%X\n",functionName,realFunc,moduleName,moduleHandle,newHookProc);
+	LOG_DEBUG(L"apiHook_requestFunctionHook: requesting to hook function " << functionName << L" at address 0X" << realFunc << L" in module " << moduleName << L" at address 0X" << moduleHandle << L" with new function at address 0X" << newHookProc << L"\n");
 	funcToFunc_t& funcToFunc=this->hookRequests[moduleName];
 	funcToFunc_t::iterator i=funcToFunc.find(realFunc);
 	void* oldFunc=NULL;
 	if(i!=funcToFunc.end()) {
 		oldFunc=i->second;
-		printf("api_requestHookFunction: hook already exists for function at address 0X%X, setting new hook and returning old one\n",realFunc);
+		LOG_DEBUG(L"api_requestHookFunction: hook already exists for function at address 0X" << realFunc << L", setting new hook and returning old one\n");
 		i->second=newHookProc;
 	} else {
 		oldFunc=realFunc;
-		printf("apiHook_requestFunctionHook: no existing hook for function at address 0X%X, setting hook and returning real function\n",realFunc);
+		LOG_DEBUG(L"apiHook_requestFunctionHook: no existing hook for function at address 0X" << realFunc << L", setting hook and returning real function\n");
 		funcToFunc[realFunc]=newHookProc;
 	}
 	return oldFunc;
 }
 
 BOOL DllImportTableHooks::unhookFunctions() {
-	printf("apiHook_unhookFunctions: unhooking all functions\n");
+	LOG_DEBUG(L"apiHook_unhookFunctions: unhooking all functions\n");
 	if(this->hookedFunctions.size()==0) {
-		printf("apiHook_unhookFunctions: no functions hooked, returnning true\n");
+		LOG_DEBUG(L"apiHook_unhookFunctions: no functions hooked, returning true\n");
 		return TRUE;
 	}
-	printf("apiHook_unhookFunctions: unhooking functions on module at address 0X%X\n",targetModule);
+	LOG_DEBUG(L"apiHook_unhookFunctions: unhooking functions on module at address 0X" << targetModule << L"\n");
 	for(funcToThunk_t::iterator i=this->hookedFunctions.begin();i!=this->hookedFunctions.end();i++) {
-		printf("apiHook_unhookFunctions: restoring thunk at address 0X%X back to original function at address 0X%X\n",i->second,i->first);
+		LOG_DEBUG(L"apiHook_unhookFunctions: restoring thunk at address 0X" << i->second << L" back to original function at address 0X" << i->first << L"\n");
 		DWORD oldProtect;
 		VirtualProtect(i->second,sizeof(IMAGE_THUNK_DATA),PAGE_READWRITE,&oldProtect);
 		i->second->u1.Function=(UINT_PTR)i->first;
