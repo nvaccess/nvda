80ad81675f5762ede22cd1ffadbf6811aed89724
Michael Curran <mick@kulgan.net>
2012-07-09 14:37:21 +1000

    Windows 8 Start Screen tiles: The live status info is now presented as the tile's description (e.g. current temperature for the weather tile). Also yet again the annoying "launch blah" description has been suppressed now even for tiles in the all apps list mode.

--- source/NVDAObjects/UIA/__init__.py
+++ source/NVDAObjects/UIA/__init__.py
@@ -159,6 +159,8 @@ class UIA(Window):
 		UIAClassName=self.UIAElement.cachedClassName
 		if UIAClassName=="GridTileElement":
 			clsList.append(GridTileElement)
+		elif UIAClassName=="GridListTileElement":
+			clsList.append(GridListTileElement)
 		if UIAClassName=="ToastContentHost" and UIAControlType==UIAHandler.UIA_ToolTipControlTypeId:
 			clsList.append(Toast)
 		if UIAControlType==UIAHandler.UIA_ProgressBarControlTypeId:
@@ -614,5 +616,18 @@ class Toast(UIA):
 
 class GridTileElement(UIA):
 
+	role=controlTypes.ROLE_TABLECELL
+
+	def _get_description(self):
+		name=self.name
+		descriptionStrings=[]
+		for child in self.children:
+			description=child.basicText
+			if not description or description==name: continue
+			descriptionStrings.append(description)
+		return " ".join(descriptionStrings)
+		return description
+
+class GridListTileElement(UIA):
 	role=controlTypes.ROLE_TABLECELL
 	description=None
