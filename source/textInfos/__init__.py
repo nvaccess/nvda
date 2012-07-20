4acae85c5cdedb55f39bd4de4c2e0feb882423c4
Michael Curran <mick@kulgan.net>
2012-07-20 13:47:28 +1000

    Base TextInfo's activatemethod: provide a default implementation that uses the mouse to click on the point given by the TextInfo's pointAtStart property if its implemented, and if the TextInfo's underlying object is within the foreground. This allows  focusing in edit fields when double tapping in them

--- source/textInfos/__init__.py
+++ source/textInfos/__init__.py
@@ -434,7 +434,15 @@ class TextInfo(baseObject.AutoPropertyObject):
 		For example, this might activate the object at this position or click the point at this position.
 		@raise NotImplementedError: If not supported.
 		"""
-		raise NotImplementedError
+		if not self.obj.isInForeground:
+			raise NotImplementedError
+		import winUser
+		p=self.pointAtStart
+		oldX,oldY=winUser.getCursorPos()
+		winUser.setCursorPos(p.x,p.y)
+		winUser.mouse_event(winUser.MOUSEEVENTF_LEFTDOWN,0,0,None,None)
+		winUser.mouse_event(winUser.MOUSEEVENTF_LEFTUP,0,0,None,None)
+		winUser.setCursorPos(oldX,oldY)
 
 RE_EOL = re.compile("\r\n|[\n\r]")
 def convertToCrlf(text):
