8e50da363ad52de537705ee0a8d1ac6dad35a6f0
James Teh <jamie@jantrid.net>
2012-10-25 16:40:54 +1000

    Add/fix copyright headers.

--- source/touchHandler.py
+++ source/touchHandler.py
@@ -1,3 +1,9 @@
+#touchHandler.py
+#A part of NonVisual Desktop Access (NVDA)
+#This file is covered by the GNU General Public License.
+#See the file COPYING for more details.
+#Copyright (C) 2012 NV Access Limited
+
 import threading
 from ctypes import *
 from ctypes.wintypes import *
