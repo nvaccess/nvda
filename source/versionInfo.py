ffe17ef4162bc353daa703e1fa66e2260e53c3d3
Mesar Hameed <mhameed@src.gnome.org>
2013-03-03 07:06:49 +0100

    update copyright years variable to reflect that we are in 2013.

--- source/versionInfo.py
+++ source/versionInfo.py
@@ -1,6 +1,6 @@
 #versionInfo.py
 #A part of NonVisual Desktop Access (NVDA)
-#Copyright (C) 2006-2012 NV Access Limited
+#Copyright (C) 2006-2013 NV Access Limited
 #This file is covered by the GNU General Public License.
 #See the file COPYING for more details.
 
@@ -39,7 +39,7 @@ except ImportError:
 	_updateVersionFromVCS()
 description=_("A free and open source screen reader for Microsoft Windows")
 url="http://www.nvda-project.org/"
-copyrightYears="2006-2012"
+copyrightYears="2006-2013"
 copyright=_("Copyright (C) {years} NVDA Contributors").format(
 	years=copyrightYears)
 aboutMessage=_(u"""{longName} ({name})
