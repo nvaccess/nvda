6b9f1e3bb6c64bf71794b664402ec3d00113157a
Michael Curran <mick@kulgan.net>
2013-05-07 16:27:48 +1000

    Remove a print statement

--- source/appModules/msimn.py
+++ source/appModules/msimn.py
@@ -93,7 +93,6 @@ class MessageListItem(sysListView32.ListItem):
 		info=displayModel.DisplayModelTextInfo(self,textInfos.POSITION_FIRST)
 		info.expand(textInfos.UNIT_CHARACTER)
 		fields=info.getTextWithFields()
-		print fields
 		try:
 			isUnread=fields[0].field['bold']
 		except:
