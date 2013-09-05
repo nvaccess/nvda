99b58fbc54e6d47ea5fb014c3430b31f4709571f
James Teh <jamie@nvaccess.org>
2013-09-05 16:15:02 +1000

    Implement support for getting display text for keyboard gesture identifiers.
    Because this deals with normalized identifiers, all of the key names in the keyLabels module had to be converted to entirely lower case. Also, the generalized modifiers had to be added to KeyboardInputGesture.NORMAL_MODIFIER_KEYS so we have a way to tell they are modifiers.

--- source/keyLabels.py
+++ source/keyLabels.py
@@ -6,45 +6,45 @@
 
 localizedKeyLabels = {
 	# Translators: This is the name of the back key found on multimedia keyboards for controlling the web-browser.
-	'browserBack': _("back"),
+	'browserback': _("back"),
 	# Translators: This is the name of the forward key found on multimedia keyboards for controlling the web-browser.
-	'browserForward': _("forward"),
+	'browserforward': _("forward"),
 	# Translators: This is the name of the refresh key found on multimedia keyboards for controlling the web-browser.
-	'browserRefresh': _("refresh"),
+	'browserrefresh': _("refresh"),
 	# Translators: This is the name of the stop key found on multimedia keyboards for controlling the web-browser.
-	'browserStop': _("browser stop"),
+	'browserstop': _("browser stop"),
 	# Translators: This is the name of the back key found on multimedia keyboards to goto the search page of the web-browser.
-	'browserSearch': _("search page"),
+	'browsersearch': _("search page"),
 	# Translators: This is the name of the favorites key found on multimedia keyboards to open favorites in the web-browser.
-	'browserFavorites': _("favorites"),
+	'browserfavorites': _("favorites"),
 	# Translators: This is the name of the home key found on multimedia keyboards to goto the home page in the web-browser.
-	'browserHome': _("home page"),
+	'browserhome': _("home page"),
 	# Translators: This is the name of the mute key found on multimedia keyboards to control playback volume.
-	'volumeMute': _("mute"),
+	'volumemute': _("mute"),
 	# Translators: This is the name of the volume down key found on multimedia keyboards to reduce playback volume.
-	'volumeDown': _("volume down"),
+	'volumedown': _("volume down"),
 	# Translators: This is the name of the volume up key found on multimedia keyboards to increase playback volume.
-	'volumeUp': _("volume up"),
+	'volumeup': _("volume up"),
 	# Translators: This is the name of the next track key found on multimedia keyboards to skip to next track in the mediaplayer.
-	'mediaNextTrack': _("next track"),
+	'medianexttrack': _("next track"),
 	# Translators: This is the name of the next track key found on multimedia keyboards to skip to next track in the mediaplayer.
-	'mediaPrevTrack': _("previous track"),
+	'mediaprevtrack': _("previous track"),
 	# Translators: This is the name of the stop key found on multimedia keyboards to stop the current playing track in the mediaplayer.
-	'mediaStop': _("stop"),
+	'mediastop': _("stop"),
 	# Translators: This is the name of the play/pause key found on multimedia keyboards to play/pause the current playing track in the mediaplayer.
-	'mediaPlayPause': _("play pause"),
+	'mediaplaypause': _("play pause"),
 	# Translators: This is the name of the launch email key found on multimedia keyboards to open an email client.
-	'launchMail': _("email"),
+	'launchmail': _("email"),
 	# Translators: This is the name of the launch mediaplayer key found on multimedia keyboards to launch the mediaplayer.
-	'launchMediaPlayer': _("media player"),
+	'launchmediaplayer': _("media player"),
 	# Translators: This is the name of the launch custom application 1 key found on multimedia keyboards to launch a user-defined application.
-	'launchApp1': _("custom application 1"),
+	'launchapp1': _("custom application 1"),
 	# Translators: This is the name of the launch custom application 2 key found on multimedia keyboards to launch a user-defined application.
-	'launchApp2': _("custom application 2"),
+	'launchapp2': _("custom application 2"),
 	# Translators: This is the name of a key on the keyboard.
 	'backspace': _("backspace"),
 	# Translators: This is the name of a key on the keyboard.
-	'capsLock': _("caps lock"),
+	'capslock': _("caps lock"),
 	# Translators: This is the name of a key on the keyboard.
 	'control': _("ctrl"),
 	# Translators: This is the name of a key on the keyboard.
@@ -56,15 +56,15 @@ localizedKeyLabels = {
 	# Translators: This is the name of a key on the keyboard.
 	'enter': _("enter"),
 	# Translators: This is the name of a key on the keyboard.
-	'numpadEnter': _("numpad enter"),
+	'numpadenter': _("numpad enter"),
 	# Translators: This is the name of a key on the keyboard.
 	'escape': _("escape"),
 	# Translators: This is the name of a key on the keyboard.
 	'space': _("space"),
 	# Translators: This is the name of a key on the keyboard.
-	'pageUp': _("page up"),
+	'pageup': _("page up"),
 	# Translators: This is the name of a key on the keyboard.
-	'pageDown': _("page down"),
+	'pagedown': _("page down"),
 	# Translators: This is the name of a key on the keyboard.
 	'end': _("end"),
 	# Translators: This is the name of a key on the keyboard.
@@ -72,23 +72,23 @@ localizedKeyLabels = {
 	# Translators: This is the name of a key on the keyboard.
 	'delete': _("delete"),
 	# Translators: This is the name of a key on the keyboard.
-	'numpadDelete': _("numpad delete"),
+	'numpaddelete': _("numpad delete"),
 	# Translators: This is the name of a key on the keyboard.
-	'leftArrow': _("left arrow"),
+	'leftarrow': _("left arrow"),
 	# Translators: This is the name of a key on the keyboard.
-	'rightArrow': _("right arrow"),
+	'rightarrow': _("right arrow"),
 	# Translators: This is the name of a key on the keyboard.
-	'upArrow': _("up arrow"),
+	'uparrow': _("up arrow"),
 	# Translators: This is the name of a key on the keyboard.
-	'downArrow': _("down arrow"),
+	'downarrow': _("down arrow"),
 	# Translators: This is the name of a key on the keyboard.
 	'applications': _("applications"),
 	# Translators: This is the name of a key on the keyboard.
-	'numLock': _("num lock"),
+	'numlock': _("num lock"),
 	# Translators: This is the name of a key on the keyboard.
-	'printScreen': _("print screen"),
+	'printscreen': _("print screen"),
 	# Translators: This is the name of a key on the keyboard.
-	'scrollLock': _("scroll lock"),
+	'scrolllock': _("scroll lock"),
 	# Translators: This is the name of a key on the keyboard.
 	'numpad4': _("numpad 4"),
 	# Translators: This is the name of a key on the keyboard.
@@ -108,29 +108,29 @@ localizedKeyLabels = {
 	# Translators: This is the name of a key on the keyboard.
 	'numpad5': _("numpad 5"),
 	# Translators: This is the name of a key on the keyboard.
-	'numpadDivide': _("numpad divide"),
+	'numpaddivide': _("numpad divide"),
 	# Translators: This is the name of a key on the keyboard.
-	'numpadMultiply': _("numpad multiply"),
+	'numpadmultiply': _("numpad multiply"),
 	# Translators: This is the name of a key on the keyboard.
-	'numpadMinus': _("numpad minus"),
+	'numpadminus': _("numpad minus"),
 	# Translators: This is the name of a key on the keyboard.
-	'numpadPlus': _("numpad plus"),
+	'numpadplus': _("numpad plus"),
 	# Translators: This is the name of a key on the keyboard.
-	'leftControl': _("left control"),
+	'leftcontrol': _("left control"),
 	# Translators: This is the name of a key on the keyboard.
-	'rightControl': _("right control"),
+	'rightcontrol': _("right control"),
 	# Translators: This is the name of a key on the keyboard.
-	'leftWindows': _("left windows"),
+	'leftwindows': _("left windows"),
 	# Translators: This is the name of a key on the keyboard.
-	'leftShift': _("left shift"),
+	'leftshift': _("left shift"),
 	# Translators: This is the name of a key on the keyboard.
-	'rightShift': _("right shift"),
+	'rightshift': _("right shift"),
 	# Translators: This is the name of a key on the keyboard.
-	'leftAlt': _("left alt"),
+	'leftalt': _("left alt"),
 	# Translators: This is the name of a key on the keyboard.
-	'rightAlt': _("right alt"),
+	'rightalt': _("right alt"),
 	# Translators: This is the name of a key on the keyboard.
-	'rightWindows': _("right windows"),
+	'rightwindows': _("right windows"),
 	# Translators: This is the name of a key on the keyboard.
 	'break': _("break"),
 	# Translators: This is the name of a key on the keyboard.
