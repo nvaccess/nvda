a809e841534d51300c411b15485bb0e94eef730f
Michael Curran <mick@nvaccess.org>
2013-10-31 20:55:53 +1000

    Allow a way to use COM's getActiveObject in-process via rpc which may be useful for some sandboxed applications (e.g. MS Office Protected mode).
    comHelper.getActiveObject if ggiven an appModule via a new appModule keyword argument will make the call via rpc to the given application, rather than doing it out-of-process.

--- nvdaHelper/remote/rpcSrv.cpp
+++ nvdaHelper/remote/rpcSrv.cpp
@@ -114,3 +114,17 @@ error_status_t nvdaInProcUtils_unregisterNVDAProcess(nvdaRegistrationHandle_t* r
 void __RPC_USER nvdaRegistrationHandle_t_rundown(nvdaRegistrationHandle_t registrationHandle) {
 	nvdaInProcUtils_unregisterNVDAProcess(&registrationHandle);
 }
+
+error_status_t nvdaInProcUtils_getActiveObject(handle_t bindingHandle, const wchar_t* progid, IUnknown** ppUnknown) {
+	if(!progid) {
+		LOG_DEBUGWARNING(L"NULL progid");
+		return E_FAIL;
+	}
+	IID clsid;
+	HRESULT res=CLSIDFromString(progid,&clsid);
+	if(res!=NOERROR) {
+		LOG_DEBUGWARNING(L"CLSIDFromString for "<<progid<<L" returned "<<res);
+		return res;
+	}
+	return GetActiveObject(clsid,NULL,ppUnknown);
+}
