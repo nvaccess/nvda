a60a98c8b40f9de85f696f55272be77c6f040d7a
James Teh <jamie@nvaccess.org>
2014-01-20 22:52:01 +0000

    Make the description for NVDA slave just "NVDA".
    This description is what appears in the UAC dialog whenever we request elevation. "Slave" might confuse/worry users and it also isn't translated.

--- source/setup.py
+++ source/setup.py
@@ -153,7 +153,7 @@ setup(
 			"script": "nvda_slave.pyw",
 			"icon_resources": [(1,"images/nvda.ico")],
 			"version": "0.0.0.0",
-			"description": "NVDA slave",
+			"description": versionInfo.name,
 			"product_version": version,
 			"copyright": copyright,
 			"company_name": publisher,
