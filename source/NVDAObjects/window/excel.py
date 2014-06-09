c2f345c3396eb472d8751e2017cd4dcf8cb5e22b
Michael Curran <mick@nvaccess.org>
2014-06-09 16:43:36 +1000

    Move HeaderCellInfo and HeaderCellTracker out of the Excel module and into a new tableUtils module which allows these classes to be ossibly used by other code in the future, such as for MS Word auto header announcement.

--- source/NVDAObjects/window/excel.py
+++ source/NVDAObjects/window/excel.py
@@ -13,6 +13,7 @@ import uuid
 import collections
 import oleacc
 import ui
+from tableUtils import HeaderCellInfo, HeaderCellTracker
 import config
 import textInfos
 import colors
@@ -102,46 +103,6 @@ class Excel7Window(ExcelBase):
 			return
 		if selection:
 			eventHandler.executeEvent('gainFocus',selection)
-
-class HeaderCellInfo(object):
-	__slots__=['rowNumber','columnNumber','rowSpan','colSpan','minRowNumber','maxRowNumber','minColumnNumber','maxColumnNumber','name','isRowHeader','isColumnHeader']
-	def __init__(self,**kwargs):
-		self.minColumnNumber=self.maxColumnNumber=self.minRowNumber=self.maxRowNumber=None
-		for  name,value in kwargs.iteritems():
-			setattr(self,name,value)
-
-class HeaderCellTracker(object):
-
-	def __init__(self):
-		self.infosDict={}
-		self.listByRow=[]
-		self.listByColumn=[]
-
-	def addHeaderCellInfo(self,**kwargs):
-		info=HeaderCellInfo(**kwargs)
-		key=(info.rowNumber,info.columnNumber)
-		self.infosDict[key]=info
-		self.listByRow.append(key)
-		self.listByRow.sort(reverse=True)
-		self.listByColumn.append(key)
-		self.listByColumn.sort(key=lambda k: (k[1],k[0]),reverse=True)
-
-	def removeHeaderCellInfo(self,info):
-		key=(info.rowNumber,info.columnNumber)
-		self.listByRow.remove(key)
-		self.listByColumn.remove(key)
-		del self.infosDict[key]
-
-	def getHeaderCellInfoAt(self,rowNumber,columnNumber):
-		return self.infosDict.get((rowNumber,columnNumber))
-
-	def iterPossibleHeaderCellInfosFor(self,rowNumber,columnNumber,columnHeader=False):
-		for key in (self.listByColumn if columnHeader else self.listByRow):
-			info=self.infosDict[key]
-			if (info.minColumnNumber and info.minColumnNumber>columnNumber) or (info.maxColumnNumber and info.maxColumnNumber<columnNumber) or (info.minRowNumber and info.minRowNumber>rowNumber) or (info.maxRowNumber and info.maxRowNumber<rowNumber):
-				continue
-			if (columnHeader and info.isColumnHeader and (info.rowNumber+info.rowSpan-1)<rowNumber and info.columnNumber<=columnNumber) or (not columnHeader and info.isRowHeader and (info.columnNumber+info.colSpan-1)<columnNumber and info.rowNumber<=rowNumber):
-				yield info
 
 class ExcelWorksheet(ExcelBase):
 
