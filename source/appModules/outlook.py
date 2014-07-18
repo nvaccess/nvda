14fef6b6c3d04afbca04a6a07167292a3c3603dc
Joseph Lee <joseph.lee22590@gmail.com>
2014-07-18 01:56:37 -0700

    Outlook/CalendarView: be sure to distinguish between an actual time slot and a journal entry, as COM erro is thrown when entering journal folder. re #4305 In report focus method for calendar view, the method assumes that the user wishes to hear time slots. This may not be the case for all objects claiming to be calendar view, such as journal entries. Thus catch this case for journal. This prevents error tones from being heard when switching to Journal in Outlook 2013 (may apply to Outlook 2010).

--- source/appModules/outlook.py
+++ source/appModules/outlook.py
@@ -340,8 +340,11 @@ class CalendarView(IAccessible):
 				speech.speakMessage("Appointment %s, %s"%(p.subject,t))
 			else:
 				v=e.currentView
-				selectedStartTime=v.selectedStartTime
-				selectedEndTime=v.selectedEndTime
+				try:
+					selectedStartTime=v.selectedStartTime
+					selectedEndTime=v.selectedEndTime
+				except COMError:
+					return super(CalendarView,self).reportFocus()
 				timeSlotText=self._generateTimeRangeText(selectedStartTime,selectedEndTime)
 				startLimit=u"%s %s"%(winKernel.GetDateFormat(winKernel.LOCALE_USER_DEFAULT, winKernel.DATE_LONGDATE, selectedStartTime, None),winKernel.GetTimeFormat(winKernel.LOCALE_USER_DEFAULT, winKernel.TIME_NOSECONDS, selectedStartTime, None))
 				endLimit=u"%s %s"%(winKernel.GetDateFormat(winKernel.LOCALE_USER_DEFAULT, winKernel.DATE_LONGDATE, selectedEndTime, None),winKernel.GetTimeFormat(winKernel.LOCALE_USER_DEFAULT, winKernel.TIME_NOSECONDS, selectedEndTime, None))
