1c590c2f396b241bcb5a23b308f2c3563e4d4195
James Teh <jamie@nvaccess.org>
2014-10-10 18:09:49 +1000

    When selecting text in the code editing control in the Eclipse IDE, the entire selection is no longer announced every time the selection changes.
    Fixes #2314.

--- source/appModules/eclipse.py
+++ source/appModules/eclipse.py
@@ -2,10 +2,20 @@
 #A part of NonVisual Desktop Access (NVDA)
 #This file is covered by the GNU General Public License.
 #See the file COPYING for more details.
-#Copyright (C) 2010 James Teh <jamie@jantrid.net>
+#Copyright (C) 2010-2014 NV Access Limited
 
 import controlTypes
 import appModuleHandler
+from NVDAObjects.IAccessible import IAccessible
+
+class EclipseTextArea(IAccessible):
+
+	def event_valueChange(self):
+		# #2314: Eclipse incorrectly fires valueChange when the selection changes.
+		# Unfortunately, this causes us to speak the entire selection
+		# instead of just the changed selection.
+		# Therefore, just drop this event.
+		pass
 
 class AppModule(appModuleHandler.AppModule):
 
@@ -14,3 +24,7 @@ class AppModule(appModuleHandler.AppModule):
 			# Eclipse tree views seem to fire a focus event on the previously focused item before firing focus on the new item (EclipseBug:315339).
 			# Try to filter this out.
 			obj.shouldAllowIAccessibleFocusEvent = False
+
+	def chooseNVDAObjectOverlayClasses(self, obj, clsList):
+		if obj.windowClassName == "SWT_Window0" and obj.role == controlTypes.ROLE_EDITABLETEXT:
+			clsList.insert(0, EclipseTextArea)
