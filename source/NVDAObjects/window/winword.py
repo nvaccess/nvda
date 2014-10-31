3b6a3658e647f9918084535dbe6ed40ada974880
Michael Curran <mick@nvaccess.org>
2014-10-31 13:39:51 +1000

    Add some winword goto constants needed for future quickNav work

--- source/NVDAObjects/window/winword.py
+++ source/NVDAObjects/window/winword.py
@@ -83,8 +83,23 @@ wdGoToRelative=2
 wdGoToNext=2
 wdGoToPrevious=3
 #GoTo - units
+wdGoToBookmark=-1
+wdGoToSection=0
 wdGoToPage=1
+wdGoToTable=2
 wdGoToLine=3
+wdGoToFootnote=4
+wdGoToEndnote=5
+wdGoToComment=6
+wdGoToField=7
+wdGoToGraphic=8
+wdGoToObject=9
+wdGoToEquation=10
+wdGoToHeading=11
+wdGoToPercent=12
+wdGoToSpellingError=13
+wdGoToGrammaticalError=14
+wdGoToProofreadingError=15
 
 wdCommentsStory=4
 wdEndnotesStory=3
