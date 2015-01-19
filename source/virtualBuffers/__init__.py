dcb1c0e0077cae3a58b1b3f2aef20c3a59c8c2d9
James Teh <jamie@nvaccess.org>
2015-01-19 17:09:50 +1000

    In browse mode, make it possible to interact with math under the browse mode cursor by simply pressing enter.

--- source/virtualBuffers/__init__.py
+++ source/virtualBuffers/__init__.py
@@ -603,6 +603,19 @@ class VirtualBuffer(cursorManager.CursorManager, browseMode.BrowseModeTreeInterc
 		obj = info.NVDAObjectAtStart
 		if not obj:
 			return
+		if obj.role == controlTypes.ROLE_MATH:
+			import mathPres
+			mathPres.ensureInit()
+			if not mathPres.interactionProvider:
+				# Translators: Reported when the user attempts math interaction
+				# but math interaction is not supported.
+				ui.message(_("Math interaction not supported."))
+				return
+			try:
+				return mathPres.interactionProvider.interactWithMathMl(obj.mathMl)
+			except (NotImplementedError, LookupError):
+				pass
+			return
 		if self.shouldPassThrough(obj):
 			obj.setFocus()
 			self.passThrough = True
