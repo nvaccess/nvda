295d708e813d82b11f7b81f0bd643f1774b580e3
James Teh <jamie@nvaccess.org>
2015-02-03 12:11:59 +1000

    Update copyright headers.

--- source/browseMode.py
+++ source/browseMode.py
@@ -1,5 +1,5 @@
 #A part of NonVisual Desktop Access (NVDA)
-#Copyright (C) 2006-2014 NVDA Contributors
+#Copyright (C) 2015 NV Access Limited
 #This file is covered by the GNU General Public License.
 #See the file COPYING for more details.
 
