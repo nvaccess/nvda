92af37e94d8e0fd360cdf44fb989825b4e833d93
Siddhartha <gupta.siddharthasbs@gmail.com>
2015-02-25 17:49:47 +0530

    Set default font size to 11; changed obscured to cropped in and obscuring to overflowing

--- source/NVDAObjects/window/excel.py
+++ source/NVDAObjects/window/excel.py
@@ -582,7 +582,7 @@ class ExcelCell(ExcelBase):
 		deviceCaps = ctypes.windll.gdi32.GetDeviceCaps(tempDC, 88)
 		#Fetching Font Size and Weight information
 		iFontSize = self.excelCellObject.Font.Size
-		iFontSize = int(iFontSize)
+		iFontSize = 11 if iFontSize is None else int(iFontSize)
 		iFontSize = ctypes.c_int(iFontSize)
 		iFontSize = ctypes.windll.kernel32.MulDiv(iFontSize, deviceCaps, 72)
 		#Font  Weight for Bold FOnt is 700 and for normal font it's 400
--- source/controlTypes.py
+++ source/controlTypes.py
@@ -549,9 +549,9 @@ stateLabels={
 	# Translators: a state that denotes the existance of a comment.
 	STATE_HASCOMMENT:_("has comment"),
 	# Translators: a state that denotes that the object is covered partially or fully by another object
-	STATE_OBSCURED:_("obscured"),
+	STATE_OBSCURED:_("cropped in"),
 	# Translators: a state that denotes that the object is overlapping onto adjacent object(s)
-	STATE_OBSCURING:_("obscuring"),
+	STATE_OBSCURING:_("overflowing"),
 }
 
 negativeStateLabels={
