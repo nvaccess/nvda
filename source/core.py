fcd157e2af332332ffc6991448c275284425f8ba
Leonard de Ruijter <alderuijter@gmail.com>
2015-04-23 14:09:01 +1000

    Improve Windows version reporting for logging and update checks.
    1. The service pack string is now always English for consistency.
    2. The version logged by NVDA is now a nice, easy to read string.
    3. The version also includes whether this is a workstation or server version of Windows.
    Fixes #3724.

--- source/core.py
+++ source/core.py
@@ -18,6 +18,7 @@ comtypes.gen.__path__.append(comInterfaces.__path__[0])
 import comtypesMonkeyPatches
 
 import sys
+import winVersion
 import thread
 import nvwave
 import os
@@ -172,7 +173,7 @@ This initializes all modules such as audio, IAccessible, keyboard, mouse, and GU
 		log.warning("Could not set language to %s"%lang)
 	import versionInfo
 	log.info("NVDA version %s" % versionInfo.version)
-	log.info("Using Windows version %r" % (sys.getwindowsversion(),))
+	log.info("Using Windows version %s" % winVersion.winVersionText)
 	log.info("Using Python version %s"%sys.version)
 	log.info("Using comtypes version %s"%comtypes.__version__)
 	# Set a reasonable timeout for any socket connections NVDA makes.
