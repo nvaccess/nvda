e5b107e744ca12e12b2d62cc1c06bf8dab424a33
James Teh <jamie@nvaccess.org>
2015-05-07 16:25:59 +1000

    Update copyright header.

--- source/NVDAObjects/window/excelChart.py
+++ source/NVDAObjects/window/excelChart.py
@@ -1,7 +1,7 @@
 # -*- coding: UTF-8 -*- 
 #NVDAObjects/excel.py
 #A part of NonVisual Desktop Access (NVDA)
-#Copyright (C) 2006-2007 NVDA Contributors <http://www.nvda-project.org/>
+#Copyright (C) 2014-2015 Dinesh Kaushal, NV Access Limited
 #This file is covered by the GNU General Public License.
 #See the file COPYING for more details.
 
