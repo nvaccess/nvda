a22fd46c4f5c0ef01174a1e99759e111553240c5
Davy Kager <mail@davykager.nl>
2015-11-21 19:51:49 +0100

    When toggling between browse mode and focus mode, also show the new mode name in braille instead of only speaking it. Fixes #5239.

--- source/browseMode.py
+++ source/browseMode.py
@@ -47,9 +47,9 @@ def reportPassThrough(treeInterceptor,onlyIfChanged=True):
 			nvwave.playWaveFile(sound)
 		else:
 			if treeInterceptor.passThrough:
-				speech.speakMessage(_("focus mode"))
+				ui.message(_("focus mode"))
 			else:
-				speech.speakMessage(_("browse mode"))
+				ui.message(_("browse mode"))
 		reportPassThrough.last = treeInterceptor.passThrough
 reportPassThrough.last = False
 
