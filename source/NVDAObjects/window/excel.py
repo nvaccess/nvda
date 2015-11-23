a67106571bcb6f798fb1122d017895ae0d59adf4
James Teh <jamie@nvaccess.org>
2015-11-23 12:04:28 +1000

    Microsoft Excel: In the description of the Elements List command, don't list the specific types of elements.
    This will only get longer as time goes on and it's something we forget to udpate.
    Fixes #5529.

--- source/NVDAObjects/window/excel.py
+++ source/NVDAObjects/window/excel.py
@@ -420,7 +420,7 @@ class ExcelBrowseModeTreeInterceptor(browseMode.BrowseModeTreeInterceptor):
 	def script_elementsList(self,gesture):
 		super(ExcelBrowseModeTreeInterceptor,self).script_elementsList(gesture)
 	# Translators: the description for the elements list command in Microsoft Excel.
-	script_elementsList.__doc__ = _("Presents a list of charts, cells with comments and cells with formulas")
+	script_elementsList.__doc__ = _("Lists various types of elements in this spreadsheet")
 	script_elementsList.ignoreTreeInterceptorPassThrough=True
 
 	__gestures = {
