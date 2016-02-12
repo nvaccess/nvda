702ea63e56a1b96dc205772816b7bb68e6dca90c
Renaud Paquay <rpaquay@google.com>
2016-02-12 10:59:17 -0800

    Remove UTF8 BOM.

--- source/JABHandler.py
+++ source/JABHandler.py
@@ -1,4 +1,4 @@
-﻿#javaAccessBridgeHandler.py
+#javaAccessBridgeHandler.py
 #A part of NonVisual Desktop Access (NVDA)
 #Copyright (C) 2006-2007 NVDA Contributors <http://www.nvda-project.org/>
 #This file is covered by the GNU General Public License.
