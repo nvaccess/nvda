7fd029a5cb176cbf15ddd9effd26de2cfc97683f
James Teh <jamie@nvaccess.org>
2016-06-02 14:49:52 +1000

    Bump version for 2016.2 final.

--- source/versionInfo.py
+++ source/versionInfo.py
@@ -31,7 +31,7 @@ def _updateVersionFromVCS():
 # Otherwise, py2exe will break.
 name="NVDA"
 longName=_("NonVisual Desktop Access")
-version="2016.2dev"
+version="2016.2"
 publisher="unknown"
 updateVersionType=None
 try:
