0eb9015d31b23cd7f6c1e1f871f2d9e7b6010926
Reef Turner <reef@nvaccess.org>
2016-06-07 17:23:27 +0800

    Fix issue #5490 - lock keys double announce
    Issue: Lock keys double announce when Speak command keys is on
    https://github.com/nvaccess/nvda/issues/5490
    
    Added a special case to _get_shouldReportAsCommand if the key
    is in the TOGGLE_KEYS list.

--- source/keyboardHandler.py
+++ source/keyboardHandler.py
@@ -387,6 +387,11 @@ class KeyboardInputGesture(inputCore.InputGesture):
 			# This could be for an event such as gyroscope movement,
 			# so don't report it.
 			return False
+		if self.vkCode in self.TOGGLE_KEYS:
+			# #5490: Dont report for keys that toggle on off.
+			# This is to avoid them from reported twice: once by the 'speak command keys' feature,
+			# and once by the 'speak typed characters' feature
+			return False
 		return not self.isCharacter
 
 	def _get_isCharacter(self):
