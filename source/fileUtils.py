7940568969c0734d028469d224f7369946304e95
Bram Duvigneau <bram@bramd.nl>
2016-06-16 15:19:45 +0200

    fileUtils: add copyright/license header

--- source/fileUtils.py
+++ source/fileUtils.py
@@ -1,3 +1,8 @@
+#fileUtils.py
+#A part of NonVisual Desktop Access (NVDA)
+#Copyright (C) 2006-2016 NV Access Limited
+#This file is covered by the GNU General Public License.
+#See the file COPYING for more details.
 import os
 import osreplace
 from contextlib import contextmanager
