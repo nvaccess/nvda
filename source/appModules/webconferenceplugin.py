59dd829de29e9fe8043922eeb155e61700a7216b
derekriemer <derek.riemer@colorado.edu>
2016-06-30 12:25:15 -0600

    Fixes to code review: Remove unneccessary imports . Fix Copyright information. cc @MichaelDCurran

--- source/appModules/webconferenceplugin.py
+++ source/appModules/webconferenceplugin.py
@@ -1,7 +1,12 @@
+#webconferenceplugin.py
+#A part of NonVisual Desktop Access (NVDA)
+#Copyright (C) 2016 NV Access Limited, Derek Riemer
+#This file is covered by the GNU General Public License.
+#See the file COPYING for more details.
+
+""" AppModule to remove redundant progressbar announcement from the web conference plugin. """
 import appModuleHandler
 from NVDAObjects.behaviors import ProgressBar
-import controlTypes
-import ui
 
 class AppModule(appModuleHandler.AppModule):
 	def chooseNVDAObjectOverlayClasses(self, obj, clsList):
