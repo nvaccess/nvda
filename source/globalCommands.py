954021a8e6762594318b5a7799b3b6497a075737
derekriemer <derek.riemer@colorado.edu>
2016-07-06 21:09:04 -0600

    Fix copyright header.

--- source/globalCommands.py
+++ source/globalCommands.py
@@ -3,7 +3,7 @@
 #A part of NonVisual Desktop Access (NVDA)
 #This file is covered by the GNU General Public License.
 #See the file COPYING for more details.
-#Copyright (C) 2006-2015 NV Access Limited, Peter Vágner, Aleksey Sadovoy, Rui Batista, Joseph Lee, Leonard de Ruijter
+#Copyright (C) 2006-2016 NV Access Limited, Peter Vágner, Aleksey Sadovoy, Rui Batista, Joseph Lee, Leonard de Ruijter, Derek Riemer
 
 import time
 import itertools
