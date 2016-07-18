8f4b4f0bfd55bfe522270d7abe8c838463eaa8e0
Leonard de Ruijter <leonardder@users.noreply.github.com>
2016-07-18 04:41:35 +0200

    Fix scons.bat failure when the NVDA source tree resides in a path containing one or more spaces (#6169)

--- scons.bat
+++ scons.bat
@@ -2,4 +2,4 @@
 rem We need this script because .py probably isn't in pathext.
 rem We can't just call python -c because it may not be in the path.
 rem Python registers itself with the .py extension, so call scons.py.
-%~dp0\scons.py %*
+"%~dp0\scons.py" %*
