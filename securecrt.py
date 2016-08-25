8b82cce8d595e124881f44e9e475b48000b55e44
nromey <nromey@gmail.com>
2016-08-24 23:10:04 -0400

    Add files via upload

---
+++ securecrt.py
@@ -0,0 +1,23 @@
+#appModules/securecrt.py
+#A part of NonVisual Desktop Access (NVDA)
+#This file is covered by the GNU General Public License.
+#See the file COPYING for more details.
+#Copyright (C) 2010-2012 NV Access Limited
+
+"""App module for SecureCRT
+"""
+
+import oleacc
+from NVDAObjects.behaviors import Terminal
+from NVDAObjects.window import DisplayModelEditableText, DisplayModelLiveText
+import appModuleHandler
+
+class AppModule(appModuleHandler.AppModule):
+
+	def chooseNVDAObjectOverlayClasses(self, obj, clsList):
+		if obj.windowClassName in ("AfxFrameOrView80u", "AfxFrameOrView90u", "AfxFrameOrView100u",	"AfxFrameOrView110u",	"AfxFrameOrView120u",	"AfxFrameOrView130u",	"AfxFrameOrView140u") and obj.IAccessibleRole == oleacc.ROLE_SYSTEM_CLIENT:
+			try:
+				clsList.remove(DisplayModelEditableText)
+			except ValueError:
+				pass
+			clsList[0:0] = (Terminal, DisplayModelLiveText)
