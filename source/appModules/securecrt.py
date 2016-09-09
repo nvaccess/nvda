906642fb5abd4b4ecbeee0dd1619d03fd4f25df1
nromey <nromey@gmail.com>
2016-09-09 17:18:39 -0400

    added compilaton of regexp.
    I didn't think that this logic ran as much as it did. Works perfectly now.

--- source/appModules/securecrt.py
+++ source/appModules/securecrt.py
@@ -2,11 +2,10 @@
 #A part of NonVisual Desktop Access (NVDA)
 #This file is covered by the GNU General Public License.
 #See the file COPYING for more details.
-#Copyright (C) 2010-2012 NV Access Limited
+#Copyright (C) 2010-2016 NV Access Limited
 
 """App module for automatic reading of the terminal in all versions of SecureCRT
-"""
-
+Uses regular expressions to support the ever-changing window title for different incremental SecureCRT Versions."""
 import oleacc
 import re
 from NVDAObjects.behaviors import Terminal
@@ -16,7 +15,10 @@ import appModuleHandler
 class AppModule(appModuleHandler.AppModule):
 
 	def chooseNVDAObjectOverlayClasses(self, obj, clsList):
-		if re.match("AfxFrameOrView\d{2,}u", obj.windowClassName) and obj.IAccessibleRole == oleacc.ROLE_SYSTEM_CLIENT:
+		#initialize a compiles regexp object for all window names following the standard SecureCrt Window naming comvention for all versions
+		MATCHER = re.compile ("AfxFrameOrView\d{2,}u")
+			#perform the match with the compiled object
+		if MATCHER.match (obj.windowClassName) and obj.IAccessibleRole == oleacc.ROLE_SYSTEM_CLIENT: 
 			try:
 				clsList.remove(DisplayModelEditableText)
 			except ValueError:
