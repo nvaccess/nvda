b575cff0a7282f6b457820f7cc1c4a12f0b60857
nromey <nromey@gmail.com>
2016-09-20 00:41:27 -0400

    made changes requested in pull comments

--- source/appModules/securecrt.py
+++ source/appModules/securecrt.py
@@ -1,26 +1,27 @@
 #appModules/securecrt.py
+# Modified to include a regular expression so that all Secure CRT versions are supported past, present, and future.
+# Modification of original securecrt.py by Noel Romey 9/2016
 #A part of NonVisual Desktop Access (NVDA)
 #This file is covered by the GNU General Public License.
 #See the file COPYING for more details.
 #Copyright (C) 2010-2016 NV Access Limited
 
-"""App module for automatic reading of the terminal in all versions of SecureCRT
-Uses regular expressions to support the ever-changing window title for different incremental SecureCRT Versions."""
+"""App module for automatic reading of the terminal in all versions of SecureCRT """
+
 import oleacc
 import re
 from NVDAObjects.behaviors import Terminal
 from NVDAObjects.window import DisplayModelEditableText, DisplayModelLiveText
 import appModuleHandler
-
+#initialize a compiles regexp object for all window names following the standard SecureCrt Window naming comvention for all versions
+MATCHER = re.compile ("AfxFrameOrView\d{2,}u")
 class AppModule(appModuleHandler.AppModule):
 
-	def chooseNVDAObjectOverlayClasses(self, obj, clsList):
-		#initialize a compiles regexp object for all window names following the standard SecureCrt Window naming comvention for all versions
-		MATCHER = re.compile ("AfxFrameOrView\d{2,}u")
+		def chooseNVDAObjectOverlayClasses(self, obj, clsList):
 			#perform the match with the compiled object
-		if MATCHER.match (obj.windowClassName) and obj.IAccessibleRole == oleacc.ROLE_SYSTEM_CLIENT: 
-			try:
-				clsList.remove(DisplayModelEditableText)
-			except ValueError:
-				pass
-			clsList[0:0] = (Terminal, DisplayModelLiveText)
+			if MATCHER.match (obj.windowClassName) and obj.IAccessibleRole == oleacc.ROLE_SYSTEM_CLIENT: 
+				try:
+					clsList.remove(DisplayModelEditableText)
+				except ValueError:
+					pass
+				clsList[0:0] = (Terminal, DisplayModelLiveText)
