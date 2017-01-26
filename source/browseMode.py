bb0440e7d5aac8a9917f841bfd6e645e26875f85
Davy Kager <mail@davykager.nl>
2017-01-26 13:44:51 +0100

    Change the order of the landmark name and the word landmark as displayed in braille. Third part of #3975. Rationale: the word landmark is the more significant part. That is, the word landmark indicates that we are dealing with a landmark while the landmark name describes the specific type.

--- source/browseMode.py
+++ source/browseMode.py
@@ -975,8 +975,8 @@ class BrowseModeDocumentTextInfo(textInfos.TextInfo):
 				# The word landmark is superfluous for regions.
 				textList.append(braille.landmarkLabels[landmark])
 			else:
-				# Translators: This is brailled to indicate a landmark (example output: main lmk).
-				textList.append(_("%s lmk") % braille.landmarkLabels[landmark])
+				# Translators: This is brailled to indicate a landmark (example output: lmk main).
+				textList.append(_("lmk %s") % braille.landmarkLabels[landmark])
 		text = super(BrowseModeDocumentTextInfo, self).getControlFieldBraille(field, ancestors, reportStart, formatConfig)
 		if text:
 			textList.append(text)
