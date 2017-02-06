f534b77bc7cbf926f188c8bf314d00755e312b29
James Teh <jamie@nvaccess.org>
2017-02-07 07:12:36 +1000

    Update to pySerial 2.7.
    This is needed to fix a UnicodeDecodeError in some languages when opening a serial port fails. This was preventing braille display drivers from trying further ports which might have worked.

--- source/sourceEnv.py
+++ source/sourceEnv.py
@@ -1,6 +1,6 @@
 #sourceEnv.py
 #A part of NonVisual Desktop Access (NVDA)
-#Copyright (C) 2013 NV Access Limited
+#Copyright (C) 2013-2017 NV Access Limited
 #This file is covered by the GNU General Public License.
 #See the file COPYING for more details.
 
@@ -10,5 +10,11 @@
 import sys
 import os
 
-sys.path.insert(1, os.path.abspath(
-	os.path.join(__file__, "..", "..", "miscDeps", "python")))
+# Get the path to the top of the repo; i.e. where include and miscDeps are.
+top = os.path.dirname(os.path.dirname(__file__))
+# sys.path[0] will always be the current dir, which should take precedence.
+# Insert our include paths after that.
+sys.path[1:1] = (
+	os.path.join(top, "include", "pyserial"),
+	os.path.join(top, "miscDeps", "python"),
+)
