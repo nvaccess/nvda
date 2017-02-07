fcdda0e5bee6cf6417d50b90b28b99453d2320a8
James Teh <jamie@nvaccess.org>
2017-02-07 17:12:39 +1000

    Make sure binary builds get the new pySerial too.

--- source/sourceEnv.py
+++ source/sourceEnv.py
@@ -11,10 +11,13 @@ import sys
 import os
 
 # Get the path to the top of the repo; i.e. where include and miscDeps are.
-top = os.path.dirname(os.path.dirname(__file__))
+TOP_DIR = os.path.dirname(os.path.dirname(__file__))
+# Directories containing Python modules included in git submodules.
+PYTHON_DIRS = (
+	os.path.join(TOP_DIR, "include", "pyserial"),
+	os.path.join(TOP_DIR, "miscDeps", "python"),
+)
+
 # sys.path[0] will always be the current dir, which should take precedence.
 # Insert our include paths after that.
-sys.path[1:1] = (
-	os.path.join(top, "include", "pyserial"),
-	os.path.join(top, "miscDeps", "python"),
-)
+sys.path[1:1] = PYTHON_DIRS
