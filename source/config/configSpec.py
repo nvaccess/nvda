0e2c85db5cdd8d9bf05f5d5da6ef1e4563f0fefc
Bill Dengler <codeofdusk@gmail.com>
2017-03-09 19:51:52 +0000

    Fix #6952

--- source/config/configSpec.py
+++ source/config/configSpec.py
@@ -50,7 +50,7 @@ schemaVersion = integer(min=0, default={latestSchemaVersion})
 # Braille settings
 [braille]
 	display = string(default=noBraille)
-	translationTable = string(default=en-us-comp8.ctb)
+	translationTable = string(default=en-ueb-g2.ctb)
 	inputTable = string(default=en-us-comp8.ctb)
 	expandAtCursor = boolean(default=true)
 	showCursor = boolean(default=true)
