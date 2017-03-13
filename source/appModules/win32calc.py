e5aa3180824502e20c2564325192407c4d88bf59
Joseph Lee <joseph.lee22590@gmail.com>
2017-03-13 05:24:13 -0700

    Updated copyright years.
    Reviewed by Reef Turner (NV Access): update copyright to say 2007-2017.

--- source/appModules/win32calc.py
+++ source/appModules/win32calc.py
@@ -1,6 +1,6 @@
 #appModules/win32calc.py
 #A part of NonVisual Desktop Access (NVDA)
-#Copyright (C) 2007-2012 NV Access Limited
+#Copyright (C) 2007-2017 NV Access Limited
 #This file is covered by the GNU General Public License.
 #See the file COPYING for more details.
 
