700db42565e9c7cc1260a99baf54a76a708fb76a
Reef Turner <reef@nvaccess.org>
2017-03-17 14:15:23 +0800

    Review actions for #6987

--- source/fileUtils.py
+++ source/fileUtils.py
@@ -1,6 +1,6 @@
 #fileUtils.py
 #A part of NonVisual Desktop Access (NVDA)
-#Copyright (C) 2006-2016 NV Access Limited
+#Copyright (C) 2017 NV Access Limited, Bram Duvigneau
 #This file is covered by the GNU General Public License.
 #See the file COPYING for more details.
 import os
@@ -9,8 +9,23 @@ from contextlib import contextmanager
 from tempfile import NamedTemporaryFile
 from logHandler import log
 
+#: Constant; flag for MoveFileEx(). If a file with the destination filename already exists, it is overwritten.
+MOVEFILE_REPLACE_EXISTING = 1
+
 @contextmanager
 def FaultTolerantFile(name):
+	'''Used to write out files in a more fault tolerant way. A temporary file is used, and replaces the 
+	file `name' when the context manager scope ends and the the context manager __exit__ is called. This
+	means writing out the complete file can be performed with less concern of corrupting the original file
+	if the process is interrupted by windows shutting down.
+
+	Usage:
+		with FaultTolerantFile("myFile.txt") as f:
+			f.write("This is a test")
+
+	This creates a temporary file, and the writes actually happen on this temp file. At the end of the 
+	`with` block, when `f` goes out of context the temporary file is closed and, this temporary file replaces "myFile.txt"
+	'''
 	dirpath, filename = os.path.split(name)
 	with NamedTemporaryFile(dir=dirpath, prefix=filename, suffix='.tmp', delete=False) as f:
 		log.debug(f.name)
@@ -18,7 +33,6 @@ def FaultTolerantFile(name):
 		f.flush()
 		os.fsync(f)
 		f.close()
-		MOVEFILE_REPLACE_EXISTING = 1
 		moveFileResult = ctypes.windll.kernel32.MoveFileExW(f.name, name, MOVEFILE_REPLACE_EXISTING)
 		if moveFileResult == 0:
 			raise ctypes.WinError()
