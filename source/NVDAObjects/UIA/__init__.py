ba21c102e06882082d2899d6766e7755dc42da32
Joseph Lee <joseph.lee22590@gmail.com>
2017-05-11 10:00:41 -0700

    NVDAObjects.UIA: catch UnboundLocalError for parentElement if parentElement fetcher fails. re #6241.
    In some cases, when Start menu opens, it isn't announced by NVDA. As a result, parent element fetcher fails when trying to instantiate suggestions list item, with a traceback that ends with UnboundLocalError. Catch this by moving the SuggestionsListItem selector to inside of the try block.

--- source/NVDAObjects/UIA/__init__.py
+++ source/NVDAObjects/UIA/__init__.py
@@ -744,15 +744,15 @@ class UIA(Window):
 		if self.UIAElement.cachedAutomationID in ("SearchTextBox", "TextBox", "addressEditBox"):
 			clsList.append(SearchField)
 		try:
-			# Nested block here in order to catch value error when attempting to access automation ID for invalid elements.
+			# Nested block here in order to catch value error and variable binding error when attempting to access automation ID for invalid elements.
 			try:
 				# #6241: Raw UIA base tree walker is better than simply looking at self.parent when locating suggestion list items.
 				parentElement=UIAHandler.handler.baseTreeWalker.GetParentElementBuildCache(self.UIAElement,UIAHandler.handler.baseCacheRequest)
+				# Sometimes, fetching parent (list control) via base tree walker fails, especially when dealing with suggestions in Windows10 Start menu.
+				if parentElement.cachedAutomationId.lower()=="suggestionslist":
+					clsList.append(SuggestionListItem)
 			except COMError:
 				pass
-			# Sometimes, fetching parent (list control) via base tree walker fails, especially when dealing with suggestions in Windows10 Start menu.
-			if parentElement.cachedAutomationId.lower()=="suggestionslist":
-				clsList.append(SuggestionListItem)
 		except ValueError:
 			pass
 
