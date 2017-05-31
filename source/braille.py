0101cc4027ea124d6813679060c4e3ba49c6b28b
Michael Curran <michaelDCurran@users.noreply.github.com>
2017-05-31 12:21:43 +1000

    In Microsoft Powerpoint, the exact type of a shape is now reported when editing a slide (Examples include: triangle, circle, video, arrow), rather than just 'shape'. (#7111)
    * Add audio and video roles to NVDA.
    
    * Add a 'roleText' property to NVDAObjects. If its value is a string,  this string is used in place of the label looked up from the 'role' property, in both speech and braille, allowing an NVDAObject to override the presented role.
    
    * Powerpoint: ensure that all shape types are presented to the user in speech and braille.
    This Includes mapping  media shapes to audio and video roles, and for all other shapes, exposing localizable label as role text, keeping the actual role still as shape.
    For action button shapes, the action is now exposed via the name property.
    We also no longer expose the programmatic name of the shape, as this was usually wrong and was only done in the past as a hope of giving some guess as to the shape type.
    
    * Remove accidental symbols in comments.

--- source/braille.py
+++ source/braille.py
@@ -587,6 +587,7 @@ def getBrailleTextForProperties(**propertyValues):
 	if name:
 		textList.append(name)
 	role = propertyValues.get("role")
+	roleText = propertyValues.get("roleText")
 	states = propertyValues.get("states")
 	positionInfo = propertyValues.get("positionInfo")
 	level = positionInfo.get("level") if positionInfo else None
@@ -594,7 +595,7 @@ def getBrailleTextForProperties(**propertyValues):
 	rowNumber = propertyValues.get("rowNumber")
 	columnNumber = propertyValues.get("columnNumber")
 	includeTableCellCoords = propertyValues.get("includeTableCellCoords", True)
-	if role is not None:
+	if role is not None and not roleText:
 		if role == controlTypes.ROLE_HEADING and level:
 			# Translators: Displayed in braille for a heading with a level.
 			# %s is replaced with the level.
@@ -609,9 +610,8 @@ def getBrailleTextForProperties(**propertyValues):
 			roleText = None
 		else:
 			roleText = roleLabels.get(role, controlTypes.roleLabels[role])
-	else:
+	elif role is None: 
 		role = propertyValues.get("_role")
-		roleText = None
 	value = propertyValues.get("value")
 	if value and role not in controlTypes.silentValuesForRoles:
 		textList.append(value)
@@ -685,7 +685,7 @@ class NVDAObjectRegion(Region):
 		obj = self.obj
 		presConfig = config.conf["presentation"]
 		role = obj.role
-		text = getBrailleTextForProperties(name=obj.name, role=role, current=obj.isCurrent,
+		text = getBrailleTextForProperties(name=obj.name, role=role, roleText=obj.roleText, current=obj.isCurrent,
 			value=obj.value if not NVDAObjectHasUsefulText(obj) else None ,
 			states=obj.states,
 			description=obj.description if presConfig["reportObjectDescriptions"] else None,
