nvaccess/nvda

commit c967b21aa36de96bd38fd451f47b6c665c8077d4

Avoid full caret move timeout when pressing the delete key by treating valueChange events as caret moves. Also, treat caret events as caret moves as well, as this should be more reliable than comparing bookmarks in some cases.

source/editableText.py
@@ -71,17 +71,37 @@ class EditableText(ScriptableObject):
 				return (False,None)
 			api.processPendingEvents(processEventQueue=False)
 			if eventHandler.isPendingEvents("gainFocus"):
+				log.debug("Focus event. Elapsed: %d ms" % elapsed)
 				return (True,None)
-			#The caret may stop working as the focus jumps, we want to stay in the while loop though
+			# If the focus changes after this point, fetching the caret may fail,
+			# but we still want to stay in this loop.
 			try:
 				newInfo = self.makeTextInfo(textInfos.POSITION_CARET)
-				newBookmark = newInfo.bookmark
 			except (RuntimeError,NotImplementedError):
-				newInfo=None
-			else:
-				if newBookmark!=bookmark:
-					log.debug("Caret moved. Elapsed: %d ms" % elapsed)
-					return (True,newInfo)
+				newInfo = None
+			if eventHandler.isPendingEvents("caret"):
+				log.debug("caret event. Elapsed: %d ms" % elapsed)
+				return (True, newInfo)
+			if eventHandler.isPendingEvents("valueChange"):
+				# When pressing delete, the caret might not physically move,
+				# so the control might not fire a caret event,
+				# even though the text under the caret changes.
+				# We still want to treat this as a caret move.
+				# Otherwise, the user will have to wait
+				# the full timeout for feedback after pressing delete.
+				log.debug("valueChange event. Elapsed: %d ms" % elapsed)
+				return (True, newInfo)
+			# Some controls don't fire caret events.
+			# Try to detect with bookmarks.
+			newBookmark = None
+			if newInfo:
+				try:
+					newBookmark = newInfo.bookmark
+				except (RuntimeError,NotImplementedError):
+					pass
+			if newBookmark and newBookmark!=bookmark:
+				log.debug("Caret move detected using bookmarks. Elapsed: %d ms" % elapsed)
+				return (True, newInfo)
 			if  elapsed >= timeoutMs:
 				break
 			time.sleep(retryInterval)

commit 4a908f3c405dd925f03e525d0ec1f69be33cc2f9

Fix regression of Automatic focus mode for caret movement (#7323) caused by c967b21a.
Firefox fires caret events even when you use caret movement commands when already at the edge of the control. That means we don't fire caretMovementFailed events in these cases. Furthermore, it seems quite a few things (e.g. Wordpad, Notepad++) don't fire valueChange or caret events when you press delete.
To work around this, drop the checks for caret and valueChange events (and revert the shouldAllowSystemCaretEvent stuff). Instead, when pressing delete, compare the word at the caret (in addition to bookmarks) to detect movement.

source/IAccessibleHandler.py
@@ -612,12 +612,8 @@ def processGenericWinEvent(eventID,window,objectID,childID):
 	appModuleHandler.update(winUser.getWindowThreadProcessID(window)[0])
 	#Handle particular events for the special MSAA caret object just as if they were for the focus object
 	focus=eventHandler.lastQueuedFocusObject
-	if objectID==winUser.OBJID_CARET and eventID in (winUser.EVENT_OBJECT_LOCATIONCHANGE,winUser.EVENT_OBJECT_SHOW):
-		if isinstance(focus,NVDAObjects.window.Window) and focus.shouldAllowSystemCaretEvent:
-			NVDAEvent=("caret",focus)
-		else:
-			# No focus or the focus doesn't want these caret events.
-			return
+	if focus and objectID==winUser.OBJID_CARET and eventID in (winUser.EVENT_OBJECT_LOCATIONCHANGE,winUser.EVENT_OBJECT_SHOW):
+		NVDAEvent=("caret",focus)
 	else:
 		NVDAEvent=winEventToNVDAEvent(eventID,window,objectID,childID)
 		if not NVDAEvent:

source/NVDAObjects/IAccessible/__init__.py
@@ -643,12 +643,6 @@ the NVDAObject for IAccessible
 			return IA2TextTextInfo
 		return super(IAccessible,self).TextInfo
 
-	def _get_shouldAllowSystemCaretEvent(self):
-		if hasattr(self, 'IAccessibleTextObject'):
-			# IAccessibleText implementations fire IA2 caret events which are more reliable.
-			return False
-		return super(IAccessible, self).shouldAllowSystemCaretEvent
-
 	def _isEqual(self,other):
 		if self.IAccessibleChildID!=other.IAccessibleChildID:
 			return False

source/NVDAObjects/window/__init__.py
@@ -371,10 +371,6 @@ An NVDAObject for a window
 		info.append("displayText: %s" % ret)
 		return info
 
-	#: Whether to allow caret events generated by the system caret;
-	#: i.e. as triggered by the Win32 functions ShowCaret and SetCaretPos.
-	shouldAllowSystemCaretEvent = True
-
 class Desktop(Window):
 
 	isPresentableFocusAncestor = False

source/editableText.py
@@ -43,7 +43,7 @@ class EditableText(ScriptableObject):
 	#: Whether or not to announce text found before the caret on a new line (e.g. auto numbering)
 	announceNewLineText=True
 
-	def _hasCaretMoved(self, bookmark, retryInterval=0.01, timeout=None):
+	def _hasCaretMoved(self, bookmark, retryInterval=0.01, timeout=None, origWord=None):
 		"""
 		Waits for the caret to move, for a timeout to elapse, or for a new focus event or script to be queued.
 		@param bookmark: a bookmark representing the position of the caret before  it was instructed to move
@@ -53,6 +53,9 @@ class EditableText(ScriptableObject):
 		@param timeout: the over all amount of time in seconds the method should wait before giving up completely,
 			C{None} to use the value from the configuration.
 		@type timeout: float
+		@param origWord: The word at the caret before the movement command,
+			C{None} if the word at the caret should not be used to detect movement.
+			This is intended for use with the delete key.
 		@return: a tuple containing a boolean denoting whether this method timed out, and  a TextInfo representing the old or updated caret position or None if interupted by a script or focus event.
 		@rtype: tuple
 		"""
@@ -79,19 +82,7 @@ class EditableText(ScriptableObject):
 				newInfo = self.makeTextInfo(textInfos.POSITION_CARET)
 			except (RuntimeError,NotImplementedError):
 				newInfo = None
-			if eventHandler.isPendingEvents("caret"):
-				log.debug("caret event. Elapsed: %d ms" % elapsed)
-				return (True, newInfo)
-			if eventHandler.isPendingEvents("valueChange"):
-				# When pressing delete, the caret might not physically move,
-				# so the control might not fire a caret event,
-				# even though the text under the caret changes.
-				# We still want to treat this as a caret move.
-				# Otherwise, the user will have to wait
-				# the full timeout for feedback after pressing delete.
-				log.debug("valueChange event. Elapsed: %d ms" % elapsed)
-				return (True, newInfo)
-			# Some controls don't fire caret events.
+			# Caret events are unreliable in some controls.
 			# Try to detect with bookmarks.
 			newBookmark = None
 			if newInfo:
@@ -102,7 +93,15 @@ class EditableText(ScriptableObject):
 			if newBookmark and newBookmark!=bookmark:
 				log.debug("Caret move detected using bookmarks. Elapsed: %d ms" % elapsed)
 				return (True, newInfo)
-			if  elapsed >= timeoutMs:
+			if origWord is not None and newInfo:
+				# When pressing delete, bookmarks might not be enough to detect caret movement.
+				wordInfo = newInfo.copy()
+				wordInfo.expand(textInfos.UNIT_WORD)
+				word = wordInfo.text
+				if word != origWord:
+					log.debug("Word at caret changed. Elapsed: %d ms" % elapsed)
+					return (True, newInfo)
+			if elapsed >= timeoutMs:
 				break
 			time.sleep(retryInterval)
 			elapsed += retryMs
@@ -232,9 +231,11 @@ class EditableText(ScriptableObject):
 			gesture.send()
 			return
 		bookmark=info.bookmark
+		info.expand(textInfos.UNIT_WORD)
+		word=info.text
 		gesture.send()
 		# We'll try waiting for the caret to move, but we don't care if it doesn't.
-		caretMoved,newInfo=self._hasCaretMoved(bookmark)
+		caretMoved,newInfo=self._hasCaretMoved(bookmark,origWord=word)
 		self._caretScriptPostMovedHelper(textInfos.UNIT_CHARACTER,gesture,newInfo)
 		braille.handler.handleCaretMove(self)
 