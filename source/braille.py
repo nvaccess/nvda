20f3c21c188b100c97fa71c7076d6d388ff37256
Davy Kager <mail@davykager.nl>
2017-07-19 13:15:38 +0200

    Review actions.

--- source/braille.py
+++ source/braille.py
@@ -792,7 +792,19 @@ def getControlFieldBraille(info, field, ancestors, reportStart, formatConfig):
 		return (_("%s end") %
 			getBrailleTextForProperties(role=role))
 
-def getFormatFieldBraille(field, isAtStart, formatConfig, fieldCache=None):
+def getFormatFieldBraille(field, fieldCache, isAtStart, formatConfig):
+	"""Generates the braille text for the given format field.
+	@param field: The format field to examine.
+	@type field: {str : str, ...}
+	@param fieldCache: The format field of the previous run; i.e. the cached format field.
+	@type fieldCache: {str : str, ...}
+	@param isAtStart: True if this format field precedes any text in the line/paragraph.
+	This is useful to restrict display of information which should only appear at the start of the line/paragraph;
+	e.g. the line number or line prefix (list bullet/number).
+	@type isAtStart: bool
+	@param formatConfig: The formatting config.
+	@type formatConfig: {str : bool, ...}
+	"""
 	textList = []
 	if isAtStart:
 		if formatConfig["reportLineNumber"]:
@@ -810,12 +822,11 @@ def getFormatFieldBraille(field, isAtStart, formatConfig, fieldCache=None):
 				textList.append(_("h%s")%headingLevel)
 	if formatConfig["reportLinks"]:
 		link=field.get("link")
-		oldLink=fieldCache.get("link") if fieldCache is not None else None
+		oldLink=fieldCache.get("link")
 		if link and link != oldLink:
 			textList.append(roleLabels[controlTypes.ROLE_LINK])
-	if fieldCache is not None:
-		fieldCache.clear()
-		fieldCache.update(field)
+	fieldCache.clear()
+	fieldCache.update(field)
 	return TEXT_SEPARATOR.join([x for x in textList if x])
 
 class TextInfoRegion(Region):
@@ -886,15 +897,11 @@ class TextInfoRegion(Region):
 		self.rawTextTypeforms.extend((louis.plain_text,) * textLen)
 		self._rawToContentPos.extend((contentPos,) * textLen)
 
-	def _addTextWithFields(self, info, formatConfig, isSelection=False, useCache=True):
+	def _addTextWithFields(self, info, formatConfig, isSelection=False):
 		shouldMoveCursorToFirstContent = not isSelection and self.cursorPos is not None
 		ctrlFields = []
 		typeform = louis.plain_text
-		if useCache:
-			formatFieldAttributesCache = getattr(info.obj, "_brailleFormatFieldAttributesCache", {})
-		else:
-			formatFieldAttributesCache = None
-
+		formatFieldAttributesCache = getattr(info.obj, "_brailleFormatFieldAttributesCache", {})
 		for command in info.getTextWithFields(formatConfig=formatConfig):
 			if isinstance(command, basestring):
 				self._isFormatFieldAtStart = False
@@ -929,7 +936,7 @@ class TextInfoRegion(Region):
 				field = command.field
 				if cmd == "formatChange":
 					typeform = self._getTypeformFromFormatField(field)
-					text = getFormatFieldBraille(field, self._isFormatFieldAtStart, formatConfig, formatFieldAttributesCache)
+					text = getFormatFieldBraille(field, formatFieldAttributesCache, self._isFormatFieldAtStart, formatConfig)
 					if not text:
 						continue
 					# Map this field text to the start of the field's content.
@@ -972,8 +979,7 @@ class TextInfoRegion(Region):
 			# We only render fields that aren't at the start of their nodes for the first part of the reading unit.
 			# Otherwise, we'll render fields that have already been rendered.
 			self._skipFieldsNotAtStartOfNode = True
-		if useCache:
-			info.obj._brailleFormatFieldAttributesCache = formatFieldAttributesCache
+		info.obj._brailleFormatFieldAttributesCache = formatFieldAttributesCache
 
 	def _getReadingUnit(self):
 		return textInfos.UNIT_PARAGRAPH if config.conf["braille"]["readByParagraph"] else textInfos.UNIT_LINE
