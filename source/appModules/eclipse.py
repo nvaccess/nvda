d01046cd6d3debd66c5ea3fbac67e481b1cb2f21
Francisco R. Del Roio <francipvb@hotmail.com>
2017-09-22 09:19:06 -0300

    Added EditableTextWithSuggestion as a base class of EclipseTextArea Changed AutocompletionListItem to fire suggestion events Added caret event to handle suggestion list hiding Removed AutocompletionListView and it's related code

--- source/appModules/eclipse.py
+++ source/appModules/eclipse.py
@@ -7,16 +7,15 @@
 import controlTypes
 import appModuleHandler
 from NVDAObjects.IAccessible import IAccessible
+from NVDAObjects.behaviors import EditableTextWithSuggestions
 import speech
 import ui
 import api
 import sayAllHandler
 import eventHandler
 
-# Translators: This will be the name of the list that contains the autocompleter suggestions:
-AUTOCOMPLETION_LIST_NAME = _("Autocompletion List")
 
-class EclipseTextArea(IAccessible):
+class EclipseTextArea(EditableTextWithSuggestions, IAccessible):
 
 	def event_valueChange(self):
 		# #2314: Eclipse incorrectly fires valueChange when the selection changes.
@@ -25,28 +24,34 @@ class EclipseTextArea(IAccessible):
 		# Therefore, just drop this event.
 		pass
 
-class AutocompletionListView(IAccessible):
+	def event_caret(self):
+		super(EclipseTextArea, self).event_caret()
 
-	def _get_name(self):
-		return AUTOCOMPLETION_LIST_NAME
-
-class AutocompletionListItem(IAccessible):
-
-	def event_selection(self):
-		# Fixme: I don't know if it is the correct way to do this.
-
-		# It's to prevent multiple focus event being sent, I.E when you press CTRL or Shift.
-		if self.appModule.selectedItem != self:
-			self.appModule.selectedItem = self
-			eventHandler.executeEvent("gainFocus", self)
+		# Check suggestion item and close the list if it is not valid
+		try:
+			if not self.appModule.selectedItem.name:
+				self.event_suggestionsClosed()
+				self.appModule.selectedItem = None
+		except:
+			pass
 
 	def script_closeAutocompleter(self, gesture):
 		gesture.send()
+		
+		# Close the suggestions list if it is opened
+		if self.appModule.selectedItem:
+			self.event_suggestionsClosed()
+
 		self.appModule.selectedItem = None
-		eventHandler.executeEvent("gainFocus", api.getDesktopObject().objectWithFocus())
 
 	def script_readDocumentation(self, gesture):
-		obj = api.getDesktopObject()
+		# If there aren't any suggestion selected, there is no way to find quick documentation
+		if not self.appModule.selectedItem:
+			gesture.send()
+			return
+
+		# This may be needed to be changed accordingly to the windows version, but I am unsure
+		obj = api.getDesktopObject().firstChild.next
 
 		# It works for Java and XML editor.
 		while(obj):
@@ -62,8 +67,8 @@ class AutocompletionListItem(IAccessible):
 				break
 
 		if not obj:
-			# Translators: This will be spoken if the script cann't find the documentation text for the selected autocompletion entry
-			ui.message(_("Cann't find documentation for this entry."))
+			# Translators: This will be spoken if the script cannot find the documentation text for the selected autocompletion entry
+			ui.message(_("Cannot find documentation for this entry."))
 			return
 
 		ui.message(obj.basicText)
@@ -76,6 +81,22 @@ class AutocompletionListItem(IAccessible):
 		"kb:escape": "closeAutocompleter",
 	}
 
+
+class AutocompletionListItem(IAccessible):
+
+	def event_selection(self):
+		# Fixme: I don't know if it is the correct way to do this.
+		if not self.appModule.selectedItem:
+			api.getDesktopObject().objectWithFocus().event_suggestionsOpened()
+
+		# It's to prevent multiple focus event being sent, I.E when you press CTRL or Shift.
+		if self.appModule.selectedItem != self:
+			self.appModule.selectedItem = self
+			
+			# Speak suggestion
+			speech.cancelSpeech()
+			self.reportFocus()
+
 class AppModule(appModuleHandler.AppModule):
 
 	LIST_VIEW_CLASS = "SysListView32"
@@ -83,7 +104,6 @@ class AppModule(appModuleHandler.AppModule):
 
 	def __init__(self,processID,appName=None):
 		super(AppModule,self).__init__(processID,appName)
-		eventHandler.requestEvents("destroy", processID, "#32770")
 
 	def event_NVDAObject_init(self, obj):
 		if obj.windowClassName == "SysTreeView32" and obj.role in (controlTypes.ROLE_TREEVIEWITEM, controlTypes.ROLE_CHECKBOX) and controlTypes.STATE_FOCUSED not in obj.states:
@@ -101,14 +121,5 @@ class AppModule(appModuleHandler.AppModule):
 				and obj.parent.parent.parent.parent.parent == api.getDesktopObject()
 				and obj.parent.parent.parent.parent.simpleNext.role == controlTypes.ROLE_BUTTON):
 				clsList.insert(0, AutocompletionListItem)
-		except:
-			pass
-
-		try:
-			if (obj.role == controlTypes.ROLE_LIST
-				and obj.parent.parent.role == controlTypes.ROLE_DIALOG
-				and obj.parent.parent.parent.parent == api.getDesktopObject()
-				and obj.parent.parent.parent.simpleNext.role == controlTypes.ROLE_BUTTON):
-				clsList.insert(0, AutocompletionListView)
 		except:
 			pass
