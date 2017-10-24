f0551ff3912c98a1e1aad2e3d183bd3fafab35e7
James Teh <jamie@jantrid.net>
2017-10-24 14:41:30 +1000

    Update Spotify app module to support recent versions of Spotify.
    1. Spotify no longer fires focus on objects that die before we can fetch them, so remove the hack which worked around this.
    2. Spotify still fires focus on an ancestor window instead of the correct focus. However, the trick we previously used to get the correct focus no longer works. Switch to a new trick which does work.

--- source/appModules/spotify.py
+++ source/appModules/spotify.py
@@ -2,30 +2,27 @@
 #A part of NonVisual Desktop Access (NVDA)
 #This file is covered by the GNU General Public License.
 #See the file COPYING for more details.
-#Copyright (C) 2013 NV Access Limited
+#Copyright (C) 2013-2017 NV Access Limited, James Teh
 
 """App module for Spotify
 """
 
 import appModuleHandler
 import controlTypes
-from NVDAObjects.IAccessible import IAccessible
 import eventHandler
 
 class AppModule(appModuleHandler.AppModule):
 
-	def event_NVDAObject_init(self, obj):
-		if obj.windowClassName == "Chrome_RenderWidgetHostHWND" and isinstance(obj, IAccessible) and obj.IAccessibleChildID < 0 and obj.role == controlTypes.ROLE_UNKNOWN:
-			# #5439: Focus seems to hit Chromium objects that die before we can fetch them.
-			obj.shouldAllowIAccessibleFocusEvent = False
-
 	def event_gainFocus(self, obj, nextHandler):
 		if not eventHandler.isPendingEvents("gainFocus") and obj.windowClassName == "Chrome_WidgetWin_0" and obj.role == controlTypes.ROLE_WINDOW:
 			# Spotify doesn't fire focus on the correct object when it gets the foreground.
+			# Instead, it fires focus on a window ancestor.
+			# Try to get the correct focus.
 			try:
-				focus = obj.activeChild.activeChild
+				focus = obj.firstChild.firstChild.activeChild
 			except AttributeError:
 				focus = None
 			if focus:
+				# Bounce focus to the correct object.
 				return eventHandler.executeEvent("gainFocus", focus)
 		return nextHandler()
