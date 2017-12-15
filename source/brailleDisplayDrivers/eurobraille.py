97a43652ab0e5eafd26f8494c5abda1d9237d2cc
Leonard de Ruijter <leonard@babbage.com>
2017-12-15 07:19:06 +0100

    Add Eurobraille to the contributors and driver copyright header

--- source/brailleDisplayDrivers/eurobraille.py
+++ source/brailleDisplayDrivers/eurobraille.py
@@ -3,7 +3,7 @@
 #A part of NonVisual Desktop Access (NVDA)
 #This file is covered by the GNU General Public License.
 #See the file COPYING for more details.
-#Copyright (C) 2017 NV Access Limited, Babbage B.V.
+#Copyright (C) 2017 NV Access Limited, Babbage B.V., Eurobraille
 
 from collections import OrderedDict, defaultdict
 from cStringIO import StringIO
