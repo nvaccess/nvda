f02e41f6870d8f3849596e48bcebdf00d9d95e51
Michael Curran <mick@nvaccess.org>
2018-02-09 14:04:34 +1000

    Update copyright header

--- source/NVDAObjects/IAccessible/__init__.py
+++ source/NVDAObjects/IAccessible/__init__.py
@@ -1,5 +1,5 @@
 #A part of NonVisual Desktop Access (NVDA)
-#Copyright (C) 2006-2015 NVDA Contributors
+#Copyright (C) 2006-2018 NV Access Limited 
 #This file is covered by the GNU General Public License.
 #See the file COPYING for more details.
 
