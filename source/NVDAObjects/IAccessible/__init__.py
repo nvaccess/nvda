611a9423087249e82056af4c1d09cd5433ce8e29
Michael Curran <michaelDCurran@users.noreply.github.com>
2018-03-07 12:05:08 +1000

    Support tables in Kindle for PC (#7977)
    * Add support for IAccessibleTable2 and IAccessibleTableCell
    
    * Add support for tables in Kindle
    
    * Kindle: initial support for jumping to tables with t / shift+t.
    
    * Kindle: support jumping to the beginning/end of containers (so far just tables) with comma and shift comma.
    
    * Add code comments
    
    * Kindle appModule: update method docstring.

--- source/NVDAObjects/IAccessible/__init__.py
+++ source/NVDAObjects/IAccessible/__init__.py
@@ -608,9 +608,12 @@ the NVDAObject for IAccessible
 		except COMError:
 			pass
 		try:
-			self.IAccessibleTableObject=self.IAccessibleObject.QueryInterface(IAccessibleHandler.IAccessibleTable)
+			self.IAccessibleTable2Object=self.IAccessibleObject.QueryInterface(IAccessibleHandler.IAccessibleTable2)
 		except COMError:
-			pass
+			try:
+				self.IAccessibleTableObject=self.IAccessibleObject.QueryInterface(IAccessibleHandler.IAccessibleTable)
+			except COMError:
+				pass
 		try:
 			self.IAccessibleTextObject=IAccessibleObject.QueryInterface(IAccessibleHandler.IAccessibleText)
 		except COMError:
@@ -1049,6 +1052,9 @@ the NVDAObject for IAccessible
 		self.event_stateChange()
 
 	def _get_IA2PhysicalRowNumber(self):
+		tableCell=self._IATableCell
+		if tableCell:
+			return tableCell.rowIndex+1
 		table=self.table
 		if table:
 			if self.IAccessibleTableUsesTableCellIndexAttrib:
@@ -1079,6 +1085,9 @@ the NVDAObject for IAccessible
 		raise NotImplementedError
 
 	def _get_IA2PhysicalColumnNumber(self):
+		tableCell=self._IATableCell
+		if tableCell:
+			return tableCell.columnIndex+1
 		table=self.table
 		if table:
 			if self.IAccessibleTableUsesTableCellIndexAttrib:
@@ -1120,6 +1129,11 @@ the NVDAObject for IAccessible
 		raise NotImplementedError
 
 	def _get_IA2PhysicalRowCount(self):
+		if hasattr(self,'IAccessibleTable2Object'):
+			try:
+				return self.IAccessibleTable2Object.nRows
+			except COMError:
+				log.debugWarning("IAccessibleTable2::nRows failed", exc_info=True)
 		if hasattr(self,'IAccessibleTableObject'):
 			try:
 				return self.IAccessibleTableObject.nRows
@@ -1134,6 +1148,11 @@ the NVDAObject for IAccessible
 		return count
 
 	def _get_IA2PhysicalColumnCount(self):
+		if hasattr(self,'IAccessibleTable2Object'):
+			try:
+				return self.IAccessibleTable2Object.nColumns
+			except COMError:
+				log.debugWarning("IAccessibleTable2::nColumns failed", exc_info=True)
 		if hasattr(self,'IAccessibleTableObject'):
 			try:
 				return self.IAccessibleTableObject.nColumns
@@ -1197,10 +1216,10 @@ the NVDAObject for IAccessible
 		if self.IAccessibleTableUsesTableCellIndexAttrib and "table-cell-index" in self.IA2Attributes:
 			checkAncestors=True
 		obj=self.parent
-		while checkAncestors and obj and not hasattr(obj,'IAccessibleTableObject'):
+		while checkAncestors and obj and not hasattr(obj,'IAccessibleTable2Object') and not hasattr(obj,'IAccessibleTableObject'):
 			parent=obj.parent=obj.parent
 			obj=parent
-		if not obj or not hasattr(obj,'IAccessibleTableObject'):
+		if not obj or (not hasattr(obj,'IAccessibleTable2Object') and not hasattr(obj,'IAccessibleTableObject')):
 			return None
 		self._table=obj
 		return obj
