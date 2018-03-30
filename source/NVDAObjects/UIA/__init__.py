6f528b7a70e74f99935514a652253cc102a98666
Joseph Lee <joseph.lee22590@gmail.com>
2018-03-30 08:10:43 -0700

    NVDAObjects/UIA: rename UWPToolTip to XAMLToolTip to better reflect where this is coming from. Re #8118

--- source/NVDAObjects/UIA/__init__.py
+++ source/NVDAObjects/UIA/__init__.py
@@ -752,7 +752,7 @@ class UIA(Window):
 			clsList.append(Toast_win10)
 		# #8118: treat UWP tooltips as proper tooltips, especially those found in Microsoft Edge and other apps.
 		elif UIAClassName=="ToolTip" and self.UIAElement.cachedFrameworkID == "XAML":
-			clsList.append(UWPToolTip)
+			clsList.append(XAMLToolTip)
 		elif self.UIAElement.cachedFrameworkID in ("InternetExplorer","MicrosoftEdge"):
 			import edge
 			if UIAClassName in ("Internet Explorer_Server","WebView") and self.role==controlTypes.ROLE_PANE:
@@ -1566,7 +1566,7 @@ class Toast_win10(Notification, UIA):
 		Notification.event_alert(self)
 
 
-class UWPToolTip(ToolTip, UIA):
+class XAMLToolTip(ToolTip, UIA):
 
 	event_UIA_toolTipOpened=ToolTip.event_show
 
