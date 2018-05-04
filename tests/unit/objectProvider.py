cfe7278a5217118464823013272064c158195791
Leonard de Ruijter <leonard@babbage.com>
2018-05-04 19:53:29 +0200

    Finish unit tests

--- tests/unit/objectProvider.py
+++ tests/unit/objectProvider.py
@@ -9,6 +9,7 @@
 
 from NVDAObjects import NVDAObject
 import controlTypes
+from scriptHandler import script
 
 class PlaceholderNVDAObject(NVDAObject):
 	processID = None # Must be implemented to instantiate.
@@ -25,3 +26,75 @@ class NVDAObjectWithRole(PlaceholderNVDAObject):
 	def _get_name(self):
 		return controlTypes.roleLabels.get(self.role,controlTypes.ROLE_UNKNOWN)
 
+class NVDAObjectWithDecoratedScript(PlaceholderNVDAObject):
+	"""An object with a decorated script."""
+
+	@script(gestures=["kb:a"])
+	def script_a(self, gesture):
+		return
+
+class NVDAObjectWithGesturesDictionary(PlaceholderNVDAObject):
+	"""An object with a script that is bound to a gesture in a L{__gestures} dictionary."""
+
+	def script_b(self, gesture):
+		return
+
+	__gestures = {
+		"kb:b": "b"
+	}
+
+class NVDAObjectWithDecoratedScriptAndGesturesDictionary(PlaceholderNVDAObject):
+	"""An object with a decorated script
+	and a script that is bound to a gesture in a L{__gestures} dictionary.
+	"""
+
+	@script(gestures=["kb:c"])
+	def script_c(self, gesture):
+		return
+
+	def script_d(self, gesture):
+		return
+
+	__gestures = {
+		"kb:d": "d",
+	}
+
+class SubclassedNVDAObjectWithDecoratedScriptAndGesturesDictionary(
+	NVDAObjectWithDecoratedScript,
+	NVDAObjectWithGesturesDictionary,
+	NVDAObjectWithDecoratedScriptAndGesturesDictionary
+):
+	"""An object with decorated scripts and L{__gestures} dictionaries, based on subclassing."""
+
+	@script(gestures=["kb:e"])
+	def script_e(self, gesture):
+		return
+
+	def script_f(self, gesture):
+		return
+
+	__gestures = {
+		"kb:f": "f",
+	}
+
+class DynamicNVDAObjectWithDecoratedScriptAndGesturesDictionary(PlaceholderNVDAObject):
+	"""An object with decorated scripts and L{__gestures} dictionaries,
+	using the chooseOverlayClasses logic to construct a dynamic object."""
+
+	def findOverlayClasses(self, clsList):
+		clsList.extend([
+			NVDAObjectWithDecoratedScript,
+			NVDAObjectWithGesturesDictionary,
+			NVDAObjectWithDecoratedScriptAndGesturesDictionary
+		])
+
+	@script(gestures=["kb:g"])
+	def script_g(self, gesture):
+		return
+
+	def script_h(self, gesture):
+		return
+
+	__gestures = {
+		"kb:h": "h",
+	}
--- tests/unit/test_baseObject.py
+++ tests/unit/test_baseObject.py
@@ -8,62 +8,7 @@
 
 import unittest
 from baseObject import ScriptableObject
-from objectProvider import PlaceholderNVDAObject
-import controlTypes
-from config import conf
-import api
-import globalVars
-from scriptHandler import script
-
-class NVDAObjectWithDecoratedScript(PlaceholderNVDAObject):
-	"""An object with a decorated script."""
-
-	@script(gestures=["kb:a"])
-	def script_a(self, gesture):
-		return
-
-class NVDAObjectWithGestureMap(PlaceholderNVDAObject):
-	"""An object with a script that is bound to a gesture in a gesture map."""
-
-	def script_b(self, gesture):
-		return
-
-	__gestures = {
-		"kb:b": "b"
-	}
-
-class NVDAObjectWithDecoratedScriptAndGestureMap(PlaceholderNVDAObject):
-	"""An object with a decorated script
-	and a script that is bound to a gesture in a gesture map.
-	"""
-
-	@script(gestures=["kb:c"])
-	def script_c(self, gesture):
-		return
-
-	def script_d(self, gesture):
-		return
-
-	__gestures = {
-		"kb:d": "d",
-	}
-
-class SubclassedNVDAObjectWithDecoratedScriptAndGestureMap(
-	NVDAObjectWithDecoratedScript,
-	NVDAObjectWithGestureMap,
-	NVDAObjectWithDecoratedScriptAndGestureMap
-):
-
-	@script(gestures=["kb:e"])
-	def script_e(self, gesture):
-		return
-
-	def script_f(self, gesture):
-		return
-
-	__gestures = {
-		"kb:f": "f",
-	}
+from objectProvider import *
 
 class TestScriptableObject(unittest.TestCase):
 	"""A test that verifies whether scripts are properly bound to associated gestures."""
@@ -72,16 +17,21 @@ class TestScriptableObject(unittest.TestCase):
 		obj = NVDAObjectWithDecoratedScript()
 		self.assertIn("kb:a", obj._gestureMap)
 
-	def test_gestureMap(self):
-		obj = NVDAObjectWithGestureMap()
+	def test_gesturesDictionary(self):
+		obj = NVDAObjectWithGesturesDictionary()
 		self.assertIn("kb:b", obj._gestureMap)
 
-	def test_decoratedScriptAndGestureMap(self):
-		obj = NVDAObjectWithDecoratedScriptAndGestureMap()
+	def test_decoratedScriptAndGesturesDictionary(self):
+		obj = NVDAObjectWithDecoratedScriptAndGesturesDictionary()
 		self.assertIn("kb:c", obj._gestureMap)
 		self.assertIn("kb:d", obj._gestureMap)
 
-	def test_decoratedScriptsAndGestureMapsIfSubclassed(self):
-		obj = SubclassedNVDAObjectWithDecoratedScriptAndGestureMap()
+	def test_decoratedScriptsAndGestureDictionariesIfSubclassed(self):
+		obj = SubclassedNVDAObjectWithDecoratedScriptAndGesturesDictionary()
 		for key in ("a", "b", "c", "d", "e", "f"):
+			self.assertIn("kb:%s" % key, obj._gestureMap)
+
+	def test_decoratedScriptsAndGestureDictionariesIfDynamic(self):
+		obj = DynamicNVDAObjectWithDecoratedScriptAndGesturesDictionary()
+		for key in ("a", "b", "c", "d", "g", "h"):
 			self.assertIn("kb:%s" % key, obj._gestureMap)
