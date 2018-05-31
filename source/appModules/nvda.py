0f69cd1d9e60ebc40d314ae8592d5277801cda82
Leonard de Ruijter <leonardder@users.noreply.github.com>
2018-05-31 07:44:16 +0200

    Fix currently edited profile announcements, numpad enter functionality and validation behavior (#8253)
    * Fix another case where the switch of the currently edited profile is not announced
    
    * Restore event_appModule_loseFocus
    
    * Fix operation of the numpad enter key in settings dialogs
    
    * Remove obsolete char hooks from lists
    
    * Rename handler for enter and control+s to _enterActivatesOk_ctrlSActivatesApply
    
    * Add additional information in the enter/escape event handler that mentions wx ticket 3725
    
    * Add preSave and postSave handlers
    
    * Fixed doc string for SettingsPanel on discard
    
    * preSave, explicitly check for False
    
    * Review actions

--- source/appModules/nvda.py
+++ source/appModules/nvda.py
@@ -52,9 +52,6 @@ class NvdaSettingsCategoryPanel(IAccessible):
 
 class AppModule(appModuleHandler.AppModule):
 
-	def event_appModule_gainFocus(self):
-		NvdaSettingsCategoryPanel.handlePossibleProfileSwitch()
-
 	def event_appModule_loseFocus(self):
 		NvdaSettingsCategoryPanel.oldProfile = None
 
@@ -98,6 +95,8 @@ class AppModule(appModuleHandler.AppModule):
 	def event_foreground         (self, obj, nextHandler):
 		if not gui.shouldConfigProfileTriggersBeSuspended():
 			config.conf.resumeProfileTriggers()
+		else:
+			NvdaSettingsCategoryPanel.handlePossibleProfileSwitch()
 		nextHandler()
 
 	def chooseNVDAObjectOverlayClasses(self, obj, clsList):
