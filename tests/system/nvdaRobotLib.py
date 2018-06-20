89816971a52e7195e30cce18611d334dab8487bb
Reef Turner <reef@nvaccess.org>
2018-06-20 02:43:06 +0000

    Some cleanup of connection/disconnection code

--- tests/system/nvdaRobotLib.py
+++ tests/system/nvdaRobotLib.py
@@ -1,11 +1,17 @@
 import os
 import sys
 from timeit import default_timer as timer
-
+from robotremoteserver import test_remote_server, stop_remote_server
 from robot.libraries.BuiltIn import BuiltIn
+
+
 builtIn = BuiltIn()
 process = builtIn.get_library_instance('Process')
 opSys = builtIn.get_library_instance('OperatingSystem')
+
+spyServerPort = 8270  # is `registered by IANA` for remote server usage. Two ASCII values:'RF'
+spyServerURI = 'http://127.0.0.1:{}'.format(spyServerPort)
+spyAlias = "nvdaSpy"
 
 systemTestSpyFileName = "systemTestSpy.py"
 systemTestSourceDir = os.path.abspath("tests/system")
@@ -45,38 +51,33 @@ class nvdaRobotLib(object):
 		"""Equiv robot text:
 		Import Library  Remote         WITH NAME    nvdaSpy
 		"""
-		port = 8270  # default:8270 is `registered by IANA` for remote server usage. Two ASCII values, RF.
-		uri = 'http://127.0.0.1:{}'.format(port)
-		spyAlias = "nvdaSpy"
-
 		startTime = timer()
 		giveUpAfter = 10  # seconds
 		intervalBetweenTries = 0.1  # seconds
 		lastRunTime = startTime - intervalBetweenTries+1  # ensure we start trying immediately
-		canConnect=False
-		from robotremoteserver import test_remote_server
-		while not canConnect and (timer() - startTime) < giveUpAfter:
+
+		while (timer() - startTime) < giveUpAfter:
 			if (timer() - lastRunTime) > intervalBetweenTries:
 				lastRunTime = timer()
 
 				# Importing the 'Remote' library always succeeds, even when a connection can not be made.
 				# If that happens, then some 'Remote' keyword will fail at some later point.
 				# therefore we use 'test_remote_server' to ensure that we can in fact connect before proceeding.
-				canConnect = test_remote_server(uri)
-
-		if not canConnect:
-			raise RuntimeError("Unable to connect to nvdaSpy")
+				if test_remote_server(spyServerURI, log=False):
+					break
 		else:
-			builtIn.import_library(
-				"Remote",  # name of library to import
-				# Arguments to construct the library instance:
-				"uri={}".format(uri),
-				"timeout=2",  # seconds
-				# Set an alias for the imported library instance
-				"WITH NAME",
-				"nvdaSpy",
-			)
-			self.nvdaSpy = builtIn.get_library_instance(spyAlias)
+			raise RuntimeError("Unable to connect to nvdaSpy")
+
+		builtIn.import_library(
+			"Remote",  # name of library to import
+			# Arguments to construct the library instance:
+			"uri={}".format(spyServerURI),
+			"timeout=2",  # seconds
+			# Set an alias for the imported library instance
+			"WITH NAME",
+			"nvdaSpy",
+		)
+		self.nvdaSpy = builtIn.get_library_instance(spyAlias)
 
 	def start_NVDA(self):
 		self.copy_in_system_test_spy()
@@ -90,12 +91,8 @@ class nvdaRobotLib(object):
 		while not self.nvdaSpy.run_keyword("is_NVDA_startup_complete", [], {}):
 			builtIn.sleep(0.1)
 
-
 	def quit_NVDA(self):
-		try:
-			self.nvdaSpy.run_keyword("stop_remote_server", [], {})
-		except RuntimeError:
-			pass  # if the test manually exits, then we are unable to run this keyword.
+		stop_remote_server(spyServerURI, log=False)
 		self.remove_system_test_spy()
 		process.run_process(
 			"pythonw nvda.pyw -q --disable-addons",
