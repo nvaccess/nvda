cb9059285ca71d226645f415e7fd1eebe60f1dd6
Ralf Kefferpuetz <novalis7747@live.com>
2018-06-20 19:01:35 +0200

    solves issue #8244
    solves #8244 , NVDA does not speak email contact autocompletion options in outlook 2016 and NVDA 2018.1
    modified class:
    class AutoCompleteListItem(IAccessible):
    
    	def event_stateChange(self):
    		states=self.states
    		focus=api.getFocusObject()
    		if (focus.role==controlTypes.ROLE_EDITABLETEXT or focus.role==controlTypes.ROLE_BUTTON) and controlTypes.STATE_SELECTED in states and controlTypes.STATE_INVISIBLE not in states and controlTypes.STATE_UNAVAILABLE not in states and controlTypes.STATE_OFFSCREEN not in states:
    			address = self.name
    			if address:
    				speech.cancelSpeech()
    				ui.message(self.name)

--- source/appModules/outlook.py
+++ source/appModules/outlook.py
@@ -351,8 +351,10 @@ class AutoCompleteListItem(IAccessible):
 		states=self.states
 		focus=api.getFocusObject()
 		if (focus.role==controlTypes.ROLE_EDITABLETEXT or focus.role==controlTypes.ROLE_BUTTON) and controlTypes.STATE_SELECTED in states and controlTypes.STATE_INVISIBLE not in states and controlTypes.STATE_UNAVAILABLE not in states and controlTypes.STATE_OFFSCREEN not in states:
-			speech.cancelSpeech()
-			ui.message(self.name)
+			address = self.name
+			if address:
+				speech.cancelSpeech()
+				ui.message(self.name)
 
 class CalendarView(IAccessible):
 	"""Support for announcing time slots and appointments in Outlook Calendar.
