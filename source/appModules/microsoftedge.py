efc41821f5ad97e8bb021d5956ddc66361a511e8
Joseph Lee <joseph.lee22590@gmail.com>
2018-06-19 21:52:56 -0700

    Edge: announce notifications fired by main Edge process such as reading view availability. Re #8423.
    Certain notifications, including reading view availability are fired by main Edge process, not the content process. This means NvDA will ignore such notifications. Resolved by adding a dedicated app module for Edge main process that'll catch notifications and announce them, and propogate them no more.

---
+++ source/appModules/microsoftedge.py
@@ -0,0 +1,17 @@
+# MicrosoftEdge.py
+#A part of NonVisual Desktop Access (NVDA)
+#This file is covered by the GNU General Public License.
+#See the file COPYING for more details.
+#Copyright (C) 2018 NV Access Limited, Joseph Lee
+
+"""appModule for Microsoft Edge main process"""
+
+import appModuleHandler
+import ui
+
+class AppModule(appModuleHandler.AppModule):
+
+	def event_UIA_notification(self, obj, nextHandler, displayString=None, **kwargs):
+		# #8423: even though content process is focused, notifications are fired by main Edge process.
+		# The base object will simply ignore this, so notifications must be announced here and no more.
+		ui.message(displayString)
