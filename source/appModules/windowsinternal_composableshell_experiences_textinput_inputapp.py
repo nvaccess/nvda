eeecece62b0db84f98403496cdb0b38e8746ed37
Joseph Lee <joseph.lee22590@gmail.com>
2018-07-12 20:59:01 -0700

    Modern keyboard: support various changes introduced in build 17700 series. Re #7273.
    Several changes as a result of deploying build 17700 series:
    * Clipboard: NVDA will no longer announce label for clipboard candidates list.
    * Emoji panel: NVDA will not respond to verbose name change events fired whenever items are selected.

--- source/appModules/windowsinternal_composableshell_experiences_textinput_inputapp.py
+++ source/appModules/windowsinternal_composableshell_experiences_textinput_inputapp.py
@@ -19,6 +19,9 @@ import winVersion
 
 class AppModule(appModuleHandler.AppModule):
 
+	# Cache the most recently selected item.
+	_recentlySelected = None
+
 	def event_UIA_elementSelected(self, obj, nextHandler):
 		# #7273: When this is fired on categories, the first emoji from the new category is selected but not announced.
 		# Therefore, move the navigator object to that item if possible.
@@ -27,6 +30,9 @@ class AppModule(appModuleHandler.AppModule):
 		# #8189: do not announce candidates list itself (not items), as this is repeated each time candidate items are selected.
 		if obj.UIAElement.cachedAutomationID == "CandidateList": return
 		speech.cancelSpeech()
+		# Sometimes clipboard candidates list gets selected, so ask NvDA to descend one more level.
+		if obj.UIAElement.cachedAutomationID == "TEMPLATE_PART_ClipboardItemsList":
+			obj = obj.firstChild
 		candidate = obj
 		if obj.UIAElement.cachedClassName == "ListViewItem" and obj.parent.UIAElement.cachedAutomationID != "TEMPLATE_PART_ClipboardItemsList":
 			# The difference between emoji panel and suggestions list is absence of categories/emoji separation.
@@ -40,6 +46,8 @@ class AppModule(appModuleHandler.AppModule):
 			api.setNavigatorObject(obj)
 			obj.reportFocus()
 			braille.handler.message(braille.getBrailleTextForProperties(name=obj.name, role=obj.role, positionInfo=obj.positionInfo))
+			# Cache selected item.
+			self._recentlySelected = obj.name
 		else:
 			# Translators: presented when there is no emoji when searching for one in Windows 10 Fall Creators Update and later.
 			ui.message(_("No emoji"))
@@ -49,7 +57,7 @@ class AppModule(appModuleHandler.AppModule):
 		# Make sure to announce most recently used emoji first in post-1709 builds.
 		# Fake the announcement by locating 'most recently used" category and calling selected event on this.
 		# However, in build 17666 and later, child count is the same for both emoji panel and hardware keyboard candidates list.
-		if winVersion.winVersion.build < 17666 and obj.childCount == 3:
+		if winVersion.winVersion.build <= 17134 and obj.childCount == 3:
 			self.event_UIA_elementSelected(obj.lastChild.firstChild, nextHandler)
 		# Handle hardware keyboard suggestions.
 		# Treat it the same as CJK composition list - don't announce this if candidate announcement setting is off.
@@ -64,23 +72,27 @@ class AppModule(appModuleHandler.AppModule):
 					pass
 			# Emoji panel in build 17666 and later (unless this changes).
 			elif childAutomationID == "TEMPLATE_PART_ExpressionGroupedFullView":
-				self._emojiPanelOpened = True
+				self._emojiPanelJustOpened = True
 				self.event_UIA_elementSelected(obj.firstChild.firstChild.next.next.firstChild.firstChild, nextHandler)
 		nextHandler()
 
 	# Argh, name change event is fired right after emoji panel opens in build 17666 and later.
-	_emojiPanelOpened = False
+	_emojiPanelJustOpened = False
 
 	def event_nameChange(self, obj, nextHandler):
-		# On some systems, touch keyboard keys keeps firing name change event.
-		if obj.UIAElement.cachedClassName == "CRootKey":
+		# #49: reported by a user: on some systems, touch keyboard keys keeps firing name change event.
+		# Argh, in build 17704, whenever skin tones are selected, name change is fired by emoji entries (GridViewItem).
+		if ((obj.UIAElement.cachedClassName in ("CRootKey", "GridViewItem"))
+		or (obj.UIAElement.cachedAutomationID == "TEMPLATE_PART_ClipboardItemsList")
+		# And no, emoji entries should not be announced here.
+		or (self._recentlySelected is not None and self._recentlySelected in obj.name)):
 			return
 		# The word "blank" is kept announced, so suppress this on build 17666 and later.
-		if winVersion.winVersion.build >= 17672:
+		if winVersion.winVersion.build > 17134:
 			# In build 17672 and later, return immediatley when element selected event on clipboard item was fired just prior to this.
 			if obj.UIAElement.cachedAutomationID == "TEMPLATE_PART_ClipboardItemIndex" or obj.parent.UIAElement.cachedAutomationID == "TEMPLATE_PART_ClipboardItemsList": return
-			if not self._emojiPanelOpened or obj.UIAElement.cachedAutomationID != "TEMPLATE_PART_ExpressionGroupedFullView": speech.cancelSpeech()
-			self._emojiPanelOpened = False
+			if not self._emojiPanelJustOpened or obj.UIAElement.cachedAutomationID != "TEMPLATE_PART_ExpressionGroupedFullView": speech.cancelSpeech()
+			self._emojiPanelJustOpened = False
 		# Don't forget to add "Microsoft Candidate UI" as something that should be suppressed.
 		if obj.UIAElement.cachedAutomationID not in ("TEMPLATE_PART_ExpressionFullViewItemsGrid", "TEMPLATE_PART_ClipboardItemIndex", "CandidateWindowControl"):
 			ui.message(obj.name)
