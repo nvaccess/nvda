b4e9e83242f90d18d2be51395f3f11e4da3f7756
Michael Curran <michaelDCurran@users.noreply.github.com>
2018-10-30 16:47:28 +1000

    Revert "Don't announce 'selected' when the focus moves in Google sheets if the focused cell is the only cell selected (#8879)" (#8893)
    This reverts commit 51f5b2fdcd1b480c79c379eb2f83b8fb44372c61.
    The pr was causing 'not selected' to be announced when navigating by table cell in browse mdoe.

--- source/controlTypes.py
+++ source/controlTypes.py
@@ -727,18 +727,7 @@ def processNegativeStates(role, states, reason, negativeStates=None):
 	# but only if it is either focused or this is something other than a change event.
 	# The condition stops "not selected" from being spoken in some broken controls
 	# when the state change for the previous focus is issued before the focus change.
-	if (
-		STATE_SELECTABLE in states 
-		and (reason != REASON_CHANGE or STATE_FOCUSED in states)
-		and role in (
-			ROLE_LISTITEM, 
-			ROLE_TREEVIEWITEM, 
-			ROLE_TABLEROW,
-			ROLE_TABLECELL,
-			ROLE_TABLECOLUMNHEADER,
-			ROLE_TABLEROWHEADER
-		)
-	):
+	if role in (ROLE_LISTITEM, ROLE_TREEVIEWITEM, ROLE_TABLEROW) and STATE_SELECTABLE in states and (reason != REASON_CHANGE or STATE_FOCUSED in states):
 		speakNegatives.add(STATE_SELECTED)
 	# Restrict "not checked" in a similar way to "not selected".
 	if (role in (ROLE_CHECKBOX, ROLE_RADIOBUTTON, ROLE_CHECKMENUITEM) or STATE_CHECKABLE in states)  and (STATE_HALFCHECKED not in states) and (reason != REASON_CHANGE or STATE_FOCUSED in states):
