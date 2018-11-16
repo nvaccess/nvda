5de88a57f5955136a6bb39238fdbd53230679d04
Josiel Santos <josiel.lkp@gmail.com>
2018-11-16 14:04:33 -0200

    fixup! Added DBeaver AppModule

--- source/appModules/dbeaver.py
+++ source/appModules/dbeaver.py
@@ -1,3 +1,9 @@
+#appModules/dbeaver.py
+#A part of NonVisual Desktop Access (NVDA)
+#This file is covered by the GNU General Public License.
+#See the file COPYING for more details.
+#Copyright (C) 2018 NV Access Limited, Josiel Santos
+
 """App module for DBeaver
 This simply uses the app module for Eclipse.
 """
