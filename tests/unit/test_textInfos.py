1faccf0f318b41f0c116271c67def90ffad74f50
Leonard de Ruijter <leonard@babbage.com>
2018-11-24 09:53:50 +0100

    UTF-8 without BOM for tests

--- tests/unit/test_textInfos.py
+++ tests/unit/test_textInfos.py
@@ -1,4 +1,4 @@
-﻿# -*- coding: UTF-8 -*-
+# -*- coding: UTF-8 -*-
 #tests/unit/test_textInfos.py
 #A part of NonVisual Desktop Access (NVDA)
 #This file is covered by the GNU General Public License.
