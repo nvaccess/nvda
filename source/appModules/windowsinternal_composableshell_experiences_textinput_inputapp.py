2e15015bc2e678c1ff21445a14b1f48c96e4285f
Joseph Lee <joseph.lee22590@gmail.com>
2018-12-26 08:44:56 -0800

    Modern keyboard: update app module description text to include cloud clipboard paste functionality.

--- source/appModules/windowsinternal_composableshell_experiences_textinput_inputapp.py
+++ source/appModules/windowsinternal_composableshell_experiences_textinput_inputapp.py
@@ -6,7 +6,7 @@
 
 """App module for Windows 10 Modern Keyboard aka new touch keyboard panel.
 The chief feature is allowing NVDA to announce selected emoji when using the keyboard to search for and select one.
-Another feature is to announce candidates for misspellings if suggestions for hardware keyboard is selected.
+Other features include announcing candidates for misspellings if suggestions for hardware keyboard is selected, and managing cloud clipboard paste.
 This is applicable on Windows 10 Fall Creators Update and later."""
 
 import appModuleHandler
