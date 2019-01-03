ad6ff9f87bf75ca5940e5adfb656b6de87c1bb10
Joseph Lee <joseph.lee22590@gmail.com>
2019-01-02 20:55:18 -0800

    App modules/WWAHost: update copyright header.

--- source/appModules/wwahost.py
+++ source/appModules/wwahost.py
@@ -1,7 +1,13 @@
 #A part of NonVisual Desktop Access (NVDA)
 #This file is covered by the GNU General Public License.
 #See the file COPYING for more details.
-#Copyright (C) 2012 NV Access Limited
+#Copyright (C) 2012-2019 NV Access Limited, Joseph Lee
+
+"""App module host for Windows 8.x and 10 web apps hosted by wwahost.exe.
+In Windows 8, apps written in Javascript are executed inside WWAHost, including some WinRT apps.
+In Windows 10, progressive web apps (PWA) and friends are hosted inside this process.
+App modules wishing to support apps hosted inside this process must import contents of this app module.
+"""
 
 from comtypes import COMError
 import IAccessibleHandler
