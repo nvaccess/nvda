0dedba6f4158f8fb06e0e9652d60209dc1c5d54c
Bill Dengler <codeofdusk@gmail.com>
2019-01-03 19:22:13 +0000

    Review items.

--- source/appModules/mmc.py
+++ source/appModules/mmc.py
@@ -6,34 +6,18 @@
 
 import appModuleHandler
 import controlTypes
-import ui
 from NVDAObjects.IAccessible import IAccessible
 
 
 class MMCTable(IAccessible):
-	def script_getCurrentObject(self, gesture):
+	def _get_value(self):
 		"#1486: workaround to read tables in MMC, such as the disk management graphical view."
-		gesture.send()
 		for i in self.children:
 			if controlTypes.STATE_SELECTED in i.states:
-				ui.message(i.name + ' ' + i.value)
-
-	__tableReviewGestures = (
-		"kb:rightarrow",
-		"kb:leftarrow",
-		"kb:uparrow",
-		"kb:downarrow",
-		"kb:pageup",
-		"kb:pagedown"
-	)
-
-	def initOverlayClass(self):
-		# #1486: bind the navigation keys to our custom script
-		for i in self.__tableReviewGestures:
-			self.bindGesture(i, "getCurrentObject")
+				return i.name + ' ' + i.value
 
 
 class AppModule(appModuleHandler.AppModule):
 	def chooseNVDAObjectOverlayClasses(self, obj, clsList):
-		if obj.role == controlTypes.ROLE_TABLE:
+		if obj.role == controlTypes.ROLE_TABLE and obj.windowClassName == "AfxWnd42u":
 			clsList.insert(0, MMCTable)
