b483e40f09c9f6dbf012114b0222065f62d0c621
Bill Dengler <codeofdusk@gmail.com>
2019-01-04 12:59:58 +0000

    Style.

--- source/appModules/mmc.py
+++ source/appModules/mmc.py
@@ -12,22 +12,25 @@ import api
 
 
 class MMCTable(IAccessible):
-	def _get_focusRedirect(self):
-		#1486: workaround to read tables in MMC, such as the disk management graphical view.
-		for child in self.children:
-			if controlTypes.STATE_SELECTED in child.states:
-				return child
-		return None
+    def _get_focusRedirect(self):
+        #1486: workaround to read tables in MMC, such as the disk management graphical view.
+        for child in self.children:
+            if controlTypes.STATE_SELECTED in child.states:
+                return child
+        return None
+
 
 class MMCTableCell(IAccessible):
-	def event_selection(self):
-		if self.parent.hasFocus and api.getFocusObject() != self:
-			eventHandler.executeEvent("gainFocus", self)
+    def event_selection(self):
+        if self.parent.hasFocus and api.getFocusObject() != self:
+            eventHandler.executeEvent("gainFocus", self)
+
 
 class AppModule(appModuleHandler.AppModule):
-	def chooseNVDAObjectOverlayClasses(self, obj, clsList):
-		if obj.windowClassName == "AfxWnd42u":
-			if obj.role == controlTypes.ROLE_TABLE:
-				clsList.insert(0, MMCTable)
-			elif obj.role in (controlTypes.ROLE_TABLECELL, controlTypes.ROLE_TABLEROWHEADER):
-				clsList.insert(0, MMCTableCell)
+    def chooseNVDAObjectOverlayClasses(self, obj, clsList):
+        if obj.windowClassName == "AfxWnd42u":
+            if obj.role == controlTypes.ROLE_TABLE:
+                clsList.insert(0, MMCTable)
+            elif obj.role in (controlTypes.ROLE_TABLECELL,
+                              controlTypes.ROLE_TABLEROWHEADER):
+                clsList.insert(0, MMCTableCell)
