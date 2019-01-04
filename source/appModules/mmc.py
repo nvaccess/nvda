eaaf60202978f8be0473f7a1b5c845356bdbbc0b
Leonard de Ruijter <leonard@babbage.com>
2019-01-04 12:17:47 +0100

    New implementation based on focus redirect and gainFocus events

--- source/appModules/mmc.py
+++ source/appModules/mmc.py
@@ -1,23 +1,30 @@
 #appModules/mmc.py
 #A part of NonVisual Desktop Access (NVDA)
-#Copyright (C) 2015-2019 NVDA Contributors, David Parduhn, Bill Dengler
+#Copyright (C) 2015-2019 NV Access Limited, David Parduhn, Bill Dengler, Leonard de Ruijter
 #This file is covered by the GNU General Public License.
 #See the file COPYING for more details.
 
 import appModuleHandler
 import controlTypes
 from NVDAObjects.IAccessible import IAccessible
+import eventHandler
 
 
 class MMCTable(IAccessible):
-	def _get_value(self):
-		"#1486: workaround to read tables in MMC, such as the disk management graphical view."
-		for i in self.children:
-			if controlTypes.STATE_SELECTED in i.states:
-				return i.name + ' ' + i.value
-
+	def _get_focusRedirect(self):
+		#1486: workaround to read tables in MMC, such as the disk management graphical view.
+		for child in self.children:
+			if controlTypes.STATE_SELECTED in child.states:
+				return child
+		return None
 
 class AppModule(appModuleHandler.AppModule):
 	def chooseNVDAObjectOverlayClasses(self, obj, clsList):
 		if obj.role == controlTypes.ROLE_TABLE and obj.windowClassName == "AfxWnd42u":
 			clsList.insert(0, MMCTable)
+
+	def event_selection(self, obj, nextHandler):
+		parent = obj.parent
+		if isinstance(parent, MMCTable) and parent.hasFocus:
+			eventHandler.executeEvent("gainFocus", obj)
+		nextHandler()
