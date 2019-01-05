9e7d228608c2ad1bdda1a6be342cbc1ad3602216
Joseph Lee <joseph.lee22590@gmail.com>
2019-01-05 09:08:03 -0800

    Cosmetics

--- source/appModules/mmc.py
+++ source/appModules/mmc.py
@@ -3,16 +3,16 @@
 #Copyright (C) 2015-2019 NV Access Limited, David Parduhn, Bill Dengler, Leonard de Ruijter
 #This file is covered by the GNU General Public License.
 #See the file COPYING for more details.
+
 import api
 import appModuleHandler
 import controlTypes
 import eventHandler
 from NVDAObjects.IAccessible import IAccessible
 
-
 class MMCTable(IAccessible):
 	def _get_focusRedirect(self):
-		#1486: workaround to read tables in MMC, such as the disk management graphical view.
+		# #1486: workaround to read tables in MMC, such as the disk management graphical view.
 		for child in self.children:
 			if controlTypes.STATE_SELECTED in child.states:
 				return child
