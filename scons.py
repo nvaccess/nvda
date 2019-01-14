f071a42c002f2d74eab72912d05ffbb48793b3a3
Łukasz Golonka <wulfryk1@gmail.com>
2019-01-14 12:30:55 +0100

    Review actions.

--- scons.py
+++ scons.py
@@ -11,11 +11,11 @@ requiredPythonArchitecture = "32bit"
 installedPythonMajor = str(sys.version_info.major)
 installedPythonMinor = str(sys.version_info.minor)
 installedPythonArchitecture = platform.architecture()[0]
-# Ensure, that we are running with Python 2.7 32-bit, otherwise inform the user and exit.
+# Ensure, that we are running with required version of Python, otherwise inform the user and exit.
 if installedPythonArchitecture != requiredPythonArchitecture or installedPythonMajor != requiredPythonMajor or installedPythonMinor != requiredPythonMinor:
 	print("This script is started with Python %s.%s %s, however to build NVDA you have to use Python %s.%s %s." %(installedPythonMajor, installedPythonMinor, installedPythonArchitecture, requiredPythonMajor, requiredPythonMinor, requiredPythonArchitecture))
 	print("Please install the needed version of Python and launch SCons again, or if you have mulltiple versions of Python installed start this script with required version explicitly.")
-	sys.exit()
+	sys.exit(1)
 sys.path.append(os.path.abspath(
 	os.path.join(__file__, "..", "source")))
 import sourceEnv
