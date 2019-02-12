86692c02b363eb6ea46fe503cd11e25e84034b36
Leonard de Ruijter <leonard@babbage.com>
2019-02-12 11:34:37 +0100

    Remove legacy Windows xp support from windowUtils

--- source/windowUtils.py
+++ source/windowUtils.py
@@ -51,14 +51,9 @@ try:
 	_logicalToPhysicalPoint = ctypes.windll.user32.LogicalToPhysicalPointForPerMonitorDPI
 	_physicalToLogicalPoint = ctypes.windll.user32.PhysicalToLogicalPointForPerMonitorDPI
 except AttributeError:
-	try:
-		# Windows Vista..Windows 8
-		_logicalToPhysicalPoint = ctypes.windll.user32.LogicalToPhysicalPoint
-		_physicalToLogicalPoint = ctypes.windll.user32.PhysicalToLogicalPoint
-	except AttributeError:
-		# Windows <= XP
-		_logicalToPhysicalPoint = None
-		_physicalToLogicalPoint = None
+	# Windows Vista..Windows 8
+	_logicalToPhysicalPoint = ctypes.windll.user32.LogicalToPhysicalPoint
+	_physicalToLogicalPoint = ctypes.windll.user32.PhysicalToLogicalPoint
 
 def logicalToPhysicalPoint(window, x, y):
 	"""Converts the logical coordinates of a point in a window to physical coordinates.
@@ -71,8 +66,6 @@ def logicalToPhysicalPoint(window, x, y):
 	@return: The physical x and y coordinates.
 	@rtype: tuple of (int, int)
 	"""
-	if not _logicalToPhysicalPoint:
-		return x, y
 	point = ctypes.wintypes.POINT(x, y)
 	_logicalToPhysicalPoint(window, ctypes.byref(point))
 	return point.x, point.y
@@ -88,8 +81,6 @@ def physicalToLogicalPoint(window, x, y):
 	@return: The logical x and y coordinates.
 	@rtype: tuple of (int, int)
 	"""
-	if not _physicalToLogicalPoint:
-		return x, y
 	point = ctypes.wintypes.POINT(x, y)
 	_physicalToLogicalPoint(window, ctypes.byref(point))
 	return point.x, point.y
