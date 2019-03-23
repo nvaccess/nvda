9adadee9356cfe94df5cd8767c60c576c6ced401
awalvie <agrawal.vishesh.178@gmail.com>
2019-03-23 11:12:04 +0530

    Added a condition that gives an error in the log file if the Directory is empty.

--- source/addonHandler/__init__.py
+++ source/addonHandler/__init__.py
@@ -180,8 +180,8 @@ def _getAvailableAddonsFromPath(path):
 	for p in os.listdir(path):
 		if p.endswith(DELETEDIR_SUFFIX): continue
 		addon_path = os.path.join(path, p)
-		if os.path.isdir(addon_path) and addon_path not in ('.', '..') ):
-			if (len(os.listdir(addon_path))):
+		if os.path.isdir(addon_path) and addon_path not in ('.', '..'):
+			if(len(os.listdir(addon_path))):
 				log.debug("Loading add-on from %s", addon_path)
 				try:
 					a = Addon(addon_path)
@@ -201,8 +201,8 @@ def _getAvailableAddonsFromPath(path):
 					yield a
 				except:
 					log.error("Error loading Addon from path: %s", addon_path, exc_info=True)
-			else(log.error("Addon Directory Emtpy at %s, please try reinstalling %s", addon_path, name)
-
+			else:
+				log.error("Error loading Addon from path: %s, try reinstalling: %s", addon_path, name,exc_info=True)
 _availableAddons = collections.OrderedDict()
 def getAvailableAddons(refresh=False, filterFunc=None):
 	""" Gets all available addons on the system.
