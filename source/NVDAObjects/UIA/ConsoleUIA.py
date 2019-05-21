b3565326e664eb9e1219bce5d96998f61c225300
Bill Dengler <codeofdusk@gmail.com>
2019-05-21 15:40:11 -0400

    Introduce basic automatic readout support, and rename ConsoleUIATextInfo->consoleUIATextInfo for consistency.
    See the source code comments in this commit for some steps still incomplete.

--- source/NVDAObjects/UIA/ConsoleUIA.py
+++ source/NVDAObjects/UIA/ConsoleUIA.py
@@ -12,9 +12,9 @@ from . import UIATextInfo
 from ..behaviors import Terminal
 
 
-class ConsoleUIATextInfo(UIATextInfo):
+class consoleUIATextInfo(UIATextInfo):
 	def __init__(self,obj,position,_rangeObj=None):
-		super(ConsoleUIATextInfo, self).__init__(obj,position,_rangeObj)
+		super(consoleUIATextInfo, self).__init__(obj,position,_rangeObj)
 		if position==textInfos.POSITION_CARET:
 			if winVersion.build >= 18362: #Windows 10 version 1903
 				# The UIA implementation in 1903 causes the caret to be off-by-one, so move it one position to the right to compensate.
@@ -22,4 +22,12 @@ class ConsoleUIATextInfo(UIATextInfo):
 
 
 class consoleUIA(Terminal):
-	_TextInfo=ConsoleUIATextInfo
+	_TextInfo=consoleUIATextInfo
+	def event_textChanged(self):
+		# fire textChange for liveText
+		#Todo: this is probably a good place to filter out extraneous events from user input
+		self.event_textChange()
+	def _getTextLines(self):
+		# Filter out extraneous empty lines from UIA
+		#Todo: do this (also) somewhere else so they aren't in document review either
+		return self.makeTextInfo(textInfos.POSITION_ALL).text.strip().split("\r\n")
