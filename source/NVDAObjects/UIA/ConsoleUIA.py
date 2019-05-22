3b7ab8820ee11816f15df6b1f3e9d4fa65565787
Bill Dengler <codeofdusk@gmail.com>
2019-05-22 01:25:44 -0400

    Cleanup.

--- source/NVDAObjects/UIA/ConsoleUIA.py
+++ source/NVDAObjects/UIA/ConsoleUIA.py
@@ -27,23 +27,28 @@ class consoleUIA(Terminal):
 	_TextInfo=consoleUIATextInfo
 	_isTyping=False
 	_lastCharTime=0
+	
 	def _reportNewText(self, line):
 		# Additional typed character filtering beyond that in LiveText
 		if self._isTyping and time.time()-self._lastCharTime <= 2:
 			return
 		super(consoleUIA, self)._reportNewText(line)
+	
 	def event_textChanged(self):
 		# fire textChange for liveText
 		self.event_textChange()
+	
 	def event_typedCharacter(self, ch):
 		if len(''.join(ch.split())) > 0:
 			self._isTyping=True
 		self._lastCharTime=time.time()
 		super(consoleUIA, self).event_typedCharacter(ch)
+	
 	@script(gestures=["kb:enter","kb:numpadEnter"])
 	def script_clear_isTyping(self, gesture):
 		gesture.send()
 		self._isTyping=False
+	
 	def _getTextLines(self):
 		# Filter out extraneous empty lines from UIA
 		#Todo: do this (also) somewhere else so they aren't in document review either
