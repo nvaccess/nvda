6dc4ad8ba46b20b37744938a8f2142dddb0da7d0
Joseph Lee <joseph.lee22590@gmail.com>
2019-05-25 09:36:29 -0700

    Python 3: file() -> open() in NVDA helper and watchdog. Re #9038.
    Even in Python 2, open() function is used, and in Python 3, file() is gone. Thus unify under open() with explanatory comments added.

--- source/NVDAHelper.py
+++ source/NVDAHelper.py
@@ -410,7 +410,8 @@ class RemoteLoader64(object):
 		pipeRead = self._duplicateAsInheritable(pipeReadOrig)
 		winKernel.closeHandle(pipeReadOrig)
 		# stdout/stderr of the loader process should go to nul.
-		with file("nul", "w") as nul:
+		# #9038 (Py3 review required): File() function is gone, replaced by open().
+		with open("nul", "w") as nul:
 			nulHandle = self._duplicateAsInheritable(msvcrt.get_osfhandle(nul.fileno()))
 		# Set the process to start with the appropriate std* handles.
 		si = winKernel.STARTUPINFO(dwFlags=winKernel.STARTF_USESTDHANDLES, hSTDInput=pipeRead, hSTDOutput=nulHandle, hSTDError=nulHandle)
