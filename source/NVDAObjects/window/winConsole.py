0cf311e9301f04202e6f1ef67ddbf9a499f1cc8c
Bill Dengler <codeofdusk@gmail.com>
2019-05-31 10:14:30 -0400

    Add winConsole compatibility module.

---
+++ source/NVDAObjects/window/winConsole.py
@@ -0,0 +1,11 @@
+# NVDAObjects/window/winConsole.py
+# A part of NonVisual Desktop Access (NVDA)
+# This file is covered by the GNU General Public License.
+# See the file COPYING for more details.
+# Copyright (C) 2019 Bill Dengler
+
+import warnings
+from ..IAccessible.winConsoleLegacy import winConsoleLegacy as WinConsole
+
+warnings.warn("NVDAObjects.window.winConsole is deprecated. Use NVDAObjects.IAccessible.winConsoleLegacy or NVDAObjects.UIA.winConsoleUIA instead",
+	DeprecationWarning, stacklevel=3)
