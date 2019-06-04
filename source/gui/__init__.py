02363062db9e7f9ed1b27018e1b1c3a4fdb15937
Joseph Lee <joseph.lee22590@gmail.com>
2019-06-04 09:15:34 -0700

    GUI: relative imports. Re #8712.

--- source/gui/__init__.py
+++ source/gui/__init__.py
@@ -329,7 +329,7 @@ class MainFrame(wx.Frame):
 		if isInMessageBox:
 			return
 		self.prePopup()
-		from addonGui import AddonsDialog
+		from .addonGui import AddonsDialog
 		d=AddonsDialog(gui.mainFrame)
 		d.Show()
 		self.postPopup()
@@ -388,7 +388,7 @@ class MainFrame(wx.Frame):
 		if isInMessageBox:
 			return
 		self.prePopup()
-		from configProfiles import ProfilesDialog
+		from .configProfiles import ProfilesDialog
 		ProfilesDialog(gui.mainFrame).Show()
 		self.postPopup()
 
