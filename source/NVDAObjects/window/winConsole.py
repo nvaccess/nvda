617f176fda59424a1939feda93c0c2bff5a1ae92
Bill Dengler <codeofdusk@gmail.com>
2019-06-05 02:39:32 -0400

    Revert legacy console rename.

--- source/NVDAObjects/window/winConsole.py
+++ source/NVDAObjects/window/winConsole.py
@@ -1,11 +1,76 @@
-# NVDAObjects/window/winConsole.py
-# A part of NonVisual Desktop Access (NVDA)
-# This file is covered by the GNU General Public License.
-# See the file COPYING for more details.
-# Copyright (C) 2019 Bill Dengler
+#NVDAObjects/WinConsole.py
+#A part of NonVisual Desktop Access (NVDA)
+#This file is covered by the GNU General Public License.
+#See the file COPYING for more details.
+#Copyright (C) 2007-2012 NV Access Limited
 
-import warnings
-from ..IAccessible.winConsoleLegacy import WinConsoleLegacy as WinConsole
+import winConsoleHandler
+from . import Window
+from ..behaviors import Terminal, EditableTextWithoutAutoSelectDetection
+import api
+import core
 
-warnings.warn("NVDAObjects.window.winConsole is deprecated. Use NVDAObjects.IAccessible.WinConsoleLegacy or NVDAObjects.UIA.winConsoleUIA instead",
-	DeprecationWarning, stacklevel=3)
+class WinConsole(Terminal, EditableTextWithoutAutoSelectDetection, Window):
+	"""
+		NVDA's legacy Windows Console support.
+		This is used in situations where UIA isn't available.
+		Please consider using NVDAObjects.UIA.winConsoleUIA instead.
+	"""
+	STABILIZE_DELAY = 0.03
+
+	def _get_TextInfo(self):
+		consoleObject=winConsoleHandler.consoleObject
+		if consoleObject and self.windowHandle == consoleObject.windowHandle:
+			return winConsoleHandler.WinConsoleTextInfo
+		return super(WinConsole,self).TextInfo
+
+	def event_becomeNavigatorObject(self, isFocus=False):
+		if winConsoleHandler.consoleObject is not self:
+			if winConsoleHandler.consoleObject:
+				winConsoleHandler.disconnectConsole()
+			winConsoleHandler.connectConsole(self)
+			if self == api.getFocusObject():
+				# The user is returning to the focus object with object navigation.
+				# The focused console should always be monitored if possible.
+				self.startMonitoring()
+		super(WinConsole,self).event_becomeNavigatorObject(isFocus=isFocus)
+
+	def event_gainFocus(self):
+		if winConsoleHandler.consoleObject is not self:
+			if winConsoleHandler.consoleObject:
+				winConsoleHandler.disconnectConsole()
+			winConsoleHandler.connectConsole(self)
+		super(WinConsole, self).event_gainFocus()
+
+	def event_loseFocus(self):
+		super(WinConsole, self).event_loseFocus()
+		if winConsoleHandler.consoleObject is self:
+			winConsoleHandler.disconnectConsole()
+
+	def event_nameChange(self):
+		pass
+
+	def _getTextLines(self):
+		return winConsoleHandler.getConsoleVisibleLines()
+
+	def script_caret_backspaceCharacter(self, gesture):
+		super(WinConsole, self).script_caret_backspaceCharacter(gesture)
+		# #2586: We use console update events for typed characters,
+		# so the typedCharacter event is never fired for the backspace key.
+		# Call it here so that speak typed words works as expected.
+		self.event_typedCharacter(u"\b")
+
+	def script_close(self,gesture):
+		# #5343: New consoles in Windows 10 close with alt+f4 and take any processes attached with it (including NVDA).
+		# Therefore detach from the console temporarily while sending the gesture. 
+		winConsoleHandler.disconnectConsole()
+		gesture.send()
+		def reconnect():
+			if api.getFocusObject()==self:
+				winConsoleHandler.connectConsole(self)
+				self.startMonitoring()
+		core.callLater(200,reconnect)
+
+	__gestures={
+		"kb:alt+f4":"close",
+	}
--- source/wincon.py
+++ source/wincon.py
@@ -1,11 +1,96 @@
-# NVDAObjects/wincon.py
-# A part of NonVisual Desktop Access (NVDA)
-# This file is covered by the GNU General Public License.
-# See the file COPYING for more details.
-# Copyright (C) 2019 Bill Dengler
+from ctypes import *
+from ctypes.wintypes import *
 
-import warnings
-from winconLegacy import *
+"""
+Lower level utility functions and constants for NVDA's
+legacy Windows console support, for situations where UIA isn't available.
+"""
 
-warnings.warn("wincon is deprecated. Use winconLegacy instead, and consider switching to the UIA console implementation.",
-	DeprecationWarning, stacklevel=3)
+CONSOLE_REAL_OUTPUT_HANDLE=-2
+
+class COORD(Structure):
+	_fields_=[
+		('x',c_short),
+		('y',c_short),
+	]
+
+class CONSOLE_SCREEN_BUFFER_INFO(Structure):
+	_fields_=[
+		('dwSize',COORD),
+		('dwCursorPosition',COORD),
+		('wAttributes',WORD),
+		('srWindow',SMALL_RECT),
+		('dwMaximumWindowSize',COORD),
+	]
+
+class CONSOLE_SELECTION_INFO(Structure):
+	_fields_=[
+		('dwFlags',DWORD),
+		('dwSelectionAnchor',COORD),
+		('srSelection',SMALL_RECT),
+	]
+
+class CHAR_INFO(Structure):
+	_fields_ = [
+		('Char', c_wchar), #union of char and wchar_t isn't needed since we deal only with unicode
+		('Attributes', WORD)
+	]
+
+PHANDLER_ROUTINE=WINFUNCTYPE(BOOL,DWORD)
+
+CTRL_C_EVENT=0
+CTRL_BREAK_EVENT=1
+CTRL_CLOSE_EVENT=2
+
+CONSOLE_NO_SELECTION=0X0
+CONSOLE_SELECTION_IN_PROGRESS=0X1
+CONSOLE_SELECTION_NOT_EMPTY=0x2
+CONSOLE_MOUSE_SELECTION=0X4
+CONSOLE_MOUSE_DOWN=0x8
+
+def GetConsoleSelectionInfo():
+	info=CONSOLE_SELECTION_INFO()
+	if windll.kernel32.GetConsoleSelectionInfo(byref(info))==0:
+		raise WinError()
+	return info
+
+def ReadConsoleOutputCharacter(handle,length,x,y):
+	buf=create_unicode_buffer(length)
+	numCharsRead=c_int()
+	if windll.kernel32.ReadConsoleOutputCharacterW(handle,buf,length,COORD(x,y),byref(numCharsRead))==0:
+		raise WinError()
+	return buf.value
+
+def ReadConsoleOutput(handle, length, rect):
+	BufType=CHAR_INFO*length
+	buf=BufType()
+	#rect=SMALL_RECT(x, y, x+length-1, y)
+	if windll.kernel32.ReadConsoleOutputW(handle, buf, COORD(rect.Right-rect.Left+1, rect.Bottom-rect.Top+1), COORD(0,0), byref(rect))==0:
+		raise WinError()
+	return buf
+
+def GetConsoleScreenBufferInfo(handle):
+	info=CONSOLE_SCREEN_BUFFER_INFO()
+	if windll.kernel32.GetConsoleScreenBufferInfo(handle,byref(info))==0:
+		raise WinError()
+	return info
+
+def FreeConsole():
+	if windll.kernel32.FreeConsole()==0:
+		raise WinError()
+
+def AttachConsole(processID):
+	if windll.kernel32.AttachConsole(processID)==0:
+		raise WinError()
+
+def GetConsoleWindow():
+	return windll.kernel32.GetConsoleWindow()
+
+def GetConsoleProcessList(maxProcessCount):
+	processList=(c_int*maxProcessCount)()
+	num=windll.kernel32.GetConsoleProcessList(processList,maxProcessCount)
+	return processList[0:num]
+
+def SetConsoleCtrlHandler(handler,add):
+	if windll.kernel32.SetConsoleCtrlHandler(handler,add)==0:
+		raise WinError()
