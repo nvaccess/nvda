8b82c5d5a13ce8d140e51f1e79be5573a705843a
Joseph Lee <joseph.lee22590@gmail.com>
2019-06-07 20:45:14 -0700

    Python 3: use importlib for importing modules (#9693)
    * IAccessible objects/Python 3: use importlib. Re #8768.
    
    * App module handler/Python 3: use importlib. Re #8768.
    
    * Braille display drivers/Python 3: use importlib. Re #8768.
    
    * Global plugin handler/Python 3: use importlib. Re #8768.
    
    * Synth drivers/Python 3: use importlib. Re #8768.

--- source/NVDAObjects/IAccessible/__init__.py
+++ source/NVDAObjects/IAccessible/__init__.py
@@ -10,6 +10,7 @@ import ctypes
 import os
 import re
 import itertools
+import importlib
 from comInterfaces.tom import ITextDocument
 import tones
 import languageHandler
@@ -438,7 +439,8 @@ the NVDAObject for IAccessible
 			if classString and classString.find('.')>0:
 				modString,classString=os.path.splitext(classString)
 				classString=classString[1:]
-				mod=__import__(modString,globals(),locals(),[])
+				# #8712: Python 3 wants a dot (.) when loading a module from the same folder via relative imports, and this is done via package argument.
+				mod=importlib.import_module("NVDAObjects.IAccessible.%s"%modString, package="NVDAObjects.IAccessible")
 				newCls=getattr(mod,classString)
 			elif classString:
 				newCls=globals()[classString]
