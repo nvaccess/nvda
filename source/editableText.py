acb50568d9cd885d881126fb47869d5f73670b71
Leonard de Ruijter <leonard@babbage.com>
2019-07-01 15:49:55 +0200

    Clear the current words buffer in more cases

--- source/editableText.py
+++ source/editableText.py
@@ -127,6 +127,8 @@ class EditableText(TextContainerObject,ScriptableObject):
 				info = self.makeTextInfo(textInfos.POSITION_CARET)
 			except:
 				return
+		# Forget the word currently being typed as the user has moved the caret somewhere else.
+		speech.clearTypedWordBuffer()
 		review.handleCaretMove(info)
 		if speakUnit and not willSayAllResume(gesture):
 			info.expand(speakUnit)
@@ -134,8 +136,6 @@ class EditableText(TextContainerObject,ScriptableObject):
 		braille.handler.handleCaretMove(self)
 
 	def _caretMovementScriptHelper(self, gesture, unit):
-		# Forget the word currently being typed as the user is moving the caret somewhere else.
-		speech.clearTypedWordBuffer()
 		try:
 			info=self.makeTextInfo(textInfos.POSITION_CARET)
 		except:
