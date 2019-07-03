2c27c4b438f887f44a35ebaafbadc24f9942245a
Leonard de Ruijter <leonard@babbage.com>
2019-07-03 09:02:24 +0200

    Add vision to the python console namespace

--- source/pythonConsole.py
+++ source/pythonConsole.py
@@ -153,6 +153,7 @@ class PythonConsole(code.InteractiveConsole, AutoPropertyObject):
 		import controlTypes
 		import globalPlugins
 		import textInfos
+		import vision
 		self.namespace.clear()
 		self.namespace.update({
 			"help": HelpCommand(),
@@ -171,6 +172,7 @@ class PythonConsole(code.InteractiveConsole, AutoPropertyObject):
 			"queueHandler": queueHandler,
 			"speech": speech,
 			"textInfos": textInfos,
+			"vision": vision,
 		})
 
 	def updateNamespaceSnapshotVars(self):
