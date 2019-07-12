nvaccess/nvda

commit 74431b8bc894b9b3e887e589f7b40efc6f66588a

Python 3: Fix usages of "open" function (PR #9853)
Searched for, inspected, and fixed usages of "open" function
Used `git grep "\Wopen("` to search of usages of "open"

Fixes #9838

Python can open a file in binary mode and in text mode. In python 2, this difference was less distinct than in Python 3. In python 3, reading a file in text mode returns unicode strings, whereas binary mode returns bytes objects. Therefore, a call of open without supplying proper arguments would open a file in text mode with the default system encoding assumed. 

1. #9838: wrong encoding assumed when decoding add-on manifests. As open didn't have an encoding specified, the default text encoding of the system was specified. This failed for Japanese systems.
2. To read Espeak VARIANT DATA, NVDA parses files in the espeak data folder. For this, the open function was also used without an encoding. This resulted in the inability to load espeak on systems where the beta option "Use Unicode UTF-8 for worldwide language support" was enabled.
3. To open calls in NVDAHelper and watchdog are used to open a file without actually writing or reading to it using Python directly. These files are now opened in binary mode. While not strictly necessary, this makes it clearer from a code perspective that we aren't dealing with Python's ability to write or read encoded text files.
4. The logViewer now consistently uses open instead of codecs.open

source/synthDrivers/_espeak.py
@@ -366,16 +366,23 @@ def getVariantDict():
 	# Translators: name of the default espeak varient.
 	variantDict={"none": pgettext("espeakVarient", "none")}
 	for fileName in os.listdir(dir):
-		if os.path.isfile("%s\\%s"%(dir,fileName)):
-			file=codecs.open("%s\\%s"%(dir,fileName))
-			for line in file:
-				if line.startswith('name '):
-					temp=line.split(" ")
-					if len(temp) ==2:
-						name=temp[1].rstrip()
-						break
-				name=None
-			file.close()
+		absFilePath = os.path.join(dir, fileName)
+		if os.path.isfile(absFilePath):
+			# In python 3, open assumes the default system encoding by default.
+			# This fails if Windows' "use Unicode UTF-8 for worldwide language support" option is enabled.
+			# The expected encoding is unknown, therefore use latin-1 to stay as close to Python 2 behavior as possible.
+			try:
+				with open(absFilePath, 'r', encoding="latin-1") as file:
+					for line in file:
+						if line.startswith('name '):
+							temp=line.split(" ")
+							if len(temp) ==2:
+								name=temp[1].rstrip()
+								break
+					name=None
+			except:
+				log.error("Couldn't parse espeak variant file %s" % fileName, exc_info=True)
+				continue
 		if name is not None:
 			variantDict[fileName]=name
 	return variantDict

source/NVDAHelper.py
@@ -427,8 +427,9 @@ class RemoteLoader64(object):
 		pipeRead = self._duplicateAsInheritable(pipeReadOrig)
 		winKernel.closeHandle(pipeReadOrig)
 		# stdout/stderr of the loader process should go to nul.
-		# #9038 (Py3 review required): File() function is gone, replaced by open().
-		with open("nul", "w") as nul:
+		# Though we aren't using pythonic functions to write to nul,
+		# open it in binary mode as opening it in text mode (the default) doesn't make sense.
+		with open("nul", "wb") as nul:
 			nulHandle = self._duplicateAsInheritable(msvcrt.get_osfhandle(nul.fileno()))
 		# Set the process to start with the appropriate std* handles.
 		si = winKernel.STARTUPINFO(dwFlags=winKernel.STARTF_USESTDHANDLES, hSTDInput=pipeRead, hSTDOutput=nulHandle, hSTDError=nulHandle)

source/addonHandler/__init__.py
@@ -283,13 +283,13 @@ class Addon(AddonBase):
 		self.path = os.path.abspath(path)
 		self._extendedPackages = set()
 		manifest_path = os.path.join(path, MANIFEST_FILENAME)
-		with open(manifest_path) as f:
+		with open(manifest_path, 'r', encoding="utf_8") as f:
 			translatedInput = None
 			for translatedPath in _translatedManifestPaths():
 				p = os.path.join(self.path, translatedPath)
 				if os.path.exists(p):
 					log.debug("Using manifest translation from %s", p)
-					translatedInput = open(p, 'r')
+					translatedInput = open(p, 'r', encoding="utf_8")
 					break
 			self.manifest = AddonManifest(f, translatedInput)
 
@@ -564,11 +564,18 @@ class AddonBundle(AddonBase):
 		with zipfile.ZipFile(self._path, 'r') as z:
 			for translationPath in _translatedManifestPaths(forBundle=True):
 				try:
+					# ZipFile.open opens every file in binary mode.
+					# decoding is handled by configobj.
 					translatedInput = z.open(translationPath, 'r')
 					break
 				except KeyError:
 					pass
-			self._manifest = AddonManifest(z.open(MANIFEST_FILENAME), translatedInput=translatedInput)
+			self._manifest = AddonManifest(
+				# ZipFile.open opens every file in binary mode.
+				# decoding is handled by configobj.
+				z.open(MANIFEST_FILENAME, 'r'),
+				translatedInput=translatedInput
+			)
 			if self.manifest.errors is not None:
 				_report_manifest_errors(self.manifest)
 				raise AddonError("Manifest file has errors.")
@@ -609,7 +616,7 @@ def createAddonBundleFromPath(path, destDir=None):
 	manifest_path = os.path.join(basedir, MANIFEST_FILENAME)
 	if not os.path.isfile(manifest_path):
 		raise AddonError("Can't find %s manifest file." % manifest_path)
-	with open(manifest_path) as f:
+	with open(manifest_path, 'r', encoding="utf_8") as f:
 		manifest = AddonManifest(f)
 	if manifest.errors is not None:
 		_report_manifest_errors(manifest)

source/gui/logViewer.py
@@ -7,7 +7,6 @@
 """Provides functionality to view the NVDA log.
 """
 
-import codecs
 import wx
 import globalVars
 import gui
@@ -55,7 +54,7 @@ class LogViewer(wx.Frame):
 		pos = self.outputCtrl.GetInsertionPoint()
 		# Append new text to the output control which has been written to the log file since the last refresh.
 		try:
-			f = codecs.open(globalVars.appArgs.logFileName, "r", encoding="UTF-8")
+			f = open(globalVars.appArgs.logFileName, "r", encoding="UTF-8")
 			f.seek(self._lastFilePos)
 			self.outputCtrl.AppendText(f.read())
 			self._lastFilePos = f.tell()
@@ -78,8 +77,6 @@ class LogViewer(wx.Frame):
 		if not filename:
 			return
 		try:
-			# codecs.open() forces binary mode, which is bad under Windows because line endings won't be converted to crlf automatically.
-			# Therefore, do the encoding manually.
 			# #9038: work with UTF-8 from the start.
 			with open(filename, "w", encoding="UTF-8") as f:
 				f.write(self.outputCtrl.GetValue())

source/watchdog.py
@@ -153,8 +153,9 @@ def _crashHandler(exceptionInfo):
 	# Write a minidump.
 	dumpPath = os.path.abspath(os.path.join(globalVars.appArgs.logFileName, "..", "nvda_crash.dmp"))
 	try:
-		# #9038 (Py3 review required): file() function is gone, replaced by open().
-		with open(dumpPath, "w") as mdf:
+		# Though we aren't using pythonic functions to write to the dump file,
+		# open it in binary mode as opening it in text mode (the default) doesn't make sense.
+		with open(dumpPath, "wb") as mdf:
 			mdExc = MINIDUMP_EXCEPTION_INFORMATION(ThreadId=threadId,
 				ExceptionPointers=exceptionInfo, ClientPointers=False)
 			if not ctypes.windll.DbgHelp.MiniDumpWriteDump(

commit ed15de9961ad6866fa7477e3dd32382d9d3e35ad

Python 3: Fix loading of espeak variants (PR #9920)
Fixes mistake introduced in #9853

source/synthDrivers/_espeak.py
@@ -377,7 +377,7 @@ def getVariantDict():
 							if len(temp) ==2:
 								name=temp[1].rstrip()
 								break
-					name=None
+						name=None
 			except:
 				log.error("Couldn't parse espeak variant file %s" % fileName, exc_info=True)
 				continue