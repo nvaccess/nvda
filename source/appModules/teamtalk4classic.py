c84128eefcfea954e2145df0bbc3afe00d0b95fc
Bill Dengler <codeofdusk@gmail.com>
2019-08-11 14:37:32 -0400

    Style.

--- source/appModules/teamtalk4classic.py
+++ source/appModules/teamtalk4classic.py
@@ -1,3 +1,3 @@
 # The appModule we need lives in appModules.teamtalk5classic.
 # We don't actually use it here, hence the noqa.
-from .teamtalk5classic import AppModule #noqa F401
+from .teamtalk5classic import AppModule # noqa F401
