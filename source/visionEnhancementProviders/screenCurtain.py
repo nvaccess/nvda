d9683c40b8ae2434e6d931f7b7c02e7fbf15aed5
Leonard de Ruijter <alderuijter@gmail.com>
2019-08-13 18:19:38 +0200

    Remove winMagnification module, just store everything in one file and remove all stuff we don't need

--- source/visionEnhancementProviders/screenCurtain.py
+++ source/visionEnhancementProviders/screenCurtain.py
@@ -1,4 +1,3 @@
-#visionEnhancementProviders/screenCurtain.py
 #A part of NonVisual Desktop Access (NVDA)
 #This file is covered by the GNU General Public License.
 #See the file COPYING for more details.
@@ -10,15 +9,48 @@ This implementation only works on Windows 8 and above.
 
 import vision
 import winVersion
-from ctypes import byref
+from ctypes import Structure, windll, byref, c_int, c_float, POINTER, WINFUNCTYPE, WinError
+from ctypes.wintypes import BOOL, HWND, RECT, DWORD
 from collections import OrderedDict
-try:
-	import winMagnification
-except AttributeError:
-	winMagnification = None
-else:
-	TRANSFORM_BLACK = winMagnification.MAGCOLOREFFECT()
-	TRANSFORM_BLACK.transform[4][4] = 1.0
+from comtypes import GUID
+
+class MAGCOLOREFFECT(Structure):
+	_fields_ = (("transform", c_float*5*5),)
+
+TRANSFORM_BLACK = MAGCOLOREFFECT()
+TRANSFORM_BLACK.transform[4][4] = 1.0
+
+def _errCheck(result, func, args):
+	if result == 0:
+		raise WinError()
+	return args
+
+
+class Magnification:
+	"""Singleton that wraps necessary functions from the Windows magnification API."""
+
+	_magnification = windll.Magnification
+
+	_MagInitializeFuncType = WINFUNCTYPE(BOOL)
+	_MagUninitializeFuncType = WINFUNCTYPE(BOOL)
+	_MagSetFullscreenColorEffectFuncType = WINFUNCTYPE(BOOL, POINTER(MAGCOLOREFFECT))
+	_MagSetFullscreenColorEffectArgTypes = ((1, "effect"),)
+	_MagGetFullscreenColorEffectFuncType = WINFUNCTYPE(BOOL, POINTER(MAGCOLOREFFECT))
+	_MagGetFullscreenColorEffectArgTypes = ((2, "effect"),)
+
+	MagInitialize = _MagInitializeFuncType(("MagInitialize", _magnification))
+	MagInitialize.errcheck = _errCheck
+	MagUninitialize = _MagUninitializeFuncType(("MagUninitialize", _magnification))
+	MagUninitialize.errcheck = _errCheck
+	try:
+		MagSetFullscreenColorEffect = _MagSetFullscreenColorEffectFuncType(("MagSetFullscreenColorEffect", _magnification), _MagSetFullscreenColorEffectArgTypes )
+		MagSetFullscreenColorEffect.errcheck = _errCheck
+		MagGetFullscreenColorEffect = _MagGetFullscreenColorEffectFuncType(("MagGetFullscreenColorEffect", _magnification), _MagGetFullscreenColorEffectArgTypes)
+		MagGetFullscreenColorEffect.errcheck = _errCheck
+	except AttributeError:
+		MagSetFullscreenColorEffect = None
+		MagGetFullscreenColorEffect = None
+
 
 class VisionEnhancementProvider(vision.providerBase.VisionEnhancementProvider):
 	name = "screenCurtain"
@@ -28,17 +60,17 @@ class VisionEnhancementProvider(vision.providerBase.VisionEnhancementProvider):
 	supportedRoles = frozenset([vision.constants.Role.COLORENHANCER])
 
 	@classmethod
-	def check(cls):
+	def canStart(cls):
 		return winVersion.isFullScreenMagnificationAvailable()
 
 	def __init__(self):
 		super(VisionEnhancementProvider, self).__init__()
-		winMagnification.MagInitialize()
-		winMagnification.MagSetFullscreenColorEffect(byref(TRANSFORM_BLACK))
+		Magnification.MagInitialize()
+		Magnification.MagSetFullscreenColorEffect(TRANSFORM_BLACK)
 
 	def terminate(self):
 		super(VisionEnhancementProvider, self).terminate()
-		winMagnification.MagUninitialize()
+		Magnification.MagUninitialize()
 
 	def registerEventExtensionPoints(self, extensionPoints):
 		# The screen curtain isn't interested in any events
