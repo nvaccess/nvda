eafb125343b1bee3e647ae85dc625e67c1a28576
Joseph Lee <joseph.lee22590@gmail.com>
2019-08-13 23:25:36 -0700

    WWAHost: lint fixes.
    Lint fixes include splitting long lines and comment style.

--- source/appModules/wwahost.py
+++ source/appModules/wwahost.py
@@ -1,7 +1,7 @@
-#A part of NonVisual Desktop Access (NVDA)
-#This file is covered by the GNU General Public License.
-#See the file COPYING for more details.
-#Copyright (C) 2012-2019 NV Access Limited, Joseph Lee
+# A part of NonVisual Desktop Access (NVDA)
+# This file is covered by the GNU General Public License.
+# See the file COPYING for more details.
+# Copyright (C) 2012-2019 NV Access Limited, Joseph Lee
 
 """App module host for Windows 8.x and 10 apps hosted by wwahost.exe.
 In Windows 8, apps written in Javascript are executed inside WWAHost, including some WinRT apps.
@@ -18,10 +18,13 @@ import controlTypes
 import winUser
 import winKernel
 
+
 def getAppNameFromHost(processId):
 	# Some apps that come with Windows 8 and 8.1 are hosted by wwahost.exe.
 	# App modules for these are named after the hosted app name.
-	processHandle = winKernel.openProcess(winKernel.SYNCHRONIZE|winKernel.PROCESS_QUERY_INFORMATION,False,processId)
+	processHandle = winKernel.openProcess(
+		winKernel.SYNCHRONIZE | winKernel.PROCESS_QUERY_INFORMATION, False, processId
+	)
 	length = ctypes.c_uint()
 	winKernel.kernel32.GetApplicationUserModelId(processHandle, ctypes.byref(length), None)
 	appModel = ctypes.create_unicode_buffer(length.value)
@@ -30,7 +33,8 @@ def getAppNameFromHost(processId):
 	# Sometimes app model might be empty, so raise errors and fall back to wwahost.
 	if not appModel.value:
 		raise LookupError
-	# App model is shown as familyName!appName, and importing files with the exclamation point in the middle of the name isn't supported.
+	# App model is shown as familyName!appName,
+	# and importing files with the exclamation point in the middle of the name isn't supported.
 	# Therefore return only the app name portion.
 	# Convert this into lowercase to make the file name consistent with other NVDA app modules.
 	return appModel.value.split("!")[-1].lower()
