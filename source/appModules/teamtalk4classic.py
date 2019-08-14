cab9d0b8eccb1acceff952d3fa955dc97aed94ab
Bill Dengler <codeofdusk@gmail.com>
2019-08-14 05:03:33 -0400

    Update TeamTalk 4 appModule.

--- source/appModules/teamtalk4classic.py
+++ source/appModules/teamtalk4classic.py
@@ -1,3 +1,10 @@
+# appModules/teamtalk4classic.py
+# A part of NonVisual Desktop Access (NVDA)
+# Copyright (C) 2019 Bill Dengler
+# This file is covered by the GNU General Public License.
+# See the file COPYING for more details.
+
 # The appModule we need lives in appModules.teamtalk5classic.
 # We don't actually use it here, hence the noqa.
+# We just import it for compatibility.
 from .teamtalk5classic import AppModule # noqa F401
