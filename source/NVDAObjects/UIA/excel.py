3ecc0431e69a3644ea307c3ee5e8be01f805dbb4
Michael Curran <mick@nvaccess.org>
2019-09-19 15:12:11 +1000

    Address linting issues. Remove UIASpreadsheetItemPattern as it is deprecated.

--- source/NVDAObjects/UIA/excel.py
+++ source/NVDAObjects/UIA/excel.py
@@ -1,40 +1,43 @@
-#A part of NonVisual Desktop Access (NVDA)
-#This file is covered by the GNU General Public License.
-#See the file COPYING for more details.
-#Copyright (C) 2018 NV Access Limited
+# A part of NonVisual Desktop Access (NVDA)
+# This file is covered by the GNU General Public License.
+# See the file COPYING for more details.
+# Copyright (C) 2018 NV Access Limited
 
 import controlTypes
-import UIAHandler
 from . import UIA
+
 
 class ExcelCell(UIA):
 
-	name=u""
-	role=controlTypes.ROLE_TABLECELL
-	rowHeaderText=None
-	columnHeaderText=None
+	name = u""
+	role = controlTypes.ROLE_TABLECELL
+	rowHeaderText = None
+	columnHeaderText = None
 
 	def _get_description(self):
 		return self.UIAElement.currentItemStatus
 
 	def _get_cellCoordsText(self):
-		name=super(ExcelCell,self).name
+		name = super().name
 		# Later builds of Excel 2016 quote the letter coordinate.
 		# We don't want the quotes.
-		name=name.replace('"','')
+		name = name.replace('"', '')
 		return name
 
+
 class ExcelWorksheet(UIA):
-	role=controlTypes.ROLE_TABLE
+	role = controlTypes.ROLE_TABLE
 
 	def _get_name(self):
-		return super(ExcelWorksheet,self).parent.name
+		return super().parent.name
 
 	def _get_parent(self):
-		return super(ExcelWorksheet,self).parent.parent
+		return super().parent.parent
+
 
 class CellEdit(UIA):
-	name=u""
+	name = u""
+
 
 class BadExcelFormulaEdit(UIA):
-	shouldAllowUIAFocusEvent=False
+	shouldAllowUIAFocusEvent = False
