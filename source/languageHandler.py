6fa4a8ce230cd0ac45d87cad100a2abd14dbad62
Cyrille Bougot <cyrille.bougot2@laposte.net>
2019-10-11 14:15:56 +0200

    Clean up and simplify code

--- source/languageHandler.py
+++ source/languageHandler.py
@@ -101,18 +101,11 @@ def getAvailableLanguages(presentational=False):
 	if 'en' not in locales:
 		locales.append('en')
 		locales.sort()
-	#For each locale, ask Windows for its human readable display name
-	displayNames = []
-	for entry in locales:
-		desc=getLanguageDescription(entry)
-		displayNames.append(desc)
-	#Prepare a zipped view of language codes and descriptions.
-	# #7284: especially for sorting by description.
-	# Python 3: zip function changed from returning a list to an iterator, thus wrap this inside a list call.
-	langs = list(zip(locales,displayNames))
+	# Prepare a 2-tuple list of language code and human readable language description.
+	langs = [(lc, getLanguageDescription(lc)) for lc in locales]
 	if presentational:
 		langs.sort(key=lambda lang: locale.strxfrm(lang[1] if lang[1] else lang[0]))
-	langs = [(loc, (f"{name}, {loc}" if name else loc)) for loc, name in langs]
+	langs = [(lc, (f"{desc}, {lc}" if desc else lc)) for lc, desc in langs]
 	#include a 'user default, windows' language, which just represents the default language for this user account
 	langs.append(("Windows",
 		# Translators: the label for the Windows default NVDA interface language.
