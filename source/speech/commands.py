86c7df987a908b9ca44972ca41bcc987d43228c4
Reef Turner <reef@nvaccess.org>
2019-10-14 18:19:31 +0200

    Passing None to LangChangeCommand is accepted

--- source/speech/commands.py
+++ source/speech/commands.py
@@ -7,6 +7,8 @@
 """Commands that can be embedded in a speech sequence for changing synth parameters, playing sounds or running other callbacks."""
  
 from abc import ABCMeta, abstractmethod
+from typing import Optional
+
 import config
 import languageHandler
 from synthDriverHandler import getSynth
@@ -69,12 +71,11 @@ class CharacterModeCommand(SynthParamCommand):
 class LangChangeCommand(SynthParamCommand):
 	"""A command to switch the language within speech."""
 
-	def __init__(self,lang):
+	def __init__(self, lang: Optional[str]):
 		"""
 		@param lang: the language to switch to: If None then the NVDA locale will be used.
-		@type lang: string
 		"""
-		self.lang=lang # if lang else languageHandler.getLanguage()
+		self.lang = lang
 		self.isDefault = not lang
 
 	def __repr__(self):
