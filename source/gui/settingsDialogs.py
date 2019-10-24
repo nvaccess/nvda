c220dbd78c022a82b544a27a7ab2a433ddf9536e
Leonard de Ruijter <alderuijter@gmail.com>
2019-10-24 19:47:41 +0200

    Add articles to gui and globalCommands

--- source/gui/settingsDialogs.py
+++ source/gui/settingsDialogs.py
@@ -1920,6 +1920,11 @@ class DocumentFormattingPanel(SettingsPanel):
 
 		# Translators: This is the label for a checkbox in the
 		# document formatting settings panel.
+		self.articlesCheckBox=elementsGroup.addItem(wx.CheckBox(self,label=_("Arti&cles")))
+		self.articlesCheckBox.SetValue(config.conf["documentFormatting"]["reportArticles"])
+
+		# Translators: This is the label for a checkbox in the
+		# document formatting settings panel.
 		self.framesCheckBox=elementsGroup.addItem(wx.CheckBox(self,label=_("Fra&mes")))
 		self.framesCheckBox.Value=config.conf["documentFormatting"]["reportFrames"]
 
@@ -1965,6 +1970,7 @@ class DocumentFormattingPanel(SettingsPanel):
 		config.conf["documentFormatting"]["reportLists"]=self.listsCheckBox.IsChecked()
 		config.conf["documentFormatting"]["reportBlockQuotes"]=self.blockQuotesCheckBox.IsChecked()
 		config.conf["documentFormatting"]["reportLandmarks"]=self.landmarksCheckBox.IsChecked()
+		config.conf["documentFormatting"]["reportArticles"] = self.articlesCheckBox.IsChecked()
 		config.conf["documentFormatting"]["reportFrames"]=self.framesCheckBox.Value
 		config.conf["documentFormatting"]["reportClickable"]=self.clickableCheckBox.Value
 
