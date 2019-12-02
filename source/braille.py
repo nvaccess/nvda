254c1539bc8ea978d95f6e9685b377d56101155b
Adriani <adriani.botez@googlemail.com>
2019-12-02 22:00:01 +0100

    Reverted the change in braille.py and replaced ntfc back with hlp in the user guide

--- source/braille.py
+++ source/braille.py
@@ -78,7 +78,7 @@ roleLabels = {
 	controlTypes.ROLE_GRAPHIC: _("gra"),
 	# Translators: Displayed in braille for toast notifications and for an object which is a
 	# help balloon.
-	controlTypes.ROLE_HELPBALLOON: _("ntfc"),
+	controlTypes.ROLE_HELPBALLOON: _("hlp"),
 	# Translators: Displayed in braille for an object which is a
 	# tool tip.
 	controlTypes.ROLE_TOOLTIP: _("tltip"),
