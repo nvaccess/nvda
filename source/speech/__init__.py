c7b9ac88d48fa87a2541a0207773abfbb0338aca
Reef Turner <reef@nvaccess.org>
2019-12-05 16:47:40 +0100

    Rename _speakTextInfo_addMath -> _extendSpeechSequence_addMathForTextInfo
    Clarify that the sequence is modified in place, nothing is spoken.

--- source/speech/__init__.py
+++ source/speech/__init__.py
@@ -899,7 +899,7 @@ class SpeakTextInfoState(object):
 		return self.__class__(self)
 
 
-def _speakTextInfo_addMath(
+def _extendSpeechSequence_addMathForTextInfo(
 		speechSequence: SpeechSequence, info: textInfos.TextInfo, field: textInfos.Field
 ) -> None:
 	import mathPres
@@ -1048,7 +1048,7 @@ def speakTextInfo(  # noqa: C901
 				shouldConsiderTextInfoBlank = False
 			if field.get("role")==controlTypes.ROLE_MATH:
 				shouldConsiderTextInfoBlank = False
-				_speakTextInfo_addMath(speechSequence,info,field)
+				_extendSpeechSequence_addMathForTextInfo(speechSequence, info, field)
 
 	# When true, we are inside a clickable field, and should therefore not announce any more new clickable fields
 	inClickable=False
@@ -1077,7 +1077,7 @@ def speakTextInfo(  # noqa: C901
 			shouldConsiderTextInfoBlank = False
 		if field.get("role")==controlTypes.ROLE_MATH:
 			shouldConsiderTextInfoBlank = False
-			_speakTextInfo_addMath(speechSequence,info,field)
+			_extendSpeechSequence_addMathForTextInfo(speechSequence, info, field)
 		commonFieldCount+=1
 
 	#Fetch the text for format field attributes that have changed between what was previously cached, and this textInfo's initialFormatField.
@@ -1201,7 +1201,7 @@ def speakTextInfo(  # noqa: C901
 						lastLanguage=None
 					relativeSpeechSequence.extend(fieldSequence)
 				if command.command=="controlStart" and command.field.get("role")==controlTypes.ROLE_MATH:
-					_speakTextInfo_addMath(relativeSpeechSequence,info,command.field)
+					_extendSpeechSequence_addMathForTextInfo(relativeSpeechSequence, info, command.field)
 				if autoLanguageSwitching and newLanguage!=lastLanguage:
 					relativeSpeechSequence.append(LangChangeCommand(newLanguage))
 					lastLanguage=newLanguage
