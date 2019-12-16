bb824089c3d8cee7492636027bafe243a4c56788
jakubl7545 <jakubl7545@gmail.com>
2019-12-16 12:32:02 +0100

    Modify copyright header

--- source/NVDAObjects/UIA/wordDocument.py
+++ source/NVDAObjects/UIA/wordDocument.py
@@ -1,7 +1,7 @@
-#A part of NonVisual Desktop Access (NVDA)
-#This file is covered by the GNU General Public License.
-#See the file COPYING for more details.
-#Copyright (C) 2016 NV Access Limited
+# A part of NonVisual Desktop Access (NVDA)
+# This file is covered by the GNU General Public License.
+# See the file COPYING for more details.
+# Copyright (C) 2016-2019 NV Access Limited, Jakub Lukowicz
 
 from comtypes import COMError
 from collections import defaultdict
