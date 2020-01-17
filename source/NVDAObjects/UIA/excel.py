d86358f7c7bc48f1a03409e7a8b3d465115b980a
Michael Curran <mick@nvaccess.org>
2020-01-17 16:45:07 +1000

    excel with UIA: announce multiple selected cells.

--- source/NVDAObjects/UIA/excel.py
+++ source/NVDAObjects/UIA/excel.py
@@ -3,27 +3,43 @@
 # See the file COPYING for more details.
 # Copyright (C) 2018 NV Access Limited
 
+import UIAHandler
 import controlTypes
 from . import UIA
 
 
 class ExcelCell(UIA):
 
-	name = u""
+	name=u""
 	role = controlTypes.ROLE_TABLECELL
 	rowHeaderText = None
 	columnHeaderText = None
 
+	def _get_value(self):
+		if self.selectionContainer and self.selectionContainer.getSelectedItemsCount()>1:
+			return
+		return super().value
+
 	def _get_description(self):
+		if self.selectionContainer and self.selectionContainer.getSelectedItemsCount()>1:
+			return
 		return self.UIAElement.currentItemStatus
 
 	def _get_cellCoordsText(self):
+		if self.selectionContainer and self.selectionContainer.getSelectedItemsCount()>1:
+			sc = self._getUIACacheablePropertyValue(UIAHandler.UIA_SelectionItemSelectionContainerPropertyId).QueryInterface(UIAHandler.IUIAutomationElement)
+			firstSelected = sc.GetCurrentPropertyValue(UIAHandler.UIA_Selection2FirstSelectedItemPropertyId).QueryInterface(UIAHandler.IUIAutomationElement)
+			firstAddress = firstSelected.GetCurrentPropertyValue(UIAHandler.UIA_NamePropertyId).replace('"','')
+			firstValue = firstSelected.GetCurrentPropertyValue(UIAHandler.UIA_ValueValuePropertyId)
+			lastSelected = sc.GetCurrentPropertyValue(UIAHandler.UIA_Selection2LastSelectedItemPropertyId).QueryInterface(UIAHandler.IUIAutomationElement)
+			lastAddress = lastSelected.GetCurrentPropertyValue(UIAHandler.UIA_NamePropertyId).replace('"','')
+			lastValue = lastSelected.GetCurrentPropertyValue(UIAHandler.UIA_ValueValuePropertyId)
+			return _("{firstAddress} {firstValue} through {lastAddress} {lastValue}").format(firstAddress=firstAddress,firstValue=firstValue,lastAddress=lastAddress,lastValue=lastValue)
 		name = super().name
 		# Later builds of Excel 2016 quote the letter coordinate.
 		# We don't want the quotes.
 		name = name.replace('"', '')
 		return name
-
 
 class ExcelWorksheet(UIA):
 	role = controlTypes.ROLE_TABLE
