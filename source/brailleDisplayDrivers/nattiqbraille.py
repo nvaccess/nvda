8579262b9167e934176996ec51439bc873663652
m-noman <60925266+m-noman@users.noreply.github.com>
2020-02-17 06:01:35 -0800

    Update nattiqbraille.py

--- source/brailleDisplayDrivers/nattiqbraille.py
+++ source/brailleDisplayDrivers/nattiqbraille.py
@@ -2,8 +2,7 @@
 # A part of NonVisual Desktop Access (NVDA)
 # This file is covered by the GNU General Public License.
 # See the file COPYING for more details.
-# Copyright (C) 2020 NV Access Limited, Mohammed Noman
-
+# Copyright (C) 2020 NV Access Limited, Mohammed Noman - Nattiq Technologies
 
 
 import serial
