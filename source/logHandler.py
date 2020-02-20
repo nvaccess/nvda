b08cf5951137b64c790412bdc09fdb1255d3ee65
Dawid Pieper <dawidpieper@o2.pl>
2020-02-20 09:50:06 +0100

    Update copyrights dates

--- source/logHandler.py
+++ source/logHandler.py
@@ -1,5 +1,5 @@
 # A part of NonVisual Desktop Access (NVDA)
-# Copyright (C) 2007-2019 NV Access Limited, Rui Batista, Joseph Lee, Leonard de Ruijter, Babbage B.V.
+# Copyright (C) 2007-2020 NV Access Limited, Rui Batista, Joseph Lee, Leonard de Ruijter, Babbage B.V.
 # This file is covered by the GNU General Public License.
 # See the file COPYING for more details.
 
