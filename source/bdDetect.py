2ef5a61f4258a8967fdd26f5579f8ee5afe766b5
Michael Curran <mick@nvaccess.org>
2020-03-02 08:17:33 +1000

    Support for APH Mantis Q40, APH Chameleon, HumanWare BrailleOne, BrailleNote Touch v2, and NLS eReader braille displays.

--- source/bdDetect.py
+++ source/bdDetect.py
@@ -459,9 +459,14 @@ addBluetoothDevices("brailleNote", lambda m:
 
 # brailliantB
 addUsbDevices("brailliantB", KEY_HID, {
+	"VID_1C71&PID_C111", # Mantis Q 40
+	"VID_1C71&PID_C101", # Chameleon 20
+	"VID_1C71&PID_C121", # Humanware BrailleOne 20 HID
+	"VID_1C71&PID_CE01", # NLS eReader 20  HID
 	"VID_1C71&PID_C006", # Brailliant BI 32, 40 and 80
 	"VID_1C71&PID_C022", # Brailliant BI 14
 	"VID_1C71&PID_C00A", # BrailleNote Touch
+	"VID_1C71&PID_C00E", # BrailleNote Touch v2
 })
 addUsbDevices("brailliantB", KEY_SERIAL, {
 	"VID_1C71&PID_C005", # Brailliant BI 32, 40 and 80
@@ -474,7 +479,13 @@ addBluetoothDevices("brailliantB", lambda m: (
 		or "BrailleNote Touch" in m.id
 	)) or (m.type==KEY_HID
 		and m.deviceInfo.get("manufacturer") == "Humanware"
-		and m.deviceInfo.get("product") == "Brailliant HID"
+		and m.deviceInfo.get("product") in (
+			"Brailliant HID",
+			"APH Chameleon 20",
+			"APH Mantis Q40",
+			"Humanware BrailleOne",
+			"NLS eReader",
+		)
 ))
 
 # eurobraille
