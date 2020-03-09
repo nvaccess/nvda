1477d7792c6dd51a07fd54ffad0c0df49fdd7150
Joseph Lee <joseph.lee22590@gmail.com>
2020-03-08 22:48:22 -0700

    NVDAObjects/UIA/WordDocument: update copyright header

--- source/NVDAObjects/UIA/wordDocument.py
+++ source/NVDAObjects/UIA/wordDocument.py
@@ -1,7 +1,7 @@
-#A part of NonVisual Desktop Access (NVDA)
-#This file is covered by the GNU General Public License.
-#See the file COPYING for more details.
-#Copyright (C) 2016 NV Access Limited
+# A part of NonVisual Desktop Access (NVDA)
+# This file is covered by the GNU General Public License.
+# See the file COPYING for more details.
+# Copyright (C) 2016-2020 NV Access Limited, Joseph Lee
 
 from comtypes import COMError
 from collections import defaultdict
