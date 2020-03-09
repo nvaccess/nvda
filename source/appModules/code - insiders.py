e56e8c5fa91ddb5a239dae485ca9b8d7ea069e91
Leonard de Ruijter <L.de.Ruijter@sevenp.nl>
2020-03-09 13:28:42 +0100

    Add appModule for Visual Studio Code Insiders

---
+++ source/appModules/code - insiders.py
@@ -0,0 +1,11 @@
+# A part of NonVisual Desktop Access (NVDA)
+# Copyright (C) 2020 NV Access Limited, Babbage B.V., Leonard de Ruijter
+# This file is covered by the GNU General Public License.
+# See the file COPYING for more details.
+
+"""
+appModule for Visual Studio Code Insiders.
+Imports the appModule for Visual Studio Code.
+"""
+
+from .code import AppModule
