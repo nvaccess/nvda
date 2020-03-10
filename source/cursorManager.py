nvaccess/nvda

commit 74f53978df9cef17d24aae77638009d295fdeef7

Browse mode: When the user moves the cursor, speak before setting selection. (#10389)
Previously, for both normal cursor movement and quick navigation, we set the selection before speaking.
However, setting selection might move the focus, which might mutate the document.
For virtual buffers, the TextInfo is offset-based, so this could invalidate the offsets, causing the wrong part of the document to be spoken.
To get around this, we now speak before setting the selection.

source/cursorManager.py
@@ -142,11 +142,15 @@ class CursorManager(documentBase.TextContainerObject,baseObject.ScriptableObject
 					pass
 				else:
 					info=self.makeTextInfo(textInfos.POSITION_FIRST if direction>0 else textInfos.POSITION_LAST)
-		self.selection=info
+		# #10343: Speak before setting selection because setting selection might
+		# move the focus, which might mutate the document, potentially invalidating
+		# info if it is offset-based.
+		selection = info.copy()
 		info.expand(unit)
 		if not willSayAllResume(gesture): speech.speakTextInfo(info,unit=unit,reason=controlTypes.REASON_CARET)
 		if not oldInfo.isCollapsed:
 			speech.speakSelectionChange(oldInfo,self.selection)
+		self.selection = selection
 
 	def doFindText(self,text,reverse=False,caseSensitive=False):
 		if not text:

source/browseMode.py
@@ -408,9 +408,12 @@ class BrowseModeTreeInterceptor(treeInterceptorHandler.TreeInterceptor):
 		except StopIteration:
 			ui.message(errorMessage)
 			return
-		item.moveTo()
+		# #8831: Report before moving because moving might change the focus, which
+		# might mutate the document, potentially invalidating info if it is
+		# offset-based.
 		if not gesture or not willSayAllResume(gesture):
 			item.report(readUnit=readUnit)
+		item.moveTo()
 
 	@classmethod
 	def addQuickNav(cls, itemType, key, nextDoc, nextError, prevDoc, prevError, readUnit=None):

commit 550283d4d99bd79da03a0858780ab879ad0f1f99

Fix for #10731: cursorManager: ensure that NVDA speaks text being unselected in browseMode again. Fixes regression caused by #10389.

source/cursorManager.py
@@ -145,7 +145,7 @@ class CursorManager(documentBase.TextContainerObject,baseObject.ScriptableObject
 		info.expand(unit)
 		if not willSayAllResume(gesture): speech.speakTextInfo(info,unit=unit,reason=controlTypes.REASON_CARET)
 		if not oldInfo.isCollapsed:
-			speech.speakSelectionChange(oldInfo,self.selection)
+			speech.speakSelectionChange(oldInfo,selection)
 		self.selection = selection
 
 	def doFindText(self,text,reverse=False,caseSensitive=False):