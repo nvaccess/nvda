b10beea39a972793c092c2b99e03a80b2047063d
Bill Dengler <codeofdusk@gmail.com>
2020-04-03 06:23:11 -0400

    Fix regressions from (PR #10932)
    Fixes  #10931
    Follow on from #10919
    
    The "report formatting script" (NVDA+f) was causing an error because the `reportSuperscriptsAndSubscripts` key was missing from the `formatConfig` dictionary used as a config section created in ` _reportFormattingHelper`. Updated the generation of this dictionary so this error is unlikely to be encountered again. Instead, now the risk is that any format option separated in the future my not be reported by the "report formatting script" when it should be.
    
    Some superscript/subscript reporting was not separated. To fix this the superscript/subscript fetching has been separated in more situations (PowerPoint, UIA).

--- source/NVDAObjects/UIA/__init__.py
+++ source/NVDAObjects/UIA/__init__.py
@@ -157,7 +157,17 @@ class UIATextInfo(textInfos.TextInfo):
 			if formatConfig["reportFontSize"]:
 				IDs.add(UIAHandler.UIA_FontSizeAttributeId)
 			if formatConfig["reportFontAttributes"]:
-				IDs.update({UIAHandler.UIA_FontWeightAttributeId,UIAHandler.UIA_IsItalicAttributeId,UIAHandler.UIA_UnderlineStyleAttributeId,UIAHandler.UIA_StrikethroughStyleAttributeId,UIAHandler.UIA_IsSuperscriptAttributeId,UIAHandler.UIA_IsSubscriptAttributeId,})
+				IDs.update({
+					UIAHandler.UIA_FontWeightAttributeId,
+					UIAHandler.UIA_IsItalicAttributeId,
+					UIAHandler.UIA_UnderlineStyleAttributeId,
+					UIAHandler.UIA_StrikethroughStyleAttributeId
+				})
+			if formatConfig["reportSuperscriptsAndSubscripts"]:
+				IDs.update({
+					UIAHandler.UIA_IsSuperscriptAttributeId,
+					UIAHandler.UIA_IsSubscriptAttributeId
+				})
 			if formatConfig["reportAlignment"]:
 				IDs.add(UIAHandler.UIA_HorizontalTextAlignmentAttributeId)
 			if formatConfig["reportColor"]:
@@ -196,6 +206,7 @@ class UIATextInfo(textInfos.TextInfo):
 			val=fetcher.getValue(UIAHandler.UIA_StrikethroughStyleAttributeId,ignoreMixedValues=ignoreMixedValues)
 			if val!=UIAHandler.handler.reservedNotSupportedValue:
 				formatField['strikethrough']=bool(val)
+		if formatConfig["reportSuperscriptsAndSubscripts"]:
 			textPosition=None
 			val=fetcher.getValue(UIAHandler.UIA_IsSuperscriptAttributeId,ignoreMixedValues=ignoreMixedValues)
 			if val!=UIAHandler.handler.reservedNotSupportedValue and val:
