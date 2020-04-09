e8cde2caabd18a71490999b9c2682deedf504229
Łukasz Golonka <lukasz.golonka@mailbox.org>
2020-04-09 11:14:46 +0200

    Remove unused plugins directories when initializing config. (PR #10746)
    Closes #10014
    
    If empty the following directories are removed from the config directory:
    - appModules
    - brailleDisplayDrivers
    - globalPlugins
    - synthDrivers

--- source/config/__init__.py
+++ source/config/__init__.py
@@ -17,6 +17,7 @@ import ctypes
 import ctypes.wintypes
 import os
 import sys
+import errno
 import itertools
 import contextlib
 from copy import deepcopy
@@ -166,6 +167,19 @@ def initConfigPath(configPath=None):
 		configPath=globalVars.appArgs.configPath
 	if not os.path.isdir(configPath):
 		os.makedirs(configPath)
+	else:
+		OLD_CODE_DIRS = ("appModules", "brailleDisplayDrivers", "globalPlugins", "synthDrivers")
+		# #10014: Since #9238 code from these directories is no longer loaded.
+		# However they still exist in config for older installations. Remove them if empty to minimize confusion.
+		for dir in OLD_CODE_DIRS:
+			dir = os.path.join(configPath, dir)
+			if os.path.isdir(dir):
+				try:
+					os.rmdir(dir)
+					log.info("Removed old plugins dir: %s", dir)
+				except OSError as ex:
+					if ex.errno == errno.ENOTEMPTY:
+						log.info("Failed to remove old plugins dir: %s. Directory not empty.", dir)
 	subdirs=["speechDicts","profiles"]
 	if not isAppX:
 		subdirs.append("addons")
