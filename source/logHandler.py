71a532a1345bab90654867463fc2e776ca382099
Julien Cochuyt <JulienCochuyt@users.noreply.github.com>
2020-05-04 00:58:30 +0200

    Fix linting issues

--- source/logHandler.py
+++ source/logHandler.py
@@ -1,5 +1,5 @@
 # A part of NonVisual Desktop Access (NVDA)
-# Copyright (C) 2007-2020 NV Access Limited, Rui Batista, Joseph Lee, Leonard de Ruijter, Babbage B.V., Accessolutions, Julien Cochuyt
+# Copyright (C) 2007-2020 NV Access Limited, Rui Batista, Joseph Lee, Leonard de Ruijter, Babbage B.V., Accessolutions, Julien Cochuyt  # noqa: E501 line too long
 # This file is covered by the GNU General Public License.
 # See the file COPYING for more details.
 
