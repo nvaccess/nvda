14224b3c3ee86dac1f55211d65cdcc7e9239f5f9
Joseph Lee <joseph.lee22590@gmail.com>
2020-05-04 13:15:59 -0700

    appModules/foobar2000: add an explanation regarding why custom status bar getter is defined. Re #11082.

--- source/appModules/foobar2000.py
+++ source/appModules/foobar2000.py
@@ -46,6 +46,7 @@ class AppModule(appModuleHandler.AppModule):
 
 	def _get_statusBar(self):
 		# #11082: retrieve status bar handle and resulting NVDA object from playlist window.
+		# If only default status bar routine is used to assign status bar object, it can result in recursion limit error.
 		try:
 			statusBarHwnd = windowUtils.findDescendantWindow(
 				api.getForegroundObject().windowHandle,
