50b469cab72240f2717a6424af9ead308933ec8d
André-Abush Clause <dev@andreabc.net>
2020-06-19 12:26:59 +0200

    Adds Ukrainian computer braille table in the GUI

--- source/brailleTables.py
+++ source/brailleTables.py
@@ -403,6 +403,9 @@ addTable("tr.ctb", _("Turkish grade 1"))
 addTable("uk.utb", _("Ukrainian"))
 # Translators: The name of a braille table displayed in the
 # braille settings dialog.
+addTable("uk-comp.utb", _("Ukrainian computer braille"))
+# Translators: The name of a braille table displayed in the
+# braille settings dialog.
 addTable("uz-g1.utb", _("Uzbek grade 1"))
 # Translators: The name of a braille table displayed in the
 # braille settings dialog.
