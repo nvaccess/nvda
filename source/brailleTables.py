d2b4ce51b2b73ac847cf4d8a5a4910056a410627
André-Abush Clause <dev@andreabc.net>
2020-06-19 12:37:04 +0200

    renames a braille table in the GUI: 'Ukrainian' -> 'Ukrainian grade 1'

--- source/brailleTables.py
+++ source/brailleTables.py
@@ -400,7 +400,7 @@ addTable("te-in-g1.utb", _("Telugu grade 1"))
 addTable("tr.ctb", _("Turkish grade 1"))
 # Translators: The name of a braille table displayed in the
 # braille settings dialog.
-addTable("uk.utb", _("Ukrainian"))
+addTable("uk.utb", _("Ukrainian grade 1"))
 # Translators: The name of a braille table displayed in the
 # braille settings dialog.
 addTable("uk-comp.utb", _("Ukrainian computer braille"))
