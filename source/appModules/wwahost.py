dc611be3ac167692275f1d922b940d2885a1a353
Joseph Lee <joseph.lee22590@gmail.com>
2020-07-05 23:58:32 -0700

    appModules/WWAHost: remove bulk of app module class apart from browse mode off flag.
    Comment from Mick Curran (NV Access): in 2012, WWA Host app module was reponsible for turning what was essentially a web application into a proper 'application'. This was needed prior to introduction of browse mode flag for app modules. Now that this flag is present, there is no need for role coercion anymore (users can toggle browse mode instead).

--- source/appModules/wwahost.py
+++ source/appModules/wwahost.py
@@ -9,13 +9,8 @@ In Windows 10, progressive web apps (PWA) and friends are hosted inside this pro
 App modules wishing to support apps hosted inside this process must subclass the AppModule class.
 """
 
-from comtypes import COMError
 import ctypes
-import IAccessibleHandler
-from NVDAObjects.IAccessible.MSHTML import Body
 import appModuleHandler
-import controlTypes
-import winUser
 import winKernel
 
 
@@ -43,16 +38,3 @@ class AppModule(appModuleHandler.AppModule):
 
 	# WWAHost app content is treated as part of an app, not a browse mode document.
 	disableBrowseModeByDefault = True
-
-	def event_NVDAObject_init(self,obj):
-		#The root document of HTML Metro Apps must be treeted as an application. 
-		if isinstance(obj,Body) and obj.windowClassName=="Internet Explorer_Server":
-			try:
-				paccParent=obj.IAccessibleObject.accParent.accParent
-				identity=IAccessibleHandler.getIAccIdentity(paccParent,0)
-			except (COMError,AttributeError):
-				identity=None
-			if identity:
-				windowHandle=identity.get('windowHandle')
-				if windowHandle and winUser.getClassName(windowHandle)=="Web Platform Embedding":
-					obj.role=controlTypes.ROLE_APPLICATION
