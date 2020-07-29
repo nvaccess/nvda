1701cb888d0ec02d5c90968ee55bf3847f80fdda
Michael Curran <mick@nvaccess.org>
2020-07-29 15:24:21 +1000

    Add ROLE_MARKED_CONTENT to NVDA, which should represent marked (highlighted) content. Have speech and braille report its start and end within rich text if the report emphasis document formatting option is enabled. Map the IAccessible2 MARK role to the NVDA MARK role. This all means that the mark role will be reported in both Firefox and Chrome.

--- source/IAccessibleHandler/__init__.py
+++ source/IAccessibleHandler/__init__.py
@@ -97,6 +97,7 @@ from comInterfaces.IAccessible2Lib import (
 	IA2_ROLE_BLOCK_QUOTE,
 	IA2_ROLE_DESKTOP_ICON,
 	IA2_ROLE_FOOTER,
+	IA2_ROLE_MARK,
 )
 
 from . import internalWinEventHandler
@@ -256,6 +257,7 @@ IAccessibleRolesToNVDARoles = {
 	IA2_ROLE_CONTENT_INSERTION: controlTypes.ROLE_INSERTED_CONTENT,
 	IA2_ROLE_BLOCK_QUOTE: controlTypes.ROLE_BLOCKQUOTE,
 	IA2.IA2_ROLE_LANDMARK: controlTypes.ROLE_LANDMARK,
+	IA2_ROLE_MARK: controlTypes.ROLE_MARKED_CONTENT,
 	# some common string roles
 	"frame": controlTypes.ROLE_FRAME,
 	"iframe": controlTypes.ROLE_INTERNALFRAME,
