781a9e804b59fc950d0a6be2806772ac7750fec9
Joseph Lee <joseph.lee22590@gmail.com>
2020-08-03 06:41:59 -0700

    Modern keyboard: update copyright header (year and removed intro text for consistency with other files)

--- source/appModules/windowsinternal_composableshell_experiences_textinput_inputapp.py
+++ source/appModules/windowsinternal_composableshell_experiences_textinput_inputapp.py
@@ -1,6 +1,5 @@
-# App module for Composable Shell (CShell) input panel
 #A part of NonVisual Desktop Access (NVDA)
-#Copyright (C) 2017-2018 NV Access Limited, Joseph Lee
+#Copyright (C) 2017-2020 NV Access Limited, Joseph Lee
 #This file is covered by the GNU General Public License.
 #See the file COPYING for more details.
 
