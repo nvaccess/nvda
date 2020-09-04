2db2db22133cde8732ed8f3f43755301302f0c42
Luke Davis <newanswertech@gmail.com>
2020-09-04 07:48:46 -0400

    addonHandler/__init__.py: corrected another "addon" to "add-on".

--- source/addonHandler/__init__.py
+++ source/addonHandler/__init__.py
@@ -448,7 +448,7 @@ class Addon(AddonBase):
 			return None
 
 	def getTranslationsInstance(self, domain='nvda'):
-		""" Gets the gettext translation instance for this addon.
+		""" Gets the gettext translation instance for this add-on.
 		<addon-path><\locale will be used to find .mo files, if exists.
 		If a translation file is not found the default fallback null translation is returned.
 		@param domain: the translation domain to retrieve. The 'nvda' default should be used in most cases.
