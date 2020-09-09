cee784953ebb30549569f5f1672c5df2eef2879f
Michael Curran <mick@nvaccess.org>
2020-09-09 17:55:24 +1000

    Add system test for list item change.

--- tests/system/robot/chromeTests.py
+++ tests/system/robot/chromeTests.py
@@ -39,3 +39,40 @@ def checkbox_labelled_by_inner_element():
 		# Instead this should be spoken as:
 		"Simulate evil cat  check box  not checked"
 	)
+
+def announce_list_item_when_moving_by_word_or_character():
+	_chrome.prepareChrome(
+		r"""
+			<ul style="list-style-type:none">
+				<li>xy</li>
+				<li>z</li>
+			</ul>
+		"""
+	)
+	# Ensure that moving into a list by line, "list item" is not reported.
+	actualSpeech = _chrome.getSpeechAfterKey("downArrow")
+	_asserts.strings_match(
+		actualSpeech,
+		"list  with 2 items  xy"
+	)
+	# Ensure that when moving by character (rightArrow)
+	# within the list item, "list item" is not announced.
+	actualSpeech = _chrome.getSpeechAfterKey("rightArrow")
+	_asserts.strings_match(
+		actualSpeech,
+		"y"
+	)
+	# Ensure that when moving by character (rightArrow)
+	# onto the next list item, "list item" is reported.
+	actualSpeech = _chrome.getSpeechAfterKey("rightArrow")
+	_asserts.strings_match(
+		actualSpeech,
+		"list item  level 1  \nz"
+	)
+	# Ensure that when moving by character (leftArrow)
+	# onto the previous list item, "list item" is reported.
+	actualSpeech = _chrome.getSpeechAfterKey("leftArrow")
+	_asserts.strings_match(
+		actualSpeech,
+		"list item  level 1  \ny"
+	)
--- tests/system/robot/chromeTests.robot
+++ tests/system/robot/chromeTests.robot
@@ -26,3 +26,6 @@ default teardown
 checkbox labelled by inner element
 	[Documentation]	A checkbox labelled by an inner element should not read the label element twice.
 	checkbox_labelled_by_inner_element
+Announce list item when moving by word or character
+	[Documentation]	Entering a list item when moving by word or character should be announced, but not by line.
+	announce_list_item_when_moving_by_word_or_character
