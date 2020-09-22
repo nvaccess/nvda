b2cc6c8ef030ef0c308b31b4ef78d41e5e6b1ff0
Leonard de Ruijter <alderuijter@gmail.com>
2020-09-22 19:48:55 +0200

    Work around VS Code specific performance issues

---
+++ source/appModules/code - insiders.py
@@ -0,0 +1,10 @@
+#A part of NonVisual Desktop Access (NVDA)
+#Copyright (C) 2020 NV Access Limited, Leonard de Ruijter
+#This file is covered by the GNU General Public License.
+#See the file COPYING for more details.
+
+""" App module for Visual Studio Code Insiders.
+This app module simply inherrits from the app module for the stable version of Code.
+"""
+
+from .code import *
