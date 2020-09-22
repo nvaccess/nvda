de18dc12880f7467ddfd05c2e5f899bb1a1cc29e
Dawid Pieper <dawidpieper@o2.pl>
2020-09-22 14:36:12 +0200

    Fix SAPI5 synthesizers missing some messages (PR #11586)
    Fix for someSAPI5 sy

--- source/synthDrivers/sapi5.py
+++ source/synthDrivers/sapi5.py
@@ -1,9 +1,9 @@
 # -*- coding: UTF-8 -*-
-#synthDrivers/sapi5.py
-#A part of NonVisual Desktop Access (NVDA)
-#Copyright (C) 2006-2017 NV Access Limited, Peter Vágner, Aleksey Sadovoy
-#This file is covered by the GNU General Public License.
-#See the file COPYING for more details.
+# synthDrivers/sapi5.py
+# A part of NonVisual Desktop Access (NVDA)
+# Copyright (C) 2006-2020 NV Access Limited, Peter Vágner, Aleksey Sadovoy
+# This file is covered by the GNU General Public License.
+# See the file COPYING for more details.
 
 import locale
 from collections import OrderedDict
@@ -364,6 +364,11 @@ class SynthDriver(SynthDriver):
 		tagsChanged[0] = True
 		outputTags()
 
+		# Some SAPI5 synthesizers complete speech sequence just after the last text is said
+		# and ignore any indexes passed afterward.
+		# Therefore we add the pause of 1ms at the end of each sequence.
+		textList.append('<silence msec="1" />')
+		# Join textList to create xml document passed to synthesizer
 		text = "".join(textList)
 		flags = constants.SVSFIsXML | constants.SVSFlagsAsync
 		self.tts.Speak(text, flags)
