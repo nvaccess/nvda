f6899ce67dd5e15c3ab8d8c8f4c61e06ea1aa7d2
Michael Curran <mick@nvaccess.org>
2020-09-25 13:21:35 +1000

    speech.getControlFieldSpeech: treeview items which are not normally spoken at all, should at least have expanded / collapsed / level announced.

--- source/speech/__init__.py
+++ source/speech/__init__.py
@@ -1910,6 +1910,18 @@ def getControlFieldSpeech(  # noqa: C901
 		out = []
 		if ariaCurrent:
 			out.extend(ariaCurrentSequence)
+		# Speak expanded / collapsed / level for treeview items (in ARIA treegrids)
+		if role == controlTypes.ROLE_TREEVIEWITEM:
+			if controlTypes.STATE_EXPANDED in states:
+				out.extend(
+					getPropertiesSpeech(reason=reason, states={controlTypes.STATE_EXPANDED}, _role=role)
+				)
+			elif controlTypes.STATE_COLLAPSED in states:
+				out.extend(
+					getPropertiesSpeech(reason=reason, states={controlTypes.STATE_COLLAPSED}, _role=role)
+				)
+			if levelSequence:
+				out.extend(levelSequence)
 		if role == controlTypes.ROLE_GRAPHIC and content:
 			out.append(content)
 		types.logBadSequenceTypes(out)
