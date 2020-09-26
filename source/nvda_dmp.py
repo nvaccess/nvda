7bc466eba9ca3b2e603958ba654e50d0391fb323
Bill Dengler <codeofdusk@gmail.com>
2020-09-26 08:43:58 -0400

    Uniformize ends of inserted chunks.

--- source/nvda_dmp.py
+++ source/nvda_dmp.py
@@ -1,3 +1,7 @@
+# A proxy to allow NVDA to use diff-match-patch without linking
+# for licensing reasons.
+# Copyright 2020 Bill Dengler
+
 import struct
 import sys
 
@@ -13,8 +17,8 @@ if __name__ == "__main__":
 		newText = sys.stdin.buffer.read(newLen).decode("utf-8")
 		res = ""
 		for op, text in diff(oldText, newText, counts_only=False):
-			if (op == "=" and text.isspace()) or op == "+":
-				res += text
+			if op == "+":
+				res += text.rstrip() + "\n"
 		sys.stdout.buffer.write(struct.pack("=I", len(res)))
 		sys.stdout.buffer.write(res.encode("utf-8"))
 		sys.stdin.flush()
