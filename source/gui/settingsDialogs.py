738e0993a5ebe46a198427c1439574fe2a15ea0e
Cyrille Bougot <cyrille.bougot2@laposte.net>
2020-11-03 23:57:00 +0100

    Added an accelerator key to 'Marked (highlighted) text' checkbox in Document formatting settings panel.

--- source/gui/settingsDialogs.py
+++ source/gui/settingsDialogs.py
@@ -2123,7 +2123,7 @@ class DocumentFormattingPanel(SettingsPanel):
 
 		# Translators: This is the label for a checkbox in the
 		# document formatting settings panel.
-		highlightText = _("Marked (highlighted text)")
+		highlightText = _("Mar&ked (highlighted text)")
 		self.highlightCheckBox = fontGroup.addItem(
 			wx.CheckBox(self, label=highlightText)
 		)
