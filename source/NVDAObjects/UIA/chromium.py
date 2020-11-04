c4b4dd0acda6e522c7d2395e15cbf42237c2ff11
Michael Curran <mick@nvaccess.org>
2020-11-05 09:55:33 +1000

    ChromiumUIA: as headings are exposed in the element tree, don't expose them in format fields, as headings were being reported twice.

--- source/NVDAObjects/UIA/chromium.py
+++ source/NVDAObjects/UIA/chromium.py
@@ -8,8 +8,22 @@ from . import web
 import controlTypes
 
 
+class ChromiumUIATextInfo(web.UIAWebTextInfo):
+
+	def _getFormatFieldAtRange(self, textRange, formatConfig, ignoreMixedValues=False):
+		formatField = super()._getFormatFieldAtRange(textRange, formatConfig, ignoreMixedValues=ignoreMixedValues)
+		# Headings are also exposed in the element tree,
+		# And therefore exposing in a formatField is redundant and causes duplicate reporting.
+		# So remove heading-level from the formatField if it exists.
+		try:
+			del formatField.field['heading-level']
+		except KeyError:
+			pass
+		return formatField
+
+
 class ChromiumUIA(web.UIAWeb):
-	...
+	_TextInfo = ChromiumUIATextInfo
 
 
 class ChromiumUIATreeInterceptor(web.UIAWebTreeInterceptor):
