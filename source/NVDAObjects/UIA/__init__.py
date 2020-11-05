50a3097798e18b3b6bc7eb288478446735813580
Michael Curran <mick@nvaccess.org>
2020-11-05 10:01:01 +1000

    Fix list presentation while in browse mode for Chromium with UIA
    Make sure interactive / presentational lists are presented
    correctly in browseMode in Edgium.

--- source/NVDAObjects/UIA/__init__.py
+++ source/NVDAObjects/UIA/__init__.py
@@ -925,6 +925,7 @@ class UIA(Window):
 				clsList.append(spartanEdge.EdgeNode)
 		elif self.windowClassName == "Chrome_RenderWidgetHostHWND":
 			from . import chromium
+			from . import web
 			if (
 				self.UIATextPattern
 				and self.role == controlTypes.ROLE_DOCUMENT
@@ -933,6 +934,8 @@ class UIA(Window):
 			):
 				clsList.append(chromium.ChromiumUIADocument)
 			else:
+				if self.role == controlTypes.ROLE_LIST:
+					clsList.append(web.List)
 				clsList.append(chromium.ChromiumUIA)
 		elif (
 			self.role == controlTypes.ROLE_DOCUMENT
