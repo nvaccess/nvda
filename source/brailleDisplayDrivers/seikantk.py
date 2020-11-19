d498c773b1fd81eba6dc45849aa8157e5a550853
moyanming2013 <moyanming2013@qq.com>
2020-11-19 17:11:10 +0800

    fixed flake8 errors.

--- source/brailleDisplayDrivers/seikantk.py
+++ source/brailleDisplayDrivers/seikantk.py
@@ -1,7 +1,9 @@
-#A part of NonVisual Desktop Access (NVDA)
-#This file is covered by the GNU General Public License.
-#See the file COPYING for more details.
-#Copyright (C) 2012/20 Ulf Beckmann <beckmann@flusoft.de>
+# brailleDisplayDrivers/seikantk.py
+#
+# A part of NonVisual Desktop Access (NVDA)
+# Copyright (C) 2012-2020 NV Access Limited, Ulf Beckmann <beckmann@flusoft.de>
+# This file may be used under the terms of the GNU General Public License, version 2 or later.
+# For more details see: https://www.gnu.org/licenses/gpl-2.0.html
 #
 # This file represents the braille display driver for
 # Seika Notetaker, a product from Nippon Telesoft
@@ -9,10 +11,7 @@
 # 29.06.2020 / 12:36
 
 from io import BytesIO
-from typing import Union, List, Optional
-from ctypes import *
-import time
-import wx
+from typing import List
 import braille
 import brailleInput
 import inputCore
@@ -36,43 +35,44 @@ DOT_8 = 0x80
 
 
 _keyNames = {
-0x000001 : "BACKSPACE",
-0x000002 : "SPACE",
-0x000004 : "LB",
-0x000008 : "RB",
-0x000010 : "LJ_CENTER", 
-0x000020 : "LJ_LEFT", 
-0x000040 : "LJ_RIGHT", 
-0x000080 : "LJ_UP",
-0x000100 : "LJ_DOWN",
-0x000200 : "RJ_CENTER", 
-0x000400 : "RJ_LEFT", 
-0x000800 : "RJ_RIGHT", 
-0x001000 : "RJ_UP", 
-0x002000 : "RJ_DOWN"
+	0x000001: "BACKSPACE",
+	0x000002: "SPACE",
+	0x000004: "LB",
+	0x000008: "RB",
+	0x000010: "LJ_CENTER", 
+	0x000020: "LJ_LEFT", 
+	0x000040: "LJ_RIGHT", 
+	0x000080: "LJ_UP",
+	0x000100: "LJ_DOWN",
+	0x000200: "RJ_CENTER", 
+	0x000400: "RJ_LEFT", 
+	0x000800: "RJ_RIGHT", 
+	0x001000: "RJ_UP", 
+	0x002000: "RJ_DOWN"
 }
 
 SEIKA_REQUEST_INFO = b"\x03\xff\xff\xa1"
-SEIKA_INFO         = b"\xff\xff\xa2"
-SEIKA_SEND_TEXT    = b"\x2c\xff\xff\xa3"
-SEIKA_ROUTING      = b"\xff\xff\xa4"
-SEIKA_KEYS         = b"\xff\xff\xa6"
-SEIKA_KEYS_ROU     = b"\xff\xff\xa8"
+SEIKA_INFO = b"\xff\xff\xa2"
+SEIKA_SEND_TEXT = b"\x2c\xff\xff\xa3"
+SEIKA_ROUTING = b"\xff\xff\xa4"
+SEIKA_KEYS = b"\xff\xff\xa6"
+SEIKA_KEYS_ROU = b"\xff\xff\xa8"
 
-SEIKA_CONFIG       = b"\x50\x00\x00\x25\x80\x00\x00\x03\x00"
-SEIKA_CMD_ON       = b"\x41\x01"
+SEIKA_CONFIG = b"\x50\x00\x00\x25\x80\x00\x00\x03\x00"
+SEIKA_CMD_ON = b"\x41\x01"
 
-vidpid ="VID_10C4&PID_EA80"
-hidvidpid ="HID\\VID_10C4&PID_EA80"
+vidpid = "VID_10C4&PID_EA80"
+hidvidpid = "HID\\VID_10C4&PID_EA80"
 SEIKA_NAME = "seikantk"
 
 _dotNames = {}
-for i in range(1,9):
+for i in range(1, 9):
 	key = globals()["DOT_%d" % i]
 	_dotNames[key] = "d%d" % i
 bdDetect.addUsbDevices(SEIKA_NAME, bdDetect.KEY_HID, {
-vidpid, # USB-HID adapter
-})
+	vidpid,  # USB-HID adapter
+	})
+
 
 class BrailleDisplayDriver(braille.BrailleDisplayDriver):
 	_dev: hwIo.IoBase
@@ -83,7 +83,7 @@ class BrailleDisplayDriver(braille.BrailleDisplayDriver):
 	for d in hwPortUtils.listHidDevices():
 		if d["hardwareID"].startswith(hidvidpid):
 			path = d["devicePath"]
-			#log.info("Devicepath: "+path)
+			# log.info("Devicepath: "+path)
 
 	@classmethod
 	def check(cls):
@@ -91,8 +91,7 @@ class BrailleDisplayDriver(braille.BrailleDisplayDriver):
 
 	@classmethod
 	def getManualPorts(cls):
-		return path
-
+		return self.path
 
 	def __init__(self, port="hid"):
 		super(BrailleDisplayDriver, self).__init__()
@@ -102,25 +101,25 @@ class BrailleDisplayDriver(braille.BrailleDisplayDriver):
 		self.cmdlen = 0
 		self.handle = None
 		self._hidBuffer = b""
-		log.info("Versuch: "+self.path)
-#		for portType, portId, port, portInfo in self._getTryPorts(port):
-#			log.info("port:"+port)
+		log.info("Versuch: " + self.path)
+		# for portType, portId, port, portInfo in self._getTryPorts(port):
+		# log.info("port:"+port)
 		if self.path == "":
 			raise RuntimeError("No MINI-SEIKA display found, no path found")
 		self._dev = hwIo.Hid(path=self.path, onReceive=self._onReceive)
 		if self._dev._file == INVALID_HANDLE_VALUE:
 			raise RuntimeError("No MINI-SEIKA display found, open error")
-		self._dev.setFeature(SEIKA_CONFIG) # baudrate, stopbit usw
-		self._dev.setFeature(SEIKA_CMD_ON) # device on
-		self._dev.write(SEIKA_REQUEST_INFO) # Request the Info from the device
-		for i in range(30): # the info-block is about 
-			self._dev.waitForRead(TIMEOUT)		
+		self._dev.setFeature(SEIKA_CONFIG)  # baudrate, stopbit usw
+		self._dev.setFeature(SEIKA_CMD_ON)  # device on
+		self._dev.write(SEIKA_REQUEST_INFO)  # Request the Info from the device
+		for i in range(30):  # the info-block is about
+			self._dev.waitForRead(TIMEOUT)
 			if self.numCells:
-				log.info("Seikanotetaker an USB-HID, Cells {c} Buttons {b}".format(c=self.numCells,b=self.numBtns))
+				log.info("Seikanotetaker an USB-HID, Cells {c} Buttons {b}".format(c=self.numCells, b=self.numBtns))
 				config.conf["braille"]["display"] = SEIKA_NAME
 				break
 
-		if self.numCells==0:
+		if self.numCells == 0:
 			self._dev.close()
 			raise RuntimeError("No MINI-SEIKA display found, no response")
 
@@ -132,7 +131,7 @@ class BrailleDisplayDriver(braille.BrailleDisplayDriver):
 
 	def display(self, cells: List[int]):
 		# cells will already be padded up to numCells.
-		cellBytes = SEIKA_SEND_TEXT +self.numCells.to_bytes(1,'little') +bytes(cells)
+		cellBytes = SEIKA_SEND_TEXT + self.numCells.to_bytes(1, 'little') + bytes(cells)
 		self._dev.write(cellBytes)
 
 	def _onReceive(self, data: bytes):
@@ -144,12 +143,13 @@ class BrailleDisplayDriver(braille.BrailleDisplayDriver):
 		elif len(self._hidBuffer) == 4:
 			self.cmdlen = cmd[1]
 			self.status = 2
-		elif self.status == 2 and len(self._hidBuffer) == self.cmdlen+4:
+		elif self.status == 2 and len(self._hidBuffer) == self.cmdlen + 4:
 			command = self._hidBuffer[0:3]
-			arg =  self._hidBuffer[3:self.cmdlen+4]
+			arg = self._hidBuffer[3:self.cmdlen + 4]
 			self.status = 0
-			self._hidBuffer=b""
-			# log.info("auswerten nach cmd"+"".join(format(x, ' 02X') for x in command)+" ... "+"".join(format(x, ' 02X') for x in arg));
+			self._hidBuffer = b""
+			# log.info("auswerten nach cmd"+
+			# "".join(format(x, ' 02X') for x in command)+" ... "+"".join(format(x, ' 02X') for x in arg));
 			if command == SEIKA_INFO:
 				self._handInfo(arg)
 			elif command == SEIKA_ROUTING:
@@ -159,19 +159,19 @@ class BrailleDisplayDriver(braille.BrailleDisplayDriver):
 			elif command == SEIKA_KEYS_ROU:
 				self._handKeysRouting(arg)
 			else:
-		        	log.debug("hier haette ich nicht ankommen sollen.")
+				log.debug("hier haette ich nicht ankommen sollen.")
 
 	def _handInfo(self, arg: bytes):
 		self.numCells = arg[2]
-		self.numBtns =  arg[1]
+		self.numBtns = arg[1]
 
 	def _handRouting(self, arg: bytes):
 		Rou = 0
-		#log.info("Rou: "+""+"".join(format(x, ' 02X') for x in arg));
+		# log.info("Rou: "+""+"".join(format(x, ' 02X') for x in arg));
 		for i in range(arg[0]):
 			for j in range(8):
-				if arg[i+1] & (1<<j):
-					Rou = i*8+j
+				if arg[i + 1] & (1 << j):
+					Rou = i * 8 + j
 					# log.info("Routing: {c}".format(c=Rou))
 					gesture = InputGestureRouting(Rou)
 					try:
@@ -181,18 +181,18 @@ class BrailleDisplayDriver(braille.BrailleDisplayDriver):
 						pass
 
 	def _handKeys(self, arg: bytes):
-		#log.info("Keys: "+""+"".join(format(x, ' 02X') for x in arg));
+		# log.info("Keys: "+""+"".join(format(x, ' 02X') for x in arg));
 		Brl = arg[1]
 		Key = arg[2] | (arg[3] << 8)
-		space = arg[2] & 2
-		Btn = 0 # Seika has no button
+		# space = arg[2] & 2
+		Btn = 0  # Seika has no button
 		if not (Key or Btn or Brl):
 			pass
-		if Key: # Mini Seika has 2 Top and 4 Front ....
+		if Key:  # Mini Seika has 2 Top and 4 Front ....
 			gesture = InputGesture(keys=Key)
-		if Btn: # Mini Seika has no Btn ....
-       			gesture = InputGesture(keys=Btn)
-		if Brl: # or how to handle Brailleinput?
+		if Btn:  # Mini Seika has no Btn ....
+			gesture = InputGesture(keys=Btn)
+		if Brl:  # or how to handle Brailleinput?
 			gesture = InputGesture(dots=Brl)
 		if Key or Btn or Brl:
 			try:
@@ -203,7 +203,7 @@ class BrailleDisplayDriver(braille.BrailleDisplayDriver):
 
 	def _handKeysRouting(self, arg: bytes):
 		argk = b"\x03" + arg[1:]
-		argr = (arg[0]-3).to_bytes(1,'little')+ arg[4:]
+		argr = (arg[0] - 3).to_bytes(1, 'little') + arg[4:]
 		self._handRouting(argr)
 		self._handKeys(argk)
 
@@ -233,7 +233,7 @@ class BrailleDisplayDriver(braille.BrailleDisplayDriver):
 			"kb:shift+leftArrow": ("br(seikantk):BACKSPACE+RJ_LEFT",),
 			"kb:shift+rightArrow": ("br(seikantk):BACKSPACE+RJ_RIGHT",),
 			"kb:windows": ("br(seikantk):BACKSPACE+RJ_CENTER",),
-			"kb:space": ("br(seikantk):BACKSPACE","br(seikantk):SPACE",),
+			"kb:space": ("br(seikantk):BACKSPACE", "br(seikantk):SPACE",),
 			"kb:backspace": ("br(seikantk):d7",),
 			"kb:pageup": ("br(seikantk):SPACE+LJ_RIGHT",),
 			"kb:pagedown": ("br(seikantk):SPACE+LJ_LEFT",),
@@ -241,9 +241,10 @@ class BrailleDisplayDriver(braille.BrailleDisplayDriver):
 			"kb:end": ("br(seikantk):SPACE+LJ_DOWN",),
 			"kb:control+home": ("br(seikantk):BACKSPACE+LJ_UP",),
 			"kb:control+end": ("br(seikantk):BACKSPACE+LJ_DOWN",),
-			"kb:enter": ("br(seikantk):RJ_CENTER","br(seikantk):d8"),
+			"kb:enter": ("br(seikantk):RJ_CENTER", "br(seikantk):d8"),
 		},
 	})
+
 
 class InputGestureRouting(braille.BrailleDisplayGesture):
 	source = BrailleDisplayDriver.name
@@ -253,6 +254,7 @@ class InputGestureRouting(braille.BrailleDisplayGesture):
 		self.id = "routing"
 		self.routingIndex = index
 
+
 class InputGesture(braille.BrailleDisplayGesture, brailleInput.BrailleInputGesture):
 	source = BrailleDisplayDriver.name
 
@@ -261,19 +263,16 @@ class InputGesture(braille.BrailleDisplayGesture, brailleInput.BrailleInputGestu
 		# see what thumb keys are pressed:
 		names = set()
 		if keys is not None:
-			names.update(_keyNames[1 << i] for i in range(22)
-					if (1 << i) & keys)
+			names.update(_keyNames[1 << i] for i in range(22) if (1 << i) & keys)
 		elif dots is not None:
-		# now the dots
+			# now the dots
 			self.dots = dots
 			if space:
 				self.space = space
 				names.add(_keyNames[1])
-			names.update(_dotNames[1 << i] for i in range(8)
-					if (1 << i) & dots)
+			names.update(_dotNames[1 << i] for i in range(8) if (1 << i) & dots)
 		elif routing is not None:
 			self.routingIndex = routing
 			names.add('routing')
 		self.id = "+".join(names)
-		#log.info("SNT: keys {keys}".format(keys=names))
-
+		# log.info("SNT: keys {keys}".format(keys=names))
