nvaccess/nvda

commit 82c68350a0657a46a9a58f5e13854411e35c9d41

Prevent infinite recursion for unspeakable chars (PR #11865)
Fixes #11752
When reading characters that do not have any description in NVDA, a 'no content'
utterance can be produced. In this case the empty string is the last
'command' in the sequence passed to 'ensureEndUtterance', resulting in
the sequence not being given an indexCommand or an EndUtteranceCommand.
The indexCommand is required for the cancellable speech processing, to
be able to track which utterance a cancellableSpeechCommand belongs to.

Noticed and fixed a similar issue for 'latestCancelledUtteranceIndex': zero is a might
be a valid index. Only no index (None) should take this path.

source/speech/manager.py
@@ -333,7 +333,7 @@ class SpeechManager(object):
 			else:
 				lastOutSeq = outSeqs[-1] if outSeqs else None
 			lastCommand = lastOutSeq[-1] if lastOutSeq else None
-			if not lastCommand or isinstance(lastCommand, (EndUtteranceCommand, ConfigProfileTriggerCommand)):
+			if lastCommand is None or isinstance(lastCommand, (EndUtteranceCommand, ConfigProfileTriggerCommand)):
 				# It doesn't make sense to start with or repeat EndUtteranceCommands.
 				# We also don't want an EndUtteranceCommand immediately after a ConfigProfileTriggerCommand.
 				return
@@ -458,13 +458,18 @@ class SpeechManager(object):
 		# apply any parameters changed before the preemption.
 		params = self._curPriQueue.paramTracker.getChanged()
 		utterance.extend(params)
-		for seq in self._curPriQueue.pendingSequences:
+		lastSequenceIndexAddedToUtterance = None
+		for seqIndex, seq in enumerate(self._curPriQueue.pendingSequences):
 			if isinstance(seq[0], EndUtteranceCommand):
 				# The utterance ends here.
 				break
 			utterance.extend(seq)
+			lastSequenceIndexAddedToUtterance = seqIndex
 		# if any items are cancelled, cancel the whole utterance.
 		if utterance and not self._checkForCancellations(utterance):
+			log.error(f"Checking for cancellations failed, cancelling sequence: {utterance}")
+			# Avoid infinite recursion by removing the problematic sequences:
+			del self._curPriQueue.pendingSequences[:lastSequenceIndexAddedToUtterance + 1]
 			return self._buildNextUtterance()
 		return utterance
 
@@ -542,7 +547,7 @@ class SpeechManager(object):
 		)
 		for index in cancelledIndexes:
 			if (
-				not latestCancelledUtteranceIndex
+				latestCancelledUtteranceIndex is None
 				or self._isIndexABeforeIndexB(latestCancelledUtteranceIndex, index)
 			):
 				latestCancelledUtteranceIndex = index

tests/unit/test_speechManager/__init__.py
@@ -1139,12 +1139,15 @@ class InitialDevelopmentTests_withCancellableSpeechEnabled(InitialDevelopmentTes
 		config.conf['featureFlag']['cancelExpiredFocusSpeech'] = 1  # yes
 
 
-class Test_pr11651(unittest.TestCase):
+class RegressionTests(unittest.TestCase):
+	"""Tests to prevent regressions after issues are fixed.
+	"""
 
 	def test_redundantSequenceAfterEndUtterance(self):
 		"""
 		Tests that redundant param change and index commands are not emitted as an extra utterance
 		when the preceeding utterance contained param change commands and an EndUtterance command.
+		See PR #11651
 		E.g. speaking a character.
 		"""
 		smi = SpeechManagerInteractions(self)
@@ -1168,3 +1171,28 @@ class Test_pr11651(unittest.TestCase):
 			smi.indexReached(1)
 			smi.doneSpeaking()
 			smi.pumpAll()
+
+	def test_nonSpokenCharacter(self):
+		"""Test for fix to GH#11752 - NVDA Freeze with unicode value U+000B
+		Actually, the speech manager receives an empty string for the character U+000B. NVDA
+		does not have a mapping for this character.
+		It is questionable whether we should send anything to the synth when there is no content, however
+		what constitutes 'content' is currently not easy to define.
+		"""
+		smi = SpeechManagerInteractions(self)
+
+		speechSequence = [
+			CharacterModeCommand(True),
+			'',
+			smi.create_EndUtteranceCommand(expectedToBecomeIndex=1)
+		]
+		with smi.expectation():
+			seqIndexes = smi.speak(speechSequence)
+			smi.expect_synthSpeak(seqIndexes)
+
+
+class RegressionTests_withCancellableSpeechEnabled(RegressionTests):
+	"""Note, while cancellable speech is configurable test with and without it enabled."""
+	def setUp(self):
+		super().setUp()
+		config.conf['featureFlag']['cancelExpiredFocusSpeech'] = 1  # yes

commit f99644e9ac9d6910a584b5b2ea2c36be4fc89b5f

Fix frequent error sounds with Cancellable speech (PR #11887)
Fix an issue introduced with #11865, which incorrectly used the return value of _checkForCancellations
to indicate error/success, rather than utterance canceled (correctly) or no cancellations found.

Instead, an exception is raised when the method can not complete, and the error is logged in this case.

source/speech/manager.py
@@ -466,12 +466,21 @@ class SpeechManager(object):
 			utterance.extend(seq)
 			lastSequenceIndexAddedToUtterance = seqIndex
 		# if any items are cancelled, cancel the whole utterance.
-		if utterance and not self._checkForCancellations(utterance):
-			log.error(f"Checking for cancellations failed, cancelling sequence: {utterance}")
+		try:
+			utteranceValid = len(utterance) == 0 or self._checkForCancellations(utterance)
+		except IndexError:
+			log.error(
+				f"Checking for cancellations failed, cancelling sequence: {utterance}",
+				exc_info=True
+			)
 			# Avoid infinite recursion by removing the problematic sequences:
 			del self._curPriQueue.pendingSequences[:lastSequenceIndexAddedToUtterance + 1]
+			utteranceValid = False
+
+		if utteranceValid:
+			return utterance
+		else:
 			return self._buildNextUtterance()
-		return utterance
 
 	def _checkForCancellations(self, utterance: SpeechSequence) -> bool:
 		"""
@@ -485,8 +494,9 @@ class SpeechManager(object):
 			return True
 		utteranceIndex = self._getUtteranceIndex(utterance)
 		if utteranceIndex is None:
-			log.error("no utterance index, cant save cancellable commands")
-			return False
+			raise IndexError(
+				f"no utterance index({utteranceIndex}, cant save cancellable commands"
+			)
 		cancellableItems = list(
 			item for item in reversed(utterance) if isinstance(item, _CancellableSpeechCommand)
 		)