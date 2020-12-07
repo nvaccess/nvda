0f4429c474b820f5af2782d488f83235b5c239a3
Reef Turner <reef@nvaccess.org>
2020-12-07 18:27:27 +0800

    Update Addon API min ver
    2021.1 will have several API breaking changes.
    Addons need to be re-tested and have their manifest updated.

--- source/addonAPIVersion.py
+++ source/addonAPIVersion.py
@@ -13,7 +13,7 @@ how the API has changed as well as the range of API versions supported by this b
 """
 
 CURRENT = (buildVersion.version_year, buildVersion.version_major, buildVersion.version_minor)
-BACK_COMPAT_TO = (2019, 3, 0)
+BACK_COMPAT_TO = (2021, 1, 0)
 """
 As BACK_COMPAT_TO is incremented, the changed / removed parts / or reasoning should be added below.
 EG: (x, y, z): Large changes to speech.py
