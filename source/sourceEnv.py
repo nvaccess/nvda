8ea46a7fb088cb76f82bee6fc895502ca76e7c4e
Łukasz Golonka <lukasz.golonka@mailbox.org>
2020-12-23 13:52:58 +0100

    Stop bundling Py2exe as a git submodule

--- source/sourceEnv.py
+++ source/sourceEnv.py
@@ -17,7 +17,6 @@ PYTHON_DIRS = (
 	os.path.join(TOP_DIR, "include", "comtypes"),
 	os.path.join(TOP_DIR, "include", "configobj", "src"),
 	os.path.join(TOP_DIR, "include", "wxPython"),
-	os.path.join(TOP_DIR, "include", "py2exe"),
 	os.path.join(TOP_DIR, "miscDeps", "python"),
 )
 
