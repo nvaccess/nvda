f33d668f2d29aad01cfc6f72bebd80dca4475527
Joseph Lee <joseph.lee22590@gmail.com>
2020-12-25 14:33:17 -0800

    Dev docs/Sphinx config: update copyright header

--- devDocs/conf.py
+++ devDocs/conf.py
@@ -1,5 +1,5 @@
 # A part of NonVisual Desktop Access (NVDA)
-# Copyright (C) 2019 NV Access Limited, Leonard de Ruijter
+# Copyright (C) 2019-2020 NV Access Limited, Leonard de Ruijter, Joseph Lee
 # This file is covered by the GNU General Public License.
 # See the file COPYING for more details.
 
