19120dd5b212a655d57d525255937e0cfe3d39af
Michael Curran <mick@nvaccess.org>
2021-03-01 19:20:25 +1000

    linting for venvUtils/ensureVenv.py

--- venvUtils/ensureVenv.py
+++ venvUtils/ensureVenv.py
@@ -20,6 +20,7 @@ requirements_path: str = os.path.join(top_dir, "requirements.txt")
 venv_orig_requirements_path: str = os.path.join(venv_path, "_requirements.txt")
 venv_python_version_path: str = os.path.join(venv_path, "python_version")
 
+
 def askYesNoQuestion(message: str) -> bool:
 	"""
 	Displays the given message to the user and accepts y or n as input.
@@ -28,25 +29,28 @@ def askYesNoQuestion(message: str) -> bool:
 	"""
 	while True:
 		answer = input(
-			 message + " [y/n]: "
-		) 
+			message + " [y/n]: "
+		)
 		if answer == 'n':
 			return False
 		elif answer == 'y':
 			return True
 		else:
-			continue # ask again
+			continue  # ask again
+
 
 def fetchRequirementsSet(path: str) -> Set[str]:
 	"""
 	Fetches all the package lines from a pip requirements.txt file
 	returning them as a set of strings.
-	The returned set could be compared with a set from another file which would allow easy identification of which requirements were added or removed.
+	The returned set could be compared with a set from another file
+	which would allow easy identification of which requirements were added or removed.
 	"""
 	with open(path, "r") as f:
 		lines = [x.strip() for x in f.readlines()]
 		lines = [x for x in lines if x and not x.isspace() and not x.startswith('#')]
 	return set(lines)
+
 
 def createVenvAndPopulate():
 	"""
@@ -60,7 +64,7 @@ def createVenvAndPopulate():
 			"-m", "venv",
 			"--clear",
 			venv_path,
-	],
+		],
 		check=True
 	)
 	with open(venv_python_version_path, "w") as f:
@@ -75,7 +79,7 @@ def createVenvAndPopulate():
 			"py", "-m", "pip",
 			"install", "--upgrade", "pip",
 			"&&",
-			# Install required packages with pip 
+			# Install required packages with pip
 			"py", "-m", "pip",
 			"install", "-r", requirements_path,
 		],
@@ -84,10 +88,12 @@ def createVenvAndPopulate():
 	)
 	shutil.copy(requirements_path, venv_orig_requirements_path)
 
+
 def ensureVenvAndRequirements():
 	"""
 	Ensures that the NVDA build system's Python virtual environment is created and up to date.
-	If a previous virtual environment exists but has a miss-matching Python version or pip package requirements have changed,
+	If a previous virtual environment exists but has a miss-matching Python version
+	or pip package requirements have changed,
 	The virtual environment is recreated with the updated version of Python and packages.
 	If a virtual environment is found but does not seem to be ours,
 	This function asks the user if it should be overwritten or not.
@@ -100,7 +106,8 @@ def ensureVenvAndRequirements():
 		or not os.path.exists(venv_orig_requirements_path)
 	):
 		if askYesNoQuestion(
-			f"Virtual environment at {venv_path} probably not created by NVDA. This directory must be removed before continuing. Should it be removed?"
+			f"Virtual environment at {venv_path} probably not created by NVDA. "
+			"This directory must be removed before continuing. Should it be removed?"
 		):
 			return createVenvAndPopulate()
 		else:
@@ -108,7 +115,7 @@ def ensureVenvAndRequirements():
 		sys.exit(1)
 	venv_python_version = open(venv_python_version_path, "r").read()
 	if venv_python_version != sys.version:
-		print(f"Python version changed. Was {venv_python_version}, now is {sys.version}") 
+		print(f"Python version changed. Was {venv_python_version}, now is {sys.version}")
 		return createVenvAndPopulate()
 	oldRequirements = fetchRequirementsSet(venv_orig_requirements_path)
 	newRequirements = fetchRequirementsSet(requirements_path)
@@ -117,13 +124,14 @@ def ensureVenvAndRequirements():
 		print(f"Added or changed package requirements. {addedRequirements}")
 		return createVenvAndPopulate()
 
+
 if __name__ == '__main__':
-	# Ensure we are not inside an already active Python virtual environment. 
+	# Ensure we are not inside an already active Python virtual environment.
 	virtualEnv = os.getenv("VIRTUAL_ENV")
 	if virtualEnv:
 		print(
 			"Error: It looks like another Python virtual environment is already active in this shell.\n"
-			"Please deactivate the current Python virtual environment and try again." 
+			"Please deactivate the current Python virtual environment and try again."
 		)
 		sys.exit(1)
 	ensureVenvAndRequirements()
