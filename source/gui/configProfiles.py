23ed95ebe609ee15cd694332f0eed4fd74b37ee5
buddsean <sean@nvaccess.org>
2021-03-04 17:27:36 +1100

    move to supported usage of wx.StaticBoxSizer
    standardize LTRStaticBoxSizer usage
    
    standardize LTRStaticBoxSizer usage
    
    test wx RTL workaround
    
    use wxWidgets workaround
    
    commit c59e874058cbe0f521806ae3f6ad1c59a68b6324
    Author: buddsean <sean@nvaccess.org>
    Date:   Tue Mar 16 14:09:26 2021 +1100
    
        try parent instead of sibling method
    
    commit 11017debd9047be24938ce4745d3a234ae3e8829
    Author: buddsean <sean@nvaccess.org>
    Date:   Tue Mar 16 13:25:59 2021 +1100
    
        remove LTRStaticBoxSizer workaround
    
    commit e17de62433affe91cc3029a25798f759a5c60702
    Author: buddsean <sean@nvaccess.org>
    Date:   Tue Mar 16 13:14:39 2021 +1100
    
        try parent instead of sibling method
    
    commit 45514f271ec569eb2fa363afbc0f35bd28e7c8f2
    Author: buddsean <sean@nvaccess.org>
    Date:   Tue Mar 16 11:48:24 2021 +1100
    
        try parent instead of sibling method
    
    commit 9575740f52590b9d4fa75e548b8d2b94f344a6dd
    Author: buddsean <sean@nvaccess.org>
    Date:   Tue Mar 16 11:23:59 2021 +1100
    
        try parent instead of sibling method
    
    commit 608fb66bb6c98225ac6298c22efcbd77d4a04ac6
    Author: buddsean <sean@nvaccess.org>
    Date:   Mon Mar 15 14:38:37 2021 +1100
    
        test wx RTL workaround
    
    commit f1189bf06b07c97a398169484ffcfe2c117e6bba
    Author: buddsean <sean@nvaccess.org>
    Date:   Mon Mar 15 14:03:20 2021 +1100
    
        test wx RTL workaround

--- source/gui/configProfiles.py
+++ source/gui/configProfiles.py
@@ -42,12 +42,13 @@ class ProfilesDialog(
 
 		mainSizer = wx.BoxSizer(wx.VERTICAL)
 		sHelper = guiHelper.BoxSizerHelper(self,orientation=wx.VERTICAL)
-		profilesListGroupSizer = wx.StaticBoxSizer(wx.StaticBox(self), wx.HORIZONTAL)
+		profilesListGroupSizer = wx.StaticBoxSizer(wx.HORIZONTAL, self)
+		profilesListBox = profilesListGroupSizer.GetStaticBox()
 		profilesListGroupContents = wx.BoxSizer(wx.HORIZONTAL)
 
 		#contains the profile list and activation button in vertical arrangement.
 		changeProfilesSizer = wx.BoxSizer(wx.VERTICAL)
-		item = self.profileList = wx.ListBox(self,
+		item = self.profileList = wx.ListBox(profilesListBox,
 			choices=[self.getProfileDisplay(name, includeStates=True) for name in self.profileNames])
 		self.bindHelpEvent("ProfilesBasicManagement", self.profileList)
 		item.Bind(wx.EVT_LISTBOX, self.onProfileListChoice)
@@ -56,7 +57,7 @@ class ProfilesDialog(
 
 		changeProfilesSizer.AddSpacer(guiHelper.SPACE_BETWEEN_BUTTONS_VERTICAL)
 
-		self.changeStateButton = wx.Button(self)
+		self.changeStateButton = wx.Button(profilesListBox)
 		self.bindHelpEvent("ConfigProfileManual", self.changeStateButton)
 		self.changeStateButton.Bind(wx.EVT_BUTTON, self.onChangeState)
 		self.AffirmativeId = self.changeStateButton.Id
@@ -68,17 +69,17 @@ class ProfilesDialog(
 
 		buttonHelper = guiHelper.ButtonHelper(wx.VERTICAL)
 		# Translators: The label of a button to create a new configuration profile.
-		newButton = buttonHelper.addButton(self, label=_("&New"))
+		newButton = buttonHelper.addButton(profilesListBox, label=_("&New"))
 		self.bindHelpEvent("ProfilesCreating", newButton)
 		newButton.Bind(wx.EVT_BUTTON, self.onNew)
 
 		# Translators: The label of a button to rename a configuration profile.
-		self.renameButton = buttonHelper.addButton(self, label=_("&Rename"))
+		self.renameButton = buttonHelper.addButton(profilesListBox, label=_("&Rename"))
 		self.bindHelpEvent("ProfilesBasicManagement", self.renameButton)
 		self.renameButton.Bind(wx.EVT_BUTTON, self.onRename)
 
 		# Translators: The label of a button to delete a configuration profile.
-		self.deleteButton = buttonHelper.addButton(self, label=_("&Delete"))
+		self.deleteButton = buttonHelper.addButton(profilesListBox, label=_("&Delete"))
 		self.bindHelpEvent("ProfilesBasicManagement", self.deleteButton)
 		self.deleteButton.Bind(wx.EVT_BUTTON, self.onDelete)
 
