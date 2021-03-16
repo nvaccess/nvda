714e3456c457f63fc61f6bb2fabd3b8284ce151b
Michael Curran <mick@nvaccess.org>
2021-03-17 08:33:00 +1000

    venvUtils\ensureAndActivate.bat: rather than calling the standard activate.bat from the generated .venv, set the necessary environment variables manually, and make VIRTUAL_ENV be relative to this script, rather than a hard-coded path. This allows the NVDA repository to be moved or copied somewhere else on the same system and the environment used without having to be recreated.

--- venvUtils/ensureAndActivate.bat
+++ venvUtils/ensureAndActivate.bat
@@ -1,9 +1,19 @@
 @echo off
 rem this script ensures the NVDA build system Python virtual environment is created and up to date,
 rem and then activates it.
-rem this script should be used only in the case where many commands will be executed within the environment and the shell will be eventually thrown away. 
-rem E.g. an Appveyor build.
+rem This is an internal script and should not be used directly.
+
+rem Ensure the environment is created and up to date
 py -3.8-32 "%~dp0\ensureVenv.py"
 if ERRORLEVEL 1 goto :EOF
-call "%~dp0\..\.venv\scripts\activate.bat"
+rem unset the PYTHONHOME variable so as to not pick up possible global site-packages
+set PYTHONHOME=
+rem set the VIRTUAL_ENV variable instructing Python to use a virtual environment
+set VIRTUAL_ENV=%~dp0..\.venv
+rem Add the virtual environment's scripts directory to the path
+set PATH=%VIRTUAL_ENV%\scripts;%PATH%
+rem Set an NVDA-specific variable to identify this official NVDA virtual environment from other 3rd party ones
 set NVDA_VENV=%VIRTUAL_ENV%
+rem mention the environment in the prompt to make it obbvious it is active
+rem just in case this script is executed outside of a local block and not cleaned up.
+set PROMPT=[NVDA Venv] %PROMPT%
