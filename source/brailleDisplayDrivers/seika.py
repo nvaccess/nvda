2c8fbc22c32e5b60600fe675f1ab6ca211d26363
Reef Turner <reef@nvaccess.org>
2021-04-01 16:17:44 +0800

    Use f-strings

--- source/brailleDisplayDrivers/seika.py
+++ source/brailleDisplayDrivers/seika.py
@@ -47,22 +47,25 @@ class BrailleDisplayDriver(braille.BrailleDisplayDriver):
 				self._ser = serial.Serial(port, baudrate=BAUDRATE, timeout=TIMEOUT, writeTimeout=TIMEOUT, parity=serial.PARITY_ODD, bytesize=serial.EIGHTBITS, stopbits=serial.STOPBITS_ONE)
 			except serial.SerialException:
 				continue
-			log.debug("serial port open {port}".format(port=port))
+
+			log.debug(f"serial port open {port}")
 			# get the version infos
 			self._ser.write(BUF_START + b"\x1C")
 			self._ser.flush()
 			# Read out the input buffer
 			versionS = self._ser.read(13)
-			log.debug("receive {p}".format(p=versionS))
+			log.debug(f"receive {versionS}")
 			if versionS.startswith(b"seika80"):
-				log.info("Found Seika80 connected via {port} Version {versionS}".format(port=port, versionS=versionS))
+				log.info(f"Found Seika80 connected via {port} Version {versionS}")
 				self.numCells = 80
+				self._maxCellRead = 20
 				# data header for seika 80
 				self.sendHeader = (BUF_START + b"s80").ljust(8, b"\x00")
 				break
 			if versionS.startswith(b"seika3"):
-				log.info("Found Seika3/5 connected via {port} Version {versionS}".format(port=port, versionS=versionS))
+				log.info(f"Found Seika3/5 connected via {port} Version {versionS}")
 				self.numCells = 40
+				self._maxCellRead = 10
 				# data header for v3, v5
 				self.sendHeader = (BUF_START + b"seika").ljust(8, b"\x00")
 				break
@@ -72,13 +75,14 @@ class BrailleDisplayDriver(braille.BrailleDisplayDriver):
 			self._ser.flush()
 			# Read out the input buffer
 			versionS = self._ser.read(12)
-			log.debug("receive {p}".format(p=versionS))
+			log.debug(f"receive {versionS}")
 			if versionS.startswith(prefix=(
 				b'\x00\x05(\x08v5.0\x01\x01\x01\x01',
 				b'\x00\x05(\x08seika\x00'
 			)):
-				log.info("Found Seika3 old Version connected via {port} Version {versionS}".format(port=port, versionS=versionS))
+				log.info(f"Found Seika3 old Version connected via {port} Version {versionS}")
 				self.numCells = 40
+				self._maxCellRead = 10
 				self.sendHeader = BUF_START + b"\x04\x00\x63\x00\x50\x00"
 				break
 			self._ser.close()
@@ -110,55 +114,59 @@ class BrailleDisplayDriver(braille.BrailleDisplayDriver):
 	def handleResponses(self):
 		if not self._ser.in_waiting:
 			return
-		key = 0
-		keys = set()
-		maxCellRead = self.numCells // 4  # for 80 maxCellRead is 20, for 40 cell maxCellRead is 10
+
 		chars: bytes = self._ser.read(2)
-		keytyp=1
-		if not chars[0] & 0x60: # a cursorrouting block is expected
-			chars: bytes = self._ser.read(maxCellRead)
-			keytyp=2
+		isCursorRoutingBlock = not chars[0] & 0x60
 		
-		if keytyp == 1: # normal key
-			if chars[0] & 1: # LEFT
-				keys.add("left")
-			if chars[0] & 4: # RIGHT
-				keys.add("right")
-			if chars[1] & 2: # B1 
-				keys.add("b1")
-			if chars[1] & 8:
-				keys.add("b2")
-			if chars[1] & 16:
-				keys.add("b3")
-			if chars[0] & 16:
-				keys.add("b4")
-			if chars[0] & 8:
-				keys.add("b5")
-			if chars[0] & 2:
-				keys.add("b6")
-			data= "+".join(keys)
+		if not isCursorRoutingBlock:  # normal key
+			self._handleNormalKey(chars)
+		else:  # Cursor Routing Block
+			chars: bytes = self._ser.read(self._maxCellRead)
+			self._handleCursorRoutingBlock(chars)
+
+	def _handleCursorRoutingBlock(self, chars: bytes) -> None:
+		key = 0
+		i = 0
+		k = self._maxCellRead // 2
+		while i < k:
+			j = 0
+			while j < 8:
+				if chars[5 + i] & (1 << j):
+					key = i * 8 + j + 1
+					break
+				j += 1
+			i += 1
+		if key:  # Routing key is pressed
 			try:
-				inputCore.manager.executeGesture(InputGestureKeys(data))
+				inputCore.manager.executeGesture(InputGestureRouting(key - 1))
 			except inputCore.NoInputGestureAction:
-				log.debug("No Action for keys {keys}".format(keys=data))
+				log.debug("No Action for routing command")
 				pass
-		else:
-			i = 0
-			k = maxCellRead // 2
-			while i < k:
-				j = 0
-				while j < 8:
-					if chars[5+i] & (1<<j):
-						key = i*8+j+1
-						break
-					j+=1
-				i+=1
-			if key: # Routing key is pressed
-				try:
-					inputCore.manager.executeGesture(InputGestureRouting(key-1))
-				except inputCore.NoInputGestureAction:
-					log.debug("No Action for routing command")
-					pass
+
+	def _handleNormalKey(self, chars: bytes) -> None:
+		keys = set()
+		if chars[0] & 1:  # LEFT
+			keys.add("left")
+		if chars[0] & 4:  # RIGHT
+			keys.add("right")
+		if chars[1] & 2:  # B1
+			keys.add("b1")
+		if chars[1] & 8:
+			keys.add("b2")
+		if chars[1] & 16:
+			keys.add("b3")
+		if chars[0] & 16:
+			keys.add("b4")
+		if chars[0] & 8:
+			keys.add("b5")
+		if chars[0] & 2:
+			keys.add("b6")
+		data = "+".join(keys)
+		try:
+			inputCore.manager.executeGesture(InputGestureKeys(data))
+		except inputCore.NoInputGestureAction:
+			log.debug(f"No Action for keys {data}")
+			pass
 
 	gestureMap = inputCore.GlobalGestureMap({
 		"globalCommands.GlobalCommands": {
