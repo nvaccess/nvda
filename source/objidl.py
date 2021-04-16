246b414d85e394e496612ed913a9fb43e11e0f8a
Łukasz Golonka <lukasz.golonka@mailbox.org>
2021-04-16 14:26:28 +0200

    Lint for objidl:
    - Standardize copyright header.
    - Do not rely on star imports from ctypes

--- source/objidl.py
+++ source/objidl.py
@@ -1,9 +1,9 @@
-#objidl.py
-#A part of NonVisual Desktop Access (NVDA)
-#This file is covered by the GNU General Public License.
-#See the file COPYING for more details.
+# A part of NonVisual Desktop Access (NVDA)
+# Copyright (C) 2010-2021 NV Access Limited, Leonard de Ruijter, Joseph Lee
+# This file may be used under the terms of the GNU General Public License, version 2 or later.
+# For more details see: https://www.gnu.org/licenses/gpl-2.0.html
 
-from ctypes import *
+from ctypes import c_int, c_longlong, c_ubyte, c_ulong, c_ulonglong, c_wchar_p, POINTER, Structure, windll
 from ctypes.wintypes import HWND, BOOL
 from comtypes import HRESULT, GUID, COMMETHOD, IUnknown, tagBIND_OPTS2
 from comtypes.persist import IPersist
