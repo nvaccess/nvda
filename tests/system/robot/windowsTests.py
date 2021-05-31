784f085fa9ae8df4320b374b459a9f14d286e77a
buddsean <sean@nvaccess.org>
2021-05-31 16:44:01 +1000

    emoji panel and clipboard system tests

---
+++ tests/system/robot/windowsTests.py
@@ -0,0 +1,107 @@
+# A part of NonVisual Desktop Access (NVDA)
+# Copyright (C) 2021 NV Access Limited
+# This file may be used under the terms of the GNU General Public License, version 2 or later.
+# For more details see: https://www.gnu.org/licenses/gpl-2.0.html
+
+"""Logic for testing NVDA interacting with the Windows system.
+"""
+from robot.libraries.BuiltIn import BuiltIn
+# relative import not used for 'systemTestUtils' because the folder is added to the path for 'libraries'
+# imported methods start with underscore (_) so they don't get imported into robot files as keywords
+from SystemTestSpy import (
+	_getLib,
+)
+
+# Imported for type information
+from robot.libraries.Process import Process as _ProcessLib
+
+from AssertsLib import AssertsLib as _AssertsLib
+import NvdaLib as _nvdaLib
+from NvdaLib import NvdaLib as _nvdaRobotLib
+_nvdaProcessAlias = _nvdaRobotLib.nvdaProcessAlias
+
+builtIn: BuiltIn = BuiltIn()
+_process: _ProcessLib = _getLib("Process")
+_asserts: _AssertsLib = _getLib("AssertsLib")
+
+
+def open_clipboard_history() -> str:
+	"""
+	Returns the last read item after opening the clipboard history.
+	"""
+	spy = _nvdaLib.getSpyLib()
+	spy.emulateKeyPress("leftWindows+v")
+	spy.wait_for_speech_to_finish()
+	return spy.get_last_speech()
+
+
+def write_and_copy_text(text: str):
+	"""
+	Expects an editable field to be focused.
+	"""
+	spy = _nvdaLib.getSpyLib()
+	spy.emulateKeyPress("control+a")
+	for c in text:
+		spy.emulateKeyPress(c, blockUntilProcessed=False)
+	spy.wait_for_speech_to_finish()
+	spy.emulateKeyPress("control+a")
+	spy.emulateKeyPress("control+c")
+
+
+def read_clipboard_history(*expectedClipboardHistory: str):
+	"""
+	Expects the clipboard panel to already be open and focused on an editable field.
+	"""
+	spy = _nvdaLib.getSpyLib()
+	spy.wait_for_speech_to_finish()
+	for copyText in expectedClipboardHistory:
+		spy.wait_for_specific_speech(copyText)
+		spy.emulateKeyPress('rightArrow')
+
+
+def open_emoji_panel() -> str:
+	"""
+	Returns the first read emoji after opening the emoji panel.
+	"""
+	spy = _nvdaLib.getSpyLib()
+	spy.emulateKeyPress("leftWindows+.")
+	spy.wait_for_speech_to_finish()
+	lastSpeech = spy.get_last_speech()
+	try:
+		return lastSpeech.split("  ")[1]
+	except IndexError:
+		AssertionError(f"Emoji not announced. Last speech: '{lastSpeech}'")
+
+
+def search_emojis(emojiNameSearchStr: str):
+	"""
+	Types a string search with the emoji panel open.
+	Expects the emoji panel to already be open and focused on an editable field.
+	"""
+	spy = _nvdaLib.getSpyLib()
+	for c in emojiNameSearchStr:
+		spy.emulateKeyPress(c, blockUntilProcessed=False)
+	spy.wait_for_speech_to_finish()
+
+
+def read_emojis(*expectedEmojiNameList: str):
+	"""
+	Navigates the emoji panel and confirms the expected emoji list.
+	Expects the emoji panel to already be open.
+	"""
+	spy = _nvdaLib.getSpyLib()
+	for emojiName in expectedEmojiNameList:
+		spy.wait_for_specific_speech(emojiName)
+		spy.emulateKeyPress('rightArrow')
+
+
+def open_windows_search():
+	spy = _nvdaLib.getSpyLib()
+	spy.emulateKeyPress("leftWindows+s")
+	spy.wait_for_speech_to_finish()
+
+
+def close_windows_search():
+	spy = _nvdaLib.getSpyLib()
+	spy.emulateKeyPress("escape")  # escape any focus or close the win search bar
+	spy.emulateKeyPress("escape")  # ensure the win search bar is closed
---
+++ tests/system/robot/windowsTests.robot
@@ -0,0 +1,70 @@
+# A part of NonVisual Desktop Access (NVDA)
+# Copyright (C) 2021 NV Access Limited
+# This file may be used under the terms of the GNU General Public License, version 2 or later.
+# For more details see: https://www.gnu.org/licenses/gpl-2.0.html
+*** Settings ***
+Documentation	Tests how NVDA interacts with various features of the Windows system
+Force Tags	NVDA	smoke test
+
+# for start & quit in Test Setup and Test Test Teardown
+Library	NvdaLib.py
+Library	windowsTests.py
+Library	ScreenCapLibrary
+
+Test Setup	start NVDA	standard-dontShowWelcomeDialog.ini
+Test Teardown	default teardown
+
+*** Keywords ***
+default teardown
+	${screenshotName}=	create_preserved_test_output_filename	failedTest.png
+	Run Keyword If Test Failed	Take Screenshot	${screenShotName}
+	quit NVDA
+
+setup and open windows search
+	start NVDA	standard-dontShowWelcomeDialog.ini
+	open windows search
+
+close windows search and teardown
+	default teardown
+	close windows search
+
+*** Test Cases ***
+emoji panel search
+	[Documentation]	Read emoji by navigating the emoji panel
+	[Setup]	setup and open windows search
+	[Teardown]	close windows search and teardown
+	open emoji panel
+	search emojis	came
+	read emojis	camel	two-hump camel	camera
+
+
+emoji panel open
+	[Documentation]	Confirm that opening the emoji panel announces an emoji
+	[Setup]	setup and open windows search
+	[Teardown]	close windows search and teardown
+	${firstEmoji}=	open emoji panel	# set expected first emoji
+	search emojis	${firstEmoji}
+	read emojis	${firstEmoji}
+
+
+clipboard history
+	[Documentation]	Copy text and read from the clipboard history
+	[Setup]	setup and open windows search
+	[Teardown]	close windows search and teardown
+	write and copy text	foo
+	write and copy text	lorem ipsum
+	write and copy text	bar
+	open clipboard history
+	read clipboard history	bar	lorem ipsum	foo
+
+
+toggle between emoji panel and clipboard history
+	[Documentation]	Toggle between clipboard history and emoji panel and ensure items are announced
+	[Setup]	setup and open windows search
+	[Teardown]	close windows search and teardown
+	write and copy text	test toggle between
+	${firstEmoji}=	open emoji panel
+	open clipboard history
+	read clipboard history	test toggle between
+	open emoji panel
+	read emojis	${firstEmoji}
