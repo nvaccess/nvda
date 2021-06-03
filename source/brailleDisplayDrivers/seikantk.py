3741220f767cafaeee54ea9760d7114540b1445a
Reef Turner <feerrenrut@users.noreply.github.com>
2021-06-03 11:07:31 +0800

    Apply review actions
    Co-authored-by: Sean Budd <seanbudd123@gmail.com>

--- source/brailleDisplayDrivers/seikantk.py
+++ source/brailleDisplayDrivers/seikantk.py
@@ -1,5 +1,5 @@
 # A part of NonVisual Desktop Access (NVDA)
-# Copyright (C) 2012-2020 NV Access Limited, Ulf Beckmann <beckmann@flusoft.de>
+# Copyright (C) 2012-2021 NV Access Limited, Ulf Beckmann <beckmann@flusoft.de>
 # This file may be used under the terms of the GNU General Public License, version 2 or later.
 # For more details see: https://www.gnu.org/licenses/gpl-2.0.html
 #
