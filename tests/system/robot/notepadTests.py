d89a533dd90907d2c240836c0488385c827ae56f
buddsean <sean@nvaccess.org>
2021-06-07 17:41:07 +1000

    add notepad system tests for symbol level word
    safer attempt at closing notepad
    
    genericise notepad regex

---
+++ tests/system/robot/notepadTests.py
@@ -0,0 +1,40 @@
+# A part of NonVisual Desktop Access (NVDA)
+# Copyright (C) 2021 NV Access Limited
+# This file may be used under the terms of the GNU General Public License, version 2 or later.
+# For more details see: https://www.gnu.org/licenses/gpl-2.0.html
+
+"""Logic for reading text using NVDA in the notepad text editor.
+"""
+# imported methods start with underscore (_) so they don't get imported into robot files as keywords
+from SystemTestSpy import (
+	_getLib,
+)
+
+# Imported for type information
+from NotepadLib import NotepadLib as _NotepadLib
+from AssertsLib import AssertsLib as _AssertsLib
+
+_notepad: _NotepadLib = _getLib("NotepadLib")
+_asserts: _AssertsLib = _getLib("AssertsLib")
+
+
+def test_symbolLevelWord(isSymbolLevelWordAllExpected=True):
+	wordsWithSymbols = ['He', 'said', '(quietly),', '"Hello,', 'Jim".']
+	symbolMap = {
+		'(': 'left paren',
+		')': 'right paren',
+		',': 'comma',
+		'"': 'quote',
+		'.': 'dot',
+	}
+	textStr = ' '.join(wordsWithSymbols)
+	_asserts.strings_match(textStr, 'He said (quietly), "Hello, Jim".')
+
+	_notepad.prepareNotepad(f"Test: {textStr}")
+	for expectedWord in wordsWithSymbols:
+		wordSpoken = _notepad.getSpeechAfterKey("numpad6")
+		for symbol in symbolMap.keys():
+			if isSymbolLevelWordAllExpected:
+				expectedWord = expectedWord.replace(symbol, f" {symbolMap[symbol]}{symbol}")
+		expectedWord = expectedWord.replace('"', ' ').strip()  # always strip quote symbols
+		_asserts.strings_match(wordSpoken, expectedWord)
