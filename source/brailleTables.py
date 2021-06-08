60c2a01eff0f88bfa91a445a5faa5bf1ad19aa18
André-Abush Clause <dev@andreabc.net>
2021-06-08 07:38:59 +0200

    Update copyright years

--- source/brailleTables.py
+++ source/brailleTables.py
@@ -1,7 +1,7 @@
 # A part of NonVisual Desktop Access (NVDA)
 # This file is covered by the GNU General Public License.
 # See the file COPYING for more details.
-# Copyright (C) 2008-2020 NV Access Limited, Joseph Lee, Babbage B.V.
+# Copyright (C) 2008-2021 NV Access Limited, Joseph Lee, Babbage B.V.
 
 """Manages information about available braille translation tables.
 """
