nvaccess/nvda

commit 93bbd21b77e1596309a67a4e4fc291b9f3af7589

Add --copy-portable-config command line parameter to automatically copy the portable configuration with a silent installation (#9679)
* gui.installerGui.doInstall, kwargs

* Added command line parameter to copy the portable configuration when installing

* Fix linting issues

* Fix linting

Co-authored-by: Leonard de Ruijter <leonard@babbage.com>

source/gui/installerGui.py
@@ -22,7 +22,15 @@ from gui.dpiScalingHelper import DpiScalingHelperMixin
 import tones
 import systemUtils
 
-def doInstall(createDesktopShortcut,startOnLogon,copyPortableConfig,isUpdate,silent=False,startAfterInstall=True):
+
+def doInstall(
+		createDesktopShortcut=True,
+		startOnLogon=True,
+		isUpdate=False,
+		copyPortableConfig=False,
+		silent=False,
+		startAfterInstall=True
+):
 	progressDialog = gui.IndeterminateProgressDialog(gui.mainFrame,
 		# Translators: The title of the dialog presented while NVDA is being updated.
 		_("Updating NVDA") if isUpdate
@@ -55,7 +63,14 @@ def doInstall(createDesktopShortcut,startOnLogon,copyPortableConfig,isUpdate,sil
 		# Translators: the title of a retry cancel dialog when NVDA installation fails
 		title=_("File in Use")
 		if winUser.MessageBox(None,message,title,winUser.MB_RETRYCANCEL)==winUser.IDRETRY:
-			return doInstall(createDesktopShortcut,startOnLogon,copyPortableConfig,isUpdate,silent,startAfterInstall)
+			return doInstall(
+				createDesktopShortcut=createDesktopShortcut,
+				startOnLogon=startOnLogon,
+				copyPortableConfig=copyPortableConfig,
+				isUpdate=isUpdate,
+				silent=silent,
+				startAfterInstall=startAfterInstall
+			)
 	if res!=0:
 		log.error("Installation failed: %s"%res)
 		# Translators: The message displayed when an error occurs during installation of NVDA.
@@ -88,16 +103,20 @@ def doInstall(createDesktopShortcut,startOnLogon,copyPortableConfig,isUpdate,sil
 	else:
 		wx.GetApp().ExitMainLoop()
 
-def doSilentInstall(startAfterInstall=True):
+
+def doSilentInstall(
+		copyPortableConfig=False,
+		startAfterInstall=True
+):
 	prevInstall=installer.comparePreviousInstall() is not None
 	startOnLogon=globalVars.appArgs.enableStartOnLogon
 	if startOnLogon is None:
 		startOnLogon=config.getStartOnLogonScreen() if prevInstall else True
 	doInstall(
-		installer.isDesktopShortcutInstalled() if prevInstall else True,
-		startOnLogon,
-		False,
-		prevInstall,
+		createDesktopShortcut=installer.isDesktopShortcutInstalled() if prevInstall else True,
+		startOnLogon=startOnLogon,
+		isUpdate=prevInstall,
+		copyPortableConfig=copyPortableConfig,
 		silent=True,
 		startAfterInstall=startAfterInstall
 	)
@@ -181,8 +200,10 @@ class InstallerDialog(wx.Dialog, DpiScalingHelperMixin):
 		# Translators: The label of a checkbox option in the Install NVDA dialog.
 		createPortableText = _("Copy &portable configuration to current user account")
 		self.copyPortableConfigCheckbox = optionsSizer.addItem(wx.CheckBox(self, label=createPortableText))
-		self.copyPortableConfigCheckbox.Value = False
-		if globalVars.appArgs.launcher:
+		self.copyPortableConfigCheckbox.Value = bool(globalVars.appArgs.copyPortableConfig)
+		if globalVars.appArgs.copyPortableConfig is None:
+			# copyPortableConfig is set to C{None} in the main loop,
+			# when copying the portable configuration should be disabled at all costs.
 			self.copyPortableConfigCheckbox.Disable()
 
 		bHelper = sHelper.addDialogDismissButtons(guiHelper.ButtonHelper(wx.HORIZONTAL))
@@ -213,7 +234,12 @@ class InstallerDialog(wx.Dialog, DpiScalingHelperMixin):
 
 	def onInstall(self, evt):
 		self.Hide()
-		doInstall(self.createDesktopShortcutCheckbox.Value,self.startOnLogonCheckbox.Value,self.copyPortableConfigCheckbox.Value,self.isUpdate)
+		doInstall(
+			createDesktopShortcut=self.createDesktopShortcutCheckbox.Value,
+			startOnLogon=self.startOnLogonCheckbox.Value,
+			copyPortableConfig=self.copyPortableConfigCheckbox.Value,
+			silent=self.isUpdate
+		)
 		self.Destroy()
 
 	def onCancel(self, evt):
@@ -227,7 +253,9 @@ class InstallerDialog(wx.Dialog, DpiScalingHelperMixin):
 		)
 		incompatibleAddons.ShowModal()
 
+
 class InstallingOverNewerVersionDialog(wx.Dialog, DpiScalingHelperMixin):
+
 	def __init__(self):
 		# Translators: The title of a warning dialog.
 		wx.Dialog.__init__(self, gui.mainFrame, title=_("Warning"))

source/core.py
@@ -228,6 +228,17 @@ def main():
 	log.debug("loading config")
 	import config
 	config.initialize()
+	if globalVars.appArgs.configPath == config.getUserDefaultConfigPath(useInstalledPathIfExists=True):
+		# Make sure not to offer the ability to copy the current configuration to the user account.
+		# This case always applies to the launcher when configPath is not overridden by the user,
+		# which is the default.
+		# However, if a user wants to run the launcher with a custom configPath,
+		# it is likely that he wants to copy that configuration when installing.
+		# This check also applies to cases where a portable copy is run using the installed configuration,
+		# in which case we want to avoid copying a configuration to itself.
+		# We set the value to C{None} in order for the gui to determine
+		# when to disable the checkbox for this feature.
+		globalVars.appArgs.copyPortableConfig = None
 	if config.conf['development']['enableScratchpadDir']:
 		log.info("Developer Scratchpad mode enabled")
 	if not globalVars.appArgs.minimal and config.conf["general"]["playStartAndExitSounds"]:
@@ -483,7 +494,11 @@ def main():
 	globalPluginHandler.initialize()
 	if globalVars.appArgs.install or globalVars.appArgs.installSilent:
 		import gui.installerGui
-		wx.CallAfter(gui.installerGui.doSilentInstall,startAfterInstall=not globalVars.appArgs.installSilent)
+		wx.CallAfter(
+			gui.installerGui.doSilentInstall,
+			copyPortableConfig=globalVars.appArgs.copyPortableConfig,
+			startAfterInstall=not globalVars.appArgs.installSilent
+		)
 	elif globalVars.appArgs.portablePath and (globalVars.appArgs.createPortable or globalVars.appArgs.createPortableSilent):
 		import gui.installerGui
 		wx.CallAfter(gui.installerGui.doCreatePortable,portableDirectory=globalVars.appArgs.portablePath,

source/nvda.pyw
@@ -90,6 +90,7 @@ def stringToBool(string):
 	except ValidateError as e:
 		raise argparse.ArgumentTypeError(e.message)
 
+
 #Process option arguments
 parser=NoConsoleOptionParser()
 quitGroup = parser.add_mutually_exclusive_group()
@@ -113,6 +114,16 @@ parser.add_argument('--portable-path',dest='portablePath',default=None,type=str,
 parser.add_argument('--launcher',action="store_true",dest='launcher',default=False,help="Started from the launcher")
 parser.add_argument('--enable-start-on-logon',metavar="True|False",type=stringToBool,dest='enableStartOnLogon',default=None,
 	help="When installing, enable NVDA's start on the logon screen")
+parser.add_argument(
+	'--copy-portable-config',
+	action="store_true",
+	dest='copyPortableConfig',
+	default=False,
+	help=(
+		"When installing, copy the portable configuration "
+		"from the provided path (--config-path, -c) to the current user account"
+	)
+)
 # This option is passed by Ease of Access so that if someone downgrades without uninstalling
 # (despite our discouragement), the downgraded copy won't be started in non-secure mode on secure desktops.
 # (Older versions always required the --secure option to start in secure mode.)

user_docs/en/userGuide.t2t
@@ -3128,6 +3128,7 @@ Following are the command line options for NVDA:
 | None | --install | Installs NVDA (starting the newly installed copy) |
 | None | --install-silent | Silently installs NVDA (does not start the newly installed copy) |
 | None | --enable-start-on-logon=True|False | When installing, enable NVDA's [Use NVDA during Windows sign-in #StartAtWindowsLogon] |
+| None | --copy-portable-config | When installing, copy the portable configuration from the provided path (--config-path, -c) to the current user account |
 | None | --create-portable | Creates a portable copy of NVDA (starting the newly created copy). Requires --portable-path to be specified |
 | None | --create-portable-silent | Creates a portable copy of NVDA (does not start the newly installed copy). Requires --portable-path to be specified |
 | None | --portable-path=PORTABLEPATH | The path where a portable copy will be created |

commit 5ec5ee939e9fd204e938c9ecd298c1957683ad8e

Fix conflation of silent and update
Fix an issue introduced in commit 93bbd21b77e1596309a67a4e4fc291b9f3af7589
"Add --copy-portable-config command line parameter to automatically copy
 the portable configuration with a silent installation #9679"

source/gui/installerGui.py
@@ -260,7 +260,7 @@ class InstallerDialog(
 			createDesktopShortcut=self.createDesktopShortcutCheckbox.Value,
 			startOnLogon=self.startOnLogonCheckbox.Value,
 			copyPortableConfig=self.copyPortableConfigCheckbox.Value,
-			silent=self.isUpdate
+			isUpdate=self.isUpdate
 		)
 		wx.GetApp().ScheduleForDestruction(self)
 