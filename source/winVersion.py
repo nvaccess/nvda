f391a2dbad6ca0ef8b3f3bcea10b2692b2309011
Joseph Lee <joseph.lee22590@gmail.com>
2021-06-23 10:53:53 -0700

    Revert "winVersion/Windows 10 releases: remove unused releases to builds map. Re #12509."
    This reverts commit e5f39aa0ba08a1ed05f2bde7ec2ec92373f743ba.

--- source/winVersion.py
+++ source/winVersion.py
@@ -144,5 +144,21 @@ def isUwpOcrAvailable():
 	return os.path.isdir(UWP_OCR_DATA_PATH)
 
 
+WIN10_RELEASE_NAME_TO_BUILDS = {
+	"1507": 10240,
+	"1511": 10586,
+	"1607": 14393,
+	"1703": 15063,
+	"1709": 16299,
+	"1803": 17134,
+	"1809": 17763,
+	"1903": 18362,
+	"1909": 18363,
+	"2004": 19041,
+	"20H2": 19042,
+	"21H1": 19043,
+}
+
+
 def isFullScreenMagnificationAvailable():
 	return getWinVer() >= WIN8
