8c538375517e2e0de3e0fcab3ce68db8b4aee995
buddsean <sean@nvaccess.org>
2021-06-24 10:35:14 +1000

    add unit testing for seika notetaker driver

--- tests/unit/test_brailleDisplayDrivers.py
+++ tests/unit/test_brailleDisplayDrivers.py
@@ -6,8 +6,43 @@
 """Unit tests for braille display drivers.
 """
 
+from brailleDisplayDrivers.seikantk import BrailleDisplayDriver as SeikaNotetakerDriver, SEIKA_INFO
+
 import unittest
 import braille
+
+
+class TestSeikaNotetakerDriver(unittest.TestCase):
+	def test_onReceive(self):
+		sampleCommand = SEIKA_INFO
+		sampleArgument = b"test"
+		sampleArgLen = bytes([len(sampleArgument)])
+		sampleMessage = sampleCommand + sampleArgLen + sampleArgument + b"\0\0\0"
+		paddedMessage = b"\0" + sampleMessage + b"\0"
+
+		class FakeSeikaNotetakerDriver(SeikaNotetakerDriver):
+			def __init__(self):
+				"""Sets the variables necessary to test _onReceive without a braille device
+				"""
+				self._hidBuffer = b""
+				self._command = None
+				self._argsLen = None
+
+			def _processCommand(self, command, arg):
+				"""Intercept processCommand to confirm _onReceive processes a message correctly.
+				"""
+				self._finalCommand = command
+				self._finalArg = arg
+		
+		seikaTestDriver = FakeSeikaNotetakerDriver()
+		for i in range(len(sampleMessage)):
+			# the middle byte is the only one used, padded by 2 other bits
+			# paddedMessage[i:i + 2] == [sampleMessage[i-1], sampleMessage[i], sampleMessage[i+1]]
+			seikaTestDriver._onReceive(paddedMessage[i:i + 2])
+
+		self.assertEqual(sampleCommand, seikaTestDriver._finalCommand)
+		self.assertEqual(sampleArgLen + sampleArgument, seikaTestDriver._finalArg)
+
 
 class TestGestureMap(unittest.TestCase):
 	"""Tests the integrity of braille display driver gesture maps."""
