1ad586e21812f502dd89a818764056b619ed0e6e
buddsean <sean@nvaccess.org>
2021-07-05 11:18:47 +1000

    don't exit on warnings

--- appveyor/scripts/decryptFilesForSigning.ps1
+++ appveyor/scripts/decryptFilesForSigning.ps1
@@ -3,14 +3,18 @@ if(!$env:APPVEYOR_PULL_REQUEST_NUMBER) {
 	Try {
 		openssl enc -d -md sha256 -aes-256-cbc -pbkdf2 -salt -pass pass:$env:secure_authenticode_pass -in appveyor\authenticode.pfx.enc -out appveyor\authenticode.pfx
 	} Catch {
-		$errorCode=1
-		Add-AppveyorMessage "Unable to decrypt authenticode certificate"
+		if($LastExitCode -ne 0) {
+			$errorCode=1
+			Add-AppveyorMessage "Unable to decrypt authenticode certificate"
+		}
 	}
 	Try {
 		openssl enc -md md5 -aes-256-cbc -d -pass pass:$env:secure_ssh_pass -in appveyor\ssh_id_rsa.enc -out appveyor\ssh_id_rsa
 	} Catch {
-		$errorCode=1
-		Add-AppveyorMessage "Unable to decrypt ssh key"
+		if($LastExitCode -ne 0) {
+			$errorCode=1
+			Add-AppveyorMessage "Unable to decrypt ssh key"
+		}
 	}
 	# Install ssh stuff.
 	Copy-Item -Path appveyor\ssh_id_rsa -Destination $env:userprofile\.ssh\id_rsa
