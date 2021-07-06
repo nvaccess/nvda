372dd0fe3de29266747f778522ad1ef8f2429101
Reef Turner <reef@nvaccess.org>
2021-07-06 16:14:23 +0800

    add wx MonkeyPatches to package

--- source/monkeyPatches/__init__.py
+++ source/monkeyPatches/__init__.py
@@ -3,6 +3,11 @@
 # This file is covered by the GNU General Public License.
 # See the file COPYING for more details.
 
+from . import wxMonkeyPatches
+
+
+applyWxMonkeyPatches = wxMonkeyPatches.apply
+
 
 def applyMonkeyPatches():
 	# Apply several monkey patches to comtypes
