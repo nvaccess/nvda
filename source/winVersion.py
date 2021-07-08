8460d1c0b580e462750e6a7eed60fd56fae76aa1
Joseph Lee <joseph.lee22590@gmail.com>
2021-07-08 14:08:48 -0700

    winVersion: recognize build 22000 as Windows 11 (21H2). Re #12626.
    Despite the name, Windows 11 is powered by Windows 10 technologies internally (therefore major.minor is 10.0). The Windows 11 branding first appeared in build 22000 for Windows Insiders, therefore recognize 'Windows 10' build 22000 as Windows 11 (both the constant and release name were added).

--- source/winVersion.py
+++ source/winVersion.py
@@ -31,7 +31,8 @@ _BUILDS_TO_RELEASE_NAMES = {
 	18363: "Windows 10 1909",
 	19041: "Windows 10 2004",
 	19042: "Windows 10 20H2",
-	19043: "Windows 10 21H1"
+	19043: "Windows 10 21H1",
+	22000: "Windows 11 21H2"
 }
 
 
@@ -150,6 +151,7 @@ WIN10_1909 = WinVersion(major=10, minor=0, build=18363)
 WIN10_2004 = WinVersion(major=10, minor=0, build=19041)
 WIN10_20H2 = WinVersion(major=10, minor=0, build=19042)
 WIN10_21H1 = WinVersion(major=10, minor=0, build=19043)
+WIN11 = WIN11_21H2 = WinVersion(major=10, minor=0, build=22000)
 
 
 def getWinVer():
