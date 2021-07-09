b908e6606665882b24dda778eede64c16a32b2e7
Joseph Lee <joseph.lee22590@gmail.com>
2021-07-08 21:07:48 -0700

    SearchUI: add 'searchhost' as an alias of Search UI app module in Windows 11.
    Windows Search executable was renamed to searchhost.exe in Windows 11, affecting suggestion count announcement. Therefore add an alias app module to support Windows 11.

---
+++ source/appModules/searchhost.py
@@ -0,0 +1,10 @@
+# A part of NonVisual Desktop Access (NVDA)
+# Copyright (C) 2021 NV Access Limited, Joseph Lee
+# This file is covered by the GNU General Public License.
+# See the file COPYING for more details.
+
+"""Windows Search app module for Windows 11 (build 22000) and later.
+"""
+
+# Flake8/F403: this is an alias for SearchUI app module.
+from .searchui import * # NOQA
