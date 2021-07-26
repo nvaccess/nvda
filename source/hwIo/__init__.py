nvaccess/nvda

commit 00cd67b737bc5b23a6f5e31cf28110b64ebf2fee

Support Braille displays that conform to the HID Braille standard  (#12523)
Braille Display devices allow being controlled by Screen Readers using a variety of connections such as Serial, USB and Bluetooth. The protocols used over these channels have traditionally been Braille Display manufacturer specific.
This has meant that in order for a Screen Reader to support a particular Braille display, it must have specific logic in the Screen Reader that knows how to speak the required device-specific protocol. Further to this, On Windows at least, an OS-level driver provided by the Braille Display manufacturer must also be installed by the user in order for the computer to detect and communicate with the device.
With the introduction of the HID (Human Interface Device) standard for USB (and later Bluetooth), it became possible to remove the need for the required OS-level device driver on Windows if the Braille Display manufacturer exposed the device as a custom HID device, however the Screen Reader still needed device-specific code, as being custom HID only simplified the low-level bytes communication, but did not standardize what those bytes actually meant.
In 2018, The HID specification was extended to define the concept of a Braille Display device, including setting of braille cells, and the standardizing of common buttons found on Braille displays such as routing keys, Braille dot input keys, braille space and panning keys.
NVDA should add specific support for a Braille HID device, so that Braille Displays that support this standard can automatically be detected and usable by NVDA.

Description of how this pull request fixes the issue:
• Adds hidpi.py which contains all necessary types and defines from Windows' hidpi.h (the HID parser header).
• Some HidP types that were previously added to hwIo.py have been moved to hidpi.py.

source/hwIo/__init__.py
@@ -1,0 +1,21 @@
+# A part of NonVisual Desktop Access (NVDA)
+# This file is covered by the GNU General Public License.
+# See the file COPYING for more details.
+# Copyright (C) 2015-2021 NV Access Limited, Babbage B.V.
+
+
+"""Raw input/output for braille displays via serial and HID.
+See the L{Serial} and L{Hid} classes.
+Braille display drivers must be thread-safe to use this, as it utilises a background thread.
+See L{braille.BrailleDisplayDriver.isThreadSafe}.
+"""
+
+
+from .base import (  # noqa: F401
+	IoBase,
+	Serial,
+	Bulk,
+	boolToByte,
+	intToByte,
+	getByte
+)

source/bdDetect.py
@@ -32,6 +32,10 @@ import appModuleHandler
 from baseObject import AutoPropertyObject
 import re
 
+
+HID_USAGE_PAGE_BRAILLE = 0x41
+
+
 DBT_DEVNODES_CHANGED=7
 
 _driverDevices = OrderedDict()
@@ -119,6 +123,9 @@ def getDriversForConnectedUsbDevices():
 			for port in deviceInfoFetcher.comPorts if "usbID" in port)
 	)
 	for match in usbDevs:
+		# check for the Braille HID protocol before any other device matching.
+		if match.type == KEY_HID and match.deviceInfo.get('HIDUsagePage') == HID_USAGE_PAGE_BRAILLE:
+			yield ("hid", match)
 		for driver, devs in _driverDevices.items():
 			for type, ids in devs.items():
 				if match.type==type and match.id in ids:
@@ -137,6 +144,9 @@ def getDriversForPossibleBluetoothDevices():
 			for port in deviceInfoFetcher.hidDevices if port["provider"]=="bluetooth"),
 	)
 	for match in btDevs:
+		# check for the Braille HID protocol before any other device matching.
+		if match.type == KEY_HID and match.deviceInfo.get('HIDUsagePage') == HID_USAGE_PAGE_BRAILLE:
+			yield ("hid", match)
 		for driver, devs in _driverDevices.items():
 			matchFunc = devs[KEY_BLUETOOTH]
 			if not callable(matchFunc):
@@ -324,7 +334,6 @@ def getConnectedUsbDevicesForDriver(driver) -> Iterable[DeviceMatch]:
 	@return: Device information for each device.
 	@raise LookupError: If there is no detection data for this driver.
 	"""
-	devs = _driverDevices[driver]
 	usbDevs = itertools.chain(
 		(DeviceMatch(KEY_CUSTOM, port["usbID"], port["devicePath"], port)
 			for port in deviceInfoFetcher.usbDevices),
@@ -334,9 +343,15 @@ def getConnectedUsbDevicesForDriver(driver) -> Iterable[DeviceMatch]:
 			for port in deviceInfoFetcher.comPorts if "usbID" in port)
 	)
 	for match in usbDevs:
-		for type, ids in devs.items():
-			if match.type==type and match.id in ids:
+		# check for the Braille HID protocol before any other device matching.
+		if driver == "hid":
+			if match.type == KEY_HID and match.deviceInfo.get('HIDUsagePage') == HID_USAGE_PAGE_BRAILLE:
 				yield match
+		else:
+			devs = _driverDevices[driver]
+			for type, ids in devs.items():
+				if match.type == type and match.id in ids:
+					yield match
 
 def getPossibleBluetoothDevicesForDriver(driver) -> Iterable[DeviceMatch]:
 	"""Get any possible Bluetooth devices associated with a particular driver.
@@ -345,9 +360,14 @@ def getPossibleBluetoothDevicesForDriver(driver) -> Iterable[DeviceMatch]:
 	@return: Port information for each port.
 	@raise LookupError: If there is no detection data for this driver.
 	"""
-	matchFunc = _driverDevices[driver][KEY_BLUETOOTH]
-	if not callable(matchFunc):
-		return
+	if driver == "hid":
+		# check for the Braille HID protocol before any other device matching.
+		def matchFunc(match):
+			return match.type == KEY_HID and match.deviceInfo.get('HIDUsagePage') == HID_USAGE_PAGE_BRAILLE
+	else:
+		matchFunc = _driverDevices[driver][KEY_BLUETOOTH]
+		if not callable(matchFunc):
+			return
 	btDevs = itertools.chain(
 		(DeviceMatch(KEY_SERIAL, port["bluetoothName"], port["port"], port)
 			for port in deviceInfoFetcher.comPorts

source/brailleDisplayDrivers/hid.py
@@ -1,0 +1,296 @@
+# A part of NonVisual Desktop Access (NVDA)
+# This file is covered by the GNU General Public License.
+# See the file COPYING for more details.
+# Copyright (C) 2021 NV Access Limited
+
+from dataclasses import dataclass
+from typing import List, Optional
+import enum
+import braille
+import inputCore
+from logHandler import log
+import brailleInput
+import bdDetect
+import hidpi
+import hwIo.hid
+from hwIo import intToByte
+
+from bdDetect import HID_USAGE_PAGE_BRAILLE
+
+
+class BraillePageUsageID(enum.IntEnum):
+	UNDEFINED = 0
+	BRAILLE_DISPLAY = 0x1
+	BRAILLE_ROW = 0x2
+	EIGHT_DOT_BRAILLE_CELL = 0x3
+	SIX_DOT_BRAILLE_CELL = 0x4
+	NUMBER_OF_BRAILLE_CELLS = 0x5
+	SCREEN_READER_CONTROL = 0x6
+	SCREEN_READER_IDENTIFIER = 0x7
+	ROUTER_SET_1 = 0xFA
+	ROUTER_SET_2 = 0xFB
+	ROUTER_SET_3 = 0xFC
+	ROUTER_KEY = 0x100
+	ROW_ROUTER_KEY = 0x101
+	BRAILLE_BUTTONS = 0x200
+	BRAILLE_KEYBOARD_DOT_1 = 0x201
+	BRAILLE_KEYBOARD_DOT_2 = 0x202
+	BRAILLE_KEYBOARD_DOT_3 = 0x203
+	BRAILLE_KEYBOARD_DOT_4 = 0x204
+	BRAILLE_KEYBOARD_DOT_5 = 0x205
+	BRAILLE_KEYBOARD_DOT_6 = 0x206
+	BRAILLE_KEYBOARD_DOT_7 = 0x207
+	BRAILLE_KEYBOARD_DOT_8 = 0x208
+	BRAILLE_KEYBOARD_SPACE = 0x209
+	BRAILLE_KEYBOARD_LEFT_SPACE = 0x20A
+	BRAILLE_KEYBOARD_RIGHT_SPACE = 0x20B
+	BRAILLE_FACE_CONTROLS = 0x20C
+	BRAILLE_LEFT_CONTROLS = 0x20D
+	BRAILLE_RIGHT_CONTROLS = 0x20E
+	BRAILLE_TOP_CONTROLS = 0x20F
+	BRAILLE_JOYSTICK_CENTER = 0x210
+	BRAILLE_JOYSTICK_UP = 0x211
+	BRAILLE_JOYSTICK_DOWN = 0x212
+	BRAILLE_JOYSTICK_LEFT = 0x213
+	BRAILLE_JOYSTICK_RIGHT = 0x214
+	BRAILLE_DPAD_CENTER = 0x215
+	BRAILLE_DPAD_UP = 0x216
+	BRAILLE_DPAD_DOWN = 0x217
+	BRAILLE_DPAD_LEFT = 0x218
+	BRAILLE_DPAD_RIGHT = 0x219
+	BRAILLE_PAN_LEFT = 0x21A
+	BRAILLE_PAN_RIGHT = 0x21B
+	BRAILLE_ROCKER_UP = 0x21C
+	BRAILLE_ROCKER_DOWN = 0x21D
+	BRAILLE_ROCKER_PRESS = 0x21E
+
+
+@dataclass
+class ButtonCapsInfo:
+	buttonCaps: hidpi.HIDP_VALUE_CAPS
+	relativeIndexInCollection: int = 0
+
+
+class BrailleDisplayDriver(braille.BrailleDisplayDriver):
+	_dev: hwIo.hid.Hid
+	name = "hid"
+	# Translators: The name of a series of braille displays.
+	description = _("Standard HID Braille Display")
+	isThreadSafe = True
+
+	def __init__(self, port="auto"):
+		super().__init__()
+		self.numCells = 0
+
+		for portType, portId, port, portInfo in self._getTryPorts(port):
+			if portType != bdDetect.KEY_HID:
+				continue
+			# Try talking to the display.
+			try:
+				self._dev = hwIo.hid.Hid(port, onReceive=self._hidOnReceive)
+			except EnvironmentError:
+				log.debugWarning("", exc_info=True)
+				continue  # Couldn't connect.
+			if self._dev.usagePage != HID_USAGE_PAGE_BRAILLE:
+				log.debug("Not braille")
+				continue
+			cellValueCaps = self._findCellValueCaps()
+			if cellValueCaps:
+				self._cellValueCaps = cellValueCaps
+				self.numCells = cellValueCaps.ReportCount
+				# A display responded.
+				log.info("Found display with {cells} cells connected via {type} ({port})".format(
+					cells=self.numCells, type=portType, port=port))
+				break
+			# This device can't be initialized. Move on to the next (if any).
+			self._dev.close()
+		else:
+			raise RuntimeError("No display found")
+		self._inputButtonCapsByDataIndex = self._collectInputButtonCapsByDataIndex()
+		self._keysDown = set()
+		self._ignoreKeyReleases = False
+
+	def _findCellValueCaps(self) -> Optional[hidpi.HIDP_VALUE_CAPS]:
+		for valueCaps in self._dev.outputValueCaps:
+			if (
+				valueCaps.LinkUsagePage == HID_USAGE_PAGE_BRAILLE
+				and valueCaps.LinkUsage == BraillePageUsageID.BRAILLE_ROW
+				and valueCaps.u1.NotRange.Usage in (
+					BraillePageUsageID.EIGHT_DOT_BRAILLE_CELL,
+					BraillePageUsageID.SIX_DOT_BRAILLE_CELL
+				)
+				and valueCaps.ReportCount > 0
+			):
+				return valueCaps
+		return None
+
+	def _collectInputButtonCapsByDataIndex(self):
+		capsByDataIndex = {}
+		relativeIndexInCollection = 0
+		lastLinkCollection = None
+		for buttonCaps in self._dev.inputButtonCaps:
+			if buttonCaps.LinkCollection != lastLinkCollection:
+				lastLinkCollection = buttonCaps.LinkCollection
+				relativeIndexInCollection = 0
+			else:
+				relativeIndexInCollection += 1
+			if buttonCaps.IsRange:
+				r = buttonCaps.u1.Range
+				for index in range(r.DataIndexMin, r.DataIndexMax + 1):
+					capsByDataIndex[index] = ButtonCapsInfo(buttonCaps, relativeIndexInCollection=relativeIndexInCollection)
+			else:
+				nr = buttonCaps.u1.NotRange
+				index = nr.DataIndex
+				capsByDataIndex[index] = ButtonCapsInfo(buttonCaps, relativeIndexInCollection=relativeIndexInCollection)
+		return capsByDataIndex
+
+	def terminate(self):
+		try:
+			super().terminate()
+		finally:
+			# Make sure the device gets closed.
+			# If it doesn't, we may not be able to re-open it later.
+			self._dev.close()
+
+	def _hidOnReceive(self, data: bytes):
+		report = hwIo.hid.HidInputReport(self._dev, data)
+		keys = []
+		for dataItem in report.getDataItems():
+			if dataItem.DataIndex in self._inputButtonCapsByDataIndex and dataItem.u1.On:
+				keys.append(dataItem.DataIndex)
+		if len(keys) > len(self._keysDown):
+			# Press. This begins a new key combination.
+			self._ignoreKeyReleases = False
+		elif len(keys) < len(self._keysDown):
+			self._handleKeyRelease()
+		self._keysDown = keys
+
+	def _handleKeyRelease(self):
+		if self._ignoreKeyReleases or not self._keysDown:
+			return
+		try:
+			inputCore.manager.executeGesture(InputGesture(self, self._keysDown))
+		except inputCore.NoInputGestureAction:
+			pass
+		# Any further releases are just the rest of the keys in the combination being released,
+		# so they should be ignored.
+		self._ignoreKeyReleases = True
+
+	def display(self, cells: List[int]):
+		# cells will already be padded up to numCells.
+		cellBytes = b"".join(intToByte(cell) for cell in cells)
+		report = hwIo.hid.HidOutputReport(self._dev, reportID=self._cellValueCaps.ReportID)
+		report.setUsageValueArray(
+			HID_USAGE_PAGE_BRAILLE,
+			self._cellValueCaps.LinkCollection,
+			self._cellValueCaps.u1.NotRange.Usage,
+			cellBytes
+		)
+		self._dev.write(report.data)
+
+	gestureMap = inputCore.GlobalGestureMap({
+		"globalCommands.GlobalCommands": {
+			"braille_scrollBack": ("br(HID):panLeft",),
+			"braille_scrollForward": ("br(HID):panRight",),
+			"braille_previousLine": ("br(HID):space+dot1",),
+			"braille_nextLine": ("br(HID):space+dot4",),
+			"braille_routeTo": ("br(HID):routerSet1_routerKey",),
+			"braille_toggleTether": ("br(HID):up+down",),
+			"kb:upArrow": ("br(HID):joystickUp",),
+			"kb:downArrow": ("br(HID):joystickDown",),
+			"kb:leftArrow": ("br(HID):space+dot3", "br(HID):joystickLeft"),
+			"kb:rightArrow": ("br(HID):space+dot6", "br(HID):joystickRight"),
+			"showGui": (
+				"br(HID):space+dot1+dot3+dot4+dot5",
+			),
+			"kb:shift+tab": ("br(HID):space+dot1+dot3",),
+			"kb:tab": ("br(HID):space+dot4+dot6",),
+			"kb:alt": ("br(HID):space+dot1+dot3+dot4",),
+			"kb:escape": ("br(HID):space+dot1+dot5",),
+			"kb:enter": ("br(HID):joystickCenter"),
+			"kb:windows+d": (
+				"br(HID):Space+dot1+dot4+dot5",
+			),
+			"kb:windows": ("br(HID):space+dot3+dot4",),
+			"kb:alt+tab": ("br(HID):space+dot2+dot3+dot4+dot5",),
+			"sayAll": (
+				"br(HID):Space+dot1+dot2+dot3+dot4+dot5+dot6",
+			),
+		},
+	})
+
+
+class InputGesture(braille.BrailleDisplayGesture, brailleInput.BrailleInputGesture):
+
+	source = BrailleDisplayDriver.name
+
+	def __init__(self, driver, dataIndices):
+		super().__init__()
+		self.keyCodes = set(dataIndices)
+
+		self.keyNames = names = []
+		namePrefix = None
+		isBrailleInput = True
+		for index in dataIndices:
+			buttonCapsInfo = driver._inputButtonCapsByDataIndex.get(index)
+			buttonCaps = buttonCapsInfo.buttonCaps
+			usagePage = buttonCaps.UsagePage
+			if buttonCaps.IsRange:
+				r = buttonCaps.u1.Range
+				relativeIndex = index - r.DataIndexMin
+				usageID = r.UsageMin + relativeIndex
+			else:
+				usageID = buttonCaps.u1.NotRange.Usage
+			linkUsagePage = buttonCaps.LinkUsagePage
+			linkUsageID = buttonCaps.LinkUsage
+			if usagePage == HID_USAGE_PAGE_BRAILLE and isBrailleInput:
+				if BraillePageUsageID.BRAILLE_KEYBOARD_DOT_1 <= usageID <= BraillePageUsageID.BRAILLE_KEYBOARD_DOT_8:
+					self.dots |= 1 << (usageID - BraillePageUsageID.BRAILLE_KEYBOARD_DOT_1)
+				elif usageID in (
+					BraillePageUsageID.BRAILLE_KEYBOARD_SPACE,
+					BraillePageUsageID.BRAILLE_KEYBOARD_LEFT_SPACE,
+					BraillePageUsageID.BRAILLE_KEYBOARD_RIGHT_SPACE,
+				):
+					self.space = True
+			else:
+				# This is not braille input.
+				isBrailleInput = False
+				self.dots = 0
+				self.space = False
+			if (
+				usagePage == HID_USAGE_PAGE_BRAILLE
+				and usageID == BraillePageUsageID.ROUTER_KEY
+			):
+				self.routingIndex = buttonCapsInfo.relativeIndexInCollection + 1
+				# Prefix the gesture name with the specific routing collection name (E.g. routerSet1)
+				namePrefix = self._usageIDToGestureName(linkUsagePage, linkUsageID)
+		name = self._usageIDToGestureName(usagePage, usageID)
+		if namePrefix:
+			name = "_".join([namePrefix, name])
+		names.append(name)
+		self.id = "+".join(names)
+
+	def _usageIDToGestureName(self, usagePage: int, usageID: int):
+		if usagePage != HID_USAGE_PAGE_BRAILLE:
+			# The usage ID is from another HID usage page
+			# Return a generic name with page and ID included
+			return f"usagePage{usagePage}&usageID{usageID}"
+		try:
+			rawName = BraillePageUsageID(usageID).name
+		except ValueError:
+			# An unknown usage ID within the braille usage page
+			# Return a generic name with the unknown ID included
+			return f"brailleUsage{usageID}"
+		name = rawName.lower()
+		# Remove braille_keyboard or braille_ from the beginning of the name
+		if name.startswith('braille_keyboard_'):
+			name = name[len('braille_keyboard_'):]
+		elif name.startswith('braille_'):
+			name = name[len('braille_'):]
+		# Capitalize the start of all words except the first
+		wordList = name.split('_')
+		for index in range(1, len(wordList)):
+			wordList[index] = wordList[index].title()
+		# Join the words together as  camelcase.
+		name = "".join(wordList)
+		return name

source/hidpi.py
@@ -1,0 +1,150 @@
+# A part of NonVisual Desktop Access (NVDA)
+# This file is covered by the GNU General Public License.
+# See the file COPYING for more details.
+# Copyright (C) 2021 NV Access Limited
+
+"""
+Required types and defines from Windows SDK's hidpi.h
+(Public Interface to the Windows HID parsing library).
+"""
+
+import enum
+from ctypes import Structure, Union, c_byte
+from ctypes.wintypes import USHORT, BOOLEAN, ULONG, LONG
+
+
+FACILITY_HID_ERROR_CODE = 0x11
+
+
+def HIDP_ERROR_CODES(sev, code):
+	return (sev << 28) | (FACILITY_HID_ERROR_CODE << 16) | code
+
+
+class HIDP_STATUS(enum.IntEnum):
+	SUCCESS = HIDP_ERROR_CODES(0x0, 0)
+	NULL = HIDP_ERROR_CODES(0x8, 1)
+	INVALID_PREPARSED_DATA = HIDP_ERROR_CODES(0xC, 1)
+	INVALID_REPORT_TYPE = HIDP_ERROR_CODES(0xC, 2)
+	INVALID_REPORT_LENGTH = HIDP_ERROR_CODES(0xC, 3)
+	USAGE_NOT_FOUND = HIDP_ERROR_CODES(0xC, 4)
+	VALUE_OUT_OF_RANGE = HIDP_ERROR_CODES(0xC, 5)
+	BAD_LOG_PHY_VALUES = HIDP_ERROR_CODES(0xC, 6)
+	BUFFER_TOO_SMALL = HIDP_ERROR_CODES(0xC, 7)
+	INTERNAL_ERROR = HIDP_ERROR_CODES(0xC, 8)
+	I8042_TRANS_UNKNOWN = HIDP_ERROR_CODES(0xC, 9)
+	INCOMPATIBLE_REPORT_ID = HIDP_ERROR_CODES(0xC, 0xA)
+	NOT_VALUE_ARRAY = HIDP_ERROR_CODES(0xC, 0xB)
+	IS_VALUE_ARRAY = HIDP_ERROR_CODES(0xC, 0xC)
+	DATA_INDEX_NOT_FOUND = HIDP_ERROR_CODES(0xC, 0xD)
+	DATA_INDEX_OUT_OF_RANGE = HIDP_ERROR_CODES(0xC, 0xE)
+	BUTTON_NOT_PRESSED = HIDP_ERROR_CODES(0xC, 0xF)
+	REPORT_DOES_NOT_EXIST = HIDP_ERROR_CODES(0xC, 0x10)
+	NOT_IMPLEMENTED = HIDP_ERROR_CODES(0xC, 0x20)
+
+
+NTSTATUS = ULONG
+USAGE = USHORT
+UCHAR = c_byte
+
+
+class HIDP_REPORT_TYPE(enum.IntEnum):
+	INPUT = 0
+	OUTPUT = 1
+	FEATURE = 2
+
+
+class _HIDP_DATA_U1(Union):
+	_fields_ = [
+		('RawValue', ULONG),
+		('On', BOOLEAN),
+	]
+
+
+class HIDP_DATA(Structure):
+	_fields_ = [
+		('DataIndex', USHORT),
+		('Reserved', USHORT),
+		('u1', _HIDP_DATA_U1),
+	]
+
+
+class _HIDP_VALUE_CAPS_U1_RANGE(Structure):
+	_fields_ = [
+		('UsageMin', USAGE),
+		('UsageMax', USAGE),
+		('StringMin', USHORT),
+		('StringMax', USHORT),
+		('DesignatorMin', USHORT),
+		('DesignatorMax', USHORT),
+		('DataIndexMin', USHORT),
+		('DataIndexMax', USHORT),
+	]
+
+
+class _HIDP_VALUE_CAPS_U1_NOT_RANGE(Structure):
+	_fields_ = [
+		('Usage', USAGE),
+		('Reserved1', USAGE),
+		('StringIndex', USHORT),
+		('Reserved2', USHORT),
+		('DesignatorIndex', USHORT),
+		('Reserved3', USHORT),
+		('DataIndex', USHORT),
+		('Reserved4', USHORT),
+	]
+
+
+class _HIDP_VALUE_CAPS_U1(Union):
+	_fields_ = [
+		('Range', _HIDP_VALUE_CAPS_U1_RANGE),
+		('NotRange', _HIDP_VALUE_CAPS_U1_NOT_RANGE),
+	]
+
+
+class HIDP_VALUE_CAPS(Structure):
+	_fields_ = [
+		('UsagePage', USAGE),
+		('ReportID', UCHAR),
+		('IsAlias', BOOLEAN),
+		('BitField', USHORT),
+		('LinkCollection', USHORT),
+		('LinkUsage', USAGE),
+		('LinkUsagePage', USAGE),
+		('IsRange', BOOLEAN),
+		('IsStringRange', BOOLEAN),
+		('IsDesignatorRange', BOOLEAN),
+		('IsAbsolute', BOOLEAN),
+		('HasNull', BOOLEAN),
+		('Reserved1', UCHAR),
+		('BitSize', USHORT),
+		('ReportCount', USHORT),
+		('Reserved2', USHORT * 5),
+		('UnitsExp', ULONG),
+		('Units', ULONG),
+		('LogiclMin', LONG),
+		('LogicalMax', LONG),
+		('PhysicalMin', LONG),
+		('PhysicalMax', LONG),
+		('u1', _HIDP_VALUE_CAPS_U1),
+	]
+
+
+class HIDP_CAPS (Structure):
+	_fields_ = (
+		("Usage", USHORT),
+		("UsagePage", USHORT),
+		("InputReportByteLength", USHORT),
+		("OutputReportByteLength", USHORT),
+		("FeatureReportByteLength", USHORT),
+		("Reserved", USHORT * 17),
+		("NumberLinkCollectionNodes", USHORT),
+		("NumberInputButtonCaps", USHORT),
+		("NumberInputValueCaps", USHORT),
+		("NumberInputDataIndices", USHORT),
+		("NumberOutputButtonCaps", USHORT),
+		("NumberOutputValueCaps", USHORT),
+		("NumberOutputDataIndices", USHORT),
+		("NumberFeatureButtonCaps", USHORT),
+		("NumberFeatureValueCaps", USHORT),
+		("NumberFeatureDataIndices", USHORT)
+	)

source/hwIo/base.py
@@ -1,8 +1,8 @@
-#hwIo.py
-#A part of NonVisual Desktop Access (NVDA)
-#This file is covered by the GNU General Public License.
-#See the file COPYING for more details.
-#Copyright (C) 2015-2018 NV Access Limited, Babbage B.V.
+# A part of NonVisual Desktop Access (NVDA)
+# This file is covered by the GNU General Public License.
+# See the file COPYING for more details.
+# Copyright (C) 2015-2018 NV Access Limited, Babbage B.V.
+
 
 """Raw input/output for braille displays via serial and HID.
 See the L{Serial} and L{Hid} classes.
@@ -13,7 +13,7 @@ See L{braille.BrailleDisplayDriver.isThreadSafe}.
 import sys
 import ctypes
 from ctypes import byref
-from ctypes.wintypes import DWORD, USHORT
+from ctypes.wintypes import DWORD
 from typing import Optional, Any, Union, Tuple, Callable
 
 import serial
@@ -50,8 +50,8 @@ class IoBase(object):
 		@param onReceiveSize: The size (in bytes) of the data with which to call C{onReceive}.
 		"""
 		self._file = fileHandle
-		self._writeFile = writeFileHandle if writeFileHandle is not None else fileHandle
 		self._onReceive = onReceive
+		self._writeFile = writeFileHandle if writeFileHandle is not None else fileHandle
 		self._readSize = onReceiveSize
 		self._readBuf = ctypes.create_string_buffer(onReceiveSize)
 		self._readOl = OVERLAPPED()
@@ -235,144 +235,6 @@ class Serial(IoBase):
 				timeouts.WriteTotalTimeoutConstant = max(int(self._ser._write_timeout * 1000), 1)
 		SetCommTimeouts(self._ser._port_handle, ctypes.byref(timeouts))
 
-class HIDP_CAPS (ctypes.Structure):
-	_fields_ = (
-		("Usage", USHORT),
-		("UsagePage", USHORT),
-		("InputReportByteLength", USHORT),
-		("OutputReportByteLength", USHORT),
-		("FeatureReportByteLength", USHORT),
-		("Reserved", USHORT * 17),
-		("NumberLinkCollectionNodes", USHORT),
-		("NumberInputButtonCaps", USHORT),
-		("NumberInputValueCaps", USHORT),
-		("NumberInputDataIndices", USHORT),
-		("NumberOutputButtonCaps", USHORT),
-		("NumberOutputValueCaps", USHORT),
-		("NumberOutputDataIndices", USHORT),
-		("NumberFeatureButtonCaps", USHORT),
-		("NumberFeatureValueCaps", USHORT),
-		("NumberFeatureDataIndices", USHORT)
-	)
-
-class Hid(IoBase):
-	"""Raw I/O for HID devices.
-	"""
-	_featureSize: int
-
-	def __init__(self, path: str, onReceive: Callable[[bytes], None], exclusive: bool = True):
-		"""Constructor.
-		@param path: The device path.
-			This can be retrieved using L{hwPortUtils.listHidDevices}.
-		@param onReceive: A callable taking a received input report as its only argument.
-		@param exclusive: Whether to block other application's access to this device.
-		"""
-		if _isDebug():
-			log.debug("Opening device %s" % path)
-		handle = CreateFile(
-			path,
-			winKernel.GENERIC_READ | winKernel.GENERIC_WRITE,
-			0 if exclusive else winKernel.FILE_SHARE_READ|winKernel.FILE_SHARE_WRITE,
-			None,
-			winKernel.OPEN_EXISTING,
-			FILE_FLAG_OVERLAPPED,
-			None
-		)
-		if handle == INVALID_HANDLE_VALUE:
-			if _isDebug():
-				log.debug("Open failed: %s" % ctypes.WinError())
-			raise ctypes.WinError()
-		pd = ctypes.c_void_p()
-		if not ctypes.windll.hid.HidD_GetPreparsedData(handle, byref(pd)):
-			raise ctypes.WinError()
-		caps = HIDP_CAPS()
-		ctypes.windll.hid.HidP_GetCaps(pd, byref(caps))
-		ctypes.windll.hid.HidD_FreePreparsedData(pd)
-		if _isDebug():
-			log.debug("Report byte lengths: input %d, output %d, feature %d"
-				% (caps.InputReportByteLength, caps.OutputReportByteLength,
-					caps.FeatureReportByteLength))
-		self._featureSize = caps.FeatureReportByteLength
-		self._writeSize = caps.OutputReportByteLength
-		# Reading any less than caps.InputReportByteLength is an error.
-		super(Hid, self).__init__(handle, onReceive,
-			onReceiveSize=caps.InputReportByteLength
-		)
-
-	def _prepareWriteBuffer(self, data: bytes) -> Tuple[int, ctypes.c_char_p]:
-		""" For HID devices, the buffer to be written must match the
-		OutputReportByteLength fetched from HIDP_CAPS, to ensure this is the case
-		we create a buffer of that size. We also check that data is not bigger than
-		the write size, which we do not currently support. If it becomes necessary to
-		support this, we could split the data and send it several chunks.
-		"""
-		# On Windows 7, writing any less than caps.OutputReportByteLength is also an error.
-		# See also: http://www.onarm.com/forum/20152/
-		if len(data) > self._writeSize:
-			log.error(u"Attempting to send a buffer larger than supported.")
-			raise RuntimeError("Unable to send buffer of: %d", len(data))
-		return (
-			self._writeSize,
-			ctypes.create_string_buffer(data, self._writeSize)
-		)
-
-	def getFeature(self, reportId: bytes) -> bytes:
-		"""Get a feature report from this device.
-		@param reportId: The report id.
-		@return: The report, including the report id.
-		"""
-		buf = ctypes.create_string_buffer(reportId, size=self._featureSize)
-		if not ctypes.windll.hid.HidD_GetFeature(self._file, buf, self._featureSize):
-			if _isDebug():
-				log.debug("Get feature %r failed: %s"
-					% (reportId, ctypes.WinError()))
-			raise ctypes.WinError()
-		if _isDebug():
-			log.debug("Get feature: %r" % buf.raw)
-		return buf.raw
-
-	def setFeature(self, report: bytes) -> None:
-		"""Send a feature report to this device.
-		@param report: The report, including its id.
-		"""
-		buf = ctypes.create_string_buffer(report, size=len(report))
-		bufSize = ctypes.sizeof(buf)
-		if _isDebug():
-			log.debug("Set feature: %r" % report)
-		result = ctypes.windll.hid.HidD_SetFeature(
-			self._file,
-			buf,
-			bufSize
-		)
-		if not result:
-			if _isDebug():
-				log.debug("Set feature failed: %s" % ctypes.WinError())
-			raise ctypes.WinError()
-
-	def setOutputReport(self, report: bytes) -> None:
-		"""
-		Write the given report to the device using HidD_SetOutputReport.
-		This is instead of using the standard WriteFile which may freeze with some USB HID implementations.
-		@param report: The report, including its id.
-		"""
-		buf = ctypes.create_string_buffer(report, size=len(report))
-		bufSize = ctypes.sizeof(buf)
-		if _isDebug():
-			log.debug("Set output report: %r" % report)
-		result = ctypes.windll.hid.HidD_SetOutputReport(
-			self._writeFile,
-			buf,
-			bufSize
-		)
-		if not result:
-			if _isDebug():
-				log.debug("Set output report failed: %s" % ctypes.WinError())
-			raise ctypes.WinError()
-
-	def close(self):
-		super(Hid, self).close()
-		winKernel.closeHandle(self._file)
-		self._file = None
 
 class Bulk(IoBase):
 	"""Raw I/O for bulk USB devices.

commit c5d301d98c70da49430821faec15134dad798406

Emergency fix for #12523: import Hid class in hwIo package (#12686)
Fixes regression introduced by pr #12523.

source/hwIo/__init__.py
@@ -19,3 +19,4 @@ from .base import (  # noqa: F401
 	intToByte,
 	getByte
 )
+from .hid import Hid  # noqa: F401