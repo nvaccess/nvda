d4ffe2efb6c55cf8330a2c4fe313170b731e3aae
Quin Marilyn <quin.marilyn05@gmail.com>
2021-08-24 16:17:46 -0600

    Removed me from copyright headers, and removed filename comments.

--- source/XMLFormatting.py
+++ source/XMLFormatting.py
@@ -1,5 +1,5 @@
 # A part of NonVisual Desktop Access (NVDA)
-# Copyright (C) 2008-2021 NV Access Limited, Babbage B.V., Quin Marilyn
+# Copyright (C) 2008-2021 NV Access Limited, Babbage B.V.
 # This file is covered by the GNU General Public License.
 # See the file COPYING for more details.
 
