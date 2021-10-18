nvaccess/nvda

commit fab35ec3c03038b22c9c288d78520aa03d4f2155

Report aria-description always (PR #12500)
# Summary:
The HTML attribute aria-description has been introduced as one aspect of supporting a broader concept of annotations on the web. It maps to accDescription. In contrast to other sources for accDescription such as title, our expectation is annotations should be reported whenever present.

# Description of change:
Experimental support added for reporting aria-description.
Firefox use IA2 attribute 'description' matching accDescription to indicate source is aria-description.
Chrome use  IA2 attribute `description-from` with value of 'aria-description' to indicate source, otherwise
fallback to Firefox behavior.

# Other changes:
* Rename UIA._getControlFieldForObject to _getControlFieldForUIAObject
  - Since type info in NVDA is not yet ubiquitous, a lot of development relies on
  text search / grep.
  - This name was easy to confuse with CompoundTextInfo._getControlFieldForObject

* Add typing for virtual buffer / text info / fields normalaziation
  - Add return type info for XMLFormatting.XMLTextParser.parse
  - Try to confirm if data is an optional string, log a warning if it is None.

* Use constants for Unicode specials
  - Symbols referenced regularly in the code, descriptions
    from https://en.wikipedia.org/wiki/Specials_(Unicode_block)
  - ￼ OBJECT REPLACEMENT CHARACTER, placeholder in the text
    for another unspecified object, for example in a compound document.
  - � REPLACEMENT CHARACTER, used to replace an unknown, unrecognized,
     or unrepresentable character.

* Remove ignoreEditableText param
  - As a result of this, controlStack previously had None inserted, now it can be empty.

* Fix names, simplify logic in getTextWithFields
  - field -> childObject
  - field -> textWithEmbeddedObjectsItem

source/speech/speech.py
@@ -8,6 +8,7 @@
 """ 
 
 import itertools
+import typing
 import weakref
 import unicodedata
 import time
@@ -59,6 +60,8 @@ from enum import IntEnum
 from dataclasses import dataclass
 from copy import copy
 
+if typing.TYPE_CHECKING:
+	import NVDAObjects
 
 _speechState: Optional['SpeechState'] = None
 _curWordChars: List[str] = []
@@ -406,7 +409,7 @@ def speakObjectProperties(  # noqa: C901
 # Note: when working on getObjectPropertiesSpeech, look for opportunities to simplify
 # and move logic out into smaller helper functions.
 def getObjectPropertiesSpeech(  # noqa: C901
-		obj,
+		obj: "NVDAObjects.NVDAObject",
 		reason: OutputReason = OutputReason.QUERY,
 		_prefixSpeechCommand: Optional[SpeechCommand] = None,
 		**allowedProperties
@@ -425,6 +428,11 @@ def getObjectPropertiesSpeech(  # noqa: C901
 			# getPropertiesSpeech names this "current", but the NVDAObject property is
 			# named "isCurrent", it's type should always be controltypes.IsCurrent
 			newPropertyValues['current'] = obj.isCurrent
+		elif value and name == "descriptionFrom" and (
+			obj.descriptionFrom == controlTypes.DescriptionFrom.ARIA_DESCRIPTION
+		):
+			newPropertyValues['_description-from'] = obj.descriptionFrom
+			newPropertyValues['description'] = obj.description
 		elif value:
 			# Certain properties such as row and column numbers have presentational versions, which should be used for speech if they are available.
 			# Therefore redirect to those values first if they are available, falling back to the normal properties if not.
@@ -624,6 +632,7 @@ def _objectSpeech_calculateAllowedProps(reason, shouldReportTextContent):
 		'states': True,
 		'value': True,
 		'description': True,
+		'descriptionFrom': config.conf["annotations"]["reportAriaDescription"],
 		'keyboardShortcut': True,
 		'positionInfo_level': True,
 		'positionInfo_indexInGroup': True,
@@ -1762,10 +1771,34 @@ def getControlFieldSpeech(  # noqa: C901
 	isCurrent = attrs.get('current', controlTypes.IsCurrent.NO)
 	placeholderValue=attrs.get('placeholder', None)
 	value=attrs.get('value',"")
-	if reason == OutputReason.FOCUS or attrs.get('alwaysReportDescription', False):
-		description=attrs.get('description',"")
-	else:
-		description=""
+
+	description: Optional[str] = None
+	_descriptionFrom = attrs.get('_description-from', controlTypes.DescriptionFrom.UNKNOWN)
+	if (
+		(
+			config.conf["presentation"]["reportObjectDescriptions"]
+			and (
+				reason == OutputReason.FOCUS
+				# 'alwaysReportDescription' provides symmetry with 'alwaysReportName'.
+				# Not used internally, but may be used by addons.
+				or attrs.get('alwaysReportDescription', False)
+			)
+		)
+		or (
+			# Don't report other sources of description like "title" all the time
+			# The usages of these is not consistent and often does not seem to have
+			# Screen Reader users in mind
+			config.conf["annotations"]["reportAriaDescription"]
+			and controlTypes.DescriptionFrom.ARIA_DESCRIPTION == _descriptionFrom
+			and reason in (
+				OutputReason.FOCUS,
+				OutputReason.CARET,
+				OutputReason.SAYALL,
+			)
+		)
+	):
+		description = attrs.get('description')
+
 	level=attrs.get('level',None)
 
 	if presCat != attrs.PRESCAT_LAYOUT:
@@ -1794,7 +1827,7 @@ def getControlFieldSpeech(  # noqa: C901
 	nameSequence = getPropertiesSpeech(reason=reason, name=name)
 	valueSequence = getPropertiesSpeech(reason=reason, value=value)
 	descriptionSequence = []
-	if config.conf["presentation"]["reportObjectDescriptions"]:
+	if description is not None:
 		descriptionSequence = getPropertiesSpeech(
 			reason=reason, description=description
 		)
@@ -1979,6 +2012,8 @@ def getControlFieldSpeech(  # noqa: C901
 		out = []
 		if isCurrent != controlTypes.IsCurrent.NO:
 			out.extend(isCurrentSequence)
+		if descriptionSequence:
+			out.extend(descriptionSequence)
 		# Speak expanded / collapsed / level for treeview items (in ARIA treegrids)
 		if role == controlTypes.Role.TREEVIEWITEM:
 			if controlTypes.STATE_EXPANDED in states:

tests/system/robot/chromeTests.py
@@ -468,3 +468,173 @@ def test_tableInStyleDisplayTable():
 		nextActualSpeech,
 		"row 2  First content cell"
 	)
+
+
+annotation = "User nearby, Aaron"
+linkDescription = "opens in a new tab"
+linkTitle = "conduct a search"
+ariaDescriptionSample = f"""
+		<div>
+			<div
+				contenteditable=""
+				spellcheck="false"
+				role="textbox"
+				aria-multiline="true"
+			><p>This is a line with no annotation</p>
+			<p><span
+					aria-description="{annotation}"
+				>Here is a sentence that is being edited by someone else.</span>
+				<b>Multiple can edit this.</b></p>
+			<p>An element with a role, follow <a
+				href="www.google.com"
+				aria-description="{linkDescription}"
+				>to google's</a
+			> website</p>
+			<p>Testing the title attribute, <a
+				href="www.google.com"
+				title="{linkTitle}"
+				>to google's</a
+			> website</p>
+			</div>
+		</div>
+	"""
+
+
+def test_ariaDescription_focusMode():
+	""" Ensure aria description is read in focus mode.
+	"""
+	_chrome.prepareChrome(ariaDescriptionSample)
+	# Focus the contenteditable and automatically switch to focus mode (due to contenteditable)
+	actualSpeech = _chrome.getSpeechAfterKey("tab")
+	_asserts.strings_match(
+		actualSpeech,
+		"edit  multi line  This is a line with no annotation\nFocus mode"
+	)
+
+	actualSpeech = _chrome.getSpeechAfterKey('downArrow')
+	# description-from hasn't reached Chrome stable yet.
+	# reporting aria-description only supported in Chrome canary 92.0.4479.0+
+	_builtIn.run_keyword_and_expect_error(
+		# expected_error
+		"Actual speech != Expected speech:"
+		" Here is a sentence that is being edited by someone else."
+		"  Multiple can edit this. != *",
+		# keyword
+		"strings_match",
+		# args
+		actualSpeech,
+		f"{annotation}  Here is a sentence that is being edited by someone else."
+		"  Multiple authors can edit this."
+	)
+
+	linkRole = "link"
+	linkName = "to google's"
+	actualSpeech = _chrome.getSpeechAfterKey('downArrow')
+	# description-from hasn't reached Chrome stable yet.
+	# reporting aria-description only supported in Chrome canary 92.0.4479.0+
+	_builtIn.run_keyword_and_expect_error(
+		# expected_error
+		"Actual speech != Expected speech:"
+		f" An element with a role, follow  {linkRole}  {linkName}  website"
+		" != *",
+		# keyword
+		"strings_match",
+		# args
+		actualSpeech,
+		f"An element with a role, follow  {linkRole}  {linkDescription}  {linkName}  website"
+	)
+
+	# 'title' attribute for link ("conduct a search") should not be announced.
+	# too often title is used without screen reader users in mind, and is overly verbose.
+	actualSpeech = _chrome.getSpeechAfterKey('downArrow')
+	_asserts.strings_match(
+		actualSpeech,
+		f"Testing the title attribute,  {linkRole}  {linkName}  website"
+	)
+
+
+def test_ariaDescription_browseMode():
+	""" Ensure aria description is read in browse mode.
+	"""
+	_chrome.prepareChrome(ariaDescriptionSample)
+	actualSpeech = _chrome.getSpeechAfterKey("downArrow")
+	_asserts.strings_match(
+		actualSpeech,
+		"edit  multi line  This is a line with no annotation"
+	)
+
+	actualSpeech = _chrome.getSpeechAfterKey('downArrow')
+	# description-from hasn't reached Chrome stable yet.
+	# reporting aria-description only supported in Chrome canary 92.0.4479.0+
+	_builtIn.run_keyword_and_expect_error(
+		# expected_error
+		"Actual speech != Expected speech:"
+		" Here is a sentence that is being edited by someone else."
+		"  Multiple can edit this. != *",
+		# keyword
+		"strings_match",
+		# args
+		actualSpeech,
+		f"{annotation}  Here is a sentence that is being edited by someone else."
+		"  Multiple authors can edit this."
+	)
+
+	linkRole = "link"
+	linkName = "to google's"
+	actualSpeech = _chrome.getSpeechAfterKey('downArrow')
+	# description-from hasn't reached Chrome stable yet.
+	# reporting aria-description only supported in Chrome canary 92.0.4479.0+
+	_builtIn.run_keyword_and_expect_error(
+		# expected_error
+		"Actual speech != Expected speech:"
+		f" An element with a role, follow  {linkRole}  {linkName}  website"
+		" != *",
+		# keyword
+		"strings_match",
+		# args
+		actualSpeech,
+		f"An element with a role, follow  {linkRole}  {linkDescription}  {linkName}  website"
+	)
+
+	# 'title' attribute for link ("conduct a search") should not be announced.
+	# too often title is used without screen reader users in mind, and is overly verbose.
+	actualSpeech = _chrome.getSpeechAfterKey('downArrow')
+	_asserts.strings_match(
+		actualSpeech,
+		f"Testing the title attribute,  {linkRole}  {linkName}  website"
+	)
+
+
+def test_ariaDescription_sayAll():
+	""" Ensure aria description is read by say all.
+	"""
+	_chrome.prepareChrome(ariaDescriptionSample)
+	actualSpeech = _chrome.getSpeechAfterKey("NVDA+downArrow")
+
+	linkRole = "link"
+	linkName = "to google's"
+
+	# description-from hasn't reached Chrome stable yet.
+	# reporting aria-description only supported in Chrome canary 92.0.4479.0+
+	_builtIn.run_keyword_and_expect_error(
+		# asserting multiline error messages doesn't seem to work, instead just Glob the details
+		# of the error message:
+		"Multiline strings are different:*",
+		# keyword
+		"strings_match",
+		# args
+		actualSpeech,
+		"\n".join([
+			"Test page load complete",
+			"edit  multi line  This is a line with no annotation",
+			f"{annotation}  Here is a sentence that is being edited by someone else.",
+			"Multiple authors can edit this.",
+			"An element with a role, "  # no comma, concat these two long strings.
+			f"follow  {linkRole}  {linkDescription}  {linkName}  website",
+			# 'title' attribute for link ("conduct a search") should not be announced.
+			# too often title is used without screen reader users in mind, and is overly verbose.
+			f"Testing the title attribute,  {linkRole}  {linkName}  website"
+			"  out of edit",
+			"After Test Case Marker"
+		])
+	)

tests/system/robot/chromeTests.robot
@@ -60,3 +60,12 @@ i12147
 Table in style display: table
 	[Documentation]	Properly announce table row/column count and working table navigation for a HTML table in a div with style display: table
 	test_tableInStyleDisplayTable
+ARIA description Focus Mode
+	[Documentation]	Navigate to a span with aria-description in focus mode
+	test_ariaDescription_focusMode
+ARIA description Browse Mode
+	[Documentation]	Navigate to a span with aria-description in browse mode
+	test_ariaDescription_browseMode
+ARIA description Say All
+	[Documentation]	Say all, contents includes aria-description
+	test_ariaDescription_sayAll

source/IAccessibleHandler/__init__.py
@@ -1,5 +1,5 @@
 # A part of NonVisual Desktop Access (NVDA)
-# Copyright (C) 2006-2007 NVDA Contributors <http://www.nvda-project.org/>
+# Copyright (C) 2006-2021 NV Access Limited, Łukasz Golonka, Leonard de Ruijter
 # This file is covered by the GNU General Public License.
 # See the file COPYING for more details.
 
@@ -23,6 +23,7 @@ import comtypes.client
 import oleacc
 import JABHandler
 import UIAHandler
+import textUtils
 
 from comInterfaces import Accessibility as IA
 
@@ -1073,7 +1074,7 @@ def getRecursiveTextFromIAccessibleTextObject(obj, startOffset=0, endOffset=-1):
 		return text
 	textList = []
 	for i, t in enumerate(text):
-		if ord(t) == 0xFFFC:
+		if ord(t) == ord(textUtils.OBJ_REPLACEMENT_CHAR):
 			try:
 				index = hypertextObject.hyperlinkIndex(i + startOffset)
 				childTextObject = hypertextObject.hyperlink(index).QueryInterface(IA.IAccessible)

source/NVDAObjects/IAccessible/__init__.py
@@ -1,7 +1,8 @@
-#A part of NonVisual Desktop Access (NVDA)
-#Copyright (C) 2006-2018 NV Access Limited, Babbage B.V.
-#This file is covered by the GNU General Public License.
-#See the file COPYING for more details.
+# A part of NonVisual Desktop Access (NVDA)
+# Copyright (C) 2006-2021 NV Access Limited, Babbage B.V.
+# This file is covered by the GNU General Public License.
+# See the file COPYING for more details.
+import typing
 
 from comtypes.automation import IEnumVARIANT, VARIANT
 from comtypes import COMError, IServiceProvider, GUID
@@ -176,11 +177,11 @@ class IA2TextTextInfo(textInfos.offsets.OffsetsTextInfo):
 			if not text:
 				return
 			try:
-				self._startOffset=text.rindex(u'\ufffc',0,oldStart-self._startOffset)
+				self._startOffset = text.rindex(textUtils.OBJ_REPLACEMENT_CHAR, 0, oldStart - self._startOffset)
 			except ValueError:
 				pass
 			try:
-				self._endOffset=text.index(u'\ufffc',oldEnd-self._startOffset)
+				self._endOffset = text.index(textUtils.OBJ_REPLACEMENT_CHAR, oldEnd - self._startOffset)
 			except ValueError:
 				pass
 
@@ -328,7 +329,11 @@ class IA2TextTextInfo(textInfos.offsets.OffsetsTextInfo):
 	def _lineNumFromOffset(self,offset):
 		return -1
 
-	def _iterTextWithEmbeddedObjects(self, withFields, formatConfig=None):
+	def _iterTextWithEmbeddedObjects(
+			self,
+			withFields,
+			formatConfig=None
+	) -> typing.Generator[typing.Union[textInfos.FieldCommand, str, int], None, None]:
 		"""Iterate through the text, splitting at embedded object characters.
 		Where an embedded object character occurs, its offset is provided.
 		@param withFields: Whether to output control/format fields.
@@ -352,7 +357,7 @@ class IA2TextTextInfo(textInfos.offsets.OffsetsTextInfo):
 			while chunkStart < itemLen:
 				# Find the next embedded object character.
 				try:
-					chunkEnd = item.index(u"\uFFFC", chunkStart)
+					chunkEnd = item.index(textUtils.OBJ_REPLACEMENT_CHAR, chunkStart)
 				except ValueError:
 					# This is the last chunk of text.
 					yield item[chunkStart:]

source/NVDAObjects/IAccessible/chromium.py
@@ -13,9 +13,27 @@ import controlTypes
 from NVDAObjects.IAccessible import IAccessible
 from virtualBuffers.gecko_ia2 import Gecko_ia2 as GeckoVBuf, Gecko_ia2_TextInfo as GeckoVBufTextInfo
 from . import ia2Web
+from logHandler import log
 
 
 class ChromeVBufTextInfo(GeckoVBufTextInfo):
+
+	def _calculateDescriptionFrom(self, attrs) -> controlTypes.DescriptionFrom:
+		"""Overridable calculation of DescriptionFrom
+		@param attrs: source attributes for the TextInfo
+		@return: the origin for accDescription.
+		@note: Chrome provides 'IAccessible2::attribute_description-from' which declares the origin used for
+			accDescription. Chrome also provides `IAccessible2::attribute_description` to maintain compatibility
+			with FireFox.
+		"""
+		ia2attrDescriptionFrom = attrs.get("IAccessible2::attribute_description-from")
+		try:
+			return controlTypes.DescriptionFrom(ia2attrDescriptionFrom)
+		except ValueError:
+			if ia2attrDescriptionFrom:
+				log.debugWarning(f"Unknown 'description-from' IA2Attribute value: {ia2attrDescriptionFrom}")
+		# fallback to Firefox approach
+		return super()._calculateDescriptionFrom(attrs)
 
 	def _normalizeControlField(self, attrs):
 		attrs = super()._normalizeControlField(attrs)

commit f2bb38cf130445e3872055576e8994a98826dd49

Fix reading order / description spoken during quick nav (PR #12933)
Fixes #12751
Broken in #12500

When using quick nav ("h" in browse mode) to navigate to a heading nested in an article with a description, the description was announced before the heading. For quicknav and for focus changes, the most inner element should always be announced first, working outwards to provide additional context.

source/speech/speech.py
@@ -1784,6 +1784,20 @@ def getControlFieldSpeech(  # noqa: C901
 	description: Optional[str] = None
 	_descriptionFrom = attrs.get('_description-from', controlTypes.DescriptionFrom.UNKNOWN)
 	_descriptionIsContent: bool = attrs.get("descriptionIsContent", False)
+	_reportDescriptionAsAnnotation: bool = (
+		# Don't report other sources of description like "title" all the time
+		# The usages of these is not consistent and often does not seem to have
+		# Screen Reader users in mind
+		config.conf["annotations"]["reportAriaDescription"]
+		and not _descriptionIsContent
+		and controlTypes.DescriptionFrom.ARIA_DESCRIPTION == _descriptionFrom
+		and reason in (
+			OutputReason.FOCUS,
+			OutputReason.QUICKNAV,
+			OutputReason.CARET,
+			OutputReason.SAYALL,
+		)
+	)
 	if (
 		(
 			config.conf["presentation"]["reportObjectDescriptions"]
@@ -1795,19 +1809,7 @@ def getControlFieldSpeech(  # noqa: C901
 			# Not used internally, but may be used by addons.
 			attrs.get('alwaysReportDescription', False)
 		)
-		or (
-			# Don't report other sources of description like "title" all the time
-			# The usages of these is not consistent and often does not seem to have
-			# Screen Reader users in mind
-			config.conf["annotations"]["reportAriaDescription"]
-			and not _descriptionIsContent
-			and controlTypes.DescriptionFrom.ARIA_DESCRIPTION == _descriptionFrom
-			and reason in (
-				OutputReason.FOCUS,
-				OutputReason.CARET,
-				OutputReason.SAYALL,
-			)
-		)
+		or _reportDescriptionAsAnnotation
 	):
 		description = attrs.get('description')
 
@@ -2024,7 +2026,7 @@ def getControlFieldSpeech(  # noqa: C901
 		out = []
 		if isCurrent != controlTypes.IsCurrent.NO:
 			out.extend(isCurrentSequence)
-		if descriptionSequence:
+		if descriptionSequence and _reportDescriptionAsAnnotation:
 			out.extend(descriptionSequence)
 		# Speak expanded / collapsed / level for treeview items (in ARIA treegrids)
 		if role == controlTypes.Role.TREEVIEWITEM:

tests/system/robot/chromeTests.py
@@ -1133,3 +1133,199 @@ def test_ensureNoBrowseModeDescription():
 		]),
 		message="Test focus mode with reportObjectDescriptions=False"
 	)
+
+
+def test_quickNavTargetReporting():
+	"""
+	When using quickNav, the target object should be spoken first, inner context should be given before outer
+	context.
+	"""
+	spy = _NvdaLib.getSpyLib()
+	REPORT_ARTICLES = ["documentFormatting", "reportArticles"]
+	spy.set_configValue(REPORT_ARTICLES, False)
+
+	_chrome.prepareChrome(
+		"""
+		<div
+			aria-describedby="descId"
+			aria-labelledby="labelId"
+			role="article"
+		>
+			<h1>Quick Nav Target</h1>
+			<div id="labelId">
+					<div>Some name.</div>
+			</div>
+			<div id="descId">
+					<span>A bunch of text.</span>
+			</div>
+		</div>
+		"""
+	)
+	# Quick nav to heading
+	actualSpeech = _chrome.getSpeechAfterKey("h")
+	_asserts.strings_match(
+		actualSpeech,
+		SPEECH_SEP.join([
+			"Quick Nav Target",  # Heading content (quick nav target), should read first
+			"heading",  # Heading role
+			"level 1",  # Heading level
+		])
+	)
+	# Reset to allow trying again with report articles enabled
+	actualSpeech = _chrome.getSpeechAfterKey("control+home")
+	_asserts.strings_match(
+		actualSpeech,
+		SPEECH_SEP.join([
+			"Before Test Case Marker",
+		])
+	)
+
+	# Quick nav to heading with report articles enabled
+	spy.set_configValue(REPORT_ARTICLES, True)
+	actualSpeech = _chrome.getSpeechAfterKey("h")
+	_asserts.strings_match(
+		actualSpeech,
+		SPEECH_SEP.join([
+			"Quick Nav Target",  # Heading content (quick nav target), should read first
+			"heading",  # Heading role
+			"level 1",  # Heading level
+			"article",  # article role, enabled via report article
+			"A bunch of text.",  # article (ancestor) description
+		])
+	)
+
+
+def test_focusTargetReporting():
+	"""
+	When moving focus the target object should be spoken first, inner context should be given before outer
+	context.
+	"""
+	spy = _NvdaLib.getSpyLib()
+	REPORT_ARTICLES = ["documentFormatting", "reportArticles"]
+	spy.set_configValue(REPORT_ARTICLES, False)
+
+	_chrome.prepareChrome(
+		"""
+		<a href="#">before Target</a>
+		<div
+			aria-describedby="descId"
+			aria-labelledby="labelId"
+			role="article"
+		>
+			<a href="#">Focus Target</a>
+			<div id="labelId">
+					<div>Some name.</div>
+			</div>
+			<div id="descId">
+					<span>A bunch of text.</span>
+			</div>
+		</div>
+		"""
+	)
+	# Set focus
+	actualSpeech = _chrome.getSpeechAfterKey("tab")
+	_asserts.strings_match(
+		actualSpeech,
+		SPEECH_SEP.join([
+			"before Target",
+			"link",
+		])
+	)
+
+	# Focus the link
+	actualSpeech = _chrome.getSpeechAfterKey("tab")
+	_asserts.strings_match(
+		actualSpeech,
+		SPEECH_SEP.join([
+			"Focus Target",  # link content (focus target), should read first
+			"link",  # link role
+		]),
+		message="browse mode - focus with Report Articles disabled"
+	)
+	# Reset to allow trying again with report articles enabled
+	actualSpeech = _chrome.getSpeechAfterKey("shift+tab")
+	_asserts.strings_match(
+		actualSpeech,
+		SPEECH_SEP.join([
+			"before Target",
+			"link",
+		])
+	)
+
+	# Focus the link with report articles enabled
+	spy.set_configValue(REPORT_ARTICLES, True)
+	actualSpeech = _chrome.getSpeechAfterKey("tab")
+	_asserts.strings_match(
+		actualSpeech,
+		SPEECH_SEP.join([
+			"Focus Target",  # link content (focus target), should read first
+			"link",  # link role
+			"article",  # article role, enabled via report article
+			"A bunch of text.",  # article (ancestor) description
+		]),
+		message="browse mode - focus with Report Articles enabled"
+	)
+
+	# Reset to allow trying again in focus mode
+	actualSpeech = _chrome.getSpeechAfterKey("shift+tab")
+	_asserts.strings_match(
+		actualSpeech,
+		SPEECH_SEP.join([
+			"before Target",
+			"link",
+		])
+	)
+
+	# Force focus mode
+	actualSpeech = _chrome.getSpeechAfterKey("NVDA+space")
+	_asserts.strings_match(
+		actualSpeech,
+		"Focus mode"
+	)
+
+	spy.set_configValue(REPORT_ARTICLES, False)
+	# Focus the link
+	actualSpeech = _chrome.getSpeechAfterKey("tab")
+	_asserts.strings_match(
+		actualSpeech,
+		SPEECH_CALL_SEP.join([
+			SPEECH_SEP.join([
+				"Some name.",  # name for article
+				"article",  # article role, enabled via report article
+				"A bunch of text.",  # description for article
+			]),
+			SPEECH_SEP.join([
+				"Focus Target",  # link content (focus target), should read first
+				"link",  # link role
+			]),
+		]),
+		message="focus mode - focus with Report Articles disabled"
+	)
+	# Reset to allow trying again with report articles enabled
+	actualSpeech = _chrome.getSpeechAfterKey("shift+tab")
+	_asserts.strings_match(
+		actualSpeech,
+		SPEECH_SEP.join([
+			"before Target",
+			"link",
+		])
+	)
+
+	# Focus the link with report articles enabled
+	spy.set_configValue(REPORT_ARTICLES, True)
+	actualSpeech = _chrome.getSpeechAfterKey("tab")
+	_asserts.strings_match(
+		actualSpeech,
+		SPEECH_CALL_SEP.join([
+			SPEECH_SEP.join([
+				"Some name.",  # name for article
+				"article",  # article role, enabled via report article
+				"A bunch of text.",  # description for article
+			]),
+			SPEECH_SEP.join([
+				"Focus Target",  # link content (focus target), should read first
+				"link",  # link role
+			]),
+		]),
+		message="focus mode - focus with Report Articles enabled"
+	)

tests/system/robot/chromeTests.robot
@@ -101,4 +101,9 @@ Prevent Duplicate Speech From Description while in Browse mode with tab nav
 Only report description in focus mode due to reportObjectDescriptions
 	[Documentation]	The term object in reportObjectDescriptions (essentially) means focus mode.
 	test_ensureNoBrowseModeDescription
-
+Quick Nav reports target first
+	[Documentation]	Quick Nav target should always be reported before ancestors. Ancestors should be reported from inner to outer.
+	test_quickNavTargetReporting
+Focus reports target first
+	[Documentation]	Focus target should always be reported before ancestors. Ancestors should be reported from inner to outer.
+	test_focusTargetReporting