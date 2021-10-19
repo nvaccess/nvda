10c9e7adc3b8ad37204b3a6ba83158e6bb0c24b3
Michael Curran <mick@nvaccess.org>
2021-10-19 17:38:34 +1000

    review_nextLine script: even if move by line succeeds, double check that the new line does in deed start after the original review cursor position. Otherwise, we have hit the bottom.

--- source/globalCommands.py
+++ source/globalCommands.py
@@ -1325,17 +1325,18 @@ class GlobalCommands(ScriptableObject):
 		gestures=("kb:numpad9", "kb(laptop):NVDA+downArrow", "ts(text):flickDown")
 	)
 	def script_review_nextLine(self,gesture):
-		info=api.getReviewPosition().copy()
-		info.expand(textInfos.UNIT_LINE)
-		info.collapse()
+		origInfo = api.getReviewPosition().copy()
+		origInfo.collapse()
+		info = origInfo.copy()
 		res=info.move(textInfos.UNIT_LINE,1)
-		if res==0:
+		newLine = info.copy()
+		newLine.expand(textInfos.UNIT_LINE)
+		if res == 0 or newLine.start <= origInfo.start: 
 			# Translators: a message reported when review cursor is at the bottom line of the current navigator object.
 			ui.reviewMessage(_("Bottom"))
 		else:
 			api.setReviewPosition(info)
-		info.expand(textInfos.UNIT_LINE)
-		speech.speakTextInfo(info, unit=textInfos.UNIT_LINE, reason=controlTypes.OutputReason.CARET)
+		speech.speakTextInfo(newLine, unit=textInfos.UNIT_LINE, reason=controlTypes.OutputReason.CARET)
 
 	@script(
 		# Translators: Input help mode message for move review cursor to bottom line command.
