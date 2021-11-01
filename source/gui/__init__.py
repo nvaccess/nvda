812dd6b879c874ac94d617cbe131d77dc9810cee
Sean Budd <seanbudd@me.com>
2021-11-01 13:22:53 +1100

    fix note

--- source/gui/__init__.py
+++ source/gui/__init__.py
@@ -61,7 +61,7 @@ mainFrame = None
 if version_year < 2022:
 	# Like other module level constants, this must be used as follows (#13011):
 	# import gui; doSomething(gui.isInMessageBox)
-	# from gui import isInMessageBox; doSomething(isInMessageBox)
+	# NOT the following: from gui import isInMessageBox; doSomething(isInMessageBox)
 	isInMessageBox = False
 
 class MainFrame(wx.Frame):
