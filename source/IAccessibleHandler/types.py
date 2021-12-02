nvaccess/nvda

commit 83125f2eb488f46751159e2e919f3ab7729de330

Encapsulate IA2 relations constants (PR #13096)
* Relations constants

Compat breaking refactor:
- Removed IA2_RELATION_FLOWS_FROM
- Removed IA2_RELATION_FLOWS_TO
- Removed IA2_RELATION_CONTAINING_DOCUMENT
- Replaced with IAccessibleHandler.RelationType enum

* Breaks compatibility in 2022.1

source/IAccessibleHandler/types.py
@@ -7,7 +7,7 @@
 """Types used in IAccessibleHander.
 Kept here so they can be re-used without having to worry about circular imports.
 """
-
+import enum
 from typing import Tuple
 
 IAccessibleObjectIdentifierType = Tuple[
@@ -15,3 +15,13 @@ IAccessibleObjectIdentifierType = Tuple[
 	int,  # objectID
 	int,  # childID
 ]
+
+
+# IAccessible2 relations (not included in the typelib)
+@enum.unique
+class RelationType(enum.Enum):
+	FLOWS_FROM = "flowsFrom"
+	FLOWS_TO = "flowsTo"
+	CONTAINING_DOCUMENT = "containingDocument"
+	DETAILS = "details"
+	DETAILS_FOR = "detailsFor"

source/NVDAObjects/IAccessible/__init__.py
@@ -1486,11 +1486,17 @@ the NVDAObject for IAccessible
 				pass
 		return None
 
-	def _get_flowsTo(self):
-		return self._getIA2RelationFirstTarget(IAccessibleHandler.IA2_RELATION_FLOWS_TO)
+	#: Type definition for auto prop '_get_flowsTo'
+	flowsTo: typing.Optional["IAccessible"]
 
-	def _get_flowsFrom(self):
-		return self._getIA2RelationFirstTarget(IAccessibleHandler.IA2_RELATION_FLOWS_FROM)
+	def _get_flowsTo(self) -> typing.Optional["IAccessible"]:
+		return self._getIA2RelationFirstTarget(IAccessibleHandler.RelationType.FLOWS_TO)
+
+	#: Type definition for auto prop '_get_flowsFrom'
+	flowsFrom: typing.Optional["IAccessible"]
+
+	def _get_flowsFrom(self) -> typing.Optional["IAccessible"]:
+		return self._getIA2RelationFirstTarget(IAccessibleHandler.RelationType.FLOWS_FROM)
 
 	def event_valueChange(self):
 		if isinstance(self, EditableTextWithAutoSelectDetection):

source/IAccessibleHandler/__init__.py
@@ -3,6 +3,10 @@
 # This file is covered by the GNU General Public License.
 # See the file COPYING for more details.
 
+# F401 imported but unused. RelationType should be exposed from IAccessibleHandler, in future __all__
+# should be used to export it.
+from .types import RelationType  # noqa: F401
+
 import re
 import struct
 from typing import (
@@ -57,9 +61,6 @@ NAVRELATION_LABELLED_BY = 0x1003
 NAVRELATION_NODE_CHILD_OF = 0x1005
 NAVRELATION_EMBEDS = 0x1009
 
-# IAccessible2 relations (not included in the typelib)
-IA2_RELATION_FLOWS_FROM = "flowsFrom"
-IA2_RELATION_FLOWS_TO = "flowsTo"
 
 # A place to store live IAccessible NVDAObjects, that can be looked up by their window,objectID,
 # childID event params.

source/virtualBuffers/gecko_ia2.py
@@ -13,6 +13,7 @@ import NVDAObjects.behaviors
 import winUser
 import mouseHandler
 import IAccessibleHandler
+
 import oleacc
 from logHandler import log
 import textInfos
@@ -22,9 +23,6 @@ from comtypes import COMError
 import aria
 import config
 from NVDAObjects.IAccessible import normalizeIA2TextFormatField, IA2TextTextInfo
-
-IA2_RELATION_CONTAINING_DOCUMENT = "containingDocument"
-
 
 def _getNormalizedCurrentAttrs(attrs: textInfos.ControlField) -> typing.Dict[str, typing.Any]:
 	valForCurrent = attrs.get("IAccessible2::attribute_current", "false")
@@ -193,7 +191,10 @@ class Gecko_ia2(VirtualBuffer):
 				# IAccessible NVDAObjects currently fetch IA2, but we need IA2_2 for relationTargetsOfType.
 				# (Out-of-process, for a single relation, this is cheaper than IA2::relations.)
 				acc = acc.QueryInterface(IA2.IAccessible2_2)
-			targets, count = acc.relationTargetsOfType(IA2_RELATION_CONTAINING_DOCUMENT, 1)
+			targets, count = acc.relationTargetsOfType(
+				IAccessibleHandler.RelationType.CONTAINING_DOCUMENT,
+				1  # max relations to fetch
+			)
 			if count == 0:
 				return None
 			doc = targets[0].QueryInterface(IA2.IAccessible2_2)

commit a46d8d6d3c872afee61830e1b609e4d5e5b1ce4d

Fix Enum str mixup (PR #13112)
- The RelationType enum now also inherits from str, its values can now be used as a string value.
- Fixed conversion of plain str types to RelationType
- Use faster approach to get relations of type (using dedicated method in IAccessible_2_2)

Fixes #13109 - Thunderbird and Firefox were broken with PR #13096.

source/IAccessibleHandler/types.py
@@ -19,7 +19,7 @@ IAccessibleObjectIdentifierType = Tuple[
 
 # IAccessible2 relations (not included in the typelib)
 @enum.unique
-class RelationType(enum.Enum):
+class RelationType(str, enum.Enum):
 	FLOWS_FROM = "flowsFrom"
 	FLOWS_TO = "flowsTo"
 	CONTAINING_DOCUMENT = "containingDocument"

source/NVDAObjects/IAccessible/__init__.py
@@ -5,7 +5,12 @@
 import typing
 
 from comtypes.automation import IEnumVARIANT, VARIANT
-from comtypes import COMError, IServiceProvider, GUID
+from comtypes import (
+	COMError,
+	IServiceProvider,
+	GUID,
+	IUnknown,
+)
 from comtypes.hresult import S_OK, S_FALSE
 import ctypes
 import os
@@ -1467,19 +1472,66 @@ the NVDAObject for IAccessible
 			raise NotImplementedError
 		return list(relations)
 
+	def _getIA2TargetsForRelationsOfType(
+			self,
+			relationType: "IAccessibleHandler.RelationType",
+			maxRelations: int = 1,
+	) -> typing.List[ctypes.POINTER(IUnknown)]:
+		"""Gets the target IAccessible (actually IUnknown; use QueryInterface or
+		normalizeIAccessible to resolve) for the relations with given type."""
+		acc = self.IAccessibleObject
+		if not isinstance(acc, IA2.IAccessible2):
+			raise NotImplementedError
+		if not isinstance(relationType, IAccessibleHandler.RelationType):
+			raise NotImplementedError
+		if 1 > maxRelations:
+			raise ValueError
+		if not isinstance(acc, IA2.IAccessible2_2):
+			acc = acc.QueryInterface(IA2.IAccessible2_2)
+		targets, count = acc.relationTargetsOfType(
+			relationType.value,
+			maxRelations
+		)
+		if count == 0:
+			return list()
+		relationsGen = (
+			targets[i]
+			for i in range(min(maxRelations, count))
+		)
+		return list(relationsGen)
+
 	def _getIA2RelationFirstTarget(
 			self,
-			relationType: str
+			relationType: typing.Union[str, "IAccessibleHandler.RelationType"]
 	) -> typing.Optional["IAccessible"]:
 		""" Get the first target for the relation of type.
-		@param relationType: A IAccessibleHandler.IA2_RELATION_* constant.
+		@param relationType: The type of relation to fetch.
 		"""
+		if not isinstance(relationType, IAccessibleHandler.RelationType):
+			if isinstance(relationType, str):
+				relationType = IAccessibleHandler.RelationType(relationType)
+			else:
+				raise TypeError(f"Bad type for 'relationType' arg, got: {type(relationType)}")
 
+		relationType = typing.cast(IAccessibleHandler.RelationType, relationType)
+
+		try:
+			# rather than fetch all the relations and querying the type, do that in process for performance reasons
+			targets = self._getIA2TargetsForRelationsOfType(relationType, maxRelations=1)
+			if targets:
+				return targets[0]
+		except (NotImplementedError, COMError):
+			log.debugWarning("Unable to use _getIA2TargetsForRelationsOfType, fallback to _IA2Relations.")
+
+		# eg IA2_2 is not available, fall back to old approach
 		for relation in self._IA2Relations:
 			try:
 				if relation.relationType == relationType:
+					# Take the first of 'relation.nTargets' see IAccessibleRelation._methods_
+					target = relation.target(0)
+					ia2Object = IAccessibleHandler.normalizeIAccessible(target)
 					return IAccessible(
-						IAccessibleObject=IAccessibleHandler.normalizeIAccessible(relation.target(0)),
+						IAccessibleObject=ia2Object,
 						IAccessibleChildID=0
 					)
 			except COMError: