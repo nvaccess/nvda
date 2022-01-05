b396626ee6df9ae687338ca3fb506012fac725a0
Michael Curran <mick@nvaccess.org>
2022-01-05 13:13:37 +1000

    do stuff

--- nvdaHelper/UIARemote/main.cpp
+++ nvdaHelper/UIARemote/main.cpp
@@ -18,24 +18,31 @@ using namespace UiaOperationAbstraction;
 
 wchar_t dllDirectory[MAX_PATH];
 
-winrt::guid GUID_MSWORD_EXPANDTOENCLOSINGSENTENCE { 0x98fe8b34, 0xf317, 0x459a, { 0x96, 0x27, 0x21, 0x12, 0x3e, 0xa9, 0x5b, 0xea } };
+winrt::guid guid_msWord_extendedTextRangePattern{ 0x93514122, 0xff04, 0x4b2c, { 0xa4, 0xad, 0x4a, 0xb0, 0x45, 0x87, 0xc1, 0x29 } };
+winrt::guid guid_msWord_getCustomAttributeValue{ 0x81aca91, 0x32f2, 0x46f0, { 0x9f, 0xb9, 0x1, 0x70, 0x38, 0xbc, 0x45, 0xf8 } };
+winrt::guid guid_msWord_moveEndpointBySentence{ 0x368e89a2, 0x1bc2, 0x4402, { 0x8c, 0x58, 0x33, 0xc6, 0x3e, 0xcf, 0xfa, 0x3b } };
+winrt::guid guid_msWord_moveBySentence{ 0xf39655ac, 0x133a, 0x435b, { 0xa3, 0x18, 0xc1, 0x97, 0xf0, 0xd3, 0xd2, 0x3 } };
+winrt::guid guid_msWord_expandToEnclosingSentence { 0x98fe8b34, 0xf317, 0x459a, { 0x96, 0x27, 0x21, 0x12, 0x3e, 0xa9, 0x5b, 0xea } };
+winrt::guid guid_msWord_getMathText{ 0x380198e5, 0xa51f, 0x4618, { 0xa7, 0x8d, 0x57, 0xe9, 0x56, 0x8a, 0x38, 0x62 } };
 
 extern "C" __declspec(dllexport) IUIAutomationTextRange* __stdcall msWord_expandToEnclosingSentence(IUIAutomationTextRange* pTextRangeArg) {
 	try {
 		auto scope=UiaOperationScope::StartNew();
-		UiaBool isSupported{false};
+		UiaBool isPatternSupported{false};
 		UiaTextRange textRange{pTextRangeArg};
 		auto element = textRange.GetEnclosingElement();
-		scope.If(element.IsExtensionSupported(GUID_MSWORD_EXPANDTOENCLOSINGSENTENCE),[&]() {
-			isSupported = true;
-			element.CallExtension(GUID_MSWORD_EXPANDTOENCLOSINGSENTENCE, textRange);
+		isPatternSupported = element.IsExtensionSupported(guid_msWord_extendedTextRangePattern);
+		scope.If(isPatternSupported,[&]() {
+			UiaElement patternElement{nullptr};
+			element.CallExtension(guid_msWord_extendedTextRangePattern, patternElement);
+			patternElement.CallExtension(guid_msWord_expandToEnclosingSentence, textRange);
 		});
-		scope.BindResult(isSupported, textRange);
+		scope.BindResult(isPatternSupported, textRange);
 		auto res = scope.ResolveHr();
 		if(res != S_OK) {
 			LOG_ERROR(L"Error in scope.Resolve: code "<<res);
 		}
-		if(isSupported) {
+		if(isPatternSupported) {
 			return (*textRange).detach();
 		} else {
 			LOG_ERROR(L"Extension not supported");
