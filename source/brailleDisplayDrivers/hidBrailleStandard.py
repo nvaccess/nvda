nvaccess/nvda

commit dd2400139257a21f7254cc09350a1ee83785fe49

Rename standard hid braille driver
Ensure there is no confusion with KEY_HID="hid" in bdDetect.py

source/brailleDisplayDrivers/hidBrailleStandard.py
@@ -73,7 +73,7 @@ class ButtonCapsInfo:
 
 class HidBrailleDriver(braille.BrailleDisplayDriver):
 	_dev: hwIo.hid.Hid
-	name = "hid"
+	name = "hidBrailleStandard"
 	# Translators: The name of a series of braille displays.
 	description = _("Standard HID Braille Display")
 	isThreadSafe = True

source/bdDetect.py
@@ -157,8 +157,8 @@ def getDriversForConnectedUsbDevices() -> typing.Iterator[typing.Tuple[str, Devi
 def _getStandardHidDriverName() -> str:
 	"""Return the name of the standard HID Braille device driver
 	"""
-	import brailleDisplayDrivers.hid
-	return brailleDisplayDrivers.hid.HidBrailleDriver.name
+	import brailleDisplayDrivers.hidBrailleStandard
+	return brailleDisplayDrivers.hidBrailleStandard.HidBrailleDriver.name
 
 
 def _isHIDBrailleMatch(match: DeviceMatch) -> bool:

source/braille.py
@@ -2258,10 +2258,15 @@ class _BgThread:
 			if cls.exit:
 				break
 
-#: Maps old braille display driver names to new drivers that supersede old drivers.
+
+# Maps old braille display driver names to new drivers that supersede old drivers.
+# Ensure that if a user has set a preferred driver which has changed name, the new
+# user preference is retained.
 RENAMED_DRIVERS = {
-	"syncBraille":"hims",
-	"alvaBC6":"alva"
+	# "oldDriverName": "newDriverName"
+	"syncBraille": "hims",
+	"alvaBC6": "alva",
+	"hid": "hidBrailleStandard",
 }
 
 handler: BrailleHandler

commit cc7aabee0bb45b94dce7f0d80ea706ce468de4e7

HID Braille: allow all gesture bindings to function again  (#13228)
When using a braille display with the HID Braille driver, none of the gesture bindings (such as scrolling, routing etc) function anymore.
This was broken in commit dd24001 between 2021.3 and 2021.3.1.
In this commit, the HID braille driver was renamed to hidBrailleStandard, however, all the gesture bindings within the driver were not updated to reflect the rename.

Description of how this pull request fixes the issue:
Correct the driver name on all gesture bindings in this driver to reference hidBrailleStandard instead of HID.

source/brailleDisplayDrivers/hidBrailleStandard.py
@@ -213,36 +213,36 @@ class HidBrailleDriver(braille.BrailleDisplayDriver):
 	gestureMap = inputCore.GlobalGestureMap({
 		"globalCommands.GlobalCommands": {
 			"braille_scrollBack": (
-				"br(HID):panLeft",
-				"br(HID):rockerUp",
+				"br(hidBrailleStandard):panLeft",
+				"br(hidBrailleStandard):rockerUp",
 			),
 			"braille_scrollForward": (
-				"br(HID):panRight",
-				"br(HID):rockerDown",
+				"br(hidBrailleStandard):panRight",
+				"br(hidBrailleStandard):rockerDown",
 			),
-			"braille_previousLine": ("br(HID):space+dot1",),
-			"braille_nextLine": ("br(HID):space+dot4",),
-			"braille_routeTo": ("br(HID):routerSet1_routerKey",),
-			"braille_toggleTether": ("br(HID):up+down",),
-			"kb:upArrow": ("br(HID):joystickUp",),
-			"kb:downArrow": ("br(HID):joystickDown",),
-			"kb:leftArrow": ("br(HID):space+dot3", "br(HID):joystickLeft"),
-			"kb:rightArrow": ("br(HID):space+dot6", "br(HID):joystickRight"),
+			"braille_previousLine": ("br(hidBrailleStandard):space+dot1",),
+			"braille_nextLine": ("br(hidBrailleStandard):space+dot4",),
+			"braille_routeTo": ("br(hidBrailleStandard):routerSet1_routerKey",),
+			"braille_toggleTether": ("br(hidBrailleStandard):up+down",),
+			"kb:upArrow": ("br(hidBrailleStandard):joystickUp",),
+			"kb:downArrow": ("br(hidBrailleStandard):joystickDown",),
+			"kb:leftArrow": ("br(hidBrailleStandard):space+dot3", "br(hidBrailleStandard):joystickLeft"),
+			"kb:rightArrow": ("br(hidBrailleStandard):space+dot6", "br(hidBrailleStandard):joystickRight"),
 			"showGui": (
-				"br(HID):space+dot1+dot3+dot4+dot5",
+				"br(hidBrailleStandard):space+dot1+dot3+dot4+dot5",
 			),
-			"kb:shift+tab": ("br(HID):space+dot1+dot3",),
-			"kb:tab": ("br(HID):space+dot4+dot6",),
-			"kb:alt": ("br(HID):space+dot1+dot3+dot4",),
-			"kb:escape": ("br(HID):space+dot1+dot5",),
-			"kb:enter": ("br(HID):joystickCenter"),
+			"kb:shift+tab": ("br(hidBrailleStandard):space+dot1+dot3",),
+			"kb:tab": ("br(hidBrailleStandard):space+dot4+dot6",),
+			"kb:alt": ("br(hidBrailleStandard):space+dot1+dot3+dot4",),
+			"kb:escape": ("br(hidBrailleStandard):space+dot1+dot5",),
+			"kb:enter": ("br(hidBrailleStandard):joystickCenter"),
 			"kb:windows+d": (
-				"br(HID):Space+dot1+dot4+dot5",
+				"br(hidBrailleStandard):Space+dot1+dot4+dot5",
 			),
-			"kb:windows": ("br(HID):space+dot3+dot4",),
-			"kb:alt+tab": ("br(HID):space+dot2+dot3+dot4+dot5",),
+			"kb:windows": ("br(hidBrailleStandard):space+dot3+dot4",),
+			"kb:alt+tab": ("br(hidBrailleStandard):space+dot2+dot3+dot4+dot5",),
 			"sayAll": (
-				"br(HID):Space+dot1+dot2+dot3+dot4+dot5+dot6",
+				"br(hidBrailleStandard):Space+dot1+dot2+dot3+dot4+dot5+dot6",
 			),
 		},
 	})