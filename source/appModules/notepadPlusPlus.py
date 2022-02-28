nvaccess/nvda

commit a1a6598b19f71bd108bef64373dd35c89c14cd4a

Fix crashes in 64-bit builds of Notepad++ 8.3 and later (#13364)
Fixes #13311

Summary of the issue:
NVDA causes a crash when interacting with 64-bit builds of Notepad+++ 8.3. This happens because Notepad++ developers decided to deviate from the official Scintilla interface (they character points are represented by longlong rather than long in 64-bit builds). Since this allowed them to introduce the support for much bigger files this change is going to stay and NVDA has to adapt (see explanations in notepad-plus-plus/notepad-plus-plus#11133).

Description of how this pull request fixes the issue:
For 64-bit builds of Notepad++ 8.3 and above longlong is used for representing characters points.

Testing strategy:

- Notepad++ 8.3 x64 no longer crashes and text can be read normally.
- 32-bit build of the same version of NPP works as well as it used to before this change.
- Older version of Notepad++ x64 works normally and long is used for characters points there.
- Different editor (teXnicCenter) using Scintilla is not negatively affected.
- The newly introduced appmodule can be imported by using from nvdaBuiltin.appModules import notepadPlusPlus in an add-on.

source/appModules/notepadPlusPlus.py
@@ -1,0 +1,58 @@
+# A part of NonVisual Desktop Access (NVDA)
+# Copyright (C) 2022 NV Access Limited, Łukasz Golonka
+# This file may be used under the terms of the GNU General Public License, version 2 or later.
+# For more details see: https://www.gnu.org/licenses/gpl-2.0.html
+
+"""AppModule for Notepad++.
+Do not rename! The executable file for Notepad++ is named `notepad++` and `+` is not a valid character
+in Python's import statements.
+This module is mapped to the right binary separately
+and the current name makes it possible to expose it from `nvdaBuiltin` for add-on developers.
+"""
+
+import ctypes
+
+import appModuleHandler
+import NVDAObjects.window.scintilla as ScintillaBase
+
+
+class CharacterRangeStructLongLong(ctypes.Structure):
+	"""By default character ranges in Scintilla are represented by longs.
+	However long is not big enough for files over 2 GB,
+	therefore in 64-bit builds of Notepad++ 8.3 and later
+	these ranges are represented by longlong.
+	"""
+	_fields_ = [
+		('cpMin', ctypes.c_longlong),
+		('cpMax', ctypes.c_longlong),
+	]
+
+
+class ScintillaTextInfoNpp83(ScintillaBase.ScintillaTextInfo):
+	"""Text info for 64-bit builds of Notepad++ 8.3 and later.
+	"""
+
+	class TextRangeStruct(ctypes.Structure):
+		_fields_ = [
+			('chrg', CharacterRangeStructLongLong),
+			('lpstrText', ctypes.c_char_p),
+		]
+
+
+class NppEdit(ScintillaBase.Scintilla):
+
+	name = None  # The name of the editor is not useful.
+
+	def _get_TextInfo(self):
+		if self.appModule.is64BitProcess:
+			appVerMajor, appVerMinor, *__ = self.appModule.productVersion.split(".")
+			if int(appVerMajor) >= 8 and int(appVerMinor) >= 3:
+				return ScintillaTextInfoNpp83
+		return super().TextInfo
+
+
+class AppModule(appModuleHandler.AppModule):
+
+	def chooseNVDAObjectOverlayClasses(self, obj, clsList):
+		if obj.windowClassName == "Scintilla" and obj.windowControlID == 0:
+			clsList.insert(0, NppEdit)

source/NVDAObjects/window/scintilla.py
@@ -1,19 +1,16 @@
 # -*- coding: UTF-8 -*-
 # A part of NonVisual Desktop Access (NVDA)
-# Copyright (C) 2009-2019 NV Access Limited, Arnold Loubriat, Babbage B.V., Łukasz Golonka
+# Copyright (C) 2007-2022 NV Access Limited, Arnold Loubriat, Babbage B.V., Łukasz Golonka, Joseph Lee,
+# Peter Vágner
 # This file is covered by the GNU General Public License.
 # See the file COPYING for more details.
 
 import ctypes
-import speech
 import textInfos.offsets
 import winKernel
 import winUser
-import globalVars
 import controlTypes
-import config
 from . import Window
-from .. import NVDAObjectTextInfo
 from ..behaviors import EditableTextWithAutoSelectDetection
 import watchdog
 import eventHandler
@@ -67,13 +64,14 @@ class CharacterRangeStruct(ctypes.Structure):
 		('cpMax',ctypes.c_long),
 	]
 
-class TextRangeStruct(ctypes.Structure):
-	_fields_=[
-		('chrg',CharacterRangeStruct),
-		('lpstrText',ctypes.c_char_p),
-	]
 
 class ScintillaTextInfo(textInfos.offsets.OffsetsTextInfo):
+
+	class TextRangeStruct(ctypes.Structure):
+		_fields_ = [
+			('chrg', CharacterRangeStruct),
+			('lpstrText', ctypes.c_char_p),
+		]
 
 	def _get_encoding(self):
 		cp=watchdog.cancellableSendMessage(self.obj.windowHandle,SCI_GETCODEPAGE,0,0)
@@ -171,7 +169,7 @@ class ScintillaTextInfo(textInfos.offsets.OffsetsTextInfo):
 
 	def _getTextRange(self,start,end):
 		bufLen = (end - start) + 1
-		textRange = TextRangeStruct()
+		textRange = self.TextRangeStruct()
 		textRange.chrg.cpMin = start
 		textRange.chrg.cpMax = end
 		processHandle = self.obj.processHandle

source/appModules/notepad++.py
@@ -1,0 +1,10 @@
+# A part of NonVisual Desktop Access (NVDA)
+# Copyright (C) 2022 NV Access Limited, Łukasz Golonka
+# This file may be used under the terms of the GNU General Public License, version 2 or later.
+# For more details see: https://www.gnu.org/licenses/gpl-2.0.html
+
+"""This is just an alias which maps the appModule for Notepad++ to the right binary file
+by exposing everything from the real module in its namespace.
+"""
+
+from .notepadPlusPlus import *  # noqa: F401, F403

commit 4c3a67b2eafa9721c5de3f671d10e60ab2d43865

Fix regression causing older versions of NotePad++ to crash (#13382)
Fixes regression caused by #13364
Closes #13397

Summary of the issue:
The version matching to apply the new NP++ 8.3+ behavior was incorrect, as it doesn't work for versions like 8.2.1 and 8.1.9.2.

Description of how this pull request fixes the issue:
Only match against the first character of the minor version.

Testing strategy:
Tested on version 8.1.9.2, 8.2.1, 8.3 and 8.3.1 of NP++

source/appModules/notepadPlusPlus.py
@@ -46,7 +46,12 @@ class NppEdit(ScintillaBase.Scintilla):
 	def _get_TextInfo(self):
 		if self.appModule.is64BitProcess:
 			appVerMajor, appVerMinor, *__ = self.appModule.productVersion.split(".")
-			if int(appVerMajor) >= 8 and int(appVerMinor) >= 3:
+			# When retrieving the version, Notepad++ concatenates
+			# minor, patch, build in major.minor.patch.build to the form of major.minor
+			# https://github.com/notepad-plus-plus/npp-usermanual/blob/master/content/docs/plugin-communication.md#nppm_getnppversion
+			# e.g. '8.3' for '8.3', '8.21' for '8.2.1' and '8.192' for '8.1.9.2'.
+			# Therefore, only use the first digit of the minor version to match against version 8.3 or later.
+			if int(appVerMajor) >= 8 and int(appVerMinor[0]) >= 3:
 				return ScintillaTextInfoNpp83
 		return super().TextInfo
 