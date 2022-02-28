551b4547e6ffe545e28168f74310d97a4a4a8080
Łukasz Golonka <lukasz.golonka@mailbox.org>
2022-02-28 20:38:12 +0100

    Move the mapping

--- source/appModuleHandler.py
+++ source/appModuleHandler.py
@@ -53,71 +53,9 @@ _getAppModuleLock=threading.RLock()
 post_appSwitch = extensionPoints.Action()
 
 
-_EXECUTABLE_NAMES_TO_APP_MODS_CORE: Dict[str, str] = {
-	# Azure Data Studio (both stable and Insiders versions) should use module for Visual Studio Code
-	"azuredatastudio": "code",
-	"azuredatastudio-insiders": "code",
-	# Windows 11 calculator should use module for the Windows 10 one.
-	"calculatorapp": "calculator",
-	# The Insider version of Visual Studio Code should use the module for the stable version.
-	"code - insiders": "code",
-	# commsapps is an alias for the Windows 10 mail and calendar.
-	"commsapps": "hxmail",
-	# DBeaver is based on Eclipse and should use its appModule.
-	"dbeaver": "eclipse",
-	# Preview version of the Adobe Digital Editions should use the module for the stable version.
-	"digitaleditionspreview": "digitaleditions",
-	# Esybraille should use module for esysuite.
-	"esybraille": "esysuite",
-	# hxoutlook is an alias for Windows 10 mail in Creators update.
-	"hxoutlook": "hxmail",
-	# 64-bit versions of Miranda IM should use module for the 32-bit executable.
-	"miranda64": "miranda32",
-	# Various incarnations of Media Player Classic.
-	"mpc-hc": "mplayerc",
-	"mpc-hc64": "mplayerc",
-	# The binary file for Notepad++ is named `notepad++` which makes its appModule not importable
-	# (Python's import statement cannot deal with `+` in the file name).
-	# Therefore the module is named `notepadPlusPlus` and mapped to the right binary below.
-	"notepad++": "notepadPlusPlus",
-	# searchapp is an alias for searchui in Windows 10 build 18965 and later.
-	"searchapp": "searchui",
-	# Windows search in Windows 11.
-	"searchhost": "searchui",
-	# Spring Tool Suite is based on Eclipse and should use its appModule.
-	"springtoolsuite4": "eclipse",
-	"sts": "eclipse",
-	# Various versions of Teamtalk.
-	"teamtalk3": "teamtalk4classic",
-	# App module for Windows 10/11 Modern Keyboard aka new touch keyboard panel
-	# should use Composable Shell modern keyboard app module
-	"textinputhost": "windowsinternal_composableshell_experiences_textinput_inputapp",
-	# Total Commander X64 should use the module for the 32-bit version.
-	"totalcmd64": "totalcmd",
-	# The calculator on Windows Server and LTSB versions of  Windows 10
-	# should use the module for the desktop calculator from the earlier Windows releases.
-	"win32calc": "calc",
-	# Windows Mail should use module for Outlook Express.
-	"winmail": "msimn",
-	# Zend Eclipse PHP Developer Tools is based on Eclipse and should use its appModule.
-	"zend-eclipse-php": "eclipse",
-	# Zend Studio is based on Eclipse and should use its appModule.
-	"zendstudio": "eclipse",
-}
-
-"""Maps names of the executables to the names of the appModule which  should be loaded for the given program.
-Note that this map is used only for appModules included in NVDA
-and appModules registered by add-ons are placed in a different one.
-This mapping is needed since:
-- Names of some programs are incompatible with the Python's import system (they contain a dot or a plus)
-- Sometimes it is necessary to map one module to multiple executables - this map saves us from adding multiple
- appModules in such cases.
-"""
-
-
 _executableNamesToAppModsAddons: Dict[str, str] = dict()
 """AppModules registered with a given binary by add-ons are placed here.
-We cannot use l{_EXECUTABLE_NAMES_TO_APP_MODS_CORE} for modules included in add-ons,
+We cannot use l{appModules.EXECUTABLE_NAMES_TO_APP_MODS} for modules included in add-ons,
 since appModules in add-ons should take precedence over the one bundled in NVDA.
 """
 
@@ -157,13 +95,13 @@ def _getPossibleAppModuleNamesForExecutable(executableName: str) -> Tuple[str, .
 	The names in the tuple are placed in order in which import of these aliases should be attempted that is:
 	- The alias registered by add-ons if any add-on registered an appModule for the executable
 	- Just the name of the executable to cover a standard appModule named the same as the executable
-	- The alias from `_EXECUTABLE_NAMES_TO_APP_MODS_CORE` if it exists.
+	- The alias from `appModules.EXECUTABLE_NAMES_TO_APP_MODS` if it exists.
 	"""
 	return (
 		aliasName for aliasName in (
 			_executableNamesToAppModsAddons.get(executableName),
 			executableName,
-			_EXECUTABLE_NAMES_TO_APP_MODS_CORE.get(executableName)
+			appModules.EXECUTABLE_NAMES_TO_APP_MODS.get(executableName)
 		) if aliasName is not None
 	)
 
@@ -290,7 +228,7 @@ def fetchAppModule(processID: int, appName: str) -> AppModule:
 	@param processID: process ID for it to be associated with
 	@param appName: the application name for which an appModule should be found.
 	@returns: the appModule, or None if not found
-	"""  
+	"""
 	# First, check whether the module exists.
 	# We need to do this separately because even though an ImportError is raised when a module can't be found, it might also be raised for other reasons.
 	modName = appName
@@ -440,8 +378,8 @@ class AppModule(baseObject.ScriptableObject):
 	If the name of the executable is not compatible with the Python's import system
 	i.e. contains some special characters such as "." or "+" you can name the module however you like
 	and then map the executable name to the module name
-	by adding an entry to `_EXECUTABLE_NAMES_TO_APP_MODS_CORE` dictionary.
-	It should containa  C{AppModule} class which inherits from this base class.
+	by adding an entry to `appModules.EXECUTABLE_NAMES_TO_APP_MODS` dictionary.
+	It should containa C{AppModule} class which inherits from this base class.
 	App modules can implement and bind gestures to scripts.
 	These bindings will only take effect while an object in the associated application has focus.
 	See L{ScriptableObject} for details.
