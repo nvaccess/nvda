a5b84fd274baf451b927a98027467d49de11c17e
André-Abush Clause <dev@andreabc.net>
2022-03-07 20:56:03 +0100

    Update liblouis to 3.21.0

--- source/brailleTables.py
+++ source/brailleTables.py
@@ -63,29 +63,31 @@ def listTables():
 
 #: Maps old table names to new table names for tables renamed in newer versions of liblouis.
 RENAMED_TABLES = {
-	"ar-fa.utb" : "fa-ir-g1.utb",
-	"da-dk-g16.utb":"da-dk-g16.ctb",
-	"da-dk-g18.utb":"da-dk-g18.ctb",
-	"de-de-g0.utb":"de-g0.utb",
-	"de-de-g1.ctb":"de-g1.ctb",
-	"de-de-g2.ctb":"de-g2.ctb",
-	"en-us-comp8.ctb" : "en-us-comp8-ext.utb",
-	"fr-ca-g1.utb":"fr-bfu-comp6.utb",
-	"Fr-Ca-g2.ctb":"fr-bfu-g2.ctb",
+	"ar-fa.utb": "fa-ir-g1.utb",
+	"da-dk-g16.utb": "da-dk-g16.ctb",
+	"da-dk-g18.utb": "da-dk-g18.ctb",
+	"de-de-g0.utb": "de-g0.utb",
+	"de-de-g1.ctb": "de-g1.ctb",
+	"de-de-g2.ctb": "de-g2.ctb",
+	"de-g0-bidi.utb": "de-g0-detailed.utb",
+	"de-g1-bidi.ctb": "de-g1-detailed.ctb",
+	"en-us-comp8.ctb": "en-us-comp8-ext.utb",
+	"fr-ca-g1.utb": "fr-bfu-comp6.utb",
+	"Fr-Ca-g2.ctb": "fr-bfu-g2.ctb",
 	"gr-bb.ctb": "grc-international-en.utb",
-	"gr-gr-g1.utb":"el.ctb",
+	"gr-gr-g1.utb": "el.ctb",
 	"he.ctb": "he-IL-comp8.utb",
-	"hr.ctb":"hr-comp8.utb",
-	"mn-MN.utb":"mn-MN-g1.utb",
+	"hr.ctb": "hr-comp8.utb",
+	"mn-MN.utb": "mn-MN-g1.utb",
 	"nl-BE-g0.utb": "nl-NL-g0.utb",
-	"nl-NL-g1.ctb":"nl-NL-g0.utb",
-	"no-no.ctb":"no-no-8dot.utb",
-	"no-no-comp8.ctb":"no-no-8dot.utb",
-	"ru-compbrl.ctb":"ru.ctb",
+	"nl-NL-g1.ctb": "nl-NL-g0.utb",
+	"no-no.ctb": "no-no-8dot.utb",
+	"no-no-comp8.ctb": "no-no-8dot.utb",
+	"ru-compbrl.ctb": "ru.ctb",
 	"ru-ru-g1.utb": "ru-litbrl-detailed.utb",
-	"sk-sk-g1.utb":"sk-g1.ctb",
-	"UEBC-g1.ctb":"en-ueb-g1.ctb",
-	"UEBC-g2.ctb":"en-ueb-g2.ctb",
+	"sk-sk-g1.utb": "sk-g1.ctb",
+	"UEBC-g1.ctb": "en-ueb-g1.ctb",
+	"UEBC-g2.ctb": "en-ueb-g2.ctb",
 	"vi-g1.ctb": "vi-vn-g1.ctb",
 }
 
@@ -161,6 +163,9 @@ addTable("da-dk-g26.ctb", _("Danish 6 dot grade 2"), contracted=True)
 addTable("da-dk-g28.ctb", _("Danish 8 dot grade 2"), contracted=True)
 # Translators: The name of a braille table displayed in the
 # braille settings dialog.
+addTable("de-comp6.utb", _("German 6 dot computer braille"))
+# Translators: The name of a braille table displayed in the
+# braille settings dialog.
 addTable("de-de-comp8.ctb", _("German 8 dot computer braille"))
 # Translators: The name of a braille table displayed in the
 # braille settings dialog.
@@ -168,14 +173,14 @@ addTable("de-g0.utb", _("German grade 0"), input=False)
 
 # Translators: The name of a braille table displayed in the
 # braille settings dialog.
-addTable("de-g0-bidi.utb", _("German grade 0 (detailed)"))
+addTable("de-g0-detailed.utb", _("German grade 0 (detailed)"))
 # Translators: The name of a braille table displayed in the
 # braille settings dialog.
 addTable("de-g1.ctb", _("German grade 1"), input=False)
 
 # Translators: The name of a braille table displayed in the
 # braille settings dialog.
-addTable("de-g1-bidi.ctb", _("German grade 1 (detailed)"))
+addTable("de-g1-detailed.ctb", _("German grade 1 (detailed)"))
 # Translators: The name of a braille table displayed in the
 # braille settings dialog.
 addTable("de-g2.ctb", _("German grade 2"), contracted=True, input=False)
