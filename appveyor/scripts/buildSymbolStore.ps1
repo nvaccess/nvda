796b5b947a3f642cf0d1bc6b3fda2c02b43e72ad
buddsean <sean@nvaccess.org>
2022-03-08 12:45:13 +1100

    Fix symstore usage to be inline with documentation

--- appveyor/scripts/buildSymbolStore.ps1
+++ appveyor/scripts/buildSymbolStore.ps1
@@ -7,5 +7,6 @@ foreach ($syms in
 	"source\lib64\*.dll", "source\lib64\*.exe", "source\lib64\*.pdb",
 	"source\synthDrivers\*.dll", "source\synthDrivers\*.pdb"
 ) {
-	& $env:symstore add /s symbols /compress /t NVDA /f $syms
+	# https://docs.microsoft.com/en-us/windows-hardware/drivers/debugger/symstore-command-line-options
+	& $env:symstore add /f $syms /s symbols /t NVDA /compress
 }
