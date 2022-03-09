3e04691fdfc0ad434e6a9d000f68968702aca9b1
Reef Turner <reef@nvaccess.org>
2022-03-09 10:49:14 +0800

    Fix docs strings

--- source/IAccessibleHandler/__init__.py
+++ source/IAccessibleHandler/__init__.py
@@ -3,10 +3,9 @@
 # This file is covered by the GNU General Public License.
 # See the file COPYING for more details.
 
+import typing
 # F401 imported but unused. RelationType should be exposed from IAccessibleHandler, in future __all__
 # should be used to export it.
-import typing
-
 from .types import RelationType  # noqa: F401
 
 import re
@@ -328,10 +327,24 @@ def NVDARoleFromAttr(accRole: Union[int, str, None]) -> Role:
 
 
 def transformRoleStates(role: Role, states: Set[State]) -> Tuple[Role, Set[State]]:
+	""" Map NVDA Role-State combinations to adjusted NVDA Role-State combinations.
+	Some combinations of roles and states may be better represented with some alternative combination.
+	As an example:
+	Role.PROGRESSBAR with State.INDETERMINATE should be represented by only the Role.BUSY_INDICATOR, with
+	no State.INDETERMINATE, or State.HALFCHECKED.
+	@param role: NVDA Role to consider
+	@param states: NVDA States to consider
+	@return: A tuple with the new Role and modified States set.
+	"""
 	if(
 		role in [Role.PROGRESSBAR, Role.BUSY_INDICATOR]
 		and states.intersection({State.INDETERMINATE, State.HALFCHECKED})
 	):
+		# Don't report indeterminate progress bars as "half-checked"
+		# L{State.HALFCHECKED} maps from oleacc.STATE_SYSTEM_MIXED,
+		# which has the same value as oleacc.STATE_SYSTEM_INDETERMINATE.
+		# L{State.INDETERMINATE} is not mapped directly from any IA or IA2 state.
+		# A progress bar that can not convey progress, only activity, is a busy indicator.
 		states.discard(State.HALFCHECKED)
 		states.add(State.INDETERMINATE)
 		return Role.BUSY_INDICATOR, states
