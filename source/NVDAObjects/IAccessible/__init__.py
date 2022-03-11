1dca110009918c6a88b629d239bb54a25edf54e1
Michael Weghorn <m.weghorn@posteo.de>
2022-03-11 02:54:56 +0100

    Add handling for accSelection returning IDispatch or negative child ID (#13277)
    Fixes #13276
    
    Summary of the issue:
    In case a single child is selected, IAccessible::get_accSelection can either return the child ID of a selected simple element or an IDispatch for a selected accessible object, s.
    https://docs.microsoft.com/en-us/windows/win32/api/oleacc/nf-oleacc-iaccessible-get_accselection
    and
    https://docs.microsoft.com/en-us/windows/win32/winauto/how-servers-implement-child-ids .
    Previously, only positive child IDs were considered in NVDA, while LibreOffice was returning negative child IDs for Calc cells, which does not fit the MSAA spec.
    NVDA did not yet support the case where an IDispatch is returned.
    
    Description of how this pull request fixes the issue:
    This PR implements the missing handling for an IDispatch returned as accSelection in NVDA.
    LibreOffice was adapted to return such a one from LibreOffice 7.4 on, corresponding LibreOffice commit:
    https://git.libreoffice.org/core/commit/00c0ee8cf0fac0c933c5ae600e99a64b1c7d4397
    In addition, negative child IDs are now also accepted.
    As pointed out by James Teh in the discussion on the
    initial version of the pull request:
    The use of negative child ids doesn't fit well into the IAccessible spec, but
    it has been done by IAccessible2 for a very long time and should be considered
    standard for all intents and purposes. A negative child id should be treated as
    a unique id, while a positive child id should be treated as a child index.
    Supporting negative child IDs fixes the issue for LibreOffice 7.3.

--- source/NVDAObjects/IAccessible/__init__.py
+++ source/NVDAObjects/IAccessible/__init__.py
@@ -13,6 +13,7 @@ from comtypes import (
 	BSTR,
 )
 from comtypes.hresult import S_OK, S_FALSE
+import comtypes.client
 import ctypes
 import os
 import re
@@ -1317,8 +1318,11 @@ the NVDAObject for IAccessible
 		if not sel:
 			raise NotImplementedError
 		# accSelection can return a child ID of a simple element, for instance in QT tree tables. 
-		# Therefore treat this as a single selection.
-		if isinstance(sel,int) and sel>0:
+		# Therefore treat this as a single selection unless the child ID is CHILDID_SELF (0).
+		if isinstance(sel, int) and sel != 0:
+			return 1
+		# accSelection can return IDispatch for a single selected child object
+		if isinstance(sel, comtypes.client.dynamic._Dispatch):
 			return 1
 		enumObj=sel.QueryInterface(IEnumVARIANT)
 		if not enumObj:
