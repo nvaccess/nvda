nvaccess/nvda

commit 44b16a1c863b95a1bec3fa72de411d3e04626bbe

Fix table navigation commands through merged cells by caching last column/row index (#13345)
Follow up on the work done by @mltony in #11923

Fixes #7278, #11919

Summary of the issue:
When issuing table navigation commands repeatedly, e.g. pressing Control+Alt+Down several times in a row, the original column index is currently not preserved. Thus, if there happens to be a merged cell on the way, the column index will be reset to the minimal column index of merged cell.

Description of how this pull request fixes the issue:
This PR caches original column index for vertical table navigation and original row index for horizontal table navigation.

source/documentBase.py
@@ -1,12 +1,42 @@
-#A part of NonVisual Desktop Access (NVDA)
-#Copyright (C) 2017 NV Access Limited
-#This file is covered by the GNU General Public License.
-#See the file COPYING for more details.
+# A part of NonVisual Desktop Access (NVDA)
+# Copyright (C) 2017-2022 NV Access Limited
+# This file is covered by the GNU General Public License.
+# See the file COPYING for more details.
 
+from dataclasses import dataclass
+from enum import Enum
+from typing import Any, Optional, Tuple, Union
 from baseObject import AutoPropertyObject, ScriptableObject
 import config
 import textInfos
 import controlTypes
+
+
+_TableID = Union[int, Tuple, Any]
+"""
+A variety of types can be used for a tableID.
+Known to be a tuple for UIA, an integer for virtual buffers.
+"""
+
+
+class _Axis(str, Enum):
+	ROW = "row"
+	COLUMN = "column"
+
+
+class _Movement(str, Enum):
+	NEXT = "next"
+	PREVIOUS = "previous"
+
+
+@dataclass
+class _TableSelection:
+	selection: textInfos.TextInfo
+	axis: _Axis
+	row: int
+	rowSpan: int
+	col: int
+	colSpan: int
 
 
 class TextContainerObject(AutoPropertyObject):
@@ -21,6 +51,8 @@ class TextContainerObject(AutoPropertyObject):
 	def makeTextInfo(self, position) -> textInfos.TextInfo:
 		return self.TextInfo(self,position)
 
+	selection: textInfos.TextInfo
+
 	def _get_selection(self):
 		return self.makeTextInfo(textInfos.POSITION_SELECTION)
 
@@ -32,6 +64,8 @@ class DocumentWithTableNavigation(TextContainerObject,ScriptableObject):
 	A document that supports standard table navigiation comments (E.g. control+alt+arrows to move between table cells).
 	The document could be an NVDAObject, or a BrowseModeDocument treeIntercepter for example.
 	"""
+
+	_lastTableSelection: Optional[_TableSelection] = None
 
 	def _getTableCellCoords(self, info):
 		"""
@@ -87,28 +121,31 @@ class DocumentWithTableNavigation(TextContainerObject,ScriptableObject):
 		raise NotImplementedError
 
 	_missingTableCellSearchLimit=3 #: The number of missing  cells L{_getNearestTableCell} is allowed to skip over to locate the next available cell
-	def _getNearestTableCell(self, tableID, startPos, origRow, origCol, origRowSpan, origColSpan, movement, axis):
+
+	def _getNearestTableCell(
+			self,
+			tableID: _TableID,
+			startPos: textInfos.TextInfo,
+			origRow: int,
+			origCol: int,
+			origRowSpan: int,
+			origColSpan: int,
+			movement: _Movement,
+			axis: _Axis,
+	) -> textInfos.TextInfo:
 		"""
 		Locates the nearest table cell relative to another table cell in a given direction, given its coordinates.
 		For example, this is used to move to the cell in the next column, previous row, etc.
 		This method will skip over missing table cells (where L{_getTableCellAt} raises LookupError), up to the number of times set by _missingTableCellSearchLimit set on this instance.
 		@param tableID: the ID of the table
 		@param startPos: the position in the document to start searching from.
-		@type startPos: L{textInfos.TextInfo}
 		@param origRow: the row number of the starting cell
-		@type origRow: int
 		@param origCol: the column number  of the starting cell
-		@type origCol: int
 		@param origRowSpan: the row span of the row of the starting cell
-		@type origRowSpan: int
 		@param origColSpan: the column span of the column of the starting cell
-		@type origColSpan: int
 		@param movement: the direction ("next" or "previous")
-		@type movement: string
 		@param axis: the axis of movement ("row" or "column")
-		@type axis: string
 		@returns: the position of the nearest table cell
-		@rtype: L{textInfos.TextInfo}
 		"""
 		if not axis:
 			raise ValueError("Axis must be row or column")
@@ -138,7 +175,11 @@ class DocumentWithTableNavigation(TextContainerObject,ScriptableObject):
 				destCol+=1 if movement=="next" else -1
 		raise LookupError
 
-	def _tableMovementScriptHelper(self, movement="next", axis=None):
+	def _tableMovementScriptHelper(
+			self,
+			movement: _Movement = _Movement.NEXT,
+			axis: Optional[_Axis] = None
+	):
 		# documentBase is a core module and should not depend on these UI modules and so they are imported
 		# at run-time. (#12404)
 		from scriptHandler import isScriptWaiting
@@ -157,6 +198,22 @@ class DocumentWithTableNavigation(TextContainerObject,ScriptableObject):
 			ui.message(_("Not in a table cell"))
 			return
 
+		# The following lines check whether user has been issuing table navigation commands repeatedly.
+		# In this case, instead of using current column/row index, we used cached value
+		# to allow users being able to skip merged cells without affecting the initial column/row index.
+		# For more info see issue #11919 and #7278.
+		if (
+			self._lastTableSelection
+			and self.selection == self._lastTableSelection.selection
+			and self._lastTableSelection.axis == axis
+		):
+			if axis == _Axis.ROW:
+				origCol = self._lastTableSelection.col
+				origColSpan = self._lastTableSelection.colSpan
+			else:
+				origRow = self._lastTableSelection.row
+				origRowSpan = self._lastTableSelection.rowSpan
+
 		try:
 			info = self._getNearestTableCell(tableID, self.selection, origRow, origCol, origRowSpan, origColSpan, movement, axis)
 		except LookupError:
@@ -169,6 +226,14 @@ class DocumentWithTableNavigation(TextContainerObject,ScriptableObject):
 		speakTextInfo(info, formatConfig=formatConfig, reason=controlTypes.OutputReason.CARET)
 		info.collapse()
 		self.selection = info
+		self._lastTableSelection = _TableSelection(
+			selection=self.selection,
+			axis=axis,
+			row=origRow,
+			rowSpan=origRow,
+			col=origCol,
+			colSpan=origColSpan,
+		)
 
 	def script_nextRow(self, gesture):
 		self._tableMovementScriptHelper(axis="row", movement="next")

tests/system/robot/chromeTests.py
@@ -1654,6 +1654,52 @@ def test_focusTargetReporting():
 	)
 
 
+def test_tableNavigationWithMergedColumns():
+	"""When navigating through a merged cell,
+	NVDA should preserve the column/row position from the previous cell.
+	Refer to #7278, #11919.
+	"""
+	_chrome.prepareChrome("""
+	<p>This is text</p>
+	<table
+	 border=0 cellpadding=0 cellspacing=0 width=192
+	 style='border-collapse: collapse;table-layout:fixed;width:144pt'
+	>
+	<col width=64 span=3 style='width:48pt'>
+	<tr height=20 style='height:15.0pt'>
+		<td height=20 width=64 style='height:15.0pt;width:48pt'>a1</td>
+		<td width=64 style='width:48pt'>b1</td>
+		<td width=64 style='width:48pt'>c1</td>
+	</tr>
+	<tr height=20 style='height:15.0pt'>
+		<td colspan=2 height=20 style='height:15.0pt'>a2 and b2</td>
+		<td>c2</td>
+	</tr>
+	</table>
+	""")
+	# Navigate to end of text, this aligns the cursor with column 2
+	actualSpeech = _chrome.getSpeechAfterKey("downArrow")
+	_asserts.strings_match(actualSpeech, "This is text")
+
+	# Navigate to the table
+	actualSpeech = _chrome.getSpeechAfterKey("downArrow")
+	_asserts.strings_match(actualSpeech, "table  with 2 rows and 3 columns  row 1  column 1  a 1")
+
+	# Navigate to a cell in row 1, column 2
+	actualSpeech = _chrome.getSpeechAfterKey("downArrow")
+	_asserts.strings_match(actualSpeech, "column 2  b 1")
+
+	# Navigate to a merged cell below
+	actualSpeech = _chrome.getSpeechAfterKey("control+alt+downArrow")
+	_asserts.strings_match(actualSpeech, "row 2  column 1  through 2  a 2 and b 2")
+
+	# Return to row 1, column 2
+	# In #7278, #11919, NVDA would return to row 1, column 1
+	# This caused column position to be lost when navigating through merged cells
+	actualSpeech = _chrome.getSpeechAfterKey("control+alt+upArrow")
+	_asserts.strings_match(actualSpeech, "row 1  column 2  b 1")
+
+
 def test_focus_mode_on_focusable_read_only_lists():
 	"""
 	If a list is read-only, but is focusable, and a list element receives focus, switch to focus mode.

tests/system/robot/chromeTests.robot
@@ -120,6 +120,9 @@ Quick Nav reports target first
 Focus reports target first
 	[Documentation]	Focus target should always be reported before ancestors. Ancestors should be reported from inner to outer.
 	test_focusTargetReporting
+Table navigation with merged columns
+	[Documentation]	When navigating through a merged cell, preserve the column/row position from the previous cell.
+	test_tableNavigationWithMergedColumns
 focus mode is turned on on focused read-only list item
 	[Documentation]	Focused list items with a focusable list container should cause focus mode to be turned on automatically.
 	test_focus_mode_on_focusable_read_only_lists

commit a55f9a62e30ae1eaa4a4b3a6e7c2d4cb7d510800

Fixing GC unreachable error of table navigation  (#13613)
Intermittent GC error "unreachable objects" caused by PR #13345.

Description of how this pull request fixes the issue:
The root cause was that we used to store TextInfo object inside Document, thus creating a cyclic reference. Such reference loops cannot be deleted by zeroing reference counter and can only be deleted by full GC sweep. I am not sure why NVDA prints warnings in this case.
This was fixed by not storing TextInfo in the first place. We now store last cell coordinates as lastRow/lastCol. Table cache is going to be valid as long as row/col of current selection are the same as stored lastRow/lastCol - and in this case we will be using trueRow/trueCol to compute next cell to preserve coordinates when navigating through merged cells.

source/documentBase.py
@@ -31,11 +31,19 @@ class _Movement(str, Enum):
 
 @dataclass
 class _TableSelection:
-	selection: textInfos.TextInfo
+	"""
+		Caches information about user navigating around the table.
+		This class is used to store true row/column number when navigating through merged cells.
+		lastRow/lastCol store coordinates of the last cell user explicitly navigated to.
+		If they don't match current selection we invalidate this cache.
+		If they match, we use trueRow/trueCol to maintain row/column through merged cells.
+	"""
 	axis: _Axis
-	row: int
+	lastRow: int
+	lastCol: int
+	trueRow: int
 	rowSpan: int
-	col: int
+	trueCol: int
 	colSpan: int
 
 
@@ -204,34 +212,38 @@ class DocumentWithTableNavigation(TextContainerObject,ScriptableObject):
 		# For more info see issue #11919 and #7278.
 		if (
 			self._lastTableSelection
-			and self.selection == self._lastTableSelection.selection
+			and origRow == self._lastTableSelection.lastRow
+			and origCol == self._lastTableSelection.lastCol
 			and self._lastTableSelection.axis == axis
 		):
 			if axis == _Axis.ROW:
-				origCol = self._lastTableSelection.col
+				origCol = self._lastTableSelection.trueCol
 				origColSpan = self._lastTableSelection.colSpan
 			else:
-				origRow = self._lastTableSelection.row
+				origRow = self._lastTableSelection.trueRow
 				origRowSpan = self._lastTableSelection.rowSpan
 
 		try:
 			info = self._getNearestTableCell(tableID, self.selection, origRow, origCol, origRowSpan, origColSpan, movement, axis)
+			newTableID, newRow, newCol, newRowSpan, newColSpan = self._getTableCellCoords(info)
 		except LookupError:
 			# Translators: The message reported when a user attempts to use a table movement command
 			# but the cursor can't be moved in that direction because it is at the edge of the table.
 			ui.message(_("Edge of table"))
 			# Retrieve the cell on which we started.
 			info = self._getTableCellAt(tableID, self.selection,origRow, origCol)
+			newTableID, newRow, newCol, newRowSpan, newColSpan = self._getTableCellCoords(info)
 
 		speakTextInfo(info, formatConfig=formatConfig, reason=controlTypes.OutputReason.CARET)
 		info.collapse()
 		self.selection = info
 		self._lastTableSelection = _TableSelection(
-			selection=self.selection,
+			lastRow=newRow,
+			lastCol=newCol,
 			axis=axis,
-			row=origRow,
+			trueRow=origRow,
 			rowSpan=origRow,
-			col=origCol,
+			trueCol=origCol,
 			colSpan=origColSpan,
 		)
 