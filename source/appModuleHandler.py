a67b5431edaf8a408c318a10bc8d48542c5a5547
Sean Budd <sean@nvaccess.org>
2022-05-04 11:06:57 +1000

    Add helperLocalBindingHandle to NVDAObject appModule devInfo

--- source/appModuleHandler.py
+++ source/appModuleHandler.py
@@ -746,6 +746,37 @@ class AppModule(baseObject.ScriptableObject):
 		"""
 		raise NotImplementedError()
 
+	devInfo: List[str]
+	"""Information about this appModule useful to developers."""
+
+	def _get_devInfo(self) -> List[str]:
+		"""Information about this appModule useful to developers.
+		For an NVDAObject, its appModule devInfo is appended to NVDAObject.devInfo.
+		Subclasses may extend this, calling the superclass property first.
+		@return: A list of text strings providing information about this appModule useful to developers.
+		"""
+		info = []
+		try:
+			ret = repr(self)
+		except Exception as e:
+			ret = "exception: %s" % e
+		info.append("appModule: %s" % ret)
+		try:
+			ret = repr(self.productName)
+		except Exception as e:
+			ret = "exception: %s" % e
+		info.append("appModule.productName: %s" % ret)
+		try:
+			ret = repr(self.productVersion)
+		except Exception as e:
+			ret = "exception: %s" % e
+		info.append("appModule.productVersion: %s" % ret)
+		try:
+			ret = repr(self.helperLocalBindingHandle)
+		except Exception as e:
+			ret = "exception: %s" % e
+		info.append("appModule.helperLocalBindingHandle: %s" % ret)
+		return info
 
 class AppProfileTrigger(config.ProfileTrigger):
 	"""A configuration profile trigger for when a particular application has focus.
