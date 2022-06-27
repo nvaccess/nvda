ebb05fe50ac635e7bd1d9c21e374833447d0ab04
Łukasz Golonka <lukasz.golonka@mailbox.org>
2022-06-27 03:02:31 +0200

    Logging: No longer mark log messages from nvda.pyw as coming from external code when running from sources (#13827)
    Summary of the issue:
    When starting NVDA from sources messages logged from nvda.pyw are shown as external. Example message:
    
    INFO - external:__main__ (17:20:02.477) - MainThread (4316):
    Starting NVDA version source-master-b817b03
    
    This is clearly wrong. The problem is caused by the trailing backslash added in bat scripts (%~dp0 is already terminated with one and adding a second backslash causes logHandler.isPathExternalToNVDA to think that the path is external).
    
    Description of user facing changes
    Log messages from nvda.pyw are no longer marked as external.
    
    Description of development approach
    All usages of %~dp0 are inspected and trailing backslash is no longer added to the paths. To make logHandler.isPathExternalToNVDA more resilient to invalid paths the path is normalized before checking if it is the same as sys.path.

--- venvUtils/ensureAndActivate.bat
+++ venvUtils/ensureAndActivate.bat
@@ -2,9 +2,14 @@
 rem this script ensures the NVDA build system Python virtual environment is created and up to date,
 rem and then activates it.
 rem This is an internal script and should not be used directly.
+set hereOrig=%~dp0
+set here=%hereOrig%
+if #%hereOrig:~-1%# == #\# set here=%hereOrig:~0,-1%
+set scriptsDir=%here%
+set venvLocation=%here%\..\.venv
 
 rem Ensure the environment is created and up to date
-py -3.7-32 "%~dp0\ensureVenv.py"
+py -3.7-32 "%scriptsDir%\ensureVenv.py"
 if ERRORLEVEL 1 goto :EOF
 
 rem Set the necessary environment variables to have Python use this virtual environment.
@@ -18,7 +23,7 @@ rem set the VIRTUAL_ENV variable instructing Python to use a virtual environment
 rem py.exe will honor VIRTUAL_ENV and launch the python.exe that it finds in %VIRTUAL_ENV%\scripts.
 rem %VIRTUAL_ENV%\scripts\python.exe will find pyvenv.cfg in its parent directory,
 rem which is actually what then causes Python to use the site-packages found in this virtual environment.
-set VIRTUAL_ENV=%~dp0..\.venv
+set VIRTUAL_ENV=%venvLocation%
 rem Add the virtual environment's scripts directory to the path
 set PATH=%VIRTUAL_ENV%\scripts;%PATH%
 rem Set an NVDA-specific variable to identify this official NVDA virtual environment from other 3rd party ones
