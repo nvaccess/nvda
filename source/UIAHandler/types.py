a30b7cf695d9ea271c879cce1b8e78883bb0f1ed
Sean Budd <sean@nvaccess.org>
2022-07-14 14:20:53 +1000

    Create typing hints for IUIAutomationTextRangeT

---
+++ source/UIAHandler/types.py
@@ -0,0 +1,62 @@
+# A part of NonVisual Desktop Access (NVDA)
+# Copyright (C) 2022 NV Access Limited
+# This file is covered by the GNU General Public License.
+# See the file COPYING for more details.
+
+from typing import (
+	Any,
+)
+from typing_extensions import (
+	Protocol,
+)
+
+from comInterfaces.UIAutomationClient import IUIAutomationTextRange
+
+
+class _IUIAutomationTextRangeT(Protocol):
+	def findText(self) -> "IUIAutomationTextRangeT":
+		...
+
+	def clone(self) -> "IUIAutomationTextRangeT":
+		...
+
+	def MoveEndpointByRange(
+			self,
+			source: int,
+			rangeObject: "IUIAutomationTextRangeT",
+			target: int
+	) -> None:
+		...
+
+	def CompareEndpoints(
+			self,
+			source: int,
+			rangeObject: "IUIAutomationTextRangeT",
+			target: int
+	) -> Any:
+		...
+
+	def compare(self, other: "IUIAutomationTextRangeT") -> Any:
+		...
+
+	def getText(self, index: int) -> str:
+		...
+
+	def Select(self) -> None:
+		...
+
+	def MoveEndpointByUnit(self, endPoint: int, unit: int, direction: int) -> None:
+		...
+
+	def Move(self, unit: int, direction: int) -> None:
+		...
+
+	def ExpandToEnclosingUnit(self, unit: int) -> None:
+		...
+
+	def GetBoundingRectangles(self) -> Any:
+		...
+
+
+class IUIAutomationTextRangeT(type(IUIAutomationTextRange), _IUIAutomationTextRangeT):
+	pass
