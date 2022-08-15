399d1d89ea741aa06b2c32ea6c3fd86b619060ed
Sean Budd <sean@nvaccess.org>
2022-08-15 13:12:38 +1000

    move manifest template to separate file

--- source/setup.py
+++ source/setup.py
@@ -1,9 +1,8 @@
 # -*- coding: UTF-8 -*-
-#setup.py
-#A part of NonVisual Desktop Access (NVDA)
-#Copyright (C) 2006-2018 NV Access Limited, Peter Vágner, Joseph Lee
-#This file is covered by the GNU General Public License.
-#See the file COPYING for more details.
+# A part of NonVisual Desktop Access (NVDA)
+# Copyright (C) 2006-2022 NV Access Limited, Peter Vágner, Joseph Lee
+# This file is covered by the GNU General Public License.
+# See the file COPYING for more details.
 
 import os
 import sys
@@ -11,18 +10,18 @@ import copy
 import gettext
 gettext.install("nvda")
 from setuptools import setup
-import py2exe as py2exeModule
 from glob import glob
 import fnmatch
 # versionInfo names must be imported after Gettext
 # Suppress E402 (module level import not at top of file)
 from versionInfo import (
+	description,
 	formatBuildVersionString,
 	name,
 	version,
-	publisher
+	publisher,
+	url,
 )  # noqa: E402
-from versionInfo import *
 from py2exe import distutils_buildexe
 from py2exe.dllfinder import DllFinder
 import wx
@@ -30,58 +29,7 @@ import importlib.machinery
 # Explicitly put the nvda_dmp dir on the build path so the DMP library is included
 sys.path.append(os.path.join("..", "include", "nvda_dmp"))
 RT_MANIFEST = 24
-manifest_template = """\
-<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<assembly
-	xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0"
-	xmlns:asmv3="urn:schemas-microsoft-com:asm.v3"
->
-	<trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
-		<security>
-			<requestedPrivileges>
-				<requestedExecutionLevel
-					level="asInvoker"
-					uiAccess="%(uiAccess)s"
-				/>
-			</requestedPrivileges>
-		</security>
-	</trustInfo>
-	<compatibility xmlns="urn:schemas-microsoft-com:compatibility.v1">
-		<application>
-			<!-- Windows 7 -->
-			<supportedOS
-				Id="{35138b9a-5d96-4fbd-8e2d-a2440225f93a}"
-			/>
-			<!-- Windows 8 -->
-			<supportedOS
-				Id="{4a2f28e3-53b9-4441-ba9c-d69d4a4a6e38}"
-			/>
-			<!-- Windows 8.1 -->
-			<supportedOS
-				Id="{1f676c76-80e1-4239-95bb-83d0f6d0da78}"
-			/>
-			<!-- Windows 10 -->
-			<supportedOS
-				Id="{8e0f7a12-bfb3-4fe8-b9a5-48fd50a15a9a}"
-			/>
-		</application> 
-	</compatibility>
-	<asmv3:application>
-		<asmv3:windowsSettings>
-			<dpiAware
-				xmlns="http://schemas.microsoft.com/SMI/2005/WindowsSettings"
-			>
-				true/pm
-			</dpiAware>
-			<dpiAwareness
-				xmlns="http://schemas.microsoft.com/SMI/2016/WindowsSettings"
-			>
-				PerMonitorV2, PerMonitor
-			</dpiAwareness>
-		</asmv3:windowsSettings>
-	</asmv3:application>
-</assembly>
-"""
+manifestTemplateFilePath = "manifest.template.xml"
 
 # py2exe's idea of whether a dll is a system dll appears to be wrong sometimes, so monkey patch it.
 orig_determine_dll_type = DllFinder.determine_dll_type
@@ -109,6 +57,8 @@ class py2exe(distutils_buildexe.py2exe):
 		self.enable_uiAccess = False
 
 	def run(self):
+		with open(manifestTemplateFilePath, "r") as manifestTemplateFile:
+			manifestTemplate = manifestTemplateFile.read()
 		dist = self.distribution
 		if self.enable_uiAccess:
 			# Add a target for nvda_uiAccess, using nvda_noUIAccess as a base.
@@ -125,7 +75,7 @@ class py2exe(distutils_buildexe.py2exe):
 				(
 					RT_MANIFEST,
 					1,
-					(manifest_template % dict(uiAccess=target['uiAccess'])).encode("utf-8")
+					(manifestTemplate % dict(uiAccess=target['uiAccess'])).encode("utf-8")
 				),
 			]
 		super(py2exe, self).run()
