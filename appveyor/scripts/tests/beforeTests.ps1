88c4ba79a19e0e8a56b820832e35d57be8e18de6
Reef Turner <reef@nvaccess.org>
2022-08-17 16:11:33 +0800

    Open chrome prior to system tests
    Theory: Chrome is busy with post install tasks, so start chrome in the
    background ahead of the tests.
    
    The first system test to run that requires chrome occasionally fails.
    This has been replicated on developer machines after chrome updates,
    however it is difficult to reproduce, and difficult to detect/recover-from
    in an automated way.
    It may be possible to handle this better within NVDA, however it seems
    to actually affect users infrequently, and they seem to be able to
    recover by waiting a few seconds and de/refocus chrome.
    In the mean time, the system tests failing delays development.

--- appveyor/scripts/tests/beforeTests.ps1
+++ appveyor/scripts/tests/beforeTests.ps1
@@ -2,4 +2,14 @@ New-Item -ItemType directory -Path testOutput
 New-Item -ItemType directory -Path testOutput\unit
 New-Item -ItemType directory -Path testOutput\system
 New-Item -ItemType directory -Path testOutput\lint
+# The first system test to run that requires chrome occasionally fails.
+# This has been replicated on developer machines after chrome updates,
+# however it is difficult to reproduce, and difficult to detect/recover-from
+# in an automated way.
+# It may be possible to handle this better within NVDA, however it seems to actually affect users
+# infrequently, and they seem to be able to recover by waiting a few seconds and de/refocus chrome.
+# In the mean time, the system tests failing delays development.
+#
+# Theory: Chrome is busy with post install tasks, so start chrome in the background ahead of the tests.
+cmd /c start /min chrome.exe
 Set-AppveyorBuildVariable "testFailExitCode" 0
