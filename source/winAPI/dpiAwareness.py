2101e705e1a57f2f9ccd19693285a9ed0b3de587
Sean Budd <sean@nvaccess.org>
2022-08-17 14:01:40 +1000

    improve docs

--- source/winAPI/dpiAwareness.py
+++ source/winAPI/dpiAwareness.py
@@ -19,14 +19,22 @@ def setDPIAwareness() -> None:
 	This function attempts to set process DPI awareness using the most modern Windows API method available.
 
 	Only call this function once per instance of NVDA.
+
 	Only call this function when running from source.
+	It is recommended that you set the process-default DPI awareness via application manifest.
+	Setting the process-default DPI awareness via these API calls can lead to unexpected application behavior.
 	"""
 	# Support is inconsistent across versions of Windows, so try/excepts are used rather than explicit
 	# version checks.
+	# https://docs.microsoft.com/en-us/windows/win32/hidpi/setting-the-default-dpi-awareness-for-a-process
 	try:
-		# Method introduced in Windows 10
+		# An advancement over the original per-monitor DPI awareness mode,
+		# which enables applications to access new DPI-related scaling behaviors on a per top-level window basis.
+		# For more information on behaviours, refer to:
 		# https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-setprocessdpiawarenesscontext
 		DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2 = -4
+		# Method introduced in Windows 10
+		# https://docs.microsoft.com/en-us/windows/win32/hidpi/dpi-awareness-context
 		success = ctypes.windll.user32.SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2)
 	except AttributeError:
 		log.debug("Cannot set DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2")
@@ -51,9 +59,11 @@ def setDPIAwareness() -> None:
 	log.debug("Falling back to older method of setting DPI Awareness")
 
 	try:
-		# Method introduced in Windows 8
 		# https://docs.microsoft.com/en-us/windows/win32/api/shellscalingapi/nf-shellscalingapi-setprocessdpiawareness
+		# This window checks for the DPI when it is created and adjusts the scale factor whenever the DPI changes.
+		# These processes are not automatically scaled by the system.
 		PROCESS_PER_MONITOR_DPI_AWARE = 2
+		# Method introduced in Windows 8
 		hResult = ctypes.windll.shcore.SetProcessDpiAwareness(PROCESS_PER_MONITOR_DPI_AWARE)
 	except AttributeError:
 		log.debug("Cannot set PROCESS_PER_MONITOR_DPI_AWARE")
