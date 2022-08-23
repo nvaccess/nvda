ae45408bbf23f66dd87fde618148f67b71a4faff
Sean Budd <sean@nvaccess.org>
2022-08-23 12:15:15 +1000

    add deprecation comments to encapsulated variables

--- source/globalVars.py
+++ source/globalVars.py
@@ -56,25 +56,42 @@ class DefaultAppArgs(argparse.Namespace):
 # Encapsulated by api module,
 # refer to #14037 for removal strategy.
 desktopObject: typing.Optional['NVDAObjects.NVDAObject'] = None
+"""Deprecated, use `setDesktopObject|getDesktopObject` from `api` instead"""
 foregroundObject: typing.Optional['NVDAObjects.NVDAObject'] = None
+"""Deprecated, use `setForegroundObject|getForegroundObject` from `api` instead"""
 focusObject: typing.Optional['NVDAObjects.NVDAObject'] = None
+"""Deprecated, use `setFocusObject|getFocusObject` from `api` instead"""
 focusAncestors: typing.List['NVDAObjects.NVDAObject'] = []
+"""Deprecated, use `getFocusAncestors` from `api` instead"""
 focusDifferenceLevel=None
+"""Deprecated, use `getFocusDifferenceLevel` from `api` instead"""
 mouseObject: typing.Optional['NVDAObjects.NVDAObject'] = None
+"""Deprecated, use ``setMouseObject|getMouseObject`` from `api` instead"""
 navigatorObject: typing.Optional['NVDAObjects.NVDAObject'] = None
+"""Deprecated, use ``setNavigatorObject|getNavigatorObject`` from `api` instead"""
 reviewPosition=None
+"""Deprecated, use ``getReviewPosition|setReviewPosition`` from `api` instead"""
 reviewPositionObj=None
+"""Deprecated, use ``api.getReviewPosition().obj`` instead"""
 
 # unused, should eventually get removed.
 mouseOldX = None
+"""Deprecated, this is unused and not set by NVDA core"""
 mouseOldY = None
+"""Deprecated, this is unused and not set by NVDA core"""
 lastProgressValue = 0
+"""Deprecated, this is unused and not set by NVDA core"""
 
 # TODO: encapsulate in NVDAState
 startTime: float = 0.0
+"""Deprecated, use ``NVDAState.getStartTime`` instead"""
 appArgs = DefaultAppArgs()
 unknownAppArgs: typing.List[str] = []
 exitCode=0
+"""
+Deprecated, this should not be used by add-on authors.
+NVDA core should use `NVDAState._getExitCode|_setExitCode` instead.
+"""
 appPid: int = 0
 """The process ID of NVDA itself.
 """
