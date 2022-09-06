135a5519b8b6a7f8e5dc6e86fca07bf39c524dca
Reef Turner <reef@nvaccess.org>
2022-09-06 19:13:00 +0800

    Appveyor timing collection

---
+++ appveyor/scripts/logCiTiming.ps1
@@ -0,0 +1,21 @@
+# Input: timing.csv
+# Expect two cols:
+#   - Build-stage name
+#   - Time [Get-Date -o] (Round trip format)
+# Process: add elapsed minutes between entries
+# Output:
+# - timingWithElapsed.csv
+# - CI Timing message to Appveyor messages.
+
+$entries = Import-Csv -Path .\timing.csv -Header Stage, Time
+$lastTime = Get-Date -Date $entries[0].Time
+
+foreach ($e in $entries) {
+    $nextTime = Get-Date -Date $e.Time
+    $elapsedMin = ($nextTime - $lastTime).TotalMinutes
+    $e | Add-Member -NotePropertyName ElapsedMin -NotePropertyValue $elapsedMin
+}
+
+$entries | Export-Csv -Append -Path timingWithElapsed.csv
+
+Add-AppveyorMessage "CI timing:\n$entries"
