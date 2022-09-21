a19ceff9ffbb9d531720b5efe74aa563cd2ad401
Sean Budd <sean@nvaccess.org>
2022-09-21 11:29:00 +1000

    update copyright headers

--- source/addonAPIVersion.py
+++ source/addonAPIVersion.py
@@ -1,7 +1,7 @@
-#A part of NonVisual Desktop Access (NVDA)
-#Copyright (C) 2018 NV Access Limited
-#This file is covered by the GNU General Public License.
-#See the file COPYING for more details.
+# A part of NonVisual Desktop Access (NVDA)
+# Copyright (C) 2018-2023 NV Access Limited
+# This file is covered by the GNU General Public License.
+# See the file COPYING for more details.
 
 import buildVersion
 import re
