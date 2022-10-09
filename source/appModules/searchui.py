971d63e4e94bc7a66868afca4ff5d3c17e6a1668
Joseph Lee <joseph.lee22590@gmail.com>
2022-10-09 16:56:18 -0700

    appModules/searchui: add type hint for description getter. Re #13841.
    Comment from Sean Budd (NV Access): add type hint please (returns str).

--- source/appModules/searchui.py
+++ source/appModules/searchui.py
@@ -20,7 +20,7 @@ class StartMenuSearchField(SearchField):
 	# #7370: do not announce text when start menu (searchui) closes.
 	announceNewLineText = False
 
-	def _get_description(self):
+	def _get_description(self) -> str:
 		# #13841: detect search highlights and anounce it.
 		if self.lastChild.UIAAutomationId == "PlaceholderTextContentPresenter":
 			return self.lastChild.name
