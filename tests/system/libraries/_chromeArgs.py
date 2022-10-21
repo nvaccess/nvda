9a30254dc17b7329f0e353296b7c19bc45c04e48
Reef Turner <feerrenrut@users.noreply.github.com>
2022-10-21 18:32:20 +0800

    Prevent Chrome 'session crashed bubble' (PR #14262)
    When Chrome crashes, it shows a bubble informing of the crash when it restarts.
    The bubble takes focus and interferes with subsequent tests.
    It is rare that this happens, but it can cause confusion when trying to investigate.
    Instead, start Chrome with parameter to prevent the bubble.

--- tests/system/libraries/_chromeArgs.py
+++ tests/system/libraries/_chromeArgs.py
@@ -21,6 +21,15 @@ def getChromeArgs() -> str:
 		" --no-default-browser-check"  # Don't bother to check if Chrome is the default browser.
 		" --lang=en-US"  # Set GUI lang to English to ensure tests pass on non-English systems. Must be supplied
 		# to the first Chrome process started.
+		" --disable-session-crashed-bubble"
+		# --disable-session-crashed-bubble: If chrome crashes, don't cause subsequent tests to fail.
+		# However, the config can be checked to determine if a crash occurred.
+		# See https://superuser.com/a/1343331
+		# Use %APPDATA%\Local\Google\Chrome\User Data\Default\Preference
+		# Values:
+		# "exit_type": "none",
+		# "exited_cleanly": true,
+		#
 	)
 
 
