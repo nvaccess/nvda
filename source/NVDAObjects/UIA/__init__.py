12803e92dfd58b93f9dfbd9608ee253eccc55854
Michael Curran <mick@nvaccess.org>
2022-10-23 13:18:28 +1000

    Add significantly more debug logging to UIAHandler (#14256)
    Summary of the issue:
    Although NVDA has a specific uIA debug logging category, this does not really enable all that many more log messages a part from some debug warnings. thus it is almost useless to ask for a user's log with this flag enabled.
    
    Description of user facing changes
    Setting NVDA's log level to debug, and enabling the UIA debug logging category in NVDA's advanced settings panel, will now produce significantly more logging for UIA. This includes logging when all UIA event handler methods are called, along with their parameters, and logging for utility utility methods such as checking if a window or element is native UIA.
    
    Description of development approach
    Added a bunch of methods to UIAHandler for generating a string representation for debugging, for things such as UIA elements, property IDs, event IDs, notification types etc.
    Added conditional debug logging to all UIA event handler methods, plus isUIAWindow, isNativeUIAElement and getNearestWindowhandle.

--- source/NVDAObjects/UIA/__init__.py
+++ source/NVDAObjects/UIA/__init__.py
@@ -1237,6 +1237,11 @@ class UIA(Window):
 		windowHandle=kwargs.get('windowHandle')
 		if isinstance(relation,tuple):
 			UIAElement=UIAHandler.handler.clientObject.ElementFromPointBuildCache(POINT(relation[0],relation[1]),UIAHandler.handler.baseCacheRequest)
+			if UIAHandler._isDebug():
+				log.debug(
+					f"kwargsFromSuper: given coordinates {relation}, "
+					f"fetched element {UIAHandler.handler.getUIAElementDebugString(UIAElement)}"
+				)
 			# Ignore this object if it is non native.
 			if not UIAHandler.handler.isNativeUIAElement(UIAElement):
 				if UIAHandler._isDebug():
@@ -1254,6 +1259,11 @@ class UIA(Window):
 			except COMError:
 				log.debugWarning("getFocusedElement failed", exc_info=True)
 				return False
+			if UIAHandler._isDebug():
+				log.debug(
+					f"kwargsFromSuper: fetched focused element "
+					f"{UIAHandler.handler.getUIAElementDebugString(UIAElement)}"
+				)
 			# Ignore this object if it is non native.
 			if ignoreNonNativeElementsWithFocus and not UIAHandler.handler.isNativeUIAElement(UIAElement):
 				if UIAHandler._isDebug():
@@ -1294,6 +1304,11 @@ class UIA(Window):
 		UIACachedWindowHandle=UIAElement.cachedNativeWindowHandle
 		self.UIAIsWindowElement=bool(UIACachedWindowHandle)
 		if not windowHandle:
+			if UIAHandler._isDebug():
+				log.debug(
+					f"No windowHandle for UIA NvDAObject. "
+					f"Searching UIA element ancestry for nearest windowHandle"
+				)
 			windowHandle=UIAHandler.handler.getNearestWindowHandle(UIAElement)
 		if not windowHandle:
 			raise InvalidNVDAObject("no windowHandle")
