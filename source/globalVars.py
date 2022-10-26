cd8c2abb27fd267bdd6e768c35ba24816104c18e
Sean Budd <sean@nvaccess.org>
2022-10-26 15:35:05 +1100

    consistent lang

--- source/globalVars.py
+++ source/globalVars.py
@@ -37,7 +37,7 @@ class DefaultAppArgs(argparse.Namespace):
 	This is set to True when NVDA starts with the --secure parameter.
 	This is also set to True when NVDA is running on a secure screen
 	(systemUtils._isSecureDesktop() returns True)
-	and the serviceDebug parameter has not been set.
+	and the serviceDebug parameter is not set.
 
 	For more information, refer to devDocs/technicalDesignOverview.md 'Logging in secure mode'
 	and the following userGuide sections:
