30bb50c0fa759c7c334ee2d79b11f36061a2b499
Sean Budd <sean@nvaccess.org>
2022-11-04 13:41:13 +1100

    queue update of object caches during first core pump

--- source/core.py
+++ source/core.py
@@ -9,6 +9,8 @@
 
 from dataclasses import dataclass
 from typing import (
+	Any,
+	Callable,
 	List,
 	Optional,
 )
@@ -419,6 +421,30 @@ def _handleNVDAModuleCleanupBeforeGUIExit():
 	brailleViewer.destroyBrailleViewer()
 
 
+def _pollForForegroundHWND() -> Optional[int]:
+	"""
+	@note: The foreground window should usually be fetched on the first try,
+	however it may take longer if Windows is taking a long time changing window focus.
+	Gives up after 10 seconds (MAX_WAIT_TIME_SECS).
+	"""
+	from utils.blockUntilConditionMet import blockUntilConditionMet
+	import winUser
+
+	# winUser.getForegroundWindow may return NULL in certain circumstances,
+	# such as when a window is losing activation.
+	# This should not remain the case for an extended period of time.
+	MAX_WAIT_TIME_SECS = 10
+
+	success, foregroundHWND = blockUntilConditionMet(
+		getValue=winUser.getForegroundWindow,
+		giveUpAfterSeconds=MAX_WAIT_TIME_SECS,
+	)
+	if success:
+		return foregroundHWND
+	log.error("NVDA could not fetch the foreground window.")
+	return None
+
+
 def _initializeObjectCaches():
 	"""
 	Caches the desktop object.
@@ -443,6 +469,35 @@ def _initializeObjectCaches():
 		api.setFocusObject(desktopObject)
 		api.setNavigatorObject(desktopObject)
 		api.setMouseObject(desktopObject)
+
+
+def _updateObjectCachesToForeground():
+	"""
+	Initializes object caches (other than desktop object) to the foreground window.
+	Before this, the object that is cached is the desktopObject,
+	however this may leak secure information to the lock screen.
+	The foreground window is set as the object cache,
+	as the foreground window would already be accessible on the lock screen (e.g. Magnifier).
+	It also is more intuitive that NVDA focuses the foreground window,
+	as opposed to the desktop object.
+
+	@note: The foreground window should usually be fetched on the first try,
+	however it may take longer if Windows is taking a long time changing window focus.
+	Gives up after 10 seconds (MAX_WAIT_TIME_SECS in _pollForForegroundHWND).
+	"""
+	import api
+	import NVDAObjects
+	
+	foregroundHWND = _pollForForegroundHWND()
+	if not foregroundHWND:
+		log.error("Could not update object caches to foreground window.")
+		return
+
+	foregroundObject = NVDAObjects.window.Window(windowHandle=foregroundHWND)
+	api.setForegroundObject(foregroundObject)
+	api.setFocusObject(foregroundObject)
+	api.setNavigatorObject(foregroundObject)
+	api.setMouseObject(foregroundObject)
 
 
 def main():
@@ -771,10 +826,18 @@ def main():
 	log.debug("Initializing core pump")
 	class CorePump(gui.NonReEntrantTimer):
 		"Checks the queues and executes functions."
+
+		def __init__(self, run: Optional[Callable[[None], Any]] = None):
+			self._firstRun = True
+			super().__init__(run)
+
 		def run(self):
 			global _isPumpPending
 			_isPumpPending = False
 			watchdog.alive()
+			if self._firstRun:
+				_updateObjectCachesToForeground()
+				self.firstRun = False
 			try:
 				if touchHandler.handler:
 					touchHandler.handler.pump()
