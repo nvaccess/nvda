cb47620f5cc635950f92ff6522e1af7ecec6ee1a
Sean Budd <sean@nvaccess.org>
2022-12-06 11:45:57 +1100

    improve test docs

--- tests/unit/test_util/test_security.py
+++ tests/unit/test_util/test_security.py
@@ -6,6 +6,10 @@
 """Unit tests for the blockUntilConditionMet submodule.
 """
 
+from dataclasses import dataclass
+from typing import (
+	Optional,
+)
 import unittest
 from unittest.mock import patch
 
@@ -15,8 +19,17 @@ from utils.security import (
 import winUser
 
 
+@dataclass
+class _MoveWindow:
+	startIndex: int  # A window at this index
+	endIndex: int  # is moved to this index
+	triggerIndex: int  # when this index is reached
+	triggered = False
+
+
 class _Test_getWindowZIndex(unittest.TestCase):
 	def _getWindow_patched(self, hwnd: winUser.HWNDVal, relation: int) -> int:
+		"""Fetch current window, find adjacent window by relation."""
 		currentWindowIndex = self._windows.index(hwnd)
 		if relation == winUser.GW_HWNDNEXT:
 			try:
@@ -37,7 +50,8 @@ class _Test_getWindowZIndex(unittest.TestCase):
 		return _helper
 
 	def setUp(self) -> None:
-		self._getDesktopWindowPatch = patch("winUser.getDesktopWindow", lambda: 0)  # value is discarded by _getTopWindowPatch
+		# value from _getDesktopWindowPatch is discarded by _getTopWindowPatch
+		self._getDesktopWindowPatch = patch("winUser.getDesktopWindow", lambda: 0)
 		self._getTopWindowPatch = patch("winUser.getTopWindow", lambda _hwnd: self._windows[0])
 		self._getWindowPatch = patch("winUser.getWindow", self._getWindow_patched)
 		self._getDesktopWindowPatch.start()
@@ -69,7 +83,7 @@ class Test_getWindowZIndex_static(_Test_getWindowZIndex):
 
 
 class Test_getWindowZIndex_dynamic(_Test_getWindowZIndex):
-	_triggered = False
+	_queuedMove: Optional[_MoveWindow] = None
 
 	def _getWindow_patched(self, hwnd: winUser.HWNDVal, relation: int) -> int:
 		self._moveIndexToNewIndexAtIndexOnce(self._windows.index(hwnd))
@@ -80,42 +94,83 @@ class Test_getWindowZIndex_dynamic(_Test_getWindowZIndex):
 		from logging import getLogger
 		
 		getLogger().error(f"{currentIndex}")
-		if currentIndex == self._triggerIndex and not self._triggered:
-			self._triggered = True
-			window = self._windows.pop(self._startIndex)
-			self._windows.insert(self._endIndex, window)
+		if (
+			self._queuedMove
+			and currentIndex == self._queuedMove.triggerIndex
+			and not self._queuedMove.triggered
+		):
+			self._queuedMove.triggered = True
+			window = self._windows.pop(self._queuedMove.startIndex)
+			self._windows.insert(self._queuedMove.endIndex, window)
 
 	def test_prev_windowMoves_pastTarget(self):
 		"""A previous window is moved past the target window. This does not affect the z-order."""
-		self._startIndex = 2  # A window at this index
-		self._endIndex = 9  # is moved to this index
-		self._triggerIndex = 5  # when this index is reached
+		self._queuedMove = _MoveWindow(
+			startIndex=2,
+			endIndex=9,
+			triggerIndex=5
+		)
 		targetWindow = 7
 		expectedIndex = targetWindow - 1
 		self.assertEqual(expectedIndex, _getWindowZIndex(self._windowMatches(targetWindow)))
 
 	def test_prev_windowMoves_beforeTarget(self):
 		"""A previous window is moved before the target window. It is counted twice."""
-		self._startIndex = 2
-		self._endIndex = 5
-		self._triggerIndex = 3
+		self._queuedMove = _MoveWindow(
+			startIndex=2,
+			endIndex=5,
+			triggerIndex=3
+		)
 		targetWindow = 7
 		expectedIndex = targetWindow  # difference is normally 1, however a window is counted twice
 		self.assertEqual(expectedIndex, _getWindowZIndex(self._windowMatches(targetWindow)))
 
 	def test_active_windowMoves_pastTarget(self):
 		"""A window we are looking at moves past the match, skipping our target window."""
-		self._startIndex = 3
-		self._endIndex = 8
-		self._triggerIndex = 3
+		self._queuedMove = _MoveWindow(
+			startIndex=3,
+			endIndex=8,
+			triggerIndex=3
+		)
 		targetWindow = 6
 		self.assertEqual(None, _getWindowZIndex(self._windowMatches(targetWindow)))
 
 	def test_active_windowMoves_beforeTarget(self):
-		pass
+		"""A future window we are looking at moves forward, but before the match, decreasing our z-index."""
+		self._queuedMove = _MoveWindow(
+			startIndex=3,
+			endIndex=4,
+			triggerIndex=3
+		)
+		targetWindow = 7
+		# difference is normally 1, however a window is skipped due to the move
+		expectedIndex = targetWindow - 2
+		self.assertEqual(expectedIndex, _getWindowZIndex(self._windowMatches(targetWindow)))
 
 	def test_future_windowMoves_pastTarget(self):
-		pass
+		"""A future window moves forward, and past the match.
+		This does not change anything in practice, the windows affected are yet to be indexed.
+		However, during the move, the z-index to target is decreased,
+		as a window is moved past the match."""
+		self._queuedMove = _MoveWindow(
+			startIndex=5,
+			endIndex=9,
+			triggerIndex=3
+		)
+		targetWindow = 7
+		# difference is normally 1, however a window is skipped due to the move
+		expectedIndex = targetWindow - 2
+		self.assertEqual(expectedIndex, _getWindowZIndex(self._windowMatches(targetWindow)))
 
 	def test_future_windowMoves_beforeTarget(self):
-		pass
+		"""A future window moves forward, and past the match.
+		This does not change anything in practice, the windows affected are yet to be indexed."""
+		self._queuedMove = _MoveWindow(
+			startIndex=5,
+			endIndex=7,
+			triggerIndex=3
+		)
+		targetWindow = 9
+		# difference is normally 1
+		expectedIndex = targetWindow - 1
+		self.assertEqual(expectedIndex, _getWindowZIndex(self._windowMatches(targetWindow)))
