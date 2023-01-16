b5523fd0a26b860bf7a760a0d594fddfac966199
Joseph Lee <joseph.lee22590@gmail.com>
2023-01-15 17:37:21 -0800

    appModules/explorer: hwnd: int -> hwnd type
    Co-authored-by: Sean Budd <seanbudd123@gmail.com>

--- source/appModules/explorer.py
+++ source/appModules/explorer.py
@@ -504,7 +504,8 @@ class AppModule(appModuleHandler.AppModule):
 
 		nextHandler()
 
-	def isGoodUIAWindow(self, hwnd: int) -> bool:
+	from winAPI.types import HWNDValT
+	def isGoodUIAWindow(self, hwnd: HWNDValT) -> bool:
 		currentWinVer = winVersion.getWinVer()
 		# #9204: shell raises window open event for emoji panel in build 18305 and later.
 		if (
