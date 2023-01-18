309b733a99a9985a9738de5d6e5c03a1d4b8b96e
Cyrille Bougot <cyrille.bougot2@laposte.net>
2023-01-18 08:03:44 +0100

    Update source/speechDictHandler/__init__.py
    Co-authored-by: Sean Budd <seanbudd123@gmail.com>

--- source/speechDictHandler/__init__.py
+++ source/speechDictHandler/__init__.py
@@ -1,4 +1,3 @@
-# speechDictHandler.py
 # A part of NonVisual Desktop Access (NVDA)
 # Copyright (C) 2006-2023 NVDA Contributors <http://www.nvda-project.org/>
 # This file is covered by the GNU General Public License.
