6f7b6c3f2e9488e2bfad6733a02c8f52a5a41273
Cyrille Bougot <cyrille.bougot2@laposte.net>
2023-01-19 08:39:08 +0100

    Reformat error message strings.

--- source/gui/speechDict.py
+++ source/gui/speechDict.py
@@ -111,7 +111,7 @@ class DictionaryEntryDialog(
 				raise(e)
 			gui.messageBox(
 				# Translators: This is an error message to let the user know that the dictionary entry is not valid.
-				_("Regular Expression error in the pattern field: \"%s\".") % e,
+				_("Regular Expression error in the pattern field: \"{error}\".").format(error=e),
 				# Translators: The title of an error message raised by the Dictionary Entry dialog
 				_("Dictionary Entry Error"),
 				wx.OK | wx.ICON_WARNING,
@@ -127,7 +127,7 @@ class DictionaryEntryDialog(
 				raise(e)
 			gui.messageBox(
 				# Translators: This is an error message to let the user know that the dictionary entry is not valid.
-				_("Regular Expression error in the replacement field: \"%s\".") % e,
+				_("Regular Expression error in the replacement field: \"{error}\".").format(error=e),
 				# Translators: The title of an error message raised by the Dictionary Entry dialog
 				_("Dictionary Entry Error"),
 				wx.OK | wx.ICON_WARNING,
