2aa07a5595f0f7ca679e0158588a173085b6458a
Joseph Lee <joseph.lee22590@gmail.com>
2023-03-25 13:36:22 -0700

    Calculator/UIA notification: fetch results via UIA descendants. Re #14679.
    If a portable copy of NVDA is running and compact overlay mode is active from Calculator, NVDA will not announce results as users type expressions, caused by index error stemming from tree traversal failure. As an alternative, use UIA descendants to locate results element, allowing portable NVDA to announce results when Calculator is in compact overlay mde. The find descendants code ultimately comes from the one employed in File Explorer when fetching status bar items.

--- source/appModules/calculator.py
+++ source/appModules/calculator.py
@@ -13,6 +13,8 @@ import queueHandler
 import ui
 import scriptHandler
 import braille
+import UIAHandler
+from comtypes import COMError
 
 # #9428: do not announce current values until calculations are done in order to avoid repetitions.
 noCalculatorEntryAnnouncements = [
@@ -91,13 +93,18 @@ class AppModule(appModuleHandler.AppModule):
 		# Therefore, only handle the below activity Id.
 		if activityId == "DisplayUpdated":
 			braille.handler.message(displayString)
-			resultElement = api.getForegroundObject().children[1].lastChild
-			# Descend one more time in Windows 11 Calculator.
-			if calculatorVersion >= 11:
-				resultElement = resultElement.firstChild
+			# Locate results via UIA tree traversal.
 			# Redesigned in 2019 due to introduction of "always on top" i.e. compact overlay mode.
-			if resultElement.UIAElement.cachedClassName != "LandmarkTarget":
-				resultElement = resultElement.parent.children[1]
+			clientObject = UIAHandler.handler.clientObject
+			condition = clientObject.createPropertyCondition(UIAHandler.UIA_ClassNamePropertyId, "LandmarkTarget")
+			walker = clientObject.createTreeWalker(condition)
+			uiItemWindow = clientObject.elementFromHandle(obj.windowHandle)
+			try:
+				element = walker.getFirstChildElement(uiItemWindow)
+				element = element.buildUpdatedCache(UIAHandler.handler.baseCacheRequest)
+			except (ValueError, COMError):
+				return
+			resultElement = UIA(UIAElement=element)
 			# Display string announcement is redundant if speak typed characters is on.
 			if (
 				resultElement
