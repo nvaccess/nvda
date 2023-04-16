2da161787690eced25dc185fb948bca3a6cfa505
Cyrille Bougot <cyrille.bougot2@laposte.net>
2023-04-16 23:23:34 +0200

    Make appModules path more robust. (Credit @lukaszgo1)

--- source/appModuleHandler.py
+++ source/appModuleHandler.py
@@ -48,7 +48,7 @@ from systemUtils import getCurrentProcessLogonSessionId, getProcessLogonSessionI
 
 # Dictionary of processID:appModule pairs used to hold the currently running modules
 runningTable: Dict[int, AppModule] = {}
-_CORE_APP_MODULES_PATH: os.PathLike = appModules.__path__[-1]
+_CORE_APP_MODULES_PATH: os.PathLike = os.path.dirname(appModules.__spec__.origin)
 _getAppModuleLock=threading.RLock()
 #: Notifies when another application is taking foreground.
 #: This allows components to react upon application switches.
