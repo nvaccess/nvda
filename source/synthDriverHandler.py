34062a1ef68e13b52c8fb00f33a8ee80bcf50c1a
Sean Budd <sean@nvaccess.org>
2023-06-28 10:50:09 +1000

    fix up mistake

--- source/synthDriverHandler.py
+++ source/synthDriverHandler.py
@@ -348,7 +348,6 @@ class SynthDriver(driverHandler.Driver):
 			val = c[s.id]
 			if onlyChanged and getattr(self, s.id) == val:
 				continue
-			# log.debug() ? what is being set here and why?
 			setattr(self, s.id, val)
 		log.debug(
 			(
