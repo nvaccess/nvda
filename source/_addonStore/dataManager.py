c88df67e95b7c6e1c1aa8195b7a17043bc1377a9
Sean Budd <sean@nvaccess.org>
2023-07-10 10:21:30 +1000

    Add-on Store: Remove invalid cache file

--- source/_addonStore/dataManager.py
+++ source/_addonStore/dataManager.py
@@ -158,6 +158,7 @@ class _DataManager:
 				cacheData = json.load(cacheFile)
 			except Exception:
 				log.exception(f"Invalid add-on store cache")
+				os.remove(cacheFilePath)
 				return None
 		try:
 			data = cacheData["data"]
@@ -166,6 +167,7 @@ class _DataManager:
 			nvdaAPIVersion = cacheData["nvdaAPIVersion"]
 		except KeyError:
 			log.exception(f"Invalid add-on store cache:\n{cacheData}")
+			os.remove(cacheFilePath)
 			return None
 		return CachedAddonsModel(
 			cachedAddonData=_createStoreCollectionFromJson(data),
