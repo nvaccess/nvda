23b6698b09140b62b40487bf5f2a0842623e0829
burmancomp <otayhteytta@burmanintk.fi>
2023-07-12 19:06:22 +0300

    indentation fix

--- source/braille.py
+++ source/braille.py
@@ -2469,7 +2469,7 @@ class BrailleHandler(baseObject.AutoPropertyObject):
 			self.setTether(TetherTo.FOCUS.value, auto=True)
 		if self._tether != TetherTo.FOCUS.value:
 			# However, braille display content is updated in case where:
-	# braille is tethered to review, review cursor does not follow system caret,
+			# braille is tethered to review, review cursor does not follow system caret,
 			# and navigator object is focus object or its ancestor.
 			if (
 				not config.conf["reviewCursor"]["followCaret"]
