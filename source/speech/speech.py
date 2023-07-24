78628be5987465895e995253c8a7e566ab14a16f
SamKacer <35394441+SamKacer@users.noreply.github.com>
2023-07-24 06:27:29 +0100

    Add option for ignoring blank lines for report line indentation (#15057)
    closes #13394
    
    Summary of the issue:
    As elaborated in #13394, line indentation reporting can be too noisy in contexts where indentation changes for blank lines aren't meaningful and distract from the actually meaningful indentation changes, such as is the case when reading source code for Python and many other programming languages.
    
    Description of user facing changes
    A new checkbox is added to Document formatting, "Ignore blank lines for line indentation reporting". By default it is off, in which case line indentation reporting behaves as it does currently. If the user ticks the checkbox, then indentation changes are not reported for blank lines.
    
    There is also a new unassigned global command for toggling the setting.
    
    Description of development approach
    new config property: ignoreBlankLinesForRLI = boolean(default=false)
    short for ignoreBlankLinesForReportLineIndentation, which was too long and causing Flake8 issues
    new settings panel checkbox for toggling ignoreBlankLinesForRLI
    in speech.speech.getTextInfoSpeech, if ignoreBlankLinesForRLI is true and the line is blank, the block of code that inserts the indentation announcement and updates the indentation cache is skipped, otherwise it behaves as usual
    user guide updated with brief info on new option
    added unassigned global command for toggling ignoreBlankLinesForRLI
    added system test for confirming the correct behavior

--- source/speech/speech.py
+++ source/speech/speech.py
@@ -1540,7 +1540,21 @@ def getTextInfoSpeech(  # noqa: C901
 				if autoLanguageSwitching and newLanguage!=lastLanguage:
 					relativeSpeechSequence.append(LangChangeCommand(newLanguage))
 					lastLanguage=newLanguage
-	if reportIndentation and speakTextInfoState and allIndentation!=speakTextInfoState.indentationCache:
+	if (
+		reportIndentation
+		and speakTextInfoState
+		and(
+			# either not ignoring blank lines
+			not formatConfig['ignoreBlankLinesForRLI']
+			# or line isn't completely blank
+			or any(
+				not (set(t) <= LINE_END_CHARS)
+				for t in textWithFields
+				if isinstance(t, str)
+			)
+		)
+		and allIndentation != speakTextInfoState.indentationCache
+	):
 		indentationSpeech=getIndentationSpeech(allIndentation, formatConfig)
 		if autoLanguageSwitching and speechSequence[-1].lang is not None:
 			# Indentation must be spoken in the default language,
@@ -1602,6 +1616,10 @@ def getTextInfoSpeech(  # noqa: C901
 
 	yield speechSequence
 	return True
+
+
+# for checking a line is completely blank, i.e. doesn't even contain spaces
+LINE_END_CHARS = frozenset(('\r', '\n'))
 
 
 def _isControlEndFieldCommand(command: Union[str, textInfos.FieldCommand]):
