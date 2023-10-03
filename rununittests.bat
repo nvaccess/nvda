c6fcafa0678fc50afc237d049a65480ec611d738
Sean Budd <sean@nvaccess.org>
2023-10-03 12:22:23 +1100

    use xmlrunner for xml unit test output

--- rununittests.bat
+++ rununittests.bat
@@ -4,5 +4,7 @@ set here=%hereOrig%
 if #%hereOrig:~-1%# == #\# set here=%hereOrig:~0,-1%
 set scriptsDir=%here%\venvUtils
 set unitTestsPath=%here%\tests\unit
+set testOutput=%here%\testOutput\unit
+md %testOutput%
 
-call "%scriptsDir%\venvCmd.bat" py -m unittest discover -v -s "%unitTestsPath%" -t "%here%" %*
+call "%scriptsDir%\venvCmd.bat" py -m xmlrunner discover -v -s "%unitTestsPath%" -t "%here%" --output-file "%testOutput%\report.xml" %*
