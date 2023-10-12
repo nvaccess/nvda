nvaccess/nvda

commit d1ce3bc9359088d9184ef60e3e2891a513d42795

Try to log exceptions in IoThread (#14899)
Summary of the issue:
I'm still seeing access violations in the IoThread here and there, same for @bramd. I really want to track these down! We need to handle those more gracefully anyway.

Description of user facing changes
Better logging

Description of development approach
De run block of the IOThread is now wrapped in a try/except that logs the stack. I hope the stack will give us more info about what's going wrong here.

source/hwIo/ioThread.py
@@ -17,6 +17,7 @@ from extensionPoints.util import AnnotatableWeakref, BoundMethodWeakref
 from inspect import ismethod
 from buildVersion import version_year
 import NVDAState
+from watchdog import getFormattedStacksForAllThreads
 
 LPOVERLAPPED_COMPLETION_ROUTINE = ctypes.WINFUNCTYPE(
 	None,
@@ -197,7 +198,12 @@ class IoThread(threading.Thread):
 		self.handle = None
 
 	def run(self):
-		while True:
-			ctypes.windll.kernel32.SleepEx(winKernel.INFINITE, True)
-			if self.exit:
-				break
+		try:
+			while True:
+				ctypes.windll.kernel32.SleepEx(winKernel.INFINITE, True)
+				if self.exit:
+					break
+		except Exception:
+			log.critical("Exception in IoThread function", exc_info=True)
+			stacks = getFormattedStacksForAllThreads()
+			log.info(f"Listing stacks for Python threads after IoThread crash:\n{stacks}")

commit 03c8abe6b730727a2fcd4fa3bbb813f1818660e5

Move getFormattedStacksForAllThreads from watchdog to logHandler, to fix build of developer documentation. (#15616)
Related to #12971 problem caused by PR #14899

Summary of the issue:
When trying to build developer documentation with Sphinx, many modules failed to be parsed

Description of development approach
getFormattedStacksForAllThreads is moved from watchdog to the logHandler, which avoids these warnings. Appropriate backwards compatibility is added to watchdog, to make sure add-on authors are warned to use the moved function from logHandler in the future.

source/hwIo/ioThread.py
@@ -11,11 +11,9 @@ import winKernel
 import typing
 from logHandler import log
 from serial.win32 import OVERLAPPED, LPOVERLAPPED
-from contextlib import contextmanager
 from extensionPoints.util import AnnotatableWeakref, BoundMethodWeakref
 from inspect import ismethod
-from buildVersion import version_year
-from watchdog import getFormattedStacksForAllThreads
+from logHandler import getFormattedStacksForAllThreads
 
 
 LPOVERLAPPED_COMPLETION_ROUTINE = ctypes.WINFUNCTYPE(

source/logHandler.py
@@ -9,6 +9,7 @@
 import os
 import ctypes
 import sys
+import threading
 import warnings
 import logging
 import inspect
@@ -37,6 +38,24 @@ _NVDA_CODE_PATH = os.path.dirname(__file__)
 We cannot use `globalVars.appDir`, since for binary builds it points to the directory with NVDA binaries,
 whereas for compiled versions NVDA's code files are in `library.zip`.
 """
+
+
+def getFormattedStacksForAllThreads() -> str:
+	"""Generates a string containing a call stack for every Python thread in this process.
+
+	The generated string is suitable for logging.
+	"""
+	# First collect the names of all threads that have actually been started by Python itself.
+	threadNamesByID = {x.ident: x.name for x in threading.enumerate()}
+	stacks = []
+	# If a Python function is entered by a thread that was not started by Python itself,
+	# It will have a frame, but won't be tracked by Python's threading module and therefore will have no name.
+	for ident, frame in sys._current_frames().items():
+		# The strings in the formatted stack all end with \n, so no join separator is necessary.
+		stack = "".join(traceback.format_stack(frame))
+		name = threadNamesByID.get(ident, "Unknown")
+		stacks.append(f"Python stack for thread {ident} ({name}):\n{stack}")
+	return "\n".join(stacks)
 
 
 def isPathExternalToNVDA(path: str) -> bool:

source/watchdog.py
@@ -5,10 +5,12 @@
 
 import sys
 import os
-import traceback
 import time
 from time import perf_counter as _timer
 import threading
+from typing import (
+	Any,
+)
 import inspect
 from ctypes import windll, oledll
 import ctypes.wintypes
@@ -17,10 +19,25 @@ import comtypes
 import winUser
 import winKernel
 from logHandler import log
+import logHandler
 import globalVars
 import core
 import exceptions
 import NVDAHelper
+import NVDAState
+
+
+def __getattr__(attrName: str) -> Any:
+	"""Module level `__getattr__` used to preserve backward compatibility."""
+	if attrName == "getFormattedStacksForAllThreads" and NVDAState._allowDeprecatedAPI():
+		log.warning(
+			"Importing getFormattedStacksForAllThreads from here is deprecated. "
+			"getFormattedStacksForAllThreads should be imported from logHandler instead.",
+			stack_info=True,
+		)
+		return logHandler.getFormattedStacksForAllThreads
+	raise AttributeError(f"module {repr(__name__)} has no attribute {repr(attrName)}")
+
 
 MIN_CORE_ALIVE_TIMEOUT = 0.5
 """The minimum time (seconds) to wait for the core to be alive.
@@ -51,23 +68,6 @@ _watcherThread=None
 _cancelCallEvent = None
 
 
-def getFormattedStacksForAllThreads():
-	"""
-	Generates a string containing a call stack for every Python thread in this process, suitable for logging.
-	"""
-	# First collect the names of all threads that have actually been started by Python itself.
-	threadNamesByID = {x.ident: x.name for x in threading.enumerate()}
-	stacks = []
-	# If a Python function is entered by a thread that was not started by Python itself,
-	# It will have a frame, but won't be tracked by Python's threading module and therefore will have no name.
-	for ident, frame in sys._current_frames().items():
-		# The strings in the formatted stack all end with \n, so no join separator is necessary.
-		stack = "".join(traceback.format_stack(frame))
-		name = threadNamesByID.get(ident, "Unknown")
-		stacks.append(f"Python stack for thread {ident} ({name}):\n{stack}")
-	return "\n".join(stacks)
-
-
 def alive():
 	"""Inform the watchdog that the core is alive.
 	"""
@@ -92,6 +92,7 @@ def alive():
 			-int(SECOND_TO_100_NANOSECOND * MIN_CORE_ALIVE_TIMEOUT)
 		)),
 		0, None, None, False)
+
 
 def asleep():
 	"""Inform the watchdog that the core is going to sleep.
@@ -162,7 +163,7 @@ def _watcher():
 def waitForFreezeRecovery(waitedSince: float):
 	log.info(f"Starting freeze recovery after {_timer() - waitedSince} seconds.")
 	if log.isEnabledFor(log.DEBUGWARNING):
-		stacks = getFormattedStacksForAllThreads()
+		stacks = logHandler.getFormattedStacksForAllThreads()
 		log.debugWarning(
 			f"Listing stacks for Python threads:\n{stacks}"
 		)
@@ -183,7 +184,7 @@ def waitForFreezeRecovery(waitedSince: float):
 			# Core is completely frozen.
 			# Collect formatted stacks for all Python threads.
 			log.error(f"Core frozen in stack! ({curTime - waitedSince} seconds)")
-			stacks = getFormattedStacksForAllThreads()
+			stacks = logHandler.getFormattedStacksForAllThreads()
 			log.info(f"Listing stacks for Python threads:\n{stacks}")
 		_recoverAttempt()
 		time.sleep(RECOVER_ATTEMPT_INTERVAL)
@@ -255,7 +256,7 @@ def _crashHandler(exceptionInfo):
 		log.critical("NVDA crashed! Minidump written to %s" % dumpPath)
 
 	# Log Python stacks for every thread.
-	stacks = getFormattedStacksForAllThreads()
+	stacks = logHandler.getFormattedStacksForAllThreads()
 	log.info(f"Listing stacks for Python threads:\n{stacks}")
 
 	log.info("Restarting due to crash")