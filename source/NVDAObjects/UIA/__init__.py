e64d9f358bd88eeccc92057d955b17be0adc1d46
Joseph Lee <joseph.lee22590@gmail.com>
2023-10-19 10:42:04 -0600

    NVDAObjects.UIA: remove UIA text range freeze notice for Windows 7.
    As NVDA supports Windows 8.1 and later, remove notice on UIA range code that references Windows 7 UIA library freeze in Start menu.

--- source/NVDAObjects/UIA/__init__.py
+++ source/NVDAObjects/UIA/__init__.py
@@ -460,9 +460,6 @@ class UIATextInfo(textInfos.TextInfo):
 			# sometimes rangeFromChild can return a NULL range
 			if not self._rangeObj: raise LookupError
 		elif isinstance(position,locationHelper.Point):
-			if winVersion.getWinVer() <= winVersion.WIN7_SP1:
-				# #9435: RangeFromPoint causes a freeze in UIA client library in the Windows 7 start menu!
-				raise NotImplementedError("RangeFromPoint not supported on Windows 7")
 			self._rangeObj: IUIAutomationTextRangeT = self.obj.UIATextPattern.RangeFromPoint(position.toPOINT())
 		elif isinstance(position, UIAHandler.IUIAutomationTextRange):
 			position = typing.cast(IUIAutomationTextRangeT, position)
