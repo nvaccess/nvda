f64a065cff68618a2255a1629c4af28d310abb77
Joseph Lee <joseph.lee22590@gmail.com>
2023-10-22 15:40:33 -0600

    Ease of Access: update copyright header

--- source/easeOfAccess.py
+++ source/easeOfAccess.py
@@ -1,5 +1,5 @@
 # A part of NonVisual Desktop Access (NVDA)
-# Copyright (C) 2014-2022 NV Access Limited
+# Copyright (C) 2014-2023 NV Access Limited
 # This file is covered by the GNU General Public License.
 # See the file COPYING for more details.
 
