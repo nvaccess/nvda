dd56d486238e2f8cf5f3093402cd2ec402c54769
James Teh <jamie@jantrid.net>
2023-11-15 11:57:49 +1000

    [WIP] WASAPI: Play silence to keep the audio device alive.

--- source/nvwave.py
+++ source/nvwave.py
@@ -779,6 +779,8 @@ class WasapiWavePlayer(garbageHandler.TrackedObject):
 	_isIdleCheckPending: bool = False
 	#: Use the default device, this is the configSpec default value.
 	DEFAULT_DEVICE_KEY = "default"
+	#: The silence output device, None if not initialized.
+	_silenceDevice: typing.Optional[str] = None
 
 	def __init__(
 			self,
@@ -832,6 +834,13 @@ class WasapiWavePlayer(garbageHandler.TrackedObject):
 		self.open()
 		self._lastActiveTime: typing.Optional[float] = None
 		self._isPaused: bool = False
+		# todo: Make silence configurable.
+		if WasapiWavePlayer._silenceDevice != outputDevice:
+			# The output device has changed. (Re)initialize silence.
+			if self._silenceDevice is not None:
+				NVDAHelper.localLib.wasSilence_terminate()
+			NVDAHelper.localLib.wasSilence_init(outputDevice)
+			WasapiWavePlayer._silenceDevice = outputDevice
 
 	@wasPlay_callback
 	def _callback(cppPlayer, feedId):
@@ -912,6 +921,8 @@ class WasapiWavePlayer(garbageHandler.TrackedObject):
 			self._doneCallbacks[feedId.value] = onDone
 		self._lastActiveTime = time.time()
 		self._scheduleIdleCheck()
+		# todo: Make silence duration configurable.
+		NVDAHelper.localLib.wasSilence_playFor(30000)
 
 	def sync(self):
 		"""Synchronise with playback.
@@ -1063,6 +1074,7 @@ def initialize():
 		NVDAHelper.localLib.wasPlay_pause,
 		NVDAHelper.localLib.wasPlay_resume,
 		NVDAHelper.localLib.wasPlay_setChannelVolume,
+		NVDAHelper.localLib.wasSilence_init,
 	):
 		func.restype = HRESULT
 		func.errcheck = _wasPlay_errcheck
@@ -1071,6 +1083,8 @@ def initialize():
 
 
 def terminate() -> None:
+	if WasapiWavePlayer._silenceDevice is not None:
+		NVDAHelper.localLib.wasSilence_terminate()
 	getOnErrorSoundRequested().unregister(playErrorSound)
 
 
