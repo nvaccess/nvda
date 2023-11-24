81b600b13ed8931c465fa19ed38dfa1b81c05cc9
Sean Budd <sean@nvaccess.org>
2023-11-24 11:11:53 +1100

    test adding ctyles.util manually

--- source/setup.py
+++ source/setup.py
@@ -219,10 +219,9 @@ freeze(
 		],
 		"includes": [
 			"nvdaBuiltin",
-			# #3368: bisect was implicitly included with Python 2.7.3, but isn't with 2.7.5.
-			"bisect",
 			# robotremoteserver (for system tests) depends on xmlrpc.server
 			"xmlrpc.server",
+			"ctypes.util",
 		],
 	},
 	data_files=[
