79b2274cb689f66ff6d5fa9b3c515f4e93da2484
Emil-18 <135248352+Emil-18@users.noreply.github.com>
2023-11-24 18:45:54 +0100

    Update source/NVDAObjects/behaviors.py
    Co-authored-by: Leonard de Ruijter <leonardder@users.noreply.github.com>

--- source/NVDAObjects/behaviors.py
+++ source/NVDAObjects/behaviors.py
@@ -888,7 +888,11 @@ class _FakeTableCell(NVDAObject):
 		states.discard(controlTypes.State.CHECKED)
 		return states
 	def _isEqual(self, other):
-		return(self.columnNumber == other.columnNumber and self.rowNumber == other.rowNumber)
+		return (
+			self.parent == other.parent
+			and self.columnNumber == other.columnNumber
+			and self.rowNumber == other.rowNumber
+		)
 
 class FocusableUnfocusableContainer(NVDAObject):
 	"""Makes an unfocusable container focusable using its first focusable descendant.
