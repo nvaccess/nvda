bd23536fcf13619399373eb6856aaae36ad8cf35
Leonard de Ruijter <L.de.Ruijter@sevenp.nl>
2023-11-29 13:43:14 +0100

    Update copyright headers according to wiki

--- extras/controllerClient/examples/example_rust/src/bindgen.rs
+++ extras/controllerClient/examples/example_rust/src/bindgen.rs
@@ -1,6 +1,7 @@
-// Copyright(C) 2023 NV Access Limited, Leonard de Ruijter
-// This file is covered by the GNU Lesser General Public License, version 2.1.
-// See the file license.txt for more details.
+// A part of NonVisual Desktop Access (NVDA)
+// Copyright (C) 2023 NV Access Limited, Leonard de Ruijter
+// This file may be used under the terms of the GNU Lesser General Public License, version 2.1.
+// For more details see: https://www.gnu.org/licenses/lgpl-2.1.html
 
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
