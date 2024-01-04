ddd9696b41a90907a66924cedf2fd72cc80a2296
Sean Budd <sean@nvaccess.org>
2024-01-04 16:07:24 +1100

    fix lint

--- keyCommandsDoc.py
+++ keyCommandsDoc.py
@@ -17,7 +17,8 @@ Generation of the Key Commands document requires certain commands to be included
 These commands must begin at the start of the line and take the form::
 	%kc:command: arg
 
-The kc:title command must appear first and specifies the title of the Key Commands document. For example::
+The kc:title command must appear first and specifies the title of the Key Commands document.
+For example:
 	%kc:title: NVDA Key Commands
 
 The rest of these commands are used to include key commands into the document.
@@ -32,7 +33,8 @@ For example::
 	%kc:endInclude
 
 The kc:settingsSection command indicates the beginning of a section documenting individual settings.
-It specifies the header row for a table summarising the settings indicated by the kc:setting command (see below).
+It specifies the header row for a table summarising the settings indicated by the kc:setting command
+(see below).
 In order, it must consist of a name column, a column for each keyboard layout and a description column.
 For example::
 	%kc:settingsSection: || Name | Desktop command | Laptop command | Description |
@@ -40,15 +42,17 @@ For example::
 The kc:setting command indicates a section for an individual setting.
 It must be followed by:
 	* A heading containing the name of the setting;
-	* A table row for each keyboard layout, or if the key is common to all layouts, a single line of text specifying the key after a colon;
+	* A table row for each keyboard layout, or if the key is common to all layouts,
+	a single line of text specifying the key after a colon;
 	* A blank line; and
 	* A line describing the setting.
-For example::
+For example:
 	%kc:setting
 	==== Braille Tethered To ====
 	| Desktop command | NVDA+control+t |
 	| Laptop Command | NVDA+control+t |
-	This option allows you to choose whether the braille display will follow the system focus, or whether it follows the navigator object / review cursor.
+	This option allows you to choose whether the braille display will follow the system focus,
+	or whether it follows the navigator object / review cursor.
 """
 
 from enum import auto, Enum, IntEnum, StrEnum
@@ -247,7 +251,10 @@ class KeyCommandsPreprocessor(Preprocessor):
 				keys.append(m.group(1))
 				break
 			elif not Regex.TABLE_ROW.value.match(line):
-				raise KeyCommandsError(f"{self._lineNum}, setting command: There must be one table row for each keyboard layout")
+				raise KeyCommandsError(
+					f"{self._lineNum}, setting command: "
+					"There must be one table row for each keyboard layout"
+				)
 
 			# This is a table row.
 			# The key will be the second column.
