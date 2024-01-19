77cb13d048b09457962634e273c6ecc4f2461a13
Łukasz Golonka <lukasz.golonka@mailbox.org>
2024-01-19 14:30:36 +0100

    Lint

--- source/gui/settingsDialogs.py
+++ source/gui/settingsDialogs.py
@@ -145,7 +145,9 @@ class SettingsDialog(
 		if state is cls.DialogState.DESTROYED and not multiInstanceAllowed:
 			# The dialog has been destroyed by wx, but the instance is still available.
 			# This indicates there is something keeping it alive.
-			raise RuntimeError(f"Cannot open new settings dialog while instance still exists: {firstMatchingInstance!r}")
+			raise RuntimeError(
+				f"Cannot open new settings dialog while instance still exists: {firstMatchingInstance!r}"
+			)
 		obj = super().__new__(cls, *args, **kwargs)
 		SettingsDialog._instances[obj] = cls.DialogState.CREATED
 		return obj
