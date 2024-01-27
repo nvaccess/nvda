16e6c0146d9ec2c08d2bb63ef42092a2f9ebbec0
Emil-18 <emilhesmyr@icloud.com>
2024-01-27 19:36:32 +0100

    Updated inputCore.py

--- source/inputCore.py
+++ source/inputCore.py
@@ -526,14 +526,14 @@ class InputManager(baseObject.AutoPropertyObject):
 		immediate = getattr(gesture, "_immediate", True)
 		speechEffect = gesture.speechEffectWhenExecuted
 		if speechEffect == gesture.SPEECHEFFECT_CANCEL:
-			from globalCommands import commands
-			clearRegions = not(script in [commands.script_braille_scrollBack, commands.script_braille_scrollForward])
-			queueHandler.queueFunction(
-				queueHandler.eventQueue,
-				speech.cancelSpeech,
-				_immediate=immediate,
-				clearBrailleRegions=clearRegions
-			)
+			# Import late to avoid circular import.
+			import braille
+			with braille.handler.suppressClearBrailleRegions(script):
+				queueHandler.queueFunction(
+					queueHandler.eventQueue,
+					speech.cancelSpeech,
+					_immediate=immediate,
+				)
 		elif speechEffect in (gesture.SPEECHEFFECT_PAUSE, gesture.SPEECHEFFECT_RESUME):
 			queueHandler.queueFunction(queueHandler.eventQueue, speech.pauseSpeech, speechEffect == gesture.SPEECHEFFECT_PAUSE)
 
