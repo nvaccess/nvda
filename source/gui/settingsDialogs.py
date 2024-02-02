f4b64f9a5d99fc804704dc739ced5c5ffffc90d5
mltony <anton.malykh+0@gmail.com>
2024-02-02 11:39:53 -0800

    Addressing comments by @CyrilleB79

--- source/gui/settingsDialogs.py
+++ source/gui/settingsDialogs.py
@@ -3235,6 +3235,7 @@ class AdvancedPanelControls(
 			keyPath=["audio", "WASAPI"],
 			conf=config.conf,
 		))
+		self.wasapiComboBox .Bind(wx.EVT_CHOICE, self._onWasapiChange)
 		self.bindHelpEvent("WASAPI", self.wasapiComboBox)
 
 		silenceDurationLabelText = _(
@@ -3252,7 +3253,9 @@ class AdvancedPanelControls(
 			max=maxDuration,
 			initial=config.conf["audio"]["silenceTimeSeconds"]
 		)
+		self.silenceDurationEdit.defaultValue = self._getDefaultValue(["audio", "silenceTimeSeconds"])
 		self.bindHelpEvent("SilenceDuration", self.silenceDurationEdit)
+		self._onWasapiChange(None)
 
 		# Translators: This is the label for a group of advanced options in the
 		# Advanced settings panel
@@ -3316,6 +3319,12 @@ class AdvancedPanelControls(
 		path=config.getScratchpadDir(ensureExists=True)
 		os.startfile(path)
 
+	def _onWasapiChange(self, evt: wx.CommandEvent) -> None:
+		self.silenceDurationEdit.Enable(
+			config.conf["audio"]["WASAPI"]
+			and self.wasapiComboBox._getControlCurrentValue().value != 2 # wasapi not disabled
+		)
+
 	def _getDefaultValue(self, configPath):
 		return config.conf.getConfigValidation(configPath).default
 
@@ -3371,6 +3380,7 @@ class AdvancedPanelControls(
 		self.caretMoveTimeoutSpinControl.SetValue(self.caretMoveTimeoutSpinControl.defaultValue)
 		self.reportTransparentColorCheckBox.SetValue(self.reportTransparentColorCheckBox.defaultValue)
 		self.wasapiComboBox.resetToConfigSpecDefault()
+		self.silenceDurationEdit.SetValue(self.silenceDurationEdit.defaultValue)
 		self.logCategoriesList.CheckedItems = self.logCategoriesList.defaultCheckedItems
 		self.playErrorSoundCombo.SetSelection(self.playErrorSoundCombo.defaultValue)
 		self._defaultsRestored = True
