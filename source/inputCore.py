a4c44036ed47844d6b8eec7009f5769ad83908b6
Emil-18 <135248352+Emil-18@users.noreply.github.com>
2024-02-12 20:14:19 +0100

    Update source/inputCore.py
    Co-authored-by: Sean Budd <seanbudd123@gmail.com>

--- source/inputCore.py
+++ source/inputCore.py
@@ -528,11 +528,16 @@ class InputManager(baseObject.AutoPropertyObject):
 		if speechEffect == gesture.SPEECHEFFECT_CANCEL:
 			# Import late to avoid circular import.
 			import braille
+			def delayedCanceledSpeech():
+				import time
+				time.sleep(3)
+				speech.cancelSpeech()
+
 			with braille.handler.suppressClearBrailleRegions(script):
 				queueHandler.queueFunction(
 					queueHandler.eventQueue,
-					speech.cancelSpeech,
-					_immediate=immediate,
+					delayedCanceledSpeech,
+					_immediate=False,
 				)
 		elif speechEffect in (gesture.SPEECHEFFECT_PAUSE, gesture.SPEECHEFFECT_RESUME):
 			queueHandler.queueFunction(queueHandler.eventQueue, speech.pauseSpeech, speechEffect == gesture.SPEECHEFFECT_PAUSE)
