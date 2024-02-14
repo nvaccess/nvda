47e85b48bdeed8da24d53dabe6b0ad69fadcfe04
Nael-Sayegh <naelousay@gmail.com>
2024-02-14 16:37:31 +0100

    Correction too lint

--- source/gui/settingsDialogs.py
+++ source/gui/settingsDialogs.py
@@ -888,7 +888,10 @@ class GeneralSettingsPanel(SettingsPanel):
 				item.Disable()
 			settingsSizerHelper.addItem(item)
 			# Translators: The label of a checkbox in general settings to toggle allowing of usage stats gathering
-			item=self.allowUsageStatsCheckBox=wx.CheckBox(self,label=_("Allow NV Access to gather NVDA usage statistics"))
+			item = self.allowUsageStatsCheckBox = wx.CheckBox(
+				self,
+				label=_("Allow NV Access to gather NVDA usage statistics")
+			)
 			self.bindHelpEvent("GeneralSettingsGatherUsageStats", self.allowUsageStatsCheckBox)
 			item.Value=config.conf["update"]["allowUsageStats"]
 			if globalVars.appArgs.secure:
