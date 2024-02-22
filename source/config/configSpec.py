5da261dcd33a41f375441d4a47e132ef4d6e8a47
mltony <34804124+mltony@users.noreply.github.com>
2024-02-21 16:32:50 -0800

    Update source/config/configSpec.py
    Co-authored-by: Sean Budd <seanbudd123@gmail.com>

--- source/config/configSpec.py
+++ source/config/configSpec.py
@@ -58,7 +58,7 @@ schemaVersion = integer(min=0, default={latestSchemaVersion})
 	keepAudioAwakeTimeSeconds = integer(default=30, min=0, max=3600)
 	whiteNoiseVolume = integer(default=0, min=0, max=100)
 	soundSplitState = integer(default=0)
-	includedSoundSplitModes =int_list(default=list(0, 1, 2))
+	includedSoundSplitModes = int_list(default=list(0, 1, 2))
 
 # Braille settings
 [braille]
