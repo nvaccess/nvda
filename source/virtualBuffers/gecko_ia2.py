6723a1ebb752694603ac63efc5f8f61cd7693aa5
Sascha Cowley <16543535+SaschaCowley@users.noreply.github.comclear>
2024-03-06 13:50:10 +1100

    Undid some no-longer needed changes

--- source/virtualBuffers/gecko_ia2.py
+++ source/virtualBuffers/gecko_ia2.py
@@ -128,14 +128,12 @@ class Gecko_ia2_TextInfo(VirtualBufferTextInfo):
 			attrs['placeholder']= placeholder
 
 		role = IAccessibleHandler.NVDARoleFromAttr(attrs['IAccessible::role'])
-		
 		if attrs.get('IAccessible2::attribute_tag',"").lower()=="blockquote":
 			role=controlTypes.Role.BLOCKQUOTE
-		
+
 		states = IAccessibleHandler.getStatesSetFromIAccessibleAttrs(attrs)
 		states |= IAccessibleHandler.getStatesSetFromIAccessible2Attrs(attrs)
 		role, states = controlTypes.transformRoleStates(role, states)
-		if role == controlTypes.Role.GROUPING: log.debug("Found a grouping")
 
 		if role == controlTypes.Role.EDITABLETEXT and not (controlTypes.State.FOCUSABLE in states or controlTypes.State.UNAVAILABLE in states or controlTypes.State.EDITABLE in states):
 			# This is a text leaf.
