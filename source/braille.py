ac0e758a98e341cb44f5855e87a689e15875a15a
Leonard de Ruijter <3049216+LeonarddeR@users.noreply.github.com>
2024-05-07 15:09:01 +0200

    Revert "Remove redundant braille.BrailleHandler.update executions (#16463)"
    This reverts commit 29f4c5c00d97e61053488cbd8824a5c4b55586d1.

--- source/braille.py
+++ source/braille.py
@@ -2535,9 +2535,8 @@ class BrailleHandler(baseObject.AutoPropertyObject):
 		self.mainBuffer.update()
 		# Last region should receive focus.
 		self.mainBuffer.focus(region)
-		if isinstance(region, TextInfoRegion):
-			self.scrollToCursorOrSelection(region)
-		elif self.buffer is self.mainBuffer:
+		self.scrollToCursorOrSelection(region)
+		if self.buffer is self.mainBuffer:
 			self.update()
 		elif self.buffer is self.messageBuffer and keyboardHandler.keyCounter>self._keyCountForLastMessage:
 			self._dismissMessage()
@@ -2592,7 +2591,7 @@ class BrailleHandler(baseObject.AutoPropertyObject):
 			self.mainBuffer.restoreWindow()
 			if scrollTo is not None:
 				self.scrollToCursorOrSelection(scrollTo)
-			elif self.buffer is self.mainBuffer:
+			if self.buffer is self.mainBuffer:
 				self.update()
 			elif (
 				self.buffer is self.messageBuffer
