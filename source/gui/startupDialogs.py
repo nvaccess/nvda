faf2789d89c8d59723d7aaaaff94ed19c3f0cb19
TylerChristie302 <94720793+TylerChristie302@users.noreply.github.com>
2024-05-08 18:17:33 -0700

    Normalize references to Insert keys #13648 (#16489)
    Fixes #13648
    
    Summary of the issue:
    The Welcome Screen and User Guide used different terms, "main Insert" and "Extended Insert" respectively, to refer to the same key.
    
    Description of user facing changes
    Both the Welcome Screen and User Guide now call Extended/main Insert by simply "Insert."
    
    Description of development approach
    Modified wording in userGuide and startupDialogs files, as well as the startup system test that checks the Welcome Screen text.

--- source/gui/startupDialogs.py
+++ source/gui/startupDialogs.py
@@ -35,7 +35,7 @@ class WelcomeDialog(
 		# Translators: The main message for the Welcome dialog when the user starts NVDA for the first time.
 		"Most commands for controlling NVDA require you to hold down"
 		" the NVDA key while pressing other keys.\n"
-		"By default, the numpad Insert and main Insert keys may both be used as the NVDA key.\n"
+		"By default, the Insert and numpad Insert keys may both be used as the NVDA key.\n"
 		"You can also configure NVDA to use the CapsLock as the NVDA key.\n"
 		"Press NVDA+n at any time to activate the NVDA menu.\n"
 		"From this menu, you can configure NVDA, get help, and access other NVDA functions."
