916fd30dfc8da538e3bbc17fd5e4b2320a8dba0d
Luke Davis <XLTechie+github@newanswertech.com>
2024-05-09 06:20:23 -0400

    Converted function comment to docstring.

--- source/monkeyPatches/wxMonkeyPatches.py
+++ source/monkeyPatches/wxMonkeyPatches.py
@@ -5,12 +5,14 @@
 
 
 def apply(mainFrame, winUser, wx):
-	# In wxPython >= 4.1,
-	# wx.CallAfter no longer executes callbacks while NVDA's main thread is within a popup menu or message box.
-	# To work around this,
-	# MonkeyPatch wx.CallAfter to
-	# post a WM_NULL message to our top-level window after calling the original CallAfter,
-	# which causes wx's event loop to wake up enough to execute the callback.
+	"""Patch wx.CallAfter to overcome an execution timing issue.
+
+	In wxPython >= 4.1, wx.CallAfter no longer executes callbacks while NVDA's main thread is within
+	a popup menu or message box.
+	To work around this, monkeypatch wx.CallAfter to
+	post a WM_NULL message to our top-level window after calling the original CallAfter,
+	which causes wx's event loop to wake up enough to execute the callback.
+	"""
 	old_wx_CallAfter = wx.CallAfter
 
 	def wx_CallAfter_wrapper(func, *args, **kwargs):
