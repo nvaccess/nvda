67ed588477fa02dc144b43994cbc89624c5239be
Luke Davis <XLTechie+github@newanswertech.com>
2024-05-09 03:18:28 -0400

    Update copyright

--- source/monkeyPatches/wxMonkeyPatches.py
+++ source/monkeyPatches/wxMonkeyPatches.py
@@ -1,5 +1,5 @@
 # A part of NonVisual Desktop Access (NVDA)
-# Copyright (C) 2021 NV Access Limited
+# Copyright (C) 2021-2024 NV Access Limited
 # This file is covered by the GNU General Public License.
 # See the file COPYING for more details.
 
