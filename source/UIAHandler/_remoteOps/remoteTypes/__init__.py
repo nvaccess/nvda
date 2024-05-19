f8eb1d329647eee8289155eac51288cbc8fae863
Michael Curran <mick@nvaccess.org>
2024-05-20 08:09:47 +1000

    Split private enum utils out of remotetypes.py into _enum.py

--- source/UIAHandler/_remoteOps/remoteTypes/__init__.py
+++ source/UIAHandler/_remoteOps/remoteTypes/__init__.py
@@ -30,56 +30,15 @@ from comtypes import (
 )
 import enum
 from UIAHandler import UIA
-from . import lowLevel
-from . import instructions
-from . import builder
-from .remoteFuncWrapper import (
+from .. import lowLevel
+from .. import instructions
+from .. import builder
+from ..remoteFuncWrapper import (
 	remoteMethod,
 	remoteMethod_mutable
 )
-from . import operation
-
-
-class c_long_enum(c_long):
-	_enumType: Type[enum.IntEnum]
-
-	def __repr__(self):
-		return f"{c_long.__name__} enum {repr(self._enumType(self.value))}"
-
-
-_ctypeIntEnumCache: dict[Type[enum.IntEnum], Type[_SimpleCData]] = {}
-
-
-def _makeCtypeIntEnum(enumType: Type[enum.IntEnum]) -> Type[_SimpleCData]:
-	cachedCls = _ctypeIntEnumCache.get(enumType)
-	if cachedCls is not None:
-		return cachedCls
-
-	class cls(c_long_enum):
-		_enumType = enumType
-
-	cls.__name__ = f"{cls.__name__}_{enumType.__name__}"
-	cast(Type[_SimpleCData], cls)
-	_ctypeIntEnumCache[enumType] = cls
-	return cls
-
-
-_RemoteEnumCache: dict[Type[enum.IntEnum], Type[RemoteInt]] = {}
-
-
-def _makeRemoteEnum(enumType: Type[enum.IntEnum]) -> Type[RemoteInt]:
-	cachedCls = _RemoteEnumCache.get(enumType)
-	if cachedCls is not None:
-		return cachedCls
-
-	class cls(RemoteInt):
-		LocalType = enumType
-		_ctype = _makeCtypeIntEnum(enumType)
-
-	cls.__name__ = f"RemoteEnum_{enumType.__name__}"
-	cast(Type[RemoteInt], cls)
-	_RemoteEnumCache[enumType] = cls
-	return cls
+from .. import operation
+from ._enum import _makeCtypeIntEnum
 
 
 _remoteFunc_self = TypeVar('_remoteFunc_self', bound=builder._RemoteBase)
