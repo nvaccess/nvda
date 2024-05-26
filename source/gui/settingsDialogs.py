51fe9b7f8282e0bf9edb2e45b3e92a55c604ebd1
mltony <34804124+mltony@users.noreply.github.com>
2024-05-26 16:24:10 -0700

    Making sound split classes and functions private (#16606)
    Following @seanbudd's suggestion in #16591 making classes and functions private.
    The rationale is that we would like to refactor sound split code so that more code is going to be reused in application volume adjuster, but #16591 will have to be postponed to v2024.3. So we need to make sound split API private to avoid breaking API changes.

--- source/gui/settingsDialogs.py
+++ source/gui/settingsDialogs.py
@@ -2812,7 +2812,7 @@ class AudioPanel(SettingsPanel):
 		index = self.soundSplitComboBox.GetSelection()
 		config.conf["audio"]["soundSplitState"] = index
 		if nvwave.usingWasapiWavePlayer():
-			audio.setSoundSplitState(audio.SoundSplitState(index))
+			audio._setSoundSplitState(audio.SoundSplitState(index))
 		config.conf["audio"]["includedSoundSplitModes"] = [
 			mIndex
 			for mIndex in range(len(self._allSoundSplitModes))
