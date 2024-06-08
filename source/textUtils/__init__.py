5bf722fe991bc16886798e3e199a7e4842b5ab4e
Leonard de Ruijter <alderuijter@gmail.com>
2024-06-08 13:52:48 +0200

    Import Generator

--- source/textUtils/__init__.py
+++ source/textUtils/__init__.py
@@ -13,7 +13,7 @@ import locale
 import unicodedata
 from abc import ABCMeta, abstractmethod, abstractproperty
 from functools import cached_property
-from typing import Optional, Tuple, Type
+from typing import Generator, Optional, Tuple, Type
 
 from logHandler import log
 
