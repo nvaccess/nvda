ff13fda2a32cbc433eae8144bb8f845af349d2b4
gerald-hartig <153188145+gerald-hartig@users.noreply.github.com>
2024-06-11 14:22:41 +1000

    Adding signpath module

--- appveyor/scripts/sign.ps1
+++ appveyor/scripts/sign.ps1
@@ -8,12 +8,4 @@ param(
     [string]$FileToSign
 )
 
-# Check if the Submit-SigningRequest command is available
-if (-not (Get-Command -Name Submit-SigningRequest -ErrorAction SilentlyContinue)) {
-    # If the command is not available, install the SignPath module
-    Install-Module -Name SignPath -Scope CurrentUser -Force
-}
-
-# Execute Submit-SigningRequest command from the SignPath module
-# TODO replace test_signing_policy with prod policy slug
 Submit-SigningRequest -ApiToken $ApiToken -InputArtifactPath $FileToSign -OutputArtifactPath $FileToSign -OrganizationId "12147e94-bba9-4fef-b29b-300398e90c5a" -ProjectSlug "NVDA"  -SigningPolicySlug "release_signing_policy" -WaitForCompletion -Force
