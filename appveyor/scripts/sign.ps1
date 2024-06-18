42080181d81bdcadbe7c53f2dfddc40df127e2df
gerald-hartig <153188145+gerald-hartig@users.noreply.github.com>
2024-06-18 16:51:13 +1000

    Update sign.ps1

--- appveyor/scripts/sign.ps1
+++ appveyor/scripts/sign.ps1
@@ -8,33 +8,5 @@ param(
     [string]$FileToSign
 )
 
-$VerbosePreference = "Continue"
-$DebugPreference = "Continue"
-
-# Check if the Submit-SigningRequest command is available
-if (-not (Get-Command -Name Submit-SigningRequest -ErrorAction SilentlyContinue)) {
-
-    # connectivity checks
-    # try {
-    #     Invoke-WebRequest -Uri "https://www.powershellgallery.com" -UseBasicParsing -ErrorAction Stop
-    #     Write-Output "Internet connectivity check passed."
-    # } catch {
-    #     Write-Error "Internet connectivity check failed. Cannot reach PowerShell Gallery."
-    # }    
-    
-    Get-PSRepository
-    Write-Output "PowerShell Version: $($PSVersionTable.PSVersion)"
-    
-    # if (-not (Get-PSRepository -Name "PSGallery" -ErrorAction SilentlyContinue)) {
-    #     Write-Output "Registering PSGallery repository."
-    #     Register-PSRepository -Default -Verbose -Debug
-    # }
-    # Get-PSRepository
-
-    # If the command is not available, install the SignPath module
-    Install-Module -Verbose -Debug -Name SignPath -Scope CurrentUser -Force
-}
-
 # Execute Submit-SigningRequest command from the SignPath module
-# TODO replace test_signing_policy with prod policy slug
 Submit-SigningRequest -ApiToken $ApiToken -InputArtifactPath $FileToSign -OutputArtifactPath $FileToSign -OrganizationId "12147e94-bba9-4fef-b29b-300398e90c5a" -ProjectSlug "NVDA"  -SigningPolicySlug "release_signing_policy" -WaitForCompletion -Force
