8d6a20eef3815e991dc315fd1a325689052efa78
Sean Budd <sean@nvaccess.org>
2024-06-27 14:07:03 +1000

    fix up runlint script

--- runlint.bat
+++ runlint.bat
@@ -6,6 +6,6 @@ set here=%hereOrig%
 if #%hereOrig:~-1%# == #\# set here=%hereOrig:~0,-1%
 set scriptsDir=%here%\venvUtils
 
-if "%1" NEQ "" set ruffArgs=%ruffArgs% --output-file=%1 --output-format=junit
+if "%1" NEQ "" set ruffArgs=--output-file=%1 --output-format=junit
 call "%scriptsDir%\venvCmd.bat" ruff check --fix %ruffArgs%
 if ERRORLEVEL 1 exit /b %ERRORLEVEL%
