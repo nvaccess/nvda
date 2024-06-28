23c5580e5d40656ff10a981665f7925e453b50ed
Sean Budd <sean@nvaccess.org>
2024-06-28 14:47:22 +1000

    Add license check pre-commit hook

--- runlicensecheck.bat
+++ runlicensecheck.bat
@@ -6,6 +6,7 @@ set here=%hereOrig%
 if #%hereOrig:~-1%# == #\# set here=%hereOrig:~0,-1%
 set scriptsDir=%here%\venvUtils
 
+set checkArgs=
 if "%1" NEQ "" set checkArgs=--file=%1 --format=markdown
 call "%scriptsDir%\venvCmd.bat" py -m licensecheck -0 --format ansi %checkArgs%
 if ERRORLEVEL 1 exit /b %ERRORLEVEL%
