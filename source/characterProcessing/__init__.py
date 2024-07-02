88b2e2541f748079f2269bd0f00db631439e4c2c
Leonard de Ruijter <alderuijter@gmail.com>
2024-07-02 21:40:16 +0200

    Add __init__

---
+++ source/characterProcessing/__init__.py
@@ -0,0 +1,5 @@
+# A part of NonVisual Desktop Access (NVDA)
+# Copyright (C) 2010-2024 NV Access Limited, World Light Information Limited,
+# Hong Kong Blind Union, Babbage B.V., Julien Cochuyt, Cyrille Bougot
+# This file is covered by the GNU General Public License.
+# See the file COPYING for more details.
