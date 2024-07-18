d6e7e447cf3cbc160c96a772c24966b13bc0307d
Sean Budd <seanbudd123@gmail.com>
2024-07-18 14:24:38 +1000

    Update source/NVDAObjects/UIA/winConsoleUIA.py

--- source/NVDAObjects/UIA/winConsoleUIA.py
+++ source/NVDAObjects/UIA/winConsoleUIA.py
@@ -450,7 +450,7 @@ class _DiffBasedWinTerminalUIA(EnhancedTermTypedCharSupport):
 		"Block notification events when diffing to prevent double reporting."
 		log.debugWarning(f"Notification event blocked to avoid double-report: {kwargs}")
 
-	def _get_caretMovementDetectionUsesEvents(self):
+	def _get_caretMovementDetectionUsesEvents(self) -> bool:
 		"Windows Terminal has a good implementation of caret move detection."
 		return True
 
