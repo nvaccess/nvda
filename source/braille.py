77383c60e9120488d15ea1e9b35792195b3213f2
Noelia Ruiz Martínez <nrm1977@gmail.com>
2024-07-28 11:30:05 +0200

    Handle moving to previous window when word wrap is active

--- source/braille.py
+++ source/braille.py
@@ -1770,7 +1770,8 @@ class BrailleBuffer(baseObject.AutoPropertyObject):
 			and self.regions[-1].rawText.startswith(PARAGRAPH_START + TEXT_SEPARATOR)
 		):
 			region, regionStart, regionEnd = list(self.regionsWithPositions)[-1]
-			startPos = self.regionPosToBufferPos(region, regionStart)
+			if startPos < regionStart:
+				startPos = self.regionPosToBufferPos(region, regionStart)
 		self.windowStartPos = startPos
 
 	def _nextWindow(self):
