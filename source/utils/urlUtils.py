13bf11db39aa32648479a16653691fc89c5931fd
Noelia Ruiz Martínez <nrm1977@gmail.com>
2024-08-15 10:50:08 +0200

    Use urlparse for function to check same page links, suggested by reviewr

--- source/utils/urlUtils.py
+++ source/utils/urlUtils.py
@@ -1,18 +1,23 @@
 # A part of NonVisual Desktop Access (NVDA)
-# Copyright (C) 2020-2024 NV Access Limited
+# Copyright (C) 2024 NV Access Limited
 # This file may be used under the terms of the GNU General Public License, version 2 or later.
 # For more details see: https://www.gnu.org/licenses/gpl-2.0.html
 
+from urllib.parse import urlparse
+
 
 def _valueToSamePage(value: str, constantIdentifier: str) -> bool:
-	"""Function used to check if link destination points to the same page"""
 	if not value or not constantIdentifier:
 		return False
-	if constantIdentifier.endswith("/"):
-		constantIdentifier = constantIdentifier[:-1]
-	queryParamCharPos = constantIdentifier.find("?")
-	if queryParamCharPos > 0:
-		constantIdentifier = constantIdentifier[:queryParamCharPos]
-	if value.startswith(f"{constantIdentifier}#"):
+	# Parse the URLs
+	valueParsed = urlparse(value)
+	constantIdentifierParsed = urlparse(constantIdentifier)
+	# Compare the netloc and path
+	if (
+		valueParsed.scheme == constantIdentifierParsed.scheme
+		and valueParsed.netloc == constantIdentifierParsed.netloc
+		and valueParsed.path == constantIdentifierParsed.path
+		and valueParsed.fragment
+	):
 		return True
 	return False
