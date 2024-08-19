293a553dcde025a4d3ec5f284523d907cf021032
Leonard de Ruijter <alderuijter@gmail.com>
2024-08-19 19:26:21 +0200

    Fixup

--- source/appModules/powerpnt.py
+++ source/appModules/powerpnt.py
@@ -1063,10 +1063,10 @@ class TextFrameTextInfo(textInfos.offsets.OffsetsTextInfo):
 		"""
 		Retrieves a text range from the PowerPoint object, with optional clamping of the start and end indices.
 		:param start: The starting character index of the text range (zero based).
-		:param end: The ending character index (zero based) of the text range.
+		:param end: The ending character index of the text range(zero based).
 		:param clamp: If True, the start and end indices will be clamped to valid values within the text range. Defaults to False.
-		:Returns: The text range object as a comtypes.client.lazybind.Dispatch object.
-		:Raises ValueError: If the start index is greater than the end index.
+		:returns: The text range object as a comtypes.client.lazybind.Dispatch object.
+		:raises ValueError: If the start index is greater than the end index.
 		:note: For more information about text ranges, see https://learn.microsoft.com/en-us/office/vba/api/powerpoint.textrange
 		"""
 		if not start <= end:
@@ -1094,7 +1094,7 @@ class TextFrameTextInfo(textInfos.offsets.OffsetsTextInfo):
 		Retrieves the text content of a PowerPoint text range, replacing any newline characters with standard newline characters.
 		:param start: The starting character index of the text range (zero based).
 		:param end: The ending character index (zero based) of the text range.
-		:Returns: The text content of the specified text range, with newline characters normalized.
+		:returns: The text content of the specified text range, with newline characters normalized.
 		"""
 		return self._getPptTextRange(start, end).text.replace("\x0b", "\n")
 
@@ -1112,7 +1112,8 @@ class TextFrameTextInfo(textInfos.offsets.OffsetsTextInfo):
 	@staticmethod
 	def _getOffsets(ranges: comtypes.client.lazybind.Dispatch, offset: int) -> tuple[int, int, int]:
 		"""
-		Retrieves the start and end offsets of a range of elements (e.g. words, lines, paragraphs) within a text range, given a specific offset.
+		Retrieves the start and end offsets of a range of elements
+		(e.g. words, lines, paragraphs) within a text range, given a specific offset.
 		:param ranges: The collection of elements (e.g. words, lines, paragraphs) to search.
 			These are retrieved by calling a method on the text range object, such as textRange.words(), textRange.lines(), or textRange.paragraphs().
 		:param offset: The zero based character offset to search for within the text range.
