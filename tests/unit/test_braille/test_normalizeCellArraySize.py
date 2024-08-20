4bfe6e2bcaaa5cdac7a2adc7a9400d78770d9c02
Michael Curran <mick@nvaccess.org>
2024-08-20 14:11:44 +1000

    Format lists in unit tests for braille.handler._normalizeCellArraySize  so that they are one row per line, and telling Ruff to not touch them.

--- tests/unit/test_braille/test_normalizeCellArraySize.py
+++ tests/unit/test_braille/test_normalizeCellArraySize.py
@@ -3,7 +3,7 @@
 # See the file COPYING for more details.
 # Copyright (C) 2024 NV Access Limited
 
-"""Unit tests for resizing a cell array before displaying on device. """
+"""Unit tests for resizing a cell array before displaying on device."""
 
 import braille
 import unittest
@@ -14,17 +14,16 @@ class Test_normalizeCellArraySize(unittest.TestCase):
 	Tests for BrailleHandler._normalizeCellArraySize.
 	"""
 
-
 	def test_shrinkSingleLine(self):
-		oldCells = [1, 2, 3, 4, 5, 6, 7, 8]
-		expectedNewCells = [1, 2, 3, 4]
+		oldCells = [1, 2, 3, 4, 5, 6, 7, 8]  # fmt: skip
+		expectedNewCells = [1, 2, 3, 4]  # fmt: skip
 		assert braille.handler is not None
 		newcells = braille.handler._normalizeCellArraySize(oldCells, 8, 1, 4, 1)
 		self.assertEqual(newcells, expectedNewCells)
 
 	def test_growSingleLine(self):
-		oldCells = [1, 2, 3, 4]
-		expectedNewCells = [1, 2, 3, 4, 0, 0, 0, 0]
+		oldCells = [1, 2, 3, 4]  # fmt: skip
+		expectedNewCells = [1, 2, 3, 4, 0, 0, 0, 0]  # fmt: skip
 		assert braille.handler is not None
 		newcells = braille.handler._normalizeCellArraySize(oldCells, 4, 1, 8, 1)
 		self.assertEqual(newcells, expectedNewCells)
@@ -34,11 +33,11 @@ class Test_normalizeCellArraySize(unittest.TestCase):
 			1, 2, 3, 4, 5,
 			11, 12, 13, 14, 15,
 			21, 22, 23, 24, 25,
-		]
+		]  # fmt: skip
 		expectedNewCells = [
 			1, 2, 3, 4, 5,
 			11, 12, 13, 14, 15,
-		]
+		]  # fmt: skip
 		assert braille.handler is not None
 		newcells = braille.handler._normalizeCellArraySize(oldCells, 15, 3, 10, 2)
 		self.assertEqual(newcells, expectedNewCells)
@@ -47,12 +46,12 @@ class Test_normalizeCellArraySize(unittest.TestCase):
 		oldCells = [
 			1, 2, 3, 4, 5,
 			11, 12, 13, 14, 15,
-		]
+		]  # fmt: skip
 		expectedNewCells = [
 			1, 2, 3, 4, 5,
 			11, 12, 13, 14, 15,
 			0, 0, 0, 0, 0,
-		]
+		]  # fmt: skip
 		assert braille.handler is not None
 		newcells = braille.handler._normalizeCellArraySize(oldCells, 10, 2, 15, 3)
 		self.assertEqual(newcells, expectedNewCells)
@@ -62,12 +61,12 @@ class Test_normalizeCellArraySize(unittest.TestCase):
 			1, 2, 3, 4,
 			11, 12, 13, 14,
 			21, 22, 23, 24,
-		]
+		]  # fmt: skip
 		expectedNewCells = [
 			1, 2,
 			11, 12,
 			21, 22,
-		]
+		]  # fmt: skip
 		assert braille.handler is not None
 		newcells = braille.handler._normalizeCellArraySize(oldCells, 12, 3, 6, 3)
 		self.assertEqual(newcells, expectedNewCells)
@@ -77,12 +76,12 @@ class Test_normalizeCellArraySize(unittest.TestCase):
 			1, 2,
 			11, 12,
 			21, 22,
-		]
+		]  # fmt: skip
 		expectedNewCells = [
 			1, 2, 0, 0,
 			11, 12, 0, 0,
 			21, 22, 0, 0,
-		]
+		]  # fmt: skip
 		assert braille.handler is not None
 		newcells = braille.handler._normalizeCellArraySize(oldCells, 6, 3, 12, 3)
 		self.assertEqual(newcells, expectedNewCells)
@@ -92,11 +91,11 @@ class Test_normalizeCellArraySize(unittest.TestCase):
 			1, 2, 3, 4, 5,
 			11, 12, 13, 14, 15,
 			21, 22, 23, 24, 25,
-		]
+		]  # fmt: skip
 		expectedNewCells = [
 			1, 2,
 			11, 12,
-		]
+		]  # fmt: skip
 		assert braille.handler is not None
 		newcells = braille.handler._normalizeCellArraySize(oldCells, 15, 3, 4, 2)
 		self.assertEqual(newcells, expectedNewCells)
@@ -105,12 +104,12 @@ class Test_normalizeCellArraySize(unittest.TestCase):
 		oldCells = [
 			1, 2,
 			11, 12,
-		]
+		]  # fmt: skip
 		expectedNewCells = [
 			1, 2, 0, 0,
 			11, 12, 0, 0,
 			0, 0, 0, 0,
-		]
+		]  # fmt: skip
 		assert braille.handler is not None
 		newcells = braille.handler._normalizeCellArraySize(oldCells, 4, 2, 12, 3)
 		self.assertEqual(newcells, expectedNewCells)
@@ -120,11 +119,11 @@ class Test_normalizeCellArraySize(unittest.TestCase):
 			1, 2,
 			11, 12,
 			21, 22,
-		]
+		]  # fmt: skip
 		expectedNewCells = [
 			1, 2, 0, 0,
 			11, 12, 0, 0,
-		]
+		]  # fmt: skip
 		assert braille.handler is not None
 		newcells = braille.handler._normalizeCellArraySize(oldCells, 6, 3, 8, 2)
 		self.assertEqual(newcells, expectedNewCells)
@@ -133,12 +132,12 @@ class Test_normalizeCellArraySize(unittest.TestCase):
 		oldCells = [
 			1, 2, 3, 4,
 			11, 12, 13, 14,
-		]
+		]  # fmt: skip
 		expectedNewCells = [
 			1, 2,
 			11, 12,
 			0, 0,
-		]
+		]  # fmt: skip
 		assert braille.handler is not None
 		newcells = braille.handler._normalizeCellArraySize(oldCells, 8, 2, 6, 3)
 		self.assertEqual(newcells, expectedNewCells)
