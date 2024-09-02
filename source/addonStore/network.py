0d2a6137a47c64133fbb81854d52230c35946716
Sascha Cowley <16543535+SaschaCowley@users.noreply.github.com>
2024-09-02 16:19:19 +1000

    Renamed `BASE_URL` to `_DEFAULT_BASE_URL`

--- source/addonStore/network.py
+++ source/addonStore/network.py
@@ -42,7 +42,7 @@ if TYPE_CHECKING:
 	from gui.addonStoreGui.viewModels.addonList import AddonListItemVM
 
 
-BASE_URL = "https://nvaccess.org/addonStore"
+_DEFAULT_BASE_URL = "https://nvaccess.org/addonStore"
 _LATEST_API_VER = "latest"
 """
 A string value used in the add-on store to fetch the latest version of all add-ons,
@@ -53,7 +53,7 @@ i.e include older incompatible versions.
 def _getBaseURL() -> str:
 	if url := conf["addonStore"]["baseURL"]:
 		return url
-	return BASE_URL
+	return _DEFAULT_BASE_URL
 
 
 def _getCurrentApiVersionForURL() -> str:
