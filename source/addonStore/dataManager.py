94fe401201be6f179b0d0b34c5ed0d9e80ecc5f2
Sascha Cowley <16543535+SaschaCowley@users.noreply.github.com>
2024-09-04 09:20:12 +1000

    Apply suggestions from code review
    Co-authored-by: Sean Budd <sean@nvaccess.org>

--- source/addonStore/dataManager.py
+++ source/addonStore/dataManager.py
@@ -211,7 +211,7 @@ class _DataManager:
 		"addonStore",
 		# Translators: A suggestion of what to do when fetching add-on data from the store fails and a metadata mirror is being used.
 		# {url} will be replaced with the mirror URL.
-		"Make sure you are connected to the internet, and the add-on store mirror URL is valid.\n"
+		"Make sure you are connected to the internet, and the Add-on Store mirror URL is valid.\n"
 		"Mirror URL: {url}",
 	)
 	_updateFailureDefaultSuggestion = pgettext(
