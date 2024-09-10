70e2f61536b7df67ea34ee6b39dbde4947648be1
Luke Davis <XLTechie+github@newanswertech.com>
2024-09-10 01:54:07 -0400

    Make ContinueCancelDialog private. Type hint ButtonHelper instances module-wide.

--- source/gui/nvdaControls.py
+++ source/gui/nvdaControls.py
@@ -289,7 +289,7 @@ class MessageDialog(DPIScaledDialog):
 		DIALOG_TYPE_WARNING: winsound.MB_ICONASTERISK,
 	}
 
-	def _addButtons(self, buttonHelper):
+	def _addButtons(self, buttonHelper: guiHelper.ButtonHelper) -> None:
 		"""Adds ok / cancel buttons. Can be overridden to provide alternative functionality."""
 		ok = buttonHelper.addButton(
 			self,
@@ -377,7 +377,7 @@ class MessageDialog(DPIScaledDialog):
 		evt.Skip()
 
 
-class ContinueCancelDialog(MessageDialog):
+class _ContinueCancelDialog(MessageDialog):
 	"""
 	This implementation of a `gui.nvdaControls.MessageDialog`, provides `Continue` and `Cancel` buttons as its controls.
 	These serve the same functions as `OK` and `Cancel` in other dialogs, but may be more desirable in some situations.
@@ -403,17 +403,17 @@ class ContinueCancelDialog(MessageDialog):
 		self.continueButtonFirst: bool = continueButtonFirst
 		super().__init__(parent, title, message, dialogType)
 
-	def _addButtons(self, buttonHelper) -> None:
+	def _addButtons(self, buttonHelper: guiHelper.ButtonHelper) -> None:
 		"""Override to add Continue and Cancel buttons."""
 
 		# Note: the order of the Continue and Cancel buttons is important, because running SetDefault()
 		# on the Cancel button while the Continue button is first, has no effect. Therefore the only way to
 		# allow a caller to make Cancel the default, is to put it first.
-		def _makeContinue(self, buttonHelper) -> wx.Button:
+		def _makeContinue(self, buttonHelper: guiHelper.ButtonHelper) -> wx.Button:
 			# Translators: The label for the Continue button in an NVDA dialog.
 			return buttonHelper.addButton(self, id=wx.ID_OK, label=_("&Continue"))
 
-		def _makeCancel(self, buttonHelper) -> wx.Button:
+		def _makeCancel(self, buttonHelper: guiHelper.ButtonHelper) -> wx.Button:
 			# Translators: The label for the Cancel button in an NVDA dialog.
 			return buttonHelper.addButton(self, id=wx.ID_CANCEL, label=_("Cancel"))
 
