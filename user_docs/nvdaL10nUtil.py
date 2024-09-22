e163f4d116cb8ca91becccfdfbe23baa1f45e0ae
Michael Curran <mick@nvaccess.org>
2024-09-22 15:52:23 +1000

    nvdaL10nUtil: add stripXliff command which removes everything from an xliff except for valid target translations, ready for upload to Crowdin. It also optionally ignores all existing target translations froma n existing xliff file so that only a delta can be uploaded.

--- user_docs/nvdaL10nUtil.py
+++ user_docs/nvdaL10nUtil.py
@@ -4,10 +4,83 @@
 # See the file COPYING for more details.
 
 
+import lxml.etree
 import os
 import argparse
 import markdownTranslate
 import md2html
+
+
+def stripXliff(xliffPath: str, outputPath: str, oldXliffPath: str | None= None):
+	namespace = {"xliff": "urn:oasis:names:tc:xliff:document:2.0"}
+	xliff = lxml.etree.parse(xliffPath)
+	xliffRoot = xliff.getroot()
+	if xliffRoot.tag != "{urn:oasis:names:tc:xliff:document:2.0}xliff":
+		raise ValueError(f"Not an xliff file: {xliffPath}")
+	skeletonNode = xliffRoot.find(f'./xliff:file/xliff:skeleton', namespaces=namespace)
+	if skeletonNode is not None:
+		skeletonNode.getparent().remove(skeletonNode)
+	if oldXliffPath:
+		oldXliff = lxml.etree.parse(oldXliffPath)
+		oldXliffRoot = oldXliff.getroot()
+		if oldXliffRoot.tag != "{urn:oasis:names:tc:xliff:document:2.0}xliff":
+			raise ValueError(f"Not an xliff file: {oldXliffPath}")
+	else:
+		oldXliffRoot = None
+	file = xliffRoot.find(f'./xliff:file', namespaces=namespace)
+	units = file.findall(f'./xliff:unit', namespaces=namespace)
+	segmentCount = 0
+	emptyTargetCount = 0
+	corruptTargetcount = 0
+	sourceTargetcount = 0
+	existingTranslationCount = 0
+	for unit in units:
+		notes = unit.find("./xliff:notes", namespaces=namespace)
+		if notes is not None:
+			unit.remove(notes)
+		segment = unit.find("./xliff:segment", namespaces=namespace)
+		if segment is None:
+			print("Warning: No segment element in unit")
+			continue
+		state = segment.get("state")
+		source = segment.find("./xliff:source", namespaces=namespace)
+		if source is None:
+			print("Warning: No source element in segment")
+			continue
+		sourceText = source.text
+		segmentCount += 1
+		target = segment.find("./xliff:target", namespaces=namespace)
+		if target is None:
+			continue
+		targetText = target.text
+		# remove empty / self-closing target tags
+		if not targetText:
+			emptyTargetCount += 1
+			file.remove(unit)
+		# remove corrupt target tags
+		elif targetText in (
+			"<target/>",
+			"&lt;target/&gt;",
+			"<target></target>",
+			"&lt;target&gt;&lt;/target&gt;"
+		):
+			corruptTargetcount += 1
+			file.remove(unit)
+		# remove target tags pre-filled with source text
+		elif (not state or state == "initial") and targetText == sourceText:
+			sourceTargetcount += 1
+			file.remove(unit)
+		# remove translations that already exist in the old xliff file
+		elif oldXliffRoot is not None:
+			unitId = unit.get("id")
+			oldTarget = oldXliffRoot.find(f'./xliff:file/xliff:unit[@id="{unitId}"]/xliff:segment/xliff:target', namespaces=namespace)
+			if oldTarget is not None and oldTarget.text == targetText:
+				existingTranslationCount += 1
+				file.remove(unit)
+	xliff.write(outputPath)
+	keptTranslations = segmentCount - emptyTargetCount - corruptTargetcount - sourceTargetcount - existingTranslationCount
+	print(f"Processed {segmentCount} segments, removing {emptyTargetCount} empty targets, {corruptTargetcount} corrupt targets, {sourceTargetcount} source targets, and {existingTranslationCount} existing translations, resulting in {keptTranslations} translations kept")
+
 
 if __name__ == "__main__":
 	args = argparse.ArgumentParser()
@@ -21,11 +94,17 @@ if __name__ == "__main__":
 	command_md2html.add_argument("-t", "--docType", help="Type of document", action="store", choices=["userGuide", "developerGuide", "changes", "keyCommands"])
 	command_md2html.add_argument("mdPath", help="Path to the markdown file")
 	command_md2html.add_argument("htmlPath", help="Path to the resulting html file")
+	command_stripXliff = commands.add_parser("stripXliff", help="Remove prefilled, empty or corrupt target tags from an xliff file before upload to Crowdin. Optionally also remove translations that already exist in an old xliff file")
+	command_stripXliff.add_argument('-o', '--oldXliffPath', help="Path to the old xliff file containing existing translations that should be stripped", action="store", default=None)
+	command_stripXliff.add_argument("xliffPath", help="Path to the xliff file")
+	command_stripXliff.add_argument("outputPath", help="Path to the resulting xliff file")
 	args = args.parse_args()
 	match args.command:
 		case "xliff2md":
 			markdownTranslate.generateMarkdown(xliffPath=args.xliffPath, outputPath=args.mdPath, translated=not args.untranslated)
 		case "md2html":
 			md2html.main(source=args.mdPath, dest=args.htmlPath, lang=args.lang, docType=args.docType)
+		case "stripXliff":
+			stripXliff(args.xliffPath, args.outputPath, args.oldXliffPath)
 		case _:
 			raise ValueError(f"Unknown command {args.command}")
