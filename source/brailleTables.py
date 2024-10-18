44e911b34995c28eb0a3c0738ae4ec041cd6fc3c
Noelia Ruiz Martínez <nrm1977@gmail.com>
2024-10-18 06:01:13 +0200

    Apply review suggestions

--- source/brailleTables.py
+++ source/brailleTables.py
@@ -6,7 +6,7 @@
 """Manages information about available braille translation tables."""
 
 import collections
-from enum import StrEnum
+from enum import Enum, StrEnum, auto
 import os
 from locale import strxfrm
 from typing import NamedTuple
@@ -21,6 +21,9 @@ import languageHandler
 
 TABLES_DIR = os.path.join(globalVars.appDir, "louis", "tables")
 """The directory in which liblouis braille tables are located."""
+
+DEFAULT_TABLE = "en-ueb-g1.ctb"
+"""The default braille table."""
 
 
 class TableSource(StrEnum):
@@ -133,10 +136,12 @@ def addTable(
 	if inputForLangs is not None:
 		for lang in inputForLangs:
 			if lang in _inputTableForLangs:
-				log.warning(f"input table lang {lang} already set to {_inputTableForLangs[lang]} overwriting to {table.fileName}"
+				log.warning(f"input table lang {lang} already set to {_inputTableForLangs[lang]} overwriting to {table.fileName}")
 			_inputTableForLangs[lang] = table.fileName
 	if outputForLangs is not None:
 		for lang in outputForLangs:
+			if lang in _outputTableForLangs:
+				log.warning(f"output table lang {lang} already set to {_outputTableForLangs[lang]} overwriting to {table.fileName}")
 			_outputTableForLangs[lang] = table.fileName
 
 
