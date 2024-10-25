bc5da55629e20241558a680d705e2bfcd4640d26
Noelia Ruiz Martínez <nrm1977@gmail.com>
2024-10-25 07:07:26 +0200

    Fix translators comment position

--- source/appModules/winword.py
+++ source/appModules/winword.py
@@ -27,8 +27,8 @@ class AppModule(appModuleHandler.AppModule):
 
 	@script(
 		category=globalCommands.SCRCAT_FOCUS,
-		# Translators: Message presented in input help mode to describe a command in Microsoft Word documents.
 		description=_(
+			# Translators: Input help mode message for report title bar command.
 			"In Microsoft Word, reports the title and the layout of the current document. If pressed twice, spells this information. If pressed three times, copies it to the clipboard"
 		),
 		gesture="kb:NVDA+t",
