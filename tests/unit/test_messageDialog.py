1d02fffcec5b01329f10f157eda7e8c705a3a9f8
Sascha Cowley <16543535+SaschaCowley@users.noreply.github.com>
2024-10-30 17:59:40 +1100

    Refactored to use a common base class

--- tests/unit/test_messageDialog.py
+++ tests/unit/test_messageDialog.py
@@ -12,13 +12,17 @@ import wx
 from gui.messageDialog import MessageDialog, MessageDialogType
 
 
-@patch.object(wx.ArtProvider, "GetIconBundle")
-class Test_MessageDialog_Icons(unittest.TestCase):
-	"""Tests for the message dialog API."""
+class MDTestBase(unittest.TestCase):
+	"""Base class for test cases testing MessageDialog. Handles wx initialisation."""
 
 	def setUp(self) -> None:
 		self.app = wx.App()
 		self.dialog = MessageDialog(None, "Test dialog")
+
+
+@patch.object(wx.ArtProvider, "GetIconBundle")
+class Test_MessageDialog_Icons(MDTestBase):
+	"""Test that message dialog icons are set correctly."""
 
 	def test_setIcon_with_type_with_icon(self, mocked_GetIconBundle: MagicMock):
 		mocked_GetIconBundle.return_value = wx.IconBundle()
@@ -33,10 +37,8 @@ class Test_MessageDialog_Icons(unittest.TestCase):
 
 
 @patch("winsound.MessageBeep")
-class Test_MessageDialog_Sounds(unittest.TestCase):
-	def setUp(self):
-		self.app = wx.App()
-		self.dialog = MessageDialog(None, "Test dialog")
+class Test_MessageDialog_Sounds(MDTestBase):
+	"""Test that message dialog sounds are set and played correctly."""
 
 	def test_playSound_with_type_with_Sound(self, mocked_MessageBeep: MagicMock):
 		type = MessageDialogType.ERROR
