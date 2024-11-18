34d83c49bcee8fce45b369637f0449388eb229b1
Sean Budd <sean@nvaccess.org>
2024-11-18 20:07:09 +1100

    Create unit tests for magnification API

---
+++ tests/unit/test_visionEnhancementProviders/__init__.py
@@ -0,0 +1,6 @@
+# A part of NonVisual Desktop Access (NVDA)
+# This file is covered by the GNU General Public License.
+# See the file COPYING for more details.
+# Copyright (C) 2024 NV Access Limited.
+
+"""Unit tests for the vision enhancement providers submodule."""
---
+++ tests/unit/test_visionEnhancementProviders/test_magnificationAPI.py
@@ -0,0 +1,51 @@
+# A part of NonVisual Desktop Access (NVDA)
+# This file is covered by the GNU General Public License.
+# See the file COPYING for more details.
+# Copyright (C) 2024 NV Access Limited.
+
+"""Unit tests for the magnification Windows API."""
+
+import unittest
+
+from visionEnhancementProviders.screenCurtain import Magnification, MAGCOLOREFFECT, TRANSFORM_BLACK
+
+class Test_ScreenCurtain(unittest.TestCase):
+
+	def setUp(self):
+		self.assertTrue(Magnification.MagInitialize())
+
+	def tearDown(self):
+		self.assertTrue(Magnification.MagUninitialize())
+
+	def test_setAndConfirmBlackFullscreenColorEffect(self):
+		result = Magnification.MagSetFullscreenColorEffect(TRANSFORM_BLACK)
+		self.assertTrue(result)
+		resultEffect = Magnification.MagGetFullscreenColorEffect()
+		for i in range(5):
+			for j in range(5):
+				self.assertEqual(TRANSFORM_BLACK.transform[i][j], resultEffect.transform[i][j])
+
+	def test_getDefaultIdentityFullscreenColorEffect(self):
+		resultEffect = Magnification.MagGetFullscreenColorEffect()
+		for i in range(5):
+			for j in range(5):
+				# The transform matrix should be the identity matrix
+				self.assertEqual(int(i == j), resultEffect.transform[i][j], f"i={i}, j={j}")
+
+
+class Test_Mouse(unittest.TestCase):
+
+	def setUp(self):
+		self.assertTrue(Magnification.MagInitialize())
+
+	def tearDown(self):
+		self.assertTrue(Magnification.MagUninitialize())
+
+	def test_MagShowSystemCursor(self):
+		result = Magnification.MagShowSystemCursor(True)
+		self.assertTrue(result)
+
+	def test_MagHideSystemCursor(self):
+		result = Magnification.MagShowSystemCursor(False)
+		self.assertTrue(result)
+
