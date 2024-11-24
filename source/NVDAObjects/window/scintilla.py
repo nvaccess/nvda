b2ff5d4bc1d01344996f192b8f1b3221139e971e
Noelia Ruiz Martínez <nrm1977@gmail.com>
2024-11-24 13:21:35 +0100

    Add docstring to collapse method for Scintilla TextInfo

--- source/NVDAObjects/window/scintilla.py
+++ source/NVDAObjects/window/scintilla.py
@@ -314,6 +314,11 @@ class ScintillaTextInfo(textInfos.offsets.OffsetsTextInfo):
 		return [start, end]
 
 	def collapse(self, end=False):
+		"""Before collapsing to end, TextInfo is expanded to line.
+		This fixes a bug where next braille line command didn't move the cursor to the last empty line
+		in Notepad++ documents.
+		https://github.com/nvaccess/nvda/issues/17430
+		"""
 		if end:
 			self.expand(textInfos.UNIT_LINE)
 		super().collapse(end=end)
