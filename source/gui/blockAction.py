90f0a269378fd756a65a8f254966a280a50219c3
Sascha Cowley <16543535+SaschaCowley@users.noreply.github.com>
2024-12-20 11:46:59 +1100

    Revert "Create a `MessageDialog` API (#17304)"
    This reverts commit 2477780dd3e9b1f44b73fee2a180a4f20553bdf4.

--- source/gui/blockAction.py
+++ source/gui/blockAction.py
@@ -1,48 +1,25 @@
 # A part of NonVisual Desktop Access (NVDA)
-# Copyright (C) 2023-2024 NV Access Limited, Cyrille Bougot
+# Copyright (C) 2023 NV Access Limited, Cyrille Bougot
 # This file is covered by the GNU General Public License.
 # See the file COPYING for more details.
 
-from collections.abc import Callable
 import config
 from config.configFlags import BrailleMode
 from dataclasses import dataclass
 from enum import Enum
 from functools import wraps
 import globalVars
-from typing import Any
-from speech.priorities import SpeechPriority
+from typing import Callable
 import ui
 from utils.security import isLockScreenModeActive, isRunningOnSecureDesktop
-import core
-
-_DELAY_BEFORE_MESSAGE_MS = 1
-"""Duration in milliseconds for which to delay announcing that an action has been blocked, so that any UI changes don't interrupt it.
-1ms is a magic number. It can be increased if it is found to be too short, but it should be kept to a minimum.
-"""
-
-
-def _isModalMessageBoxActive() -> bool:
-	"""Avoid circular import of isModalMessageBoxActive"""
-	from gui.message import isModalMessageBoxActive
-
-	return isModalMessageBoxActive()
-
-
-def _modalDialogOpenCallback():
-	"""Focus any open blocking :class:`MessageDialog` instances."""
-	# Import late to avoid circular import
-	from gui.message import MessageDialog
-
-	if MessageDialog.blockingInstancesExist():
-		MessageDialog.FocusBlockingInstances()
+from gui.message import isModalMessageBoxActive
+import queueHandler
 
 
 @dataclass
 class _Context:
 	blockActionIf: Callable[[], bool]
 	translatedMessage: str
-	callback: Callable[[], Any] | None = None
 
 
 class Context(_Context, Enum):
@@ -58,11 +35,10 @@ class Context(_Context, Enum):
 		_("Action unavailable in NVDA Windows Store version"),
 	)
 	MODAL_DIALOG_OPEN = (
-		_isModalMessageBoxActive,
+		isModalMessageBoxActive,
 		# Translators: Reported when an action cannot be performed because NVDA is waiting
 		# for a response from a modal dialog
 		_("Action unavailable while a dialog requires a response"),
-		_modalDialogOpenCallback,
 	)
 	WINDOWS_LOCKED = (
 		lambda: isLockScreenModeActive() or isRunningOnSecureDesktop(),
@@ -98,15 +74,7 @@ def when(*contexts: Context):
 		def funcWrapper(*args, **kwargs):
 			for context in contexts:
 				if context.blockActionIf():
-					if context.callback is not None:
-						context.callback()
-					# We need to delay this message so that, if a UI change is triggered by the callback, the UI change doesn't interrupt it.
-					core.callLater(
-						_DELAY_BEFORE_MESSAGE_MS,
-						ui.message,
-						context.translatedMessage,
-						SpeechPriority.NOW,
-					)
+					queueHandler.queueFunction(queueHandler.eventQueue, ui.message, context.translatedMessage)
 					return
 			return func(*args, **kwargs)
 
