40ceeff7e743508ff2e7ea1a41341ebebaffc51b
Christopher Toth <q.alpha@gmail.com>
2024-12-28 22:57:53 -0700

    Naming and typing fixes

--- source/remoteClient/beepSequence.py
+++ source/remoteClient/beepSequence.py
@@ -8,9 +8,10 @@ import tones
 local_beep = tones.beep
 
 BeepElement = Union[int, Tuple[int, int]]  # Either delay_ms or (frequency_hz, duration_ms)
+BeepSequence = collections.abc.Iterable[BeepElement]
 
 
-def beep_sequence(*sequence: BeepElement) -> None:
+def beepSequence(*sequence: BeepElement) -> None:
 	"""Play a simple synchronous monophonic beep sequence
 	A beep sequence is an iterable containing one of two kinds of elements.
 	An element consisting of a tuple of two items is interpreted as a frequency and duration. Note, this function plays beeps synchronously, unlike tones.beep
@@ -25,10 +26,10 @@ def beep_sequence(*sequence: BeepElement) -> None:
 			local_beep(tone, duration)
 
 
-def beep_sequence_async(*sequence: BeepElement) -> threading.Thread:
+def beepSequenceAsync(*sequence: BeepElement) -> threading.Thread:
 	"""Play an asynchronous beep sequence.
-	This is the same as beep_sequence, except it runs in a thread."""
-	thread = threading.Thread(target=beep_sequence, args=sequence)
+	This is the same as `beepSequence`, except it runs in a thread."""
+	thread = threading.Thread(target=beepSequence, args=sequence)
 	thread.daemon = True
 	thread.start()
 	return thread
