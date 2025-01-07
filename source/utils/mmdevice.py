a052a0ecabedffe2fdee785909ef08f9b7aa4862
Sascha Cowley <16543535+SaschaCowley@users.noreply.github.com>
2025-01-07 14:27:48 +1100

    Update source/utils/mmdevice.py
    Co-authored-by: Sean Budd <sean@nvaccess.org>

--- source/utils/mmdevice.py
+++ source/utils/mmdevice.py
@@ -2,6 +2,7 @@
 # Copyright (C) 2025 NV Access Limited
 # This file is covered by the GNU General Public License.
 # See the file COPYING for more details.
+
 from collections.abc import Generator
 from typing import NamedTuple, cast
 
