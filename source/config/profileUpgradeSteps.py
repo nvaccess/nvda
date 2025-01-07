nvaccess/nvda

commit 0f7e961a06c5bfb11f7528e81020b1f1047a2fe3

Use endpoint IDs instead of device friendly names to store user's preferred output device (#17547)
Closes #17497

Summary of the issue:
NVDA currently stores the friendly name of the user's preferred audio output device.

Description of user facing changes
None.

Description of development approach
In `nvdaHelper/local/wasapi.cpp`, rewrote `getPreferredDevice` to fetch the preferred device directly via `MMDeviceEnumerator.GetDevice`.
Added manual checks that the fetched device is a render device, and that its status is active, since these conditions were guaranteed to be met since the previous code only iterated over devices which met those prerequisites.
Renamed `deviceName` to `endpointId`.

In `source/mmwave.py`, added a parameter to `_getOutputDevices` to return a value representing the system default output device.
Also made the type hints more self-documenting by using a `NamedTuple`.

In `source/gui/settingsDialogs.py`, used `nvwave._getOutputDevices` rather than `nvwave.getOutputDeviceNames` to fetch the available output devices.
When saving, used the selection index of `AudioSettingsPanel.deviceList` to index into the tuple of IDs to get the value to save to config.

In `source/config/configSpec.py`, moved the `outputDevice` key from `speech` to `audio`, and incremented the schema version to 14. Added an associated profile upgrade function in `profileUpgradeSteps.py`, and tests for same in `tests/unit/test_config.py`. Updated all occurrences of this config key that I am aware of to point to the new location.

In `source/synthDrivers/sapi5.py`, rewrote the device selection logic again to work with endpoint IDs.

Testing strategy:
Built from source and ensured that changing output devices works as expected.
Ensured that saving the config worked, and that the output device was selected correctly when restarting NVDA.

Tested activating SAPI5 with different output devices selected.

Known issues with pull request:

source/config/profileUpgradeSteps.py
@@ -414,3 +414,60 @@ def upgradeConfigFrom_12_to_13(profile: ConfigObj) -> None:
 	log.debug(
 		f"Handled cldr value of {setting!r}. List is now: {profile['speech']['symbolDictionaries']}",
 	)
+
+
+def upgradeConfigFrom_13_to_14(profile: ConfigObj):
+	"""Set [audio][outputDevice] to the endpointID of [speech][outputDevice], and delete the latter."""
+	try:
+		friendlyName = profile["speech"]["outputDevice"]
+	except KeyError:
+		log.debug("Output device not present in config. Taking no action.")
+		return
+	if friendlyName == "default":
+		log.debug("Output device is set to default. Not writing a new value to config.")
+	elif endpointId := _friendlyNameToEndpointId(friendlyName):
+		log.debug(
+			f"Best match for device with {friendlyName=} has {endpointId=}. Writing new value to config.",
+		)
+		if "audio" not in profile:
+			profile["audio"] = {}
+		profile["audio"]["outputDevice"] = endpointId
+	else:
+		log.debug(
+			f"Could not find an audio output device with {friendlyName=}. Not writing a new value to config.",
+		)
+	log.debug("Deleting old config value.")
+	del profile["speech"]["outputDevice"]
+
+
+def _friendlyNameToEndpointId(friendlyName: str) -> str | None:
+	"""Convert a device friendly name to an endpoint ID string.
+
+	Since friendly names are not unique, there may be many devices on one system with the same friendly name.
+	As the order of devices in an IMMEndpointEnumerator is arbitrary, we cannot assume that the first device with a matching friendly name is the device the user wants.
+	We also can't guarantee that the device the user has selected is active, so we need to retrieve devices by state, in order from most to least preferable.
+	It is probably a safe bet that the device the user wants to use is either active or unplugged.
+	Thus, the preference order for states is:
+	1. ACTIVE- The audio adapter that connects to the endpoint device is present and enabled.
+	   In addition, if the endpoint device plugs into a jack on the adapter, then the endpoint device is plugged in.
+	2. UNPLUGGED - The audio adapter that contains the jack for the endpoint device is present and enabled, but the endpoint device is not plugged into the jack.
+	3. DISABLED - The user has disabled the device in the Windows multimedia control panel.
+	4. NOTPRESENT - The audio adapter that connects to the endpoint device has been removed from the system, or the user has disabled the adapter device in Device Manager.
+	Within a state, if there is more than one device with the selected friendly name, we use the first one.
+
+	:param friendlyName: Friendly name of the device to search for.
+	:return: Endpoint ID string of the best match device, or `None` if no device with a matching friendly name is available.
+	"""
+	from nvwave import _getOutputDevices
+	from pycaw.constants import DEVICE_STATE
+
+	states = (DEVICE_STATE.ACTIVE, DEVICE_STATE.UNPLUGGED, DEVICE_STATE.DISABLED, DEVICE_STATE.NOTPRESENT)
+	for state in states:
+		try:
+			return next(
+				device for device in _getOutputDevices(stateMask=state) if device.friendlyName == friendlyName
+			).id
+		except StopIteration:
+			# Proceed to the next device state.
+			continue
+	return None

source/gui/settingsDialogs.py
@@ -3041,17 +3041,15 @@ class AudioPanel(SettingsPanel):
 		# Translators: This is the label for the select output device combo in NVDA audio settings.
 		# Examples of an output device are default soundcard, usb headphones, etc.
 		deviceListLabelText = _("Audio output &device:")
-		# The Windows Core Audio device enumeration does not have the concept of an ID for the default output device, so we have to insert something ourselves instead.
-		# Translators: Value to show when choosing to use the default audio output device.
-		deviceNames = (_("Default output device"), *nvwave.getOutputDeviceNames())
+		self._deviceIds, deviceNames = zip(*nvwave._getOutputDevices(includeDefault=True))
 		self.deviceList = sHelper.addLabeledControl(deviceListLabelText, wx.Choice, choices=deviceNames)
 		self.bindHelpEvent("SelectSynthesizerOutputDevice", self.deviceList)
-		selectedOutputDevice = config.conf["speech"]["outputDevice"]
-		if selectedOutputDevice == "default":
+		selectedOutputDevice = config.conf["audio"]["outputDevice"]
+		if selectedOutputDevice == config.conf.getConfigValidation(("audio", "outputDevice")).default:
 			selection = 0
 		else:
 			try:
-				selection = deviceNames.index(selectedOutputDevice)
+				selection = self._deviceIds.index(selectedOutputDevice)
 			except ValueError:
 				selection = 0
 		self.deviceList.SetSelection(selection)
@@ -3176,13 +3174,10 @@ class AudioPanel(SettingsPanel):
 		self.soundSplitModesList.Select(0)
 
 	def onSave(self):
-		# We already use "default" as the key in the config spec, so use it here as an alternative to Microsoft Sound Mapper.
-		selectedOutputDevice = (
-			"default" if self.deviceList.GetSelection() == 0 else self.deviceList.GetStringSelection()
-		)
-		if config.conf["speech"]["outputDevice"] != selectedOutputDevice:
+		selectedOutputDevice = self._deviceIds[self.deviceList.GetSelection()]
+		if config.conf["audio"]["outputDevice"] != selectedOutputDevice:
 			# Synthesizer must be reload if output device changes
-			config.conf["speech"]["outputDevice"] = selectedOutputDevice
+			config.conf["audio"]["outputDevice"] = selectedOutputDevice
 			currentSynth = getSynth()
 			if not setSynth(currentSynth.name):
 				_synthWarningDialog(currentSynth.name)

source/nvwave.py
@@ -1,10 +1,10 @@
 # A part of NonVisual Desktop Access (NVDA)
-# Copyright (C) 2007-2023 NV Access Limited, Aleksey Sadovoy, Cyrille Bougot, Peter Vágner, Babbage B.V.,
+# Copyright (C) 2007-2024 NV Access Limited, Aleksey Sadovoy, Cyrille Bougot, Peter Vágner, Babbage B.V.,
 # Leonard de Ruijter, James Teh
 # This file is covered by the GNU General Public License.
 # See the file COPYING for more details.
 
-"""Provides a simple Python interface to playing audio using the Windows multimedia waveOut functions, as well as other useful utilities."""
+"""Provides a simple Python interface to playing audio using the Windows Audio Session API (WASAPI), as well as other useful utilities."""
 
 from collections.abc import Generator
 import threading
@@ -89,19 +89,40 @@ class AudioPurpose(Enum):
 	SOUNDS = auto()
 
 
-def _getOutputDevices() -> Generator[tuple[str, str]]:
-	"""Generator, yielding device ID and device Name in device ID order.
-	..note: Depending on number of devices being fetched, this may take some time (~3ms)
+class _AudioOutputDevice(typing.NamedTuple):
+	id: str
+	friendlyName: str
+
+
+def _getOutputDevices(
+	*,
+	includeDefault: bool = False,
+	stateMask: DEVICE_STATE = DEVICE_STATE.ACTIVE,
+) -> Generator[_AudioOutputDevice]:
+	"""Generator, yielding device ID and device Name.
+	.. note:: Depending on number of devices being fetched, this may take some time (~3ms)
+
+	:param includeDefault: Whether to include a value representing the system default output device in the generator, defaults to False.
+		.. note:: The ID of this device is **not** a valid mmdevice endpoint ID string, and is for internal use only.
+			The friendly name is **not** generated by the operating system, and it is highly unlikely that it will match any real output device.
+	:param state: What device states to include in the resultant generator, defaults to DEVICE_STATE.ACTIVE.
+	:return: Generator of :class:`_AudioOutputDevices` containing all enabled and present audio output devices on the system.
 	"""
+	if includeDefault:
+		yield _AudioOutputDevice(
+			id=typing.cast(str, config.conf.getConfigValidation(("audio", "outputDevice")).default),
+			# Translators: Value to show when choosing to use the default audio output device.
+			friendlyName=_("Default output device"),
+		)
 	endpointCollection = AudioUtilities.GetDeviceEnumerator().EnumAudioEndpoints(
 		EDataFlow.eRender.value,
-		DEVICE_STATE.ACTIVE.value,
+		stateMask.value,
 	)
 	for i in range(endpointCollection.GetCount()):
 		device = AudioUtilities.CreateDevice(endpointCollection.Item(i))
 		# This should never be None, but just to be sure
 		if device is not None:
-			yield device.id, device.FriendlyName
+			yield _AudioOutputDevice(device.id, device.FriendlyName)
 		else:
 			continue
 
@@ -194,7 +215,7 @@ def playWaveFile(
 		channels=f.getnchannels(),
 		samplesPerSec=f.getframerate(),
 		bitsPerSample=f.getsampwidth() * 8,
-		outputDevice=config.conf["speech"]["outputDevice"],
+		outputDevice=config.conf["audio"]["outputDevice"],
 		wantDucking=False,
 		purpose=AudioPurpose.SOUNDS,
 	)
@@ -247,7 +268,7 @@ class WasapiWavePlayer(garbageHandler.TrackedObject):
 	#: Whether there is a pending stream idle check.
 	_isIdleCheckPending: bool = False
 	#: Use the default device, this is the configSpec default value.
-	DEFAULT_DEVICE_KEY = typing.cast(str, config.conf.getConfigValidation(("speech", "outputDevice")).default)
+	DEFAULT_DEVICE_KEY = typing.cast(str, config.conf.getConfigValidation(("audio", "outputDevice")).default)
 	#: The silence output device, None if not initialized.
 	_silenceDevice: typing.Optional[str] = None
 

tests/unit/test_config.py
@@ -2,14 +2,17 @@
 # This file is covered by the GNU General Public License.
 # See the file COPYING for more details.
 # Copyright (C) 2022-2024 NV Access Limited, Cyrille Bougot, Leonard de Ruijter
+
+from collections.abc import Callable, Generator
 import enum
 import typing
 import unittest
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 import io
 
 import configobj
 import configobj.validate
+from pycaw.constants import DEVICE_STATE
 
 from config import (
 	AggregatedSection,
@@ -24,10 +27,12 @@ from config.featureFlagEnums import (
 	BoolFlag,
 )
 from config.profileUpgradeSteps import (
+	_friendlyNameToEndpointId,
 	_upgradeConfigFrom_8_to_9_lineIndent,
 	_upgradeConfigFrom_8_to_9_cellBorders,
 	_upgradeConfigFrom_8_to_9_showMessages,
 	_upgradeConfigFrom_8_to_9_tetherTo,
+	upgradeConfigFrom_13_to_14,
 	upgradeConfigFrom_9_to_10,
 	upgradeConfigFrom_11_to_12,
 )
@@ -42,6 +47,7 @@ from config.configFlags import (
 from utils.displayString import (
 	DisplayStringEnum,
 )
+from nvwave import _AudioOutputDevice
 
 
 class Config_FeatureFlagEnums_getAvailableEnums(unittest.TestCase):
@@ -910,3 +916,130 @@ class Config_AggregatedSection_pollution(unittest.TestCase):
 		self.testSection["someBool"] = False
 		# Since we set someBool to a different value, update the profile.
 		self.assertEqual(self.profile, {"someBool": False})
+
+
+_DevicesT: typing.TypeAlias = dict[DEVICE_STATE, list[_AudioOutputDevice]]
+
+
+def getOutputDevicesFactory(
+	devices: _DevicesT,
+) -> Callable[[DEVICE_STATE], Generator[_AudioOutputDevice]]:
+	"""Create a callable that can be used to patch nvwave._getOutputDevices."""
+
+	def getOutputDevices(stateMask: DEVICE_STATE, **kw) -> Generator[_AudioOutputDevice]:
+		yield from devices.get(stateMask, [])
+
+	return getOutputDevices
+
+
+class Config_ProfileUpgradeSteps_FriendlyNameToEndpointId(unittest.TestCase):
+	DEFAULT_DEVICES: _DevicesT = {
+		DEVICE_STATE.ACTIVE: [_AudioOutputDevice("id1", "Device 1")],
+		DEVICE_STATE.UNPLUGGED: [_AudioOutputDevice("id2", "Device 2")],
+		DEVICE_STATE.DISABLED: [_AudioOutputDevice("id3", "Device 3")],
+		DEVICE_STATE.NOTPRESENT: [_AudioOutputDevice("id4", "Device 4")],
+	}
+
+	def test_noDuplicates(self):
+		"""Test that mapping from a friendly name to an endpoint ID works as expected when there are no duplicate friendly names."""
+		devices = self.DEFAULT_DEVICES
+		for devicesState, devicesInState in devices.items():
+			for device in devicesInState:
+				with self.subTest(id=device.id, FriendlyName=device.friendlyName, state=devicesState):
+					self.performTest(*device, devices)
+
+	def test_orderOfPrecedence(self):
+		"""Test that, when there are devices with duplicate names in different states, the one with the preferred state is returned."""
+		FRIENDLY_NAME = "Device friendly name"
+		devices: _DevicesT = {
+			DEVICE_STATE.ACTIVE: [_AudioOutputDevice("idA", FRIENDLY_NAME)],
+			DEVICE_STATE.DISABLED: [_AudioOutputDevice("idD", FRIENDLY_NAME)],
+			DEVICE_STATE.NOTPRESENT: [_AudioOutputDevice("idN", FRIENDLY_NAME)],
+			DEVICE_STATE.UNPLUGGED: [_AudioOutputDevice("idU", FRIENDLY_NAME)],
+		}
+		with self.subTest("Friendly name is active"):
+			self.performTest(*devices[DEVICE_STATE.ACTIVE][0], devices)
+		devices[DEVICE_STATE.ACTIVE].pop()
+		with self.subTest("Friendly name is unplugged"):
+			self.performTest(*devices[DEVICE_STATE.UNPLUGGED][0], devices)
+		devices[DEVICE_STATE.UNPLUGGED].pop()
+		with self.subTest("Friendly name is disabled"):
+			self.performTest(*devices[DEVICE_STATE.DISABLED][0], devices)
+		devices[DEVICE_STATE.DISABLED].pop()
+		with self.subTest("Friendly name is notpresent"):
+			self.performTest(*devices[DEVICE_STATE.NOTPRESENT][0], devices)
+		devices[DEVICE_STATE.NOTPRESENT].pop()
+
+	def test_nonexistant(self):
+		"""Test that attempting a match for a friendly name that no device has returns None."""
+		devices = self.DEFAULT_DEVICES
+		self.performTest(friendlyName="Nonexistant", expectedId=None, devices=devices)
+
+	def test_noDevices(self):
+		"""Test that attempting a match for a friendly name that no device has returns None."""
+		devices: _DevicesT = {}
+		self.performTest(friendlyName="Anything", expectedId=None, devices=devices)
+
+	def performTest(self, expectedId: str | None, friendlyName: str, devices: _DevicesT):
+		"""Patch nvwave._getOutputDevices to return what we tell it, then test that friendlyNameToEndpointId returns the correct ID given a friendly name.
+		The odd order of arguments is so you can directly unpack an AudioOutputDevice.
+		"""
+		with patch(
+			"nvwave._getOutputDevices",
+			autospec=True,
+			side_effect=getOutputDevicesFactory(devices),
+		):
+			self.assertEqual(_friendlyNameToEndpointId(friendlyName), expectedId)
+
+
+class Config_upgradeProfileSteps_upgradeProfileFrom_13_to_14(unittest.TestCase):
+	def setUp(self):
+		devices: _DevicesT = {
+			DEVICE_STATE.ACTIVE: [_AudioOutputDevice("id", "Friendly name")],
+		}
+		self._getOutputDevicesPatcher = patch(
+			"nvwave._getOutputDevices",
+			autospec=True,
+			side_effect=getOutputDevicesFactory(devices),
+		)
+		self._getOutputDevicesPatcher.start()
+		super().setUp()
+
+	def tearDown(self):
+		self._getOutputDevicesPatcher.stop()
+		super().tearDown()
+
+	def test_outputDeviceNotSet(self):
+		"""Test that upgrading with no output device set works."""
+		configString = ""
+		profile = _loadProfile(configString)
+		upgradeConfigFrom_13_to_14(profile)
+		with self.assertRaises(KeyError):
+			profile["speech"]["outputDevice"]
+		with self.assertRaises(KeyError):
+			profile["audio"]["outputDevice"]
+
+	def test_outputDeviceFound(self):
+		"""Test that upgrading the profile correctly creates the new key and value."""
+		configString = """
+		[speech]
+			outputDevice=Friendly name
+		"""
+		profile = _loadProfile(configString)
+		upgradeConfigFrom_13_to_14(profile)
+		self.assertEqual(profile["audio"]["outputDevice"], "id")
+		with self.assertRaises(KeyError):
+			profile["speech"]["outputDevice"]
+
+	def test_outputDeviceNotFound(self):
+		"""Test that upgrading the profile with an unidentifiable device doesn't create a new entry."""
+		configString = """
+		[speech]
+			outputDevice=Nonexistant device
+		"""
+		profile = _loadProfile(configString)
+		upgradeConfigFrom_13_to_14(profile)
+		with self.assertRaises(KeyError):
+			profile["speech"]["outputDevice"]
+		with self.assertRaises(KeyError):
+			profile["audio"]["outputDevice"]

commit 15fc7972e7b6b6d792051ff7034850cddb7c324a

Fix profile upgrade from 13 to 14 (#17589)
Fixes #17587
Fixes #17588

Summary of the issue:
The profile upgrade steps introduced in #17547 would cause NVDA to crash, and the user's configuration to be reset, if the user had an audio output device set in their NVDA configuration.

Description of user facing changes
Upgrading from configuration version 13 to 14 no longer crashes NVDA and resets the user's configuration.

Description of development approach
The issue was caused because `config.profileUpgradeSteps.upgradeConfigFrom_13_to_14` attempts to import `nvwave`, which, through its own imports, eventually attempts to use gettext. Since gettext has not yet been initialised, this raises an exception. As we are so early in NVDA initialisation, this exception causes the rest of NVDA's initialisation to fail critically.

To resolve this, `_getOutputDevices` and `_AudioOutputDevice` have been moved to the new `utils.mmdevice`. I initially thought to move them to `audio.utils`, but initialisation of `audio` runs into the same problem.

Testing strategy:
Checked out `a7fa0d6`, made sure the config version was `13`, ran NVDA and selected an audio output device. Checked out `fixProfileUpgrade`, ran NVDA, and ensured that it ran as expected, the profile was correctly updated, there was no loss of settings, and the correct output device was in use.

source/config/profileUpgradeSteps.py
@@ -458,7 +458,7 @@ def _friendlyNameToEndpointId(friendlyName: str) -> str | None:
 	:param friendlyName: Friendly name of the device to search for.
 	:return: Endpoint ID string of the best match device, or `None` if no device with a matching friendly name is available.
 	"""
-	from nvwave import _getOutputDevices
+	from utils.mmdevice import _getOutputDevices
 	from pycaw.constants import DEVICE_STATE
 
 	states = (DEVICE_STATE.ACTIVE, DEVICE_STATE.UNPLUGGED, DEVICE_STATE.DISABLED, DEVICE_STATE.NOTPRESENT)

source/gui/settingsDialogs.py
@@ -20,6 +20,7 @@ import requests
 import wx
 from NVDAState import WritePaths
 
+from utils import mmdevice
 from vision.providerBase import VisionEnhancementProviderSettings
 from wx.lib.expando import ExpandoTextCtrl
 import wx.lib.newevent
@@ -46,7 +47,6 @@ import gui
 import gui.contextHelp
 import globalVars
 from logHandler import log
-import nvwave
 import audio
 import audioDucking
 import queueHandler
@@ -3041,7 +3041,7 @@ class AudioPanel(SettingsPanel):
 		# Translators: This is the label for the select output device combo in NVDA audio settings.
 		# Examples of an output device are default soundcard, usb headphones, etc.
 		deviceListLabelText = _("Audio output &device:")
-		self._deviceIds, deviceNames = zip(*nvwave._getOutputDevices(includeDefault=True))
+		self._deviceIds, deviceNames = zip(*mmdevice._getOutputDevices(includeDefault=True))
 		self.deviceList = sHelper.addLabeledControl(deviceListLabelText, wx.Choice, choices=deviceNames)
 		self.bindHelpEvent("SelectSynthesizerOutputDevice", self.deviceList)
 		selectedOutputDevice = config.conf["audio"]["outputDevice"]

source/nvwave.py
@@ -6,7 +6,6 @@
 
 """Provides a simple Python interface to playing audio using the Windows Audio Session API (WASAPI), as well as other useful utilities."""
 
-from collections.abc import Generator
 import threading
 import typing
 from typing import (
@@ -40,7 +39,8 @@ import NVDAHelper
 import core
 import globalVars
 from pycaw.utils import AudioUtilities
-from pycaw.constants import EDataFlow, DEVICE_STATE
+
+from utils.mmdevice import _getOutputDevices
 
 
 __all__ = (
@@ -87,44 +87,6 @@ class AudioPurpose(Enum):
 
 	SPEECH = auto()
 	SOUNDS = auto()
-
-
-class _AudioOutputDevice(typing.NamedTuple):
-	id: str
-	friendlyName: str
-
-
-def _getOutputDevices(
-	*,
-	includeDefault: bool = False,
-	stateMask: DEVICE_STATE = DEVICE_STATE.ACTIVE,
-) -> Generator[_AudioOutputDevice]:
-	"""Generator, yielding device ID and device Name.
-	.. note:: Depending on number of devices being fetched, this may take some time (~3ms)
-
-	:param includeDefault: Whether to include a value representing the system default output device in the generator, defaults to False.
-		.. note:: The ID of this device is **not** a valid mmdevice endpoint ID string, and is for internal use only.
-			The friendly name is **not** generated by the operating system, and it is highly unlikely that it will match any real output device.
-	:param state: What device states to include in the resultant generator, defaults to DEVICE_STATE.ACTIVE.
-	:return: Generator of :class:`_AudioOutputDevices` containing all enabled and present audio output devices on the system.
-	"""
-	if includeDefault:
-		yield _AudioOutputDevice(
-			id=typing.cast(str, config.conf.getConfigValidation(("audio", "outputDevice")).default),
-			# Translators: Value to show when choosing to use the default audio output device.
-			friendlyName=_("Default output device"),
-		)
-	endpointCollection = AudioUtilities.GetDeviceEnumerator().EnumAudioEndpoints(
-		EDataFlow.eRender.value,
-		stateMask.value,
-	)
-	for i in range(endpointCollection.GetCount()):
-		device = AudioUtilities.CreateDevice(endpointCollection.Item(i))
-		# This should never be None, but just to be sure
-		if device is not None:
-			yield _AudioOutputDevice(device.id, device.FriendlyName)
-		else:
-			continue
 
 
 def getOutputDeviceNames() -> list[str]:

source/utils/mmdevice.py
@@ -1,0 +1,49 @@
+# A part of NonVisual Desktop Access (NVDA)
+# Copyright (C) 2025 NV Access Limited
+# This file is covered by the GNU General Public License.
+# See the file COPYING for more details.
+
+from collections.abc import Generator
+from typing import NamedTuple, cast
+
+import config
+from pycaw.constants import DEVICE_STATE, EDataFlow
+from pycaw.utils import AudioUtilities
+
+
+class _AudioOutputDevice(NamedTuple):
+	id: str
+	friendlyName: str
+
+
+def _getOutputDevices(
+	*,
+	includeDefault: bool = False,
+	stateMask: DEVICE_STATE = DEVICE_STATE.ACTIVE,
+) -> Generator[_AudioOutputDevice]:
+	"""Generator, yielding device ID and device Name.
+	.. note:: Depending on number of devices being fetched, this may take some time (~3ms)
+
+	:param includeDefault: Whether to include a value representing the system default output device in the generator, defaults to False.
+		.. note:: The ID of this device is **not** a valid mmdevice endpoint ID string, and is for internal use only.
+			The friendly name is **not** generated by the operating system, and it is highly unlikely that it will match any real output device.
+	:param state: What device states to include in the resultant generator, defaults to DEVICE_STATE.ACTIVE.
+	:return: Generator of :class:`_AudioOutputDevices` containing all enabled and present audio output devices on the system.
+	"""
+	if includeDefault:
+		yield _AudioOutputDevice(
+			id=cast(str, config.conf.getConfigValidation(("audio", "outputDevice")).default),
+			# Translators: Value to show when choosing to use the default audio output device.
+			friendlyName=_("Default output device"),
+		)
+	endpointCollection = AudioUtilities.GetDeviceEnumerator().EnumAudioEndpoints(
+		EDataFlow.eRender.value,
+		stateMask.value,
+	)
+	for i in range(endpointCollection.GetCount()):
+		device = AudioUtilities.CreateDevice(endpointCollection.Item(i))
+		# This should never be None, but just to be sure
+		if device is not None:
+			yield _AudioOutputDevice(device.id, device.FriendlyName)
+		else:
+			continue

tests/unit/test_config.py
@@ -47,7 +47,7 @@ from config.configFlags import (
 from utils.displayString import (
 	DisplayStringEnum,
 )
-from nvwave import _AudioOutputDevice
+from utils.mmdevice import _AudioOutputDevice
 
 
 class Config_FeatureFlagEnums_getAvailableEnums(unittest.TestCase):
@@ -924,7 +924,7 @@ _DevicesT: typing.TypeAlias = dict[DEVICE_STATE, list[_AudioOutputDevice]]
 def getOutputDevicesFactory(
 	devices: _DevicesT,
 ) -> Callable[[DEVICE_STATE], Generator[_AudioOutputDevice]]:
-	"""Create a callable that can be used to patch nvwave._getOutputDevices."""
+	"""Create a callable that can be used to patch utils.mmdevice._getOutputDevices."""
 
 	def getOutputDevices(stateMask: DEVICE_STATE, **kw) -> Generator[_AudioOutputDevice]:
 		yield from devices.get(stateMask, [])
@@ -981,11 +981,11 @@ class Config_ProfileUpgradeSteps_FriendlyNameToEndpointId(unittest.TestCase):
 		self.performTest(friendlyName="Anything", expectedId=None, devices=devices)
 
 	def performTest(self, expectedId: str | None, friendlyName: str, devices: _DevicesT):
-		"""Patch nvwave._getOutputDevices to return what we tell it, then test that friendlyNameToEndpointId returns the correct ID given a friendly name.
+		"""Patch utils.mmdevice._getOutputDevices to return what we tell it, then test that friendlyNameToEndpointId returns the correct ID given a friendly name.
 		The odd order of arguments is so you can directly unpack an AudioOutputDevice.
 		"""
 		with patch(
-			"nvwave._getOutputDevices",
+			"utils.mmdevice._getOutputDevices",
 			autospec=True,
 			side_effect=getOutputDevicesFactory(devices),
 		):
@@ -998,7 +998,7 @@ class Config_upgradeProfileSteps_upgradeProfileFrom_13_to_14(unittest.TestCase):
 			DEVICE_STATE.ACTIVE: [_AudioOutputDevice("id", "Friendly name")],
 		}
 		self._getOutputDevicesPatcher = patch(
-			"nvwave._getOutputDevices",
+			"utils.mmdevice._getOutputDevices",
 			autospec=True,
 			side_effect=getOutputDevicesFactory(devices),
 		)