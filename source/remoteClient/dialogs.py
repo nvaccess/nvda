eacf802c5179dbe7784b302a9b316b2a602e40e5
Christopher Toth <q.alpha@gmail.com>
2025-01-11 23:34:34 -0700

    refactor: Rename variables and methods to follow camelCase convention

--- source/remoteClient/dialogs.py
+++ source/remoteClient/dialogs.py
@@ -17,7 +17,7 @@ from .protocol import SERVER_PORT, RemoteMessageType
 class ClientPanel(wx.Panel):
 	host: wx.ComboBox
 	key: wx.TextCtrl
-	generate_key: wx.Button
+	generateKey: wx.Button
 	keyConnector: Optional["transport.RelayTransport"]
 
 	def __init__(self, parent: Optional[wx.Window] = None, id: int = wx.ID_ANY):
@@ -32,12 +32,12 @@ class ClientPanel(wx.Panel):
 		self.key = wx.TextCtrl(self, wx.ID_ANY)
 		sizer.Add(self.key)
 		# Translators: The button used to generate a random key/password.
-		self.generate_key = wx.Button(parent=self, label=_("&Generate Key"))
-		self.generate_key.Bind(wx.EVT_BUTTON, self.on_generate_key)
-		sizer.Add(self.generate_key)
+		self.generateKey = wx.Button(parent=self, label=_("&Generate Key"))
+		self.generateKey.Bind(wx.EVT_BUTTON, self.onGenerateKey)
+		sizer.Add(self.generateKey)
 		self.SetSizerAndFit(sizer)
 
-	def on_generate_key(self, evt: wx.CommandEvent) -> None:
+	def onGenerateKey(self, evt: wx.CommandEvent) -> None:
 		if not self.host.GetValue():
 			gui.messageBox(
 				# Translators: A message box displayed when the host field is empty and the user tries to generate a key.
@@ -49,37 +49,37 @@ class ClientPanel(wx.Panel):
 			self.host.SetFocus()
 		else:
 			evt.Skip()
-			self.generate_key_command()
+			self.generateKeyCommand()
 
-	def generate_key_command(self, insecure: bool = False) -> None:
+	def generateKeyCommand(self, insecure: bool = False) -> None:
 		address = socket_utils.addressToHostPort(self.host.GetValue())
 		self.keyConnector = transport.RelayTransport(
 			address=address,
 			serializer=serializer.JSONSerializer(),
 			insecure=insecure,
 		)
-		self.keyConnector.registerInbound(RemoteMessageType.generate_key, self.handle_key_generated)
-		self.keyConnector.transportCertificateAuthenticationFailed.register(self.handle_certificate_failed)
+		self.keyConnector.registerInbound(RemoteMessageType.generate_key, self.handleKeyGenerated)
+		self.keyConnector.transportCertificateAuthenticationFailed.register(self.handleCertificateFailed)
 		t = threading.Thread(target=self.keyConnector.run)
 		t.start()
 
 	@alwaysCallAfter
-	def handle_key_generated(self, key: Optional[str] = None) -> None:
+	def handleKeyGenerated(self, key: Optional[str] = None) -> None:
 		self.key.SetValue(key)
 		self.key.SetFocus()
 		self.keyConnector.close()
 		self.keyConnector = None
 
 	@alwaysCallAfter
-	def handle_certificate_failed(self) -> None:
+	def handleCertificateFailed(self) -> None:
 		try:
-			cert_hash = self.keyConnector.lastFailFingerprint
+			certHash = self.keyConnector.lastFailFingerprint
 
-			wnd = CertificateUnauthorizedDialog(None, fingerprint=cert_hash)
+			wnd = CertificateUnauthorizedDialog(None, fingerprint=certHash)
 			a = wnd.ShowModal()
 			if a == wx.ID_YES:
 				config = configuration.get_config()
-				config["trusted_certs"][self.host.GetValue()] = cert_hash
+				config["trusted_certs"][self.host.GetValue()] = certHash
 			if a != wx.ID_YES and a != wx.ID_NO:
 				return
 		except Exception as ex:
@@ -87,7 +87,7 @@ class ClientPanel(wx.Panel):
 			return
 		self.keyConnector.close()
 		self.keyConnector = None
-		self.generate_key_command(True)
+		self.generateKeyCommand(True)
 
 
 class PortCheckResponse(TypedDict):
@@ -97,23 +97,23 @@ class PortCheckResponse(TypedDict):
 
 
 class ServerPanel(wx.Panel):
-	get_IP: wx.Button
-	external_IP: wx.TextCtrl
+	getIP: wx.Button
+	externalIP: wx.TextCtrl
 	port: wx.TextCtrl
 	key: wx.TextCtrl
-	generate_key: wx.Button
+	generateKey: wx.Button
 
 	def __init__(self, parent: Optional[wx.Window] = None, id: int = wx.ID_ANY):
 		super().__init__(parent, id)
 		sizer = wx.BoxSizer(wx.HORIZONTAL)
 		# Translators: Used in server mode to obtain the external IP address for the server (controlled computer) for direct connection.
-		self.get_IP = wx.Button(parent=self, label=_("Get External &IP"))
-		self.get_IP.Bind(wx.EVT_BUTTON, self.on_get_IP)
-		sizer.Add(self.get_IP)
+		self.getIP = wx.Button(parent=self, label=_("Get External &IP"))
+		self.getIP.Bind(wx.EVT_BUTTON, self.onGetIP)
+		sizer.Add(self.getIP)
 		# Translators: Label of the field displaying the external IP address if using direct (client to server) connection.
 		sizer.Add(wx.StaticText(self, wx.ID_ANY, label=_("&External IP:")))
-		self.external_IP = wx.TextCtrl(self, wx.ID_ANY, style=wx.TE_READONLY | wx.TE_MULTILINE)
-		sizer.Add(self.external_IP)
+		self.externalIP = wx.TextCtrl(self, wx.ID_ANY, style=wx.TE_READONLY | wx.TE_MULTILINE)
+		sizer.Add(self.externalIP)
 		# Translators: The label of an edit field in connect dialog to enter the port the server will listen on.
 		sizer.Add(wx.StaticText(self, wx.ID_ANY, label=_("&Port:")))
 		self.port = wx.TextCtrl(self, wx.ID_ANY, value=str(SERVER_PORT))
@@ -121,12 +121,12 @@ class ServerPanel(wx.Panel):
 		sizer.Add(wx.StaticText(self, wx.ID_ANY, label=_("&Key:")))
 		self.key = wx.TextCtrl(self, wx.ID_ANY)
 		sizer.Add(self.key)
-		self.generate_key = wx.Button(parent=self, label=_("&Generate Key"))
-		self.generate_key.Bind(wx.EVT_BUTTON, self.on_generate_key)
-		sizer.Add(self.generate_key)
+		self.generateKey = wx.Button(parent=self, label=_("&Generate Key"))
+		self.generateKey.Bind(wx.EVT_BUTTON, self.onGenerateKey)
+		sizer.Add(self.generateKey)
 		self.SetSizerAndFit(sizer)
 
-	def on_generate_key(self, evt: wx.CommandEvent) -> None:
+	def onGenerateKey(self, evt: wx.CommandEvent) -> None:
 		evt.Skip()
 		res = str(random.randrange(1, 9))
 		for n in range(6):
@@ -134,15 +134,15 @@ class ServerPanel(wx.Panel):
 		self.key.SetValue(res)
 		self.key.SetFocus()
 
-	def on_get_IP(self, evt: wx.CommandEvent) -> None:
+	def onGetIP(self, evt: wx.CommandEvent) -> None:
 		evt.Skip()
-		self.get_IP.Enable(False)
-		t = threading.Thread(target=self.do_portcheck, args=[int(self.port.GetValue())])
+		self.getIP.Enable(False)
+		t = threading.Thread(target=self.doPortcheck, args=[int(self.port.GetValue())])
 		t.daemon = True
 		t.start()
 
-	def do_portcheck(self, port: int) -> None:
-		temp_server = server.LocalRelayServer(port=port, password=None)
+	def doPortcheck(self, port: int) -> None:
+		tempServer = server.LocalRelayServer(port=port, password=None)
 		try:
 			req = request.urlopen("https://portcheck.nvdaremote.com/port/%s" % port)
 			data = req.read()
@@ -152,62 +152,62 @@ class ServerPanel(wx.Panel):
 			wx.CallAfter(self.onGetIPFail, e)
 			raise
 		finally:
-			temp_server.close()
-			wx.CallAfter(self.get_IP.Enable, True)
+			tempServer.close()
+			wx.CallAfter(self.getIP.Enable, True)
 
 	def onGetIPSucceeded(self, data: PortCheckResponse) -> None:
 		ip = data["host"]
 		port = data["port"]
-		is_open = data["open"]
+		isOpen = data["open"]
 
-		if is_open:
+		if isOpen:
 			# Translators: Message shown when successfully getting external IP and the specified port is open
-			success_msg = _("Successfully retrieved IP address. Port {port} is open.")
+			successMsg = _("Successfully retrieved IP address. Port {port} is open.")
 			# Translators: Title of success dialog
-			success_title = _("Success")
+			successTitle = _("Success")
 			wx.MessageBox(
-				message=success_msg.format(port=port),
-				caption=success_title,
+				message=successMsg.format(port=port),
+				caption=successTitle,
 				style=wx.OK,
 			)
 		else:
 			# Translators: Message shown when IP was retrieved but the specified port is not forwarded
-			warning_msg = _("Retrieved external IP, but port {port} is not currently forwarded.")
+			warningMsg = _("Retrieved external IP, but port {port} is not currently forwarded.")
 			# Translators: Title of warning dialog
-			warning_title = _("Warning")
+			warningTitle = _("Warning")
 			wx.MessageBox(
-				message=warning_msg.format(port=port),
-				caption=warning_title,
+				message=warningMsg.format(port=port),
+				caption=warningTitle,
 				style=wx.ICON_WARNING | wx.OK,
 			)
 
-		self.external_IP.SetValue(ip)
-		self.external_IP.SetSelection(0, len(ip))
-		self.external_IP.SetFocus()
+		self.externalIP.SetValue(ip)
+		self.externalIP.SetSelection(0, len(ip))
+		self.externalIP.SetFocus()
 
 	def onGetIPFail(self, exc: Exception) -> None:
 		# Translators: Error message when unable to get IP address from portcheck server
-		error_msg = _("Unable to contact portcheck server, please manually retrieve your IP address")
+		errorMsg = _("Unable to contact portcheck server, please manually retrieve your IP address")
 		# Translators: Title of error dialog
-		error_title = _("Error")
+		errorTitle = _("Error")
 		wx.MessageBox(
-			message=error_msg,
-			caption=error_title,
+			message=errorMsg,
+			caption=errorTitle,
 			style=wx.ICON_ERROR | wx.OK,
 		)
 
 
 class DirectConnectDialog(wx.Dialog):
-	client_or_server: wx.RadioBox
-	connection_type: wx.RadioBox
+	clientOrServer: wx.RadioBox
+	connectionType: wx.RadioBox
 	container: wx.Panel
 	panel: Union[ClientPanel, ServerPanel]
-	main_sizer: wx.BoxSizer
+	mainSizer: wx.BoxSizer
 
 	def __init__(self, parent: wx.Window, id: int, title: str, hostnames: Optional[List[str]] = None):
 		super().__init__(parent, id, title=title)
-		main_sizer = self.main_sizer = wx.BoxSizer(wx.VERTICAL)
-		self.client_or_server = wx.RadioBox(
+		mainSizer = self.mainSizer = wx.BoxSizer(wx.VERTICAL)
+		self.clientOrServer = wx.RadioBox(
 			self,
 			wx.ID_ANY,
 			choices=(
@@ -218,29 +218,29 @@ class DirectConnectDialog(wx.Dialog):
 			),
 			style=wx.RA_VERTICAL,
 		)
-		self.client_or_server.Bind(wx.EVT_RADIOBOX, self.onClientOrServer)
-		self.client_or_server.SetSelection(0)
-		main_sizer.Add(self.client_or_server)
+		self.clientOrServer.Bind(wx.EVT_RADIOBOX, self.onClientOrServer)
+		self.clientOrServer.SetSelection(0)
+		mainSizer.Add(self.clientOrServer)
 		choices = [
 			# Translators: A choice to control another machine.
 			_("Control another machine"),
 			# Translators: A choice to allow this machine to be controlled.
 			_("Allow this machine to be controlled"),
 		]
-		self.connection_type = wx.RadioBox(self, wx.ID_ANY, choices=choices, style=wx.RA_VERTICAL)
-		self.connection_type.SetSelection(0)
-		main_sizer.Add(self.connection_type)
+		self.connectionType = wx.RadioBox(self, wx.ID_ANY, choices=choices, style=wx.RA_VERTICAL)
+		self.connectionType.SetSelection(0)
+		mainSizer.Add(self.connectionType)
 		self.container = wx.Panel(parent=self)
 		self.panel = ClientPanel(parent=self.container)
-		main_sizer.Add(self.container)
+		mainSizer.Add(self.container)
 		buttons = self.CreateButtonSizer(wx.OK | wx.CANCEL)
-		main_sizer.Add(buttons, flag=wx.BOTTOM)
-		main_sizer.Fit(self)
-		self.SetSizer(main_sizer)
+		mainSizer.Add(buttons, flag=wx.BOTTOM)
+		mainSizer.Fit(self)
+		self.SetSizer(mainSizer)
 		self.Center(wx.BOTH | wx.CENTER)
 		ok = wx.FindWindowById(wx.ID_OK, self)
 		ok.Bind(wx.EVT_BUTTON, self.onOk)
-		self.client_or_server.SetFocus()
+		self.clientOrServer.SetFocus()
 		if hostnames:
 			self.panel.host.AppendItems(hostnames)
 			self.panel.host.SetSelection(0)
@@ -248,14 +248,14 @@ class DirectConnectDialog(wx.Dialog):
 	def onClientOrServer(self, evt: wx.CommandEvent) -> None:
 		evt.Skip()
 		self.panel.Destroy()
-		if self.client_or_server.GetSelection() == 0:
+		if self.clientOrServer.GetSelection() == 0:
 			self.panel = ClientPanel(parent=self.container)
 		else:
 			self.panel = ServerPanel(parent=self.container)
-		self.main_sizer.Fit(self)
+		self.mainSizer.Fit(self)
 
 	def onOk(self, evt: wx.CommandEvent) -> None:
-		if self.client_or_server.GetSelection() == 0 and (
+		if self.clientOrServer.GetSelection() == 0 and (
 			not self.panel.host.GetValue() or not self.panel.key.GetValue()
 		):
 			gui.messageBox(
@@ -267,7 +267,7 @@ class DirectConnectDialog(wx.Dialog):
 			)
 			self.panel.host.SetFocus()
 		elif (
-			self.client_or_server.GetSelection() == 1
+			self.clientOrServer.GetSelection() == 1
 			and not self.panel.port.GetValue()
 			or not self.panel.key.GetValue()
 		):
@@ -286,10 +286,10 @@ class DirectConnectDialog(wx.Dialog):
 		return self.panel.key.GetValue()
 
 	def getConnectionInfo(self) -> ConnectionInfo:
-		if self.client_or_server.GetSelection() == 0:  # client
+		if self.clientOrServer.GetSelection() == 0:  # client
 			host = self.panel.host.GetValue()
 			serverAddr, port = socket_utils.addressToHostPort(host)
-			mode = ConnectionMode.MASTER if self.connection_type.GetSelection() == 0 else ConnectionMode.SLAVE
+			mode = ConnectionMode.MASTER if self.connectionType.GetSelection() == 0 else ConnectionMode.SLAVE
 			return ConnectionInfo(
 				hostname=serverAddr,
 				mode=mode,
@@ -299,7 +299,7 @@ class DirectConnectDialog(wx.Dialog):
 			)
 		else:  # server
 			port = int(self.panel.port.GetValue())
-			mode = "master" if self.connection_type.GetSelection() == 0 else "slave"
+			mode = "master" if self.connectionType.GetSelection() == 0 else "slave"
 			return ConnectionInfo(
 				hostname="127.0.0.1",
 				mode=mode,
