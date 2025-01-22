nvaccess/nvda

commit b6d08a0dd6717d7a198b26334af6951a19883028

Keystrokes to adjust applications volume and mute [take 2] (#16591)
Closes #16052.
Please note that this is my second attempt to implement this. The first attempt was #16273 which was reverted in #16440# by commit f63841f.

Summary of the issue:
Provide a way to adjust volume of other applications, so that when using sound split the volume of aplications channel can be adjusted separately from NVDA volume.

Description of user facing changes
New keystrokes:

NVDA+alt+pageUp/pageDown adjusts volume of all other applications.
NVDA+alt+delete Cycles between three states
Disabled (default)
Enabled
Muted - all applications except NVDA will be muted
Description of development approach
I refactored sound split code. Sound split and applications volume adjustment need to use same callbacks in a similar fashion. In order to reduce code reduplication, I extracted common logic and put it in audio/utils.py. Now it provides an abstract class AudioSessionCallback and customers (sound split and app volume adjuster) are implementing this class. Two methods that need to be implemented are onSessionUpdate and onSessionTerminated, which operate on a single audio session. All the plumbing is hidden in utils.py so that clients don't need to worry about setting up and dealing with multiple lower-level callbacks.
Applications' voluem adjusting logic is implemented in audio/appsVolume.py and is quite straightforward now.
Volume and mute status are adjusted via ISimpleAudioVolume interface, which means that both are reflected in Windows Volume Mixer.
4.Initial volume and mute status of applications is storedand upon exit will be restored.
Applciation volume adjustment is now completely independent of sound split and both features can operate independently of each other.

source/globalCommands.py
@@ -68,6 +68,7 @@ from base64 import b16encode
 import vision
 from utils.security import objectBelowLockScreenAndWindowsIsLocked
 import audio
+from audio import appsVolume
 
 
 #: Script category for text review commands.
@@ -4705,6 +4706,50 @@ class GlobalCommands(ScriptableObject):
 	)
 	def script_cycleSoundSplit(self, gesture: "inputCore.InputGesture") -> None:
 		audio._toggleSoundSplitState()
+
+	@script(
+		description=_(
+			# Translators: Describes a command.
+			"Increases the volume of the other applications",
+		),
+		category=SCRCAT_AUDIO,
+		gesture="kb:NVDA+alt+pageUp",
+	)
+	def script_increaseApplicationsVolume(self, gesture: "inputCore.InputGesture") -> None:
+		appsVolume._adjustAppsVolume(5)
+
+	@script(
+		description=_(
+			# Translators: Describes a command.
+			"Decreases the volume of the other applications",
+		),
+		category=SCRCAT_AUDIO,
+		gesture="kb:NVDA+alt+pageDown",
+	)
+	def script_decreaseApplicationsVolume(self, gesture: "inputCore.InputGesture") -> None:
+		appsVolume._adjustAppsVolume(-5)
+
+	@script(
+		description=_(
+			# Translators: Describes a command.
+			"Toggles other applications volume adjuster status",
+		),
+		category=SCRCAT_AUDIO,
+		gesture=None,
+	)
+	def script_toggleApplicationsVolumeAdjuster(self, gesture: "inputCore.InputGesture") -> None:
+		appsVolume._toggleAppsVolumeState()
+
+	@script(
+		description=_(
+			# Translators: Describes a command.
+			"Mutes or unmutes other applications",
+		),
+		category=SCRCAT_AUDIO,
+		gesture="kb:NVDA+alt+delete",
+	)
+	def script_toggleApplicationsMute(self, gesture: "inputCore.InputGesture") -> None:
+		appsVolume._toggleAppsVolumeMute()
 
 
 #: The single global commands instance.

source/audio/__init__.py
@@ -8,9 +8,48 @@ from .soundSplit import (
 	_setSoundSplitState,
 	_toggleSoundSplitState,
 )
+from . import appsVolume, soundSplit, utils
+import atexit
+import nvwave
+from pycaw.utils import AudioUtilities
+from comtypes import COMError
+from logHandler import log
 
 __all__ = [
 	"SoundSplitState",
 	"_setSoundSplitState",
 	"_toggleSoundSplitState",
 ]
+
+audioUtilitiesInitialized: bool = False
+
+
+def initialize() -> None:
+	if nvwave.usingWasapiWavePlayer():
+		try:
+			AudioUtilities.GetAudioSessionManager()
+		except COMError:
+			log.exception("Could not initialize audio session manager")
+			return
+		log.debug("Initializing utils")
+		utils.initialize()
+		log.debug("Initializing appsVolume")
+		appsVolume.initialize()
+		log.debug("Initializing soundSplit")
+		soundSplit.initialize()
+		global audioUtilitiesInitialized
+		audioUtilitiesInitialized = True
+	else:
+		log.debug("Cannot initialize audio utilities as WASAPI is disabled")
+
+
+@atexit.register
+def terminate():
+	if not audioUtilitiesInitialized:
+		log.debug("Skipping terminating audio utilities as initialization was skipped.")
+	elif not nvwave.usingWasapiWavePlayer():
+		log.debug("Skipping terminating audio utilites as WASAPI is disabled.")
+	else:
+		soundSplit.terminate()
+		appsVolume.terminate()
+		utils.terminate()

source/audio/appsVolume.py
@@ -1,0 +1,194 @@
+# A part of NonVisual Desktop Access (NVDA)
+# Copyright (C) 2024 NV Access Limited
+# This file is covered by the GNU General Public License.
+# See the file COPYING for more details.
+
+import config
+import globalVars
+from logHandler import log
+import nvwave
+from pycaw.utils import AudioSession
+import ui
+from dataclasses import dataclass
+from threading import Lock
+from config.featureFlagEnums import AppsVolumeAdjusterFlag
+from typing import NamedTuple
+from .utils import AudioSessionCallback, DummyAudioSessionCallback
+from comtypes import COMError
+
+
+class VolumeAndMute(NamedTuple):
+	volume: float
+	mute: bool
+
+
+_appVolumesCache: dict[int, VolumeAndMute] = {}
+_appVolumesCacheLock = Lock()
+_activeCallback: DummyAudioSessionCallback | None = None
+
+
+def initialize() -> None:
+	state = config.conf["audio"]["applicationsVolumeMode"]
+	volume = config.conf["audio"]["applicationsSoundVolume"]
+	muted = config.conf["audio"]["applicationsSoundMuted"]
+	if muted:
+		# Muted flag should not be persistent.
+		config.conf["audio"]["applicationsSoundMuted"] = False
+		muted = False
+	_updateAppsVolumeImpl(volume / 100.0, muted, state)
+
+
+def terminate():
+	global _activeCallback
+	if _activeCallback is not None:
+		_activeCallback.unregister()
+		_activeCallback = None
+
+
+@dataclass(unsafe_hash=True)
+class VolumeSetter(AudioSessionCallback):
+	volumeAndMute: VolumeAndMute | None = None
+
+	def getOriginalVolumeAndMute(self, pid: int) -> VolumeAndMute:
+		try:
+			with _appVolumesCacheLock:
+				originalVolumeAndMute = _appVolumesCache[pid]
+				del _appVolumesCache[pid]
+		except KeyError:
+			originalVolumeAndMute = VolumeAndMute(volume=1.0, mute=False)
+		return originalVolumeAndMute
+
+	def onSessionUpdate(self, session: AudioSession) -> None:
+		pid = session.ProcessId
+		simpleVolume = session.SimpleAudioVolume
+		with _appVolumesCacheLock:
+			if pid not in _appVolumesCache:
+				_appVolumesCache[pid] = VolumeAndMute(
+					volume=simpleVolume.GetMasterVolume(),
+					mute=simpleVolume.GetMute(),
+				)
+		if pid != globalVars.appPid:
+			simpleVolume.SetMasterVolume(self.volumeAndMute.volume, None)
+			simpleVolume.SetMute(self.volumeAndMute.mute, None)
+
+	def onSessionTerminated(self, session: AudioSession) -> None:
+		pid = session.ProcessId
+		simpleVolume = session.SimpleAudioVolume
+		originalVolumeAndMute = self.getOriginalVolumeAndMute(pid)
+		try:
+			simpleVolume.SetMasterVolume(originalVolumeAndMute.volume, None)
+			simpleVolume.SetMute(originalVolumeAndMute.mute, None)
+		except (COMError, RuntimeError) as e:
+			log.exception(f"Could not restore master volume of process {pid} upon exit: {e}")
+
+
+def _updateAppsVolumeImpl(
+	volume: float,
+	muted: bool,
+	state: AppsVolumeAdjusterFlag,
+):
+	global _activeCallback
+	if state == AppsVolumeAdjusterFlag.DISABLED:
+		newCallback = DummyAudioSessionCallback()
+		runTerminators = True
+	else:
+		newCallback = VolumeSetter(
+			volumeAndMute=VolumeAndMute(
+				volume=volume,
+				mute=muted,
+			),
+		)
+		runTerminators = False
+	if _activeCallback is not None:
+		_activeCallback.unregister(runTerminators=runTerminators)
+	_activeCallback = newCallback
+	_activeCallback.register()
+
+
+def _adjustAppsVolume(
+	volumeAdjustment: int | None = None,
+):
+	if not nvwave.usingWasapiWavePlayer():
+		message = _(
+			# Translators: error message when wasapi is turned off.
+			"Other applications' volume cannot be adjusted. "
+			"Please enable WASAPI in the Advanced category in NVDA Settings to use it.",
+		)
+		ui.message(message)
+		return
+	volume: int = config.conf["audio"]["applicationsSoundVolume"]
+	muted: bool = config.conf["audio"]["applicationsSoundMuted"]
+	state = config.conf["audio"]["applicationsVolumeMode"]
+	if state != AppsVolumeAdjusterFlag.ENABLED:
+		# Translators: error message when applications' volume is disabled
+		msg = _("Please enable applications' volume adjuster in order to adjust applications' volume")
+		ui.message(msg)
+		return
+	volume += volumeAdjustment
+	volume = max(0, min(100, volume))
+	log.debug(f"Adjusting applications volume by {volumeAdjustment}% to {volume}%")
+	config.conf["audio"]["applicationsSoundVolume"] = volume
+
+	# We skip running terminators here to avoid application volume spiking to 100% for a split second.
+	_updateAppsVolumeImpl(volume / 100.0, muted, state)
+	# Translators: Announcing new applications' volume message
+	msg = _("Applications volume {}").format(volume)
+	ui.message(msg)
+
+
+_APPS_VOLUME_STATES_ORDER = [
+	AppsVolumeAdjusterFlag.DISABLED,
+	AppsVolumeAdjusterFlag.ENABLED,
+]
+
+
+def _toggleAppsVolumeState():
+	if not nvwave.usingWasapiWavePlayer():
+		message = _(
+			# Translators: error message when wasapi is turned off.
+			"Other applications' volume cannot be adjusted. "
+			"Please enable WASAPI in the Advanced category in NVDA Settings to use it.",
+		)
+		ui.message(message)
+		return
+	state = config.conf["audio"]["applicationsVolumeMode"]
+	volume: int = config.conf["audio"]["applicationsSoundVolume"]
+	muted: bool = config.conf["audio"]["applicationsSoundMuted"]
+	try:
+		index = _APPS_VOLUME_STATES_ORDER.index(state)
+	except ValueError:
+		index = -1
+	index = (index + 1) % len(_APPS_VOLUME_STATES_ORDER)
+	state = _APPS_VOLUME_STATES_ORDER[index]
+	config.conf["audio"]["applicationsVolumeMode"] = state.name
+	_updateAppsVolumeImpl(volume / 100.0, muted, state)
+	ui.message(state.displayString)
+
+
+def _toggleAppsVolumeMute():
+	if not nvwave.usingWasapiWavePlayer():
+		message = _(
+			# Translators: error message when wasapi is turned off.
+			"Other applications' mute status cannot be adjusted. "
+			"Please enable WASAPI in the Advanced category in NVDA Settings to use it.",
+		)
+		ui.message(message)
+		return
+	state = config.conf["audio"]["applicationsVolumeMode"]
+	volume: int = config.conf["audio"]["applicationsSoundVolume"]
+	muted: bool = config.conf["audio"]["applicationsSoundMuted"]
+	if state != AppsVolumeAdjusterFlag.ENABLED:
+		# Translators: error message when applications' volume is disabled
+		msg = _("Please enable applications' volume adjuster in order to mute other applications")
+		ui.message(msg)
+		return
+	muted = not muted
+	config.conf["audio"]["applicationsSoundMuted"] = muted
+	_updateAppsVolumeImpl(volume / 100.0, muted, state)
+	if muted:
+		# Translators: Announcing new applications' mute status message
+		msg = _("Muted other applications")
+	else:
+		# Translators: Announcing new applications' mute status message
+		msg = _("Unmuted other applications")
+	ui.message(msg)

source/audio/soundSplit.py
@@ -3,21 +3,16 @@
 # This file is covered by the GNU General Public License.
 # See the file COPYING for more details.
 
-import atexit
 import config
 from enum import IntEnum, unique
 import globalVars
 from logHandler import log
 import nvwave
-from pycaw.api.audiopolicy import IAudioSessionManager2
-from pycaw.callbacks import AudioSessionNotification, AudioSessionEvents
-from pycaw.utils import AudioSession, AudioUtilities
+from pycaw.utils import AudioSession
 import ui
 from utils.displayString import DisplayStringIntEnum
 from dataclasses import dataclass
-from comtypes import COMError
-from threading import Lock
-import core
+from .utils import AudioSessionCallback, DummyAudioSessionCallback
 
 VolumeTupleT = tuple[float, float]
 
@@ -88,91 +83,32 @@ class SoundSplitState(DisplayStringIntEnum):
 				raise RuntimeError(f"Unexpected or unknown state {self=}")
 
 
-_audioSessionManager: IAudioSessionManager2 | None = None
-_activeCallback: AudioSessionNotification | None = None
+_activeCallback: DummyAudioSessionCallback | None = None
 
 
 def initialize() -> None:
-	if nvwave.usingWasapiWavePlayer():
-		global _audioSessionManager
-		try:
-			_audioSessionManager = AudioUtilities.GetAudioSessionManager()
-		except COMError:
-			log.exception("Could not initialize audio session manager")
-			return
-		state = SoundSplitState(config.conf["audio"]["soundSplitState"])
-		_setSoundSplitState(state, initial=True)
-	else:
-		log.debug("Cannot initialize sound split as WASAPI is disabled")
+	state = SoundSplitState(config.conf["audio"]["soundSplitState"])
+	_setSoundSplitState(state)
 
 
-@atexit.register
 def terminate():
-	if nvwave.usingWasapiWavePlayer():
-		state = SoundSplitState(config.conf["audio"]["soundSplitState"])
-		if state != SoundSplitState.OFF:
-			_setSoundSplitState(SoundSplitState.OFF)
-		_unregisterCallback()
-	else:
-		log.debug("Skipping terminating sound split as WASAPI is disabled.")
-
-
-@dataclass(unsafe_hash=True)
-class _AudioSessionNotificationWrapper(AudioSessionNotification):
-	listener: AudioSessionNotification
-
-	def on_session_created(self, new_session: AudioSession):
-		pid = new_session.ProcessId
-		with _applicationExitCallbacksLock:
-			if pid not in _applicationExitCallbacks:
-				volumeRestorer = _VolumeRestorer(pid, new_session)
-				new_session.register_notification(volumeRestorer)
-				_applicationExitCallbacks[pid] = volumeRestorer
-		self.listener.on_session_created(new_session)
-
-
-def _applyToAllAudioSessions(
-	callback: AudioSessionNotification,
-	applyToFuture: bool = True,
-) -> None:
-	"""
-	Executes provided callback function on all active audio sessions.
-	Additionally, if applyToFuture is True, then it will register a notification with audio session manager,
-	which will execute the same callback for all future sessions as they are created.
-	That notification will be active until next invokation of this function,
-	or until _unregisterCallback() is called.
-	"""
-	_unregisterCallback()
-	callback = _AudioSessionNotificationWrapper(callback)
-	if applyToFuture:
-		_audioSessionManager.RegisterSessionNotification(callback)
-		# The following call is required to make callback to work:
-		_audioSessionManager.GetSessionEnumerator()
-		global _activeCallback
-		_activeCallback = callback
-	sessions: list[AudioSession] = AudioUtilities.GetAllSessions()
-	for session in sessions:
-		callback.on_session_created(session)
-
-
-def _unregisterCallback() -> None:
 	global _activeCallback
 	if _activeCallback is not None:
-		_audioSessionManager.UnregisterSessionNotification(_activeCallback)
+		_activeCallback.unregister()
 		_activeCallback = None
 
 
 @dataclass(unsafe_hash=True)
-class _VolumeSetter(AudioSessionNotification):
-	leftVolume: float
-	rightVolume: float
-	leftNVDAVolume: float
-	rightNVDAVolume: float
+class ChannelVolumeSetter(AudioSessionCallback):
+	leftVolume: float = 0.0
+	rightVolume: float = 0.0
+	leftNVDAVolume: float = 0.0
+	rightNVDAVolume: float = 0.0
 	foundSessionWithNot2Channels: bool = False
 
-	def on_session_created(self, new_session: AudioSession):
-		pid = new_session.ProcessId
-		channelVolume = new_session.channelAudioVolume()
+	def onSessionUpdate(self, session: AudioSession) -> None:
+		pid = session.ProcessId
+		channelVolume = session.channelAudioVolume()
 		channelCount = channelVolume.GetChannelCount()
 		if channelCount != 2:
 			log.warning(
@@ -187,23 +123,42 @@ class _VolumeSetter(AudioSessionNotification):
 			channelVolume.SetChannelVolume(0, self.leftNVDAVolume, None)
 			channelVolume.SetChannelVolume(1, self.rightNVDAVolume, None)
 
+	def onSessionTerminated(self, session: AudioSession) -> None:
+		pid = session.ProcessId
+		try:
+			channelVolume = session.channelAudioVolume()
+			channelCount = channelVolume.GetChannelCount()
+			if channelCount != 2:
+				log.warning(
+					f"Audio session for pid {pid} has {channelCount} channels instead of 2 - cannot set volume!",
+				)
+				return
+			channelVolume.SetChannelVolume(0, 1.0, None)
+			channelVolume.SetChannelVolume(1, 1.0, None)
+		except Exception:
+			log.exception(f"Could not restore channel volume of process {pid} upon exit.")
 
-def _setSoundSplitState(state: SoundSplitState, initial: bool = False) -> dict:
-	applyToFuture = True
+
+def _setSoundSplitState(state: SoundSplitState) -> dict:
+	global _activeCallback
+	if _activeCallback is not None:
+		_activeCallback.unregister()
+		_activeCallback = None
 	if state == SoundSplitState.OFF:
-		if initial:
-			return {}
-		else:
-			# Disabling sound split via command or via settings
-			# We need to restore volume of all applications, but then don't set up callback for future audio sessions
-			state = SoundSplitState.NVDA_BOTH_APPS_BOTH
-			applyToFuture = False
-	leftVolume, rightVolume = state.getAppVolume()
-	leftNVDAVolume, rightNVDAVolume = state.getNVDAVolume()
-	volumeSetter = _VolumeSetter(leftVolume, rightVolume, leftNVDAVolume, rightNVDAVolume)
-	_applyToAllAudioSessions(volumeSetter, applyToFuture=applyToFuture)
+		_activeCallback = DummyAudioSessionCallback()
+	else:
+		leftVolume, rightVolume = state.getAppVolume()
+		leftNVDAVolume, rightNVDAVolume = state.getNVDAVolume()
+		_activeCallback = ChannelVolumeSetter(
+			leftVolume=leftVolume,
+			rightVolume=rightVolume,
+			leftNVDAVolume=leftNVDAVolume,
+			rightNVDAVolume=rightNVDAVolume,
+		)
+	_activeCallback.register()
+	notTwoChannels = False if state == SoundSplitState.OFF else _activeCallback.foundSessionWithNot2Channels
 	return {
-		"foundSessionWithNot2Channels": volumeSetter.foundSessionWithNot2Channels,
+		"foundSessionWithNot2Channels": notTwoChannels,
 	}
 
 
@@ -236,45 +191,3 @@ def _toggleSoundSplitState() -> None:
 			"one of audio sessions is either mono, or has more than 2 audio channels.",
 		)
 		ui.message(msg)
-
-
-@dataclass(unsafe_hash=True)
-class _VolumeRestorer(AudioSessionEvents):
-	pid: int
-	audioSession: AudioSession
-
-	def on_state_changed(self, new_state: str, new_state_id: int):
-		if new_state == "Expired":
-			# For some reason restoring volume doesn't work in this thread, so scheduling in the main thread.
-			core.callLater(0, self.restoreVolume)
-
-	def restoreVolume(self):
-		# Application connected to this audio session is terminating. Restore its volume.
-		try:
-			channelVolume = self.audioSession.channelAudioVolume()
-			channelCount = channelVolume.GetChannelCount()
-			if channelCount != 2:
-				log.warning(
-					f"Audio session for pid {self.pid} has {channelCount} channels instead of 2 - cannot set volume!",
-				)
-				return
-			channelVolume.SetChannelVolume(0, 1.0, None)
-			channelVolume.SetChannelVolume(1, 1.0, None)
-		except Exception:
-			log.exception(f"Could not restore volume of process {self.pid} upon exit.")
-		self.unregister()
-
-	def unregister(self):
-		with _applicationExitCallbacksLock:
-			try:
-				del _applicationExitCallbacks[self.pid]
-			except KeyError:
-				pass
-			try:
-				self.audioSession.unregister_notification()
-			except Exception:
-				log.exception(f"Cannot unregister audio session for process {self.pid}")
-
-
-_applicationExitCallbacksLock = Lock()
-_applicationExitCallbacks: dict[int, _VolumeRestorer] = {}

source/audio/utils.py
@@ -1,0 +1,190 @@
+# A part of NonVisual Desktop Access (NVDA)
+# Copyright (C) 2024 NV Access Limited
+# This file is covered by the GNU General Public License.
+# See the file COPYING for more details.
+
+from logHandler import log
+from pycaw.api.audiopolicy import IAudioSessionManager2
+from pycaw.callbacks import AudioSessionNotification, AudioSessionEvents
+from pycaw.utils import AudioSession, AudioUtilities
+from dataclasses import dataclass, field
+from comtypes import COMError
+from threading import Lock
+from pycaw.api.audiopolicy import IAudioSessionControl2
+import weakref
+import globalVars
+import os
+
+
+_audioSessionManager: IAudioSessionManager2 | None = None
+
+
+def initialize() -> None:
+	global _audioSessionManager
+	try:
+		_audioSessionManager = AudioUtilities.GetAudioSessionManager()
+	except COMError:
+		log.exception("Could not initialize audio session manager")
+		return
+
+
+def terminate():
+	global _audioSessionManager
+	_audioSessionManager = None
+
+
+class _AudioSessionEventsListener(AudioSessionEvents):
+	"""
+	This class is a listener for audio session termination event. It is registered in WASAPI by
+	`_AudioSessionNotificationListener.on_session_created()` method. It calls custom logic defined in
+	`AudioSessionCallback.onSessionTerminated()` implementation and allows customers to restore audio volume.
+	"""
+
+	callback: "weakref.ReferenceType[AudioSessionCallback]"
+	pid: int
+	audioSession: AudioSession
+
+	def __init__(self, callback: "AudioSessionCallback", pid: int, audioSession: AudioSession):
+		self.callback = weakref.ref(callback)
+		self.pid = pid
+		self.audioSession = audioSession
+
+	def on_state_changed(self, new_state: str, new_state_id: int):
+		if new_state == "Expired":
+			self.onSessionTerminated()
+
+	def onSessionTerminated(self):
+		log.debug(f"Audio session for pid {self.pid} terminated")
+		try:
+			self.callback().onSessionTerminated(self.audioSession)
+		finally:
+			self.unregister()
+		with self.callback()._lock:
+			self.callback()._audioSessionEventListeners.remove(self)
+
+	def unregister(self):
+		try:
+			self.audioSession.unregister_notification()
+		except Exception:
+			log.exception(f"Cannot unregister audio session for process {self.pid}")
+
+
+class _AudioSessionNotificationListener(AudioSessionNotification):
+	"""
+	This class is a handler for existing and newly created audio sessions. Its method `on_session_created`
+	will be called from `AudioSessionCallback.register()` for all existing audio sessions; and, additionally,
+	it will be notified by WASAPI for every new audio session created.
+	It sets up a callback for session termination - an instance of `_AudioSessionEventsListener` class;
+	then it calls custom logic provided in `AudioSessionCallback.onSessionUpdate()` implementation.
+	"""
+
+	callback: "weakref.ReferenceType[AudioSessionCallback]"
+
+	def __init__(self, callback: "AudioSessionCallback"):
+		self.callback = weakref.ref(callback)
+
+	def on_session_created(self, new_session: AudioSession):
+		pid = new_session.ProcessId
+		if pid != globalVars.appPid:
+			process = new_session.Process
+			if process is not None:
+				exe = os.path.basename(process.exe())
+				isNvda = exe.lower() == "nvda.exe"
+				if isNvda:
+					# This process must be NVDA running on secure screen.
+					# We shouldn't change volume of such process.
+					return
+		audioSessionEventsListener = _AudioSessionEventsListener(self.callback(), pid, new_session)
+		new_session.register_notification(audioSessionEventsListener)
+		with self.callback()._lock:
+			self.callback()._audioSessionEventListeners.add(audioSessionEventsListener)
+		self.callback().onSessionUpdate(new_session)
+
+
+class DummyAudioSessionCallback:
+	def register(self, applyToFuture: bool = True):
+		pass
+
+	def unregister(self, runTerminators: bool = True):
+		pass
+
+
+@dataclass(unsafe_hash=True)
+class AudioSessionCallback(DummyAudioSessionCallback):
+	"""
+	This is an abstract class that allows implementing custom logic, that will be applied to all WASAPI
+	audio sessions. Consumers are expected to implement functions:
+	* def onSessionUpdate(self, session: AudioSession):
+		It will be called once for every existing audio session and also will be scheduled to be called
+		for every new audio session.
+	* def onSessionTerminated(self, session: AudioSession):
+		It will be called when an audio session is terminated or when unregister() is called, which
+		typically happens when NVDA quits.
+	"""
+
+	_lock: Lock = Lock()
+	_audioSessionNotification: AudioSessionNotification | None = None
+	_audioSessionEventListeners: set[_AudioSessionEventsListener] = field(default_factory=set)
+
+	def onSessionUpdate(self, session: AudioSession) -> None:
+		"""
+		Callback function to be implemented by the customer.
+		Will be called for all existing sessions and newly created sessions.
+		"""
+		pass
+
+	def onSessionTerminated(self, session: AudioSession) -> None:
+		"""
+		Callback function to be implemented by the customer.
+		Will be called when each session is terminated or when unregister() method is called.
+		"""
+		pass
+
+	def register(self, applyToFuture: bool = True):
+		"""
+		Registers this callback.
+		This will internally call onSessionUpdate() for all current audio sessions.
+		Additionally if applyToFuture is True, it will also call onSessionUpdate() for all newly created sessions
+		until this callback is unregistered.
+		"""
+		_applyToAllAudioSessions(self, applyToFuture)
+
+	def unregister(self, runTerminators: bool = True):
+		"""
+		Unregisters this callback.
+		If runTerminators is True, it will also trigger onSessionTerminated for all current audio sessions.
+		"""
+		if self._audioSessionNotification is not None:
+			_audioSessionManager.UnregisterSessionNotification(self._audioSessionNotification)
+		with self._lock:
+			listenersCopy = list(self._audioSessionEventListeners)
+		for audioSessionEventListener in listenersCopy:
+			if runTerminators:
+				audioSessionEventListener.on_state_changed("Expired", 0)
+			else:
+				audioSessionEventListener.audioSession.unregister_notification()
+
+
+def _applyToAllAudioSessions(
+	callback: AudioSessionCallback,
+	applyToFuture: bool = True,
+) -> None:
+	"""
+	Executes provided callback function on all active audio sessions.
+	Additionally, if applyToFuture is True, then it will register a notification with audio session manager,
+	which will execute the same callback for all future sessions as they are created.
+	"""
+	listener = _AudioSessionNotificationListener(callback)
+	if applyToFuture:
+		_audioSessionManager.RegisterSessionNotification(listener)
+		callback._audioSessionNotification = listener
+	sessionEnumerator = _audioSessionManager.GetSessionEnumerator()
+	count = sessionEnumerator.GetCount()
+	for i in range(count):
+		ctl = sessionEnumerator.GetSession(i)
+		if ctl is None:
+			continue
+		ctl2 = ctl.QueryInterface(IAudioSessionControl2)
+		if ctl2 is not None:
+			audioSession = AudioSession(ctl2)
+			listener.on_session_created(audioSession)

source/config/configSpec.py
@@ -63,6 +63,9 @@ schemaVersion = integer(min=0, default={latestSchemaVersion})
 	whiteNoiseVolume = integer(default=0, min=0, max=100)
 	soundSplitState = integer(default=0)
 	includedSoundSplitModes = int_list(default=list(0, 2, 3))
+	applicationsSoundVolume = integer(default=100, min=0, max=100)
+	applicationsSoundMuted = boolean(default=False)
+	applicationsVolumeMode = featureFlag(optionsEnum="AppsVolumeAdjusterFlag", behaviorOfDefault="DISABLED")
 
 # Braille settings
 [braille]

commit b8cf4cff2a1a43c694ffbed95377d6111175bcdf

Unbind application volume adjustment gestures (#17634)
Closes #17272

Summary of the issue:
The application volume adjuster feature introduced in #16591 includes default keyboard gestures that:

1. Conflict with the default gestures used by NVDA Remote; and
2. Don't do anything by default, as the feature is disabled.

Description of user facing changes
These gestures are unbound by default. Users can still bind gestures to these commands from the input gestures dialog.

Description of development approach
Removed the `gesture` argument to the `script` decorator on `GlobalCommands.script_increaseApplicationsVolume`, `GlobalCommands.script_decreaseApplicationsVolume`, and `GlobalCommands.script_toggleApplicationsMute`.

Testing strategy:
Ran NVDA from source. Ensured that the commands were unbound by executing the previous keyboard gestures.
Re-bound the gestures in the input gestures dialog, and ensured they worked as expected.

Known issues with pull request:
None.

source/globalCommands.py
@@ -4832,7 +4832,6 @@ class GlobalCommands(ScriptableObject):
 			"Increases the volume of other applications",
 		),
 		category=SCRCAT_AUDIO,
-		gesture="kb:NVDA+alt+pageUp",
 	)
 	def script_increaseApplicationsVolume(self, gesture: "inputCore.InputGesture") -> None:
 		appsVolume._adjustAppsVolume(5)
@@ -4843,7 +4842,6 @@ class GlobalCommands(ScriptableObject):
 			"Decreases the volume of other applications",
 		),
 		category=SCRCAT_AUDIO,
-		gesture="kb:NVDA+alt+pageDown",
 	)
 	def script_decreaseApplicationsVolume(self, gesture: "inputCore.InputGesture") -> None:
 		appsVolume._adjustAppsVolume(-5)
@@ -4864,7 +4862,6 @@ class GlobalCommands(ScriptableObject):
 			"Mutes or unmutes other applications",
 		),
 		category=SCRCAT_AUDIO,
-		gesture="kb:NVDA+alt+delete",
 	)
 	def script_toggleApplicationsMute(self, gesture: "inputCore.InputGesture") -> None:
 		appsVolume._toggleAppsVolumeMute()