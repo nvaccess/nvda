2aae60f8f7152f71c1118198ae196278a65efc0f
Sean Budd <sean@nvaccess.org>
2025-02-10 13:21:20 +1100

    Add cppcheck to AppVeyor

--- appveyor/scripts/tests/lintCheck.ps1
+++ appveyor/scripts/tests/lintCheck.ps1
@@ -10,3 +10,13 @@ Push-AppveyorArtifact "$lintOutput/PR-lint.xml"
 Push-AppveyorArtifact "$lintOutput/lint-diff.diff"
 $wc = New-Object 'System.Net.WebClient'
 $wc.UploadFile("https://ci.appveyor.com/api/testresults/junit/$($env:APPVEYOR_JOB_ID)", "$lintOutput/PR-lint.xml")
+
+$cppcheckOutput = "$lintOutput\cppcheck.xml"
+cppcheck --xml --enable=portability --check-level=exhaustive nvdaHelper 2> "$cppcheckOutput"
+if ($LastExitCode -ne 0) {
+	Set-AppveyorBuildVariable "testFailExitCode" $LastExitCode
+	Add-AppveyorMessage "FAIL: CPP check. See $cppcheckOutput for more information."
+} else {
+	Add-AppveyorMessage "PASS: CPP check."
+}
+Push-AppveyorArtifact $cppcheckOutput
