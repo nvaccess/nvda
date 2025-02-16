nvaccess/nvda

commit 1dc0f762834c02e40bf46248bfa1aa4b38762aa1

Add leading silence detection and removal logic (#17648)
Closes #17614

Summary of the issue:
Some voices output a leading silence part before the actual speech voice. By removing the silence part, the delay between keypress and user hearing the audio will be shorter, therefore make the voices more responsive.

Description of user facing changes
Users may find the voices more responsive. All voices using NVDA's WavePlayer will be affected, including eSpeak-NG, OneCore, SAPI5, and some third-party voice add-ons.

This should only affect the leading silence parts. Silence between sentences or at punctuation marks are not changed, but this may depend on how the voice uses WavePlayer.

Description of development approach
I wrote a header-only library silenceDetect.h in nvdaHelper/local. It supports most wave formats (8/16/24/32-bit integer and 32/64-bit float wave), and uses a simple algorithm: check each sample to see if it's outside threshold range (currently hard-coded to +/- 1/2^10 or 0.0009765625). It uses template-related code and requires C++ 20 standard.

The WasapiPlayer in wasapi.cpp is updated to handle silence. A new member function, startTrimmingLeadingSilence, and the exported version wasPlay_startTrimmingLeadingSilence, is added, to set or clear the isTrimmingLeadingSilence flag. If isTrimmingLeadingSilence is true, the next chunk fed in will have its leading silence removed. When non-silence is detected, isTrimmingLeadingSilence will be reset to false. So every time a new utterance is about to be spoken, startTrimmingLeadingSilence should be called.

In nvwave.py, startTrimmingLeadingSilence() will be called when:

the player is initialized;
the player is stopped;
idle is called;
_idleCheck determines that the player is idle.

nvdaHelper/local/wasapi.cpp
@@ -24,6 +24,7 @@ http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 #include <mmdeviceapi.h>
 #include <common/log.h>
 #include <random>
+#include "silenceDetect.h"
 
 /**
  * Support for audio playback using WASAPI.
@@ -194,6 +195,8 @@ class WasapiPlayer {
 	HRESULT resume();
 	HRESULT setChannelVolume(unsigned int channel, float level);
 
+	void startTrimmingLeadingSilence(bool start);
+
 	private:
 	void maybeFireCallback();
 
@@ -245,6 +248,7 @@ class WasapiPlayer {
 	unsigned int defaultDeviceChangeCount;
 	unsigned int deviceStateChangeCount;
 	bool isUsingPreferredDevice = false;
+	bool isTrimmingLeadingSilence = false;
 };
 
 WasapiPlayer::WasapiPlayer(wchar_t* endpointId, WAVEFORMATEX format,
@@ -341,6 +345,19 @@ HRESULT WasapiPlayer::feed(unsigned char* data, unsigned int size,
 		sentFrames = 0;
 		return true;
 	};
+
+	if (isTrimmingLeadingSilence) {
+		size_t silenceSize = SilenceDetect::getLeadingSilenceSize(&format, data, size);
+		if (silenceSize >= size) {
+			// The whole chunk is silence. Continue checking for silence in the next chunk.
+			remainingFrames = 0;
+		} else {
+			// Silence ends in this chunk. Skip the silence and continue.
+			data += silenceSize;
+			remainingFrames = (size - silenceSize) / format.nBlockAlign;
+			isTrimmingLeadingSilence = false;  // Stop checking for silence
+		}
+	}
 
 	while (remainingFrames > 0) {
 		UINT32 paddingFrames;
@@ -643,6 +660,10 @@ HRESULT WasapiPlayer::setChannelVolume(unsigned int channel, float level) {
 	return volume->SetChannelVolume(channel, level);
 }
 
+void WasapiPlayer::startTrimmingLeadingSilence(bool start) {
+	isTrimmingLeadingSilence = start;
+}
+
 HRESULT WasapiPlayer::disableCommunicationDucking(IMMDevice* device) {
 	// Disable the default ducking experience used when a communication audio
 	// session is active, as we never want NVDA's audio to be ducked.
@@ -837,6 +858,10 @@ HRESULT wasPlay_setChannelVolume(
 	float level
 ) {
 	return player->setChannelVolume(channel, level);
+}
+
+void wasPlay_startTrimmingLeadingSilence(WasapiPlayer* player, bool start) {
+	player->startTrimmingLeadingSilence(start);
 }
 
 /**

source/nvwave.py
@@ -39,6 +39,9 @@ import NVDAHelper
 import core
 import globalVars
 from pycaw.utils import AudioUtilities
+from speech import SpeechSequence
+from speech.commands import BreakCommand
+from synthDriverHandler import pre_synthSpeak
 
 from utils.mmdevice import _getOutputDevices
 
@@ -289,6 +292,14 @@ class WasapiWavePlayer(garbageHandler.TrackedObject):
 			if config.conf["audio"]["audioAwakeTime"] > 0:
 				NVDAHelper.localLib.wasSilence_init(outputDevice)
 				WasapiWavePlayer._silenceDevice = outputDevice
+		# Enable trimming by default for speech only
+		self.enableTrimmingLeadingSilence(
+			purpose is AudioPurpose.SPEECH and config.conf["speech"]["trimLeadingSilence"],
+		)
+		if self._enableTrimmingLeadingSilence:
+			self.startTrimmingLeadingSilence()
+		self._isLeadingSilenceInserted: bool = False
+		pre_synthSpeak.register(self._onPreSpeak)
 
 	@wasPlay_callback
 	def _callback(cppPlayer, feedId):
@@ -313,6 +324,7 @@ class WasapiWavePlayer(garbageHandler.TrackedObject):
 			# a weakref callback can run before __del__ in some cases, which would mean
 			# it has already been removed from _instances.
 			self._player = None
+		pre_synthSpeak.unregister(self._onPreSpeak)
 
 	def open(self):
 		"""Open the output device.
@@ -323,7 +335,7 @@ class WasapiWavePlayer(garbageHandler.TrackedObject):
 			NVDAHelper.localLib.wasPlay_open(self._player)
 		except WindowsError:
 			log.warning(
-				"Couldn't open specified or default audio device. " "There may be no audio devices.",
+				"Couldn't open specified or default audio device. There may be no audio devices.",
 			)
 			WavePlayer.audioDeviceError_static = True
 			raise
@@ -357,6 +369,10 @@ class WasapiWavePlayer(garbageHandler.TrackedObject):
 		feedId = c_uint() if onDone else None
 		# Never treat this instance as idle while we're feeding.
 		self._lastActiveTime = None
+		# If a BreakCommand is used to insert leading silence in this utterance,
+		# turn off trimming temporarily.
+		if self._purpose is AudioPurpose.SPEECH and self._isLeadingSilenceInserted:
+			self.startTrimmingLeadingSilence(False)
 		try:
 			NVDAHelper.localLib.wasPlay_feed(
 				self._player,
@@ -393,6 +409,8 @@ class WasapiWavePlayer(garbageHandler.TrackedObject):
 	def idle(self):
 		"""Indicate that this player is now idle; i.e. the current continuous segment  of audio is complete."""
 		self.sync()
+		if self._enableTrimmingLeadingSilence:
+			self.startTrimmingLeadingSilence()
 		if self._audioDucker:
 			self._audioDucker.disable()
 
@@ -401,6 +419,8 @@ class WasapiWavePlayer(garbageHandler.TrackedObject):
 		if self._audioDucker:
 			self._audioDucker.disable()
 		NVDAHelper.localLib.wasPlay_stop(self._player)
+		if self._enableTrimmingLeadingSilence:
+			self.startTrimmingLeadingSilence()
 		self._lastActiveTime = None
 		self._isPaused = False
 		self._doneCallbacks = {}
@@ -455,6 +475,17 @@ class WasapiWavePlayer(garbageHandler.TrackedObject):
 			if not (all and e.winerror == E_INVALIDARG):
 				raise
 
+	def enableTrimmingLeadingSilence(self, enable: bool) -> None:
+		"""Enable or disable automatic leading silence removal.
+		This is by default enabled for speech audio, and disabled for non-speech audio."""
+		self._enableTrimmingLeadingSilence = enable
+		if not enable:
+			self.startTrimmingLeadingSilence(False)
+
+	def startTrimmingLeadingSilence(self, start: bool = True) -> None:
+		"""Start or stop trimming the leading silence from the next audio chunk."""
+		NVDAHelper.localLib.wasPlay_startTrimmingLeadingSilence(self._player, start)
+
 	def _setVolumeFromConfig(self):
 		if self._purpose is not AudioPurpose.SOUNDS:
 			return
@@ -508,6 +539,7 @@ class WasapiWavePlayer(garbageHandler.TrackedObject):
 			if player._lastActiveTime <= threshold:
 				try:
 					NVDAHelper.localLib.wasPlay_idle(player._player)
+					player.startTrimmingLeadingSilence()
 				except OSError:
 					# #16125: IAudioClock::GetPosition sometimes fails with an access
 					# violation on a device which has been invalidated. This shouldn't happen
@@ -523,6 +555,16 @@ class WasapiWavePlayer(garbageHandler.TrackedObject):
 			# There's still at least one active stream that wasn't idle.
 			# Schedule another check here in case feed isn't called for a while.
 			cls._scheduleIdleCheck()
+
+	def _onPreSpeak(self, speechSequence: SpeechSequence):
+		self._isLeadingSilenceInserted = False
+		# Check if leading silence of the current utterance is inserted by a BreakCommand.
+		for item in speechSequence:
+			if isinstance(item, BreakCommand):
+				self._isLeadingSilenceInserted = True
+				break
+			elif isinstance(item, str):
+				break
 
 
 WavePlayer = WasapiWavePlayer

nvdaHelper/local/nvdaHelperLocal.def
@@ -82,6 +82,7 @@ EXPORTS
 	wasPlay_pause
 	wasPlay_resume
 	wasPlay_setChannelVolume
+	wasPlay_startTrimmingLeadingSilence
 	wasPlay_startup
 	wasSilence_init
 	wasSilence_playFor

nvdaHelper/local/silenceDetect.h
@@ -1,0 +1,219 @@
+// A part of NonVisual Desktop Access (NVDA)
+// This file is covered by the GNU General Public License.
+// See the file COPYING for more details.
+// Copyright (C) 2025 NV Access Limited, gexgd0419
+
+#ifndef SILENCEDETECT_H
+#define SILENCEDETECT_H
+
+#include <windows.h>
+#include <mmreg.h>
+#include <stdint.h>
+#include <type_traits>
+#include <limits>
+
+namespace SilenceDetect {
+
+/**
+ * Compile-time wave format tag.
+ * Supports integer and floating-point formats.
+ * `SampleType` should be the smallest numeric type that can hold a sample, for example, 32-bit int for 24-bit format.
+ * Signedness of `SampleType` matters. For unsigned types, the zero point is at middle, e.g. 128 for 8-bit unsigned.
+ * `bytesPerSample` should be <= `sizeof(SampleType)` for integer formats,
+ * and == `sizeof(SampleType)` for floating-point formats.
+ * Assumes C++20 standard.
+ */
+template <typename SampleType, size_t bytesPerSample = sizeof(SampleType)>
+struct WaveFormat {
+	static_assert(std::is_arithmetic_v<SampleType>, "SampleType should be an integer or floating-point type");
+	static_assert(!(std::is_floating_point_v<SampleType> && bytesPerSample != sizeof(SampleType)),
+		"When SampleType is a floating-point type, bytesPerSample should be equal to sizeof(SampleType)");
+	static_assert(!(std::is_integral_v<SampleType> && !(bytesPerSample <= sizeof(SampleType) && bytesPerSample > 0)),
+		"When SampleType is an integer type, bytesPerSample should be less than or equal to sizeof(SampleType) and greater than 0");
+
+	typedef SampleType SampleType;
+	static constexpr size_t bytesPerSample = bytesPerSample;
+
+	static constexpr SampleType zeroPoint() {
+		// for unsigned types, zero point is at middle
+		// for signed types, zero is zero
+		if constexpr (std::is_unsigned_v<SampleType>)
+			return SampleType(1) << (bytesPerSample * 8 - 1);
+		else
+			return SampleType();
+	}
+
+	static constexpr SampleType (max)() {
+		if constexpr (std::is_floating_point_v<SampleType>) {
+			// For floating-point samples, maximum value is 1.0
+			return SampleType(1);
+		} else {
+			// Trim the maximum value to `bytesPerSample` bytes
+			return (std::numeric_limits<SampleType>::max)() >> ((sizeof(SampleType) - bytesPerSample) * 8);
+		}
+	}
+
+	static constexpr SampleType (min)() {
+		if constexpr (std::is_floating_point_v<SampleType>) {
+			// For floating-point samples, minimum value is -1.0
+			return SampleType(-1);
+		} else {
+			// Trim the minimum value to `bytesPerSample` bytes
+			return (std::numeric_limits<SampleType>::min)() >> ((sizeof(SampleType) - bytesPerSample) * 8);
+		}
+	}
+
+	static constexpr SampleType defaultThreshold() {
+		// Default threshold: 1 / 2^10 or 0.0009765625
+		if constexpr (std::is_floating_point_v<SampleType>)
+			return SampleType(1) / (1 << 10);
+		else if constexpr (bytesPerSample * 8 > 10)
+			return SampleType(1) << (bytesPerSample * 8 - 10);
+		else
+			return SampleType();
+	}
+
+	static constexpr auto toSigned(SampleType smp) {
+		if constexpr (std::is_integral_v<SampleType>) {
+			// In C++20, signed integer types must use two's complement,
+			// so the following conversion is well-defined.
+			using SignedType = std::make_signed_t<SampleType>;
+			return SignedType(smp - zeroPoint());
+		} else {
+			return smp;
+		}
+	}
+
+	static constexpr SampleType fromSigned(SampleType smp) {
+		if constexpr (std::is_integral_v<SampleType>) {
+			// Signed overflow is undefined behavior,
+			// so convert to unsigned first.
+			using UnsignedType = std::make_unsigned_t<SampleType>;
+			return SampleType(UnsignedType(smp) + zeroPoint());
+		} else {
+			return smp;
+		}
+	}
+
+	static constexpr SampleType signExtend(SampleType smp) {
+		if constexpr (std::is_unsigned_v<SampleType> || bytesPerSample == sizeof(SampleType)) {
+			return smp;
+		} else {
+			constexpr auto shift = (sizeof(SampleType) - bytesPerSample) * 8;
+			// Convert to unsigned first to prevent left-shifting negative numbers
+			using UnsignedType = std::make_unsigned_t<SampleType>;
+			return SampleType(UnsignedType(smp) << shift) >> shift;
+		}
+	}
+};
+
+inline WORD getFormatTag(const WAVEFORMATEX* wfx) {
+	if (wfx->wFormatTag == WAVE_FORMAT_EXTENSIBLE) {
+		auto wfext = reinterpret_cast<const WAVEFORMATEXTENSIBLE*>(wfx);
+		if (IS_VALID_WAVEFORMATEX_GUID(&wfext->SubFormat))
+			return EXTRACT_WAVEFORMATEX_ID(&wfext->SubFormat);
+	}
+	return wfx->wFormatTag;
+}
+
+/**
+ * Return the leading silence wave data length, in bytes.
+ * Assumes the wave data to be of one channel (mono).
+ * Uses a `WaveFormat` type (`Fmt`) to determine the wave format.
+ */
+template <class Fmt>
+size_t getLeadingSilenceSizeMono(
+	const unsigned char* waveData,
+	size_t size,
+	typename Fmt::SampleType threshold
+) {
+	using SampleType = Fmt::SampleType;
+	constexpr size_t bytesPerSample = Fmt::bytesPerSample;
+
+	if (size < bytesPerSample)
+		return 0;
+
+	constexpr SampleType zeroPoint = Fmt::zeroPoint();
+	const SampleType minValue = zeroPoint - threshold, maxValue = zeroPoint + threshold;
+
+	// Check each sample
+	SampleType smp = SampleType();
+	const unsigned char* const pEnd = waveData + (size - (size % bytesPerSample));
+	for (const unsigned char* p = waveData; p < pEnd; p += bytesPerSample) {
+		memcpy(&smp, p, bytesPerSample);
+		smp = Fmt::signExtend(smp);
+		// this sample is out of range, so the previous sample is the final sample of leading silence.
+		if (smp < minValue || smp > maxValue)
+			return p - waveData;
+	}
+
+	// The whole data block is silence
+	return size;
+}
+
+/**
+ * Invoke a functor with an argument of a WaveFormat type that corresponds to the specified WAVEFORMATEX.
+ * Return false if the WAVEFORMATEX is unknown.
+ */
+template <class Func>
+bool callByWaveFormat(const WAVEFORMATEX* wfx, Func&& func) {
+	switch (getFormatTag(wfx)) {
+	case WAVE_FORMAT_PCM:
+		switch (wfx->wBitsPerSample) {
+		case 8:  // 8-bits are unsigned, others are signed
+			func(WaveFormat<uint8_t>());
+			break;
+		case 16:
+			func(WaveFormat<int16_t>());
+			break;
+		case 24:
+			func(WaveFormat<int32_t, 3>());
+			break;
+		case 32:
+			func(WaveFormat<int32_t>());
+			break;
+		default:
+			return false;
+		}
+		break;
+	case WAVE_FORMAT_IEEE_FLOAT:
+		switch (wfx->wBitsPerSample) {
+		case 32:
+			func(WaveFormat<float>());
+			break;
+		case 64:
+			func(WaveFormat<double>());
+			break;
+		default:
+			return false;
+		}
+		break;
+	default:
+		return false;
+	}
+	return true;
+}
+
+/**
+ * Return the leading silence wave data length, in bytes.
+ * Uses a `WAVEFORMATEX` to determine the wave format.
+ */
+inline size_t getLeadingSilenceSize(
+	const WAVEFORMATEX* wfx,
+	const unsigned char* waveData,
+	size_t size
+) {
+	size_t len;
+	if (!callByWaveFormat(wfx, [=, &len](auto fmtTag) {
+			using Fmt = decltype(fmtTag);
+			len = getLeadingSilenceSizeMono<Fmt>(
+				waveData, size, Fmt::defaultThreshold());
+		}))
+		return 0;
+
+	return len - len % wfx->nBlockAlign;  // round down to block (channel) boundaries
+}
+
+}  // namespace SilenceDetect
+
+#endif  // SILENCEDETECT_H

source/config/configSpec.py
@@ -45,6 +45,7 @@ schemaVersion = integer(min=0, default={latestSchemaVersion})
 	autoDialectSwitching = boolean(default=false)
 	delayedCharacterDescriptions = boolean(default=false)
 	excludedSpeechModes = int_list(default=list())
+	trimLeadingSilence = boolean(default=true)
 
 	[[__many__]]
 		capPitchChange = integer(default=30,min=-100,max=100)

source/gui/settingsDialogs.py
@@ -3737,6 +3737,7 @@ class AdvancedPanelControls(
 		#  Advanced settings panel
 		label = _("Speech")
 		speechSizer = wx.StaticBoxSizer(wx.VERTICAL, self, label=label)
+		speechBox = speechSizer.GetStaticBox()
 		speechGroup = guiHelper.BoxSizerHelper(speechSizer, sizer=speechSizer)
 		sHelper.addItem(speechGroup)
 
@@ -3766,6 +3767,14 @@ class AdvancedPanelControls(
 		self.cancelExpiredFocusSpeechCombo.defaultValue = self._getDefaultValue(
 			["featureFlag", "cancelExpiredFocusSpeech"],
 		)
+
+		# Translators: This is the label for a checkbox control in the
+		#  Advanced settings panel.
+		label = _("Trim leading silence in speech audio")
+		self.trimLeadingSilenceCheckBox = speechGroup.addItem(wx.CheckBox(speechBox, label=label))
+		self.bindHelpEvent("TrimLeadingSilenceSpeech", self.trimLeadingSilenceCheckBox)
+		self.trimLeadingSilenceCheckBox.SetValue(config.conf["speech"]["trimLeadingSilence"])
+		self.trimLeadingSilenceCheckBox.defaultValue = self._getDefaultValue(["speech", "trimLeadingSilence"])
 
 		# Translators: This is the label for a group of advanced options in the
 		#  Advanced settings panel
@@ -3951,6 +3960,7 @@ class AdvancedPanelControls(
 			and self.wtStrategyCombo.isValueConfigSpecDefault()
 			and self.cancelExpiredFocusSpeechCombo.GetSelection()
 			== self.cancelExpiredFocusSpeechCombo.defaultValue
+			and self.trimLeadingSilenceCheckBox.IsChecked() == self.trimLeadingSilenceCheckBox.defaultValue
 			and self.loadChromeVBufWhenBusyCombo.isValueConfigSpecDefault()
 			and self.caretMoveTimeoutSpinControl.GetValue() == self.caretMoveTimeoutSpinControl.defaultValue
 			and self.reportTransparentColorCheckBox.GetValue()
@@ -3979,6 +3989,7 @@ class AdvancedPanelControls(
 		self.diffAlgoCombo.SetSelection(self.diffAlgoCombo.defaultValue)
 		self.wtStrategyCombo.resetToConfigSpecDefault()
 		self.cancelExpiredFocusSpeechCombo.SetSelection(self.cancelExpiredFocusSpeechCombo.defaultValue)
+		self.trimLeadingSilenceCheckBox.SetValue(self.trimLeadingSilenceCheckBox.defaultValue)
 		self.loadChromeVBufWhenBusyCombo.resetToConfigSpecDefault()
 		self.caretMoveTimeoutSpinControl.SetValue(self.caretMoveTimeoutSpinControl.defaultValue)
 		self.reportTransparentColorCheckBox.SetValue(self.reportTransparentColorCheckBox.defaultValue)
@@ -3989,6 +4000,14 @@ class AdvancedPanelControls(
 
 	def onSave(self):
 		log.debug("Saving advanced config")
+
+		if config.conf["speech"]["trimLeadingSilence"] != self.trimLeadingSilenceCheckBox.IsChecked():
+			# Reload the synthesizer if "trimLeadingSilence" changes
+			config.conf["speech"]["trimLeadingSilence"] = self.trimLeadingSilenceCheckBox.IsChecked()
+			currentSynth = getSynth()
+			if not setSynth(currentSynth.name):
+				_synthWarningDialog(currentSynth.name)
+
 		config.conf["development"]["enableScratchpadDir"] = self.scratchpadCheckBox.IsChecked()
 		selectiveUIAEventRegistrationChoice = self.selectiveUIAEventRegistrationCombo.GetSelection()
 		config.conf["UIA"]["eventRegistration"] = self.selectiveUIAEventRegistrationVals[

source/speech/manager.py
@@ -22,7 +22,7 @@ from .commands import (
 
 from .priorities import Spri, SPEECH_PRIORITIES
 from logHandler import log
-from synthDriverHandler import getSynth
+from synthDriverHandler import getSynth, pre_synthSpeak
 from typing import (
 	Dict,
 	Any,
@@ -431,6 +431,7 @@ class SpeechManager(object):
 					self._indexesSpeaking.append(item.index)
 			self._cancelledLastSpeechWithSynth = False
 			log._speechManagerUnitTest(f"Synth Gets: {seq}")
+			pre_synthSpeak.notify(speechSequence=seq)
 			getSynth().speak(seq)
 
 	def _getNextPriority(self):

source/synthDriverHandler.py
@@ -567,3 +567,11 @@ The local system should be notified about synth parameters at the remote system.
 @param isFallback: Whether the synth is set as fallback synth due to another synth's failure
 @type isFallback: bool
 """
+
+pre_synthSpeak = extensionPoints.Action()
+"""
+Notifies when speak() of the current synthesizer is about to be called.
+
+:param speechSequence: the speech sequence to pass to speak()
+:type speechSequence: speech.SpeechSequence
+"""

commit 9bac9ebb4843b534713c379952674d309f1cc065

Fix leading silence removal feature (#17699)
None. Fixes problems introduced by PR #17648.

Summary of the issue:
The leading silence trimming feature should only trim the silence part of the audio. However, a longer part of the speech audio ended up being chopped off, which makes distinguishing the consonants at the beginning more difficult.

In addition, trimming is incorrectly applied to non-speech audio, and applied even when the feature is turned off in Settings.

Description of user facing changes
The problem mentioned above should be fixed.

Description of development approach
Added missing check for _enableTrimmingLeadingSilence in _idleCheck.

Before sending the trimmed audio to the device, one silent sample will be inserted at the beginning. For some reason, if the first sample is not zero, the audio may be chopped off at the beginning. Although the exact cause is still unknown, prepending one silent sample seems to fix this issue. See this discussion.

nvdaHelper/local/wasapi.cpp
@@ -346,16 +346,24 @@ HRESULT WasapiPlayer::feed(unsigned char* data, unsigned int size,
 		return true;
 	};
 
+	bool shouldInsertSilentFrame = false;
 	if (isTrimmingLeadingSilence) {
-		size_t silenceSize = SilenceDetect::getLeadingSilenceSize(&format, data, size);
+		// If data is null, treat the whole chunk as silence.
+		size_t silenceSize = data ? SilenceDetect::getLeadingSilenceSize(&format, data, size) : size;
 		if (silenceSize >= size) {
 			// The whole chunk is silence. Continue checking for silence in the next chunk.
 			remainingFrames = 0;
 		} else {
 			// Silence ends in this chunk. Skip the silence and continue.
 			data += silenceSize;
-			remainingFrames = (size - silenceSize) / format.nBlockAlign;
+			size -= silenceSize;
+			remainingFrames = size / format.nBlockAlign;
 			isTrimmingLeadingSilence = false;  // Stop checking for silence
+
+			// Signals to insert one silent frame before the trimmed audio in this chunk.
+			// Not doing so may cause the beginning of the audio to be chopped off.
+			// See: https://github.com/nvaccess/nvda/discussions/17697
+			shouldInsertSilentFrame = true;
 		}
 	}
 
@@ -407,13 +415,29 @@ HRESULT WasapiPlayer::feed(unsigned char* data, unsigned int size,
 			}
 		}
 		// We might have more frames than will fit in the buffer. Send what we can.
+		// If we need to insert a silent frame, the frame counts towards the total frame count,
+		// but does not count towards the total byte count, as it's not in the provided data buffer.
+		if (shouldInsertSilentFrame) {
+			remainingFrames++;
+		}
 		const UINT32 sendFrames = std::min(remainingFrames,
 			bufferFrames - paddingFrames);
-		const UINT32 sendBytes = sendFrames * format.nBlockAlign;
+		const UINT32 sendBytes = (sendFrames - (shouldInsertSilentFrame ? 1 : 0))
+			* format.nBlockAlign;
 		BYTE* buffer;
 		hr = render->GetBuffer(sendFrames, &buffer);
 		if (FAILED(hr)) {
 			return hr;
+		}
+		if (shouldInsertSilentFrame) {
+			// If needed, insert one frame of silence at the beginning
+			if (format.wFormatTag == WAVE_FORMAT_PCM && format.wBitsPerSample == 8) {
+				memset(buffer, 0x80, format.nBlockAlign);
+			} else {
+				memset(buffer, 0, format.nBlockAlign);
+			}
+			buffer += format.nBlockAlign;
+			shouldInsertSilentFrame = false;
 		}
 		if (data) {
 			memcpy(buffer, data, sendBytes);

source/nvwave.py
@@ -497,7 +497,8 @@ class WavePlayer(garbageHandler.TrackedObject):
 			if player._lastActiveTime <= threshold:
 				try:
 					NVDAHelper.localLib.wasPlay_idle(player._player)
-					player.startTrimmingLeadingSilence()
+					if player._enableTrimmingLeadingSilence:
+						player.startTrimmingLeadingSilence()
 				except OSError:
 					# #16125: IAudioClock::GetPosition sometimes fails with an access
 					# violation on a device which has been invalidated. This shouldn't happen