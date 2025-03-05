a22c22baa5165078cb4ef3774e72e37131103270
Sean Budd <seanbudd123@gmail.com>
2025-03-06 10:09:26 +1100

    Update source/brailleViewer/__init__.py

--- source/brailleViewer/__init__.py
+++ source/brailleViewer/__init__.py
@@ -2,6 +2,7 @@
 # Copyright (C) 2014-2025 NV Access Limited, Leonard de Ruijter
 # This file is covered by the GNU General Public License.
 # See the file COPYING for more details.
+
 import typing
 
 import gui
