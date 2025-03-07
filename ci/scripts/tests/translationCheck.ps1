5ceb477614f9b7662b6d0aca30d276788962e219
Noelia Ruiz Martínez <nrm1977@gmail.com>
2025-03-07 05:30:15 +0100

    Make checkPot script to fail on test failure

--- ci/scripts/tests/translationCheck.ps1
+++ ci/scripts/tests/translationCheck.ps1
@@ -1,3 +1,4 @@
+$ErrorActionPreference = "Stop";
 cmd.exe /c "scons checkPot $sconsArgs --all-cores"
 if($LastExitCode -ne 0) {
 	$message = "FAIL: Translation comments check. Translation comments missing or unexpectedly included. See build log for more information."
