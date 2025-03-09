570db0706079927c2dfdac5489ef8b6d3a249ee3
Noelia Ruiz Martínez <nrm1977@gmail.com>
2025-03-09 11:55:05 +0100

    Restore globalCommands, modified to force failure of checkPot

--- source/globalCommands.py
+++ source/globalCommands.py
@@ -1427,6 +1427,7 @@ class GlobalCommands(ScriptableObject):
 			if obj is not None and obj.locationText:
 				ui.message(obj.locationText)
 				return
+		# Translators: message when there is no location information
 		ui.message(_("No location information"))
 
 	@script(
