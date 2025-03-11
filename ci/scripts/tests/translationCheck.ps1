1edbeb03bae604b0042360cb9aa0564e977fac31
Noelia Ruiz Martínez <nrm1977@gmail.com>
2025-03-11 16:47:05 +0100

    Improve job summary

--- ci/scripts/tests/translationCheck.ps1
+++ ci/scripts/tests/translationCheck.ps1
@@ -1,7 +1,7 @@
-cmd.exe /c "scons checkPot $sconsArgs --all-cores"
+cmd.exe /c "scons checkPot $sconsArgs --all-cores" >> $env:GITHUB_STEP_SUMMARY
 if($LastExitCode -ne 0) {
-	$message = "FAIL: Translation comments check. Translation comments missing or unexpectedly included. See build log for more information."
-	echo "testFailExitCode=$LastExitCode" | Out-File -FilePath $Env:GITHUB_ENV -Encoding utf8 -Append
+	$message = "FAIL: Translation comments check. Translation comments missing or unexpectedly included."
+	Write-Output "testFailExitCode=$LastExitCode" | Out-File -FilePath $Env:GITHUB_ENV -Encoding utf8 -Append
 } else {
 	$message = "PASS: Translation comments check."
 }
