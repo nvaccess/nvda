5166c0ecbd3211c3a1dcf34353e2266f837e3a19
Sean Budd <sean@nvaccess.org>
2025-04-01 15:14:52 +1100

    fix sys tests

--- ci/scripts/installNVDA.ps1
+++ ci/scripts/installNVDA.ps1
@@ -1,18 +1,12 @@
 $errorCode=0
-$nvdaLauncherFile=".\output\nvda"
-if(!$env:release) {
-	$nvdaLauncherFile+="_snapshot"
-}
-$nvdaLauncherFile+="_${env:version}.exe"
-echo NVDALauncherFile: $NVDALauncherFile
 $outputDir=$(resolve-path .\testOutput)
 $installerLogFilePath="$outputDir\nvda_install.log"
-$installerProcess=start-process -FilePath "$nvdaLauncherFile" -ArgumentList "--install-silent --debug-logging --log-file $installerLogFilePath" -passthru
+$installerProcess=start-process -FilePath "$env:nvdaLauncherFile" -ArgumentList "--install-silent --debug-logging --log-file $installerLogFilePath" -passthru
 try {
 	$installerProcess | wait-process -Timeout 180 -ErrorAction Stop
 	$errorCode=$installerProcess.ExitCode
 } catch {
-	echo "NVDA installer process timed out"
+	Write-Output "NVDA installer process timed out"
 	$errorCode=1
 	# Since installer failed to exit in the specified timeout the log file is still in use.
 	# Unfortunately `Push-AppveyorArtifact` is unable to upload a file which is  locked
@@ -24,5 +18,4 @@ $crashDump = "$outputDir\nvda_crash.dmp"
 if (Test-Path -Path $crashDump){
 	$errorCode=1
 }
-if($errorCode -ne 0) { $host.SetShouldExit($errorCode) }
-echo "nvdaLauncherFile=$nvdaLauncherFile" | Out-File -FilePath $Env:GITHUB_ENV -Encoding utf8 -Append
+if ($errorCode -ne 0) { $host.SetShouldExit($errorCode) }
