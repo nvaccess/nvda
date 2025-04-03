a5a5ae3819486e76bb203ff756ac89bb822673bb
Sascha Cowley <16543535+SaschaCowley@users.noreply.github.com>
2025-04-03 19:15:02 +1100

    Add documentation and fix name

--- nvdaHelper/local/screenCurtain.cpp
+++ nvdaHelper/local/screenCurtain.cpp
@@ -11,7 +11,32 @@ using namespace Gdiplus;
 using namespace std;
 #pragma comment(lib, "Gdiplus.lib")
 
-bool captureScreen() {
+/**
+ * @brief Captures the entire virtual screen and determines if the screen is entirely black.
+ *
+ * This function captures the contents of the virtual screen, which is the bounding rectangle
+ * of all monitors on the system. It creates a device-independent bitmap (DIB) from the captured
+ * screen and calculates a histogram of colors to determine if the screen is entirely black.
+ *
+ * @return true if the screen is entirely black, false otherwise or if an error occurs.
+ *
+ * @details
+ * - The function retrieves the dimensions and origin of the virtual screen using `GetSystemMetrics`.
+ * - It creates a compatible device context and bitmap for capturing the screen.
+ * - The screen contents are copied into the compatible device context using `BitBlt`.
+ * - The captured bitmap is converted into a device-independent bitmap (DIB) using `GetDIBits`.
+ * - A GDI+ bitmap is created from the DIB, and a histogram of colors is calculated.
+ * - The function checks if the histogram indicates that the entire screen is black by verifying
+ *   that the only color present is black (0, 0, 0) and that the count of black pixels matches
+ *   the total screen area.
+ *
+ * @note
+ * - The function uses the GDI+ library for bitmap manipulation and histogram calculation.
+ * - Error handling is implemented to log and handle failures at various stages.
+ * - Per <https://learn.microsoft.com/en-us/windows/win32/gdi/the-virtual-screen>,
+ *   We assume that the screen coordinates and dimensions fit within 16-bit integers.
+ */
+bool isScreenFullyBlack() {
 	// The virtual screen is the bounding rectangle of all of the monitors on the system.
 	int screenWidth = GetSystemMetrics(SM_CXVIRTUALSCREEN),
 		screenHeight = GetSystemMetrics(SM_CYVIRTUALSCREEN),
@@ -152,99 +177,3 @@ bool captureScreen() {
 		return false;
 	}
 }
-/*
-bool oldCaptureScreen()
-{
-	// The virtual screen is the bounding rectangle of all of the monitors on the system.
-	int screenWidth = GetSystemMetrics(SM_CXVIRTUALSCREEN),
-		screenHeight = GetSystemMetrics(SM_CYVIRTUALSCREEN),
-		// While the primary monitor'sStatus top left corner is at the origin,
-		// it is not necessarily at the top left of the virtual screen.
-		// Thus, the top left of the virtual screen may be negative.
-		screenOriginX = GetSystemMetrics(SM_XVIRTUALSCREEN),
-		screenOriginY = GetSystemMetrics(SM_YVIRTUALSCREEN),
-		// Screen coordinates are 16-bit integers,
-		// and since 2^16 * 2^16 = 2^32,
-		// the area of the screen is guaranteed to fit in an int on all supported platforms.
-		screenArea = screenWidth * screenHeight;
-	LOG_INFO(L"Screen is " << screenWidth << L"x" << screenHeight << " = " << screenArea << "\n");
-	HWND desktopWnd = GetDesktopWindow();
-	HDC desktopDc = GetDC(desktopWnd),
-		captureDc = CreateCompatibleDC(desktopDc);
-	HBITMAP captureBitmap = CreateCompatibleBitmap(desktopDc, screenWidth, screenHeight);
-	BITMAP ddScreenshot;
-	DWORD screenshotSize = 0, bytesWritten = 0, dibSize = 0;
-	SelectObject(captureDc, captureBitmap);
-
-	// Replace the contents of captureDc with those of desktopDc
-	BitBlt(
-		// Destination device context
-		captureDc,
-		// Top left of destination
-		0, 0,
-		// Size of source and destination
-		screenWidth, screenHeight,
-		// Source device context
-		desktopDc,
-		// Top left of source
-		screenOriginX, screenOriginY,
-		// Raster operation to perform.
-		// In this case, replace destination with source.
-		SRCCOPY);
-
-	GetObject(captureBitmap, sizeof(BITMAP), &ddScreenshot);
-	BITMAPFILEHEADER bmpFileHeader;
-	BITMAPINFOHEADER bmpInfoHeader;
-	bmpInfoHeader.biSize = sizeof(BITMAPINFOHEADER);
-	bmpInfoHeader.biWidth = ddScreenshot.bmWidth;
-	bmpInfoHeader.biHeight = ddScreenshot.bmHeight;
-	bmpInfoHeader.biPlanes = 1;
-	bmpInfoHeader.biBitCount = 32;
-	bmpInfoHeader.biCompression = BI_RGB;
-	bmpInfoHeader.biSizeImage = 0;
-	bmpInfoHeader.biXPelsPerMeter = 0;
-	bmpInfoHeader.biYPelsPerMeter = 0;
-	bmpInfoHeader.biClrUsed = 0;
-	bmpInfoHeader.biClrImportant = 0;
-
-	screenshotSize = ((ddScreenshot.bmWidth * bmpInfoHeader.biBitCount + 31) / 32) * 4 * ddScreenshot.bmHeight;
-	dibSize = screenshotSize + sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER);
-	bmpFileHeader.bfOffBits = (DWORD)sizeof(BITMAPFILEHEADER) + (DWORD)sizeof(BITMAPINFOHEADER);
-	bmpFileHeader.bfSize = dibSize;
-	bmpFileHeader.bfType = 0x4D42; // BM
-
-	LPVOID buff = HeapAlloc(GetProcessHeap(), HEAP_GENERATE_EXCEPTIONS, screenshotSize);
-
-	GetDIBits(captureDc, captureBitmap, 0, (UINT)ddScreenshot.bmHeight, buff, (BITMAPINFO *)&bmpInfoHeader, DIB_RGB_COLORS);
-
-	IStream *stream = SHCreateMemStream(NULL, dibSize);
-
-	HANDLE hFile = CreateFile(L"ddScreenshot.bmp", GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-
-	WriteFile(hFile, (LPSTR)&bmpFileHeader, sizeof(bmpFileHeader), &bytesWritten, NULL);
-	stream->Write((LPSTR)&bmpFileHeader, sizeof(bmpFileHeader), &bytesWritten);
-	WriteFile(hFile, (LPSTR)&bmpInfoHeader, sizeof(bmpInfoHeader), &bytesWritten, NULL);
-	stream->Write((LPSTR)&bmpInfoHeader, sizeof(bmpInfoHeader), &bytesWritten);
-	WriteFile(hFile, (LPSTR)buff, screenshotSize, &bytesWritten, NULL);
-	stream->Write((LPSTR)buff, screenshotSize, &bytesWritten);
-
-	CloseHandle(hFile);
-
-	Bitmap image((BITMAPINFO *)&bmpInfoHeader, buff);
-	UINT histogramSize;
-	Status sStatus = image.GetHistogramSize(HistogramFormatARGB, &histogramSize);
-	UINT *histR = new UINT[histogramSize];
-	UINT *histG = new UINT[histogramSize];
-	UINT *histB = new UINT[histogramSize];
-
-	image.GetHistogram(HistogramFormatRGB, histogramSize, histR, histG, histB, NULL);
-	bool isBlack;
-	isBlack = (histR[0] == screenArea || histG[0] == screenArea || histB[0] == screenArea);
-
-	HeapFree(GetProcessHeap(), NULL, buff);
-	ReleaseDC(desktopWnd, desktopDc);
-	DeleteDC(captureDc);
-	DeleteObject(captureBitmap);
-	return isBlack;
-}
-*/
