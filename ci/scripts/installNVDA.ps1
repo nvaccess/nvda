1c8944977ae3e7a5503188c25d8b629f998dd437
Sean Budd <sean@nvaccess.org>
2025-04-03 23:22:10 +1100

    fix up install proc

--- ci/scripts/installNVDA.ps1
+++ ci/scripts/installNVDA.ps1
@@ -8,7 +8,7 @@ try {
 	$installerProcess | Wait-Process -Timeout 180 -ErrorAction Stop
 	$errorCode=$installerProcess.ExitCode
 } catch {
-	Write-Output "NVDA installer process timed out. `n" >> $env:GITHUB_STEP_SUMMARY
+	Write-Output "NVDA installer process timed out.`n" >> $env:GITHUB_STEP_SUMMARY
 	$errorCode=1
 }
 # If the installer failed to exit the log file is still in use.
@@ -16,7 +16,7 @@ try {
 # as a work around create a copy of the log and upload that instead.
 Copy-Item $installerLogFilePath $installerLogFileCopyPath
 if (Test-Path -Path $installerCrashDumpPath){
-	Write-Output "NVDA installer process crashed. `n" >> $env:GITHUB_STEP_SUMMARY
+	Write-Output "NVDA installer process crashed.`n" >> $env:GITHUB_STEP_SUMMARY
 	$errorCode=1
 }
 if ($errorCode -ne 0) { $host.SetShouldExit($errorCode) }
