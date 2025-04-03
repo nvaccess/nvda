332e251400b5421badc5c505c09761cba948f051
Sean Budd <sean@nvaccess.org>
2025-04-03 22:28:16 +1100

    add install artifacts

--- ci/scripts/installNVDA.ps1
+++ ci/scripts/installNVDA.ps1
@@ -1,7 +1,8 @@
 $errorCode=0
-$outputDir=$(Resolve-Path .\testOutput)
 $nvdaLauncherFile=$(Resolve-Path "$env:nvdaLauncherDir\nvda*.exe")
-$installerLogFilePath="$outputDir\nvda_install.log"
+$installerLogFilePath="$env:nvdaInstallerLogDir\nvda_install_temp.log"
+$installerLogFileCopyPath="$env:nvdaInstallerLogDir\nvda.log"
+$installerCrashDumpPath="$env:nvdaInstallerLogDir\nvda_crash.dmp"
 $installerProcess=Start-Process -FilePath "$nvdaLauncherFile" -ArgumentList "--install-silent --debug-logging --log-file $installerLogFilePath" -passthru
 try {
 	$installerProcess | Wait-Process -Timeout 180 -ErrorAction Stop
@@ -9,14 +10,13 @@ try {
 } catch {
 	Write-Output "NVDA installer process timed out"
 	$errorCode=1
-	# Since installer failed to exit in the specified timeout the log file is still in use.
-	# Unfortunately we cannot upload a file which is locked
-	# as a work around create a copy of the log and upload that instead.
-	$installerLogFileCopiedPath = "nvda_install_copy.log"
-	Copy-Item $installerLogFilePath $installerLogFileCopiedPath
 }
-$crashDump = "$outputDir\nvda_crash.dmp"
-if (Test-Path -Path $crashDump){
+# If the installer failed to exit the log file is still in use.
+# We can't/shouldn't upload a file which is locked,
+# as a work around create a copy of the log and upload that instead.
+Copy-Item $installerLogFilePath $installerLogFileCopyPath
+if (Test-Path -Path $installerCrashDumpPath){
+	Write-Output "NVDA installer process crashed"
 	$errorCode=1
 }
 if ($errorCode -ne 0) { $host.SetShouldExit($errorCode) }
