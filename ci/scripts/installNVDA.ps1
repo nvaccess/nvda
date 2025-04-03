ea785dcd4b76a3c9ef5cd79cba076c997741ccf9
Sean Budd <sean@nvaccess.org>
2025-04-04 10:12:56 +1100

    simplify installer log

--- ci/scripts/installNVDA.ps1
+++ ci/scripts/installNVDA.ps1
@@ -2,14 +2,11 @@ $errorCode=0
 $nvdaLauncherFile=$(Resolve-Path "$env:nvdaLauncherDir\nvda*.exe")
 $nvdaInstallerLogDir=$(Resolve-Path ".\testOutput\install")
 $installerLogFilePath="$nvdaInstallerLogDir\nvda_install_temp.log"
-$installerLogFileCopyPath="$nvdaInstallerLogDir\nvda.log"
 $installerCrashDumpPath="$nvdaInstallerLogDir\nvda_crash.dmp"
 $installerProcess=Start-Process -FilePath "$nvdaLauncherFile" -ArgumentList "--install-silent --debug-logging --log-file $installerLogFilePath" -passthru
 try {
 	$installerProcess | Wait-Process -Timeout 180 -ErrorAction Stop
 	$errorCode=$installerProcess.ExitCode
-	Write-Output "NVDA installer completed."
-	Get-ChildItem $nvdaInstallerLogDir
 } catch {
 	Write-Output "NVDA installer process timed out.`n" >> $env:GITHUB_STEP_SUMMARY
 	$errorCode=1
@@ -17,8 +14,7 @@ try {
 # If the installer failed to exit the log file is still in use.
 # We can't/shouldn't upload a file which is locked,
 # as a work around create a copy of the log and upload that instead.
-New-Item -ItemType File -Name $installerLogFileCopyPath
-Copy-Item $installerLogFilePath $installerLogFileCopyPath
+Copy-Item $installerLogFilePath "nvda_install.log"
 if (Test-Path -Path $installerCrashDumpPath){
 	Write-Output "NVDA installer process crashed.`n" >> $env:GITHUB_STEP_SUMMARY
 	$errorCode=1
