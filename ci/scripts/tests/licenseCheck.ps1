58f54fa60a7460f083fd18d3891fa11d91f3b1a0
Sean Budd <sean@nvaccess.org>
2025-04-03 20:17:12 +1100

    fix logging

--- ci/scripts/tests/licenseCheck.ps1
+++ ci/scripts/tests/licenseCheck.ps1
@@ -2,10 +2,10 @@ $licenseOutput = (Resolve-Path .\testOutput\license\)
 $licenseOutput = "$licenseOutput\licenseCheckResults.md"
 cmd.exe /c "runlicensecheck.bat $licenseOutput"
 if ($LastExitCode -ne 0) {
-	Write-Host "FAIL: License check." >> $env:GITHUB_STEP_SUMMARY
+	Write-Output "FAIL: License check.\n" >> $env:GITHUB_STEP_SUMMARY
 	Write-Output "testFailExitCode=$LastExitCode" | Out-File -FilePath $Env:GITHUB_ENV -Encoding utf8 -Append
 } else {
-	Write-Host "PASS: License check." >> $env:GITHUB_STEP_SUMMARY
+	Write-Output "PASS: License check.\n" >> $env:GITHUB_STEP_SUMMARY
 }
 Get-Content testOutput\translationCheckResults.log >> $env:GITHUB_STEP_SUMMARY
 exit $LastExitCode
--- ci/scripts/tests/translationCheck.ps1
+++ ci/scripts/tests/translationCheck.ps1
@@ -3,14 +3,14 @@ if ($env:RUNNER_DEBUG) {
 } else {
 	cmd.exe /c "scons checkPot --all-cores 2> testOutput\translationCheckResults.log"
 }
-Write-Host "Translation check output:"
+Write-Output "Translation check output:"
 Get-Content testOutput\translationCheckResults.log
 
 if ($LastExitCode -ne 0) {
-	Write-Host "FAIL: Translation comments check.\n" >> $env:GITHUB_STEP_SUMMARY
+	Write-Output "FAIL: Translation comments check.\n" >> $env:GITHUB_STEP_SUMMARY
 	Get-Content testOutput\translationCheckResults.log >> $env:GITHUB_STEP_SUMMARY
 	Write-Output "testFailExitCode=$LastExitCode" | Out-File -FilePath $Env:GITHUB_ENV -Encoding utf8 -Append
 } else {
-	Write-Host "PASS: Translation comments check." >> $env:GITHUB_STEP_SUMMARY
+	Write-Output "PASS: Translation comments check.\n" >> $env:GITHUB_STEP_SUMMARY
 }
 exit $LastExitCode
