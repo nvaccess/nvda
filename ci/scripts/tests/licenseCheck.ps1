abb42b6832a76bf371a7a9c98160ffc9b9264c85
Sean Budd <sean@nvaccess.org>
2025-04-03 20:34:16 +1100

    make summaries collapsable

--- ci/scripts/tests/licenseCheck.ps1
+++ ci/scripts/tests/licenseCheck.ps1
@@ -2,10 +2,12 @@ $licenseOutput = (Resolve-Path .\testOutput\license\)
 $licenseOutput = "$licenseOutput\licenseCheckResults.md"
 cmd.exe /c "runlicensecheck.bat $licenseOutput"
 if ($LastExitCode -ne 0) {
-	Write-Output "FAIL: License check.\n" >> $env:GITHUB_STEP_SUMMARY
+	Write-Output "FAIL: License check." >> $env:GITHUB_STEP_SUMMARY
 	Write-Output "testFailExitCode=$LastExitCode" | Out-File -FilePath $Env:GITHUB_ENV -Encoding utf8 -Append
 } else {
-	Write-Output "PASS: License check.\n" >> $env:GITHUB_STEP_SUMMARY
+	Write-Output "PASS: License check." >> $env:GITHUB_STEP_SUMMARY
 }
+Write-Output "<details><summary>License check summary</summary>" >> $env:GITHUB_STEP_SUMMARY
 Get-Content $licenseOutput >> $env:GITHUB_STEP_SUMMARY
+Write-Output "</details>" >> $env:GITHUB_STEP_SUMMARY
 exit $LastExitCode
