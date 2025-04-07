34636aaf2309f2c22a13b634881af22651af6f8b
Sascha Cowley <16543535+SaschaCowley@users.noreply.github.com>
2025-04-07 12:15:05 +1000

    Fix unit tests

---
+++ source/winBindings/kernel32.py
@@ -0,0 +1,21 @@
+# A part of NonVisual Desktop Access (NVDA)
+# Copyright (C) 2025 NV Access Limited
+# This file is covered by the GNU General Public License.
+# See the file COPYING for more details.
+
+"""Functions exported by kernel32.dll, and supporting data structures and enumerations."""
+
+from ctypes import c_wchar_p, windll
+from ctypes.wintypes import HMODULE
+
+_dll = windll.kernel32
+
+GetModuleHandle = _dll.GetModuleHandleW
+"""
+Retrieves a module handle for the specified module, which must have been loaded by the calling process.
+
+.. seealso::
+	https://learn.microsoft.com/en-us/windows/win32/api/libloaderapi/nf-libloaderapi-getmodulehandlew
+"""
+GetModuleHandle.argtypes = (c_wchar_p,)
+GetModuleHandle.restype = HMODULE
