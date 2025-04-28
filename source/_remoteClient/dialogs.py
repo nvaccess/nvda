d6fb03e7a0c52596e65d334abcba6194ffbe75f6
Sascha Cowley <16543535+SaschaCowley@users.noreply.github.com>
2025-04-28 10:48:49 +1000

    Merge pull request #17943 from nvaccess/remoteTerminology
    Fixes #17815
    
    Summary of the issue:
    NVDA's new Remote Access feature currently uses inconsistent terminology.
    
    Description of user facing changes
    Remote Access:
    
    Is consistently referred to as "Remote Access".
    Uses the term "computer" to refer to clients.
    Previously both "computer" and "machine" were used. I also considered "device", as this seems to be the preferred terminology in Windows.
    Uses the word "send" instead of "push" for sending the clipboard to connected clients.
    Has descriptive help items for its menu items, all of which are complete sentences.
    Uses sentence fragments for UI messages where appropriate
    Description of development approach
    Generated a pot file, and searched for the word "remote" therein. If it was referring to Remote Access, and some other name was used, replaced it with Remote Access.
    
    Searched for the word "remote" in the user guide and edited as appropriate. Also did a full readthrough of the remote section.
    
    Reviewed the strings in the settings dialog, menu, and cues explicitly and edited as appropriate.
    
    Calculated the diff 1b4a28d4d...9833d7812. Searched in it for newly introduced translator comments, and edited them if related to Remote Access and I thought they were unclear.
    
    Calculated the diff 9833d7812...42aaea791. Reviewed it for any changed strings that used gettext (_(...)), and changed to use pgettext (pgettext("remote", ...)).

--- source/_remoteClient/dialogs.py
+++ source/_remoteClient/dialogs.py
@@ -42,7 +42,7 @@ class ClientPanel(ContextHelpMixin, wx.Panel):
 			wx.ComboBox,
 		)
 		self.key = sizerHelper.addLabeledControl(
-			# Translators: Label of the edit field to enter key (password) to secure the remote connection.
+			# Translators: Label of the edit field to enter key (password) to secure the Remote Access connection.
 			_("&Key:"),
 			wx.TextCtrl,
 		)
@@ -70,9 +70,9 @@ class ClientPanel(ContextHelpMixin, wx.Panel):
 	def _generateKeyCommand(self, insecure: bool = False) -> None:
 		self._keyGenerationProgressDialog = gui.IndeterminateProgressDialog(
 			self,
-			# Translators: Title of a dialog shown to users when asking a Remote control server to generate a key
+			# Translators: Title of a dialog shown to users when asking a Remote Access server to generate a key
 			pgettext("remote", "Generating key"),
-			# Translators: Message on a dialog shown to users when asking a Remote control server to generate a key
+			# Translators: Message on a dialog shown to users when asking a Remote Access server to generate a key
 			pgettext("remote", "Generating key..."),
 		)
 		address = protocol.addressToHostPort(self.host.GetValue())
@@ -103,8 +103,8 @@ class ClientPanel(ContextHelpMixin, wx.Panel):
 		gui.messageBox(
 			pgettext(
 				"remote",
-				# Translators: Message shown to users when requesting that a Remote control server generate a key fails.
-				# {host} will be replaced with the address of the Remote control server.
+				# Translators: Message shown to users when requesting that a Remote Access server generate a key fails.
+				# {host} will be replaced with the address of the Remote Access server.
 				"Unable to connect to {host}. Check that you have internet access, and that there are no mistakes in the host field.",
 			).format(host=self.host.GetValue()),
 			# Translators: Title of a dialog.
@@ -193,8 +193,8 @@ class ServerPanel(ContextHelpMixin, wx.Panel):
 			max=65535,
 			initial=SERVER_PORT,
 		)
-		# Translators: Label of the edit field to enter key (password) to secure the remote connection.
-		self.key = sizerHelper.addLabeledControl(_("&Key"), wx.TextCtrl)
+		# Translators: Label of the edit field to enter key (password) to secure the Remote Access connection.
+		self.key = sizerHelper.addLabeledControl(pgettext("remote", "&Key"), wx.TextCtrl)
 		# Translators: The button used to generate a random key/password.
 		self._generateKeyButton = wx.Button(parent=self, label=_("&Generate Key"))
 		self._generateKeyButton.Bind(wx.EVT_BUTTON, self.onGenerateKey)
@@ -338,13 +338,13 @@ class DirectConnectDialog(ContextHelpMixin, wx.Dialog):
 		if self._selectedPanel is self._clientPanel and (
 			not self._selectedPanel.host.GetValue() or not self._selectedPanel.key.GetValue()
 		):
-			# Translators: A message box displayed when the host or key field is empty and the user tries to connect.
+			# Translators: Message displayed when the host or key field is empty and the user tries to connect.
 			message = _("Both host and key must be set.")
 			focusTarget = self._selectedPanel.host
 		elif self._selectedPanel is self._serverPanel and (
 			not self._selectedPanel.port.GetValue() or not self._selectedPanel.key.GetValue()
 		):
-			# Translators: A message box displayed when the port or key field is empty and the user tries to connect.
+			# Translators: Message displayed when the port or key field is empty and the user tries to connect.
 			message = _("Both port and key must be set.")
 			focusTarget = self._selectedPanel.port
 		if message is not None:
@@ -389,11 +389,13 @@ class DirectConnectDialog(ContextHelpMixin, wx.Dialog):
 
 class CertificateUnauthorizedDialog(wx.MessageDialog):
 	def __init__(self, parent: wx.Window | None, fingerprint: str | None = None):
-		# Translators: A title bar of a window presented when an attempt has been made to connect with a server with unauthorized certificate.
-		title = _("NVDA Remote Connection Security Warning")
-		message = _(
-			# Translators: {fingerprint} is a SHA256 fingerprint of the server certificate.
-			"The certificate of this server could not be verified. Using the wrong fingerprint may allow a third party to access the remote session..\n"
+		# Translators: Title of the dialog presented when attempting to connect to a server with an untrusted certificate.
+		title = pgettext("remote", "Security Warning")
+		message = pgettext(
+			"remote",
+			# Translators: Message presented when attempting to connect to a server with an untrusted certificate.
+			# {fingerprint} will be replaced with the SHA256 fingerprint of the server certificate.
+			"The certificate of this server could not be verified. Using the wrong fingerprint may allow a third party to access the Remote Access session.\n"
 			"\n"
 			"Before continuing, please make sure that the following server certificate fingerprint is correct.\n"
 			"Server SHA256 fingerprint: {fingerprint}\n"
