2b10e40946958464078f8b016671699c51550f74
Noelia Ruiz Martínez <nrm1977@gmail.com>
2025-06-07 12:07:21 +0200

    Add more tests

--- tests/unit/test_braille/test_calculate.py
+++ tests/unit/test_braille/test_calculate.py
@@ -6,22 +6,38 @@
 """Unit tests for the _calculate function in the braille module."""
 
 import unittest
+
 import braille
+from braille import DisplayDimensions
+
+def _getDisplayDimensions(dimensions: "DisplayDimensions") -> "DisplayDimensions":
+	"""Called by the :attr:`braille.filter_displayDimensions` extension point to get the display dimensions."""
+	return DisplayDimensions(
+		numRows=2,
+		numCols=20
+	)
 
 
 class TestCalculate(unittest.TestCase):
 
 	def setUp(self):
-		self.handler = braille.handler
+		braille.filter_displayDimensions.register(_getDisplayDimensions)
+
+	def tearDown(self):
+		braille.filter_displayDimensions.unregister(_getDisplayDimensions)
 
 	def test_noCells(self):
-		self.handler.buffer.brailleCells = []
-		self.handler.buffer._calculateWindowRowBufferOffsets(0)
+		braille.handler.buffer.brailleCells = []
+		braille.handler.buffer._calculateWindowRowBufferOffsets(0)
 		expectedOffsets = [(0, 0)]
-		self.assertEqual(self.handler.buffer._windowRowBufferOffsets, expectedOffsets)
+		self.assertEqual(braille.handler.buffer._windowRowBufferOffsets, expectedOffsets)
 
-	def test_firstPositionNotZero(self):
-		self.handler.buffer.brailleCells = [1] * self.handler.displaySize
-		self.handler.buffer._calculateWindowRowBufferOffsets(1)
-		expectedOffsets = [(1, 40)]
-		self.assertEqual(self.handler.buffer._windowRowBufferOffsets, expectedOffsets)
+	def test_firstPosition(self):
+		"""Checks that first offset is equal to start pos parameter."""
+		braille.handler.buffer.brailleCells = [1] * braille.handler.displaySize
+		braille.handler.buffer._calculateWindowRowBufferOffsets(0)
+		expectedOffsets = [(0, 20), (20, 40)]
+		self.assertEqual(braille.handler.buffer._windowRowBufferOffsets, expectedOffsets)
+		braille.handler.buffer._calculateWindowRowBufferOffsets(1)
+		expectedOffsets = [(1, 21), (21, 40)]
+		self.assertEqual(braille.handler.buffer._windowRowBufferOffsets, expectedOffsets)
