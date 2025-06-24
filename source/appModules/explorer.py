17d9a5ea67473f039c239a9802f95785240a5918
Joseph Lee <joseph.lee22590@gmail.com>
2025-06-24 07:52:20 -0600

    apModules.explorer: NotificationKind -> notificationKind, NotificationProcessing -> notificationProcessing (argumetn name/resolving TypeError)

--- source/appModules/explorer.py
+++ source/appModules/explorer.py
@@ -585,8 +585,8 @@ class AppModule(appModuleHandler.AppModule):
 		self,
 		obj: NVDAObject,
 		nextHandler: Callable[[], None],
-		NotificationKind: int | None = None,
-		NotificationProcessing: int | None = None,
+		notificationKind: int | None = None,
+		notificationProcessing: int | None = None,
 		displayString: str = "",
 		activityId: str = "",
 	) -> None:
