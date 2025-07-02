362a1f521bfc1e49a087060328570c1d2a612553
Sascha Cowley <16543535+SaschaCowley@users.noreply.github.com>
2025-07-02 11:51:46 +1000

    Improve launcher validation outputs

--- ci/scripts/validateLauncher.ps1
+++ ci/scripts/validateLauncher.ps1
@@ -1,9 +1,22 @@
 $launcherName="output\nvda*.exe"
 if (-not (Test-Path -Path $launcherName -PathType Leaf)) {
-	Write-Output "The launcher does not exist." >> $env:GITHUB_STEP_SUMMARY
+	Write-Output "FAIL: Launcher verification. The launcher does not exist." >> $env:GITHUB_STEP_SUMMARY
 	exit 1
 }
-if ($env:apiSigningToken -and (Get-AuthenticodeSignature -FilePath $launcherName).Status -ne 'Valid') {
-	Write-Output "The launcher was not signed, or the signature was invalid.`n">> $env:GITHUB_STEP_SUMMARY
-	exit 1
+if ($env:apiSigningToken) {
+	$authenticodeSignature = Get-AuthenticodeSignature -FilePath $launcherName
+	if (($authenticodeSignature).Status -ne 'Valid') {
+		Set-PSRepository PSGallery -InstallationPolicy Trusted
+		Install-Module -Name FormatMarkdownTable -Force
+		Write-Output @"
+FAIL: Launcher validation. Expected the launcher to be signed.
+<details>
+	<summary>Signature details</summary>
+
+	$($authenticodeSignature | Format-MarkdownTableTableStyle -HideStandardOutput -ShowMarkdown -DoNotCopyToClipboard)
+
+</details>
+"@ >> $env:GITHUB_STEP_SUMMARY
+		exit 1
+	}
 }
