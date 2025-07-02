a2b259985dc2ed4de101567607eb79090df5abae
Sascha Cowley <16543535+SaschaCowley@users.noreply.github.com>
2025-07-02 14:11:22 +1000

    Add copious output

--- ci/scripts/validateLauncher.ps1
+++ ci/scripts/validateLauncher.ps1
@@ -1,11 +1,18 @@
+echo "Validating launcher."
 $launcherName="output\nvda*.exe"
+echo "launcherName=$launcherName"
+echo "Checking existance"
 if (-not (Test-Path -Path $launcherName -PathType Leaf)) {
+	echo "File doesn't exist."
 	Write-Output "FAIL: Launcher verification. The launcher does not exist." >> $env:GITHUB_STEP_SUMMARY
 	exit 1
 }
+echo "File exists."
 if ($env:apiSigningToken) {
+	echo "Checking signature"
 	$authenticodeSignature = Get-AuthenticodeSignature -FilePath $launcherName
 	if (($authenticodeSignature).Status -ne 'Valid') {
+		echo "Signature not valid."
 		Set-PSRepository PSGallery -InstallationPolicy Trusted
 		Install-Module -Name FormatMarkdownTable -Force
 		Write-Output @"
@@ -18,5 +25,11 @@ FAIL: Launcher validation. Expected the launcher to be signed.
 </details>
 "@ >> $env:GITHUB_STEP_SUMMARY
 		exit 1
+	} else {
+		echo "Signature valid."
 	}
+	echo $authenticodeSignature
+} else {
+	echo "Not checking signature."
 }
+echo "Launcher verification passed."
