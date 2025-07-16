96de458b82174432caf33c41a7efd890800024d4
Michael Curran <mick@nvaccess.org>
2025-07-16 13:59:21 +1000

    Fix a ctypes overflow error at end of speech in sapi5, in ISpNotifySink_Notify, by correctly defining CoTaskMemFree in a new winBindings.ole32 module.

---
+++ source/winBindings/ole32.py
@@ -0,0 +1,31 @@
+# A part of NonVisual Desktop Access (NVDA)
+# Copyright (C) 2025 NV Access Limited
+# This file is covered by the GNU General Public License.
+# See the file COPYING for more details.
+
+"""Functions exported by ole32.dll, and supporting data structures and enumerations."""
+
+
+from ctypes import (
+	c_voidp,
+	windll,
+)
+from ctypes.wintypes import (
+	LPVOID,
+)
+
+
+dll = windll.ole32
+
+
+CoTaskMemFree = dll.CoTaskMemFree
+"""
+Frees a block of task memory previously allocated through a call to the CoTaskMemAlloc or CoTaskMemRealloc function.
+
+.. seealso::
+	https://learn.microsoft.com/en-us/windows/win32/api/combaseapi/nf-combaseapi-cotaskmemfree
+"""
+CoTaskMemFree.restype = c_voidp
+CoTaskMemFree.argtypes = (
+	LPVOID,  # pv: A pointer to the memory block to be freed.
+)
