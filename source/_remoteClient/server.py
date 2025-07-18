nvaccess/nvda

commit b9ae45286249e39ca11738dc8ef5ccd9e3f71eed

Create a confirmation dialog to update root certificates when downloading add-ons (#18402)
Related #15905 #18354 

Summary of the issue:
When trying to download an add-on from the add-on store in certain
environments such as corporates, the download fails due to the machine
not trusting the root certificate of the add-on source.

This can be caused by two issues:

1. the `requests` library using `certifi` rather than the system root
certificates
2. the system root certificates not trusting the add-on download source

Additionally https://github.com/nvaccess/nvda/pull/18354 changed the
NVDA update check to use requests rather than urllib, which would have
introduced the issue with certifi to the update check instead of just
the add-on store.

Description of user facing changes:
- Use system root certificates rather than certifi, preventing issues
accessing the add-on store or update check in corporate environments
- Add a dialog to give the user an option for trusting root certificates
for add-on sources, such as github, if the system doesn't trust the
download source

Description of developer facing changes:
None

Description of development approach:
- add `pip_system_certs` which overrides `certifi` to use system certs
for `requests`
- use our existing code to update system certs when downloading add-ons
- ignore the patch when wrapping the socket for remote. This is due to
an unexpected bug with the patch. This should be reconsidered when
updating python, pip_system_certs, and remote to e2ee
- Note: PROTOCOL_TLSv1_2 is deprecated. Fix up usage to match new python
suggested code

### Testing strategy:
- [x] Manual testing from users
- [x] Smoke tested remote connection

### Known issues with pull request:
None

pyproject.toml
@@ -29,6 +29,8 @@ dependencies = [
 	# pinned to a commit in tool.uv.sources
 	"configobj",
 	"requests==2.32.3",
+	# Overrides certifi so that requests uses system certificates instead
+	"pip_system_certs==5.2",
 	"url-normalize==1.4.3",
 	"schedule==1.2.2",
 	# NVDA_DMP requires diff-match-patch
@@ -122,6 +124,7 @@ only_licenses = ["BSD", "MIT", "Python", "LGPLV3+", "Apache"]
 ignore_packages = [
 	# Compatible licenses:
 	"certifi",  # Mozilla Public License 2.0
+	"pip_system_certs",  # BSD 2-Clause "Simplified" License, but not in PyPI
 	"markdown-link-attr-modifier",  # GPLV3 license, but not in PyPI correctly
 	"pycaw",  # MIT license, but not in PyPI
 	"urllib3",  # MIT license, but not in PyPI

source/_remoteClient/server.py
@@ -33,6 +33,7 @@ from pathlib import Path
 from select import select
 from itertools import count
 from typing import Any, Final
+from unittest import mock
 
 import cffi  # noqa # required for cryptography
 from cryptography import x509
@@ -294,7 +295,13 @@ class LocalRelayServer:
 		"""
 		serverSocket = socket.socket(family, type)
 		sslContext = self.certManager.createSSLContext()
-		serverSocket = sslContext.wrap_socket(serverSocket, server_side=True)
+
+		# Prevent pip_system_certs from patching wrap_socket due to a bug.
+		with mock.patch(
+			"pip._vendor.truststore._api._verify_peercerts",
+			lambda *a, **kw: None,
+		):
+			serverSocket = sslContext.wrap_socket(serverSocket, server_side=True)
 		serverSocket.bind(bindAddress)
 		serverSocket.listen(5)  # Set the maximum number of queued connections
 		return serverSocket

source/_remoteClient/transport.py
@@ -36,6 +36,7 @@ from dataclasses import dataclass
 from logHandler import log
 from queue import Queue
 from typing import Any, Literal, Optional, Self
+from unittest import mock
 
 import wx
 from extensionPoints import Action, HandlerRegistrar
@@ -433,14 +434,23 @@ class TCPTransport(Transport):
 			serverSock.settimeout(self.timeout)
 		serverSock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
 		serverSock.ioctl(socket.SIO_KEEPALIVE_VALS, (1, 60000, 2000))
-		ctx = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-		if insecure:
-			ctx.verify_mode = ssl.CERT_NONE
-			log.warn(f"Skipping certificate verification for {host}:{port}")
+		ctx = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+		ctx.minimum_version = ssl.TLSVersion.TLSv1_2
+		ctx.maximum_version = ssl.TLSVersion.TLSv1_3
+		# Must be set before verify_mode.
 		ctx.check_hostname = not insecure
+		if insecure:
+			log.warn(f"Skipping certificate verification for {host}:{port}")
+			ctx.verify_mode = ssl.CERT_NONE
+
 		ctx.load_default_certs()
 
-		serverSock = ctx.wrap_socket(sock=serverSock, server_hostname=host)
+		# Prevent pip_system_certs from patching wrap_socket due to a bug.
+		with mock.patch(
+			"pip._vendor.truststore._api._verify_peercerts",
+			lambda *a, **kw: None,
+		):
+			serverSock = ctx.wrap_socket(sock=serverSock, server_hostname=host)
 		return serverSock
 
 	def getpeercert(

source/core.py
@@ -32,6 +32,8 @@ import garbageHandler
 import NVDAState
 from NVDAState import WritePaths
 
+import pip_system_certs.wrapt_requests
+
 if TYPE_CHECKING:
 	import wx
 
@@ -672,6 +674,10 @@ def main():
 	Finally, it starts the wx main loop.
 	"""
 	log.debug("Core starting")
+
+	# Use Windows root certificates for requests rather than certifi.
+	pip_system_certs.wrapt_requests.inject_truststore()
+
 	if NVDAState.isRunningAsSource():
 		# When running as packaged version, DPI awareness is set via the app manifest.
 		from winAPI.dpiAwareness import setDPIAwareness
@@ -694,6 +700,7 @@ def main():
 		WritePaths.configDir = config.getUserDefaultConfigPath(
 			useInstalledPathIfExists=globalVars.appArgs.launcher,
 		)
+
 	# Initialize the config path (make sure it exists)
 	config.initConfigPath()
 	log.info(f"Config dir: {WritePaths.configDir}")

source/addonStore/dataManager.py
@@ -125,7 +125,7 @@ class _DataManager:
 		url = _getAddonStoreURL(self._preferredChannel, self._lang, apiVersion)
 		try:
 			log.debug(f"Fetching add-on data from {url}")
-			response = requests.get(url, timeout=FETCH_TIMEOUT_S)
+			response = _fetchUrlAndUpdateRootCertificates(url)
 		except requests.exceptions.RequestException as e:
 			log.debugWarning(f"Unable to fetch addon data: {e}")
 			return None

source/addonStore/network.py
@@ -1,5 +1,5 @@
 # A part of NonVisual Desktop Access (NVDA)
-# Copyright (C) 2022-2024 NV Access Limited
+# Copyright (C) 2022-2025 NV Access Limited
 # This file is covered by the GNU General Public License.
 # See the file COPYING for more details.
 
@@ -7,6 +7,7 @@ from concurrent.futures import (
 	Future,
 	ThreadPoolExecutor,
 )
+import hashlib
 import os
 import pathlib
 import shutil
@@ -18,6 +19,7 @@ from typing import (
 	Optional,
 	Tuple,
 )
+from urllib.parse import urlparse
 
 import requests
 
@@ -28,6 +30,11 @@ import NVDAState
 from NVDAState import WritePaths
 import threading
 from utils.security import sha256_checksum
+from utils.networking import (
+	_getCertificate,
+	_is_cert_verification_error,
+	_updateWindowsRootCertificates,
+)
 from config import conf
 
 from .models.addon import (
@@ -216,7 +223,11 @@ class AddonFileDownloader:
 		# Some add-ons are quite large, so we need to allow for a long download time.
 		# 1GB at 0.5 MB/s takes 4.5hr to download.
 		MAX_ADDON_DOWNLOAD_TIME = 60 * 60 * 6  # 6 hours
-		with requests.get(addonData.model.URL, stream=True, timeout=MAX_ADDON_DOWNLOAD_TIME) as r:
+		with requests.get(
+			addonData.model.URL,
+			stream=True,
+			timeout=MAX_ADDON_DOWNLOAD_TIME,
+		) as r:
 			with open(downloadFilePath, "wb") as fd:
 				# Most add-ons are small. This value was chosen quite arbitrarily, but with the intention to allow
 				# interrupting the download. This is particularly important on a slow connection, to provide
@@ -236,11 +247,54 @@ class AddonFileDownloader:
 							return False  # The download was cancelled
 		return True
 
-	def _download(self, listItem: "AddonListItemVM[_AddonStoreModel]") -> Optional[os.PathLike]:
-		from gui.message import DisplayableError
+	# Translators: A title for a dialog notifying a user of an add-on download failure.
+	_ADDON_DOWNLOAD_FAILURE_MESSAGE_TITLE = pgettext("addonStore", "Add-on download failure")
 
-		# Translators: A title for a dialog notifying a user of an add-on download failure.
-		_addonDownloadFailureMessageTitle = pgettext("addonStore", "Add-on download failure")
+	def _handleCertVerificationError(
+		self,
+		exception: requests.exceptions.SSLError,
+		listItem: "AddonListItemVM[_AddonStoreModel]",
+	) -> os.PathLike | None:
+		from gui.message import DisplayableError, MessageDialog, ReturnCode
+
+		if _is_cert_verification_error(exception):
+			cert = _getCertificate(listItem.model.URL)
+			certFingerprint = hashlib.sha256(cert).hexdigest()
+
+			if (
+				MessageDialog.confirm(
+					message=pgettext(
+						"addonStore",
+						# Translators: A message to the user if an add-on download fails.
+						# url is replaced with the base URL of the add-on download e.g. (github.com).
+						# fingerprint is replaced with the SHA256 fingerprint of the certificate.
+						"The website where you are downloading the add-on from has a certificate that is not trusted. "
+						"Do you want to trust the root certificate for {url}? "
+						"This will allow you to download add-ons from this website in the future. "
+						"Only do this if you trust the website. "
+						"The certificate's SHA256 fingerprint is: {fingerprint}. ",
+					).format(url=urlparse(listItem.model.URL).netloc, fingerprint=certFingerprint),
+					caption=self._ADDON_DOWNLOAD_FAILURE_MESSAGE_TITLE,
+				)
+				== ReturnCode.OK
+			):
+				_updateWindowsRootCertificates(cert)
+				return self._download(listItem)
+			else:
+				return None  # The download was cancelled
+		else:
+			log.debugWarning(f"Unable to download addon file: {exception}")
+			raise DisplayableError(
+				pgettext(
+					"addonStore",
+					# Translators: A message to the user if an add-on download fails
+					"Unable to download add-on: {name}",
+				).format(name=listItem.model.displayName),
+				self._ADDON_DOWNLOAD_FAILURE_MESSAGE_TITLE,
+			)
+
+	def _download(self, listItem: "AddonListItemVM[_AddonStoreModel]") -> os.PathLike | None:
+		from gui.message import DisplayableError
 
 		addonData = listItem.model
 		log.debug(f"starting download: {addonData.addonId}")
@@ -257,6 +311,8 @@ class AddonFileDownloader:
 		try:
 			if not self._downloadAddonToPath(listItem, inProgressFilePath):
 				return None  # The download was cancelled
+		except requests.exceptions.SSLError as e:
+			return self._handleCertVerificationError(e, listItem)
 		except requests.exceptions.RequestException as e:
 			log.debugWarning(f"Unable to download addon file: {e}")
 			raise DisplayableError(
@@ -265,7 +321,7 @@ class AddonFileDownloader:
 					# Translators: A message to the user if an add-on download fails
 					"Unable to download add-on: {name}",
 				).format(name=addonData.displayName),
-				_addonDownloadFailureMessageTitle,
+				self._ADDON_DOWNLOAD_FAILURE_MESSAGE_TITLE,
 			)
 		except OSError as e:
 			log.debugWarning(f"Unable to save addon file ({inProgressFilePath}): {e}")
@@ -275,7 +331,7 @@ class AddonFileDownloader:
 					# Translators: A message to the user if an add-on download fails
 					"Unable to save add-on as a file: {name}",
 				).format(name=addonData.displayName),
-				_addonDownloadFailureMessageTitle,
+				self._ADDON_DOWNLOAD_FAILURE_MESSAGE_TITLE,
 			)
 		if not self._checkChecksum(inProgressFilePath, addonData):
 			with self.DOWNLOAD_LOCK:
@@ -287,7 +343,7 @@ class AddonFileDownloader:
 					# Translators: A message to the user if an add-on download is not safe
 					"Add-on download not safe: checksum failed for {name}",
 				).format(name=addonData.displayName),
-				_addonDownloadFailureMessageTitle,
+				self._ADDON_DOWNLOAD_FAILURE_MESSAGE_TITLE,
 			)
 		log.debug(f"Download complete: {inProgressFilePath}")
 		with self.DOWNLOAD_LOCK:

source/utils/networking.py
@@ -41,10 +41,9 @@ class _CERT_CHAIN_PARA(ctypes.Structure):
 	)
 
 
-def _updateWindowsRootCertificates(url: str) -> None:
-	"""Updates the Windows root certificates by fetching the latest certificate from the server."""
-	log.debug("Updating Windows root certificates")
-	crypt = ctypes.windll.crypt32
+def _getCertificate(url: str) -> bytes:
+	"""Gets the certificate from the server."""
+	log.debug(f"Getting certificate from: {url}")
 	with requests.get(
 		url,
 		timeout=_FETCH_TIMEOUT_S,
@@ -53,7 +52,13 @@ def _updateWindowsRootCertificates(url: str) -> None:
 		stream=True,
 	) as response:
 		# Get the server certificate.
-		cert = response.raw.connection.sock.getpeercert(True)
+		return response.raw.connection.sock.getpeercert(True)
+
+
+def _updateWindowsRootCertificates(cert: bytes) -> None:
+	"""Adds the certificate to the Windows root certificates."""
+	log.debug("Updating Windows root certificates")
+	crypt = ctypes.windll.crypt32
 	# Convert to a form usable by Windows.
 	certCont = crypt.CertCreateCertificateContext(
 		0x00000001,  # X509_ASN_ENCODING
@@ -87,6 +92,7 @@ def _is_cert_verification_error(exception: requests.exceptions.SSLError) -> bool
 		and exception.__context__.__cause__
 		and exception.__context__.__cause__.__context__
 		and isinstance(exception.__context__.__cause__.__context__, ssl.SSLCertVerificationError)
+		and hasattr(exception.__context__.__cause__.__context__, "reason")
 		and exception.__context__.__cause__.__context__.reason == "CERTIFICATE_VERIFY_FAILED"
 	)
 
@@ -106,7 +112,8 @@ def _fetchUrlAndUpdateRootCertificates(url: str, certFetchUrl: str | None = None
 		if _is_cert_verification_error(e):
 			# #4803: Windows fetches trusted root certificates on demand.
 			# Python doesn't trigger this fetch (PythonIssue:20916), so try it ourselves.
-			_updateWindowsRootCertificates(certFetchUrl or url)
+			cert = _getCertificate(certFetchUrl or url)
+			_updateWindowsRootCertificates(cert)
 			log.debug(f"Retrying fetching data from: {url}")
 			result = requests.get(url, timeout=_FETCH_TIMEOUT_S)
 		else:

commit fcf71823c28ab498c001b0d11fb99cbaa202f35d

Use truststore directly not pip_system_certs (#18490)
Fixes #18460 
Fixup of #18354 

Summary of the issue:
There is a fragile workaround introduced in
https://github.com/nvaccess/nvda/pull/18402#discussion_r2196581863.
This is also a soft API breakage for add-ons, forcing teleNVDA to use
the workaround

This is likely a bug fixed in truststore
https://github.com/sethmlarson/truststore/pull/149

We are using pip_system_certs to patch in an old version of truststore.
this [is no longer being actively
maintained](https://gitlab.com/alelec/pip-system-certs/-/issues/27#note_2372267321)
and apparently truststore has the same patching feature now.

Description of user facing changes:
None

Description of developer facing changes:
Fix workaround which was required for add-ons such as tele NVDA

Testing strategy:
Ensure https://github.com/nvaccess/nvda/issues/18460 is not
reintroduced.
Make sure NVDA can still use update check and the add-on store in
corporate network environments.

pyproject.toml
@@ -30,7 +30,7 @@ dependencies = [
 	"configobj",
 	"requests==2.32.3",
 	# Overrides certifi so that requests uses system certificates instead
-	"pip_system_certs==5.2",
+	"truststore==0.10.1",
 	"url-normalize==1.4.3",
 	"schedule==1.2.2",
 	# NVDA_DMP requires diff-match-patch
@@ -124,7 +124,6 @@ only_licenses = ["BSD", "MIT", "Python", "LGPLV3+", "Apache"]
 ignore_packages = [
 	# Compatible licenses:
 	"certifi",  # Mozilla Public License 2.0
-	"pip_system_certs",  # BSD 2-Clause "Simplified" License, but not in PyPI
 	"markdown-link-attr-modifier",  # GPLV3 license, but not in PyPI correctly
 	"pycaw",  # MIT license, but not in PyPI
 	"urllib3",  # MIT license, but not in PyPI

source/_remoteClient/server.py
@@ -33,7 +33,6 @@ from pathlib import Path
 from select import select
 from itertools import count
 from typing import Any, Final
-from unittest import mock
 
 import cffi  # noqa # required for cryptography
 from cryptography import x509
@@ -296,12 +295,7 @@ class LocalRelayServer:
 		serverSocket = socket.socket(family, type)
 		sslContext = self.certManager.createSSLContext()
 
-		# Prevent pip_system_certs from patching wrap_socket due to a bug.
-		with mock.patch(
-			"pip._vendor.truststore._api._verify_peercerts",
-			lambda *a, **kw: None,
-		):
-			serverSocket = sslContext.wrap_socket(serverSocket, server_side=True)
+		serverSocket = sslContext.wrap_socket(serverSocket, server_side=True)
 		serverSocket.bind(bindAddress)
 		serverSocket.listen(5)  # Set the maximum number of queued connections
 		return serverSocket

source/_remoteClient/transport.py
@@ -36,7 +36,6 @@ from dataclasses import dataclass
 from logHandler import log
 from queue import Queue
 from typing import Any, Literal, Optional, Self
-from unittest import mock
 
 import wx
 from extensionPoints import Action, HandlerRegistrar
@@ -445,12 +444,7 @@ class TCPTransport(Transport):
 
 		ctx.load_default_certs()
 
-		# Prevent pip_system_certs from patching wrap_socket due to a bug.
-		with mock.patch(
-			"pip._vendor.truststore._api._verify_peercerts",
-			lambda *a, **kw: None,
-		):
-			serverSock = ctx.wrap_socket(sock=serverSock, server_hostname=host)
+		serverSock = ctx.wrap_socket(sock=serverSock, server_hostname=host)
 		return serverSock
 
 	def getpeercert(

source/core.py
@@ -31,8 +31,7 @@ import extensionPoints
 import garbageHandler
 import NVDAState
 from NVDAState import WritePaths
-
-import pip_system_certs.wrapt_requests
+import truststore
 
 if TYPE_CHECKING:
 	import wx
@@ -676,7 +675,7 @@ def main():
 	log.debug("Core starting")
 
 	# Use Windows root certificates for requests rather than certifi.
-	pip_system_certs.wrapt_requests.inject_truststore()
+	truststore.inject_into_ssl()
 
 	if NVDAState.isRunningAsSource():
 		# When running as packaged version, DPI awareness is set via the app manifest.