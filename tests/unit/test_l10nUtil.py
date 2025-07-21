01ce159442f20ce8d0244aaab9c0313d19ff979f
Sean Budd <sean@nvaccess.org>
2025-07-21 15:41:38 +1000

    Add copyright headers

--- tests/unit/test_l10nUtil.py
+++ tests/unit/test_l10nUtil.py
@@ -1,3 +1,8 @@
+# A part of NonVisual Desktop Access (NVDA)
+# Copyright (C) 2025 NV Access Limited
+# This file may be used under the terms of the GNU General Public License, version 2 or later, as modified by the NVDA license.
+# For full terms and any additional permissions, see the NVDA license file: https://github.com/nvaccess/nvda/blob/master/copying.txt
+
 import unittest
 from l10nUtil import _PoChecker
 
