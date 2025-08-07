d299ee6065f5c1bb15d72dda7acd33b65c112378
Sean Budd <sean@nvaccess.org>
2025-08-07 13:09:11 +1000

    abstractify logic

--- tests/unit/test_baseObject.py
+++ tests/unit/test_baseObject.py
@@ -1,7 +1,7 @@
 # A part of NonVisual Desktop Access (NVDA)
 # This file is covered by the GNU General Public License.
 # See the file COPYING for more details.
-# Copyright (C) 2018-2015 NV Access Limited, Babbage B.V.
+# Copyright (C) 2018-2025 NV Access Limited, Babbage B.V.
 
 """Unit tests for the baseObject module, its classes and their derivatives."""
 
@@ -147,33 +147,28 @@ class TestAbstractAutoPropertyObjects(unittest.TestCase):
 	It also makes sure that abstract properties can be overridden on subclasses.
 	"""
 
-	def test_abstractProperty(self):
+	@staticmethod
+	def _get_regex(className: str) -> str:
 		if sys.version_info.major == 3 and sys.version_info.minor == 13:
-			regex = r"^Can't instantiate abstract class AutoPropertyObjectWithAbstractProperty without an implementation for abstract method 'x'"
+			return rf"^Can't instantiate abstract class {className} without an implementation for abstract method 'x'"
 		elif sys.version_info.major == 3 and sys.version_info.minor == 11:
-			regex = r"^Can't instantiate abstract class AutoPropertyObjectWithAbstractProperty with abstract method x"
+			return rf"^Can't instantiate abstract class {className} with abstract method x"
 		else:
 			raise RuntimeError(
 				f"Unsupported Python version for abstract property tests: {sys.version_info.major}.{sys.version_info.minor}",
 			)
+
+	def test_abstractProperty(self):
 		self.assertRaisesRegex(
 			TypeError,
-			regex,
+			self._get_regex("AutoPropertyObjectWithAbstractProperty"),
 			AutoPropertyObjectWithAbstractProperty,
 		)
 
 	def test_subclassedAbstractProperty(self):
-		if sys.version_info.major == 3 and sys.version_info.minor == 13:
-			regex = r"^Can't instantiate abstract class SubclassedAutoPropertyObjectWithAbstractProperty without an implementation for abstract method 'x'"
-		elif sys.version_info.major == 3 and sys.version_info.minor == 11:
-			regex = r"^Can't instantiate abstract class SubclassedAutoPropertyObjectWithAbstractProperty with abstract method x"
-		else:
-			raise RuntimeError(
-				f"Unsupported Python version for abstract property tests: {sys.version_info.major}.{sys.version_info.minor}",
-			)
 		self.assertRaisesRegex(
 			TypeError,
-			regex,
+			self._get_regex("SubclassedAutoPropertyObjectWithAbstractProperty"),
 			SubclassedAutoPropertyObjectWithAbstractProperty,
 		)
 
