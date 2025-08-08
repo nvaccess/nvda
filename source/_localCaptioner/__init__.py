69e1a2153aa3060f92344ca45ca1489a444542cc
tianzeshi-study <tianzeshi_study@outlook.com>
2025-08-08 14:42:15 +0800

    Apply suggestions from code review
    Co-authored-by: Sean Budd <seanbudd123@gmail.com>

--- source/_localCaptioner/__init__.py
+++ source/_localCaptioner/__init__.py
@@ -2,6 +2,7 @@
 # Copyright (C) 2025 NV Access Limited, tianze
 # This file may be used under the terms of the GNU General Public License, version 2 or later, as modified by the NVDA license.
 # For full terms and any additional permissions, see the NVDA license file: https://github.com/nvaccess/nvda/blob/master/copying.txt
+
 from logHandler import log
 
 from .imageDescriber import ImageDescriber
@@ -20,7 +21,7 @@ def terminate():
 	"""Terminate the local captioner."""
 	global _localCaptioner
 	if _localCaptioner is None:
-		log.debug("local captioner not running.")
+		log.error("local captioner not running")
 		return
 	log.debug("Terminating local captioner")
 	_localCaptioner.terminate()
