676fc42fdd543026c9430e97bfc359e9f321c0d4
Wang Chong <306289287@qq.com>
2025-08-21 09:51:24 +0800

    add copyright header

--- source/textUtils/segFlag.py
+++ source/textUtils/segFlag.py
@@ -1,3 +1,8 @@
+# A part of NonVisual Desktop Access (NVDA)
+# Copyright (C) 2025 NV Access Limited, Wang Chong
+# This file may be used under the terms of the GNU General Public License, version 2 or later, as modified by the NVDA license.
+# For full terms and any additional permissions, see the NVDA license file: https://github.com/nvaccess/nvda/blob/master/copying.txt
+
 from enum import IntFlag
 
 # shared bit masks (explicit powers of two)
