0d1c7de534c0b87b4efed991d5a8b2e9fd27c2af
tianzeshi <tianzeshi_study@outlook.com>
2025-08-23 20:20:20 +0800

    handle default model download result

--- source/gui/_localCaptioner/messageDialogs.py
+++ source/gui/_localCaptioner/messageDialogs.py
@@ -5,12 +5,64 @@
 
 from gui.message import MessageDialog, DefaultButton, ReturnCode, DialogType
 from _localCaptioner.modelDownloader import ModelDownloader
+import threading
+import wx
 
+def onDownload() -> None:
+	modelDownloader = ModelDownloader()
+	(success, fail) = modelDownloader.downloadModelsMultithreaded()
+	if success:
+		wx.CallAfter(openSuccessDialog)
+	else:
+		wx.CallAfter(openFailDialog)
+
+def openSuccessDialog() -> None:
+	confirmation_button = (
+		DefaultButton.YES.value._replace(defaultFocus=True, fallbackAction=True),
+	)
+
+	dialog = MessageDialog(
+		parent=None,
+		# Translators: title of dialog when download successfully
+		title=pgettext("imageDesc", "Download successfully"),
+		message=pgettext(
+			"imageDesc",
+			# Translators: label of dialog when downloading image captioning
+			"Image captioning installed successfully.",
+		),
+		dialogType=DialogType.STANDARD,
+		buttons=confirmation_button,
+	)
+
+	if dialog.ShowModal() == ReturnCode.YES:
+		pass
+
+def openFailDialog() -> None:
+	confirmation_buttons = (
+		DefaultButton.YES.value._replace(defaultFocus=True, fallbackAction=True),
+		DefaultButton.NO,
+	)
+
+	dialog = MessageDialog(
+		parent=None,
+		# Translators: title of dialog when fail to download
+		title=pgettext("imageDesc", "Download failed"),
+		message=pgettext(
+			"imageDesc",
+			# Translators: label of dialog when fail to download image captioning
+			"Image captioning download failed. Would you like to retry ?",
+		),
+		dialogType=DialogType.WARNING,
+		buttons=confirmation_buttons,
+	)
+
+	if dialog.ShowModal() == ReturnCode.YES:
+		threading.Thread(target=onDownload).start()
 
 def openDownloadDialog() -> None:
 	confirmation_buttons = (
-		DefaultButton.YES,
-		DefaultButton.NO.value._replace(defaultFocus=True, fallbackAction=True),
+		DefaultButton.YES.value._replace(defaultFocus=True, fallbackAction=True),
+		DefaultButton.NO,
 	)
 
 	dialog = MessageDialog(
@@ -27,5 +79,4 @@ def openDownloadDialog() -> None:
 	)
 
 	if dialog.ShowModal() == ReturnCode.YES:
-		downloader = ModelDownloader()
-		threading.Thread(target=downloader.downloadModelsMultithreaded).start()
+		threading.Thread(target=onDownload).start()
