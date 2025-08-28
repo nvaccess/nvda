nvaccess/nvda

commit 905a7eca5878f867e4ffd0b4831103ddb25db72b

Move Windows API bindings out of hwPortUtils (#18571)
Part of #16304
Related to #18207

Summary of the issue:
`hwPortUtils` had a lot of ctypes bindings.

Description of user facing changes:
None

Description of developer facing changes:
Almost all of these bindings have been moved to the `winBindings`
subpackage.

Description of development approach:
Used VS Code's refactoring tools to move the ctypes code. Used the
Microsoft Learn pages and the header files in the Windows 10 SDK to add
type information.

Testing strategy:
Tested connecting a Brailleedge 40 via USB and Bluetooth with automatic
detection.

Used `dir(hwPortUtils)` to find all symbols accessible from
`hwPortUtils` without this PR. Manually filtered the list to exclude
transitive imports. Ran NVDA from source with this PR checked out and
made sure that:

* All of the identified symbols could still be imported; and
* All of the symbols moved by this PR log a deprecation warning on
access.

Known issues with pull request:
The definition of `SP_DEVICE_INTERFACE_DETAIL_DATA_W` has not been
moved, as its definition relies on variables in its closure.

source/utils/_deprecate.py
@@ -1,0 +1,169 @@
+# A part of NonVisual Desktop Access (NVDA)
+# Copyright (C) 2025 NV Access Limited
+# This file may be used under the terms of the GNU General Public License, version 2 or later.
+# For more details see: https://www.gnu.org/licenses/gpl-2.0.html
+
+"""Utilities for handling deprecations in NVDA's public API."""
+
+import inspect
+import sys
+from abc import ABC, abstractmethod, abstractproperty
+from collections.abc import Callable
+from importlib import import_module
+from types import ModuleType
+from typing import Any
+
+import NVDAState
+from logHandler import log
+
+
+class DeprecatedSymbol(ABC):
+	"""A deprecated symbol (variable, constant, function, class, etc).
+
+	Concrete subclasses:
+
+	:class:`MovedSymbol`
+		A symbol that has been moved (renamed, moved to a different module, etc) in the public API.
+
+	:class:`RemovedSymbol`
+		A symbol that has been removed from the API (including symbols that have been made internal).
+	"""
+
+	name: str
+	"""Name of the symbol.
+
+	This should be a valid Python name.
+	"""
+
+	def __init__(self, name: str):
+		"""Initialiser.
+
+		:param name: Old name of the deprecated symbol.
+		"""
+		self.name = name
+
+	@abstractmethod
+	def getLogMessage(self, moduleName: str) -> str:
+		"""
+		Get the message to be output to the log when attempting to access this symbol.
+
+		:param moduleName: Fully qualified module name from which the symbol is being accessed.
+		:return: String to be output to the log.
+		"""
+		...
+
+	@abstractproperty
+	def value(self) -> Any:
+		"""Value to be returned as the value of the deprecated symbol."""
+		...
+
+
+class MovedSymbol(DeprecatedSymbol):
+	"""A symbol which has been moved (renamed or relocated) in the public API."""
+
+	newModule: str
+	"""Fully qualified module name from which the symbol should now be accessed."""
+
+	newPath: tuple[str, ...]
+	"""
+	Path to access the new symbol from the new module.
+
+	Each element of the path is an attribute of the last.
+	The first element is an attribute of :attr:`newModule`.
+	"""
+
+	def __init__(self, name: str, newModule: str, *newPath: str):
+		"""Initialiser.
+
+		:param name: Old name of the symbol.
+		:param newModule: Fully qualified name of the module from which the symbol should now be accessed.
+		:param *newPath: Path by which the new symbol is accessed from the new module.
+			If the new symbol is an attribute of the new module, this should just be the name of the new symbol.
+			If the new symbol is part of a nested data structure (e.g. an enumeration),
+			The first element should be an attribute of the new module,
+			and each subsequent element should be an element of the previous element.
+			If no path segments are provided, ``name`` is used.
+		"""
+		super().__init__(name)
+		self.newModule = newModule
+		self.newPath = newPath if newPath else (name,)
+
+	def getLogMessage(self, moduleName: str) -> str:
+		return (
+			f"{moduleName}.{self.name} is deprecated. Use {self.newModule}.{'.'.join(self.newPath)} instead."
+		)
+
+	@property
+	def value(self):
+		# Get the new module in which the symbol is defined.
+		value = import_module(self.newModule)
+		# And iteratively drill down to get the actual symbol.
+		for segment in self.newPath:
+			value = getattr(value, segment)
+		return value
+
+
+class RemovedSymbol(DeprecatedSymbol):
+	"""A symbol which has been removed from the public API."""
+
+	def __init__(self, name: str, value: Any, *, message: str = "No public replacement is planned."):
+		"""Initialiser.
+
+		:param name: Old name of the symbol.
+		:param value: Old value of the symbol.
+		:param message: _description_, defaults to "No public replacement is planned."
+		"""
+		super().__init__(name)
+		self._value = value
+		self._extraMessage = message
+
+	@property
+	def value(self) -> Any:
+		return self._value
+
+	def getLogMessage(self, moduleName: str) -> str:
+		return f"{moduleName}.{self.name} is deprecated. {self._extraMessage}"
+
+
+def _getCallerModule(level: int = 0) -> ModuleType:
+	"""Get the module from which this was called.
+
+	..note::
+		This function will not work on stackless python implementations,
+		and may not work on implementations other than cpython.
+
+	:param level: Number of stack frames to skip, defaults to ``0``.
+		This can be used if calling from a helper function to skip the stack frame created by the helper.
+	:return: The module from which this function was called.
+	"""
+	# If not part of a module, the caller must be in ``__main__``.
+	return inspect.getmodule(inspect.stack()[level + 1].frame) or sys.modules["__main__"]
+
+
+def handleDeprecations(
+	*deprecated: DeprecatedSymbol,
+) -> Callable[[str], Any]:
+	"""Get a function that can be used as a module's ``__getattr__`` for handling deprecated symbols in the public API.
+
+	:param *deprecated: Symbols deprecated in the calling module's namespace.
+	:return: A function which can be used as a module's ``__getattr__``.
+	"""
+	# Get the name of the calling module.
+	modName = _getCallerModule(1).__name__
+	# Place the symbols into an indexable data structure for more efficient access.
+	deprecatedSymbols = {symbol.name: symbol for symbol in deprecated}
+
+	def module_getattr(attrName: str) -> Any:
+		if NVDAState._allowDeprecatedAPI():
+			if attrName in deprecatedSymbols:
+				deprecatedSymbol = deprecatedSymbols[attrName]
+				# TODO: #17783: switch to using warnings.warn when NVDA's support for it matures.
+				log.warning(
+					deprecatedSymbol.getLogMessage(modName),
+					stack_info=True,
+					# TODO: #18785: add stacklevel parameter so the stack trace is less noisy
+				)
+				return deprecatedSymbol.value
+		raise AttributeError(f"module {modName!r} has no attribute {attrName!r}")
+
+	return module_getattr

source/winAPI/constants.py
@@ -1,5 +1,5 @@
 # A part of NonVisual Desktop Access (NVDA)
-# Copyright (C) 2022-2024 NV Access Limited
+# Copyright (C) 2022-2025 NV Access Limited
 # This file is covered by the GNU General Public License.
 # See the file COPYING for more details.
 
@@ -31,5 +31,9 @@ class SystemErrorCodes(enum.IntEnum):
 	SHARING_VIOLATION = 0x20
 	"""The process cannot access the file because it is being used by another process."""
 	INVALID_PARAMETER = 0x57
+	INSUFFICIENT_BUFFER = 0x7A
+	"""The data area passed to a system call is too small."""
 	MOD_NOT_FOUND = 0x7E
+	NO_MORE_ITEMS = 0x103
+	"""No more data is available."""
 	CANCELLED = 0x4C7

source/winBindings/advapi32.py
@@ -13,9 +13,14 @@ from ctypes.wintypes import (
 	BOOL,
 	DWORD,
 	HANDLE,
+	HKEY,
+	LONG,
 )
 
-__all__ = ("OpenProcessToken",)
+__all__ = (
+	"OpenProcessToken",
+	"RegCloseKey",
+)
 
 
 dll = windll.advapi32
@@ -31,3 +36,15 @@ OpenProcessToken.argtypes = (
 	POINTER(HANDLE),  # TokenHandle
 )
 OpenProcessToken.restype = BOOL
+
+RegCloseKey = dll.RegCloseKey
+"""
+Closes a handle to the specified registry key.
+
+..seealso::
+	https://learn.microsoft.com/en-us/windows/win32/api/winreg/nf-winreg-regclosekey
+"""
+RegCloseKey.argtypes = (
+	HKEY,  # hKey
+)
+RegCloseKey.restype = LONG

source/winBindings/bthprops.py
@@ -1,0 +1,57 @@
+# A part of NonVisual Desktop Access (NVDA)
+# Copyright (C) 2025 NV Access Limited
+# This file may be used under the terms of the GNU General Public License, version 2 or later, as modified by the NVDA license.
+# For full terms and any additional permissions, see the NVDA license file: https://github.com/nvaccess/nvda/blob/master/copying.txt
+
+"""Functions exported by bthprops.cpl, and supporting data structures and enumerations."""
+
+from ctypes import POINTER, Structure, c_ulonglong, sizeof, windll
+from ctypes.wintypes import BOOL, DWORD, HANDLE, ULONG, WCHAR
+
+from winKernel import SYSTEMTIME
+
+cpl = windll["bthprops.cpl"]
+
+BLUETOOTH_ADDRESS = c_ulonglong
+BLUETOOTH_MAX_NAME_SIZE = 248
+
+
+class BLUETOOTH_DEVICE_INFO(Structure):
+	"""
+	Provides information about a Bluetooth device.
+
+	..seealso::
+		https://learn.microsoft.com/en-us/windows/win32/api/bluetoothapis/ns-bluetoothapis-bluetooth_device_info_struct
+	"""
+
+	_fields_ = (
+		("dwSize", DWORD),
+		("address", BLUETOOTH_ADDRESS),
+		("ulClassofDevice", ULONG),
+		("fConnected", BOOL),
+		("fRemembered", BOOL),
+		("fAuthenticated", BOOL),
+		("stLastSeen", SYSTEMTIME),
+		("stLastUsed", SYSTEMTIME),
+		("szName", WCHAR * BLUETOOTH_MAX_NAME_SIZE),
+	)
+
+	def __init__(self, **kwargs):
+		super().__init__(dwSize=sizeof(self), **kwargs)
+
+
+BLUETOOTH_DEVICE_INFO_P = POINTER(BLUETOOTH_DEVICE_INFO)
+
+
+BluetoothGetDeviceInfo = cpl.BluetoothGetDeviceInfo
+"""
+Retrieves information about a remote Bluetooth device which has been identified through a successful device inquiry function call.
+
+..seealso::
+	https://learn.microsoft.com/en-us/windows/win32/api/bluetoothapis/nf-bluetoothapis-bluetoothgetdeviceinfo
+"""
+BluetoothGetDeviceInfo.argtypes = (
+	HANDLE,  # hRadio
+	BLUETOOTH_DEVICE_INFO_P,  # pbtdi
+)
+BluetoothGetDeviceInfo.restype = DWORD

source/winBindings/cfgmgr32.py
@@ -1,0 +1,29 @@
+# A part of NonVisual Desktop Access (NVDA)
+# Copyright (C) 2025 NV Access Limited
+# This file is covered by the GNU General Public License.
+# See the file COPYING for more details.
+
+"""Functions exported by cfgmgr32.dll, and supporting data structures and enumerations."""
+
+from ctypes import c_wchar_p, windll
+from ctypes.wintypes import DWORD, ULONG
+
+dll = windll.cfgmgr32
+
+CR_SUCCESS = 0
+MAX_DEVICE_ID_LEN = 200
+
+CM_Get_Device_ID = dll.CM_Get_Device_IDW
+"""
+Retrieves the device instance ID for a specified device instance on the local machine.
+
+..seealso::
+	https://learn.microsoft.com/en-us/windows/win32/api/cfgmgr32/nf-cfgmgr32-cm_get_device_idw
+"""
+CM_Get_Device_ID.argtypes = (
+	DWORD,  # dnDevInst
+	c_wchar_p,  # Buffer
+	ULONG,  # BufferLen
+	ULONG,  # ulFlags
+)
+CM_Get_Device_ID.restype = DWORD

source/winBindings/hid.py
@@ -1,0 +1,133 @@
+# A part of NonVisual Desktop Access (NVDA)
+# Copyright (C) 2025 NV Access Limited
+# This file is covered by the GNU General Public License.
+# See the file COPYING for more details.
+
+"""Functions exported by hid.dll, and supporting data structures and enumerations."""
+
+from ctypes import POINTER, Structure, c_void_p, sizeof, windll
+from ctypes.wintypes import BOOLEAN, HANDLE, ULONG, USHORT
+
+from comtypes import GUID
+from hidpi import HIDP_CAPS, NTSTATUS
+
+dll = windll.hid
+
+
+class HIDD_ATTRIBUTES(Structure):
+	"""
+	The HIDD_ATTRIBUTES structure contains vendor information about a HIDClass device.
+
+	..seealso::
+		https://learn.microsoft.com/en-us/windows-hardware/drivers/ddi/hidsdi/ns-hidsdi-_hidd_attributes
+	"""
+
+	_fields_ = (
+		("Size", ULONG),
+		("VendorID", USHORT),
+		("ProductID", USHORT),
+		("VersionNumber", USHORT),
+	)
+
+	def __init__(self, **kwargs):
+		super().__init__(Size=sizeof(HIDD_ATTRIBUTES), **kwargs)
+
+
+PHID_ATTRIBUTES = POINTER(HIDD_ATTRIBUTES)
+
+HidD_GetAttributes = dll.HidD_GetAttributes
+"""
+The HidD_GetAttributes routine returns the attributes of a specified top-level collection.
+
+..seealso::
+	https://learn.microsoft.com/en-us/windows-hardware/drivers/ddi/hidsdi/nf-hidsdi-hidd_getattributes
+"""
+HidD_GetAttributes.argtypes = (
+	HANDLE,  # HidDeviceObject
+	PHID_ATTRIBUTES,  # Attributes
+)
+HidD_GetAttributes.restype = BOOLEAN
+
+HidD_GetManufacturerString = dll.HidD_GetManufacturerString
+"""
+Returns a top-level collection's embedded string that identifies the manufacturer.
+
+..seealso::
+	https://learn.microsoft.com/en-us/windows-hardware/drivers/ddi/hidsdi/nf-hidsdi-hidd_getmanufacturerstring
+"""
+HidD_GetManufacturerString.argtypes = (
+	HANDLE,  # HidDeviceObject
+	c_void_p,  # Buffer
+	ULONG,  # BufferLength
+)
+HidD_GetManufacturerString.restype = BOOLEAN
+
+HidD_GetProductString = dll.HidD_GetProductString
+"""
+The HidD_GetProductString routine returns the embedded string of a top-level collection that identifies the manufacturer's product.
+
+..seealso::
+	https://learn.microsoft.com/en-us/windows-hardware/drivers/ddi/hidsdi/nf-hidsdi-hidd_getproductstring
+"""
+HidD_GetProductString.argtypes = (
+	HANDLE,  # HidDeviceObject
+	c_void_p,  # Buffer
+	ULONG,  # BufferLength
+)
+HidD_GetProductString.restype = BOOLEAN
+
+PHIDP_PREPARSED_DATA = c_void_p
+
+HidD_GetPreparsedData = dll.HidD_GetPreparsedData
+"""
+The HidD_GetPreparsedData routine returns a top-level collection's preparsed data.
+
+..seealso::
+	https://learn.microsoft.com/en-us/windows-hardware/drivers/ddi/hidsdi/nf-hidsdi-hidd_getpreparseddata
+"""
+HidD_GetPreparsedData.argtypes = (
+	HANDLE,  # HidDeviceObject
+	PHIDP_PREPARSED_DATA,  # PreparsedData
+)
+HidD_GetPreparsedData.restype = BOOLEAN
+
+HidD_FreePreparsedData = dll.HidD_FreePreparsedData
+"""
+The HidD_FreePreparsedData routine releases the resources that the HID class driver allocated to hold a top-level collection's preparsed data.
+
+..seealso::
+	https://learn.microsoft.com/en-us/windows-hardware/drivers/ddi/hidsdi/nf-hidsdi-Hidd_freepreparseddata
+"""
+HidD_FreePreparsedData.argtypes = (
+	PHIDP_PREPARSED_DATA,  # PreparsedData
+)
+HidD_FreePreparsedData.restype = BOOLEAN
+
+PHIDP_CAPS = POINTER(HIDP_CAPS)
+
+HidP_GetCaps = dll.HidP_GetCaps
+"""
+Returns a top-level collection's HIDP_CAPS structure.
+
+..seealso::
+	https://learn.microsoft.com/en-us/windows-hardware/drivers/ddi/hidpi/nf-hidpi-hidp_getcaps
+"""
+HidP_GetCaps.argtypes = (
+	PHIDP_PREPARSED_DATA,  # PreparsedData
+	PHIDP_CAPS,  # Capabilities
+)
+HidP_GetCaps.restype = NTSTATUS
+
+LPGUID = POINTER(GUID)
+
+HidD_GetHidGuid = dll.HidD_GetHidGuid
+"""
+Returns the device interface GUID for HIDClass devices.
+
+..seealso::
+	https://learn.microsoft.com/en-us/windows-hardware/drivers/ddi/hidsdi/nf-hidsdi-hidd_gethidguid
+"""
+HidD_GetHidGuid.argtypes = (
+	LPGUID,  # HidGuid
+)
+HidD_GetHidGuid.restype = None

source/winBindings/setupapi.py
@@ -1,0 +1,294 @@
+# A part of NonVisual Desktop Access (NVDA)
+# Copyright (C) 2025 NV Access Limited
+# This file is covered by the GNU General Public License.
+# See the file COPYING for more details.
+
+"""Functions exported by setupapi.dll, and supporting data structures and enumerations."""
+
+from ctypes import POINTER, Structure, WinError, c_void_p, c_wchar_p, sizeof, windll
+from ctypes.wintypes import BOOL, DWORD, HKEY, HWND, PDWORD, PULONG, ULONG, WCHAR
+from enum import IntEnum
+
+from comtypes import GUID
+
+dll = windll.setupapi
+
+
+class DIGCF(IntEnum):
+	"""Possible flags for the ``Flags`` parameter of ``SetupDiGetClassDevs``."""
+
+	PRESENT = 0x02
+	"""Return only devices that are currently present in a system."""
+
+	DEVICEINTERFACE = 0x10
+	"""Return devices that support device interfaces for the specified device interface classes."""
+
+
+class SPDRP(IntEnum):
+	"""Possible values for the ``Property`` parameter to ``SetupDiGetDeviceRegistryProperty``."""
+
+	DEVICEDESC = 0x00
+	"""The function retrieves a ``REG_SZ`` string that contains the description of a device."""
+
+	HARDWAREID = 0x01
+	"""The function retrieves a ``REG_MULTI_SZ`` string that contains the list of hardware IDs for a device."""
+
+	FRIENDLYNAME = 0x0C
+	"""The function retrieves a ``REG_SZ`` string that contains the friendly name of a device."""
+
+	LOCATION_INFORMATION = 0x0D
+	"""The function retrieves a ``REG_SZ`` string that contains the hardware location of a device."""
+
+
+class DICS_FLAG(IntEnum):
+	"""Possible values of the ``Scope`` parameter of ``SetupDiOpenDevRegKey``."""
+
+	GLOBAL = 0x01
+	"""Open a key to store global configuration information, rooted at HKEY_LOCAL_MACHINE."""
+
+	CONFIGSPECIFIC = 0x02
+	"""Open a key to store hardware profile-specific configuration information, rooted at one of the hardware-profile specific branches, instead of HKEY_LOCAL_MACHINE."""
+
+
+class DIREG(IntEnum):
+	"""Possible values of the ``KeyType`` parameter to ``SetupDiOpenDevRegKey``."""
+
+	DEV = 0x01
+	"""Open a hardware key for the device."""
+
+	DRV = 0x02
+	"""Open a software key for the device."""
+
+
+HDEVINFO = c_void_p
+
+
+class DEVPROPKEY(Structure):
+	"""
+	Represents a device property key for a device property in the unified device property model.
+
+	..seealso::
+		https://learn.microsoft.com/en-us/windows-hardware/drivers/install/devpropkey
+	"""
+
+	_fields_ = (
+		("DEVPROPGUID", GUID),
+		("DEVPROPID", ULONG),
+	)
+
+
+GUID_CLASS_COMPORT = GUID("{86e0d1e0-8089-11d0-9ce4-08003e301f73}")
+"""
+Identifier for the device interface class for devices that support a 16550 UART-compatible hardware interface.
+
+..seealso::
+	https://learn.microsoft.com/en-us/windows-hardware/drivers/install/guid-class-comport
+"""
+
+GUID_DEVINTERFACE_USB_DEVICE = GUID("{a5dcbf10-6530-11d2-901f-00c04fb951ed}")
+"""
+The GUID_DEVINTERFACE_USB_DEVICE device interface class is defined for USB devices that are attached to a USB hub.
+
+..seealso::
+	https://learn.microsoft.com/en-us/windows-hardware/drivers/install/guid-devinterface-usb-device
+"""
+
+DEVPKEY_Device_BusReportedDeviceDesc = DEVPROPKEY(GUID("{540b947e-8b40-45bc-a8a2-6a0b894cbda2}"), 4)
+"""
+Represents a string value that was reported by the bus driver for the device instance.
+
+..seealso::
+	https://learn.microsoft.com/en-us/windows-hardware/drivers/install/devpkey-device-busreporteddevicedesc
+"""
+
+
+class SP_DEVINFO_DATA(Structure):
+	"""
+	An SP_DEVINFO_DATA structure defines a device instance that is a member of a device information set.
+
+	..seealso::
+		https://learn.microsoft.com/en-us/windows/win32/api/setupapi/ns-setupapi-sp_devinfo_data
+	"""
+
+	_fields_ = (
+		("cbSize", DWORD),
+		("ClassGuid", GUID),
+		("DevInst", DWORD),
+		("Reserved", PULONG),
+	)
+
+	def __str__(self):
+		return f"ClassGuid:{self.ClassGuid} DevInst:{self.DevInst}"
+
+
+PSP_DEVINFO_DATA = POINTER(SP_DEVINFO_DATA)
+
+
+class SP_DEVICE_INTERFACE_DATA(Structure):
+	"""
+	Defines a device interface in a device information set.
+
+	..seealso::
+		https://learn.microsoft.com/en-us/windows/win32/api/setupapi/ns-setupapi-sp_device_interface_data
+	"""
+
+	_fields_ = (
+		("cbSize", DWORD),
+		("InterfaceClassGuid", GUID),
+		("Flags", DWORD),
+		("Reserved", PULONG),
+	)
+
+	def __str__(self):
+		return f"InterfaceClassGuid:{self.InterfaceClassGuid} Flags:{self.Flags}"
+
+
+PSP_DEVICE_INTERFACE_DATA = POINTER(SP_DEVICE_INTERFACE_DATA)
+
+
+class _Dummy(Structure):
+	_fields_ = (("d1", DWORD), ("d2", WCHAR))
+	# SetupAPI.h in the Windows headers includes pshpack8.h when 64 bit, pshpack1.h otherwise
+	_pack_ = 8 if sizeof(c_void_p) == 8 else 1
+
+
+SIZEOF_SP_DEVICE_INTERFACE_DETAIL_DATA_W = sizeof(_Dummy)
+
+PSP_DEVICE_INTERFACE_DETAIL_DATA = c_void_p
+
+
+SetupDiDestroyDeviceInfoList = dll.SetupDiDestroyDeviceInfoList
+"""
+Deletes a device information set and frees all associated memory.
+
+..seealso::
+	https://learn.microsoft.com/en-us/windows/win32/api/setupapi/nf-setupapi-setupdidestroydeviceinfolist
+"""
+SetupDiDestroyDeviceInfoList.argtypes = (
+	HDEVINFO,  # DeviceInfoSet
+)
+SetupDiDestroyDeviceInfoList.restype = BOOL
+
+
+def _validHandle_errcheck(res, func, args):
+	if res == 0:
+		raise WinError()
+	return res
+
+
+SetupDiGetClassDevs = dll.SetupDiGetClassDevsW
+"""
+Returns a handle to a device information set that contains requested device information elements for a local computer.
+
+..seealso::
+	https://learn.microsoft.com/en-us/windows/win32/api/setupapi/nf-setupapi-setupdigetclassdevsw
+"""
+SetupDiGetClassDevs.argtypes = (
+	POINTER(GUID),  # ClassGuid
+	c_wchar_p,  # Enumerator
+	HWND,  # hwndParent
+	DWORD,  # Flags
+)
+SetupDiGetClassDevs.restype = HDEVINFO
+SetupDiGetClassDevs.errcheck = _validHandle_errcheck  # HDEVINFO
+
+SetupDiGetDeviceProperty = dll.SetupDiGetDevicePropertyW
+"""
+The SetupDiGetDeviceProperty function retrieves a device instance property.
+
+..seealso::
+	https://learn.microsoft.com/en-us/windows/win32/api/setupapi/nf-setupapi-setupdigetdevicepropertyw
+"""
+SetupDiGetDeviceProperty.argtypes = (
+	HDEVINFO,  # DeviceInfoSet
+	PSP_DEVINFO_DATA,  # DeviceInfoData
+	POINTER(DEVPROPKEY),  # PropertyKey
+	PDWORD,  # PropertyType
+	c_void_p,  # PropertyBuffer
+	DWORD,  # PropertyBufferSize
+	PDWORD,  # RequiredSize
+	DWORD,  # Flags
+)
+SetupDiGetDeviceProperty.restype = BOOL
+
+SetupDiEnumDeviceInterfaces = dll.SetupDiEnumDeviceInterfaces
+"""
+Enumerates the device interfaces that are contained in a device information set.
+
+..seealso::
+	https://learn.microsoft.com/en-us/windows/win32/api/setupapi/nf-setupapi-setupdienumdeviceinterfaces
+"""
+SetupDiEnumDeviceInterfaces.argtypes = (
+	HDEVINFO,  # DeviceInfoSet
+	PSP_DEVINFO_DATA,  # DeviceInfoData
+	POINTER(GUID),  # InterfaceClassGuid
+	DWORD,  # MemberIndex
+	PSP_DEVICE_INTERFACE_DATA,  # DeviceInterfaceData
+)
+SetupDiEnumDeviceInterfaces.restype = BOOL
+
+SetupDiGetDeviceInterfaceDetail = dll.SetupDiGetDeviceInterfaceDetailW
+"""
+Returns details about a device interface.
+
+..seealso::
+	https://learn.microsoft.com/en-us/windows/win32/api/setupapi/nf-setupapi-setupdigetdeviceinterfacedetailw
+"""
+SetupDiGetDeviceInterfaceDetail.argtypes = (
+	HDEVINFO,  # DeviceInfoSet
+	PSP_DEVICE_INTERFACE_DATA,  # DeviceInterfaceData
+	PSP_DEVICE_INTERFACE_DETAIL_DATA,  # DeviceInterfaceDetailData
+	DWORD,  # DeviceInterfaceDetailDataSize
+	PDWORD,  # RequiredSize
+	PSP_DEVINFO_DATA,  # DeviceInfoData
+)
+SetupDiGetDeviceInterfaceDetail.restype = BOOL
+
+SetupDiGetDeviceRegistryProperty = dll.SetupDiGetDeviceRegistryPropertyW
+"""
+Retrieves a specified Plug and Play device property.
+
+..seealso::
+	https://learn.microsoft.com/en-us/windows/win32/api/setupapi/nf-setupapi-setupdigetdeviceregistrypropertyw
+"""
+SetupDiGetDeviceRegistryProperty.argtypes = (
+	HDEVINFO,  # DeviceInfoSet
+	PSP_DEVINFO_DATA,  # DeviceInfoData
+	DWORD,  # Property
+	PDWORD,  # PropertyRegDataType
+	c_void_p,  # PropertyBuffer
+	DWORD,  # PropertyBufferSize
+	PDWORD,  # RequiredSize
+)
+SetupDiGetDeviceRegistryProperty.restype = BOOL
+
+SetupDiEnumDeviceInfo = dll.SetupDiEnumDeviceInfo
+"""
+Returns a SP_DEVINFO_DATA structure that specifies a device information element in a device information set.
+
+..seealso::
+	https://learn.microsoft.com/en-us/windows/win32/api/setupapi/nf-setupapi-setupdienumdeviceinfo
+"""
+SetupDiEnumDeviceInfo.argtypes = (
+	HDEVINFO,  # DeviceInfoSet
+	DWORD,  # MemberIndex
+	PSP_DEVINFO_DATA,  # DeviceInfoData
+)
+SetupDiEnumDeviceInfo.restype = BOOL
+
+SetupDiOpenDevRegKey = dll.SetupDiOpenDevRegKey
+"""
+Opens a registry key for device-specific configuration information.
+
+..seealso::
+	https://learn.microsoft.com/en-us/windows/win32/api/setupapi/nf-setupapi-setupdiopendevregkey
+"""
+SetupDiOpenDevRegKey.argtypes = (
+	HDEVINFO,  # DeviceInfoSet
+	PSP_DEVINFO_DATA,  # DeviceInfoData
+	DWORD,  # Scope
+	DWORD,  # HwProfile
+	DWORD,  # KeyType
+	DWORD,  # samDesired
+)
+SetupDiOpenDevRegKey.restype = HKEY

commit c30a78776d82581c39f085f17f0338e91994555a

Fix utils._deprecate to give the right module name when not running from source (#18823)
Follow-up to #18571

Summary of the issue:
The deprecation helper introduced in #18571 automatically detects the
module it's being used in. This detection is broken when running a
portable or installed copy.

Description of user facing changes:
None.

Description of developer facing changes:
The module is now reported correctly when emitting deprecation warnings.

Description of development approach:
Rather than using `inspect.getmodule`, get `__name__` from the frame's
globals.

Testing strategy:
Tested importing `BLUETOOTH_ADDRESS` from `hwPortUtils` from source and
in a portable (created with `scons dist`).

Applied the following patch and attempted importing
`_remoteClient.client.seven` when running from source and portable.

```patch
diff --git a/source/_remoteClient/client.py b/source/_remoteClient/client.py
index 4691a226f..541ec8f2a 100644
--- a/source/_remoteClient/client.py
+++ b/source/_remoteClient/client.py
@@ -743,3 +743,8 @@ def connectedClientsCount(self) -> int:
                        stack_info=True,
                )
                return 0
+
+from utils._deprecate import handleDeprecations, RemovedSymbol
+__getattr__ = handleDeprecations(
+       RemovedSymbol("seven", 7)
+)
```

Known issues with pull request:
None

source/utils/_deprecate.py
@@ -136,8 +136,8 @@ def _getCallerModule(level: int = 0) -> ModuleType:
 		This can be used if calling from a helper function to skip the stack frame created by the helper.
 	:return: The module from which this function was called.
 	"""
-	# If not part of a module, the caller must be in ``__main__``.
-	return inspect.getmodule(inspect.stack()[level + 1].frame) or sys.modules["__main__"]
+	moduleName = inspect.stack()[level + 1].frame.f_globals["__name__"]
+	return sys.modules[moduleName]
 
 
 def handleDeprecations(