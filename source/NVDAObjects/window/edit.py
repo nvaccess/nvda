nvaccess/nvda

commit 8742b3a613c29bf636374c6c6696c9a8553c7e87

Fix OffsetTextInfo.collapse() and OffsetTextInfo.move() (#18348)
Fixes #18320.

This is my second attempt to fix it without breaking #17430. For the
reference, the first attempt was #18338, which is abandoned, and which
didn't fix the root cause, but just decoupled @nvdaes's commit from
TextInfo API. This PR however fixes the root cause of #17430.

Summary of the issue:
Commit 70cd311 by @nvdaes introduced a bug in TextInfo API:
`TextInfo.collapse()` now unnecessarily jumps to the next paragraph in
Notepad++. This bug also made its way into 2025.1 release.

I investigated the braille issue #17430 further and it boiled down to an
issue with `OffsetTextInfo.move()` implementation: it turns out that
unless it moves by character, it is unable to move to the very last
position in the document. Fixing `OffsetTextInfo.move()` implementation.

Description of user facing changes:
None

Description of developer facing changes:
`OffsetTextInfo.collapse()` now works correctly in Notepad++.
`OffsetTextInfo.move()` can now move to the very last position even when
unit is set to line or paragraph.

Description of development approach:
1. In `OffsetTextInfo.move()` modified the condition when
   `self.allowMoveToOffsetPastEnd` to increase `highLimit` regardless of
   current unit - previously this would only allow when unit is character.
2. Removed `ScintillaTextInfo.expand()` override.

Testing strategy:
1. Tested with use case provided in #18320.
2. Tested that `OffsetTextInfo.move('line', 1)` can now successfully go
   to the very last line in Notepad++ even if the last line is empty. This
   should be the proper fix for #17430.

Known issues with pull request:
None

source/textInfos/offsets.py
@@ -648,7 +648,11 @@ class OffsetsTextInfo(textInfos.TextInfo):
 		else:
 			raise NotImplementedError
 
-	allowMoveToOffsetPastEnd = True  #: move with unit_character can move 1 past story length to allow braille routing to end insertion point. (#2096)
+	allowMoveToOffsetPastEnd = True
+	"""
+	We can move 1 past story length to allow braille routing to end insertion point. (#2096)
+	Furthermore, review cursor is able to reach the last, empty line in some controls, like Scintilla. (#18348)
+	"""
 
 	def move(self, unit, direction, endPoint=None):
 		if direction == 0:
@@ -664,7 +668,7 @@ class OffsetsTextInfo(textInfos.TextInfo):
 		count = 0
 		lowLimit = 0
 		highLimit = self._getStoryLength()
-		if self.allowMoveToOffsetPastEnd and unit == textInfos.UNIT_CHARACTER:
+		if self.allowMoveToOffsetPastEnd:
 			# #2096: There is often an uncounted character at the end of the text
 			# where the caret is placed to append text.
 			highLimit += 1

source/NVDAObjects/window/scintilla.py
@@ -313,16 +313,6 @@ class ScintillaTextInfo(textInfos.offsets.OffsetsTextInfo):
 				tempOffset -= 1
 		return [start, end]
 
-	def collapse(self, end: bool = False):
-		"""Before collapsing to end, if no text is selected, TextInfo is expanded to line.
-		This fixes a bug where next braille line command didn't move the cursor to the last empty line
-		in Notepad++ documents.
-		https://github.com/nvaccess/nvda/issues/17430
-		"""
-		if end and self.obj.makeTextInfo(textInfos.POSITION_SELECTION).isCollapsed:
-			self.expand(textInfos.UNIT_LINE)
-		super().collapse(end=end)
-
 
 # The Scintilla NVDA object, inherists the generic MSAA NVDA object
 class Scintilla(EditableTextWithAutoSelectDetection, Window):

tests/system/robot/chromeTests.py
@@ -927,7 +927,7 @@ def test_pr11606():
 	actualSpeech = _chrome.getSpeechAfterKey("end")
 	_asserts.strings_match(
 		actualSpeech,
-		"link",
+		"blank",
 	)
 	# Read the current line.
 	# Before pr #11606 the next line ("C D")  would have been read.

commit a514710ebc4a5f116322700f53d548e8082fe310

Fix off by one text length in EditTextInfo (#18767)
Link to issue number:
Related to #18744 
Follow up of #18348 

Summary of the issue:
When working on #18744, I again stumbled upon the fact that the story
length reported by EditTextInfo is too high. This was discovered after
system test failures caused by #18348, i.e. reporting of an extraneous
blank at the end of the document when using NVDA review cursor. Note,
this is not a bug in #18348 and was accounted for in #18744 after the
test failure.

To reproduce
1. Open notepad classic
2. type `a`
3. Position the caret at the end of the line
4. Press nvda+delete or nvda+numpad delete

Observe that NVDA reports 50%, not 100%
Observe that `nav.makeTextInfo("caret")._getStoryLength()` reports 2,
not 1

Description of user facing changes:
None

Description of developer facing changes:
None

Description of development approach:
Fixed the off by one errors by removing the `+ 1` cases.

Testing strategy:
Tested str from this pr.

Known issues with pull request:
None

source/NVDAObjects/window/edit.py
@@ -465,7 +465,7 @@ class EditTextInfo(textInfos.offsets.OffsetsTextInfo):
 
 	def _getStoryText(self):
 		if controlTypes.State.PROTECTED in self.obj.states:
-			return "*" * (self._getStoryLength() - 1)
+			return "*" * self._getStoryLength()
 		return self.obj.windowText
 
 	def _getStoryLength(self):
@@ -500,18 +500,12 @@ class EditTextInfo(textInfos.offsets.OffsetsTextInfo):
 				)
 			finally:
 				winKernel.virtualFreeEx(processHandle, internalInfo, 0, winKernel.MEM_RELEASE)
-			# Py3 review: investigation with Python 2 NVDA revealed that
-			# adding 1 to this creates an off by one error.
-			# Tested using Wordpad, enforcing EditTextInfo as the textInfo implementation.
-			return textLen + 1
+			return textLen
 		else:
 			# ForWM_GETTEXTLENGTH documentation, see
 			# https://docs.microsoft.com/en-us/windows/desktop/winmsg/wm-gettextlength
 			# It determines the length, in characters, of the text associated with a window.
-			# Py3 review: investigation with Python 2 NVDA revealed that
-			# adding 1 to this created an off by one error.
-			# Tested using Notepad
-			return watchdog.cancellableSendMessage(self.obj.windowHandle, winUser.WM_GETTEXTLENGTH, 0, 0) + 1
+			return watchdog.cancellableSendMessage(self.obj.windowHandle, winUser.WM_GETTEXTLENGTH, 0, 0)
 
 	def _getLineCount(self):
 		return self.obj.windowTextLineCount

source/textInfos/offsets.py
@@ -358,7 +358,7 @@ class OffsetsTextInfo(textInfos.TextInfo):
 			ctypes.byref(relEnd),
 		):
 			relStart = relStart.value
-			relEnd = min(lineLength, relEnd.value)
+			relEnd = relEnd.value
 			if self.encoding != textUtils.WCHAR_ENCODING:
 				# We need to convert the uniscribe based offsets to str offsets.
 				relStart, relEnd = offsetConverter.encodedToStrOffsets(relStart, relEnd)

tests/system/robot/symbolPronunciationTests.py
@@ -596,23 +596,12 @@ def test_symbolInSpeechUI():
 	"""Replace a translation string to include a character that is can be substituted,
 	check if the 'speech UI' translation string the character substituted.
 	"""
-	_notepad.prepareNotepad(
-		(
-			"t"  # Character doesn't matter, we just want to invoke "Right" speech UI.
-		),
-	)
+	character = "t"  # Character doesn't matter, we just want to invoke "Right" speech UI.
+	_notepad.prepareNotepad(character)
 	_setConfig(SymLevel.ALL)
 	spy = _NvdaLib.getSpyLib()
 	expected = "shouldn't sub tick symbol"
 	spy.override_translationString(EndSpeech.RIGHT.value, expected)
-
-	# get to the end char
-	actual = _pressKeyAndCollectSpeech(Move.REVIEW_CHAR.value, numberOfTimes=1)
-	_builtIn.should_be_equal(
-		actual,
-		["blank"],
-		msg="actual vs expected. Unexpected speech when moving to final character.",
-	)
 
 	actual = _pressKeyAndCollectSpeech(Move.REVIEW_CHAR.value, numberOfTimes=1)
 	_builtIn.should_be_equal(
@@ -622,7 +611,7 @@ def test_symbolInSpeechUI():
 		[
 			# todo: 'tick' is a bug
 			"shouldn tick t sub tick symbol"  # intentionally concatenate strings
-			"\nblank",
+			f"\n{character}",
 		],
 		msg="actual vs expected. NVDA speech UI substitutes symbols",
 	)
@@ -632,7 +621,7 @@ def test_symbolInSpeechUI():
 	actual = _pressKeyAndCollectSpeech(Move.REVIEW_CHAR.value, numberOfTimes=1)
 	_builtIn.should_be_equal(
 		actual,
-		[f"{expected}\nblank"],
+		[f"{expected}\n{character}"],
 		msg="actual vs expected. NVDA speech UI substitutes symbols",
 	)
 