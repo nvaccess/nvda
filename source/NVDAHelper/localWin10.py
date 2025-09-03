9dcb5343dec87a7d295e003418d63bb94579361d
Sascha Cowley <16543535+SaschaCowley@users.noreply.github.com>
2025-09-03 14:45:27 +1000

    Move ctypes bindings to NVDAHelper

---
+++ source/NVDAHelper/localWin10.py
@@ -0,0 +1,37 @@
+from ctypes import c_int, c_void_p, cdll, wstring_at
+
+import NVDAState
+
+dll = cdll.LoadLibrary(NVDAState.ReadPaths.nvdaHelperLocalWin10Dll)
+
+
+def _bstrReturnErrcheck(address: int, *_) -> str:
+	"""Handle a BSTR returned from a ctypes function call.
+	This includes freeing the memory.
+	This is needed for nvdaHelperLocalWin10 functions which return a BSTR.
+	"""
+	# comtypes.BSTR.from_address seems to cause a crash for some reason. Not sure why.
+	# Just access the string ourselves.
+	# This will terminate at a null character, even though BSTR allows nulls.
+	# We're only using this for normal, null-terminated strings anyway.
+	# import comtypes
+	# val = comtypes.BSTR.from_param(address)
+	val = wstring_at(address)
+	# windll.oleaut32.SysFreeString(address)
+	return val
+
+
+uwpOcr_getLanguages = dll.uwpOcr_getLanguages
+uwpOcr_getLanguages.argtypes = ()
+uwpOcr_getLanguages.restype = c_void_p
+uwpOcr_getLanguages.errcheck = _bstrReturnErrcheck
+
+uwpOcr_initialize = dll.uwpOcr_initialize
+uwpOcr_initialize.restype = c_void_p
+
+uwpOcr_terminate = dll.uwpOcr_terminate
+uwpOcr_terminate.argtypes = [c_void_p]
+uwpOcr_terminate.restype = None
+
+uwpOcr_recognize = dll.uwpOcr_recognize
+uwpOcr_recognize.argtypes = [c_void_p, c_void_p, c_int, c_int]
--- source/contentRecog/uwpOcr.py
+++ source/contentRecog/uwpOcr.py
@@ -9,6 +9,7 @@ import ctypes
 from ctypes.wintypes import LPWSTR, WCHAR
 import json
 import NVDAHelper
+from NVDAHelper.localWin10 import uwpOcr_getLanguages, uwpOcr_initialize, uwpOcr_recognize, uwpOcr_terminate
 from . import ContentRecognizer, LinesWordsResult
 import config
 import languageHandler
@@ -23,10 +24,7 @@ def getLanguages():
 		for use as NVDA language codes.
 	@rtype: list of str
 	"""
-	dll = NVDAHelper.getHelperLocalWin10Dll()
-	dll.uwpOcr_getLanguages.restype = ctypes.c_void_p
-	dll.uwpOcr_getLanguages.errcheck = NVDAHelper.bstrReturnErrcheck
-	langs = dll.uwpOcr_getLanguages()
+	langs = uwpOcr_getLanguages()
 	return langs.split(";")[:-1]
 
 
@@ -110,21 +108,15 @@ class UwpOcr(ContentRecognizer):
 					self._onResult(LinesWordsResult(data, imgInfo))
 				else:
 					self._onResult(RuntimeError("UWP OCR failed"))
-			uwpOcr_terminate = self._dll.uwpOcr_terminate
-			uwpOcr_terminate.argtypes = [ctypes.c_void_p]
-			self._dll.uwpOcr_terminate(self._handle)
+			uwpOcr_terminate(self._handle)
 			self._callback = None
 			self._handle = None
 
 		self._callback = callback
-		uwpOcr_initialize = self._dll.uwpOcr_initialize
-		uwpOcr_initialize.restype = ctypes.c_void_p
 		self._handle = uwpOcr_initialize(self.language, callback)
 		if not self._handle:
 			onResult(RuntimeError("UWP OCR initialization failed"))
 			return
-		uwpOcr_recognize = self._dll.uwpOcr_recognize
-		uwpOcr_recognize.argtypes = [ctypes.c_void_p, ctypes.c_void_p, ctypes.c_int, ctypes.c_int]
 		uwpOcr_recognize(self._handle, pixels, imgInfo.recogWidth, imgInfo.recogHeight)
 
 	def cancel(self):
