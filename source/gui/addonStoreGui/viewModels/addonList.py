bcee647fd9cbaf12a230f82cbb957263d107e492
Noelia Ruiz Martínez <nrm1977@gmail.com>
2025-09-03 06:12:24 +0200

    Check if isinstance is store or manifest model, for external channel

--- source/gui/addonStoreGui/viewModels/addonList.py
+++ source/gui/addonStoreGui/viewModels/addonList.py
@@ -418,12 +418,13 @@ class AddonListVM:
 		def _getSortFieldData(listItemVM: AddonListItemVM) -> "_SupportsLessThan":
 			if self._sortByModelField == AddonListField.publicationDate:
 				listItemVM = cast(AddonListItemVM[_AddonStoreModel], listItemVM)
-				return listItemVM.model.submissionTime
+				if isinstance(AddonListItemVM, _AddonStoreModel):
+					return listItemVM.model.submissionTime
 				return 0
 			if self._sortByModelField == AddonListField.installDate:
-				listItemVM = cast(AddonListItemVM[_addonHandlerModel], listItemVM)
-
-				return listItemVM.model.installDate
+				listItemVM = cast(AddonListItemVM[_AddonManifestModel], listItemVM)
+				if isinstance(AddonListItemVM, _AddonManifestModel):
+					return listItemVM.model.installDate
 			return strxfrm(self._getAddonFieldText(listItemVM, self._sortByModelField))
 
 		def _containsTerm(detailsVM: AddonListItemVM, term: str) -> bool:
