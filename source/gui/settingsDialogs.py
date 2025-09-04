9a1d85e710139c2f712c087f852b88b07ecd08f6
Ryan McCleary <remccleary@gmail.com>
2025-09-03 22:20:00 -0400

    Fixed references to user_docs in MathSettingsDialog

--- source/gui/settingsDialogs.py
+++ source/gui/settingsDialogs.py
@@ -5,8 +5,8 @@
 # Thomas Stivers, Julien Cochuyt, Peter Vágner, Cyrille Bougot, Mesar Hameed,
 # Łukasz Golonka, Aaron Cannon, Adriani90, André-Abush Clause, Dawid Pieper,
 # Takuya Nishimoto, jakubl7545, Tony Malykh, Rob Meredith,
-# Burman's Computer and Education Ltd, hwf1324, Cary-rowen, Christopher Proß.
-# Neil Soiffer, Ryan McCleary
+# Burman's Computer and Education Ltd, hwf1324, Cary-rowen, Christopher Proß
+# Neil Soiffer, Ryan McCleary.
 # This file is covered by the GNU General Public License.
 # See the file COPYING for more details.
 
