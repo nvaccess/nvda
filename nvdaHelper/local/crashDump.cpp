86010346762a29484b16905aec59422cfac5b357
Sascha Cowley <16543535+SaschaCowley@users.noreply.github.com>
2025-09-08 09:59:14 +1000

    Add copyright header

--- nvdaHelper/local/crashDump.cpp
+++ nvdaHelper/local/crashDump.cpp
@@ -1,3 +1,9 @@
+/*
+A part of NonVisual Desktop Access (NVDA)
+This file is covered by the GNU General Public License.
+See the file COPYING for more details.
+Copyright (C) 2025 NV Access Limited
+*/
 #include <windows.h>
 #include <dbghelp.h>
 #include <string>
