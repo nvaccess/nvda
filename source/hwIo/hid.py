37e91e484e1af83de9d8b33a9fe563a0b9b8a4d8
Michael Curran <mick@nvaccess.org>
2025-09-11 12:37:26 +1000

    Update copyright years

--- source/hwIo/hid.py
+++ source/hwIo/hid.py
@@ -1,7 +1,7 @@
 # A part of NonVisual Desktop Access (NVDA)
 # This file is covered by the GNU General Public License.
 # See the file COPYING for more details.
-# Copyright (C) 2015-2018 NV Access Limited, Babbage B.V.
+# Copyright (C) 2015-2025 NV Access Limited, Babbage B.V.
 
 
 """Raw input/output for braille displays via HID
