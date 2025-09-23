e372034b0b774e6ee153949c244493bd6c2769f4
Noelia Ruiz Martínez <nrm1977@gmail.com>
2025-09-23 05:02:25 +0200

    Fix bug when updating add-ons automatically

--- source/gui/addonStoreGui/controls/messageDialogs.py
+++ source/gui/addonStoreGui/controls/messageDialogs.py
@@ -590,6 +590,9 @@ class UpdatableAddonsDialog(
 				wx.CallAfter(delayCreateDialog)
 
 			case AddonsAutomaticUpdate.UPDATE:
+
+				# Translators: Message shown when updating add-ons automatically
+				wx.CallAfter(ui.message, pgettext("addonStore", "Updating add-ons..."), SpeechPriority.NEXT)
 				threading.Thread(
 					name="AutomaticAddonUpdate",
 					target=_updateAddons,
@@ -608,9 +611,6 @@ def _updateAddons(addonsPendingUpdate: list[_AddonGUIModel]):
 	This function is treated as message box to prevent NVDA from exiting while the download/install is in progress.
 	"""
 	from ..viewModels.store import AddonStoreVM
-
-	# Translators: Message shown when updating add-ons automatically
-	ui.message(pgettext("addonStore", "Updating add-ons..."), SpeechPriority.NEXT)
 	listVMs = {AddonListItemVM(a, status=getStatus(a, _StatusFilterKey.UPDATE)) for a in addonsPendingUpdate}
 	AddonStoreVM.getAddons(
 		listVMs,
