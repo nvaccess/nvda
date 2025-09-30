5e0e3fdbfefccb44cea10316620dfa05f1b01bef
Wang Chong <306289287@qq.com>
2025-09-30 11:24:03 +0800

    simplify the logic for 'Auto' option in Word Segmentation Standard settings

--- source/textUtils/__init__.py
+++ source/textUtils/__init__.py
@@ -564,15 +564,11 @@ class WordSegmenter:
 	def _chooseStrategy(self) -> wordSegStrategy.WordSegmentationStrategy:  # TODO: optimize
 		"""Choose the appropriate segmentation strategy based on the text content."""
 		if self.wordSegFlag == WordSegFlag.AUTO:
-			if WordSegmenter._CHINESE_CHARACTER_AND_JAPANESE_KANJI.search(
-				self.text,
-			) and not WordSegmenter._KANA.search(self.text):
-				if wordSegStrategy.ChineseWordSegmentationStrategy._lib:
+			if wordSegStrategy.ChineseWordSegmentationStrategy._lib \
+				and WordSegmenter._CHINESE_CHARACTER_AND_JAPANESE_KANJI.search(
+					self.text,
+				) and not WordSegmenter._KANA.search(self.text):
 					return wordSegStrategy.ChineseWordSegmentationStrategy(self.text, self.encoding)
-				else:
-					log.debugWarning("Chinese word segmenter is loading. Falling back to Uniscribe.")
-					wordSegStrategy.ChineseWordSegmentationStrategy._initCppJieba(forceInit=True)
-					return wordSegStrategy.UniscribeWordSegmentationStrategy(self.text, self.encoding)
 			else:
 				return wordSegStrategy.UniscribeWordSegmentationStrategy(self.text, self.encoding)
 		else:
