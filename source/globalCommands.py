61ffb2f4434dd66af0dafaf739907183316e4dfc
Tianze <tianzeshi_study@outlook.com>
2025-10-08 14:28:51 +0800

    Avoid runing AI image descriptions while screen curtain is enabled (#19057)
    Fixes #19045
    Summary of the issue:
    
    Image Description not disabled while Screen Curtain is enabled
    Description of user facing changes:
    
    Users will be notified that AI image description is unavailable when they try to use it while the screen curtain is enabled.
    Description of developer facing changes:
    
    The item of SCREEN_CURTAIN is added to gui.blockAction.Context , developers can use it in decoration to avoid actions when screen curtain is enabled.

--- source/globalCommands.py
+++ source/globalCommands.py
@@ -5104,6 +5104,7 @@ class GlobalCommands(ScriptableObject):
 		category=SCRCAT_IMAGE_DESC,
 		gesture="kb:NVDA+windows+,",
 	)
+	@gui.blockAction.when(gui.blockAction.Context.SCREEN_CURTAIN)
 	def script_runCaption(self, gesture: "inputCore.InputGesture"):
 		_localCaptioner._localCaptioner.runCaption(gesture)
 
@@ -5112,6 +5113,7 @@ class GlobalCommands(ScriptableObject):
 		description=pgettext("imageDesc", "Toggle image captioning"),
 		category=SCRCAT_IMAGE_DESC,
 	)
+	@gui.blockAction.when(gui.blockAction.Context.SCREEN_CURTAIN)
 	def script_toggleImageCaptioning(self, gesture: "inputCore.InputGesture"):
 		_localCaptioner._localCaptioner.toggleImageCaptioning(gesture)
 
