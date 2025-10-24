nvaccess/nvda

commit bd6b99eaa0096a7add493fff175fc872c2a46c7b

Remove AppVeyor entirely (#18556)
Part of #17878
Blocked by release of 2025.2
Summary of the issue:

We are migrating from AppVeyor to GitHub actions.
After we migrate snapshots/PRs over, we will remove all AppVeyor related code.
Description of user facing changes:

None
Description of developer facing changes:

Remove all Appveyor related code. NVDA will only be built on GitHub actions.
Description of development approach:

Update documentation and remove code

.github/workflows/testAndPublish.yml
@@ -3,24 +3,22 @@ name: CI/CD
 on:
   push:
     branches:
-    - master
-    - beta
-    - rc
-    - 'try-**'
-    # Temporarily enabled during the 64bit migration
-    - wip-buildX64
-    # Currently disabled while the 2025.2 release is in progress.
-    # Blocked by #17878
-    # tags:
-    #  - 'release-**'
+      - master
+      - beta
+      - rc
+      - 'try-**'
+      # Temporarily enabled during the 64bit migration
+      - wip-buildX64
+    tags:
+      - 'release-**'
 
   pull_request:
     branches:
-    - master
-    - beta
-    - rc
-    # Temporarily enabled during the 64bit migration
-    - wip-buildX64
+      - master
+      - beta
+      - rc
+      # Temporarily enabled during the 64bit migration
+      - wip-buildX64
 
 
   workflow_dispatch:
@@ -302,7 +300,7 @@ jobs:
       env:
         crowdinProjectID: ${{ vars.CROWDIN_PROJECT_ID }}
         crowdinAuthToken: ${{ secrets.CROWDIN_AUTH_TOKEN }}
-      run: uv run --with requests --directory ${{ github.workspace }} appveyor\crowdinSync.py uploadSourceFile 2 output\nvda.pot 2>&1
+      run: uv run --with requests --directory ${{ github.workspace }} ci\scripts\crowdinSync.py uploadSourceFile 2 output\nvda.pot 2>&1
 
   createLauncher:
     name: Create launcher
@@ -522,8 +520,7 @@ jobs:
         path: output
     - name: Upload symbols to Mozilla
       continue-on-error: true
-      # TODO: this script should be moved to ci/scripts
-      run: uv run --with requests --directory ${{ github.workspace }} appveyor\mozillaSyms.py
+      run: uv run --with requests --directory ${{ github.workspace }} ci\scripts\mozillaSyms.py
       env:
         mozillaSymsAuthToken: ${{ secrets.MOZILLA_SYMS_TOKEN }}
     - name: Warn on failure

.github/workflows/regenerate_english_userDocs_translation_source.yml
@@ -98,7 +98,7 @@ jobs:
         if ($changed) {
             Write-Host "Uploading userGuide.xliff to Crowdin"
             # 18 is the file ID for userGuide.xliff in Crowdin.
-            python appVeyor/crowdinSync.py uploadSourceFile 18 user_docs/en/userguide.xliff
+            python ci/scripts/crowdinSync.py uploadSourceFile 18 user_docs/en/userguide.xliff
         } else {
             Write-Host "Not uploading userGuide.xliff to Crowdin as it has not changed"
         }
@@ -106,7 +106,7 @@ jobs:
         if ($changed) {
             Write-Host "Uploading changes.xliff to Crowdin"
             # 20 is the file ID for changes.xliff in Crowdin.
-            python appVeyor/crowdinSync.py uploadSourceFile 20 user_docs/en/changes.xliff
+            python ci/scripts/crowdinSync.py uploadSourceFile 20 user_docs/en/changes.xliff
         } else {
             Write-Host "Not uploading changes.xliff to Crowdin as it has not changed"
         }

.gitignore
@@ -52,5 +52,4 @@ source/locale/*/cldr.dic
 .venv
 nvdaHelper/docs/
 *.pfx
-appveyor-tools/
 *.egg-info

appveyor.yml
@@ -1,114 +1,0 @@
-# Build configuration for Appveyor
-# Many more options available, see docs:
-# https://www.appveyor.com/docs/appveyor-yml/
-
-os: Visual Studio 2022
-version: "{branch}-{build}"
-
-branches:
- only:
-  - /release-.*/
-
-environment:
- PY_PYTHON: 3.11-32
- apiSigningToken:
-  secure: kfXhUTs1gmcOo0yFDwqNySP3NuLkNcB3m0qeSg+YPPS4gPyLOU9GyvBLnMuDnHe6
- secure_ssh_pass:
-  secure: Iql/RhSathGacONacsyr6gis+rjL75UFZ/R+nPAJpo3asAzQSQQd8hfxq0iv8+Th
- mozillaSymsAuthToken:
-  secure: p37Fxo78fsRdmR8v8TPz978QvVaqvbjdIBzFe8ZOpX0FUprm46rkhd374QM1CqMO
- APPVEYOR_WEBHOOK_TOKEN:
-  secure: N8GiEsalpVCro2Q7P9QAb1ZMLeYVvm2lk2Ba73OPZ9/TD8b39mc5gOmpXKNdftoA
- symstore: C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\symstore.exe
- crowdinProjectID: 598017
- crowdinAuthToken:
-  secure: E3084gj4JeMZKvZIOLIhqZefuSo/tj7iYPt4yK0geOI/eQgmPvoXt37Xq0KwvXzvZiJny4AsMj1rKMTVxio8EG8KA0YsYYuy+WV1wpFRIn25zGQS+DZ/yycL75SmTWfr
- scons_publisher: NV Access
- SCONS_CACHE_MSVC_CONFIG: "true"
- # Comment out any of the feature_* variables to disable the respective build feature.
- # They are checked for existence of content, not specific value.
- feature_buildSymbols: configured
- feature_uploadSymbolsToMozilla: configured
- #feature_buildAppx: configured
- #feature_crowdinSync: configured
- feature_signing: configured
-
-# scripts that are called at very beginning, before repo cloning
-init:
-  # set the init time for the build, used to calculate the time taken for each stage of the build.
-  - ps: |
-      "INIT, $(Get-Date -Format 'o')"| Out-File ../timing.csv -Append
-
-install:
- - ps: |
-    "INSTALL_START, $(Get-Date -Format 'o')"| Out-File ../timing.csv -Append
- - ps: appveyor\scripts\setBuildVersionVars.ps1
- - ps: appveyor\scripts\decryptFilesForSigning.ps1
- - ps: Invoke-RestMethod https://astral.sh/uv/install.ps1 | Invoke-Expression
- - set Path=%userprofile%\.local\bin;%Path%
- - git submodule update --init
- - ps: |
-    "INSTALL_END, $(Get-Date -Format 'o')"| Out-File ../timing.csv -Append
-
-build_script:
- - ps: |
-     "BUILD_START, $(Get-Date -Format 'o')"| Out-File ../timing.csv -Append
- - ps: appveyor\scripts\setSconsArgs.ps1
- - ps: Install-Module -Name SignPath -Force
- - scons source %sconsArgs%
- - scons %sconsOutTargets% %sconsArgs%
- - ps: appveyor\scripts\buildSymbolStore.ps1
- # The server expects the symbols archive to be structured as ./*.ex_ not ./symbols/*.ex_.
- # Change directory to package, as 7z will structure the archive using the relative path.
- - ps: >-
-     if ($env:feature_buildSymbols) {
-        cd symbols
-        7z a -tzip -r ..\output\symbols.zip *.dl_ *.ex_ *.pd_
-        cd ..
-     }
- - ps: |
-     "BUILD_END, $(Get-Date -Format 'o')"| Out-File ../timing.csv -Append
-
-before_test:
- - ps: |
-     "TESTSETUP_START, $(Get-Date -Format 'o')"| Out-File ../timing.csv -Append
- - ps: appveyor\scripts\tests\beforeTests.ps1
- - ps: appveyor\scripts\installNVDA.ps1
- - ps: |
-     "TESTSETUP_END, $(Get-Date -Format 'o')"| Out-File ../timing.csv -Append
-
-test_script:
- - ps: |
-     "TEST_START, $(Get-Date -Format 'o')"| Out-File ../timing.csv -Append
- - ps: appveyor\scripts\tests\translationCheck.ps1
- - ps: appveyor\scripts\tests\licenseCheck.ps1
- - ps: appveyor\scripts\tests\unitTests.ps1
- - ps: appveyor\scripts\tests\systemTests.ps1
- - ps: |
-     "TEST_END, $(Get-Date -Format 'o')"| Out-File ../timing.csv -Append
-
-after_test:
- - ps: appveyor\scripts\tests\checkTestFailure.ps1
-
-artifacts:
- - path: output\*
- - path: output\*\*
-
-# Prepare environment with additional variables for webhook
-before_deploy:
- - ps: appveyor\scripts\webhookSetup.ps1
-
-deploy:
-  - provider: Webhook
-    url: https://api.nvaccess.org/appveyor/hook
-    authorization: Bearer %APPVEYOR_WEBHOOK_TOKEN%
-    request_timeout: 5  # minutes
-
-on_failure:
- - ps: appveyor\scripts\uploadArtifacts.ps1
-
-on_finish:
- - ps: appveyor\scripts\pushPackagingInfo.ps1
- - ps: |
-     "FINISH_END, $(Get-Date -Format 'o')"| Out-File ../timing.csv -Append
- - ps: appveyor\scripts\logCiTiming.ps1

appveyor/scripts/buildSymbolStore.ps1
@@ -1,16 +1,0 @@
-$ErrorActionPreference = "Stop";
-if (!$env:feature_buildSymbols) {
-	exit
-}
-
-foreach ($syms in
-	# We don't just include source\*.dll because that would include system dlls.
-	"source\liblouis.dll", "source\*.pdb",
-	"source\lib\*.dll", "source\lib\*.pdb",
-	# We include source\lib64\*.exe to cover nvdaHelperRemoteLoader.
-	"source\lib64\*.dll", "source\lib64\*.exe", "source\lib64\*.pdb",
-	"source\synthDrivers\*.dll", "source\synthDrivers\*.pdb"
-) {
-	# https://docs.microsoft.com/en-us/windows-hardware/drivers/debugger/symstore-command-line-options
-	& $env:symstore add /f $syms /s symbols /t NVDA /compress
-}

appveyor/scripts/decryptFilesForSigning.ps1
@@ -1,11 +1,0 @@
-if(!$env:APPVEYOR_PULL_REQUEST_NUMBER -and $env:feature_signing) {
-	openssl enc -d -md sha256 -aes-256-cbc -pbkdf2 -salt -pass pass:$env:secure_ssh_pass -in appveyor\ssh_id_rsa.enc -out appveyor\ssh_id_rsa
-	if($LastExitCode -ne 0) {
-		$errorCode=$LastExitCode
-		Add-AppveyorMessage "Unable to decrypt ssh key"
-	}
-	# Install ssh stuff.
-	Copy-Item -Path appveyor\ssh_id_rsa -Destination $env:userprofile\.ssh\id_rsa
-}
-Copy-Item -Path appveyor\ssh_known_hosts -Destination $env:userprofile\.ssh\known_hosts
-if ($errorCode -ne 0) { $host.SetShouldExit($errorCode) }

commit 360a0dec4820427256e90c6e357f8189ff62d1c0

Fix uploading of symbols to Mozilla. (#19130)
### Summary of the issue:
NVDA builds are intended to upload debug symbols to Mozilla. This makes
it easier to diagnose issues related to NVDA's in-process code in
Firefox (both crash analysis and profiling), which helps both projects.
Unfortunately, this broke years ago for various reasons.

### Description of user facing changes:
None.

### Description of developer facing changes:
Mozilla crash reports and profiles will show useful symbols for NVDA
code.
I haven't included a change log entry for NVDA because this doesn't
impact NVDA developers normally, only if they're working with Firefox.

### Description of development approach:
1. The version of dump_syms.exe in miscDeps from Google Breakpad is very
outdated and probably no longer works. Mozilla completely rewrote this a
few years ago. Download that from GitHub releases and use that instead.
2. mozillaSyms.py uses relative paths, but the script was moved in
bd6b99ea, which broke these paths. Update the script accordingly.
3. The structure of NVDA's lib directory has changed significantly in
the last few years. Furthermore, NVDA now supports more architectures
(arm64, arm64ec). Update mozillaSyms.py accordingly.
4. Remove some unnecessary actions from the uploadSymbols CI job.

### Testing strategy:
Confirmed that [symbols were uploaded successfully in a try
run](https://github.com/nvaccess/nvda/actions/runs/18735136393/job/53440453068).

### Known issues with pull request:
None.

.github/workflows/testAndPublish.yml
@@ -512,7 +512,7 @@ jobs:
   uploadSymbols:
     name: Upload symbols
     runs-on: ${{ needs.matrix.outputs.defaultRunner }}
-    needs: [matrix, createSymbols]
+    needs: [matrix, buildNvda]
     if: ${{ github.event_name == 'push' && vars.feature_uploadSymbolsToMozilla }}
     steps:
     - name: Checkout cached build
@@ -526,13 +526,13 @@ jobs:
       with:
         python-version: ${{ env.defaultPythonVersion }}
         architecture: ${{ env.defaultArch }}
+    - name: Download and extract dump_syms
+      shell: bash
+      run: |
+        curl -L -o dump_syms.zip https://github.com/mozilla/dump_syms/releases/download/v2.3.5/dump_syms-x86_64-pc-windows-msvc.zip
+        7z x dump_syms.zip dump_syms.exe
     - name: Install the latest version of uv
       uses: astral-sh/setup-uv@v6
-    - name: Get symbols
-      uses: actions/download-artifact@v4
-      with:
-        name: Symbols (${{ env.defaultPythonVersion }}, ${{ env.defaultArch }})
-        path: output
     - name: Upload symbols to Mozilla
       continue-on-error: true
       run: uv run --with requests --directory ${{ github.workspace }} ci\scripts\mozillaSyms.py

ci/scripts/mozillaSyms.py
@@ -13,10 +13,10 @@ import zipfile
 import requests
 
 SCRIPT_DIR = os.path.abspath(os.path.dirname(__file__))
-DUMP_SYMS = os.path.join(os.path.dirname(SCRIPT_DIR), "miscDeps", "tools", "dump_syms.exe")
-NVDA_SOURCE = os.path.join(os.path.dirname(SCRIPT_DIR), "source")
+REPO_ROOT = os.path.dirname(os.path.dirname(SCRIPT_DIR))
+DUMP_SYMS = os.path.join(REPO_ROOT, "dump_syms.exe")
+NVDA_SOURCE = os.path.join(REPO_ROOT, "source")
 NVDA_LIB = os.path.join(NVDA_SOURCE, "lib")
-NVDA_LIB64 = os.path.join(NVDA_SOURCE, "lib64")
 ZIP_FILE = os.path.join(SCRIPT_DIR, "mozillaSyms.zip")
 URL = "https://symbols.mozilla.org/upload/"
 
@@ -28,10 +28,10 @@ DLL_NAMES = [
 	"nvdaHelperRemote.dll",
 ]
 DLL_FILES = [
-	f
+	os.path.join(NVDA_LIB, arch, dll)
 	for dll in DLL_NAMES
-	# We need both the 32 bit and 64 bit symbols.
-	for f in (os.path.join(NVDA_LIB, dll), os.path.join(NVDA_LIB64, dll))
+	# We need symbols for all supported architectures.
+	for arch in ("x86", "x64", "arm64", "arm64ec")
 ]
 
 