92baa92272302a30638836d359be6b83043aa130
Leonard de Ruijter <alderuijter@gmail.com>
2025-11-12 07:40:54 +0100

    More headers

--- source/virtualBuffers/__init__.py
+++ source/virtualBuffers/__init__.py
@@ -1,7 +1,7 @@
 # A part of NonVisual Desktop Access (NVDA)
-# This file is covered by the GNU General Public License.
-# See the file COPYING for more details.
 # Copyright (C) 2007-2025 NV Access Limited, Peter Vágner, Cyrille Bougot, Leonard de Ruijter
+# This file may be used under the terms of the GNU General Public License, version 2 or later, as modified by the NVDA license.
+# For full terms and any additional permissions, see the NVDA license file: https://github.com/nvaccess/nvda/blob/master/copying.txt
 
 import time
 import threading
