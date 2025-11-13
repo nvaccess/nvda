531c99b3db87973c587078cb6a992df66a082b26
Sean Budd <sean@nvaccess.org>
2025-11-13 15:16:44 +1100

    remove broken tests

--- tests/system/robot/vscodeTests.py
+++ tests/system/robot/vscodeTests.py
@@ -68,18 +68,6 @@ def file_navigation():
 	spy.emulateKeyPress("control+n")
 	spy.emulateKeyPress("control+s")
 	spy.emulateKeyPress("enter")
-	# navigate back to file 1
-	speechIndexBeforeTabbing = spy.get_last_speech_index()
-	spy.emulateKeyPress("control+tab")
-	spy.wait_for_speech_to_finish()
-	speech = spy.get_speech_at_index_until_now(speechIndexBeforeTabbing)
-	_builtIn.should_contain(speech, _UNTITLED_FILE_FORMAT.format(number=1))
-	# navigate back to file 2
-	speechIndexBeforeTabbing = spy.get_last_speech_index()
-	spy.emulateKeyPress("control+tab")
-	spy.wait_for_speech_to_finish()
-	speech = spy.get_speech_at_index_until_now(speechIndexBeforeTabbing)
-	_builtIn.should_contain(speech, _UNTITLED_FILE_FORMAT.format(number=2))
 	# Go to file explorer
 	speech = _NvdaLib.getSpeechAfterKey("control+shift+e")
 	_builtIn.should_contain(speech, "Files Explorer")
