cde330a7c3e2fbe3577e404103c4b85981907d5f
Michael Curran <mick@nvaccess.org>
2025-12-01 16:33:52 +1000

    markdownTranslate:  filter out markdown linter comments from the end of lines as they break parsing of other markdown entities such as table rows, they inappropriately appear in translatable strings, and they are not required for xliff, intermediate generated markdown or the final html. Specifically: * add a regular expression to match these markdown comments. * Add a preprocessMarkdownLines function, which runs this regex on all lines.  * All places that read in a markdown file now preprocess all lines with preprocessMarkdownLines, such as generateSkeleton, updateSkeleton, generateXliff, and ensureMarkdownFilesMatch.

--- source/markdownTranslate.py
+++ source/markdownTranslate.py
@@ -29,6 +29,7 @@ re_hiddenHeaderRow = re.compile(r"^\|\s*\.\s*\{\.hideHeaderRow\}\s*(\|\s*\.\s*)*
 re_postTableHeaderLine = re.compile(r"^(\|\s*-+\s*)+\|$")
 re_tableRow = re.compile(r"^(\|)(.+)(\|)$")
 re_translationID = re.compile(r"^(.*)\$\(ID:([0-9a-f-]+)\)(.*)$")
+re_inlineMarkdownLintComment = re.compile(r"^(.*?)(?:\s*<!-- markdownlint.*-->)(\s*)$")
 
 
 def prettyPathString(path: str) -> str:
@@ -88,6 +89,13 @@ def getRawGithubURLForPath(filePath: str) -> str:
 	return f"{RAW_GITHUB_REPO_URL}/{commitID}/{relativePath}"
 
 
+def preprocessMarkdownLines(mdLines):
+	for mdLine in mdLines:
+		# #18982: Remove markdown lint comments completely - not needed for intermediate markdown or final html.
+		mdLine = re_inlineMarkdownLintComment.sub(r'\1\2', mdLine)
+		yield mdLine
+
+
 def skeletonizeLine(mdLine: str) -> str | None:
 	prefix = ""
 	suffix = ""
@@ -129,7 +137,7 @@ def generateSkeleton(mdPath: str, outputPath: str) -> Result_generateSkeleton:
 		open(mdPath, "r", encoding="utf8") as mdFile,
 		open(outputPath, "w", encoding="utf8", newline="") as outputFile,
 	):
-		for mdLine in mdFile.readlines():
+		for mdLine in preprocessMarkdownLines(mdFile.readlines()):
 			res.numTotalLines += 1
 			skelLine = skeletonizeLine(mdLine)
 			if skelLine:
@@ -185,7 +193,9 @@ def updateSkeleton(
 		newMdFile = stack.enter_context(open(newMdPath, "r", encoding="utf8"))
 		origSkelFile = stack.enter_context(open(origSkelPath, "r", encoding="utf8"))
 		outputFile = stack.enter_context(open(outputPath, "w", encoding="utf8", newline=""))
-		mdDiff = difflib.ndiff(origMdFile.readlines(), newMdFile.readlines())
+		origMdLines = preprocessMarkdownLines(origMdFile.readlines())
+		newMdLines = preprocessMarkdownLines(newMdFile.readlines())
+		mdDiff = difflib.ndiff(list(origMdLines), list(newMdLines))
 		origSkelLines = iter(origSkelFile.readlines())
 		for mdDiffLine in mdDiff:
 			if mdDiffLine.startswith("?"):
@@ -261,7 +271,10 @@ def generateXliff(
 		outputFile.write(f"<skeleton>\n{xmlEscape(skelContent)}\n</skeleton>\n")
 		res.numTranslatableStrings = 0
 		for lineNo, (mdLine, skelLine) in enumerate(
-			zip_longest(mdFile.readlines(), skelContent.splitlines(keepends=True)),
+			zip_longest(
+				preprocessMarkdownLines(mdFile.readlines()),
+				skelContent.splitlines(keepends=True)
+			),
 			start=1,
 		):
 			mdLine = mdLine.rstrip()
@@ -361,7 +374,10 @@ def translateXliff(
 			raise ValueError("No skeleton found in xliff file")
 		skeletonContent = skeletonNode.text.strip()
 		for lineNo, (skelLine, pretranslatedLine) in enumerate(
-			zip_longest(skeletonContent.splitlines(), pretranslatedMdFile.readlines()),
+			zip_longest(
+				skeletonContent.splitlines(),
+				preprocessMarkdownLines(pretranslatedMdFile.readlines()),
+			),
 			start=1,
 		):
 			skelLine = skelLine.rstrip()
@@ -482,7 +498,13 @@ def ensureMarkdownFilesMatch(path1: str, path2: str, allowBadAnchors: bool = Fal
 	with contextlib.ExitStack() as stack:
 		file1 = stack.enter_context(open(path1, "r", encoding="utf8"))
 		file2 = stack.enter_context(open(path2, "r", encoding="utf8"))
-		for lineNo, (line1, line2) in enumerate(zip_longest(file1.readlines(), file2.readlines()), start=1):
+		for lineNo, (line1, line2) in enumerate(
+			zip_longest(
+				preprocessMarkdownLines(file1.readlines()),
+				preprocessMarkdownLines(file2.readlines()),
+			),
+			start=1
+		):
 			line1 = line1.rstrip()
 			line2 = line2.rstrip()
 			if line1 != line2:
