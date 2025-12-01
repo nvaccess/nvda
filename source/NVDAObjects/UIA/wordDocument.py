a65750dd79bc7744dcec17c043f0848f10ac8ef5
Leonard de Ruijter <alderuijter@gmail.com>
2025-12-01 14:14:44 +0100

    Typo

--- source/NVDAObjects/UIA/wordDocument.py
+++ source/NVDAObjects/UIA/wordDocument.py
@@ -1,5 +1,5 @@
 # A part of NonVisual Desktop Access (NVDA)
-# Copyright (C) 2016-2025 NV Access Limited, Joseph Lee, Jakub Lukowicz, Cyrille Bougot, Leonard de RUijter
+# Copyright (C) 2016-2025 NV Access Limited, Joseph Lee, Jakub Lukowicz, Cyrille Bougot, Leonard de Ruijter
 # This file may be used under the terms of the GNU General Public License, version 2 or later, as modified by the NVDA license.
 # For full terms and any additional permissions, see the NVDA license file: https://github.com/nvaccess/nvda/blob/master/copying.txt
 
