72cc84113a2df2c3695cd1dfe7b58cdc5963543e
Noelia Ruiz Martínez <nrm1977@gmail.com>
2025-12-05 06:13:57 +0100

    Increase maximum for scroll rate

--- source/config/configSpec.py
+++ source/config/configSpec.py
@@ -83,7 +83,7 @@ schemaVersion = integer(min=0, default={latestSchemaVersion})
 	# Timeout after the message will disappear from braille display
 	messageTimeout = integer(default=4, min=1, max=20)
 	# Rate for automatic scroll (cells/sec)
-	autoScrollRate = integer(default=10, min=1, max=20)
+	autoScrollRate = integer(default=10, min=1, max=30)
 	tetherTo = option("auto", "focus", "review", default="auto")
 	reviewRoutingMovesSystemCaret = featureFlag(\
 		optionsEnum="ReviewRoutingMovesSystemCaretFlag", behaviorOfDefault="NEVER")
