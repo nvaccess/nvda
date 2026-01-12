369a7a88b53783da0df348cad35f159011771fd9
Michael Curran <mick@nvaccess.org>
2026-01-12 10:55:26 +1000

    Remove commented out code and fix type import.

--- source/_bridge/runtimes/synthDriverHost/logHandler.py
+++ source/_bridge/runtimes/synthDriverHost/logHandler.py
@@ -3,29 +3,12 @@
 # This file may be used under the terms of the GNU General Public License, version 2 or later, as modified by the NVDA license.
 # For full terms and any additional permissions, see the NVDA license file: https://github.com/nvaccess/nvda/blob/master/copying.txt
 
-import inspect
+from __future__ import annotations
+import typing
 import logging
 
-
-# old_factory = logging.getLogRecordFactory()
-
-
-def record_factory(*args, **kwargs):
-	record = old_factory(*args, **kwargs)
-	frame = inspect.currentframe()
-	count = 5
-	while count > 0:
-		if not frame.f_back:
-			break
-		frame = frame.f_back
-		count -= 1
-	record.qualname = frame.f_code.co_qualname.removesuffix(".__init__")
-	mod = os.path.splitext(os.path.basename(frame.f_code.co_filename))[0]
-	record.module = mod
-	return record
-
-
-# logging.setLogRecordFactory(record_factory)
+if typing.TYPE_CHECKING:
+	import extensionPoints
 
 
 log = logging.getLogger()
