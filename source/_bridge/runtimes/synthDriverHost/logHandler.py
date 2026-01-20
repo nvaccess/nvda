d783c97fe8991b1a10940c39fbd78884b169e97b
Michael Curran <mick@nvaccess.org>
2026-01-20 16:40:19 +1000

    Remove unneeded import

--- source/_bridge/runtimes/synthDriverHost/logHandler.py
+++ source/_bridge/runtimes/synthDriverHost/logHandler.py
@@ -3,7 +3,6 @@
 # This file may be used under the terms of the GNU General Public License, version 2 or later, as modified by the NVDA license.
 # For full terms and any additional permissions, see the NVDA license file: https://github.com/nvaccess/nvda/blob/master/copying.txt
 
-from __future__ import annotations
 import typing
 import logging
 
