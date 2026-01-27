8c91c7201ca44fa990c80fb53fb769af5b3da0fe
Antoine HAFFREINGUE <antoine.haffreingue@francetravail.fr>
2026-01-27 16:59:07 +0100

    added 2026 in descriptions

--- source/_magnifier/utils/filterHandler.py
+++ source/_magnifier/utils/filterHandler.py
@@ -1,5 +1,5 @@
 # A part of NonVisual Desktop Access (NVDA)
-# Copyright (C) 2025 NV Access Limited, Antoine Haffreingue
+# Copyright (C) 2025-2026 NV Access Limited, Antoine Haffreingue
 # This file may be used under the terms of the GNU General Public License, version 2 or later, as modified by the NVDA license.
 # For full terms and any additional permissions, see the NVDA license file: https://github.com/nvaccess/nvda/blob/master/copying.txt
 
