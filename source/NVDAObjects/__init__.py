5f3dea15a3f0e7a2e8446c5149ab2618624932e7
Leonard de Ruijter <alderuijter@gmail.com>
2026-01-29 22:06:29 +0100

    Fix copyright

--- source/NVDAObjects/__init__.py
+++ source/NVDAObjects/__init__.py
@@ -1,9 +1,8 @@
-# -*- coding: UTF-8 -*-
 # A part of NonVisual Desktop Access (NVDA)
-# Copyright (C) 2006-2023 NV Access Limited, Peter Vágner, Aleksey Sadovoy, Patrick Zajda, Babbage B.V.,
+# Copyright (C) 2006-2026 NV Access Limited, Peter Vágner, Aleksey Sadovoy, Patrick Zajda, Babbage B.V.,
 # Davy Kager, Leonard de Ruijter, Cyrille Bougot
-# This file is covered by the GNU General Public License.
-# See the file COPYING for more details.
+# This file may be used under the terms of the GNU General Public License, version 2 or later, as modified by the NVDA license.
+# For full terms and any additional permissions, see the NVDA license file: https://github.com/nvaccess/nvda/blob/master/copying.txt
 
 """Module that contains the base NVDA object type with dynamic class creation support,
 as well as the associated TextInfo class."""
