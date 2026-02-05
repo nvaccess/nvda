318ee7b13e64f77453610314f7fd667067edb7b8
Sascha Cowley <16543535+SaschaCowley@users.noreply.github.com>
2026-02-05 17:33:00 +1100

    Update tests

--- tests/unit/test_addonHandler/test_addonsState.py
+++ tests/unit/test_addonHandler/test_addonsState.py
@@ -8,7 +8,6 @@
 import unittest
 
 import addonStore.models.status
-from addonStore.models.version import MajorMinorPatch
 import addonHandler
 import utils.caseInsensitiveCollections
 
@@ -28,45 +27,45 @@ class TestDefaultStateContent(unittest.TestCase):
 		self.assertEqual(state, {})
 
 
-class TestStatePopulationFromPickledData(unittest.TestCase):
+class TestStatePopulationFromJsonData(unittest.TestCase):
 	def setUp(self) -> None:
 		self.state = addonHandler.AddonsState()
 		self.state.setDefaultStateValues()
 
 	def test_addonNamesCaseInsensitive(self):
-		self.state.fromPickledDict({"pendingRemovesSet": set(("foo", "FOO"))})
+		self.state.fromJsonDict({"pendingRemovesSet": ["foo", "FOO"]})
 		self.assertEqual(self.state[addonStore.models.status.AddonStateCategory.PENDING_REMOVE], {"foo"})
 
 	def test_noBackCompatInStateBackCompatSetToDefault(self):
-		self.state.fromPickledDict({"pendingRemovesSet": set(("foo", "FOO"))})
+		self.state.fromJsonDict({"pendingRemovesSet": ["foo", "FOO"]})
 		self.assertEqual(self.state.manualOverridesAPIVersion.major, 2023)
 		self.assertEqual(self.state.manualOverridesAPIVersion.minor, 1)
 		self.assertEqual(self.state.manualOverridesAPIVersion.patch, 0)
 
 	def test_backCompatToProvidedAsATuple(self):
-		self.state.fromPickledDict({"backCompatToAPIVersion": (2024, 1, 1)})
+		self.state.fromJsonDict({"backCompatToAPIVersion": (2024, 1, 1)})
 		self.assertEqual(self.state.manualOverridesAPIVersion.major, 2024)
 		self.assertEqual(self.state.manualOverridesAPIVersion.minor, 1)
 		self.assertEqual(self.state.manualOverridesAPIVersion.patch, 1)
 
-	def test_backCompatToProvidedAsAMajorMinorPatch(self):
-		# While addons state should be pickled using builtin data types only for backward compatibility,
-		# In PR #15439 `backCompatToVersion` was mistakenly pickled as a custom named tuple `MajorMinorPatch`.
-		# This tests verifies that data in this format can be loaded by versions of NVDA where `MajorMinorPatch`
-		# is defined.
-		self.state.fromPickledDict({"backCompatToAPIVersion": MajorMinorPatch(major=2024, minor=1, patch=1)})
-		self.assertEqual(self.state.manualOverridesAPIVersion.major, 2024)
-		self.assertEqual(self.state.manualOverridesAPIVersion.minor, 1)
-		self.assertEqual(self.state.manualOverridesAPIVersion.patch, 1)
+	# def test_backCompatToProvidedAsAMajorMinorPatch(self):
+	# While addons state should be pickled using builtin data types only for backward compatibility,
+	# In PR #15439 `backCompatToVersion` was mistakenly pickled as a custom named tuple `MajorMinorPatch`.
+	# This tests verifies that data in this format can be loaded by versions of NVDA where `MajorMinorPatch`
+	# is defined.
+	# self.state.fromPickledDict({"backCompatToAPIVersion": MajorMinorPatch(major=2024, minor=1, patch=1)})
+	# self.assertEqual(self.state.manualOverridesAPIVersion.major, 2024)
+	# self.assertEqual(self.state.manualOverridesAPIVersion.minor, 1)
+	# self.assertEqual(self.state.manualOverridesAPIVersion.patch, 1)
 
 
-class TestStateConversionForPickling(unittest.TestCase):
+class TestStateConversionForJsonifying(unittest.TestCase):
 	def test_stateConvertedToBuiltInTypes(self):
 		state = addonHandler.AddonsState()
 		state.setDefaultStateValues()
-		state.fromPickledDict({"backCompatToAPIVersion": (2024, 1, 1), "pendingRemovesSet": set(("foo",))})
-		dataForPickling = state.toDict()
-		backCompatInfo = dataForPickling.pop("backCompatToAPIVersion")
+		state.fromJsonDict({"backCompatToAPIVersion": [2024, 1, 1], "pendingRemovesSet": ["foo"]})
+		dataForJsonifying = state.toDict()
+		backCompatInfo = dataForJsonifying.pop("backCompatToAPIVersion")
 		with self.assertRaises(AttributeError):
 			# Ensure this is serialized as a standard tuple, and not as `MajorMinorPatch`
 			# by checking that values of a tuple cannot be accessed as attributes.
@@ -78,15 +77,13 @@ class TestStateConversionForPickling(unittest.TestCase):
 		self.assertEqual(backCompatInfo[1], 1)
 		self.assertEqual(backCompatInfo[2], 1)
 		# All keys in the state should be strings, all values should be plain sets.
-		for key in dataForPickling.keys():
+		for key in dataForJsonifying.keys():
 			# Compare by identity, to make sure keys are not enum members.
 			self.assertIs(key, addonStore.models.status.AddonStateCategory(key).value)
 
 		for knownStateKey in addonStore.models.status.AddonStateCategory:
-			value = dataForPickling.pop(knownStateKey.value)
-			# Verify the values are converted to standard sets, by adding two strings which differ in case.
-			# Normal sets should preserve them both.
-			value.update({"bar", "BAR"})
-			self.assertTrue({"bar", "BAR"}.issubset(value))
+			value = dataForJsonifying.pop(knownStateKey.value)
+			# Verify the values are converted to lists.
+			self.assertIsInstance(value, list)
 		# Finally make sure that we handled all keys in the dictionary.
-		self.assertEqual(dataForPickling, {})
+		self.assertEqual(dataForJsonifying, {})
