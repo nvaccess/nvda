46c5c68802394a9fb1d58de7009a48a294d4dc42
Sascha Cowley <16543535+SaschaCowley@users.noreply.github.com>
2026-02-17 12:20:57 +1100

    Copyright dates

--- source/systemUtils.py
+++ source/systemUtils.py
@@ -1,5 +1,5 @@
 # A part of NonVisual Desktop Access (NVDA)
-# Copyright (C) 2020-2025 NV Access Limited, Łukasz Golonka, Luke Davis, Leonard de Ruijter
+# Copyright (C) 2020-2026 NV Access Limited, Łukasz Golonka, Luke Davis, Leonard de Ruijter
 # This file may be used under the terms of the GNU General Public License, version 2 or later.
 # For more details see: https://www.gnu.org/licenses/gpl-2.0.html
 
