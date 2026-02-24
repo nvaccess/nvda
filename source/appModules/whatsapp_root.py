3d2276f89a41f7dcc41236e932a45549e428756e
Joseph Lee <joseph.lee22590@gmail.com>
2026-02-24 15:30:32 -0700

    appModules/whatsapp.root: comment typo fix
    Co-authored-by: Noelia Ruiz Martínez <nrm1977@gmail.com>

--- source/appModules/whatsapp_root.py
+++ source/appModules/whatsapp_root.py
@@ -4,7 +4,7 @@
 # See the file COPYING for more details.
 
 """Support for WhatsApp WebView2 app.
-Notably, this app module disables browse mode for WhatsApp by defualt."""
+Notably, this app module disables browse mode for WhatsApp by default."""
 
 import appModuleHandler
 
