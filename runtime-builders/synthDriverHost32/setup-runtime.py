nvaccess/nvda

commit f32e049ef65c12bd5aa23c5520316e9a3b90c8af

Support for 32 bit sapi4 and sapi5 via a 32 bit synthDriver runtime (#19432)
Replaces pr #19412

### Summary of the issue:
With the switch to 64 bit, NVDA no longer supports sapi4, or 32 bit
specific sapi5 synthesizers. NVDA should somehow continue to support
these synthesizers.

### Description of user facing changes:
NvDA continues to support sapi4 and 32 bit specific Sapi5 synthesizers.

### Description of developer facing changes:

### Description of development approach:
* Compile nvdaHelperLocal for all archetectures, rather than just for
NVDA's core archetecture (x64). NVDA's 32 bit synthDriver host runtime
needs nvdaHelperLocal for WASAPI.
* Move the wasapi function definitions for nvdaHelperLocal.dll into
their own 'wasapi' Python module, so that they can be imported separate
to NvDAHelper. NVDA's 32 bit synthDriver host runtime uses WASAPI
directly.
* Compile sonic independently of eSpeak and for all archetectures (not
just x64), as it is needed by sapi5 when running in NVDA's 32 bit
synthDriver host runtime. The x86 sonic dll will be placed in
source/synthDrivers32.
* winBindings.kernel32: add definition for GetCurrentProcessId. Used in
logging for NVDA's 32 bit synthDriver host runtime.
* Add a jobObject module, which contains a Job class which wraps a win32
Job object, which allows automatically killing any process associated
with it. This is sued to ensure that any process started for the 32 bit
synthDriver host runtime is killed off if NvDA exits.
* Add a _bridge package, which provides a client and runtime for the 32
bit synthDriver host. The client is code that runs in NVDA, and the
runtime is code that runs in its own 32 bit child process. Communication
between NvDA and the child process is via RPYC over the child processs's
standard pipe handles. This _bridge package can be later extended with
more clients and runtimes as we start to implement a full ART.
 
* Add 32 bit sapi4 and sapi5 synthDrivers which use the
synthDriverHost32 runtime.

.github/workflows/testAndPublish.yml
@@ -84,6 +84,13 @@ jobs:
       with:
         python-version: ${{ matrix.pythonVersion }}
         architecture: ${{ matrix.arch }}
+        update-environment: true
+    - name: Install Python (32 bit)
+      uses: actions/setup-python@v6
+      with:
+        python-version: ${{ matrix.pythonVersion }}
+        architecture: x86
+        update-environment: false
     - name: Install the latest version of uv
       uses: astral-sh/setup-uv@v7
       with:
@@ -106,6 +113,13 @@ jobs:
     - name: Prepare source code
       shell: cmd
       run: scons source %sconsArgs% %sconsCores%
+    - name: Run synthDriverHost32 runtime builder with its own Python
+      shell: pwsh
+      env:
+        UV_PYTHON: ""          # clear setup-uv override
+        VIRTUAL_ENV: ""        # avoid inheriting outer venv
+      run: |
+        uv run --no-active --directory runtime-builders/synthDriverHost32 python setup-runtime.py --dest-dir ../../source/lib/x86/synthDriverHost-runtime
     - name: Prepare for tests
       run: ci/scripts/tests/beforeTests.ps1
     - name: Cache scons build

runtime-builders/synthDriverHost32/setup-runtime.py
@@ -1,0 +1,257 @@
+# A part of NonVisual Desktop Access (NVDA)
+# Copyright (C) 2006-2025 NV Access Limited, Peter Vágner, Joseph Lee
+# This file is covered by the GNU General Public License.
+# See the file COPYING for more details.
+
+import os
+import importlib
+import fnmatch
+from glob import glob
+import sys
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--dest-dir", default="dist")
+args = parser.parse_args()
+
+nvdaSourceDir = os.path.join("..", "..", "source")
+runtimeSourceDir = os.path.join(nvdaSourceDir, "_bridge", "runtimes", "synthDriverHost")
+runtimeName = "synthDriverHost"
+runtimeDestDir = args.dest_dir
+
+
+sys.path.insert(0, nvdaSourceDir)
+
+import gettext  # noqa: E402
+from buildVersion import (  # noqa: E402
+	formatBuildVersionString,
+	name,
+	publisher,
+	version,
+)
+
+gettext.install("nvda")
+
+# versionInfo names must be imported after Gettext
+# Suppress E402 (module level import not at top of file)
+from versionInfo import (  # noqa: E402
+	copyright as NVDAcopyright,  # copyright is a reserved python keyword
+	description,
+)
+
+
+from py2exe import freeze  # noqa: E402
+from py2exe.dllfinder import DllFinder  # noqa: E402
+
+RT_MANIFEST = 24
+manifestTemplateFilePath = os.path.join(nvdaSourceDir, "manifest.template.xml")
+
+with open(manifestTemplateFilePath, "r", encoding="utf-8") as manifestTemplateFile:
+	_manifestTemplate = manifestTemplateFile.read()
+
+
+def _genManifestTemplate(shouldHaveUIAccess: bool) -> tuple[int, int, bytes]:
+	return (
+		RT_MANIFEST,
+		1,
+		(_manifestTemplate % {"uiAccess": shouldHaveUIAccess}).encode("utf-8"),
+	)
+
+
+# py2exe's idea of whether a dll is a system dll appears to be wrong sometimes, so monkey patch it.
+orig_determine_dll_type = DllFinder.determine_dll_type
+
+
+def determine_dll_type(self, imagename):
+	dll = os.path.basename(imagename).lower()
+	if dll.startswith("api-ms-win-") or dll in ("powrprof.dll", "mpr.dll", "crypt32.dll"):
+		# These are definitely system dlls available on all systems and must be excluded.
+		# Including them can cause serious problems when a binary build is run on a different version of Windows.
+		return None
+	return orig_determine_dll_type(self, imagename)
+
+
+DllFinder.determine_dll_type = determine_dll_type
+
+
+def getRecursiveDataFiles(dest: str, source: str, excludes: tuple = ()) -> list[tuple[str, list[str]]]:
+	rulesList: list[tuple[str, list[str]]] = []
+	for file in glob(f"{source}/*"):
+		if not any(fnmatch.fnmatch(file, exclude) for exclude in excludes) and os.path.isfile(file):
+			rulesList.append((dest, [file]))
+	for dirName in os.listdir(source):
+		if os.path.isdir(os.path.join(source, dirName)) and not dirName.startswith("."):
+			rulesList.extend(
+				getRecursiveDataFiles(
+					os.path.join(dest, dirName),
+					os.path.join(source, dirName),
+					excludes=excludes,
+				),
+			)
+	return rulesList
+
+
+sys.path.insert(0, runtimeSourceDir)
+
+freeze(
+	version_info={
+		"version": formatBuildVersionString(),
+		"description": description,
+		"product_name": name,
+		"product_version": version,
+		"copyright": NVDAcopyright,
+		"company_name": publisher,
+	},
+	console=[
+		{
+			"script": f"{runtimeSourceDir}/main.pyw",
+			"dest_base": f"nvda_{runtimeName}",
+			"icon_resources": [(1, f"{nvdaSourceDir}/images/nvda.ico")],
+			"other_resources": [_genManifestTemplate(shouldHaveUIAccess=False)],
+			"version_info": {
+				"version": formatBuildVersionString(),
+				"description": "32 bit NVDA synthDriver host runtime",
+				"product_name": "NVDA",
+				"product_version": version,
+				"copyright": NVDAcopyright,
+				"company_name": publisher,
+			},
+		},
+	],
+	options={
+		"verbose": 2,
+		# Removes assertions for builds.
+		# https://docs.python.org/3.13/tutorial/modules.html#compiled-python-files
+		"optimize": 1,
+		"bundle_files": 3,
+		"dist_dir": runtimeDestDir,
+		"excludes": [
+			"_localCaptioner",
+			"_remoteClient",
+			"addonHandler",
+			"addonStore",
+			"appModules",
+			"audio",
+			"brailleDisplayDrivers",
+			"brailleTables",
+			"COMRegistrationFixes",
+			"documentNavigation",
+			"contentRegoc",
+			"controlTypes",
+			"globalPlugins",
+			"gui",
+			"hwIo",
+			"IAccessibleHandler",
+			"louis",
+			"mathPres",
+			"NVDAHelper",
+			"NVDAObjects",
+			"screenCurtain",
+			"textInfos",
+			"textUtils",
+			"UIAHandler",
+			"virtualBuffers",
+			"vision",
+			"visionEnhancementProviders",
+			"wx",
+			"addonAPIVersion",
+			"annotation",
+			"api",
+			"appModuleHandler",
+			"aria",
+			"bdDetect",
+			"braille",
+			"brailleInput",
+			"browseMode",
+			"characterProcessing",
+			"compoundDocuments",
+			"cursorManager",
+			"diffHandler",
+			"displayModel",
+			"documentBase",
+			"easeOfAccess",
+			"editableText",
+			"eventHandler",
+			"globalCommands",
+			"globalPluginHandler",
+			"hwPortUtils",
+			"inputCore",
+			"installer",
+			"JABHandler",
+			"keyboardHandler",
+			"keyLabels",
+			"locationHelper",
+			"louisHelper",
+			"mathType",
+			"mouseHandler",
+			"oleacc",
+			"pythonConsole",
+			"queueHandler",
+			"review",
+			"screenBitmap",
+			"screenExplorer",
+			"scriptHandler",
+			"speechViewer",
+			"tableUtils",
+			"tones",
+			"touchhandler",
+			"touchTracker",
+			"treeInterceptorHandler",
+			"ui",
+			"updateCheck",
+			"vkCodes",
+			"watchdog",
+			"wincon",
+			"winConsoleHandler",
+			"windowUtils",
+			"winInputHook",
+			"xmlFormatting",
+			"tkinter",
+			"serial.loopback_connection",
+			"serial.rfc2217",
+			"serial.serialcli",
+			"serial.serialjava",
+			"serial.serialposix",
+			"serial.socket_connection",
+			# netbios (from pywin32) is optionally used by Python3's uuid module.
+			# This is not needed.
+			# We also need to exclude win32wnet explicitly.
+			"netbios",
+			"win32wnet",
+			# winxptheme is optionally used by wx.lib.agw.aui.
+			# We don't need this.
+			"winxptheme",
+			# multiprocessing isn't going to work in a frozen environment
+			"multiprocessing",
+			"concurrent.futures.process",
+			# Tomli is part of Python 3.11+ as Tomlib, but is imported as tomli by cryptography, which causes an infinite loop in py2exe
+			"tomli",
+		],
+		"packages": [
+			"winBindings",
+			"speech",
+			"synthDrivers",
+		],
+		"includes": [
+			"win32event",
+			"win32file",
+			"win32pipe",
+			"audioDucking",
+			"comtypes.stream",
+		],
+	},
+	data_files=[
+		(".", glob("*.dll") + glob("*.manifest")),
+	]
+	+ getRecursiveDataFiles(
+		"synthDrivers",
+		f"{runtimeSourceDir}\\synthDrivers",
+		excludes=tuple(f"*{ext}" for ext in importlib.machinery.all_suffixes())
+		+ (
+			"*.exp",
+			"*.lib",
+			"*.pdb",
+		),
+	),
+)

sconstruct
@@ -5,8 +5,10 @@
 # This file may be used under the terms of the GNU General Public License, version 2 or later.
 # For more details see: https://www.gnu.org/licenses/gpl-2.0.html
 
+import copy
 import multiprocessing
 import os
+import shutil
 import platform
 import sys
 from pathlib import Path
@@ -692,3 +694,26 @@ env.Tool("listModules")
 source = env.Dir(os.path.join(os.getcwd(), "dist"))
 target = env.File(os.path.join(outputDir.abspath, "library_modules.txt"))
 env.Alias("moduleList", env.GenerateModuleList(target, source))
+
+synthDriverHost32Runtime = env.Command(
+	target="#runtime-builders/synthDriverHost32/dist",
+	source="#runtime-builders/synthDriverHost32/setup-runtime.py",
+	chdir=Dir("#runtime-builders/synthDriverHost32"),
+	action=[
+		  "uv run -v python setup-runtime.py ",
+		r"rmdir /s /q ..\..\source\lib\x86\synthDriverHost-runtime || exit 0",
+		r"xcopy /e /i /y dist ..\..\source\lib\x86\synthDriverHost-runtime",
+	],
+	ENV=os.environ,
+)
+env.AlwaysBuild(synthDriverHost32Runtime)
+env.Alias("synthDriverHost32Runtime", synthDriverHost32Runtime)
+
+# Copy sapi5 and sonic Python files to the _synthDrivers32 directory
+# As they require no changes, but need to be in an isolated directory for 32 bit.
+env.Command(sourceDir.File('_synthDrivers32/_sonic.py'), sourceDir.File('synthDrivers/_sonic.py'), Copy("$TARGET", "$SOURCE"))
+env.Command(sourceDir.File('_synthDrivers32/_sapi5.py'), sourceDir.File('synthDrivers/sapi5.py'), Copy("$TARGET", "$SOURCE"))
+
+# Parts of the speech package are quired by the 32 bit synthDriver host tunrime
+env.Command(sourceDir.File('_bridge/runtimes/synthDriverHost/speech/commands.py'), sourceDir.File('speech/commands.py'), Copy("$TARGET", "$SOURCE"))
+env.Command(sourceDir.File('_bridge/runtimes/synthDriverHost/speech/types.py'), sourceDir.File('speech/types.py'), Copy("$TARGET", "$SOURCE"))

.gitignore
@@ -53,3 +53,7 @@ nvdaHelper/docs/
 *.pfx
 *.egg-info
 source/nvda_crash_stats.txt
+source/_synthDrivers32/_sonic.py
+source/_synthDrivers32/_sapi5.py
+source/_bridge/runtimes/synthDriverHost/speech/commands.py
+source/_bridge/runtimes/synthDriverHost/speech/types.py

nvdaHelper/archBuild_sconscript
@@ -225,12 +225,13 @@ Export("detoursLib")
 apiHookObj = env.Object("apiHook", "common/apiHook.cpp")
 Export("apiHookObj")
 
+localLib = env.SConscript("local/sconscript")
+Export("localLib")
+if signExec:
+	env.AddPostAction(localLib[0], [signExec])
+env.Install(libInstallDir, localLib)
+
 if isNVDAHelperLocalArch:
-	localLib = env.SConscript("local/sconscript")
-	Export("localLib")
-	if signExec:
-		env.AddPostAction(localLib[0], [signExec])
-	env.Install(libInstallDir, localLib)
 	win10localLib = env.SConscript(
 		"localWin10/sconscript",
 	)
@@ -261,7 +262,15 @@ if not isNVDAHelperLocalArch:
 	env.Install(libInstallDir, remoteLoaderProgram)
 
 if isNVDACoreArch:
+	sonicLib = thirdPartyEnv.SConscript("sonic/sconscript")
+	Export("sonicLib")
+	env.Install(sourceDir.Dir('synthDrivers'), sonicLib)
 	thirdPartyEnv.SConscript("espeak/sconscript")
 	thirdPartyEnv.SConscript("liblouis/sconscript")
 	thirdPartyEnv.SConscript("javaAccessBridge/sconscript")
 	thirdPartyEnv.SConscript("uwp/sconscript")
+
+if TARGET_ARCH == "x86":
+	sonicLib = thirdPartyEnv.SConscript("sonic/sconscript")
+	Export("sonicLib")
+	env.Install(sourceDir.Dir('_synthDrivers32'), sonicLib)

nvdaHelper/espeak/sconscript
@@ -20,12 +20,22 @@ Import(
 	[
 		"thirdPartyEnv",
 		"sourceDir",
+		"sonicLib",
 	]
 )
 
 freeLibrary = ctypes.windll.kernel32.FreeLibrary
 freeLibrary.argtypes = (ctypes.wintypes.HANDLE,)
+DLL_DIRECTORY_COOKIE = ctypes.c_void_p
+AddDllDirectory = ctypes.windll.kernel32.AddDllDirectory
+AddDllDirectory.argtypes = (ctypes.wintypes.LPCWSTR,)
+AddDllDirectory.restype = DLL_DIRECTORY_COOKIE 
 
+# Add the directory of the sonic dll to our DLL search path
+# So that eSpeak.dll cna find it when being loaded to compile dictionaries etc.
+sonicDllDir = sonicLib[0].get_dir().abspath
+AddDllDirectory(sonicDllDir)
+ 
 
 class AutoFreeCDLL(ctypes.CDLL):
 	def __del__(self):
@@ -1043,16 +1053,6 @@ def espeak_compileDict_buildAction(
 		espeak.espeak_Terminate()
 	return ACTION_SUCCESS
 
-
-sonicLib = env.SharedLibrary(
-	target="sonic",
-	srcdir=sonicSrcDir.abspath,
-	source=[
-		"sonic.c",
-		Dir(".").File("sonic.def"),
-	],
-)
-
 espeakLib = env.SharedLibrary(
 	target="espeak",
 	srcdir=espeakSrcDir.Dir("libespeak-ng").abspath,
@@ -1112,7 +1112,7 @@ espeakLib = env.SharedLibrary(
 		# com\ttsengine.cpp
 		# We do not use the ASYNC compile option in espeak.
 	],
-	LIBS=["advapi32", "sonic"],
+	LIBS=["advapi32", sonicLib[2]] ,
 	LIBPATH=".",
 )
 
@@ -1164,7 +1164,6 @@ for dictFileName, (langCode, inputFiles) in espeakDictionaryCompileList.items():
 	)
 
 env.Install(synthDriversDir, espeakLib)
-env.Install(synthDriversDir, sonicLib)
 
 # install espeak-ng-data
 targetEspeakDataDir = synthDriversDir.Dir("espeak-ng-data")

nvdaHelper/sonic/sconscript
@@ -1,0 +1,32 @@
+# A part of NonVisual Desktop Access (NVDA)
+# This file is covered by the GNU General Public License.
+# See the file COPYING for more details.
+# Copyright (C) 2011-2025 NV Access Limited
+
+sourceLibDir: SCons.Node.FS.Dir
+thirdPartyEnv: SCons.Environment.Environment
+
+Import(
+	[
+		"thirdPartyEnv",
+	]
+)
+
+sonicSrcDir = Dir("#include/sonic")
+
+env: SCons.Environment.Environment = thirdPartyEnv.Clone()
+
+env.Append(CPPPATH=sonicSrcDir)
+
+sonicObj = env.Object("sonic.obj", sonicSrcDir.File('sonic.c'))
+
+sonicLib = env.SharedLibrary(
+	target="sonic",
+	source=[
+		sonicObj,
+		Dir(".").File("sonic.def"),
+	],
+)
+
+
+Return("sonicLib")

nvdaHelper/sonic/sonic.def
@@ -1,0 +1,37 @@
+; A part of NonVisual Desktop Access (NVDA)
+; This file is covered by the GNU General Public License.
+; See the file COPYING for more details.
+; Copyright (C) 2025 NV Access Limited, gexgd0419
+
+LIBRARY sonic
+EXPORTS
+	sonicCreateStream
+	sonicDestroyStream
+	sonicSetUserData
+	sonicGetUserData
+	sonicWriteFloatToStream
+	sonicWriteShortToStream
+	sonicWriteUnsignedCharToStream
+	sonicReadFloatFromStream
+	sonicReadShortFromStream
+	sonicReadUnsignedCharFromStream
+	sonicFlushStream
+	sonicSamplesAvailable
+	sonicGetSpeed
+	sonicSetSpeed
+	sonicGetPitch
+	sonicSetPitch
+	sonicGetRate
+	sonicSetRate
+	sonicGetVolume
+	sonicSetVolume
+	sonicGetChordPitch
+	sonicSetChordPitch
+	sonicGetQuality
+	sonicSetQuality
+	sonicGetSampleRate
+	sonicSetSampleRate
+	sonicGetNumChannels
+	sonicSetNumChannels
+	sonicChangeFloatSpeed
+	sonicChangeShortSpeed

commit 01b0001ba47b455804403a9aee8a5beaf5c5fe43

Set correct version and publisher info on synthDriver host executable (#19762)
Fixes #19654

### Summary of the issue:
The 32 bit synthDriverhost runtime introduced in PR #19432 Does not
contain correct version information in its executable.

### Description of user facing changes:

### Description of developer facing changes:

### Description of development approach:
* synthDriverHost's setup-runtime.py py2exe script now takes version and
publisher as commandline arguments, so that these can be set on the
executable dynamically.
* The github workflow and sconstruct pass the correct version and
publisher to the setup-runtime.py py2exe script.

### Testing strategy:
* [x] Locally compiled with scons synthDriverHost32Runtime, providing
custom version and publisher. Confirmed that the synthDriverHost
executable contains correct version and publisher information.
* [x] Create try bild. Confirming the same as above.

### Known issues with pull request:
None known.

.github/workflows/testAndPublish.yml
@@ -119,7 +119,7 @@ jobs:
         UV_PYTHON: ""          # clear setup-uv override
         VIRTUAL_ENV: ""        # avoid inheriting outer venv
       run: |
-        uv run --no-active --directory runtime-builders/synthDriverHost32 python setup-runtime.py --dest-dir ../../source/lib/x86/synthDriverHost-runtime
+        uv run --no-active --directory runtime-builders/synthDriverHost32 python setup-runtime.py --dest-dir ../../source/lib/x86/synthDriverHost-runtime --version "$env:version" --publisher "$env:scons_publisher"
     - name: Prepare for tests
       run: ci/scripts/tests/beforeTests.ps1
     - name: Cache scons build

runtime-builders/synthDriverHost32/setup-runtime.py
@@ -13,6 +13,8 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--dest-dir", default="dist")
+parser.add_argument("--version")
+parser.add_argument("--publisher")
 args = parser.parse_args()
 
 nvdaSourceDir = os.path.join("..", "..", "source")
@@ -30,6 +32,12 @@ from buildVersion import (  # noqa: E402
 	publisher,
 	version,
 )
+
+if args.version is not None:
+	version = args.version
+if args.publisher is not None:
+	publisher = args.publisher
+
 
 gettext.install("nvda")
 

sconstruct
@@ -708,7 +708,22 @@ synthDriverHost32Runtime = env.Command(
 	source="#runtime-builders/synthDriverHost32/setup-runtime.py",
 	chdir=Dir("#runtime-builders/synthDriverHost32"),
 	action=[
-		  "uv run -v python setup-runtime.py ",
+		  [
+			  "uv",
+			  "run",
+			  "-v",
+			  "--no-active",
+			  "--directory",
+			  ".",
+			  "python",
+			  "setup-runtime.py",
+			  "--dest-dir",
+			  "dist",
+			  "--version",
+			  version,
+			  "--publisher",
+			  publisher,
+		  ],
 		r"rmdir /s /q ..\..\source\lib\x86\synthDriverHost-runtime || exit 0",
 		r"xcopy /e /i /y dist ..\..\source\lib\x86\synthDriverHost-runtime",
 	],