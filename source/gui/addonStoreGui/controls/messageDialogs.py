410d6e380f6fed5732a08c9fde7f93a07821411a
Sascha Cowley <16543535+SaschaCowley@users.noreply.github.com>
2026-03-20 13:30:38 +1100

    --amend

--- source/gui/addonStoreGui/controls/messageDialogs.py
+++ source/gui/addonStoreGui/controls/messageDialogs.py
@@ -5,6 +5,7 @@
 
 from itertools import chain, filterfalse
 import os.path
+import sys
 import threading
 from time import sleep
 from typing import (
@@ -804,9 +805,7 @@ class _CopyAddonsDialog(
 				),
 			)
 			if userManifest is None:
-				log.debug(index)
-				log.debug(self._addonsList.removeCheckbox(index))
-				log.debug(self._addonsList.removeCheckbox(index))
+				self._addonsList.removeCheckbox(index)
 			elif systemManifest is not None:
 				self._addonsList.CheckItem(index)
 		activeIndex = 0
@@ -932,8 +931,7 @@ def _getAddonsToCopy(parent: wx.Window) -> list[str] | None:
 			),
 		)
 	}
-	systemAddonsDir = os.path.join("c:\\", "program files", "nvda", "systemConfig", "addons")
-	# systemAddonsDir = os.path.join(sys.prefix, "systemConfig", "addons")
+	systemAddonsDir = os.path.join(sys.prefix, "systemConfig", "addons")
 	try:
 		systemManifests = {
 			manifest["name"].casefold(): manifest for manifest in _getAddonManifestsFromPath(systemAddonsDir)
