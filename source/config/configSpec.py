6a7ba8e9516f27c9f837d0224b3bccce5b0ac940
kefaslungu <jameskefaslungu@gmail.com>
2026-03-24 17:20:07 +0100

    clarifying comment added above the setting
    Signed-off-by: kefaslungu <jameskefaslungu@gmail.com>

--- source/config/configSpec.py
+++ source/config/configSpec.py
@@ -215,6 +215,7 @@ schemaVersion = integer(min=0, default={latestSchemaVersion})
 	enableOnPageLoad = boolean(default=true)
 	loadChromiumVBufOnBusyState = featureFlag(optionsEnum="BoolFlag", behaviorOfDefault="enabled")
 	textParagraphRegex = string(default="{configDefaults.DEFAULT_TEXT_PARAGRAPH_REGEX}")
+	# Element types available for cycling in browse touch mode.
 	browseModeTouchNavigationElements = string_list(default=list("heading", "link", "formField", "list", "table"))
 
 [touch]
