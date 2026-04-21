00eb8bfdc3f2b909b2c0ca0d6a675aae549e62d6
Sean Budd <sean@nvaccess.org>
2026-04-21 14:08:07 +1000

    Apply suggestion from @seanbudd

--- source/gui/settingsDialogs.py
+++ source/gui/settingsDialogs.py
@@ -6352,8 +6352,6 @@ class PrivacyAndSecuritySettingsPanel(SettingsPanel):
 				config.conf["general"]["loggingLevel"] = logging.getLevelName(selectedLogLevel)
 				logHandler.setLogLevelFromConfig()
 				self._savedLogLevel = selectedLogLevel
-			else:
-				log.debug("User cancelled log level change, keeping original control value.")
 
 		if updateCheck:
 			config.conf["update"]["allowUsageStats"] = self._allowUsageStatsCheckBox.IsChecked()
