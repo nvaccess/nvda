71df59b7e035fb7cd79a028d55c9c3363bfea54e
cary-rowen <manchen_0528@outlook.com>
2026-04-26 18:20:43 +0800

    Add Changes and copyright headers.

--- tests/unit/test_addonStoreNetwork.py
+++ tests/unit/test_addonStoreNetwork.py
@@ -1,5 +1,5 @@
 # A part of NonVisual Desktop Access (NVDA)
-# Copyright (C) 2026 NV Access Limited, Christopher Pross
+# Copyright (C) 2026 NV Access Limited, Christopher Pross, Cary-rowen
 # This file may be used under the terms of the GNU General Public License, version 2 or later, as modified by the NVDA license.
 # For full terms and any additional permissions, see the NVDA license file: https://github.com/nvaccess/nvda/blob/master/copying.txt
 
