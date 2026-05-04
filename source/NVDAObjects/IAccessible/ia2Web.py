5cb3fcf8f0b2c369c3921efe99806ea51667f45e
Ryan McCleary <remccleary@gmail.com>
2026-05-04 02:48:35 -0400

    Update source/NVDAObjects/IAccessible/ia2Web.py
    Co-authored-by: Sean Budd <seanbudd123@gmail.com>

--- source/NVDAObjects/IAccessible/ia2Web.py
+++ source/NVDAObjects/IAccessible/ia2Web.py
@@ -341,10 +341,8 @@ class Math(Ia2Web):
 
 			langAttr = f' xml:lang="{self.language}"' if self.language else ""
 			mathMl = f"<math{langAttr}>{mathAttr}</math>"
-			log.debug(
-				f"Got MathML from IA2 math attribute (length {len(mathMl)}): "
-				f"{mathMl[:100]!r}{'...' if len(mathMl) > 100 else ''}",
-			)
+			if log.isEnabledFor(log.DEBUG) and isMSAADebugLoggingEnabled():
+				log.debug(f"Got MathML from IA2 math attribute: {mathMl}")
 			return mathMl
 
 		from comtypes.gen.ISimpleDOM import ISimpleDOMNode  # type: ignore[reportMissingImports]
