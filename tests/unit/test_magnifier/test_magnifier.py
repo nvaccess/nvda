bf096343342202d5aa39e281e5f271892fa8c645
Sean Budd <sean@nvaccess.org>
2026-05-20 17:54:53 +1000

    fix up values

--- tests/unit/test_magnifier/test_magnifier.py
+++ tests/unit/test_magnifier/test_magnifier.py
@@ -1,8 +1,9 @@
 # A part of NonVisual Desktop Access (NVDA)
-# Copyright (C) 2025 NV Access Limited, Antoine Haffreingue
+# Copyright (C) 2025-2026 NV Access Limited, Antoine Haffreingue
 # This file may be used under the terms of the GNU General Public License, version 2 or later, as modified by the NVDA license.
 # For full terms and any additional permissions, see the NVDA license file: https://github.com/nvaccess/nvda/blob/master/copying.txt
 
+from _magnifier.config import ZoomLevel
 from _magnifier.magnifier import Magnifier
 from _magnifier.utils.types import Filter, Direction, Coordinates, MagnifierAction
 from comtypes import COMError
@@ -63,7 +64,7 @@ class TestMagnifier(_TestMagnifier):
 
 	def testMagnifierCreation(self):
 		"""Can we create a magnifier with valid parameters?"""
-		self.assertEqual(self.magnifier.zoomLevel, 2.0)
+		self.assertEqual(self.magnifier.zoomLevel, 200)
 		self.assertEqual(self.magnifier._filterType, Filter.NORMAL)
 		self.assertFalse(self.magnifier._isActive)
 		self.assertIsNotNone(self.magnifier._focusManager)
@@ -72,25 +73,25 @@ class TestMagnifier(_TestMagnifier):
 	def testZoomLevelProperty(self):
 		"""ZoomLevel property."""
 		# Test valid directions
-		self.magnifier.zoomLevel = 5.0
-		self.assertEqual(self.magnifier.zoomLevel, 5.0)
+		self.magnifier.zoomLevel = 500
+		self.assertEqual(self.magnifier.zoomLevel, 500)
 
-		self.magnifier.zoomLevel = 1.0
+		self.magnifier.zoomLevel = 100
 		self.magnifier._zoom(Direction.IN)
-		self.assertEqual(self.magnifier.zoomLevel, 1.5)
+		self.assertEqual(self.magnifier.zoomLevel, 150)
 
-		self.magnifier.zoomLevel = 10.0
+		self.magnifier.zoomLevel = 1000
 		self.magnifier._zoom(Direction.OUT)
-		self.assertEqual(self.magnifier.zoomLevel, 9.5)
+		self.assertEqual(self.magnifier.zoomLevel, 950)
 
 		# Test limits
-		self.magnifier.zoomLevel = 1.0
+		self.magnifier.zoomLevel = ZoomLevel.MIN_ZOOM
 		self.magnifier._zoom(Direction.OUT)  # Should stay at min
-		self.assertEqual(self.magnifier.zoomLevel, 1.0)
+		self.assertEqual(self.magnifier.zoomLevel, ZoomLevel.MIN_ZOOM)
 
-		self.magnifier.zoomLevel = 10.0
+		self.magnifier.zoomLevel = ZoomLevel.MAX_ZOOM
 		self.magnifier._zoom(Direction.IN)  # Should stay at max
-		self.assertEqual(self.magnifier.zoomLevel, 10.0)
+		self.assertEqual(self.magnifier.zoomLevel, ZoomLevel.MAX_ZOOM)
 
 	def testStartMagnifier(self):
 		"""Activating the magnifier."""
@@ -269,25 +270,25 @@ class TestMagnifier(_TestMagnifier):
 	def testZoom(self):
 		"""zoom in and out with valid values and check boundaries."""
 		# Set initial zoom to 1.0 for predictable testing
-		self.magnifier.zoomLevel = 1.0
+		self.magnifier.zoomLevel = 100
 
 		# Test zoom in
 		self.magnifier._zoom(Direction.IN)
-		self.assertEqual(self.magnifier.zoomLevel, 1.5)
+		self.assertEqual(self.magnifier.zoomLevel, 150)
 
 		# Test zoom out
 		self.magnifier._zoom(Direction.OUT)
-		self.assertEqual(self.magnifier.zoomLevel, 1.0)
+		self.assertEqual(self.magnifier.zoomLevel, 100)
 
 		# Test zoom in at maximum boundary
-		self.magnifier.zoomLevel = 10.0
+		self.magnifier.zoomLevel = ZoomLevel.MAX_ZOOM
 		self.magnifier._zoom(Direction.IN)
-		self.assertEqual(self.magnifier.zoomLevel, 10.0)  # Should remain at max
+		self.assertEqual(self.magnifier.zoomLevel, ZoomLevel.MAX_ZOOM)  # Should remain at max
 
 		# Test zoom out at minimum boundary
-		self.magnifier.zoomLevel = 1.0
+		self.magnifier.zoomLevel = ZoomLevel.MIN_ZOOM
 		self.magnifier._zoom(Direction.OUT)
-		self.assertEqual(self.magnifier.zoomLevel, 1.0)  # Should remain at min
+		self.assertEqual(self.magnifier.zoomLevel, ZoomLevel.MIN_ZOOM)  # Should remain at min
 
 	def _setupPanTest(self):
 		"""Common setup for pan tests."""
@@ -510,7 +511,7 @@ class TestMagnifier(_TestMagnifier):
 	def testClampCoordinates(self):
 		"""Test all boundary clamps (left, right, top, bottom) for both modes."""
 		for isTrueCentered in (False, True):
-			self.magnifier.zoomLevel = 2.0
+			self.magnifier.zoomLevel = 200
 			with patch("_magnifier.magnifier.isTrueCentered", return_value=isTrueCentered):
 				minX, minY, maxX, maxY = self.magnifier._getScreenLimits()
 
@@ -536,7 +537,7 @@ class TestMagnifier(_TestMagnifier):
 
 	def testClampCoordinatesWithinBounds(self):
 		"""Coordinates within bounds are not modified."""
-		self.magnifier.zoomLevel = 2.0
+		self.magnifier.zoomLevel = 200
 		with patch("_magnifier.magnifier.isTrueCentered", return_value=False):
 			minX, minY, maxX, maxY = self.magnifier._getScreenLimits()
 			centerX = (minX + maxX) // 2
