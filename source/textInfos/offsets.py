0392c0854b6f0fee6beda6d333107d24db983233
Leonard de Ruijter <alderuijter@gmail.com>
2026-06-22 16:01:28 +0200

    Fix copyright

--- source/textInfos/offsets.py
+++ source/textInfos/offsets.py
@@ -1,5 +1,5 @@
 # A part of NonVisual Desktop Access (NVDA)
-# Copyright (C) 2006-2025 NV Access Limited, Babbage B.V., Leonard de Ruijter, Wang Chong
+# Copyright (C) 2006-2026 NV Access Limited, Babbage B.V., Leonard de Ruijter, Wang Chong
 # This file may be used under the terms of the GNU General Public License, version 2 or later, as modified by the NVDA license.
 # For full terms and any additional permissions, see the NVDA license file: https://github.com/nvaccess/nvda/blob/master/copying.txt
 
