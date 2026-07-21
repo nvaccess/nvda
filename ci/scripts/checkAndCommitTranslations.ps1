42aa125866c34891b594662633aeaffdb379a972
Sascha Cowley <16543535+SaschaCowley@users.noreply.github.com>
2026-07-21 18:50:12 +1000

    Verbose output for testing

--- ci/scripts/checkAndCommitTranslations.ps1
+++ ci/scripts/checkAndCommitTranslations.ps1
@@ -13,7 +13,9 @@ git config --local user.email "github-actions@github.com"
 # Temporary file to store all collected reports.
 $tempfile = New-TemporaryFile
 $errordir = New-Item -ItemType "Directory" -Path . -Name "translationErrors"
+Write-Host "$errordir: $errordir"
 $errorfiles = @()
+Write-Host "$errorfiles: $errorfiles"
 
 # Check each modified tracked po file,
 # and collect language codes pushed down the pipeline in an array.
@@ -31,9 +33,12 @@ $failures = @(git ls-files --modified "source/locale/**.po" | ForEach-Object {
 		# and getting the leaf (the trailing path component),
 		$lang = $_ | Split-Path | Split-Path | Split-Path -Leaf
 		$errorfile = New-Item -ItemType "File" -Path $errordir -Name "$lang.txt"
+		Write-Host "errorfile: $errorfile"
 		Add-Content -Path $tempfile,$errorfile -Value $output -PassThru | Out-String | Write-Error
 		Add-Content -Path $tempfile -Value "----------"
 		$errorfiles += $errorfile
+		Write-Host "${errorfile}: $((Get-Item $errorfile).Length)"
+		Write-Host "errorfiles: $errorfiles"
 
 		# and push it down the pipeline
 		$lang
