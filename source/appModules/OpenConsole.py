edf6ed2c1e392ab704f8796063b7f2fa7e26e00f
Arnold Loubriat <datatriny@gmail.com>
2026-07-28 10:21:55 +0200

    Rename app module to match executable name

--- source/appModules/OpenConsole.py
+++ source/appModules/OpenConsole.py
@@ -3,7 +3,7 @@
 # This file may be used under the terms of the GNU General Public License, version 2 or later, as modified by the NVDA license.
 # For full terms and any additional permissions, see the NVDA license file: https://github.com/nvaccess/nvda/blob/master/copying.txt
 
-"""App module for the Windows Terminal application (wt.exe / WindowsTerminal.exe)."""
+"""App module for the Windows Terminal application (OpenConsole.exe)."""
 
 import appModuleHandler
 import winUser
