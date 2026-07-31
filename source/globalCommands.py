a772a5e02346c90eb192662af474ccaded07eea9
Noelia Ruiz Martínez <nrm1977@gmail.com>
2026-07-31 03:07:26 +0200

    Add gestures for braille autoScroll commands (#20502)
    Fixes #20473
    Summary of the issue:
    
    Braille autoScroll scripts don't have associated gestures by default.
    Description of user facing changes:
    
    Now, gestures for autoScroll braille commands are available by default:
    
        NVDA+alt+k: toggles autoScroll.
        NVDA+alt+j: Decreases scroll rate.
        NVDA+alt+l: Increases scroll rate.

--- source/globalCommands.py
+++ source/globalCommands.py
@@ -851,6 +851,7 @@ class GlobalCommands(ScriptableObject):
 		# Translators: Input help mode message for command to toggle braille automatic scroll.
 		description=_("Toggles braille automatic scroll"),
 		category=SCRCAT_BRAILLE,
+		gesture="kb:NVDA+alt+k",
 	)
 	def script_toggleBrailleAutoScroll(self, gesture: inputCore.InputGesture):
 		shouldEnableAutoScroll = braille.handler._autoScrollCallLater is None
@@ -872,6 +873,7 @@ class GlobalCommands(ScriptableObject):
 		# Translators: Input help mode message for command to increase the rate for braille automatic scroll.
 		description=_("Increases the rate for braille automatic scroll"),
 		category=SCRCAT_BRAILLE,
+		gesture="kb:NVDA+alt+l",
 	)
 	def script_increaseBrailleAutoScrollRate(self, gesture: inputCore.InputGesture):
 		config.conf.clampedIncrementAndUpdateConfig("braille", "autoScrollRate", step=0.5)
@@ -884,6 +886,7 @@ class GlobalCommands(ScriptableObject):
 		# Translators: Input help mode message for command to decrease the rate for braille automatic scroll.
 		description=_("Decreases the rate for braille automatic scroll"),
 		category=SCRCAT_BRAILLE,
+		gesture="kb:NVDA+alt+j",
 	)
 	def script_decreaseBrailleAutoScrollRate(self, gesture: inputCore.InputGesture):
 		config.conf.clampedIncrementAndUpdateConfig("braille", "autoScrollRate", step=-0.5)
