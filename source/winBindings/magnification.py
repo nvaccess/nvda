adbca44a2197737cb48f37b94245dff30f084f69
Sean Budd <sean@nvaccess.org>
2026-08-03 16:23:07 +1000

    lp fix

--- source/winBindings/magnification.py
+++ source/winBindings/magnification.py
@@ -6,7 +6,7 @@
 """Functions exported by magnification.dll, and supporting data structures and enumerations."""
 
 from ctypes import POINTER, WINFUNCTYPE, Structure, WinError, c_float, c_int, windll
-from ctypes.wintypes import BOOL, RECT
+from ctypes.wintypes import BOOL, LPRECT
 from _ctypes import CFuncPtr
 from typing import Any
 
@@ -25,7 +25,6 @@ class MAGCOLOREFFECT(Structure):
 
 
 PMAGCOLOREFFECT = POINTER(MAGCOLOREFFECT)
-LPRECT = POINTER(RECT)
 
 
 def _errCheck[T: tuple[Any]](result: int, func: CFuncPtr, args: T) -> T:
