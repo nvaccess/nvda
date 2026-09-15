e1b0a128fc9ebdc0f78f59b6e12122a47f844543
Michael Curran <mick@nvaccess.org>
2026-09-15 17:09:58 +1000

    Resolve lint in browse mode ancestor handling

--- source/browseMode.py
+++ source/browseMode.py
@@ -2181,14 +2181,13 @@ class BrowseModeDocumentTreeInterceptor(
 		if obj == self.rootNVDAObject:
 			self._enteringFromOutside = True
 		focusTreeInterceptor = api.getFocusObject().treeInterceptor
-		if focusTreeInterceptor is not self:
+		if focusTreeInterceptor is not self and (
+			not focusTreeInterceptor or not focusTreeInterceptor.passThrough
+		):
 			# #20753: Focus is outside this document (e.g. in an application), so our gainFocus
-			# handler won't run to replay these ancestor events,
-			# and therefore  we need to call the nextHandler immediately.
-			# Howevr, if the focused treeInterceptor is in focus mode, we should not call nextHandler immediately,
-			# because its own replay will walk all focus ancestors.
-			if not focusTreeInterceptor or not focusTreeInterceptor.passThrough:
-				return nextHandler()
+			# handler won't run to replay these ancestor events. Call nextHandler immediately
+			# unless the focused treeInterceptor is in focus mode and will replay all ancestors itself.
+			return nextHandler()
 		# For focus within this document, drop focusEntered events even if passThrough is enabled.
 		# In order to get them back when passThrough is enabled, we replay them with the _replayFocusEnteredEvents method in event_gainFocus.
 		# The reason for this is to ensure that focusEntered events are delayed until a focus event has had a chance to disable passthrough mode.
